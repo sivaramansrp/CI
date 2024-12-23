968863820802f1e798280773fe560e5bc5cbf86b
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2024-12-23 14:09:32 -0600

    ADD - Agrega la validacion de efirma

--- vucem-3.0-frontend/src/app/shared/components/firma-electronica/firma-electronica.component.ts
+++ vucem-3.0-frontend/src/app/shared/components/firma-electronica/firma-electronica.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 import * as forge from 'node-forge';
 
@@ -12,91 +13,94 @@ import * as forge from 'node-forge';
   styleUrl: './firma-electronica.component.scss',
 })
 export class FirmaElectronicaComponent {
-  cert_file: any = null;
-  key_file: any = null;
-  validation_message: string = '';
+  cert_file: string = '';
+  key_file: string = '';
+  datos_binarios!: ArrayBuffer;
+  mensaje_validacion: string = '';
+  contrasenia: string = '';
 
   FormCertificado = this.fb.group({
     password: ['', [Validators.required]],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private toastrService: ToastrService) {}
 
-  desencriptarKey(type: string, event: any): void {
-    const type_file = type === 'cer' ? 'cert_file' : 'key_file';
-    const file = event.target.files[0];
-    const reader = new FileReader();
+  handleFile(type: string, event: Event) {
+    const input = event.target as HTMLInputElement;
 
-    reader.onload = (e: any) => {
-      this[type_file] = e.target.result;
-    };
-    reader.readAsText(file);
-  }
-
-  convertToBase64(file: File, tipo: string) {
-    const reader = new FileReader();
-    reader.onload = (event: any) => {
-      const base64String = event.target.result;
-      if (tipo == 'cer') {
-        this.cert_file = base64String;
-      } else {
-        this.key_file = base64String;
-      }
-    };
-    reader.readAsDataURL(file);
-  }
-
-  onCertFileChange(event: any) {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      this.convertToBase64(selectedFile, 'cer');
+    if (input.files) {
+      const original_file = input.files[0];
+      const reader = new FileReader();
+      reader.onload = async (e: ProgressEvent<FileReader>) => {
+        if (e.target && e.target.result) {
+          if (type === 'cer') {
+            const result = (await e.target.result) as ArrayBuffer;
+            const der = new Uint8Array(result);
+            const buff = forge.util.createBuffer(der);
+            const asn1 = forge.asn1.fromDer(buff);
+            const cert = forge.pki.certificateFromAsn1(asn1);
+            const pem = forge.pki.certificateToPem(cert);
+            this.cert_file = pem;
+          }
+          if (type === 'key') {
+            this.datos_binarios = (await e.target.result) as ArrayBuffer;
+          }
+        }
+      };
+      reader.readAsArrayBuffer(original_file);
     }
   }
 
-  onKeyFileChange(event: any) {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      this.convertToBase64(selectedFile, 'key');
-    }
+  onSubmit() {
+    const password = this.FormCertificado.get('password')?.value;
+
+    this.contrasenia =
+      password !== undefined && password !== null ? password : '';
+
+    this.validateFilesBase(
+      this.cert_file,
+      this.datos_binarios,
+      this.contrasenia
+    );
   }
 
-  validarCertificado() {
+  validateFilesBase(
+    certFile: string,
+    binaryData: ArrayBuffer,
+    password: string
+  ): void {
     try {
-      const contrasenia = this.FormCertificado.get('password')?.value;
+      const cert = forge.pki.certificateFromPem(certFile);
+      const cert_public_key = cert.publicKey as forge.pki.rsa.PublicKey;
 
-      if (contrasenia) {
-        // Remove headers, footers, and whitespace
-        const cert1 = this.cert_file.split(',');
-        this.cert_file = cert1[1];
+      const padding_start = '-----BEGIN ENCRYPTED PRIVATE KEY-----\n';
+      const padding_end = '\n-----END ENCRYPTED PRIVATE KEY-----';
+      const der = new Uint8Array(binaryData);
+      const binary_string = String.fromCharCode(...der);
+      const content = padding_start + btoa(binary_string) + padding_end; // añadir paddings
+      const private_key = forge.pki.decryptRsaPrivateKey(content, password);
 
-        const cleanedCert = this.cert_file
-          .replace(/-----.*-----/g, '')
-          .replace(/\s+/g, '');
-        const certBuffer = forge.util.decode64(cleanedCert);
-        console.log(cleanedCert);
-        const asn1Cert = forge.asn1.fromDer(
-          forge.util.createBuffer(certBuffer)
+      if (private_key && cert_public_key) {
+        if (
+          cert_public_key.n.t === private_key.n.t &&
+          cert_public_key.e.t === private_key.e.t
+        ) {
+          this.toastrService.success(
+            '¡Certificado válido y llave privada coinciden!'
+          );
+        } else {
+          this.toastrService.error(
+            'La llave privada no coincide con el certificado o la contraseña es incorrecta.'
+          );
+        }
+      } else {
+        this.toastrService.error(
+          'La llave privada no coincide con el certificado o la contraseña es incorrecta.'
         );
-        const cert = forge.pki.certificateFromAsn1(asn1Cert);
-
-        // Convert to PEM format
-        this.cert_file = forge.pki.certificateToPem(cert);
-        console.log(this.cert_file);
-
-        // Convert Base64 to DER
-
-        // *** Conversion de key to PEM *** //
-
-        // if (privateKey) {
-        //   this.validation_message =
-        //     '¡Certificado válido y llave privada coinciden!';
-        // } else {
-        //   this.validation_message =
-        //     'Error: La llave privada no coincide con el certificado o la contraseña es incorrecta.';
-        // }
       }
-    } catch (e) {
-      console.log(e);
+    } catch (error) {
+      console.log(error);
+      this.toastrService.error('Error en la validación');
     }
   }
 }
