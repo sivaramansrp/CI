8b0319361f7c043c6e8edf59134607946dea4b06
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2024-12-30 11:49:36 -0600

    Agrega - Agrega cmponente agrga transporte

--- vucem-3.0-frontend/src/app/shared/components/agregar-transporte/agregar-transporte.component.ts
+++ vucem-3.0-frontend/src/app/shared/components/agregar-transporte/agregar-transporte.component.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  Form,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { SelectCatalogosComponent } from '../select-catalogos/select-catalogos.component';
 import { ServiciosExtraordinariosService } from './../../../core/services/5701/servicios-extraordinarios/servicios-extraordinarios.service';
 import { CatalogosSelect } from '../../../core/models/shared/components.model';
 import { TablaComponent } from '../tabla/tabla.component';
-
+import { Catalogo } from '../../../core/models/5701/catalogos.model';
+import * as CONSTANTES from '../../constantes/formularios-transportes.enums';
+import { CampoForm } from '../../../core/models/shared/forms-model';
+import { InputFechaComponent } from "../input-fecha/input-fecha.component";
 @Component({
   selector: 'agregar-transporte',
   standalone: true,
@@ -14,14 +23,54 @@ import { TablaComponent } from '../tabla/tabla.component';
     ReactiveFormsModule,
     SelectCatalogosComponent,
     TablaComponent,
-  ],
+    InputFechaComponent
+],
   templateUrl: './agregar-transporte.component.html',
   styleUrl: './agregar-transporte.component.scss',
 })
 export class AgregarTransporteComponent {
   datos_tipos_transporte!: CatalogosSelect;
 
-  constructor(private sExtraordinarios: ServiciosExtraordinariosService) {}
+  FormTransporte!: FormGroup;
+  tipo_transporte!: Catalogo;
+  campos_formulario!: Array<CampoForm>;
+
+  constructor(
+    private fb: FormBuilder,
+    private sExtraordinarios: ServiciosExtraordinariosService
+  ) {
+    this.crearFormTransporte();
+  }
+
+  crearFormTransporte() {
+    this.FormTransporte = this.fb.group({});
+  }
+
+  tipoTransporte(e: Catalogo) {
+    this.tipo_transporte = e;
+  }
+
+  crearFormulario() {
+    this.campos_formulario = CONSTANTES.CARRETERO;
+    console.log(this.campos_formulario);
+
+    this.agregarCamposAlForm(this.campos_formulario);
+    console.log(this.FormTransporte);
+  }
+
+  agregarCamposAlForm(campos: Array<CampoForm>) {
+    campos.forEach((campo: CampoForm) => {
+      this.FormTransporte.addControl(campo.campo, this.fb.control(''));
+    });
+
+    // Tipo de trasporte
+    // 1 - Carretero
+    // 2 - Ferroviario
+    // 3 - Aereo
+    // 4 - Maritimo
+    // 5 - Peatonal
+    // 6 - Otro
+  }
 
   ngOnInit() {
     this.getTiposTransporte();
---
+++ vucem-3.0-frontend/src/app/shared/constantes/formularios-transportes.enums.ts
@@ -0,0 +1,101 @@
+export const CARRETERO = [
+  {
+    label_nombre: 'Empresa transportista',
+    campo: 'emp_transportista',
+    class: 'col-md-6',
+    tipo_input: 'text'
+  },
+  {
+    label_nombre: 'Número carta porte',
+    campo: 'n_carta_porte',
+    class: 'col-md-6',
+    tipo_input: 'text'
+  },
+  {
+    label_nombre: 'Fecha carta porte',
+    campo: 'f_carta_porte',
+    class: 'col-md-6',
+    tipo_input: 'date'
+  },
+  {
+    label_nombre: 'Marca',
+    campo: 'marca',
+    class: 'col-md-6',
+    tipo_input: 'text'
+
+  },
+  {
+    label_nombre: 'Modelo',
+    campo: 'modelo',
+    class: 'col-md-6',
+    tipo_input: 'select'
+  },
+  {
+    label_nombre: 'Placas',
+    campo: 'placas',
+    class: 'col-md-6',
+    tipo_input: 'text'
+  },
+  {
+    label_nombre: 'Contenerdor(es)',
+    campo: 'contenedor',
+    class: 'col-md-12',
+    tipo_input: 'textarea'
+  },
+  {
+    label_nombre: 'Observaciones',
+    campo: 'observaciones',
+    class: 'col-md-12',
+    tipo_input: 'textarea'
+  },
+];
+
+export const FERROVIARIO = [
+  'emp_transportitsa',
+  'n_carta_porte',
+  'f_carta_porte',
+  'marca',
+  'modelo',
+  'placas',
+  'contenedor',
+];
+
+export const PEATONAL = [
+  'emp_transportitsa',
+  'n_carta_porte',
+  'f_carta_porte',
+  'marca',
+  'modelo',
+  'placas',
+  'contenedor',
+];
+
+export const AEREO = [
+  'emp_transportitsa',
+  'n_carta_porte',
+  'f_carta_porte',
+  'marca',
+  'modelo',
+  'placas',
+  'contenedor',
+];
+
+export const MARITIMO = [
+  'emp_transportitsa',
+  'n_carta_porte',
+  'f_carta_porte',
+  'marca',
+  'modelo',
+  'placas',
+  'contenedor',
+];
+
+export const OTRO = [
+  'emp_transportitsa',
+  'n_carta_porte',
+  'f_carta_porte',
+  'marca',
+  'modelo',
+  'placas',
+  'contenedor',
+];
