e58782a1291cd93462136462210665500b43e975
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-01-03 08:27:08 -0600

    ADD - Agrega componente de login

--- vucem-3.0-frontend/src/app/app-routing.module.ts
+++ vucem-3.0-frontend/src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '**', pathMatch: 'full', redirectTo: 'servicios-extraordinarios'},
+  { path: '', pathMatch: 'full', redirectTo: '/auth/login' },
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
   {
     path: 'servicios-extraordinarios',
-    loadChildren: () => import('./tramites/5701/servicios-extraordinarios.module').then(m => m.ServiciosExtraordinariosModule)
-  }
+    loadChildren: () =>
+      import('./tramites/5701/servicios-extraordinarios.module').then(
+        (m) => m.ServiciosExtraordinariosModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ vucem-3.0-frontend/src/app/shared/constantes/constantes.ts
@@ -0,0 +1,7 @@
+export const COOKIE = {
+  NOMBRE_COOKIE_ID_USUARIO: 'sg-id-user',
+  NOMBRE_COOKIE_ROL: 'sg-rol',
+  NOMBRE_COOKIE_TOKEN: 'sg-token',
+  NOMBRE_COOKIE_USUARIO: 'sg-usuario',
+  NOMBRE_COOKIE_RFC: 'sg-rfc',
+}
