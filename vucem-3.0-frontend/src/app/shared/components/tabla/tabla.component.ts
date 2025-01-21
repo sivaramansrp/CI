566187b287cb361639e0fcbdda8e5dd34de2a2d8
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-01-21 17:56:33 -0600

    Agrega acciones a la tabla dinamica

---
+++ vucem-3.0-frontend/src/app/shared/components/tabla/tabla.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { AccionesTabla, ConfiguracionTabla, EncabezadosTabla } from '../../../core/models/shared/components.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ng-tabla',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tabla.component.html',
+  styleUrl: './tabla.component.scss'
+})
+export class TablaComponent {
+  @Input({required: true}) encabezadosTabla!: EncabezadosTabla[];
+  @Input({required: true}) datosTabla!: ConfiguracionTabla[];
+  @Input({required: true}) accionesTabla!: AccionesTabla[];
+
+  onAccion() : void {
+
+  }
+}
--- vucem-3.0-frontend/src/app/tramites/5701/pages/acuse-page/acuse-page.component.ts
+++ vucem-3.0-frontend/src/app/tramites/5701/pages/acuse-page/acuse-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { TXT_TITULO } from '../../../../shared/constantes/servicios-extraordinarios.enum';
+import { AccionesTabla, ConfiguracionTabla, EncabezadosTabla } from '../../../../core/models/shared/components.model';
 
 @Component({
   templateUrl: './acuse-page.component.html',
@@ -8,6 +9,35 @@ import { TXT_TITULO } from '../../../../shared/constantes/servicios-extraordinar
 export class AcusePageComponent {
   folio = '123456';
   txtTitulo = `${TXT_TITULO} <${this.folio}>`;
+
+  encabezadoTablaAcuses: EncabezadosTabla[] = [
+    {
+      key: 'id',
+      valor: 'No.'
+    },
+    {
+      key: 'documento',
+      valor: 'Documento.'
+    },
+  ];
+
+
+
+  datosTablaAcuses : ConfiguracionTabla[] = [
+    {
+      id: 1,
+      documento: 'Acuse de recepción de trámite',
+    },
+
+  ]
+
+  acciones: AccionesTabla[] = [
+    {
+      tipo: 'descargar',
+      label: 'Descargar',
+      icono: 'bi bi-arrow-bar-down bi-c'
+    }
+  ]
 
 
 }
