fac179b923d0d7a81120da493e870517042d1d03
Vinothini P <vinothini.p@ilink-systems.com>
2025-01-24 18:34:01 +0530

    component creation

---
+++ vucem-3.0-frontend/src/app/tramites/220401/component/unidad/unidad.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-unidad',
+  templateUrl: './unidad.component.html',
+  styleUrl: './unidad.component.scss'
+})
+export class UnidadComponent {
+
+}
