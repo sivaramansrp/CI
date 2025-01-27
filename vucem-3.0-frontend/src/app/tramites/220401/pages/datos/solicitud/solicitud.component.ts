4d75f197a1011ef37e0795072c9a0313097ba6b3
Moses Ramadoss <moses.ramadoss@ilink-systems.com>
2025-01-27 12:40:40 +0530

    'Refs#220401-Listerner-added'

--- vucem-3.0-frontend/src/app/tramites/220401/pages/datos/solicitud/solicitud.component.ts
+++ vucem-3.0-frontend/src/app/tramites/220401/pages/datos/solicitud/solicitud.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiciosPantallasService } from '../../../../../core/services/220471/servicios-pantallas.service';
+import { ReplaySubject, takeUntil } from 'rxjs';
+import { PantallasFormData } from '../../../../../core/models/220401/servicios-pantallas.model';
 
 @Component({
   selector: 'app-solicitud',
@@ -6,6 +9,27 @@ import { Component } from '@angular/core';
   standalone: true,
   styleUrl: './solicitud.component.scss'
 })
-export class SolicitudComponent {
+export class SolicitudComponent implements OnInit {
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  public pantallasFormData!: PantallasFormData;
 
+  constructor(private serviciosPantallasService: ServiciosPantallasService) {}
+  public ngOnInit(): void {
+    // Subscribe/ listener the form data
+    this.serviciosPantallasService.pantallasFormObservable$
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((formData: PantallasFormData) => {
+        this.pantallasFormData = formData;
+        console.log('Form data', this.pantallasFormData);
+      });
+      // Set the form data
+      this.pantallasFormData['solict'] = [];
+      this.serviciosPantallasService.setPantallasFormDataSubject(this.pantallasFormData);
+  }
+
+  // Unsubscribe the subscription
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
 }
