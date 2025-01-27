f6c37ec04a0bc83872bc13d5b5b3510443030e6a
binaydas8 <binaykumar.das@ilink-systems.com>
2025-01-27 13:06:21 +0530

    added reusable component
    added reusable component

--- vucem-3.0-frontend/src/app/tramites/220401/pages/datos/combinacion-requerida/combinacion-requerida.component.ts
+++ vucem-3.0-frontend/src/app/tramites/220401/pages/datos/combinacion-requerida/combinacion-requerida.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { TituloComponent } from '../../../../../shared/components/titulo/titulo.component';
 
 @Component({
   selector: 'app-combinacion-requerida',
   templateUrl: './combinacion-requerida.component.html',
   styleUrl: './combinacion-requerida.component.scss',
   standalone: true,
+  imports: [TituloComponent],
 })
 export class CombinacionRequeridaComponent {
 
