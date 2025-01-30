f3b00134a883c2a590fdfd21e0c7c3be9de3060b
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-01-30 17:28:04 +0530

    Enable justification selection and refactor form handling in datos-de-la-solicitud component

--- vucem-3.0-frontend/src/app/tramites/220201/components/datos-de-la-solicitud/datos-de-la-solicitud.component.ts
+++ vucem-3.0-frontend/src/app/tramites/220201/components/datos-de-la-solicitud/datos-de-la-solicitud.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TEXTOS } from '../../../../shared/constantes/issuance-extension-modification.enum';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'datos-de-la-solicitud',
@@ -9,10 +9,35 @@ import { FormBuilder } from '@angular/forms';
 })
 export class DatosDeLaSolicitudComponent {
   TEXTOS: string = TEXTOS;
+  forma!: FormGroup;
   selectRangoDias: Array<string> = [];
   colapsable: boolean = false;
+  datosDelaSolicitud!: FormGroup;
   constructor(private readonly fb: FormBuilder) {
+    this.crearFormulario();
+    this.initActionFormBuild();
+  }
+  crearFormulario(): void {
+    this.forma = this.fb.group({
+      datosDelaSolicitud: this.fb.group({}),
 
+    });
+  }
+  initActionFormBuild() {
+    this.datosDelaSolicitud = this.fb.group({
+      aduanaIngreso: ['', Validators.required],
+      oficinaInspeccion: ['', Validators.required],
+      puntoInspeccion: ['', Validators.required],
+      claveUCON: ['', [Validators.required]],
+      establecimientoTIF: ['', Validators.required],
+      nombreVeterinario: ['', Validators.required],
+      numeroGuia: [''],
+      certficacion: [''],
+      regimen: ['', Validators.required]
+    });
+
+    this.forma.setControl('datosDelaSolicitud', this.datosDelaSolicitud);
+    console.log(this.forma);
   }
 
   mostrar_colapsable() {
