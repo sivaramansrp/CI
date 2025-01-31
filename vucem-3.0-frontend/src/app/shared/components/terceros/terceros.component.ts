99ea82c52fc914860ea08da77eaf0425d423d4fb
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-01-31 09:07:29 -0600

    Corrige terceros relacionados

--- vucem-3.0-frontend/src/app/shared/components/terceros/terceros.component.ts
+++ vucem-3.0-frontend/src/app/shared/components/terceros/terceros.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PersonaTerceros } from '../../../core/models/5701/servicios-extraordinarios.model';
 import { CONSTANTES } from '../../constantes/servicios-extraordinarios.enum';
-import { SeccionQuery } from '../../../core/queries/seccion.query';
-import { SeccionState, SeccionStore } from '../../../estados/seccion.store';
-import { delay, distinctUntilChanged, map, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'terceros',
@@ -22,56 +19,11 @@ export class TercerosComponent {
 
   personas: Array<PersonaTerceros> = [];
 
-  private destroyNotifier$: Subject<void> = new Subject();
-  private seccion: SeccionState;
 
   constructor(
     private fb: FormBuilder,
-    private seccionQuery: SeccionQuery,
-    private seccionStore: SeccionStore
   ) {}
 
-  ngOnInit(): void {
-    this.seccionQuery.selectSeccionState$
-      .pipe(
-        takeUntil(this.destroyNotifier$),
-        map((seccionState) => {
-          this.seccion = seccionState;
-        })
-      )
-      .subscribe();
-    this.FormPersona.valueChanges.pipe(
-      distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
-      takeUntil(this.destroyNotifier$),
-      delay(10),
-      tap((value) => {
-        const secciones = this.seccion.seccion;
-
-        const formasValidadas = this.seccion.formaValida;
-        for (let i = 0; i < this.seccion.seccion.length; i++) {
-          if (
-            this.seccion.seccion[i] === true &&
-            this.seccion.formaValida[i] === false
-          )
-            formasValidadas[i] = true;
-          break;
-        }
-        const seccionSinValidar = secciones.findIndex(
-          (seccion) => seccion === true
-        );
-
-
-        if (this.FormPersona.valid) {
-          formasValidadas[seccionSinValidar] = true;
-          this.seccionStore.establecerFormaValida(formasValidadas);
-        } else {
-          formasValidadas[seccionSinValidar] = false;
-          this.seccionStore.establecerFormaValida(formasValidadas);
-        }
-      })
-    )
-    .subscribe();
-  }
 
   agregaPersona(): void {
     if (this.personas.length < 5 && this.FormPersona.valid) {
--- vucem-3.0-frontend/src/app/shared/constantes/seccionesTramites.ts
+++ vucem-3.0-frontend/src/app/shared/constantes/seccionesTramites.ts
@@ -5,7 +5,7 @@ export const SECCIONES_TRAMITE_5701 = {
     PASO_1: {
         VALIDACION_SECCION_1: false,
         VALIDACION_SECCION_2: true,
-        VALIDACION_SECCION_3: true,
+        VALIDACION_SECCION_3: false,
     },
     PASO_2: {
         VALIDACION_SECCION: true,
