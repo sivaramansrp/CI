fd1e554368f2f4bcc694cf79e3ea5e99dfd09bdd
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-03 16:36:05 +0530

    Added router and module folder

--- vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
@@ -1,9 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { FitosanitarioRoutingModule } from './fitosanitario-routing.module';
-
-
+import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [],
   imports: [
