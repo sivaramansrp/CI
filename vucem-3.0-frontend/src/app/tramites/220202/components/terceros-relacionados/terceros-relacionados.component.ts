14a9e2e4af880458e0fb143ba3a8aab220af709e
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-03 16:53:29 +0530

    Add components for solicitante, pago-de-derechos, datos-de-la-solicitud, terceros-relacionados, and datos-para-movilizacion-nacional

---
+++ vucem-3.0-frontend/src/app/tramites/220202/components/terceros-relacionados/terceros-relacionados.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terceros-relacionados',
+  templateUrl: './terceros-relacionados.component.html',
+  styleUrl: './terceros-relacionados.component.scss'
+})
+export class TercerosRelacionadosComponent {
+
+}
--- vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
@@ -1,11 +1,17 @@
 import { AgriculturaComponent } from './pages/agricultura/agricultura.component';
 import { CommonModule } from '@angular/common';
+import { DatosDeLaSolicitudComponent } from './components/datos-de-la-solicitud/datos-de-la-solicitud.component';
+import { DatosParaMovilizacionNacionalComponent } from './components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component';
 import { FitosanitarioRoutingModule } from './fitosanitario-routing.module';
 import { NgModule } from '@angular/core';
+import { PagoDeDerechosComponent } from './components/pago-de-derechos/pago-de-derechos.component';
 import { PasoCuatroComponent } from './pages/paso-cuatro/paso-cuatro.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { SolicitanteComponent } from './components/solicitante/solicitante.component';
+import { TercerosRelacionadosComponent } from './components/terceros-relacionados/terceros-relacionados.component';
+
 
 
 @NgModule({
@@ -14,7 +20,12 @@ import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
     PasoTresComponent,
     PasoDosComponent,
     PasoCuatroComponent,
-    AgriculturaComponent
+    AgriculturaComponent,
+    SolicitanteComponent,
+    DatosDeLaSolicitudComponent,
+    DatosParaMovilizacionNacionalComponent,
+    TercerosRelacionadosComponent,
+    PagoDeDerechosComponent
   ],
   imports: [
     CommonModule,
