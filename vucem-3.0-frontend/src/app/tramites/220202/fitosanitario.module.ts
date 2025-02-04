eee622dd13f00bbfa881558eb482cf10c0e8496d
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-04 13:09:44 +0530

    Refactor Fitosanitario module to include additional shared components and routing setup; remove unused agricultura component template

--- vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
@@ -1,4 +1,6 @@
 import { AgriculturaComponent } from './pages/agricultura/agricultura.component';
+import { AlertComponent } from '../../shared/components/alert/alert.component';
+import { AnexarDocumentosComponent } from '../../shared/components/anexar-documentos/anexar-documentos.component';
 import { CommonModule } from '@angular/common';
 import { DatosDeLaSolicitudComponent } from './components/datos-de-la-solicitud/datos-de-la-solicitud.component';
 import { DatosParaMovilizacionNacionalComponent } from './components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component';
@@ -11,6 +13,19 @@ import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { SolicitanteComponent } from './components/solicitante/solicitante.component';
 import { TercerosRelacionadosComponent } from './components/terceros-relacionados/terceros-relacionados.component';
+
+import { BtnContinuarComponent } from '../../shared/components/btn-continuar/btn-continuar.component';
+import { CrosslistComponent } from '../../shared/components/crosslist/crosslist.component';
+import { InputCheckComponent } from '../../shared/components/input-check/input-check.component';
+import { InputFechaComponent } from '../../shared/components/input-fecha/input-fecha.component';
+import { IssuanceExtensionModificationRoutingModule } from '../220201/issuance-extension-modification-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SelectCatalogosComponent } from '../../shared/components/select-catalogos/select-catalogos.component';
+import { TituloComponent } from '../../shared/components/titulo/titulo.component';
+import { WizardComponent } from '../../shared/components/wizard/wizard.component';
+
+import { FirmaElectronicaComponent } from '../../shared/components/firma-electronica/firma-electronica.component';
+
 
 
 
@@ -29,7 +44,19 @@ import { TercerosRelacionadosComponent } from './components/terceros-relacionado
   ],
   imports: [
     CommonModule,
-    FitosanitarioRoutingModule
+    FitosanitarioRoutingModule,
+    IssuanceExtensionModificationRoutingModule,
+    WizardComponent,
+    TituloComponent,
+    BtnContinuarComponent,
+    ReactiveFormsModule,
+    CrosslistComponent,
+    InputCheckComponent,
+    SelectCatalogosComponent,
+    AlertComponent,
+    InputFechaComponent,
+    AnexarDocumentosComponent,
+    FirmaElectronicaComponent
   ]
 })
 export class FitosanitarioModule { }
