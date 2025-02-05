022d10ca76771d4cb170a2631c0ef89b45dd7ae2
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-05 13:17:27 +0530

    Fix typos in instruction texts for issuance extension

--- vucem-3.0-frontend/src/app/shared/constantes/módulodemodificacióndeextensióndeemisión.enum.ts
+++ vucem-3.0-frontend/src/app/shared/constantes/módulodemodificacióndeextensióndeemisión.enum.ts
@@ -26,7 +26,7 @@ export const PASOS = [
 ];
 export const TEXTOS_REQUISITOS = {
     INSTRUCCIONES: `<h6>Instrucciones</h6>
-    <p>- De acuerdo al caso particular, algunos documentos podrían ser obligatorios</p>
+    <p>- De acuerdo al caso particular, algunos dcumentos podrían ser obligatorios</p>
     <p>- En caso de que no requieras algún documento, seleccionalo y elíminalo</p>
     <p>- Si necesitas anexar más de un documento del mismo tipo selecciónalo de la lista y presiona "Agregar nuevo".</p>`,
     ADJUNTAR: `<p>Si deseas adjuntar un nuevo documento, selecciona la opción --Adjuntar nuevo documento-- y presiona el botón "Adjuntar dcumentos"</p>`,
