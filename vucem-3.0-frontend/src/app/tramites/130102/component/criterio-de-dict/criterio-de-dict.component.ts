905d3dd05b7c6bc6cce36a4b5c5de278f257fc39
atripathi <amit.tripathi@ilink-systems.com>
2025-02-05 15:37:25 +0530

    RepresentacionComponent and CriterioDeDictComponent

--- vucem-3.0-frontend/src/app/tramites/130102/component/criterio-de-dict/criterio-de-dict.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130102/component/criterio-de-dict/criterio-de-dict.component.ts
@@ -1,12 +1,38 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Catalogo } from '../../../../core/models/shared/catalogos.model';
+import { CatalogosSelect } from '../../../../core/models/shared/components.model';
 import { Component } from '@angular/core';
+import { SelectCatalogosComponent } from '../../../../shared/components/select-catalogos/select-catalogos.component';
+import { TituloComponent } from "../../../../shared/components/titulo/titulo.component";
 
 @Component({
   selector: 'app-criterio-de-dict',
   standalone: true,
-  imports: [],
+  imports: [TituloComponent, SelectCatalogosComponent],
   templateUrl: './criterio-de-dict.component.html',
   styleUrl: './criterio-de-dict.component.scss'
 })
 export class CriterioDeDictComponent {
+
+  frmCriterioDict!: FormGroup;
+
+  solicitudMercancia: CatalogosSelect = {
+    labelNombre: 'Solicitud mercancia esquema regla octava clave',
+    required: true,
+    primerOpcion: 'Seleccione una aduana',
+    catalogos: [
+      { id: 1, descripcion: 'La SE autorizará la importación de mercancías de la Regla 8a, cuando se' },
+      { id: 2, descripcion: 'Aduana 2' },
+      { id: 3, descripcion: 'Aduana 3' }
+    ]
+  };
+
+  selectedSolicitudMercancia: Catalogo = { id: 0, descripcion: '' };
+
+  constructor(private fb: FormBuilder) {}
+
+  solicitudMercanciaSeleccion(e: Catalogo): void {
+    this.selectedSolicitudMercancia = e;
+  }
 
 }
--- vucem-3.0-frontend/src/app/tramites/130102/component/representacion/representacion.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130102/component/representacion/representacion.component.ts
@@ -1,12 +1,55 @@
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Catalogo } from '../../../../core/models/shared/catalogos.model';
+import { CatalogosSelect } from '../../../../core/models/shared/components.model';
 import { Component } from '@angular/core';
+import { SelectCatalogosComponent } from '../../../../shared/components/select-catalogos/select-catalogos.component';
+import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
 
 @Component({
   selector: 'app-representacion',
   standalone: true,
-  imports: [],
+  imports: [TituloComponent, SelectCatalogosComponent, ReactiveFormsModule],
   templateUrl: './representacion.component.html',
   styleUrl: './representacion.component.scss'
 })
 export class RepresentacionComponent {
+
+  frmRepresentacion!: FormGroup;
+
+  entidads: CatalogosSelect = {
+    labelNombre: 'Entidad federativa',
+    required: true,
+    primerOpcion: 'Seleccione una entidad federativa',
+    catalogos: [
+      { id: 1, descripcion: 'Sinaloa' },
+      { id: 2, descripcion: 'Entidad federativa 2' },
+      { id: 3, descripcion: 'Entidad federativa 3' }
+    ]
+  };
+
+  representacions: CatalogosSelect = {
+    labelNombre: 'Representación federal',
+    required: true,
+    primerOpcion: 'Seleccione una representación federal',
+    catalogos: [
+      { id: 1, descripcion: 'Culican' },
+      { id: 2, descripcion: 'Representación federal 2' },
+      { id: 3, descripcion: 'Representación federal 3' }
+    ]
+  };
+
+  selectedEntidad: Catalogo = { id: 0, descripcion: '' };
+  
+  selectedRepresentacion: Catalogo = { id: 0, descripcion: '' };
+
+  constructor(private fb: FormBuilder) {}
+
+  entidadSeleccion(entidad: Catalogo): void {
+    this.selectedEntidad = entidad;
+  }
+
+  representacionSeleccion(representacion: Catalogo): void {
+    this.selectedRepresentacion = representacion;
+  }
 
 }
