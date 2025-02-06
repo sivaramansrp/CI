399d24361c46c3b9bafb3067d8f9a153ce5bb83d
Vinothini P <vinothini.p@ilink-systems.com>
2025-02-06 18:00:14 +0530

    validation and resposive

--- vucem-3.0-frontend/src/app/tramites/110101/components/solicitante/solicitante.component.ts
+++ vucem-3.0-frontend/src/app/tramites/110101/components/solicitante/solicitante.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
+
 
 @Component({
   selector: 'app-solicitante',
   templateUrl: './solicitante.component.html',
   styleUrl: './solicitante.component.scss',
   standalone: true,
-  imports:[TituloComponent]
+  imports:[TituloComponent,ReactiveFormsModule]
 })
-export class SolicitanteComponent {
+export class SolicitanteComponent implements OnInit {
+  constructor(private fb: FormBuilder) { }
 
+  /**
+   * Grupo de formulario para el formulario de solicitud.
+   */
+  solicitudForm!: FormGroup;
+
+ /**
+ * Datos simulados que representan a un solicitante con varios atributos.
+ * 
+ * @property {string} rfc - El RFC (Registro Federal de Contribuyentes) del solicitante.
+ * @property {string} denominacion - El nombre o denominación del negocio del solicitante.
+ * @property {string} actividadEconomica - La actividad económica o sector empresarial del solicitante.
+ * @property {string} correoElectronico - La dirección de correo electrónico del solicitante.
+ */
+  mockData = {
+    rfc: 'AAL0409235E6',
+    denominacion: 'AGRICOLA ALPE S DE RL DE CV',
+    actividadEconomica: 'Siembra, cultivo y cosecha de papa',
+    correoElectronico: 'vucem2.5@hotmail.com'
+  };
+  
+  ngOnInit(): void {
+    this.solicitudForm = this.fb.group({
+      rfc: this.mockData.rfc,
+      denominacion: this.mockData.denominacion,
+      actividadEconomica: this.mockData.actividadEconomica,
+      correoElectronico: this.mockData.correoElectronico
+    });
+  }
 }
--- vucem-3.0-frontend/src/app/tramites/110101/components/tratados/tratados.component.ts
+++ vucem-3.0-frontend/src/app/tramites/110101/components/tratados/tratados.component.ts
@@ -1,36 +1,100 @@
-import { Component } from '@angular/core';
-import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
-
-import { SelectCatalogosComponent } from '../../../../shared/components/select-catalogos/select-catalogos.component';
-
+import { AlertComponent } from '../../../../shared/components/alert/alert.component';
+import { Catalogo } from '../../../../core/models/shared/catalogos.model';
 import { CommonModule } from '@angular/common';
-import tratadosDropdown from '../../../../../assets/json/110101/tratdos-dropdown.json';
-
+import { Component } from '@angular/core';
+import { SelectCatalogosComponent } from '../../../../shared/components/select-catalogos/select-catalogos.component';
 import { TableComponent } from '../../../../shared/components/table/table.component';
+import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
+import tratadosDropdown from '../../../../../assets/json/110101/tratdos-dropdown.json';
 
 @Component({
   selector: 'app-tratados',
   templateUrl: './tratados.component.html',
   styleUrl: './tratados.component.scss',
   standalone: true,
-  imports:[TituloComponent, SelectCatalogosComponent,CommonModule,TableComponent]
+  imports: [
+    TituloComponent,
+    SelectCatalogosComponent,
+    CommonModule,
+    TableComponent,
+    AlertComponent
+  ]
 })
 export class TratadosComponent {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  selectedValues: any = {};
 
+/**
+ * Configuración para menús desplegables.
+ * 
+ * @property {Object[]} dropdownConfigs - Array de configuraciones de menús desplegables.
+ * @property {string} dropdownConfigs[].labelNombre - Etiqueta para el menú desplegable.
+ * @property {boolean} dropdownConfigs[].required - Indica si el menú desplegable es obligatorio.
+ * @property {Object[]} dropdownConfigs[].catalogos - Catálogo de opciones para el menú desplegable.
+ * @property {string} dropdownConfigs[].primerOpcion - Primera opción en el menú desplegable.
+ */
   dropdownConfigs = [
     { labelNombre: 'Pais o bloque', required: true, catalogos: tratadosDropdown.pais, primerOpcion: '' },
     { labelNombre: 'Tratado o Acuerdo', required: true, catalogos: tratadosDropdown.tratado, primerOpcion: '' },
     { labelNombre: 'Criterio de origen', required: true, catalogos: tratadosDropdown.origen, primerOpcion: '' }
   ];
 
-    seleccionar(){
-      console.log('Seleccionar');
+  /**
+    * Maneja la selección de los menús desplegables.
+    * 
+    * @param {Catalogo} event - El elemento seleccionado del catálogo.
+    * @param {number} index - El índice del menú desplegable.
+    */
+  seleccionar(event: Catalogo, index: number) {
+    if (index === 0) this.selectedValues.pais = event;
+    if (index === 1) this.selectedValues.tratado = event;
+    if (index === 2) this.selectedValues.origen = event;
+  }
+
+  /**
+  * Encabezados comunes de la tabla.
+  * 
+  * @property {string[]} commonTableHeaders - Array de encabezados de tabla.
+  */
+  commonTableHeaders = ['Pais o bloque', 'Tratado o Acuerdo', 'Criterio de origen'];
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  mercanciasData: any[] = [];
+
+ /**
+  * Agrega un nuevo tratado al array mercanciasData.
+  */
+  agregarTratado() {
+    if (this.selectedValues.pais && this.selectedValues.tratado && this.selectedValues.origen) {
+      this.mercanciasData.push({
+        tbodyData: [
+          this.selectedValues.pais.descripcion,
+          this.selectedValues.tratado.descripcion,
+          this.selectedValues.origen.descripcion
+        ]
+      });
+
+      this.selectedValues = {};
     }
-  
-    commonTableHeaders = ['Pais o bloque', 'Tratado o Acuerdo', 'Criterio de origen'];
-     mercanciasData = [
-      {
-        tbodyData: ['Establecimiento 1','123-456-7890','Calle 123'],
-      }
-    ]
+  }
+
+  /**
+   * A string containing HTML markup for an alert message.
+   * The message provides instructions for selecting a treaty and criteria.
+   * 
+   * Instructions:
+   * - First, select a country or block.
+   * - Then, select a treaty or agreement.
+   * - Select the criteria to confer origin.
+   * - Finally, add your selection to the list of treaties.
+   */
+  mensajeAlerta: string = `
+  <ul>
+    Para seleccionar un tratado y criterio, siga los siguientes pasos:
+    <li>Primero seleccione un país o bloque</li>
+    <li>Después seleccione un tratado o acuerdo</li>
+    <li>Seleccione el criterio para conferir origen</li>
+    <li>Finalmente, agregue su selección a la lista de tratados</li>
+  </ul>
+`;
 }
