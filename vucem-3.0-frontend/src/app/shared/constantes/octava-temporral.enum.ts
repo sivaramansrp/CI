cea80466d51bdaa287c8d3989c8823ba4eb03417
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-02-06 10:48:52 +0530

    fix: Compeleted the form design Partidas de la mercancía

---
+++ vucem-3.0-frontend/src/app/shared/constantes/octava-temporral.enum.ts
@@ -0,0 +1,6 @@
+export const TEXTOS = {
+    INSTRUCCIONES: `
+    <p>Debes capturar una fraccion arrancelaria en el campo de texto para que se realice la busqueda y se carguen los</p>
+    <p>datos del catalogo del componente de la lista despegable</p>
+    `,
+  };
--- vucem-3.0-frontend/src/app/tramites/130102/component/partidas-de-la/partidas-de-la.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130102/component/partidas-de-la/partidas-de-la.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
+import { UppercaseDirective } from '../../../../shared/directives/Uppercase/uppercase.directive';
+import { AlertComponent } from '../../../../shared/components/alert/alert.component';
+import { SelectCatalogosComponent } from '../../../../shared/components/select-catalogos/select-catalogos.component';
+import { TEXTOS } from '../../../../shared/constantes/octava-temporral.enum';
+import { CatalogosSelect } from '../../../../core/models/shared/components.model';
 
 @Component({
   selector: 'app-partidas-de-la',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TituloComponent,
+    UppercaseDirective,
+    AlertComponent,
+    SelectCatalogosComponent
+  ],
   templateUrl: './partidas-de-la.component.html',
   styleUrl: './partidas-de-la.component.scss'
 })
-export class PartidasDeLaComponent {
+export class PartidasDeLaComponent implements OnInit {
+  form!: FormGroup;
+  TEXTOS = TEXTOS;
+  fraccionArancelariaTIGIE: CatalogosSelect = {
+    labelNombre: '',
+    required: false,
+    primerOpcion: 'Seleccione una Fraccion Arancelaria TIGIE',
+    catalogos: [
+      { id: 1, descripcion: '98020018 Mercancias para el Pro' },
+      { id: 2, descripcion: '98020019 Mercancias para el Pro' },
+      { id: 3, descripcion: '98020020 Mercancias para el Pro' }
+    ]
+  };
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.crearFormulario();
+  }
+
+  crearFormulario(): void {
+    this.form = this.fb.group({
+      cantidad: ['', Validators.required],
+      fraccionArancelariaTIGIE: [''],
+      descripcion: ['', Validators.required],
+      valorPartidaUSD: ['', [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  fraccionArancelariaTIGIESelection(aduana: any) {
+    // Handle selection logic here
+  }
 }
