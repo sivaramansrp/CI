0d32af224569749d36249f26f3077c5f1d978a2c
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-07 15:54:03 +0530

    Add personal data fields and buttons to terceros-relacionados component; enhance form structure and functionality

--- vucem-3.0-frontend/src/app/tramites/220202/components/terceros-relacionados/terceros-relacionados.component.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/components/terceros-relacionados/terceros-relacionados.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { ESREQUIREDTABLESMENSAJE } from '../../../../shared/constantes/220202/fitosanitario.enums';
 
+import { CatalogosSelect } from '../../../../core/models/shared/components.model';
+
 @Component({
   selector: 'app-terceros-relacionados',
   templateUrl: './terceros-relacionados.component.html',
   styleUrl: './terceros-relacionados.component.scss'
 })
 export class TercerosRelacionadosComponent {
-  esRequiredTablesMensaje: string = ESREQUIREDTABLESMENSAJE
+  esRequiredTablesMensaje: string = ESREQUIREDTABLESMENSAJE;
+  pairsList: CatalogosSelect = {
+    labelNombre: 'País',
+    required: true,
+    primerOpcion: 'Selecciona un valor',
+    catalogos: []
+  }
 }
