f49e4b2b4777f89ea66a6f8a9aa2917784479d44
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-02-07 11:03:54 +0530

    worked on the 130102 Datos de la solicitute
    worked on the 130102 Datos de la solicitute

--- vucem-3.0-frontend/src/app/tramites/130102/component/detos-gen-del/detos-gen-del.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130102/component/detos-gen-del/detos-gen-del.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SolicitanteComponent } from "../../../5701/components/solicitante/solicitante.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-detos-gen-del',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    SolicitanteComponent
+],
   templateUrl: './detos-gen-del.component.html',
   styleUrl: './detos-gen-del.component.scss'
 })
-export class DetosGenDelComponent {
+export class DetosGenDelComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+   
+  }
 
 }
