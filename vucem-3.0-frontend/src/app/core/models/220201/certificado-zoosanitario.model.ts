44ea0e9d11b52dda5a4129957a5ed5ab3815d55d
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-10 12:30:14 +0530

    Replace old model with new certificado-zoosanitario model and interfaces

---
+++ vucem-3.0-frontend/src/app/core/models/220201/certificado-zoosanitario.model.ts
@@ -0,0 +1,45 @@
+/**
+ * @fileoverview Archivo principal del módulo. Contiene las interfaces y tipos de datos
+ * utilizados en el módulo.
+ * @module shared
+ */
+
+/**
+ * Interfaz para representar un paso en un asistente (wizard).
+ * @interface ListaPasosWizard
+ * @property {number} indice - El índice del paso.
+ * @property {string} titulo - El título del paso.
+ * @property {boolean} activo - Indica si el paso está activo.
+ * @property {boolean} completado - Indica si el paso se ha completado.
+ */
+export interface ListaPasosWizard {
+    indice: number;
+    titulo: string;
+    activo: boolean;
+    completado: boolean;
+}
+
+/**
+ * Interfaz genérica para representar la respuesta de una API.
+ * @interface RespuestaAPI
+ * @template T - El tipo de datos que se esperan en la respuesta.
+ * @property {number} code - El código de respuesta de la API.
+ * @property {T} data - Los datos de la respuesta.
+ * @property {string} message - Un mensaje descriptivo de la respuesta.
+ */
+export interface RespuestaAPI<T> {
+    code: number;
+    data: T; // Cambiado a T para mayor flexibilidad
+    message: string;
+}
+
+/**
+ * Interfaz para representar un banco.
+ * @interface Banco
+ * @property {number} id - El identificador único del banco.
+ * @property {string} value - El nombre o valor del banco. --220201
+ */
+export interface Banco {
+    id: number;
+    value: string;
+}
