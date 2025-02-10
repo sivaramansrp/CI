786ffaee81c057d29242321aec37f0d595275527
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-10 13:03:16 +0530

    Remove unnecessary comments and clean up routing module for certificado zoosanitario

--- vucem-3.0-frontend/src/app/tramites/220201/certificado-zoosanitario-routing.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220201/certificado-zoosanitario-routing.module.ts
@@ -2,29 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ZoosanitarioPageComponent } from './pages/zoosanitario-page/zoosanitario-page.component';
 
-/**
- * @fileoverview Módulo de enrutamiento para el certificado zoosanitario.
- * Este módulo define las rutas para el componente principal del certificado zoosanitario.
- * @module certificadoZoosanitario
- */
-
-/**
- * Rutas para el certificado zoosanitario.
- * @constant ROUTES_ZOOSANITARIO
- * @type {Routes}
- */
 export const ROUTES_ZOOSANITARIO: Routes = [
   {
     path: 'zoosanitario',
     component: ZoosanitarioPageComponent,
-    /**
-     * @description Data asociada a la ruta.
-     * @property {object} data
-     * @property {string} data.title - Título de la página.
-     */
-    data: {
-      title: 'Certificado Zoosanitario' // Ejemplo: Puedes agregar un título para la página.
-    }
   },
   {
     path: '',
@@ -34,10 +15,6 @@ export const ROUTES_ZOOSANITARIO: Routes = [
 
 ];
 
-/**
- * Módulo de enrutamiento para el certificado zoosanitario. --220201
- * @class CertificadoZoosanitario
- */
 @NgModule({
   imports: [RouterModule.forChild(ROUTES_ZOOSANITARIO)],
   exports: [RouterModule]
