2a1130e91d56727f75275f744c163c5f4fc20631
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-10 10:50:53 +0530

    Refactor datos-para-movilizacion-nacional component to implement reactive form with validation for transport details

--- vucem-3.0-frontend/src/app/tramites/220202/components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { CatalogosSelect } from '../../../../core/models/shared/components.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-datos-para-movilizacion-nacional',
   templateUrl: './datos-para-movilizacion-nacional.component.html',
   styleUrl: './datos-para-movilizacion-nacional.component.scss'
 })
-export class DatosParaMovilizacionNacionalComponent {
+export class DatosParaMovilizacionNacionalComponent implements OnInit {
+  forma: FormGroup;
   transporteList: CatalogosSelect = {
     labelNombre: 'Medio de transporte',
     required: true,
@@ -19,6 +21,17 @@ export class DatosParaMovilizacionNacionalComponent {
     required: true,
     primerOpcion: 'Selecciona un valor',
     catalogos: []
+  }
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    this.forma = new FormGroup({
+      transporte: new FormControl('', Validators.required),  // for transporteList
+      guiaIdentificacion: new FormControl('', Validators.required),  // for Guia
+      empresaTransportista: new FormControl('', Validators.required),  // for Empresa Transportista
+      punto: new FormControl('', Validators.required)  // for puntoList
+    });
   }
 
 }
