ef6e91b8629e26edf036180565ab3887c7c67356
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-12 12:16:05 +0530

    ``` No code changes made. ```

--- vucem-3.0-frontend/src/app/core/models/220201/capturar-solicitud.model.ts
+++ vucem-3.0-frontend/src/app/core/models/220201/capturar-solicitud.model.ts
@@ -1,31 +1,31 @@
 /**
  * Modelo de datos de la solicitud
  * @export
- * @interface capturarSolicitud
- * @property {solicitante} solicitante - Información del solicitante.
- * @property {datosDeLaSolicitud} datosDeLaSolicitud - Datos de la solicitud.
- * @property {datosParaMovilizacionNacional} datosParaMovilizacionNacional - Datos para la movilización nacional.
- * @property {tercerosRelacionados} tercerosRelacionados - Información de terceros relacionados.
- * @property {pagoDeDerechos} pagoDeDerechos - Información de pago de derechos.--220201
+ * @interface CapturarSolicitud
+ * @property {Solicitante} solicitante - Información del solicitante.
+ * @property {DatosDeLaSolicitud} datosDeLaSolicitud - Datos de la solicitud.
+ * @property {DatosParaMovilizacionNacional} datosParaMovilizacionNacional - Datos para la movilización nacional.
+ * @property {TercerosRelacionados} tercerosRelacionados - Información de terceros relacionados.
+ * @property {PagoDeDerechos} pagoDeDerechos - Información de pago de derechos.
  */
-export interface capturarSolicitud {
-  solicitante: solicitante;
-  datosDeLaSolicitud: datosDeLaSolicitud;
-  datosParaMovilizacionNacional: datosParaMovilizacionNacional;
-  tercerosRelacionados: tercerosRelacionados;
-  pagoDeDerechos: pagoDeDerechos;
+export interface CapturarSolicitud {
+  solicitante: Solicitante;
+  datosDeLaSolicitud: DatosDeLaSolicitud;
+  datosParaMovilizacionNacional: DatosParaMovilizacionNacional;
+  tercerosRelacionados: TercerosRelacionados;
+  pagoDeDerechos: PagoDeDerechos;
 }
 
 /**
- * Interface solicitante
+ * Interface Solicitante
  * @export
- * @interface solicitante
+ * @interface Solicitante
  * @property {string} rfc - Registro Federal de Contribuyentes (RFC).
  * @property {string} nombreRazonSocial - Nombre o razón social del solicitante.
- * @property {string} aPaterno - Apellido paterno del solicitante.--220201
+ * @property {string} aPaterno - Apellido paterno del solicitante.
  * @property {string} correo - Correo electrónico del solicitante.
  */
-export interface solicitante {
+export interface Solicitante {
   rfc: string;
   nombreRazonSocial: string;
   aPaterno: string;
@@ -35,18 +35,18 @@ export interface solicitante {
 /**
  * Modelo de datos de la solicitud
  * @export
- * @interface datosDeLaSolicitud
+ * @interface DatosDeLaSolicitud
  * @property {string} aduanaIngreso - Aduana de ingreso.
  * @property {string} oficinaInspeccion - Oficina de inspección.
  * @property {string} puntoInspeccion - Punto de inspección.
  * @property {string} claveUCON - Clave UCON.
  * @property {string} establecimientoTIF - Establecimiento TIF.
- * @property {string} nombreVeterinario - Nombre del veterinario.--220201
+ * @property {string} nombreVeterinario - Nombre del veterinario.
  * @property {string} numeroGuia - Número de guía.
  * @property {string} certificacion - Certificación.
  * @property {string} regimen - Régimen.
  */
-export interface datosDeLaSolicitud {
+export interface DatosDeLaSolicitud {
   aduanaIngreso: string;
   oficinaInspeccion: string;
   puntoInspeccion: string;
@@ -61,14 +61,14 @@ export interface datosDeLaSolicitud {
 /**
  * Modelo para los datos de movilización nacional
  * @export
- * @interface datosParaMovilizacionNacional
+ * @interface DatosParaMovilizacionNacional
  * @property {string} coordenadas - Coordenadas de la ubicación.
  * @property {string} nombre - Nombre del solicitante.
  * @property {string} medio - Medio de transporte.
- * @property {string} transporte - Tipo de transporte.--220201
+ * @property {string} transporte - Tipo de transporte.
  * @property {string} punto - Punto de encuentro.
  */
-export interface datosParaMovilizacionNacional {
+export interface DatosParaMovilizacionNacional {
   coordenadas: string;
   nombre: string;
   medio: string;
@@ -79,17 +79,17 @@ export interface datosParaMovilizacionNacional {
 /**
  * Modelo para pago de derechos
  * @export
- * @interface pagoDeDerechos
+ * @interface PagoDeDerechos
  * @property {string} exentoPagoNo - Exento de pago (No).
  * @property {string} exentoPagoSi - Exento de pago (Sí).
  * @property {string} justificacion - Justificación.
  * @property {string} claveReferencia - Clave de referencia.
- * @property {string} cadenaDependencia - Cadena de dependencia. --220201
+ * @property {string} cadenaDependencia - Cadena de dependencia.
  * @property {string} banco - Banco.
  * @property {string} llavePago - Llave de pago.
  * @property {string} importePago - Importe de pago.
  */
-export interface pagoDeDerechos {
+export interface PagoDeDerechos {
   exentoPagoNo: string;
   exentoPagoSi: string;
   justificacion: string;
@@ -101,8 +101,8 @@ export interface pagoDeDerechos {
 }
 
 /**
- * Interface tercerosRelacionados --220201
+ * Interface TercerosRelacionados
  * @export
- * @interface tercerosRelacionados
+ * @interface TercerosRelacionados
  */
-export interface tercerosRelacionados { }
+export interface TercerosRelacionados { }
