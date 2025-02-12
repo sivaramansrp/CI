6133e8ac9ea8fbece0564e27dba9124d19f8b835
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-02-12 19:27:21 +0530

    Updated dropdown template as per New shared component

--- vucem-3.0-frontend/src/app/tramites/220501/sagarpa.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220501/sagarpa.module.ts
@@ -3,11 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-
 import { AgregarMercanciaComponent } from './components/agregar-mercancia/agregar-mercancia.component';
 import { AlertComponent } from '../../shared/components/alert/alert.component';
 import { AnexarDocumentosComponent } from '../../shared/components/anexar-documentos/anexar-documentos.component';
 import { BtnContinuarComponent } from '../../shared/components/btn-continuar/btn-continuar.component';
+import { CatalogoSelectComponent } from '../../shared/components/catalogo-select/catalogo-select.component';
 import { CarrosDeFerrocarrilComponent } from '../220502/shared/carros-de-ferrocarril/carros-de-ferrocarril.component';
 import { DatosGeneralesComponent } from './components/datos-generales/datos-generales.component';
 import { DatoseDelTramiteARealizerComponent } from '../220502/shared/datose-del-tramite-a-realizer/datose-del-tramite-a-realizer.component';
@@ -22,7 +22,6 @@ import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { ResponsableInspeccionEnPuntoComponent } from '../220502/shared/responsable-inspeccion-en-punto/responsable-inspeccion-en-punto.component';
 import { RevisionDocumentalComponent } from './components/revision-documental/revision-documental.component';
 import { SagarpaRoutingModule } from './sagarpa-routing.module';
-import { SelectCatalogosComponent } from '../../shared/components/select-catalogos/select-catalogos.component';
 import { SolicitanteComponent } from '../../shared/components/solicitante/solicitante.component';
 import { SolicitudComponent } from './components/solicitud/solicitud.component';
 import { SolicitudDatosComponent } from '../220502/shared/solicitud-datos/solicitud-datos.component';
@@ -31,8 +30,6 @@ import { TercerosRelacionadosComponent } from './components/terceros-relacionado
 import { TituloComponent } from '../../shared/components/titulo/titulo.component';
 import { WizardComponent } from '../../shared/components/wizard/wizard.component';
 
-
-
 @NgModule({
   declarations: [
     SolicitudComponent,
@@ -40,12 +37,12 @@ import { WizardComponent } from '../../shared/components/wizard/wizard.component
     PasoUnoComponent,
     PasoDosComponent,
     PasoTresComponent,
-    RevisionDocumentalComponent,    
+    RevisionDocumentalComponent,
     MedioTransporteComponent,
     AgregarMercanciaComponent,
     DatosGeneralesComponent,
     TercerosRelacionadosComponent,
-    PagoDeDerechosComponent
+    PagoDeDerechosComponent,
   ],
   imports: [
     CommonModule,
@@ -57,10 +54,8 @@ import { WizardComponent } from '../../shared/components/wizard/wizard.component
     FirmaElectronicaComponent,
     AnexarDocumentosComponent,
     AlertComponent,
-    SelectCatalogosComponent,
     SolicitanteComponent,
     ReactiveFormsModule,
-    SelectCatalogosComponent,
     FormsModule,
     ReactiveFormsModule,
     AlertComponent,
@@ -69,11 +64,9 @@ import { WizardComponent } from '../../shared/components/wizard/wizard.component
     DatoseDelTramiteARealizerComponent,
     ResponsableInspeccionEnPuntoComponent,
     CarrosDeFerrocarrilComponent,
-    HistorialInspeccionFisicaComponent
+    HistorialInspeccionFisicaComponent,
+    CatalogoSelectComponent,
   ],
-  exports: [ 
-    SolicitudComponent,
-    SolicitudPageComponent
-  ]
+  exports: [SolicitudComponent, SolicitudPageComponent],
 })
-export class SagarpaModule { }
+export class SagarpaModule {}
