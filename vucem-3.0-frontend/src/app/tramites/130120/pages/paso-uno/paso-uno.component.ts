394edff106b1c481ad238dd10b48708a7f6bea3e
Rohith Kannan M <rohithkannan.m@purpleslate.in>
2025-02-12 16:49:03 +0530

    feat: solicite change and removed commented lines

--- vucem-3.0-frontend/src/app/tramites/130120/pages/paso-uno/paso-uno.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130120/pages/paso-uno/paso-uno.component.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DatosDeLaSolicitudComponent } from "../../components/datos-de-la-solicitud/datos-de-la-solicitud.component";
-import { SolicitanteComponent } from "../../components/solicitante/solicitante.component";
+import { SolicitanteComponent } from "../../../../shared/components/solicitante/solicitante.component";
 
 @Component({
   selector: 'app-paso-uno',
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.scss',
-  imports: [CommonModule, SolicitanteComponent, DatosDeLaSolicitudComponent],
+  imports: [CommonModule, SolicitanteComponent, DatosDeLaSolicitudComponent, SolicitanteComponent],
   standalone: true,
 })
 export class PasoUnoComponent {
 
-  indice: number = 2;
+  indice: number = 1;
 
   seleccionaTab(i: number): void {
     this.indice = i;
