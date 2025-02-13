7a028fbf31ff15108c476d20c17bd08c7cb2b6d9
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-13 11:29:30 +0530

    No code changes made; empty commit.

--- vucem-3.0-frontend/src/app/app-routing.module.ts
+++ vucem-3.0-frontend/src/app/app-routing.module.ts
@@ -41,6 +41,13 @@ const routes: Routes = [
         (m) => m.PantallasModule
       ),
   },
+  {
+    path: 'certificado-fitosanitario',
+    loadChildren: () =>
+      import('./tramites/220202/fitosanitario.module').then(
+        (m) => m.FitosanitarioModule
+      )
+  }
 ];
 
 @NgModule({
