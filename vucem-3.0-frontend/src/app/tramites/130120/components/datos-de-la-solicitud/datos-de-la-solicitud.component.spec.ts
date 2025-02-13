4e6d03b9b7891d527cf0f422cf31e145986addba
Rohith Kannan M <rohithkannan.m@purpleslate.in>
2025-02-13 17:16:26 +0530

    feat: unit test case added for datos-de-la-solicitud

--- vucem-3.0-frontend/src/app/tramites/130120/components/datos-de-la-solicitud/datos-de-la-solicitud.component.spec.ts
+++ vucem-3.0-frontend/src/app/tramites/130120/components/datos-de-la-solicitud/datos-de-la-solicitud.component.spec.ts
@@ -1,23 +1,93 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CatalogoSelectComponent } from '../../../../shared/components/catalogo-select/catalogo-select.component';
+import { CatalogosService } from '../../../../core/services/shared/catalogos/catalogos.service';
+import { CommonModule } from '@angular/common';
 import { DatosDeLaSolicitudComponent } from './datos-de-la-solicitud.component';
+import { InputFechaComponent } from '../../../../shared/components/input-fecha/input-fecha.component';
+import { InputRadioComponent } from '../../../../shared/components/input-radio/input-radio.component';
+import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
+import { of } from 'rxjs';
 
 describe('DatosDeLaSolicitudComponent', () => {
   let component: DatosDeLaSolicitudComponent;
   let fixture: ComponentFixture<DatosDeLaSolicitudComponent>;
+  let _catalogosService: CatalogosService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DatosDeLaSolicitudComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        TituloComponent,
+        InputFechaComponent,
+        InputRadioComponent,
+        CatalogoSelectComponent,
+        DatosDeLaSolicitudComponent // Import the standalone component here
+      ],
+      providers: [
+        FormBuilder,
+        {
+          provide: CatalogosService,
+          useValue: {
+            getCatalogo: jasmine.createSpy('getCatalogo').and.returnValue(of([]))
+          }
+        }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DatosDeLaSolicitudComponent);
     component = fixture.componentInstance;
+    _catalogosService = TestBed.inject(CatalogosService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form', () => {
+    component.ngOnInit();
+    expect(component.form).toBeDefined();
+  });
+
+  it('should set radioSelectedValues', () => {
+    const mockValues = { key: 'value' };
+    component.radioSelectedValues = mockValues;
+    expect(component.radioSelectedValues).toEqual(mockValues);
+  });
+
+  it('should create form with FormBuilder', () => {
+    component.ngOnInit();
+    expect(component.form instanceof FormGroup).toBe(true);
+  });
+
+  it('should have correct input types and props', () => {
+    const inputTypes = component.config[0].menu;
+    expect(inputTypes).toBeDefined();
+    expect(inputTypes.length).toBeGreaterThan(0);
+    inputTypes.forEach(input => {
+      expect(input.inputType).toBeDefined();
+      expect(input.props).toBeDefined();
+      expect(input.class).toBe('col-md-8');
+    });
+  });
+
+  it('should call ngOnInit and initialize form', () => {
+    spyOn(component, 'ngOnInit').and.callThrough();
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
+    expect(component.form).toBeDefined();
+  });
+
+  it('should call crearFormulario', () => {
+    spyOn(component, 'crearFormulario').and.callThrough();
+    component.crearFormulario();
+    expect(component.crearFormulario).toHaveBeenCalled();
+  });
+
+  it('should call getCatalogo from CatalogosService', () => {
+    component.ngOnInit();
+    expect(_catalogosService.getCatalogo).toHaveBeenCalled();
   });
 });
