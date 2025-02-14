ddb91423e2236ac563fa589877c4f2302a4eb3ff
Kevin Jonás Martínez Bautista <kmartinezb@ultrasist.com>
2025-02-14 00:48:15 -0600

    CHANGE path evaluar solicitud

--- vucem-3.0-frontend/src/app/tramites/5701/servicios-extraordinarios-routing.module.ts
+++ vucem-3.0-frontend/src/app/tramites/5701/servicios-extraordinarios-routing.module.ts
@@ -15,7 +15,7 @@ export const ROUTES_SOLICITUDES: Routes = [
     component: AcusePageComponent,
   },
   {
-    path: 'evaluar-dictamen',
+    path: 'evaluar-solicitud',
     component: EvaluarDictamenComponent
   },
   {
