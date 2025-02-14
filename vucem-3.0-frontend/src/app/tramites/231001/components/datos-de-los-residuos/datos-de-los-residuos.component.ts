14b53b22d32f9ed75ef440fd320edf2c6d91b1aa
balamurugan-ilink <balamurugan.govinthan@ilink-systems.com>
2025-02-14 16:57:09 +0530

    Datos-de-los-residuos component done

--- vucem-3.0-frontend/src/app/tramites/231001/components/datos-de-los-residuos/datos-de-los-residuos.component.ts
+++ vucem-3.0-frontend/src/app/tramites/231001/components/datos-de-los-residuos/datos-de-los-residuos.component.ts
@@ -1,41 +1,93 @@
-import { Component, OnInit } from '@angular/core';
-import { Catalogo } from '../../../../core/models/shared/catalogos.model';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { CatalogoSelectComponent } from '../../../../shared/components/catalogo-select/catalogo-select.component';
+/**
+ * @module DatosDeLosResiduosComponent
+ * @description Este módulo define el componente `DatosDeLosResiduosComponent` que maneja la información de los residuos.
+ */
+
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { BtnContinuarComponent } from "../../../../shared/components/btn-continuar/btn-continuar.component";
+import { Catalogo } from '../../../../core/models/shared/catalogos.model';
+import { CatalogoSelectComponent } from '../../../../shared/components/catalogo-select/catalogo-select.component';
+import { DatosPasos } from '../../../../core/models/shared/components.model';
+
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TituloComponent } from '../../../../shared/components/titulo/titulo.component';
+
+import CapituloFraccion from '../../../../../assets/json/231001/comboCapituloFraccion.json';
+import FraccionArancelariaParametros from '../../../../../assets/json/231001/comboFraccionArancelariaParametros.json';
+import PartidaFraccion from '../../../../../assets/json/231001/comboPartidaFraccion.json';
+import SubPartidaFraccion from '../../../../../assets/json/231001/comboSubPartidaFraccion.json';
+import UnidadMedida from '../../../../../assets/json/231001/comboUnidadMedida.json';
 
 @Component({
   selector: 'app-datos-de-los-residuos',
   templateUrl: './datos-de-los-residuos.component.html',
   styleUrl: './datos-de-los-residuos.component.scss',
-  standalone:true,
-  imports: [ReactiveFormsModule,CatalogoSelectComponent,CommonModule]
+  standalone: true,
+  imports: [ReactiveFormsModule, CatalogoSelectComponent, CommonModule, TituloComponent, BtnContinuarComponent]
 })
-export class DatosDeLosResiduosComponent implements OnInit{
+export class DatosDeLosResiduosComponent {
 
-materiaPrimaForm: FormGroup;
-  showMessage: boolean = false;
+  /**
+   * @property {FormGroup} materiaPrimaForm - Formulario principal del componente.
+   */
+  materiaPrimaForm: FormGroup;
+
+  /**
+   * @property {boolean} mostrarMsgCantSe06 - Indica si se debe mostrar el mensaje de cantidad SE06.
+   */
   mostrarMsgCantSe06: boolean = false;
 
-  // Dropdown lists
-  comboUnidadMedida!: Catalogo[];
-  comboCapituloFraccion!: Catalogo[];
+  /**
+   * @property {DatosPasos} datosPasosGuardar - Datos de los pasos del formulario.
+   */
+  datosPasosGuardar: DatosPasos = {
+    txtBtnSig: "Guardar",
+    txtBtnAnt: "Cancelar",
+    indice: 2,
+    nroPasos: 0
+  }
+
+  /**
+   * @property {Catalogo[]} comboUnidadMedida - Opciones del catálogo de unidad de medida.
+   */
+  comboUnidadMedida: Catalogo[] = UnidadMedida;
+
+  /**
+   * @property {Catalogo[]} comboCapituloFraccion - Opciones del catálogo de capítulo de fracción.
+   */
+  comboCapituloFraccion: Catalogo[] = CapituloFraccion;
+
+  /**
+   * @property {Catalogo[]} comboPartidaFraccion - Opciones del catálogo de partida de fracción.
+   */
   comboPartidaFraccion!: Catalogo[];
+
+  /**
+   * @property {Catalogo[]} comboSubPartidaFraccion - Opciones del catálogo de subpartida de fracción.
+   */
   comboSubPartidaFraccion!: Catalogo[];
+
+  /**
+   * @property {Catalogo[]} comboFraccionArancelariaParametros - Opciones del catálogo de fracción arancelaria.
+   */
   comboFraccionArancelariaParametros!: Catalogo[];
 
-  constructor(
-    private fb: FormBuilder
-  ) {
+  /**
+   * @constructor
+   * @param {FormBuilder} fb - Instancia de FormBuilder para crear formularios.
+   */
+  constructor(private fb: FormBuilder) {
     this.materiaPrimaForm = this.fb.group({
       descUnidadMedida: [''],
       descFraccion: [''],
       generica1: [''],
       clavePartida: [''],
       claveSubPartida: [''],
-      descripcionMercancia: ['', Validators.required],
-      generica2: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,6})?$')]],
-      cantidadEnLetra: [{ value: '', disabled: true }],
+      descripcionMercancia: ['', [Validators.required, Validators.maxLength(120)]],
+      generica2: ['', [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]{1,6})?$'), Validators.maxLength(18)]],
+      cantidadEnLetra: [{ value: '', disabled: true }, Validators.maxLength(256)],
       unidadMedidaComercial: this.fb.group({
         clave: ['', Validators.required]
       }),
@@ -46,84 +98,52 @@ materiaPrimaForm: FormGroup;
     });
   }
 
-  ngOnInit(): void {
-    this.showMessage = false;
-    console.log("OnInit");
-    // this.loadComboUnidadMedida();
-    // this.loadComboCapituloFraccion();
-    // this.loadComboFraccionArancelariaParametros();
-
-    // Additional initialization if necessary
-  }
-
-  // Load dropdown data
-  static loadComboUnidadMedida(): void {
-    // this.modalMateriaPrimaService.getComboUnidadMedida().subscribe(
-    //   data => this.comboUnidadMedida = data,
-    //   error => console.error('Error loading Unidad Medida:', error)
-    // );
-  }
-
-  static loadComboCapituloFraccion(): void {
-    // this.modalMateriaPrimaService.getComboCapituloFraccion().subscribe(
-    //   data => this.comboCapituloFraccion = data,
-    //   error => console.error('Error loading Capitulo Fraccion:', error)
-    // );
-  }
-
+  /**
+   * @method loadComboPartidaFraccion
+   * @description Carga las opciones del catálogo de partida de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   loadComboPartidaFraccion(): void {
-    // const capituloClave = this.materiaPrimaForm.get('capituloFraccion')?.value;
-    // this.modalMateriaPrimaService.getComboPartidaFraccion(capituloClave).subscribe(
-    //   data => this.comboPartidaFraccion = data,
-    //   error => console.error('Error loading Partida Fraccion:', error)
-    // );
+    this.comboPartidaFraccion = PartidaFraccion;
   }
 
+  /**
+   * @method loadComboSubPartidaFraccion
+   * @description Carga las opciones del catálogo de subpartida de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   loadComboSubPartidaFraccion(): void {
-    // const partidaClave = this.materiaPrimaForm.get('partidaFraccion')?.value;
-    // this.modalMateriaPrimaService.getComboSubPartidaFraccion(partidaClave).subscribe(
-    //   data => this.comboSubPartidaFraccion = data,
-    //   error => console.error('Error loading SubPartida Fraccion:', error)
-    // );
+    this.comboSubPartidaFraccion = SubPartidaFraccion;
   }
 
+  /**
+   * @method loadComboFraccion
+   * @description Carga las opciones del catálogo de fracción arancelaria.
+   * @memberof DatosDeLosResiduosComponent
+   */
   loadComboFraccion(): void {
-    // const subPartidaClave = this.materiaPrimaForm.get('subPartidaFraccion')?.value;
-    // this.modalMateriaPrimaService.getComboFraccionArancelariaParametros(subPartidaClave).subscribe(
-    //   data => this.comboFraccionArancelariaParametros = data,
-    //   error => console.error('Error loading Fraccion:', error)
-    // );
+    this.comboFraccionArancelariaParametros = FraccionArancelariaParametros;
   }
 
-  // Form submission
-  agregarMercancia(): void {
-    if (this.materiaPrimaForm.invalid) {
-      this.showMessage = true;
-      return;
-    }
-
-    this.guardarMateriaPrima();
-  }
-
-  // Obtener letra de cantidad
+  /**
+   * @method obtenerLetraCantidad
+   * @description Obtiene la letra correspondiente a la cantidad.
+   * @param {string} cantidad - La cantidad a convertir en letra.
+   * @memberof DatosDeLosResiduosComponent
+   */
   obtenerLetraCantidad(cantidad: string): void {
-    this.materiaPrimaForm.patchValue({ cantidadEnLetra: '' });
-
-    // this.modalMateriaPrimaService.convertirNumeroALetra(cantidad).subscribe(
-    //   result => {
-    //     if (result === 'Error') {
-    //       alert('El número no es válido');
-    //     } else {
-    //       this.materiaPrimaForm.patchValue({ cantidadEnLetra: result });
-    //     }
-    //   },
-    //   error => {
-    //     console.error('Error al convertir número a letra:', error);
-    //   }
-    // );
+    if (cantidad === '1') {
+      this.materiaPrimaForm.patchValue({ cantidadEnLetra: 'UNO' });
+    } else {
+      this.materiaPrimaForm.patchValue({ cantidadEnLetra: '' });
+    }
   }
 
-  // Change handlers
+  /**
+   * @method cambiaCapituloFraccion
+   * @description Maneja el cambio de capítulo de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   cambiaCapituloFraccion(): void {
     this.materiaPrimaForm.patchValue({
       clavePartida: '',
@@ -137,6 +157,11 @@ materiaPrimaForm: FormGroup;
     this.loadComboPartidaFraccion();
   }
 
+  /**
+   * @method cambiaPartidaFraccion
+   * @description Maneja el cambio de partida de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   cambiaPartidaFraccion(): void {
     const partidaClave = this.materiaPrimaForm.get('partidaFraccion')?.value;
     this.materiaPrimaForm.patchValue({
@@ -150,6 +175,11 @@ materiaPrimaForm: FormGroup;
     this.loadComboSubPartidaFraccion();
   }
 
+  /**
+   * @method cambiaSubPartidaFraccion
+   * @description Maneja el cambio de subpartida de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   cambiaSubPartidaFraccion(): void {
     const subPartidaClave = this.materiaPrimaForm.get('subPartidaFraccion')?.value;
     this.materiaPrimaForm.patchValue({
@@ -161,6 +191,11 @@ materiaPrimaForm: FormGroup;
     this.loadComboFraccion();
   }
 
+  /**
+   * @method cambiaFraccion
+   * @description Maneja el cambio de fracción.
+   * @memberof DatosDeLosResiduosComponent
+   */
   cambiaFraccion(): void {
     const fraccionSeleccionada = this.comboFraccionArancelariaParametros.find(fr => fr.id === this.materiaPrimaForm.get('fraccion')?.value);
     if (fraccionSeleccionada) {
@@ -172,6 +207,11 @@ materiaPrimaForm: FormGroup;
     }
   }
 
+  /**
+   * @method cambiaUnidadMedida
+   * @description Maneja el cambio de unidad de medida.
+   * @memberof DatosDeLosResiduosComponent
+   */
   cambiaUnidadMedida(): void {
     const unidadSeleccionada = this.comboUnidadMedida.find(unidad => unidad.id === this.materiaPrimaForm.get('unidadMedidaComercial.clave')?.value);
     if (unidadSeleccionada) {
@@ -181,41 +221,15 @@ materiaPrimaForm: FormGroup;
     }
   }
 
-  // Validar vigencia de fraccion
+  /**
+   * @method validaVigenciaFraccion
+   * @description Valida la vigencia de la fracción.
+   * @param {number} clvFracion - Clave de la fracción a validar.
+   * @memberof DatosDeLosResiduosComponent
+   */
   validaVigenciaFraccion(clvFracion: number): void {
-    // this.modalMateriaPrimaService.validaVigenciaFraccion(clvFracion).subscribe(
-    //   result => {
-    //     if (!result) {
-    //       alert('La fracción no se encuentra vigente');
-    //       this.materiaPrimaForm.patchValue({ fraccion: '' });
-    //     }
-    //   },
-    //   error => {
-    //     console.error('Error al validar vigencia de fracción:', error);
-    //   }
-    // );
+    if (!clvFracion) {
+      this.materiaPrimaForm.patchValue({ fraccion: '' });
+    }
   }
-
-  // Guardar materia prima
-  guardarMateriaPrima(): void {
-    const formValue = this.materiaPrimaForm.getRawValue();
-    // this.modalMateriaPrimaService.guardarMateriaPrima(formValue).subscribe(
-    //   response => {
-    //     alert('Materia Prima guardada exitosamente');
-    //     this.cerrar();
-    //   },
-    //   error => {
-    //     console.error('Error al guardar Materia Prima:', error);
-    //     alert('Ocurrió un error al guardar la Materia Prima');
-    //   }
-    // );
-  }
-
-  // Cerrar modal
-  cerrar(): void {
-    // Implementar lógica para cerrar el modal, por ejemplo:
-    // this.modalService.close('modalResiduos');
-  }
-
-
 }
