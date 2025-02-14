685400106e8118f9f1c5a9dba392f2ecbf1e7394
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-02-14 12:03:50 +0530

    Add routing and module for Zoosanitario feature

--- vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
+++ vucem-3.0-frontend/src/app/tramites/220202/fitosanitario.module.ts
@@ -1,66 +1,69 @@
-import { AgriculturaComponent } from './pages/agricultura/agricultura.component';
 import { AlertComponent } from '../../shared/components/alert/alert.component';
 import { AnexarDocumentosComponent } from '../../shared/components/anexar-documentos/anexar-documentos.component';
+import { BtnContinuarComponent } from '../../shared/components/btn-continuar/btn-continuar.component';
 import { CommonModule } from '@angular/common';
+import { CrosslistComponent } from '../../shared/components/crosslist/crosslist.component';
+
+import { CertificadoZoosanitario } from './certificado-zoosanitario-routing.module';
+
 import { DatosDeLaSolicitudComponent } from './components/datos-de-la-solicitud/datos-de-la-solicitud.component';
 import { DatosParaMovilizacionNacionalComponent } from './components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component';
-import { FitosanitarioRoutingModule } from './fitosanitario-routing.module';
+import { FirmaElectronicaComponent } from '../../shared/components/firma-electronica/firma-electronica.component';
+import { InputCheckComponent } from '../../shared/components/input-check/input-check.component';
+import { InputFechaComponent } from '../../shared/components/input-fecha/input-fecha.component';
+
 import { NgModule } from '@angular/core';
 import { PagoDeDerechosComponent } from './components/pago-de-derechos/pago-de-derechos.component';
 import { PasoCuatroComponent } from './pages/paso-cuatro/paso-cuatro.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
-
-
-import { BtnContinuarComponent } from '../../shared/components/btn-continuar/btn-continuar.component';
-import { CrosslistComponent } from '../../shared/components/crosslist/crosslist.component';
-import { InputCheckComponent } from '../../shared/components/input-check/input-check.component';
-import { InputFechaComponent } from '../../shared/components/input-fecha/input-fecha.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SelectCatalogosComponent } from '../../shared/components/select-catalogos/select-catalogos.component';
+
+import { SharedModule } from '../../shared/shared.module';
+
 import { TituloComponent } from '../../shared/components/titulo/titulo.component';
 import { WizardComponent } from '../../shared/components/wizard/wizard.component';
-
-import { FirmaElectronicaComponent } from '../../shared/components/firma-electronica/firma-electronica.component';
+import { ZoosanitarioPageComponent } from './pages/zoosanitario-page/zoosanitario-page.component';
 import { SolicitanteComponent } from '../../shared/components/solicitante/solicitante.component';
-
 import { ServiciosExtraordinariosModule } from '../5701/servicios-extraordinarios.module';
+import { InputRadioComponent } from '../../shared/components/input-radio/input-radio.component';
 import { TableComponent } from '../../shared/components/table/table.component';
 import { CatalogoSelectComponent } from '../../shared/components/catalogo-select/catalogo-select.component';
-import { InputRadioComponent } from '../../shared/components/input-radio/input-radio.component';
+
 
 @NgModule({
   declarations: [
-    PasoUnoComponent,
-    PasoTresComponent,
     PasoDosComponent,
     PasoCuatroComponent,
-    AgriculturaComponent,
+    PasoTresComponent,
+    PasoUnoComponent,
+    ZoosanitarioPageComponent,
     DatosDeLaSolicitudComponent,
     DatosParaMovilizacionNacionalComponent,
-    PagoDeDerechosComponent
+    PagoDeDerechosComponent,
+
+
   ],
   imports: [
+    SharedModule,
     CommonModule,
-    ReactiveFormsModule,
-    FitosanitarioRoutingModule,
+    CertificadoZoosanitario,
     WizardComponent,
     TituloComponent,
     BtnContinuarComponent,
     ReactiveFormsModule,
     CrosslistComponent,
     InputCheckComponent,
-    SelectCatalogosComponent,
     AlertComponent,
     InputFechaComponent,
     AnexarDocumentosComponent,
     FirmaElectronicaComponent,
     SolicitanteComponent,
-    ServiciosExtraordinariosModule,
-    CatalogoSelectComponent,
+    InputRadioComponent,
     TableComponent,
-    InputRadioComponent
+    CatalogoSelectComponent,
+    ServiciosExtraordinariosModule
   ]
 })
-export class FitosanitarioModule { }
+export class CertificadoZoosanitarioModule { }
