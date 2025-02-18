99045a8c2b293ffbef1657926a791ab0f73996bb
Aditi <aditi.patil@ilink-systems.com>
2025-02-18 17:54:38 +0530

    test cases added

--- vucem-3.0-frontend/src/app/tramites/231001/pages/datos/datos.component.spec.ts
+++ vucem-3.0-frontend/src/app/tramites/231001/pages/datos/datos.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DatosComponent } from './datos.component';
+import { PASOS } from '../../../../shared/constantes/aviso.enum';
+import { WizardComponent } from '../../../../shared/components/wizard/wizard.component';
 
 describe('DatosComponent', () => {
   let component: DatosComponent;
@@ -8,10 +9,9 @@ describe('DatosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DatosComponent]
-    })
-    .compileComponents();
-    
+      declarations: [DatosComponent, WizardComponent]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(DatosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,33 @@ describe('DatosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have pasos defined', () => {
+    expect(component.pasos).toEqual(PASOS);
+  });
+
+  it('should have wizardComponent defined', () => {
+    expect(component.wizardComponent).toBeDefined();
+  });
+
+  it('should have initial indice value as 1', () => {
+    expect(component.indice).toBe(1);
+  });
+
+  it('should update indice value', () => {
+    component.indice = 2;
+    expect(component.indice).toBe(2);
+  });
+
+  it('should call wizardComponent methods', () => {
+    spyOn(component.wizardComponent, 'siguiente');
+    spyOn(component.wizardComponent, 'atras');
+
+    component.wizardComponent.siguiente();
+    expect(component.wizardComponent.siguiente).toHaveBeenCalled();
+
+    component.wizardComponent.atras();
+    expect(component.wizardComponent.atras).toHaveBeenCalled();
   });
 });
--- vucem-3.0-frontend/src/app/tramites/231001/pages/solicitante-detos-tabs/solicitante-detos-tabs.component.spec.ts
+++ vucem-3.0-frontend/src/app/tramites/231001/pages/solicitante-detos-tabs/solicitante-detos-tabs.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SolicitanteDetosTabsComponent } from './solicitante-detos-tabs.component';
 
 describe('SolicitanteDetosTabsComponent', () => {
@@ -9,9 +8,8 @@ describe('SolicitanteDetosTabsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SolicitanteDetosTabsComponent]
-    })
-    .compileComponents();
-    
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SolicitanteDetosTabsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +17,24 @@ describe('SolicitanteDetosTabsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have initial indice value as 1', () => {
+    expect(component.indice).toBe(1);
+  });
+
+  it('should update indice value when seleccionaTab is called', () => {
+    component.seleccionaTab(2);
+    expect(component.indice).toBe(2);
+  });
+
+  it('should update indice value to 0', () => {
+    component.seleccionaTab(0);
+    expect(component.indice).toBe(0);
+  });
+
+  it('should not update indice value if the same tab is selected', () => {
+    component.seleccionaTab(1);
+    expect(component.indice).toBe(1);
   });
 });
