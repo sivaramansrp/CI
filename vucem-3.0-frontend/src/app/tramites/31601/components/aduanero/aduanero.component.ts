1949d563ef8742b6dc5ca8a81f77b36b7f063ccc
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-02-18 16:12:32 +0530

    Worked on Table and modal ui
    Worked on Table and modal ui

--- vucem-3.0-frontend/src/app/tramites/31601/components/aduanero/aduanero.component.ts
+++ vucem-3.0-frontend/src/app/tramites/31601/components/aduanero/aduanero.component.ts
@@ -1,8 +1,14 @@
 import { CommonModule } from '@angular/common';
 
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
-import preOperativo from '../../../../../assets/json/31601/preOperativo.json'
+import preOperativo from '../../../../../assets/json/31601/preOperativo.json';
 
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { InputRadioComponent } from '../../../../shared/components/input-radio/input-radio.component';
@@ -20,36 +26,57 @@ import destinatarioTable from '../../../../../assets/json/220401/destinatario-ta
 import importardorTable from '../../../../../assets/json/220401/importador-table.json';
 
 import { TableComponent } from '../../../../shared/components/table/table.component';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-aduanero',
   templateUrl: './aduanero.component.html',
   styleUrl: './aduanero.component.scss',
-  standalone:true,
+  standalone: true,
   imports: [
-     CommonModule,
-     ReactiveFormsModule,
-     InputRadioComponent,
-     CatalogoSelectComponent,
-     TableComponent
-],
+    CommonModule,
+    ReactiveFormsModule,
+    InputRadioComponent,
+    CatalogoSelectComponent,
+    TableComponent,
+  ],
 })
-export class AduaneroComponent implements OnInit {
-  preOperativeForm! : FormGroup;
+export class AduaneroComponent implements OnInit, AfterViewInit {
+  @ViewChild('modifyModal', { static: false }) modifyModal!: ElementRef;
+  modalInstance!: Modal;
+
+  preOperativeForm!: FormGroup;
   radioOptions = preOperativo; // Use imported JSON data
   public establecimientoBodyData: unknown = [];
-  sectorProductivoAgace :Catalogo[] =productivo;
-  serviciosAgace : Catalogo[] = serviciosAgace;
-  comboBimestresIDC :Catalogo[] = comboBimestres;
+  sectorProductivoAgace: Catalogo[] = productivo;
+  serviciosAgace: Catalogo[] = serviciosAgace;
+  comboBimestresIDC: Catalogo[] = comboBimestres;
   public establecimientoHeaderData: string[] = [];
   public getEstablecimientoTableData = establecimientoTable;
   public getDestinatarioTableData = destinatarioTable;
   public getImportadorTableData = importardorTable;
-ngOnInit(){
-this.getEstablecimiento();
-}
+  ngOnInit() {
+    this.getEstablecimiento();
+    setTimeout(() => {
+      if (this.modifyModal) {
+        this.modalInstance = new Modal(this.modifyModal.nativeElement);
+      }
+    });
+  }
+  openModal() {
+    if (this.modalInstance) {
+      this.modalInstance.show();
+    } else {
+      console.error('Modal instance not initialized.');
+    }
+  }
+  ngAfterViewInit() {
+    this.modalInstance = new Modal(this.modifyModal.nativeElement);
+  }
+
   public getEstablecimiento() {
-    this.establecimientoHeaderData = this.getEstablecimientoTableData.tableHeader;
+    this.establecimientoHeaderData =
+      this.getEstablecimientoTableData.tableHeader;
     this.establecimientoBodyData = this.getEstablecimientoTableData.tableBody;
   }
 }
