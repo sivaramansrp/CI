fe1c08452f354a4e3901ae6602ad32a6804716bf
Rohith Kannan M <rohithkannan.m@purpleslate.in>
2025-02-19 15:12:44 +0530

    feat: added other pasos

--- vucem-3.0-frontend/src/app/tramites/130120/pages/paso-cuatro/paso-cuatro.component.ts
+++ vucem-3.0-frontend/src/app/tramites/130120/pages/paso-cuatro/paso-cuatro.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FirmaElectronicaComponent } from "../../../../shared/components/firma-electronica/firma-electronica.component";
 
 @Component({
   selector: 'app-paso-cuatro',
   templateUrl: './paso-cuatro.component.html',
   styleUrl: './paso-cuatro.component.scss',
   standalone: true,
+  imports: [FirmaElectronicaComponent],
 })
 export class PasoCuatroComponent {
 
