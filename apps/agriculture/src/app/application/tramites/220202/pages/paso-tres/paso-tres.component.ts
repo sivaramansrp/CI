48c229db7cc83734ba7837bc9b21ab30312cc4b4
Sibie Arunmozhi <sibie.a@purpleslate.in>
2025-02-20 15:13:44 +0530

    Bug fix for paso-tres import.

--- apps/agriculture/src/app/application/tramites/220202/pages/paso-tres/paso-tres.component.ts
+++ apps/agriculture/src/app/application/tramites/220202/pages/paso-tres/paso-tres.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TEXTOS } from '../../../../shared/constantes/servicios-extraordinarios.enum';
+import { TEXTOS } from 'libs/shared/data-access-user/src/tramites/constantes/servicios-extraordinarios.enum';
 
 /**
  * Componente para mostrar el subtítulo del asistente.
