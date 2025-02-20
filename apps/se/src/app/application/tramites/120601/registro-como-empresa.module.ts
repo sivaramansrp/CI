7229c64620022f806049726345d5c3a9c9c04b11
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-02-20 16:44:15 +0530

    ADD - create RegistroComoEmpresa module and routing

---
+++ apps/se/src/app/application/tramites/120601/registro-como-empresa.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { RegistroComoEmpresaRoutingModule } from './registro-como-empresa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegistroComoEmpresaRoutingModule
+  ]
+})
+export class RegistroComoEmpresaModule { }
