e1a0de9b27f0c0309b49ebefabd441304bbabac3
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-02-24 11:21:26 +0530

    fix: added constant for tabs

---
+++ libs/shared/data-access-user/src/core/services/120402/asignacion-directa-de-cupo.enum.ts
@@ -0,0 +1,26 @@
+export const ASIGNACION = [
+    {
+        indice: 1,
+        titulo: 'Capturar solicitud',
+        activo: true,
+        completado: false,
+      },
+      {
+        indice: 2,
+        titulo: 'Firmar solicitud',
+        activo: false,
+        completado: false,
+      },
+      {
+        indice: 3,
+        titulo: 'Anexar requistios',
+        activo: false,
+        completado: false,
+      },
+      {
+        indice: 4,
+        titulo: 'Firmar soclicitud',
+        activo: false,
+        completado: false,
+      }
+]
