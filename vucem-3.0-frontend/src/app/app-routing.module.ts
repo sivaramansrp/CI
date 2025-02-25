f2e4009cba52405603c6d929a1ffc679c7c8ff8c
Sibie Arunmozhi <sibie.a@purpleslate.in>
2025-02-25 05:48:57 +0530

    Revert "Merge branch 'feature/sprint2_in_p06_80208_dev' into develop"
    This reverts commit 72e63d6c41ae8dd8cabe0b8bca384712b6c30a55, reversing
    changes made to 4aacb579e785f177e3d9638a3417726741cfee99.

--- vucem-3.0-frontend/src/app/app-routing.module.ts
+++ vucem-3.0-frontend/src/app/app-routing.module.ts
@@ -55,13 +55,6 @@ const routes: Routes = [
       ),
   },
   {
-    path: 'solicitud-modalidad',
-    loadChildren: () =>
-      import('./tramites/80208/registro-solicitud-modalidad.module').then(
-        (m) => m.RegistroSolicitudModalidadModule)
-  },
-
-  {
     path: 'octava-temporal',
     loadChildren: () =>
       import('./tramites/130102/octava-temporal.module').then(
