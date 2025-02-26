c2c610eaa17edbe779ca300bbbdf27d2fae003b6
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-02-26 11:19:49 +0530

    Added table compo
    Added table compo

--- apps/se/src/app/application/tramites/90305/component/consultad-domicilios-90305/consultad-domicilios-90305.component.ts
+++ apps/se/src/app/application/tramites/90305/component/consultad-domicilios-90305/consultad-domicilios-90305.component.ts
@@ -3,14 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TituloComponent } from "../../../../../../../../../libs/shared/data-access-user/src/tramites/components/titulo/titulo.component";
 import { CatalogoSelectComponent } from "../../../../../../../../../libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component";
 import { Catalogo } from '@ng-mf/data-access-user';
-
 import estado from "../../../../../../../../../libs/shared/theme/assets/json/90305/estado.json";
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-consultad-domicilios-90305',
   standalone: true,
-  imports: [CommonModule, TituloComponent, CatalogoSelectComponent,ReactiveFormsModule],
+  imports: [CommonModule, TituloComponent, CatalogoSelectComponent,TablaDinamicaComponent,ConfiguracionColumna,ReactiveFormsModule],
   templateUrl: './consultad-domicilios-90305.component.html',
   styleUrl: './consultad-domicilios-90305.component.scss',
 })
@@ -18,6 +16,7 @@ export class ConsultadDomicilios90305Component implements OnInit {
   estadoJson: Catalogo[] = estado;
   formConsulta! :FormGroup;
   constructor(private fb : FormBuilder){}
+
   ngOnInit():void{
 
   this.formConsulta = this.fb.group(
