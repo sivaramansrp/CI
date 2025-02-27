ca2951fc171bc959c5eb1ae7b979d1ab7d09e383
Moses Ramadoss <moses.ramadoss@ilink-systems.com>
2025-02-27 23:19:53 +0530

    'Refs#5701-Akita-state-management-fixed'

--- apps/aga/src/app/application/estados/queries/seccion.query.ts
+++ apps/aga/src/app/application/estados/queries/seccion.query.ts
@@ -1,9 +1,9 @@
-import { SeccionState, SeccionStore } from 'apps/aga/src/app/application/estados/seccion.store';
+import { SeccionState, SeccionStore } from '../../estados/seccion.store';
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
 
 @Injectable({providedIn: 'root'})
-export class SeccionLibQuery extends Query<SeccionState> {
+export class SeccionQuery extends Query<SeccionState> {
 
     /**
      * Selecciona el estatdo completo de la sección
