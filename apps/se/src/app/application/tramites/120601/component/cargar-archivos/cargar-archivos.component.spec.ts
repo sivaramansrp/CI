9941161a1c2b1b5d60db3acd119a4ea4da35b1f1
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-02-27 15:07:38 +0530

    jest unit test case code

--- apps/se/src/app/application/tramites/120601/component/cargar-archivos/cargar-archivos.component.spec.ts
+++ apps/se/src/app/application/tramites/120601/component/cargar-archivos/cargar-archivos.component.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { AlertComponent } from '@ng-mf/data-access-user';
+import { TituloComponent } from '@ng-mf/data-access-user';
 import { CargarArchivosComponent } from './cargar-archivos.component';
 
 describe('CargarArchivosComponent', () => {
@@ -7,15 +10,25 @@ describe('CargarArchivosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CargarArchivosComponent],
+      imports: [CommonModule, AlertComponent, TituloComponent],
+      declarations: [CargarArchivosComponent],
     }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(CargarArchivosComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the correct initial message', () => {
+    const EXPECTED_MESSAGE =
+      'La solicitud ha quedado registrada ei numero temporal 202759017. Este no tiene validez legal y sirve solamente para efectos de identificar tu solicitud. Un folio oficial le será asignado a la solicitud al momento en que esta sea firmada.';
+    
+    expect(component.cargararchivos).toBe(EXPECTED_MESSAGE);
   });
 });
--- apps/se/src/app/application/tramites/120601/component/domicilio/domicilio.component.spec.ts
+++ apps/se/src/app/application/tramites/120601/component/domicilio/domicilio.component.spec.ts
@@ -1,21 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 import { DomicilioComponent } from './domicilio.component';
+import { FormulariosService } from '@ng-mf/data-access-user';
+import { SolicitanteService } from '@ng-mf/data-access-user';
+import { of } from 'rxjs';
 
 describe('DomicilioComponent', () => {
   let component: DomicilioComponent;
-  let fixture: ComponentFixture<DomicilioComponent>;
+  let formServices: Partial<FormulariosService>;
+  let solicitanteServicio: Partial<SolicitanteService>;
 
   beforeEach(async () => {
+    formServices = {
+      obtenerNombresCamposForm: jest.fn().mockReturnValue([]),
+      agregarValorCampoDesactivados: jest.fn(),
+    };
+
+    solicitanteServicio = {
+      getDatosGenerales: jest.fn().mockReturnValue(of({ data: JSON.stringify({ domicilioFiscal: {} }) })),
+    };
+
     await TestBed.configureTestingModule({
-      imports: [DomicilioComponent],
+      providers: [
+        FormBuilder,
+        { provide: FormulariosService, useValue: formServices },
+        { provide: SolicitanteService, useValue: solicitanteServicio },
+      ],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(DomicilioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new DomicilioComponent(
+      TestBed.inject(SolicitanteService),
+      new FormBuilder(),
+      TestBed.inject(FormulariosService)
+    );
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 });
