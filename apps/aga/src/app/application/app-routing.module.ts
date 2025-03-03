2031f58e00cdad7169e46f189c05cccf894ea2e9
swamini1212-ilink <swamini.godse@ilink-systems.com>
2025-03-03 16:31:49 +0530

    removed the se procedure routing from aga

--- apps/aga/src/app/application/app-routing.module.ts
+++ apps/aga/src/app/application/app-routing.module.ts
@@ -26,14 +26,7 @@ const ROUTES: Routes = [
     path: 'importante',
     loadChildren: () =>
       import('./tramites/301/pantallas.module').then((m) => m.Pantallas301Module),
-  },
-  {
-    path: 'submanufactureras-immex',
-    loadChildren: () =>
-      import('../../../../se/src/app/application/tramites/80207/submanufactureras-extension.module').then(
-        (m) => m.SubmanufacturerasExtentionModule
-      ),
-    },
+  }
 ];
 
 @NgModule({
