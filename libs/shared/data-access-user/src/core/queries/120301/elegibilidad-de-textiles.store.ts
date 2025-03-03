8dc9af45deb6d179158cc536e30356a40bb49820
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-03-03 16:30:44 +0530

    fix: removed the comments

--- libs/shared/data-access-user/src/core/queries/120301/elegibilidad-de-textiles.store.ts
+++ libs/shared/data-access-user/src/core/queries/120301/elegibilidad-de-textiles.store.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ElegibilidadDeTextilesState } from "../../models/120301/elegibilidad-de-textiles.model";
 import { Query } from "@datorama/akita";
-// import { ElegibilidadDeTextilesStore } from "../../../../../../../apps/se/src/app/application/store/120301/elegibilidad-de-textiles.store";
+import { ElegibilidadDeTextilesStore } from "../../../../../../../apps/se/src/app/application/store/120301/elegibilidad-de-textiles.store";
 
 
 @Injectable({ providedIn: 'root' })
