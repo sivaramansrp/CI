107f883315062e5ce25a85f731c0d10b7259168f
Moses Ramadoss <moses.ramadoss@ilink-systems.com>
2025-03-03 11:17:36 +0530

    'Refs#130160-webpack-config-added'

--- apps/agace/src/app/application/app-routing.module.ts
+++ apps/agace/src/app/application/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'pantallas-extraordinarios' },
+  { path: '', pathMatch: 'full', redirectTo: 'antecesor' },
   {
     path: 'antecesor',
     loadChildren: () =>
