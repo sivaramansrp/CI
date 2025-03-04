781c8c021b36ca762d9a8aaa965a5140627d612d
binaydas8 <binaykumar.das@ilink-systems.com>
2025-03-04 12:45:21 +0530

    but department created
    but department created

---
+++ apps/but/src/app/application/seleccion-tramite/seleccion-tramite.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seleccion-tramite',
+  templateUrl: './seleccion-tramite.component.html',
+})
+export class SeleccionTramiteComponent {}
