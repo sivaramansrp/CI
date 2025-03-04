e5cbd71b9183db7f34082fc279b48ca0afb6f42f
binaydas8 <binaykumar.das@ilink-systems.com>
2025-03-04 12:57:26 +0530

    herself department
    herself department

---
+++ apps/herself/webpack.config.ts
@@ -0,0 +1,59 @@
+// import { withModuleFederation } from '@nx/angular/module-federation';
+// import config from './module-federation.config';
+
+// export default withModuleFederation(config);
+
+// import { withModuleFederation } from '@nx/angular/module-federation';
+// import config from './module-federation.config';
+
+// module.exports = withModuleFederation(config);
+
+
+const { ModuleFederationPlugin } = require('webpack').container;
+const mf = require('@angular-architects/module-federation/webpack');
+const path = require('path');
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, '../../tsconfig.base.json'), [
+ /* mapped paths to share */
+]);
+
+module.exports = {
+ output: {
+  uniqueName: 'herself',
+  publicPath: 'auto',
+  scriptType: 'text/javascript'
+ },
+ optimization: {
+  runtimeChunk: false
+ },
+ resolve: {
+  alias: {
+   ...sharedMappings.getAliases()
+  }
+ },
+ plugins: [
+  new ModuleFederationPlugin({
+   name: 'herself',
+   filename: 'remoteAppEntry.js',
+   exposes: {
+    './Module': 'apps/herself/src/app/application/app.module.ts',
+   },
+   shared: share({ 
+    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular-architects/module-federation': {
+     singleton: true,
+     strictVersion: true,
+     requiredVersion: 'auto'
+    },
+
+    ...sharedMappings.getDescriptors()
+   })
+  }),
+  sharedMappings.getPlugin()
+ ]
+};
