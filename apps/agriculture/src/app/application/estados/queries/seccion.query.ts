55528a23ec3c8ee2de62604539a8dfb7e0e04324
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-03-05 11:02:06 +0530

    Changed agricultura folder to agriculture

--- apps/agriculture/src/app/application/estados/queries/seccion.query.ts
+++ apps/agriculture/src/app/application/estados/queries/seccion.query.ts
@@ -1,4 +1,4 @@
-import { SeccionState, SeccionStore } from '../../estados/seccion.store';
+import { SeccionState, SeccionStore } from '../seccion.store';
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
 
