0f7f38199907af80c2d20c03a6d405d1a6d6ff93
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-03-05 17:30:20 +0530

    updated seleccion-trimites under agriculture department

--- apps/agriculture/src/app/application/app-routing.module.ts
+++ apps/agriculture/src/app/application/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeleccionTramiteComponent } from './seleccion-tramite/seleccion-tramite.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'pantallas-extraordinarios' },
+  { path: '', pathMatch: 'full', redirectTo: 'seleccion-tramite' },
+  {
+    path: 'seleccion-tramite',
+    component: SeleccionTramiteComponent
+  },
   {
     path: 'pantallas-extraordinarios',
     loadChildren: () =>
