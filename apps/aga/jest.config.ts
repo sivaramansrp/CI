fcd55208ffbf7c48e7cf7effd67b107fe1afab17
parmeshwars2025 <parmeshwar.vasantrao@ilink-systems.com>
2025-03-05 18:37:52 +0530

    revert package.json file

--- apps/aga/jest.config.ts
+++ apps/aga/jest.config.ts
@@ -13,8 +13,7 @@ export default {
       },
     ],
   },
-  // transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
-  transformIgnorePatterns: [],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
