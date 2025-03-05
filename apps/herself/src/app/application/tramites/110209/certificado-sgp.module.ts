9e651f4f88a8994f3c10bfdee4ccfe201d53102c
balamurugan-ilink <balamurugan.govinthan@ilink-systems.com>
2025-03-05 14:59:56 +0530

    tab added

--- apps/herself/src/app/application/tramites/110209/certificado-sgp.module.ts
+++ apps/herself/src/app/application/tramites/110209/certificado-sgp.module.ts
@@ -7,13 +7,15 @@ import { CertificadoSGPRoutingModule } from './certificado-sgp-routing.module';
 import { DetallesDelTransporteComponent } from "./components/detalles-del-transporte/detalles-del-transporte.component";
 import { SolicitanteComponent } from "@ng-mf/data-access-user";
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
+import { TransporteComponent } from "./components/transporte/transporte.component";
 import { WizardComponent } from '@libs/shared/data-access-user/src';
+
 
 
 
 
 @NgModule({
   declarations: [SolicitudPageComponent, CapturarSolicitudComponent],
-  imports: [CommonModule, CertificadoSGPRoutingModule, WizardComponent, SolicitanteComponent, DetallesDelTransporteComponent],
+  imports: [CommonModule, CertificadoSGPRoutingModule, WizardComponent, SolicitanteComponent, DetallesDelTransporteComponent, TransporteComponent],
 })
 export class CertificadoSGPModule {}
---
+++ apps/herself/src/app/application/tramites/110209/components/transporte/transporte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-transporte',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transporte.component.html',
+  styleUrl: './transporte.component.scss',
+})
+export class TransporteComponent {}
