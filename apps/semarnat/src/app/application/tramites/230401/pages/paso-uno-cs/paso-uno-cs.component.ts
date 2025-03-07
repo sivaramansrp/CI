2605a41cc08e1e6a45b558ee9a41270d61ae0f55
Muneeshwaran Nagarajan <muneeshwaran.n@ilink-systems.com>
2025-03-07 15:20:20 +0530

    Trámite (230401) - Pod 3 - Sprint 2 - Continue button changes

--- apps/semarnat/src/app/application/tramites/230401/pages/paso-uno-cs/paso-uno-cs.component.ts
+++ apps/semarnat/src/app/application/tramites/230401/pages/paso-uno-cs/paso-uno-cs.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SECCIONES_TRAMITE_230401 } from '../../enum/pantallas-constante.enum';
+import { SeccionLibStore } from '@libs/shared/data-access-user/src/core/estados/seccion.store';
 
 @Component({
   selector: 'app-paso-uno-cs',
   templateUrl: './paso-uno-cs.component.html',
 })
-export class PasoUnoCsComponent {
+export class PasoUnoCsComponent implements OnInit{
   indice: number = 1;
+
+  constructor(private seccionStore: SeccionLibStore){
+    // do nothing
+  }
+  ngOnInit(): void {
+    this.asignarSecciones();
+  }
 
   /**
    * Selecciona una pestaña específica.
@@ -13,5 +22,23 @@ export class PasoUnoCsComponent {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+  /**
+  * Método para asignar las secciones existentes al stored
+  */
+  private asignarSecciones(): void {
+    const SECCIONES: boolean[] = [];
+    const FORMA_VALIDA: boolean[] = [];
+    const PREDETERMINADO = SECCIONES_TRAMITE_230401
+    for (const LLAVE_SECCION in PREDETERMINADO.PASO_1) {
+      if (Object.prototype.hasOwnProperty.call(PREDETERMINADO.PASO_1, LLAVE_SECCION)) {
+        // @ts-expect-error - fix this
+        SECCIONES.push(PREDETERMINADO.PASO_1[LLAVE_SECCION]);
+        FORMA_VALIDA.push(false);
+      }
+    }
+    this.seccionStore.establecerSeccion(SECCIONES);
+    this.seccionStore.establecerFormaValida(FORMA_VALIDA);
   }
 }
