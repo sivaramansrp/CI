527e06b3504923a725d6c3959535d45798c962da
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-03-07 17:46:53 +0530

    Refactor components to remove hostID assignment and improve test setup for ImportacionDeAcuiculturaService

--- apps/agriculture/src/app/application/tramites/220203/services/220203/importacion-de-acuicultura.service.spec.ts
+++ apps/agriculture/src/app/application/tramites/220203/services/220203/importacion-de-acuicultura.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ImportacionDeAcuiculturaService } from './importacion-de-acuicultura.service';
 
@@ -6,7 +7,9 @@ describe('ImportacionDeAcuiculturaService', () => {
   let service: ImportacionDeAcuiculturaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Add HttpClientTestingModule here
+    });
     service = TestBed.inject(ImportacionDeAcuiculturaService);
   });
 
--- libs/shared/data-access-user/src/tramites/components/btn-continuar/btn-continuar.component.ts
+++ libs/shared/data-access-user/src/tramites/components/btn-continuar/btn-continuar.component.ts
@@ -28,7 +28,7 @@ interface AccionBoton {
   imports: [],
   templateUrl: './btn-continuar.component.html',
   styleUrl: './btn-continuar.component.scss',
-  host: { hostID: crypto.randomUUID().toString() },
+  host: {},
 })
 export class BtnContinuarComponent {
   @Input({ required: true }) datos!: DatosPasos;
