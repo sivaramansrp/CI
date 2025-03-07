7eab6bd1aeff8f1a1770c7c727a424d9f3a5420f
Sohel Arif <sohel.arif@ilink-systems.com>
2025-03-07 11:14:14 +0530

    Added Routing

--- apps/se/src/app/application/app-routing.module.ts
+++ apps/se/src/app/application/app-routing.module.ts
@@ -1,7 +1,8 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-import { RouterModule, Routes } from '@angular/router';
+/* eslint-disable sort-imports */
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { SeleccionTramiteComponent } from './seleccion-tramite/seleccion-tramite.component';
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'seleccion-tramite' },
   {
@@ -56,6 +57,13 @@ const routes: Routes = [
         (m) => m.ImmexAmpliacionSensiblesModule
       ),
   },
+  {
+    path: 'immex-registro-solicitud-modality',
+    loadChildren: () =>
+      import('./tramites/80203/immex-registro-de-solicitud-modalidad.modulo').then(
+        (m) => m.ImmexRegistroDeSolicitudModalityModule
+      )
+    },
   {
     path: 'modificacion',
     loadChildren: () =>
