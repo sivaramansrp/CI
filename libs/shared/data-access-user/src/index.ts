76336dd9865a0451b2e2f4be09c3006099efd4de
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-03-07 11:00:42 +0530

    refactor: remove unused exports from user data access module

--- libs/shared/data-access-user/src/index.ts
+++ libs/shared/data-access-user/src/index.ts
@@ -83,10 +83,6 @@ export { TEXTOS as TEXTOS_220501 } from './tramites/constantes/220501/texto-enum
 export { CATALOGOS_ID as CATALOGOS_ID_220501 } from './tramites/constantes/220501/constantes';
 export { FECHA_INSPECCION_INPUT as FECHA_INSPECCION_INPUT_220502 } from './tramites/constantes/220502/texto-enum';
 export * from './tramites/components/tabla-dinamica/tabla-dinamica.component';
-export {
-  TEXTOS_REQUISITOS as TEXTOS_REQUISITOS_80202,
-  MENSAJE_DE_EXITO_ETAPA_UNO,
-} from './tramites/constantes/certificado-zoosanitario.enum';
 export * from './core/enums/tabla-seleccion.enum';
 export * from './tramites/constantes/110102/datos-tratados-acuerdos.enum';
 export * from './core/services/110102/datos-tratados-acuerdos/datosTratadosacuerdos.service';
