267b0f32ee7aa85259d0bb0b3b8d867f1a8c047e
Aditi <aditi.patil@ilink-systems.com>
2025-03-10 11:14:47 +0530

    added single method for all 5 input fields

--- apps/se/src/app/application/tramites/110218/components/transporte/transporte.component.ts
+++ apps/se/src/app/application/tramites/110218/components/transporte/transporte.component.ts
@@ -1,10 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { OnDestroy } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { TituloComponent } from '@ng-mf/data-access-user';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { Tramite110218Store } from '../../estados/tramites/tramite110218.store';
+
 import { Tramite110218Query } from '../../estados/queries/tramite110218.query';
+
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-transporte',
@@ -13,7 +26,7 @@ import { Observable } from 'rxjs';
   templateUrl: './transporte.component.html',
   styleUrl: './transporte.component.scss',
 })
-export class TransporteComponent implements OnInit{
+export class TransporteComponent implements OnInit, OnDestroy{
    detallestransporte : FormGroup ;
    
    puertodeEmbarque$: Observable<string | null> = this.tramite110218Query.puertodeEmbarque$;
@@ -22,7 +35,8 @@ export class TransporteComponent implements OnInit{
    nombredelaEmbarcación$: Observable<string | null> = this.tramite110218Query.nombredelaEmbarcación$;
    númerodeVuelo$: Observable<string | null> = this.tramite110218Query.númerodeVuelo$;
 
-
+   private destroyed$ = new Subject<void>();
+   
     constructor(private fb: FormBuilder,private tramite110218Store: Tramite110218Store, 
       private tramite110218Query: Tramite110218Query) { 
       this.detallestransporte = this.fb.group({
@@ -34,52 +48,54 @@ export class TransporteComponent implements OnInit{
       })
     }
     ngOnInit(): void {
-      this.puertodeEmbarque$.subscribe((puertodeEmbarque) => {
-        if(puertodeEmbarque){
-          this.detallestransporte.get('puertodeEmbarque')?.setValue(puertodeEmbarque);
-        }
-      });
-      this.puertodeDesembarque$.subscribe((puertodeDesembarque) => {
-        if(puertodeDesembarque){
-          this.detallestransporte.get('puertodeDesembarque')?.setValue(puertodeDesembarque);
-        }
-      });
-      this.nombredelaEmbarcación$.subscribe((nombredelaEmbarcación) => {
-        if(nombredelaEmbarcación){
-          this.detallestransporte.get('nombredelaEmbarcación')?.setValue(nombredelaEmbarcación);
-        }
-      });
-      this.númerodeVuelo$.subscribe((númerodeVuelo) => {
-        if(númerodeVuelo){
-          this.detallestransporte.get('númerodeVuelo')?.setValue(númerodeVuelo);
-        }
-      });
-      
-      this.puertodeTránsito$.subscribe((puertodeTránsito) => {
-        if(puertodeTránsito){
-          this.detallestransporte.get('puertodeTránsito')?.setValue(puertodeTránsito);
-        }
+      this.subscribeToStoreChanges();
+    }
+    subscribeToStoreChanges(): void {
+      const OBSERVABLES = {
+        puertodeEmbarque: this.puertodeEmbarque$,
+        puertodeDesembarque: this.puertodeDesembarque$,
+        nombredelaEmbarcación: this.nombredelaEmbarcación$,
+        númerodeVuelo: this.númerodeVuelo$,
+        puertodeTránsito: this.puertodeTránsito$,
+      };
+  
+      Object.entries(OBSERVABLES).forEach(([controlName, OBSERVABLES$]) => {
+        OBSERVABLES$.pipe(takeUntil(this.destroyed$)).subscribe((value) => {
+          if (value) {
+            this.detallestransporte.get(controlName)?.setValue(value);
+          }
+        });
       });
     }
 
-    onPuertodeEmbarque():void{
-      const PUERTODE_EMBARQUE = this.detallestransporte.get('puertodeEmbarque')?.value;
-      this.tramite110218Store.setpuertodeEmbarque(PUERTODE_EMBARQUE);
+    ngOnDestroy(): void {
+      this.destroyed$.next();
+      this.destroyed$.complete();
     }
-    onPuertodeDesembarque():void{
-      const PUERTODE_DESEMBARQUE = this.detallestransporte.get('puertodeDesembarque')?.value;
-      this.tramite110218Store.setpuertodeDesembarque(PUERTODE_DESEMBARQUE);
+
+    onDetallestransporteChange(controlName: string): void {
+      const VALUE = this.detallestransporte.get(controlName)?.value;
+  
+      switch (controlName) {
+        case 'puertodeEmbarque':
+          this.tramite110218Store.setpuertodeEmbarque(VALUE);
+          break;
+        case 'puertodeDesembarque':
+          this.tramite110218Store.setpuertodeDesembarque(VALUE);
+          break;
+        case 'nombredelaEmbarcación':
+          this.tramite110218Store.setnombredelaEmbarcación(VALUE);
+          break;
+        case 'númerodeVuelo':
+          this.tramite110218Store.setnúmerodeVuelo(VALUE);
+          break;
+        case 'puertodeTránsito':
+          this.tramite110218Store.setPuertodeTránsito(VALUE);
+          break;
+        default:
+          console.warn(`Unhandled control name: ${controlName}`);
+          break;
+      }
     }
-    onNombredelaEmbarcacion():void{
-      const NOMBREDELA_EMBARCACION = this.detallestransporte.get('nombredelaEmbarcación')?.value;
-      this.tramite110218Store.setnombredelaEmbarcación(NOMBREDELA_EMBARCACION);
-    }
-    onNumerodeVuelo():void{
-      const NUMERODE_VUELO = this.detallestransporte.get('númerodeVuelo')?.value;
-      this.tramite110218Store.setnúmerodeVuelo(NUMERODE_VUELO);
-    }
-    onPuertodeTransito():void{
-      const PUERTODE_TRANSITO = this.detallestransporte.get('puertodeTránsito')?.value;
-      this.tramite110218Store.setPuertodeTránsito(PUERTODE_TRANSITO);
-    }
-}
+  }
+
