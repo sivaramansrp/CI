1a7441e78ecd27fbf61de335550856934d19879d
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-03-11 15:19:55 +0530

    added solicitante component and paso 2 component

--- apps/se/src/app/application/tramites/130102/octava-temporal.module.ts
+++ apps/se/src/app/application/tramites/130102/octava-temporal.module.ts
@@ -24,11 +24,12 @@ import { TituloComponent } from 'libs/shared/data-access-user/src/tramites/compo
 import { WizardComponent } from 'libs/shared/data-access-user/src/tramites/components/wizard/wizard.component';
 
 import { provideHttpClient } from '@angular/common/http';
+
 import { SolicitanteComponent } from '@ng-mf/data-access-user';
 
 import { FirmaElectronicaComponent } from '@libs/shared/data-access-user/src';
 
-
+import { PasoTresComponent } from './component/paso-tres/paso-tres.component' 
 
 @NgModule({
   declarations: [
@@ -51,7 +52,8 @@ import { FirmaElectronicaComponent } from '@libs/shared/data-access-user/src';
     UsoEspicificoComponent,
     WizardComponent,
     FirmaElectronicaComponent, 
-    SolicitanteComponent   
+    SolicitanteComponent,
+    PasoTresComponent
   ],
   providers: [
     provideHttpClient(),
