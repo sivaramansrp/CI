7863c3291b16ba787ab6361d320c591d4620fe01
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-03-11 15:51:06 +0530

    Add DatosDeLa component with template, styles, and tests

---
+++ apps/cofepris/src/app/application/tramites/260211/components/datosDeLa/datosDeLa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-datos-de-la',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './datosDeLa.component.html',
+  styleUrl: './datosDeLa.component.css',
+})
+export class DatosDeLaComponent {}
