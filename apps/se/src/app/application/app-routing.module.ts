4344cdb6fa1cb4473afce5d3f29237501ac1d4cd
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-03-12 10:28:07 +0530

    fix: Route import fixed

--- apps/se/src/app/application/app-routing.module.ts
+++ apps/se/src/app/application/app-routing.module.ts
@@ -1,9 +1,6 @@
 /* eslint-disable sort-imports */
 import { NgModule } from '@angular/core';
-
 import { RouterModule, Routes } from '@angular/router';
-
-import { NgModule } from '@angular/core';
 import { SeleccionTramiteComponent } from './seleccion-tramite/seleccion-tramite.component';
 const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'seleccion-tramite' },
