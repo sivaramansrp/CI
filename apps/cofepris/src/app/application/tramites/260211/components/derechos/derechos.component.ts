949c7e3076654b1a70e6d27d53868b3653583b25
varshakatre15 <varsha.katre@ilink-systems.com>
2025-03-12 14:29:17 +0530

    json data

--- apps/cofepris/src/app/application/tramites/260211/components/derechos/derechos.component.ts
+++ apps/cofepris/src/app/application/tramites/260211/components/derechos/derechos.component.ts
@@ -2,21 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroup } from '@angular/forms';
 // import { CatalogosSelect } from '@ng-mf/data-access-user';
-import { TituloComponent } from '@ng-mf/data-access-user';
+import { Catalogo, TituloComponent } from '@ng-mf/data-access-user';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CatalogoSelectComponent } from '@ng-mf/data-access-user';
+
+
 
 @Component({
   selector: 'app-derechos',
   standalone: true,
-  imports: [CommonModule,TituloComponent,ReactiveFormsModule],
+  imports: [CommonModule,TituloComponent,ReactiveFormsModule,CatalogoSelectComponent],
   templateUrl: './derechos.component.html',
   styleUrl: './derechos.component.css',
 })
 export class DerechosComponent {
-
+  // aduanas: Catalogo[];
   derechosForm!:FormGroup;
 
   validarTransporteFormulario(){
-
-  }
+}
 }
