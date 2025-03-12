8f6b8a219996a046015a12ece97c11c4c09e1a21
mjimenezm <mjimenezm@desarrollo-ultrasist.com.mx>
2025-03-12 12:21:05 -0600

    cambios 12

--- apps/funcionario/src/app/application/components/evaluar-solicitud/capturar-requerimiento/capturar-requerimiento.component.ts
+++ apps/funcionario/src/app/application/components/evaluar-solicitud/capturar-requerimiento/capturar-requerimiento.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import data  from '../../../../../../../../libs/shared/theme/assets/json/funcionario/cat-tipo-requerimiento.json';
+import { Catalogo } from '@libs/shared/data-access-user/src';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-capturar-requerimiento',
@@ -10,5 +14,31 @@ import { CommonModule } from '@angular/common';
 })
 export class CapturarRequerimientoComponent {
 
+ /**
+   * Catálogo de tipo de requerimiento
+   */
+ catTipoRequerimiento!: Catalogo[];
+
+  ngOnInit(): void {
+    this.catTipoRequerimiento = data;
+  }
+
+  formRequerimiento: FormGroup = this.fb.group({
+    tipoRequerimiento: ['', [Validators.required]],
+    justificacionReq: ['', [Validators.required]]
+
+  });
+
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+  ) { }
+
+  /**
+   * Método para establecer el tipo de requerimiento seleccionado
+   */
+
+  tipoRequerimientoSeleccionado() {
+    this.toastr.success('Tipo de requerimiento seleccionado');
+  }
   
 }
