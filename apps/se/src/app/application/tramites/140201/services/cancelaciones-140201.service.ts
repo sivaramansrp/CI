17aaa7ebaebdd5a5a2d3f9cb879dafbd860d5632
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-03-13 12:11:38 +0530

    issue fixed

--- apps/se/src/app/application/tramites/140201/services/cancelaciones-140201.service.ts
+++ apps/se/src/app/application/tramites/140201/services/cancelaciones-140201.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+
+import { CancellationOfAuthorizations } from '../models/cancelacions.model'
 
 @Injectable({
     providedIn: 'root'
@@ -8,4 +11,7 @@ export class Cancelaciones140201Service{
     constructor(private http: HttpClient) {
         //constructor
      }
+      getCancelacionDeAutorizaciones() : Observable<CancellationOfAuthorizations[]> {
+         return this.http.get<CancellationOfAuthorizations[]>('assets/json/140201/cancelacion-de-autorizaciones-140201.json');
+       }
 }
