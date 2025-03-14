410a52d06881379df1cae69a9909f2b3ebd67b4c
David Emmanuel (Ultrasist) <idex3037@cnbv.gob.mx>
2025-03-14 16:50:16 -0600

    ADD - agrega enum para mensajes de documentos

---
+++ libs/shared/data-access-user/src/core/enums/mensajes-documentos.enum.ts
@@ -0,0 +1,6 @@
+export enum MensajesDocumentos {
+  ONLYPDF = 'Solo se aceptan archivos pdf',
+  MAXSIZE = 'El tamaño del documento que intenta cargar excede el tamaño permitido',
+  ERRORUPLOAD = 'Error al subir el documento',
+  UPLOAD = 'Documento subido'
+}
