4461cff78ce00bf52d87b0263612a99fdad2cf2f
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-17 12:00:34 +0530

    initial drop down added

--- apps/se/src/app/application/tramites/130106/components/datos-de-la-solicitude/datos-de-la-solicitude.component.ts
+++ apps/se/src/app/application/tramites/130106/components/datos-de-la-solicitude/datos-de-la-solicitude.component.ts
@@ -1,12 +1,22 @@
+import {FormGroup,ReactiveFormsModule} from '@angular/forms';
 import { Component } from '@angular/core';
+import { TituloComponent } from '@ng-mf/data-access-user';
 
 @Component({
   selector: 'app-datos-de-la-solicitude',
   standalone: true,
-  imports: [],
+  imports: [TituloComponent,ReactiveFormsModule],
   templateUrl: './datos-de-la-solicitude.component.html',
   styleUrl: './datos-de-la-solicitude.component.scss'
 })
 export class DatosDeLaSolicitudeComponent {
+
+    /**
+     * Representa el formulario del componente.
+     * Se espera que esta propiedad sea del tipo 'FormGroup'.
+     *
+     * @property {FormGroup} formulario - El formulario del componente.
+     */
+    public formulario!: FormGroup;
 
 }
