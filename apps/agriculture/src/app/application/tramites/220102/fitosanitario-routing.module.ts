4f8ac0e9718545834b3e925896ad4ba52bf407e3
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-03-17 13:53:08 +0530

    Add Fitosanitario module with routing, components, and update links in SeleccionTramite

--- apps/agriculture/src/app/application/tramites/220102/fitosanitario-routing.module.ts
+++ apps/agriculture/src/app/application/tramites/220102/fitosanitario-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'invocarModulo',
+    component: SolicitudPageComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'invocarModulo',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- apps/agriculture/src/app/application/tramites/220102/fitosanitario.module.ts
+++ apps/agriculture/src/app/application/tramites/220102/fitosanitario.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FitosanitarioRoutingModule } from './fitosanitario-routing.module';
+import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
+import { DatosMercanciaComponent } from './components/datos-mercancia/datos-mercancia.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PasoDosComponent,
+    PasoUnoComponent,
+    PasoTresComponent,
+    DatosMercanciaComponent
+  ],
   imports: [
     CommonModule,
     FitosanitarioRoutingModule
