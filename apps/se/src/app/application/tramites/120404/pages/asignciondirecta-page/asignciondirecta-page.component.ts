e68d51fa48f36768818ff66ff3ec8f9df59fd0f7
balamurugan-ilink <balamurugan.govinthan@ilink-systems.com>
2025-03-17 16:04:55 +0530

    review comments fixed

--- apps/se/src/app/application/tramites/120404/pages/asignciondirecta-page/asignciondirecta-page.component.ts
+++ apps/se/src/app/application/tramites/120404/pages/asignciondirecta-page/asignciondirecta-page.component.ts
@@ -6,10 +6,11 @@
  */
 
 import { Component, ViewChild } from '@angular/core';
+import { ASIGNACION } from '../../constants/asignacion.enum';
 
 import { DatosPasos, WizardComponent } from '@ng-mf/data-access-user';
 import { ListaPasosWizard } from '@ng-mf/data-access-user';
-import { ASIGNACION } from '../../constants/asignacion.enum';
+
 
 
 
