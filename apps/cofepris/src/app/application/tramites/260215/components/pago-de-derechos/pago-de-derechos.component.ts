acf19be36285f93b515c47d17ed10c74f5e030fe
atripathi860 <amit.tripathi@ilink-systems.com>
2025-03-17 15:00:49 +0530

    pago de derechos

--- apps/cofepris/src/app/application/tramites/260215/components/pago-de-derechos/pago-de-derechos.component.ts
+++ apps/cofepris/src/app/application/tramites/260215/components/pago-de-derechos/pago-de-derechos.component.ts
@@ -1,11 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Tramite260215Query } from '../../estados/queries/tramite260215.query';
+import {
+  Solicitud260215State,
+  Tramite260215Store,
+} from '../../estados/tramites/tramite260215.store';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Subscription, Subject, takeUntil, map } from 'rxjs';
+import { TituloComponent } from '../../../../../../../../../libs/shared/data-access-user/src/tramites/components/titulo/titulo.component';
+import { CatalogoSelectComponent } from '../../../../../../../../../libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component';
+import { CatalogosSelect } from '@libs/shared/data-access-user/src/core/models/shared/components.model';
 
 @Component({
   selector: 'app-pago-de-derechos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TituloComponent,
+    CatalogoSelectComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './pago-de-derechos.component.html',
   styleUrl: './pago-de-derechos.component.scss',
 })
-export class PagoDeDerechosComponent {}
+export class PagoDeDerechosComponent implements OnInit, OnDestroy {
+  /**
+   * Formulario reactivo para la sección 301.
+   * @type {FormGroup}
+   */
+  FormSolicitud!: FormGroup;
+
+  /**
+   * Suscripción a los cambios en el formulario react
+   */
+  private subscription: Subscription = new Subscription();
+
+  /**
+   * Estado de la solicitud de la sección 301.
+   */
+  public solicitudState!: Solicitud260215State;
+
+  /**
+   * Subject para notificar la destrucción del componente.
+   */
+  private destroyNotifier$: Subject<void> = new Subject();
+
+  constructor(
+    private fb: FormBuilder,
+    private tramite301Store: Tramite260215Store,
+    private tramite301Query: Tramite260215Query
+  ) {
+    // add initialization code here
+  }
+
+  public bancoCatalogo: CatalogosSelect = {
+    labelNombre: 'Banco',
+    required: true,
+    primerOpcion: 'Selecciona un valor',
+    catalogos: [],
+  };
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.tramite301Query.selectSolicitud$
+        .pipe(
+          takeUntil(this.destroyNotifier$),
+          map((seccionState) => {
+            this.solicitudState = seccionState;
+          })
+        )
+        .subscribe()
+    );
+
+    this.FormSolicitud = this.fb.group({
+      datosImportadorExportador: this.fb.group({
+        claveDeReferencia: [this.solicitudState?.claveDeReferencia],
+        cadenaDependencia: [this.solicitudState?.cadenaDependencia],
+        banco: [this.solicitudState?.banco],
+        llaveDePago: [this.solicitudState?.llaveDePago],
+        fechaPago: [this.solicitudState?.fechaPago],
+        importePago: [this.solicitudState?.importePago],
+      }),
+    });
+  }
+
+  setValoresStore(
+    form: FormGroup,
+    campo: string,
+    metodoNombre: keyof Tramite260215Store
+  ): void {
+    const VALOR = form.get(campo)?.value;
+    (this.tramite301Store[metodoNombre] as (value: any) => void)(VALOR);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  get datosImportadorExportador(): FormGroup {
+    return this.FormSolicitud.get('datosImportadorExportador') as FormGroup;
+  }
+}
