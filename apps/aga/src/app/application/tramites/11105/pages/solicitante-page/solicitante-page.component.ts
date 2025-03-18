03b68c9de2a24e87f4619f413d89c4cc2fddc76c
shekhar-kalluri-ilink <shekhar.kalluri@ilink-systems.com>
2025-03-18 09:51:30 +0530

    "11105  1st page changes"

---
+++ apps/aga/src/app/application/tramites/11105/pages/solicitante-page/solicitante-page.component.ts
@@ -0,0 +1,133 @@
+import { Component, ViewChild } from '@angular/core';
+import { Chofer40102Store, Choferesnacionales40102State } from '../../estados/tramite40102.store';
+import { Chofer40102Query } from '../../estados/tramite40102.query';
+import { Subject, map, takeUntil } from 'rxjs';
+import { DatosPasos } from '@ng-mf/data-access-user';
+import { ListaPasosWizard } from '@ng-mf/data-access-user';
+import { PASOS } from '@ng-mf/data-access-user';
+import { SECCIONES_TRAMITE_5701 } from '@ng-mf/data-access-user';
+import { WizardComponent } from '@ng-mf/data-access-user';
+
+/**
+ * Interfaz para definir la estructura de una acción de botón.
+ */
+interface AccionBoton {
+  accion: string;
+  valor: number;
+}
+
+/**
+ * Componente para gestionar la página del solicitante.
+ */
+@Component({
+  selector: 'app-solicitante-page',
+  templateUrl: './solicitante-page.component.html',
+  styleUrl: './solicitante-page.component.scss',
+})
+export class SolicitantePageComponent {
+  /**
+   * Lista de pasos del wizard.
+   */
+  pasos: Array<ListaPasosWizard> = PASOS.slice(0, 2);
+
+  /**
+   * Índice del paso actual.
+   */
+  indice: number = 1;
+
+  /**
+   * Estado de la sección de choferes nacionales.
+   */
+  public seccion!: Choferesnacionales40102State;
+
+  /**
+   * Notificador para destruir las suscripciones.
+   */
+  private destroyNotifier$: Subject<void> = new Subject();
+
+  /**
+   * Referencia al componente del wizard.
+   */
+  @ViewChild(WizardComponent) wizardComponent!: WizardComponent;
+
+  /**
+   * Datos de los pasos del wizard.
+   */
+  datosPasos: DatosPasos = {
+    nroPasos: this.pasos.length,
+    indice: this.indice,
+    txtBtnAnt: 'Anterior',
+    txtBtnSig: 'Continuar',
+  };
+
+  /**
+   * Constructor para inyectar las dependencias necesarias.
+   * @param chofer40102Query - Servicio para consultar el estado de choferes.
+   * @param chofer40102Store - Servicio para gestionar el estado de choferes.
+   */
+  constructor(
+    private chofer40102Query: Chofer40102Query,
+    private chofer40102Store: Chofer40102Store
+  ) {}
+
+  /**
+   * Método que se ejecuta al inicializar el componente.
+   */
+  ngOnInit() {
+    this.pasos = PASOS.slice(0, 2);
+    this.pasos = this.pasos.map((paso) => {
+      if (paso.indice === 2 && paso.titulo === 'Anexar necesarios') {
+        return { ...paso, titulo: 'Firmar solicitud' };
+      }
+      return paso;
+    });
+    this.chofer40102Query.selectSeccionState$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((seccionState) => {
+          this.seccion = seccionState;
+        })
+      )
+      .subscribe();
+
+    this.asignarSecciones();
+  }
+
+  /**
+   * Selecciona una pestaña del wizard.
+   * @param i - Índice de la pestaña a seleccionar.
+   */
+  seleccionaTab(i: number): void {
+    this.indice = i;
+  }
+
+  /**
+   * Obtiene el valor del índice del evento de acción del botón.
+   * @param e - Evento de acción del botón.
+   */
+  getValorIndice(e: AccionBoton) {
+    if (e.valor > 0 && e.valor < 6) {
+      this.indice = e.valor;
+      if (e.accion === 'cont') {
+        this.wizardComponent.siguiente();
+      } else {
+        this.wizardComponent.atras();
+      }
+    }
+  }
+
+  /**
+   * Método para asignar las secciones existentes al store.
+   */
+  private asignarSecciones() {
+    const secciones: boolean[] = [];
+    const formaValida: boolean[] = [];
+    for (const llaveSeccion in SECCIONES_TRAMITE_5701.PASO_1) {
+      // @ts-ignore - fix this
+      secciones.push(SECCIONES_TRAMITE_5701.PASO_1[llaveSeccion]);
+      formaValida.push(false);
+    }
+    this.chofer40102Store.establecerSeccion(secciones);
+    this.chofer40102Store.establecerFormaValida(formaValida);
+  }
+}
