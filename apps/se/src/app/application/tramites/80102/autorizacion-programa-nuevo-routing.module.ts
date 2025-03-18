67b0f0ce81f38112a85577d982de5e884e816880
Atul Falle <atul.falle@ilink-systems.com>
2025-03-18 12:20:24 +0530

    Corrección de errores para la implementación de Akita y la tabla

--- apps/se/src/app/application/tramites/80102/autorizacion-programa-nuevo-routing.module.ts
+++ apps/se/src/app/application/tramites/80102/autorizacion-programa-nuevo-routing.module.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContenedorComplementarPlantasComponent } from './components/contenedor-complementar-plantas/contenedor-complementar-plantas.component';
 import { NgModule } from '@angular/core';
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
-import { producerUpdatesAllowed } from '@angular/core/primitives/signals';
 
 const ROUTES: Routes = [
    {
