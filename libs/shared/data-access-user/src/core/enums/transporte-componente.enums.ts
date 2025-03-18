3f0b1492906dc13d9a02d4bc5325cddd31c20203
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-03-18 15:08:40 -0600

    Agrega tabla dinamica en trasnporte

--- libs/shared/data-access-user/src/core/enums/transporte-componente.enums.ts
+++ libs/shared/data-access-user/src/core/enums/transporte-componente.enums.ts
@@ -1,33 +1,152 @@
-export const HEADER_TABLA_FERROVIARIO = [
-    'Número BL',
-    'Tipo de Equipo',
-    'Iniciales Equipo',
-    'Número de Equipo',
-    'Observaciones'
-];
+import { ItemTransporte } from "../models/shared/agregar-trasnporte.model";
 
-export const HEADER_TABLA_CARRETERO = [
-    'Empresa transportista',
-    'Número carta porte',
-    'Fecha carta porte',
-    'Marca',
-    'Modelo',
-    'Placas',
-    'Contenedor(es)',
-    'Observaciones'
-];
+// export const HEADER_TABLA_FERROVIARIO = [
+//     'Número BL',
+//     'Tipo de Equipo',
+//     'Iniciales Equipo',
+//     'Número de Equipo',
+//     'Observaciones'
+// ];
 
-export const HEADER_TABLA_PEATONAL = [
-    'Empresa transportista',
-    'RFC empresa responsable',
-    'Nombre transportista',
-    'ID de gafete',
+// export const HEADER_TABLA_CARRETERO = [
+//     'Empresa transportista',
+//     'Número carta porte',
+//     'Fecha carta porte',
+//     'Marca',
+//     'Modelo',
+//     'Placas',
+//     'Contenedor(es)',
+//     'Observaciones'
+// ];
+
+
+// export const HEADER_TABLA_PEATONAL = [
+//     'Empresa transportista',
+//     'RFC empresa responsable',
+//     'Nombre transportista',
+//     'ID de gafete',
+//     'Observaciones'
+// ]
+
+// export const HEADER_TABLA_OTRO = [
+//     'Empresa transportista',
+//     'Tipo de transporte',
+//     'Datos de transporte',
+//     'Observaciones'
+// ]
+
+export const HEADER_TABLA_MARITIMO = [
+    'Guía BL',
+    'Guía house',
+    'Nombre del buque',
+    'Contenesor(es)',
     'Observaciones'
 ]
 
-export const HEADER_TABLA_OTRO = [
-    'Empresa transportista',
-    'Tipo de transporte',
-    'Datos de transporte',
+export const HEADER_TABLA_AEREO = [
+    'Arribo pendiente',
+    'Guía master',
+    'Guía house',
+    'Fecha de arribo',
+    'Hora de arribo',
+    'Guía válida',
     'Observaciones'
+]
+
+
+
+export const HEADER_TABLA_CARRETERO: ItemTransporte[] = [
+    {
+        llave: 'empTransportista',
+        valor: 'Empresa transportista'
+    },
+    {
+        llave: 'numeroPorte',
+        valor: 'Número carta porte'
+    },
+    {
+        llave: 'fechaPorte',
+        valor: 'Fecha carta porte'
+    },
+    {
+        llave: 'marcaTransporte',
+        valor: 'Marca'
+    },
+    {
+        llave: 'modeloTransporte',
+        valor: 'Modelo'
+    },
+    {
+        llave: 'placasTransporte',
+        valor: 'Placas'
+    },
+    {
+        llave: 'contenedorTransporte',
+        valor: 'Contenedor(es)'
+    },
+    {
+        llave: 'observaciones',
+        valor: 'Observaciones'
+    }
+];
+
+export const HEADER_TABLA_FERROVIARIO: ItemTransporte[] = [
+    {
+        llave: 'numeroBL',
+        valor: 'Número BL'
+    },
+    {
+        llave: 'tipoEquipo',
+        valor: 'Tipo de Equipo'
+    },
+    {
+        llave: 'inicialesEquipo',
+        valor: 'Iniciales Equipo'
+    },
+    {
+        llave: 'numeroEquipo',
+        valor: 'Número de Equipo'
+    },
+    {
+        llave: 'observaciones',
+        valor: 'Observaciones'
+    }
+];
+
+export const HEADER_TABLA_PEATONAL: ItemTransporte[] = [
+    {
+        llave: 'rfcEmpresa',
+        valor: 'Empresa transportista'
+    },
+    {
+        llave: 'nombreTransportista',
+        valor: 'Nombre transportista'
+    },
+    {
+        llave: 'numGafete',
+        valor: 'ID de gafete'
+    },
+    {
+        llave: 'observaciones',
+        valor: 'Observaciones'
+    }
+]
+
+export const HEADER_TABLA_OTRO: ItemTransporte[] = [
+    {
+        llave: 'empTransportista',
+        valor: 'Empresa transportista'
+    },
+    {
+        llave: 'tipoTransporteDes',
+        valor: 'Tipo de transporte'
+    },    
+    {
+        llave: 'datosTransporte',
+        valor: 'Datos de transporte'
+    },
+    {
+        llave: 'observaciones',
+        valor: 'Observaciones'
+    }
 ]
--- libs/shared/data-access-user/src/tramites/components/agregar-transporte/agregar-transporte.component.ts
+++ libs/shared/data-access-user/src/tramites/components/agregar-transporte/agregar-transporte.component.ts
@@ -2,8 +2,10 @@ import * as CONSTANTES from '../../constantes/formularios-transportes.enums';
 import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 import { HEADER_TABLA_FERROVIARIO, HEADER_TABLA_CARRETERO, HEADER_TABLA_PEATONAL, HEADER_TABLA_OTRO } from '../../../core/enums/transporte-componente.enums';
 import { CampoForm } from '../../../core/models/shared/forms-model';
@@ -14,6 +16,7 @@ import { CommonModule } from '@angular/common';
 import { InputFechaComponent } from '../input-fecha/input-fecha.component';
 import { Modal } from 'bootstrap';
 import { SelectCatalogosComponent } from '../select-catalogos/select-catalogos.component';
+import { ItemTransporte, TransporteAereo, TransporteCarretero, TransporteFerroviario, TransporteMaritimo, TransporteOtro, TransportePeatonal } from '../../../core/models/shared/agregar-trasnporte.model';
 @Component({
   selector: 'lib-agregar-transporte',
   standalone: true,
@@ -31,10 +34,12 @@ export class AgregarTransporteComponent implements OnInit, OnChanges {
   @ViewChild('agregarTransporte') agregarTransporte!: ElementRef;
   @ViewChild('btnCerrarModal') btnCerrarModal!: ElementRef;
 
-  readonly HEADER_TABLA_FERROVIARIO = HEADER_TABLA_FERROVIARIO;
-  readonly HEADER_TABLA_CARRETERO = HEADER_TABLA_CARRETERO;
-  readonly HEADER_TABLA_PEATONAL = HEADER_TABLA_PEATONAL
-  readonly HEADER_TABLA_OTRO = HEADER_TABLA_OTRO;
+  readonly HEADER_TABLA_FERROVIARIO: ItemTransporte[] = HEADER_TABLA_FERROVIARIO;
+  readonly HEADER_TABLA_CARRETERO: ItemTransporte[] = HEADER_TABLA_CARRETERO;
+  readonly HEADER_TABLA_PEATONAL: ItemTransporte[] = HEADER_TABLA_PEATONAL
+  readonly HEADER_TABLA_OTRO: ItemTransporte[] = HEADER_TABLA_OTRO;
+
+
 
   tituloModal!: string;
   mensajeModal!: string;
@@ -44,9 +49,19 @@ export class AgregarTransporteComponent implements OnInit, OnChanges {
   FormTransporte!: FormGroup;
   tipoTransporteSeleccionado!: Catalogo;
   camposFormulario!: CampoForm[];
-  headerTabla!: string[];
+  headerTabla!: ItemTransporte[];
+
+  bodyTabla: any[] = []
 
   carreteroForma!: FormGroup;
+  ferroviarioForma!: FormGroup;
+  peatonalForma!: FormGroup;
+  otroForma!: FormGroup;
+  maritimoForma!: FormGroup;
+  aereoForma!: FormGroup;
+
+  public observaciones: FormControl = new FormControl('');
+  anios!: number[];
 
   constructor(
     private fb: FormBuilder,
@@ -54,7 +69,6 @@ export class AgregarTransporteComponent implements OnInit, OnChanges {
   ) { }
 
   ngOnInit(): void {
-    this.crearCarreteroForm();
   }
 
 
@@ -66,27 +80,85 @@ export class AgregarTransporteComponent implements OnInit, OnChanges {
     }
   }
 
+  // Crea formularios 
+
   crearCarreteroForm(): void {
     this.carreteroForma = this.fb.group({
-      empresaTransportista: [],
-      numeroCartaPorte: [],
-      fechaCartaPorte: [],
-      marca: [],
-      modelo: [],
-      placas: [],
-      contenedores: [],
+      empTransportista: ['', [Validators.maxLength(80)]],
+      numeroPorte: ['', [Validators.maxLength(50)]],
+      fechaPorte: ['', [Validators.maxLength(10)]],
+      marcaTransporte: ['', [Validators.maxLength(70)]],
+      modeloTransporte: [-1],
+      placasTransporte: ['', [Validators.maxLength(150)]],
+      contenedorTransporte: ['', [Validators.maxLength(150)]],
     });
   }
 
-  tipoTabla(): string[] {
+  crearFerroviarioForm(): void {
+    this.ferroviarioForma = this.fb.group({
+      numeroBL: ['', [Validators.maxLength(25)]],
+      tipoEquipo: [-1],
+      inicialesEquipo: ['', [Validators.maxLength(10)]],
+      numeroEquipo: ['', [Validators.maxLength(15)]],
+    });
+
+  }
+
+  crearPeatonalForm(): void {
+    this.peatonalForma = this.fb.group({
+      rfcEmpresa: ['', [Validators.maxLength(13)]],
+      nombreTransportista: ['', [Validators.maxLength(100)]],
+      numGafete: ['', [Validators.maxLength(20)]],
+    });
+  }
+
+  crearOtroForm(): void {
+    this.otroForma = this.fb.group({
+      tipoTransporteDes: ['', [Validators.maxLength(100)]],
+      empTransportista: ['', [Validators.maxLength(80)]],
+      datosTransporte: ['', [Validators.maxLength(250)]],
+    });
+  }
+
+  crearAereoForm(): void {
+    this.aereoForma = this.fb.group({
+      arriboPendienteAereo: [''],
+      guiaMasterAereo: ['', [Validators.maxLength(12)]],
+      guiaHouseAereo: ['', [Validators.maxLength(25)]],
+      fechaArriboAereo: ['', [Validators.maxLength(15)]],
+      horaArriboAereo: ['', [Validators.maxLength(5)]],
+      guiaValida: [{ value: '', disabled: true }],
+    });
+  }
+
+  crearMaritimoForm(): void {
+    this.maritimoForma = this.fb.group({
+      guiaBLMaritimo: ['', [Validators.maxLength(15)]],
+      guiaHouseMaritimo: ['', [Validators.maxLength(15)]],
+      nombreBuqueMaritimo: ['', [Validators.maxLength(70)]],
+      contenedorMaritimo: [''],
+    });
+  }
+
+
+
+
+  // ----------------------------------------------------------------------------------------------
+
+  tipoTabla(): ItemTransporte[] {
     switch (parseInt(this.tipo, 10)) {
       case 1:
+        this.anios = this.obtenerAniosModelo();
+        this.crearCarreteroForm();
         return this.HEADER_TABLA_CARRETERO;
       case 2:
+        this.crearFerroviarioForm();
         return this.HEADER_TABLA_FERROVIARIO;
       case 5:
+        this.crearPeatonalForm();
         return this.HEADER_TABLA_PEATONAL;
       default:
+        this.crearOtroForm();
         return this.HEADER_TABLA_OTRO;
 
     }
@@ -125,19 +197,40 @@ export class AgregarTransporteComponent implements OnInit, OnChanges {
   * Cierra el modal.
   */
   cerrarModal(): void {
-    // this.btnCerrarModal.nativeElement.click();
-    const MODAL_AGREGA = new Modal(this.agregarTransporte.nativeElement);
-    MODAL_AGREGA.hide();
-    
+    this.btnCerrarModal.nativeElement.click();
+    // const MODAL_AGREGA = new Modal(this.agregarTransporte.nativeElement);
+    // MODAL_AGREGA.hide();
+
     this.tituloModal = '';
     this.mensajeModal = '';
   }
 
-  limpiarFormulario(): void {}
+  limpiarFormulario(): void { }
 
   agregarTipoTransporte(): void {
-    console.log('Agregando transporte');
+    const TRANSPORTE: TransporteCarretero = this.carreteroForma.value;
+    TRANSPORTE.observaciones = this.observaciones.value;
+
+    this.bodyTabla.push(TRANSPORTE);
+    console.log(this.bodyTabla);
     
+    this.cerrarModal();
+
+
+  }
+
+  seleccionarTodos(event: Event): void {
+    console.log(event);
+
+  }
+
+  obtenerAniosModelo(): number[] {
+    const ANIO_ACTUAL = new Date().getFullYear();
+    const ANIOS: number[] = [];
+    for (let i = 1980; i <= ANIO_ACTUAL; i++) {
+      ANIOS.push(i);
+    }
+    return ANIOS;
   }
 
 }
