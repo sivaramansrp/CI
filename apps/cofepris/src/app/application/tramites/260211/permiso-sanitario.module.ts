131d48394d7974b826af9f4638ddfce8ab4d848d
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-03-18 12:12:09 +0530

    Add TramiteCofepris state management and new components for Pasoduos and Pasotres

--- apps/cofepris/src/app/application/tramites/260211/permiso-sanitario.module.ts
+++ apps/cofepris/src/app/application/tramites/260211/permiso-sanitario.module.ts
@@ -3,20 +3,54 @@ import { CommonModule } from '@angular/common';
 
 import { PermisoSanitarioRoutingModule } from './permiso-sanitario-routing.module';
 import { PantallasComponent } from './pages/pantallas/pantallas.component';
-import { AlertComponent, BtnContinuarComponent, SolicitanteComponent, WizardComponent } from '@libs/shared/data-access-user/src';
+import {
+  AlertComponent,
+  AnexarDocumentosComponent,
+  BtnContinuarComponent,
+  CatalogosService,
+  FirmaElectronicaComponent,
+  SolicitanteComponent,
+  TituloComponent,
+  WizardComponent,
+} from '@libs/shared/data-access-user/src';
 import { DatosComponent } from './pages/datos/datos.component';
 import { DatosDeLaComponent } from './components/datosDeLa/datosDeLa.component';
+import { PasoduosComponent } from './pages/pasoduos/pasoduos.component';
+import { PasotresComponent } from './pages/pasotres/pasotres.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { InicioSesionService } from '@libs/shared/data-access-user/src/core/services/shared/inicio-sesion/inicio-sesion.service';
+import { SubirDocumentoService } from '@libs/shared/data-access-user/src/core/services/shared/subir-documento/subir-documento.service';
+import { ServiciosPantallaService } from '@libs/shared/data-access-user/src/core/services/31601/servicios-pantalla.service';
+
 
 @NgModule({
-  declarations: [PantallasComponent, DatosComponent],
+  declarations: [
+    PantallasComponent,
+    DatosComponent,
+    PasoduosComponent,
+    PasotresComponent,
+  ],
   imports: [
-    CommonModule, 
-    PermisoSanitarioRoutingModule, 
+    CommonModule,
+    PermisoSanitarioRoutingModule,
     WizardComponent,
     SolicitanteComponent,
     BtnContinuarComponent,
     AlertComponent,
     DatosDeLaComponent,
+    AnexarDocumentosComponent,
+    TituloComponent,
+    ToastrModule.forRoot(),
+    FirmaElectronicaComponent
   ],
+  providers: [
+      ToastrService,
+      provideHttpClient(),
+      CatalogosService,
+      InicioSesionService,
+      SubirDocumentoService,
+      ServiciosPantallaService
+    ],
 })
 export class PermisoSanitarioModule {}
