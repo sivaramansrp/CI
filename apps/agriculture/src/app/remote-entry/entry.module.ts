960b90c069d7f9127fd639fc62998be2f7bbf910
banshi-ilink-digital <banshi.bhatt@ilink-systems.com>
2025-03-19 14:37:09 +0530

    fixed lint issues

--- apps/agriculture/src/app/remote-entry/entry.module.ts
+++ apps/agriculture/src/app/remote-entry/entry.module.ts
@@ -1,7 +1,6 @@
-/* eslint-disable sort-imports */
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { RemoteEntryComponent } from './entry.component';
 import { appRoutes } from '../app.routes';
 import { provideRouter } from '@angular/router';
--- apps/se/src/app/application/app-routing.module.ts
+++ apps/se/src/app/application/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { SeleccionTramiteComponent } from './seleccion-tramite/seleccion-tramite.component';
-// eslint-disable-next-line @typescript-eslint/naming-convention
-const routes: Routes = [
+const ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'seleccion-tramite' },
   {
     path: 'seleccion-tramite',
@@ -157,7 +156,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(ROUTES)],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
