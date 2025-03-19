b143026c341fda0cd12a8333327323dd22939b3a
Renuga <renugadevim@ilink-systems.com>
2025-03-19 12:27:30 +0530

    Review comments code changes

--- apps/se/src/app/application/tramites/130118/pages/paso-uno/paso-uno.component.ts
+++ apps/se/src/app/application/tramites/130118/pages/paso-uno/paso-uno.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL, PERSONA_MORAL_NACIONAL } from '@libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
 import { FormularioDinamico, SolicitanteComponent, TIPO_PERSONA } from '@ng-mf/data-access-user';
 
@@ -38,13 +38,27 @@ export class PasoUnoComponent implements AfterViewInit {
   indice: number = 1;
 
   /**
+   * Constructor del componente.
+   * Se utiliza para la inyección de dependencias.
+   * 
+   * @param cdr Servicio para detectar cambios manualmente.
+   */
+  constructor(private cdr: ChangeDetectorRef) {
+    // El constructor se utiliza para la inyección de dependencias.
+  }
+
+  /**
    * Método del ciclo de vida de Angular que se ejecuta después de que la vista ha sido inicializada.
    * Configura los datos de persona y domicilio fiscal y asigna el tipo de persona en el componente `SolicitanteComponent`.
    */
   ngAfterViewInit(): void {
     this.persona = PERSONA_MORAL_NACIONAL;
     this.domicilioFiscal = DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL;
-    this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+
+    setTimeout(() => {
+      this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+      this.cdr.detectChanges();
+    }, 0);
   }
 
   /**
