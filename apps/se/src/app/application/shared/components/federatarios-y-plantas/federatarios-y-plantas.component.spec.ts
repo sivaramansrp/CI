2b7da363270a37c6ab92c0a2469fbd8def4ef72e
muneez-ilink <muneeshwaran.n@ilink-systems.com>
2025-03-19 14:23:58 +0530

    commited test case for fedatarios-y-plantas

--- apps/se/src/app/application/shared/components/federatarios-y-plantas/federatarios-y-plantas.component.spec.ts
+++ apps/se/src/app/application/shared/components/federatarios-y-plantas/federatarios-y-plantas.component.spec.ts
@@ -1,93 +1,24 @@
-// @ts-nocheck
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { Observable, of as observableOf, throwError } from 'rxjs';
-
-import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FederatariosYPlantasComponent } from './federatarios-y-plantas.component';
-import { Router, ActivatedRoute } from '@angular/router';
-
-@Injectable()
-class MockRouter {
-  navigate() {};
-}
-
-@Directive({ selector: '[myCustom]' })
-class MyCustomDirective {
-  @Input() myCustom;
-}
-
-@Pipe({name: 'translate'})
-class TranslatePipe implements PipeTransform {
-  transform(value) { return value; }
-}
-
-@Pipe({name: 'phoneNumber'})
-class PhoneNumberPipe implements PipeTransform {
-  transform(value) { return value; }
-}
-
-@Pipe({name: 'safeHtml'})
-class SafeHtmlPipe implements PipeTransform {
-  transform(value) { return value; }
-}
+import { ActivatedRoute } from '@angular/router';
 
 describe('FederatariosYPlantasComponent', () => {
-  let fixture;
-  let component;
+  let component: FederatariosYPlantasComponent;
+  let fixture: ComponentFixture<FederatariosYPlantasComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ FormsModule, ReactiveFormsModule ],
-      declarations: [
-        FederatariosYPlantasComponent,
-        TranslatePipe, PhoneNumberPipe, SafeHtmlPipe,
-        MyCustomDirective
-      ],
-      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FederatariosYPlantasComponent],
       providers: [
-        { provide: Router, useClass: MockRouter },
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: {url: 'url', params: {}, queryParams: {}, data: {}},
-            url: observableOf('url'),
-            params: observableOf({}),
-            queryParams: observableOf({}),
-            fragment: observableOf('fragment'),
-            data: observableOf({})
-          }
-        }
-      ]
-    }).overrideComponent(FederatariosYPlantasComponent, {
-
+        { provide: ActivatedRoute, useValue: {} }
+      ],
     }).compileComponents();
+
     fixture = TestBed.createComponent(FederatariosYPlantasComponent);
-    component = fixture.debugElement.componentInstance;
+    component = fixture.componentInstance;
   });
 
-  afterEach(() => {
-    component.ngOnDestroy = function() {};
-    fixture.destroy();
-  });
-
-  it('should run #constructor()', async () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('should run #initFederatariosFormGroup()', async () => {
-
-    component.initFederatariosFormGroup();
-
-  });
-
-  it('should run #navigateToAcciones()', async () => {
-    component.router = component.router || {};
-    component.router.navigate = jest.fn();
-    component.navigateToAcciones({});
-  });
-
 });
