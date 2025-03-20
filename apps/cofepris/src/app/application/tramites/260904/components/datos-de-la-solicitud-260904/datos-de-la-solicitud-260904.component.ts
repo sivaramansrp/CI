b63647bea23f3616e6ab9190df67b715b45f6746
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-03-20 22:23:28 +0530

    fix: UI completed for tab Datos de la solicitud

--- apps/cofepris/src/app/application/tramites/260904/components/datos-de-la-solicitud-260904/datos-de-la-solicitud-260904.component.ts
+++ apps/cofepris/src/app/application/tramites/260904/components/datos-de-la-solicitud-260904/datos-de-la-solicitud-260904.component.ts
@@ -2,18 +2,68 @@ import { ALERT } from '../../enums/datos-de-la-solicitud-260904.enum';
 import { AlertComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { InputRadioComponent } from '@libs/shared/data-access-user/src';
+import { OPCIONES_DE_BOTON_DE_RADIO } from '../../enums/datos-de-la-solicitud-260904.enum';
+import { OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TituloComponent } from '@libs/shared/data-access-user/src';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-datos-de-la-solicitud-260904',
   standalone: true,
-  imports: [CommonModule, AlertComponent],
+  imports: [
+    CommonModule,
+    AlertComponent,
+    InputRadioComponent,
+    ReactiveFormsModule,
+    TituloComponent,
+  ],
   templateUrl: './datos-de-la-solicitud-260904.component.html',
   styleUrl: './datos-de-la-solicitud-260904.component.scss',
 })
-export class DatosDeLaSolicitud260904Component {  
+export class DatosDeLaSolicitud260904Component implements OnInit {
   colapsable: boolean = true;
-  TEXTOS = ALERT
+  TEXTOS = ALERT;
+  btonDeRadio = OPCIONES_DE_BOTON_DE_RADIO;
+  form!: FormGroup;
+  datosDelEstablecimiento!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    // Constructor
+  }
+
+  ngOnInit(): void {
+    this.crearFormulario();
+  }
+
   mostrar_colapsable(): void {
     this.colapsable = !this.colapsable;
+  }
+
+  crearFormulario(): void {
+    this.form = this.fb.group({
+      btonDeRadio: ['', [Validators.required]],
+      justificación: ['', [Validators.required]],
+    });
+
+    this.datosDelEstablecimiento = this.fb.group({
+      rfcDel:  ['', Validators.required],
+      denominacion: ['', Validators.required],
+      correo: ['', Validators.required],
+    });
+  }
+
+  toggleFormControls() {
+    Object.keys(this.datosDelEstablecimiento.controls).forEach(
+      (controlName) => {
+        const CONTROL = this.datosDelEstablecimiento.get(controlName);
+        if (CONTROL?.disabled) {
+          CONTROL.enable();
+        }
+      }
+    );
   }
 }
--- apps/cofepris/src/app/application/tramites/260904/components/domicilio-del-establecimiento-260904/domicilio-del-establecimiento-260904.component.ts
+++ apps/cofepris/src/app/application/tramites/260904/components/domicilio-del-establecimiento-260904/domicilio-del-establecimiento-260904.component.ts
@@ -4,17 +4,29 @@ import { Catalogo } from '@libs/shared/data-access-user/src';
 import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ConfiguracionColumna } from '@libs/shared/data-access-user/src';
 import { FormBuilder } from '@angular/forms';
-import { FormGroup } from '@angular/forms'
+import { FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { InputRadioComponent } from '@libs/shared/data-access-user/src';
+import { MERCANCIAS_DATA } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { MercanciasInfo } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { MercanciasTabla } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { NICO_TABLA } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { NicoInfo } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { OPCIONES_DE_BOTON_DE_RADIO } from '../../enums/domicilio-del-establecimiento-260904.enum';
 import { OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RespuestaCatalogos } from '@libs/shared/data-access-user/src';
+import { RespuestaTabla } from '../../models/modificación-del-permiso-sanitario-de-importación-de-insumo.model';
+import { TablaDinamicaComponent } from '@libs/shared/data-access-user/src';
+import { TablaSeleccion } from '@libs/shared/data-access-user/src';
 import { TituloComponent } from '@libs/shared/data-access-user/src';
 import { Validators } from '@angular/forms';
-
 @Component({
   selector: 'app-domicilio-del-establecimiento-260904',
   standalone: true,
-  imports: [CommonModule, TituloComponent, ReactiveFormsModule, CatalogoSelectComponent, AlertComponent],
+  imports: [CommonModule, TituloComponent, ReactiveFormsModule, CatalogoSelectComponent, AlertComponent, TablaDinamicaComponent, InputRadioComponent],
   templateUrl: './domicilio-del-establecimiento-260904.component.html',
   styleUrl: './domicilio-del-establecimiento-260904.component.scss',
 })
@@ -22,16 +34,30 @@ export class DomicilioDelEstablecimiento260904Component implements OnInit {
   form!: FormGroup;
   estado: Catalogo[] = [];
   TEXTOS = ALERT;
-  class = 'alert-warning'
+  class = 'alert-warning';
+  tablaSeleccionCheckbox: TablaSeleccion = TablaSeleccion.CHECKBOX;
+  nicoTabla: ConfiguracionColumna<NicoInfo>[] = NICO_TABLA;
+  nicoTablaDatos: NicoInfo[] = [];
+  domicilio!: FormGroup;
+  mercanciasTabla: ConfiguracionColumna<MercanciasInfo>[] = MERCANCIAS_DATA;
+  mercanciasTablaDatos: MercanciasInfo[] = [];
+  manifests = ALERT.MANIFESTS;
+  opcionDeBotonDeRadio = OPCIONES_DE_BOTON_DE_RADIO;
+  representanteLegal!: FormGroup;
 
   constructor(
     private fb: FormBuilder,
+    private httpServicios: HttpClient,
+
   ) {
     // Constructor
   }
 
   ngOnInit(): void {
     this.crearFormulario();
+    this.obtenerTablaDatos();
+    this.obtenerEstadoList();
+    this.obtenerMercanciasDatos();
   }
 
   crearFormulario(): void {
@@ -45,5 +71,52 @@ export class DomicilioDelEstablecimiento260904Component implements OnInit {
       lada: ['', [Validators.required]],
       telefono: ['', [Validators.required]],
     });
+
+    this.domicilio = this.fb.group({
+      avisoCheckbox: [],
+      licenciaSanitaria: [
+        { value: '', disabled: false },
+      ],
+      regimen: [],
+      aduanasEntradas: [],
+      manifests: []
+    });
+
+    this.representanteLegal = this.fb.group({
+      BtonDeRadio: [],
+      rfc: ['', [Validators.required]],
+      nombre: [{ value: '', disabled: true }, [Validators.required]],
+      apellidoPaterno: [{ value: '', disabled: true }, [Validators.required]],
+      apellidoMaterno: [{ value: '', disabled: true }, [Validators.required]],
+    });
+  }
+
+  obtenerTablaDatos() {
+    this.httpServicios
+      .get<RespuestaTabla>('../../../../../assets/json/260904/tablaDatos.json')
+      .subscribe((data): void => {
+        this.nicoTablaDatos = data?.data;
+      });
+  }
+
+  obtenerEstadoList() {
+    this.httpServicios
+      .get<RespuestaCatalogos>(
+        '../../../../../assets/json/260904/seleccion.json'
+      )
+      .subscribe((data): void => {
+        const DATOS = data?.data;
+        this.estado = DATOS;
+      });
+  }
+
+  obtenerMercanciasDatos() {
+    this.httpServicios
+      .get<MercanciasTabla>(
+        '../../../../../assets/json/260904/mercanciasDatos.json'
+      )
+      .subscribe((data): void => {
+        this.mercanciasTablaDatos = data?.data;
+      });
   }
 }
