2982ecd50420a66491308504de6e8dbbf899199e
Rohith Kannan M <rohithkannan.m@purpleslate.in>
2025-03-20 23:16:59 +0530

    feat: three tab changes

--- apps/agriculture/src/app/application/tramites/220403/exportaccion-acuicola-routing.module.ts
+++ apps/agriculture/src/app/application/tramites/220403/exportaccion-acuicola-routing.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcuicolaComponent } from './pages/acuicola/acuicola.component';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [
+const ROUTES: Routes = [
   {
       path: 'acuicola',
       component: AcuicolaComponent
@@ -15,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(ROUTES)],
   exports: [RouterModule]
 })
 export class ExportaccionAcuicolaRoutingModule { }
--- apps/agriculture/src/app/application/tramites/220403/models/acuicola.module.ts
+++ apps/agriculture/src/app/application/tramites/220403/models/acuicola.module.ts
@@ -25,3 +25,61 @@ export interface DatosPasos {
   indice: number;
   nroPasos: number;
 }
+
+export interface FormularioGrupo {
+  datosRealizer: DatosRealizer,
+  combinacionRequerida: CombinacionRequerida,
+  transporte: Transporte,
+  pagoDerechos: PagoDerechos,
+}
+
+export interface DatosRealizer {
+  certificadoTipo: string,
+  aduanaEmbarque: string,
+  numeroContenedor: string,
+  parisOrigen: string,
+  entidadFederativaOrigen: string,
+  municipoOrigen: string,
+  paisDestino: string,
+}
+
+export interface CombinacionRequerida {
+  especie: string,
+  paisDeDestino: string,
+  instalacionAcuicola: string
+}
+
+export interface Transporte {
+  medioTransporte: string,
+  identificacionMedioTransporte: string,
+  numeroDeContenedor: string,
+  denodenominacionRazonSocial: string,
+  numeroFlejes: string,
+}
+
+export interface PagoDerechos {
+  claveReferencia: string,
+  cadenaDependencia: string,
+  banco: string,
+  llavePago: string,
+  fechaPago: string,
+  importePago: string,
+}
+
+export interface ColumnasTabla {
+  noPartida: string,
+  fraccionArancelaria: string,
+  descripcionFrccion: string,
+  descripcion: string,
+  undidadUmt: string,
+  cantidadUmt: string,
+  unidadUmc: string,
+  cantidadUmc: string,
+  tipoMercancia: string,
+  uso: string,
+  nombreCientifico: string,
+  nombreComun: string,
+  faseDesarrollo: string,
+  presentacion: string,
+  paisProcedencia: string,
+}
