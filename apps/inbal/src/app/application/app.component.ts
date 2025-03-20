70617cd11e2572f3b54b73be1a6b21d471763cf5
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-03-20 18:59:29 +0530

    intial department creation changes

---
+++ apps/inbal/src/app/application/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+}
