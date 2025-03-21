1b8f63c44af4eb03431963ebbac3939984e5d0c7
Ghanshyam Yadav <ghanshyam.yadav@ilink-systems.com>
2025-03-21 11:44:46 +0530

    resolved Build issues.

--- apps/aga/src/app/application/tramites/701/registro-digitalizar-documentos.module.ts
+++ apps/aga/src/app/application/tramites/701/registro-digitalizar-documentos.module.ts
@@ -6,7 +6,7 @@ import { BtnContinuarComponent } from '@ng-mf/data-access-user';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FirmaElectronicaComponent } from '@ng-mf/data-access-user';
-import { NgModule } from '@angular/core';
+import { forwardRef, NgModule } from '@angular/core';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
@@ -25,7 +25,7 @@ import { WizardComponent } from '@ng-mf/data-access-user';
     
    ],
   imports: [
-    AlertComponent,
+    forwardRef(() => AlertComponent),
     AnexarDocumentosComponent,
     BtnContinuarComponent,
     CommonModule,
@@ -38,7 +38,7 @@ import { WizardComponent } from '@ng-mf/data-access-user';
     RegistroDigitalizarDocumentosRoutingModule,
     SolicitanteComponent,
     SolicitudPageComponent,
-    TituloComponent,
+    forwardRef(() => TituloComponent),
     WizardComponent,
     SharedModule,
     
