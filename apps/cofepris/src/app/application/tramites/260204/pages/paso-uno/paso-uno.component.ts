054c1c68395234e06f5e46e340286d8791b703c0
gopal.v <gopal.v@purpleslate.in>
2025-03-21 12:56:58 +0530

    paso uno component and tabs init

--- apps/cofepris/src/app/application/tramites/260204/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260204/pages/paso-uno/paso-uno.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SolicitanteComponent } from '@ng-mf/data-access-user';
 
 @Component({
   selector: 'app-paso-uno',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SolicitanteComponent],
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.css',
 })
-export class PasoUnoComponent {}
+export class PasoUnoComponent {
+  indice: number = 1;
+
+  seleccionaTab(i: number): void {
+    this.indice = i;
+  }
+}
