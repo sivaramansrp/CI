fb6d1916159b6fce9480a62bba4cde0a3887c55c
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-03-21 09:11:05 +0530

    added comma and app-root

--- apps/inbal/src/app/application/app.component.ts
+++ apps/inbal/src/app/application/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
