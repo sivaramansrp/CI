ef7b87f233275813f33b926a79b05d29ae0f2cfb
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-03-21 12:04:26 +0530

    Implement form for Certificado de Origen with validation and state catalog integration

--- apps/se/src/app/application/tramites/110208/components/certificado-origen/certificado-origen.component.ts
+++ apps/se/src/app/application/tramites/110208/components/certificado-origen/certificado-origen.component.ts
@@ -1,11 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Catalogo, CatalogoSelectComponent, InputFecha, RespuestaCatalogos, TituloComponent } from '@libs/shared/data-access-user/src';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
+export const FECHA_INICIO = {
+  labelNombre: 'Fecha inicio',
+  required: true,
+  habilitado: true,
+};
 @Component({
   selector: 'app-certificado-origen',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TituloComponent,
+    ReactiveFormsModule,
+    CatalogoSelectComponent
+  ],
   templateUrl: './certificado-origen.component.html',
   styleUrl: './certificado-origen.component.css',
 })
-export class CertificadoOrigenComponent {}
+export class CertificadoOrigenComponent implements OnInit {
+  
+
+  formCertificado!: FormGroup
+
+  /**
+   * Constructor del componente.
+   * @param fb FormBuilder para crear formularios reactivos.
+   * @param httpServicios Cliente HTTP para servicios API.
+ 
+   */
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly httpServicios: HttpClient,
+  ) {
+    // Dependencia inyectada para uso posterior
+  }
+
+  ngOnInit(): void {
+    this.obtenerEstadoList()
+    this.formCertificado = this.fb.group({
+      entidadFederativa: ['',Validators.required],
+      bloque:['',Validators.required],
+      fraccionArancelariaForm:[''],
+      registroProductoForm:[''],
+      nombreComercialForm:['']
+    });
+  }
+
+   /**
+   * Lista de catálogos de estados.
+   */
+   estado: Catalogo[] = [];
+
+   /**
+      * Configuración de las fechas de inicio y fin.
+      * @type {InputFecha}
+      */
+     public fechaInicioInput: InputFecha = FECHA_INICIO;
+
+
+   /**
+ * Obtiene la lista de estados desde un archivo JSON.
+ */
+obtenerEstadoList(): void {
+  this.httpServicios
+    .get<RespuestaCatalogos>('../../../../../assets/json/110208/seleccion.json')
+    .subscribe((data): void => {
+      const DATOS = data?.data;
+      this.estado = DATOS;
+    });
+}
+}
