0e9299891d506b8cdde4f22b269961f110300a62
Kalai-2025 <kalaiyarasan.s@ilink-systems.com>
2025-03-24 12:21:17 +0530

    140104: UI - 01

---
+++ apps/se/src/app/application/tramites/140104/cancelacion-de-certificados-de-cupo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CancelacionDeCertificadosDeCupoRoutingModule } from './cancelacion-de-certificados-de-cupo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CancelacionDeCertificadosDeCupoRoutingModule
+  ]
+})
+export class CancelacionDeCertificadosDeCupoModule { }
