f69f3381b3c2e6ae442e699d60a8e47b4871dbd6
JamunaSri S S <jamunasri.s@purpleslate.in>
2025-03-24 10:44:11 +0530

    module creation

---
+++ apps/se/src/app/application/tramites/110211/validar-certificado.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ValidarCertificadoRoutingModule } from './validar-certificado-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ValidarCertificadoRoutingModule
+  ]
+})
+export class ValidarCertificadoModule { }
