6f62a1308daa076e370a5f5408dcf7af7f6ca07e
TejaswiniB-ilink <tejaswini.bhandare@ilink-systems.com>
2025-03-24 17:11:29 +0530

    SE Shared component make formgroup shared

--- apps/se/src/app/application/shared/components/representacion/representacion.component.ts
+++ apps/se/src/app/application/shared/components/representacion/representacion.component.ts
@@ -7,22 +7,16 @@
  * @imports TituloComponent, CatalogoSelectComponent, CommonModule, ReactiveFormsModule, AlertComponent
  * @templateUrl ./representacion.component.html
  */
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {
-  FormBuilder,
   FormGroup,
-  ReactiveFormsModule,
-  Validators,
+  ReactiveFormsModule
 } from '@angular/forms';
 import { AlertComponent } from '@libs/shared/data-access-user/src';
-import { CommonModule } from '@angular/common';
-
-import { HttpClient } from '@angular/common/http';
-
 import { Catalogo } from '@libs/shared/data-access-user/src/core/models/shared/catalogos.model';
 import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component';
-import { NeumaticosComercializarService } from './../../../tramites/130110/services/neumaticos-comercializar.service';
-import { TEXTOS } from './../../../tramites/130110/enums/representacion-federal.enum';
+import { CommonModule } from '@angular/common';
+import { TEXTOS } from '../../constantes/representacion-federal.enum';
 import { TituloComponent } from '@libs/shared/data-access-user/src/tramites/components/titulo/titulo.component';
 
 @Component({
@@ -37,26 +31,49 @@ import { TituloComponent } from '@libs/shared/data-access-user/src/tramites/comp
   ],
   templateUrl: './representacion.component.html',
 })
-export class RepresentacionComponent implements OnInit {
+export class RepresentacionComponent {
+  /**
+   * Formulario reactivo para la representación.
+   * @type {FormGroup}
+   */
+  @Input() frmRepresentacionForm!: FormGroup;
+
+  /**
+   * Lista de entidades federativas.
+   * @type {Catalogo[]}
+   */
   @Input() entidadFederativa: Catalogo[] = [];
+
+  /**
+   * Lista de representaciones federales.
+   * @type {Catalogo[]}
+   */
   @Input() representacionFederal: Catalogo[] = [];
+
+  /**
+   * Textos utilizados en el componente.
+   * @type {any}
+   */
   @Input() TEXTOS = TEXTOS;
+
+  /**
+   * Evento emitido para establecer valores en el store.
+   * @type {EventEmitter<{ form: FormGroup; campo: string; metodoNombre: string }>}
+   */
   @Output() setValoresStoreEvent = new EventEmitter<{ form: FormGroup; campo: string; metodoNombre: string }>();
-  frmRepresentacion!: FormGroup;
 
-  constructor(
-    private http: HttpClient, 
-    private fb: FormBuilder,
-    private neumaticosComercializarService: NeumaticosComercializarService) {
-    // Constructor del componente
+  /**
+   * Constructor del componente.
+   */
+  constructor() {
+    // 
   }
-
-  ngOnInit(): void {
-    this.frmRepresentacion = this.fb.group({
-      entidad: ['', Validators.required],
-      representacion: ['', Validators.required],
-    });
-  }
+  /**
+   * Establece valores en el store.
+   * @param {FormGroup} form - El formulario reactivo.
+   * @param {string} campo - El campo a actualizar.
+   * @param {string} metodoNombre - El nombre del método.
+   */
   setValoresStore(form: FormGroup, campo: string, metodoNombre: string): void {
     this.setValoresStoreEvent.emit({ form, campo, metodoNombre });
   }
