faf047e9ddeae8d46eb7fa4580264df93ae72efd
Swapnali-Mahadar-iLink <swapnali.mahadar@ilink-systems.com>
2025-03-25 10:36:56 +0530

    paso navigation added

--- apps/cofepris/src/app/application/tramites/260911/modificacion-de-dispositivos.module.ts
+++ apps/cofepris/src/app/application/tramites/260911/modificacion-de-dispositivos.module.ts
@@ -2,6 +2,7 @@ import { AlertComponent, BtnContinuarComponent, CatalogoSelectComponent, Solicit
 import { CommonModule } from '@angular/common';
 import { DatosDeLaSolicitudComponent } from './component/datos-de-la-solicitud/datos-de-la-solicitud.component';
 import { DomicilioDelEstablecimientoComponent } from './component/domicilio-del-establecimiento/domicilio-del-establecimiento.component';
+import { InicioSesionService } from '@libs/shared/data-access-user/src/core/services/shared/inicio-sesion/inicio-sesion.service';
 import { ModificacionDeDispositivosRoutingModule } from './modificacion-de-dispositivos-routing.module';
 import { NgModule } from '@angular/core';
 import { PagoDeDerechosComponent } from './component/pago-de-derechos/pago-de-derechos.component';
@@ -10,18 +11,17 @@ import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SanitaryPermitComponent } from './pages/sanitary-permit/sanitary-permit.component';
+import { SubirDocumentoService } from '@libs/shared/data-access-user/src/core/services/shared/subir-documento/subir-documento.service';
 import { TercerosRelacionadosComponent } from './component/terceros-relacionados/terceros-relacionados.component';
+import { ToastrService } from 'ngx-toastr';
 import { TramitesAsociadoComponent } from './component/tramites-asociado/tramites-asociado.component';
 import { provideHttpClient } from '@angular/common/http';
 
 
 
-
-
-
 @NgModule({
     declarations: [SanitaryPermitComponent, PasoUnoComponent],
-    providers: [provideHttpClient()],
+    providers: [provideHttpClient(), InicioSesionService, ToastrService, SubirDocumentoService],
     imports: [
         CommonModule,
         ModificacionDeDispositivosRoutingModule,
