f99b60147599c7f0ac8d64ba269b2211cf023c96
navin-ilink <navin.kumar@ilink-system.com>
2025-03-25 11:26:48 +0530

    minor fixes

--- apps/cofepris/jest.config.ts
+++ apps/cofepris/jest.config.ts
@@ -1,9 +1,8 @@
-/* eslint-disable */
 export default {
-  displayName: 'se',
+  displayName: 'cofepris',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/se',
+  coverageDirectory: '../../coverage/apps/cofepris',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
