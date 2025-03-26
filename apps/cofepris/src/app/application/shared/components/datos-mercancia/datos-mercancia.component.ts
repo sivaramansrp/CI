d09cd783dba71dd7d4292855e42f4351cb449362
gopal.v <gopal.v@purpleslate.in>
2025-03-26 23:52:11 +0530

    added doc to datosomercancia component

--- apps/cofepris/src/app/application/shared/components/datos-mercancia/datos-mercancia.component.ts
+++ apps/cofepris/src/app/application/shared/components/datos-mercancia/datos-mercancia.component.ts
@@ -1,79 +1,212 @@
-import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Catalogo, CrossListLable, MercanciaForm, TablaMercanciasDatos } from '../../models/datos-solicitud.model';
-import { CatalogoSelectComponent, CrosslistComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  Catalogo,
+  CrossListLable,
+  MercanciaForm,
+  TablaMercanciasDatos,
+} from '../../models/datos-solicitud.model';
+import {
+  CatalogoSelectComponent,
+  CrosslistComponent,
+  TituloComponent,
+} from '@libs/shared/data-access-user/src';
 import { CommonModule, Location } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CROSLISTA_DE_PAISES } from '../../constantes/datos-solicitud.enum';
 import { DatosSolicitudService } from '../../services/datos-solicitud.service';
-
+/**
+ * @component DatosMercanciaComponent
+ * @description Componente encargado de capturar y emitir los datos de una mercancía.
+ * Utiliza formularios reactivos y listas cruzadas para países de origen, procedencia y uso específico.
+ */
 @Component({
   selector: 'app-datos-mercancia',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TituloComponent, CatalogoSelectComponent,
-    CrosslistComponent
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TituloComponent,
+    CatalogoSelectComponent,
+    CrosslistComponent,
   ],
   templateUrl: './datos-mercancia.component.html',
   styleUrl: './datos-mercancia.component.scss',
   providers: [DatosSolicitudService],
 })
 export class DatosMercanciaComponent implements OnInit {
+  /**
+   * @property {FormGroup} mercanciaForm
+   * Formulario reactivo principal para capturar los datos de la mercancía.
+   */
   public mercanciaForm!: FormGroup;
 
+  /**
+   * @property {MercanciaForm} mercanciaFormState
+   * Input que recibe el estado inicial del formulario de mercancía.
+   */
   @Input() public mercanciaFormState!: MercanciaForm;
-  @Output() mercanciaSeleccionado: EventEmitter<TablaMercanciasDatos> = new EventEmitter<TablaMercanciasDatos>();
 
+  /**
+   * @event mercanciaSeleccionado
+   * Evento emitido cuando el usuario selecciona o guarda una mercancía.
+   */
+  @Output() mercanciaSeleccionado: EventEmitter<TablaMercanciasDatos> =
+    new EventEmitter<TablaMercanciasDatos>();
+
+  /** Catálogos de datos para los diferentes campos del formulario */
   public clasificacionProductoDatos!: Catalogo[];
   public especificarClasificacionProductoDatos!: Catalogo[];
   public tipoProductoDatos!: Catalogo[];
   public formaFarmaceuticaDatos!: Catalogo[];
   public estadoFisicoDatos!: Catalogo[];
   public cantidadUmcDatos!: Catalogo[];
+
+  /**
+   * @property {boolean} paisDeOriginColapsable
+   * Controla la visibilidad del listado de país de origen.
+   */
   public paisDeOriginColapsable = false;
+
+  /**
+   * @property {boolean} paisDeProcedenciaColapsable
+   * Controla la visibilidad del listado de país de procedencia.
+   */
   public paisDeProcedenciaColapsable = false;
+
+  /**
+   * @property {boolean} usoEspesificoColapsable
+   * Controla la visibilidad del listado de uso específico.
+   */
   public usoEspesificoColapsable = false;
 
+  /** Etiquetas personalizadas para los crosslists */
   public paisDeOriginLabel: CrossListLable = {
     tituluDeLaIzquierda: 'País de origen',
     derecha: 'País(es) seleccionado(s)',
   };
+
+  /**
+   * @property {CrossListLable} paisDeProcedenciaLabel
+   * Etiqueta personalizada para el componente de lista cruzada de país de procedencia.
+   * Define los títulos mostrados en la parte izquierda y derecha del componente.
+   */
   public paisDeProcedenciaLabel: CrossListLable = {
     tituluDeLaIzquierda: 'País de procedencia',
     derecha: 'País(es) seleccionados',
   };
+
+  /**
+   * @property {CrossListLable} usoEspesificoLabel
+   * Etiqueta personalizada para el componente de lista cruzada de uso específico.
+   * Define los títulos para los elementos disponibles y seleccionados.
+   */
   public usoEspesificoLabel: CrossListLable = {
     tituluDeLaIzquierda: 'Uso específico',
     derecha: 'Uso específico',
   };
-  public seleccionarOrigenDelPais = CROSLISTA_DE_PAISES;
+
+  /**
+   * @property {string[]} seleccionadasPaisDeOriginDatos
+   * Lista de países seleccionados como origen.
+   */
   public seleccionadasPaisDeOriginDatos: string[] = [];
 
-  public paisDeProcedenciaDatos = CROSLISTA_DE_PAISES;
+  /**
+   * @property {string[]} seleccionadasPaisDeProcedenciaDatos
+   * Lista de países seleccionados como procedencia.
+   */
   public seleccionadasPaisDeProcedenciaDatos: string[] = [];
 
-  public usoEspesificoDatos = CROSLISTA_DE_PAISES;
+  /**
+   * @property {string[]} seleccionadasUsoEspesificoDatos
+   * Lista de usos específicos seleccionados.
+   */
   public seleccionadasUsoEspesificoDatos: string[] = [];
 
-  constructor(private fb: FormBuilder,
+  /**
+   * @property {Catalogo[]} paisDeProcedenciaDatos
+   * Datos de países para la lista cruzada de procedencia.
+   */
+  public paisDeProcedenciaDatos = CROSLISTA_DE_PAISES;
+
+  /**
+   * @property {Catalogo[]} usoEspesificoDatos
+   * Datos de usos específicos para lista cruzada.
+   */
+  public usoEspesificoDatos = CROSLISTA_DE_PAISES;
+
+  /**
+   * @property {Catalogo[]} seleccionarOrigenDelPais
+   * Datos de países para lista cruzada de país de origen.
+   */
+  public seleccionarOrigenDelPais = CROSLISTA_DE_PAISES;
+
+  /**
+   * @constructor
+   * Inicializa el formulario de mercancía y carga catálogos desde archivos JSON.
+   *
+   * @param fb - FormBuilder para construir formularios reactivos.
+   * @param datosSolicitudService - Servicio que carga catálogos desde assets.
+   * @param ubicaccion - Servicio para manejar navegación (si es necesario).
+   */
+  constructor(
+    private fb: FormBuilder,
     private datosSolicitudService: DatosSolicitudService,
-    private ubicaccion: Location) {
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'clasificacionProductoDatos', '/260204/mercanciaClasificacionProducto.json');
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'especificarClasificacionProductoDatos', '/260204/especificarClasificacionProducto.json');
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'tipoProductoDatos', '/260204/tipoProductoDatos.json');
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'formaFarmaceuticaDatos', '/260204/formaFarmaceutica.json');
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'estadoFisicoDatos', '/260204/estadoFisicoDatos.json');
-    this.datosSolicitudService.obtenerRespuestaPorUrl(this, 'cantidadUmcDatos', '/260204/cantidadUmcDatos.json');
+    private ubicaccion: Location
+  ) {
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'clasificacionProductoDatos',
+      '/260204/mercanciaClasificacionProducto.json'
+    );
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'especificarClasificacionProductoDatos',
+      '/260204/especificarClasificacionProducto.json'
+    );
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'tipoProductoDatos',
+      '/260204/tipoProductoDatos.json'
+    );
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'formaFarmaceuticaDatos',
+      '/260204/formaFarmaceutica.json'
+    );
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'estadoFisicoDatos',
+      '/260204/estadoFisicoDatos.json'
+    );
+    this.datosSolicitudService.obtenerRespuestaPorUrl(
+      this,
+      'cantidadUmcDatos',
+      '/260204/cantidadUmcDatos.json'
+    );
   }
 
+  /**
+   * @method ngOnInit
+   * @description Hook de ciclo de vida que se ejecuta al inicializar el componente.
+   * Llama al método `crearMercanciaForm` para construir el formulario.
+   */
   ngOnInit(): void {
     this.crearMercanciaForm();
   }
 
   /**
    * Crea y configura el formulario reactivo para la gestión de datos de mercancía.
-   * 
+   *
    * Este método inicializa un formulario con validaciones requeridas para cada campo,
    * utilizando los valores iniciales proporcionados por el estado `mercanciaFormState`.
-   * 
+   *
    * Campos incluidos en el formulario:
    * - `clasificacionProducto`: Clasificación del producto (requerido).
    * - `especificarClasificacionProducto`: Detalle de la clasificación del producto (requerido).
@@ -94,30 +227,69 @@ export class DatosMercanciaComponent implements OnInit {
    * - `fechaCaducidad`: Fecha de caducidad del producto (opcional).
    * - `paisDeOriginDatos`: País de origen del producto (requerido).
    * - `paisDeProcedenciaDatos`: País de procedencia del producto (requerido).
-   * 
+   *
    * @returns void
    */
   crearMercanciaForm(): void {
     this.mercanciaForm = this.fb.group({
-      clasificacionProducto: [this.mercanciaFormState.clasificacionProducto, Validators.required],
-      especificarClasificacionProducto: [this.mercanciaFormState.especificarClasificacionProducto, Validators.required],
-      denominacionEspecificaProducto: [this.mercanciaFormState.denominacionEspecificaProducto, Validators.required],
-      denominacionDistintiva: [this.mercanciaFormState.denominacionDistintiva, Validators.required],
-      denominacionComun: [this.mercanciaFormState.denominacionComun, Validators.required],
+      clasificacionProducto: [
+        this.mercanciaFormState.clasificacionProducto,
+        Validators.required,
+      ],
+      especificarClasificacionProducto: [
+        this.mercanciaFormState.especificarClasificacionProducto,
+        Validators.required,
+      ],
+      denominacionEspecificaProducto: [
+        this.mercanciaFormState.denominacionEspecificaProducto,
+        Validators.required,
+      ],
+      denominacionDistintiva: [
+        this.mercanciaFormState.denominacionDistintiva,
+        Validators.required,
+      ],
+      denominacionComun: [
+        this.mercanciaFormState.denominacionComun,
+        Validators.required,
+      ],
       tipoProducto: [this.mercanciaFormState.tipoProducto, Validators.required],
-      formaFarmaceutica: [this.mercanciaFormState.formaFarmaceutica, Validators.required],
+      formaFarmaceutica: [
+        this.mercanciaFormState.formaFarmaceutica,
+        Validators.required,
+      ],
       estadoFisico: [this.mercanciaFormState.estadoFisico, Validators.required],
-      fraccionArancelaria: [this.mercanciaFormState.fraccionArancelaria, Validators.required],
-      descripcionFraccion: [this.mercanciaFormState.descripcionFraccion, Validators.required],
-      cantidadUmtValor: [this.mercanciaFormState.cantidadUmtValor, Validators.required],
+      fraccionArancelaria: [
+        this.mercanciaFormState.fraccionArancelaria,
+        Validators.required,
+      ],
+      descripcionFraccion: [
+        this.mercanciaFormState.descripcionFraccion,
+        Validators.required,
+      ],
+      cantidadUmtValor: [
+        this.mercanciaFormState.cantidadUmtValor,
+        Validators.required,
+      ],
       cantidadUmt: [this.mercanciaFormState.cantidadUmt, Validators.required],
-      cantidadUmcValor: [this.mercanciaFormState.cantidadUmcValor, Validators.required],
+      cantidadUmcValor: [
+        this.mercanciaFormState.cantidadUmcValor,
+        Validators.required,
+      ],
       cantidadUmc: [this.mercanciaFormState.cantidadUmc, Validators.required],
       presentacion: [this.mercanciaFormState.presentacion, Validators.required],
-      numeroRegistroSanitario: [this.mercanciaFormState.numeroRegistroSanitario, Validators.required],
+      numeroRegistroSanitario: [
+        this.mercanciaFormState.numeroRegistroSanitario,
+        Validators.required,
+      ],
       fechaCaducidad: [this.mercanciaFormState.fechaCaducidad],
-      paisDeOriginDatos: [this.mercanciaFormState.paisDeOriginDatos || [], Validators.required],
-      paisDeProcedenciaDatos: [this.mercanciaFormState.paisDeProcedenciaDatos || [], Validators.required],
+      paisDeOriginDatos: [
+        this.mercanciaFormState.paisDeOriginDatos || [],
+        Validators.required,
+      ],
+      paisDeProcedenciaDatos: [
+        this.mercanciaFormState.paisDeProcedenciaDatos || [],
+        Validators.required,
+      ],
     });
   }
 
@@ -145,43 +317,43 @@ export class DatosMercanciaComponent implements OnInit {
   paisDeOriginSeleccionadasChange(events: string[]): void {
     this.seleccionadasPaisDeOriginDatos = events;
     this.mercanciaForm.patchValue({
-      paisDeOriginDatos: events
+      paisDeOriginDatos: events,
     });
   }
 
   /**
    * Maneja el evento de cambio para las selecciones de país de procedencia.
-   * 
+   *
    * @param events - Un arreglo de cadenas que representa los países seleccionados.
-   * 
+   *
    * Actualiza la propiedad `seleccionadasPaisDeProcedenciaDatos` con los valores seleccionados
    * y sincroniza el formulario `mercanciaForm` con los datos actualizados.
    */
   paisDeProcedenciaSeleccionadasChange(events: string[]): void {
     this.seleccionadasPaisDeProcedenciaDatos = events;
     this.mercanciaForm.patchValue({
-      paisDeProcedenciaDatos: events
+      paisDeProcedenciaDatos: events,
     });
   }
 
   /**
    * Maneja el evento de cambio para las selecciones de uso específico.
-   * 
+   *
    * @param events - Un arreglo de cadenas que representa las selecciones actuales de uso específico.
-   * 
+   *
    * Este método actualiza la propiedad `seleccionadasUsoEspesificoDatos` con las selecciones proporcionadas
    * y actualiza el formulario `mercanciaForm` para reflejar los valores seleccionados en el campo `usoEspecifico`.
    */
   usoEspesificoSeleccionadasChange(events: string[]): void {
     this.seleccionadasUsoEspesificoDatos = events;
     this.mercanciaForm.patchValue({
-      usoEspecifico: events
+      usoEspecifico: events,
     });
   }
 
   /**
    * Alterna el estado colapsable de una sección específica basada en el orden proporcionado.
-   * 
+   *
    * @param orden - Número que indica la sección a modificar:
    *   - 1: Alterna el estado de `paisDeOriginColapsable`.
    *   - 2: Alterna el estado de `paisDeProcedenciaColapsable`.
