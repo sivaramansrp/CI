3311e75614583170143dba0d566caa627c939007
navin-ilink <navin.kumar@ilink-system.com>
2025-03-26 14:15:21 +0530

    add routing in dashboard

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -95,5 +95,14 @@ export const appRoutes: Route[] = [
             remoteName: 'sener',
             exposedModule: './Module'
         }).then((m) => m.AppSenerModule)
+  },
+  {
+    path: 'cofepris',
+    loadChildren: () =>
+        loadRemoteModule({
+            remoteEntry: 'http://localhost:4217/remoteAppEntry.js',
+            remoteName: 'cofepris',
+            exposedModule: './Module'
+        }).then((m) => m.cofeprisModule)
   }
 ];
