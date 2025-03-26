9ad3fc2a4f01ac41cfa5555f0f3c6e7c40c0c30c
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-03-26 10:22:38 +0530

    Changed services

--- apps/cofepris/src/app/application/tramites/260101/pages/paso-tres/paso-tres.component.ts
+++ apps/cofepris/src/app/application/tramites/260101/pages/paso-tres/paso-tres.component.ts
@@ -1,7 +1,7 @@
 import { catchError, map } from 'rxjs';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ServiciosExtraordinariosService } from '@ng-mf/data-access-user';
+import { TramiteFolioService } from '@ng-mf/data-access-user';
 
 /**
  * Componente PasoTresComponent.
@@ -22,7 +22,7 @@ export class PasoTresComponent {
    */
   constructor(
     private router: Router,
-    private serviciosExtraordinariosServices: ServiciosExtraordinariosService,
+    private serviciosExtraordinariosServices: TramiteFolioService,
   ) { 
     // Inicialización
   }
