817f633f1ad5d276da74b019def6fdf2a475b433
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-03-27 21:24:43 +0530

    update unit test case

--- apps/se/src/app/application/tramites/110216/components/datos-certificado/datos-certificado.component.spec.ts
+++ apps/se/src/app/application/tramites/110216/components/datos-certificado/datos-certificado.component.spec.ts
@@ -2,13 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
 import { of } from 'rxjs';
 import { DatosCertificadoComponent } from './datos-certificado.component';
-import { CertificadosOrigenService } from '../../services/certificadosOrigen.service';
 import { Tramite110216Store } from '../../../../estados/tramites/tramite110216.store';
 import { Tramite110216Query } from '../../../../estados/queries/tramite110216.query';
 import { TituloComponent } from '@libs/shared/data-access-user/src';
 import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { provideToastr, ToastrService } from 'ngx-toastr';
+import { CertificadosOrigenService } from '../../services/certificado-origen.service.ts';
 
 describe('DatosCertificadoComponent', () => {
   let component: DatosCertificadoComponent;
@@ -120,5 +120,34 @@ describe('DatosCertificadoComponent', () => {
     component.cargarRepresentacionFederal();
     expect(certificadosOrigenServiceMock.obtenerRepresentacionFederal).toHaveBeenCalled();
     expect(component.representacionFederal).toEqual([{ id: 1, descripcion: 'Representación 1' }]);
+  });
+  it('should call setValoresStore for representacionFederalSeleccion', () => {
+    const setValoresStoreSpy = jest.spyOn(component, 'setValoresStore');
+    component.representacionFederalSeleccion();
+    expect(setValoresStoreSpy).toHaveBeenCalledWith(component.formDatosCertificado, 'representacionFederal', 'setRepresentacionFederal');
+  });
+
+  it('should call setValoresStore for entidadFederativaSeleccion', () => {
+    const setValoresStoreSpy = jest.spyOn(component, 'setValoresStore');
+    component.entidadFederativaSeleccion();
+    expect(setValoresStoreSpy).toHaveBeenCalledWith(component.formDatosCertificado, 'entidadFederativa', 'setEntidadFederativa');
+  });
+
+  it('should call setValoresStore for idiomaSeleccion', () => {
+    const setValoresStoreSpy = jest.spyOn(component, 'setValoresStore');
+    component.idiomaSeleccion();
+    expect(setValoresStoreSpy).toHaveBeenCalledWith(component.formDatosCertificado, 'idioma', 'setIdioma');
+  });
+
+  it('should load idiomas on cargarIdioma', () => {
+    component.cargarIdioma();
+    expect(certificadosOrigenServiceMock.obtenerIdioma).toHaveBeenCalled();
+    expect(component.idiomas).toEqual([{ id: 1, descripcion: 'Español' }]);
+  });
+
+  it('should load entidadFederativas on cargarEntidadFederativa', () => {
+    component.cargarEntidadFederativa();
+    expect(certificadosOrigenServiceMock.obtenerEntidadFederativa).toHaveBeenCalled();
+    expect(component.entidadFederativas).toEqual([{ id: 1, descripcion: 'Entidad 1' }]);
   });
 });
