ea102ef01a0cf352f23c403016de4634a4ff4548
Panfilo Santiago Lorenzo <psantiagol@ultrasist.com.mx>
2025-03-27 11:49:50 -0600

    Atencion de una observacion en el metodo obtenerNombreDelTítulo

--- apps/funcionario/src/app/application/components/autorizar/autorizar-dictamen/autorizar-dictamen.component.ts
+++ apps/funcionario/src/app/application/components/autorizar/autorizar-dictamen/autorizar-dictamen.component.ts
@@ -21,26 +21,23 @@ export class AutorizarDictamenComponent implements OnInit{
     this.dictamenForm = this.fb.group({
       cumplimiento: ['1'], 
       mensajeDictamen: [{ value: '', disabled: true }]      
-    });
-    
+    });    
   }
   ngOnInit(): void {
     this.dictamenForm.get('mensajeDictamen')?.enable();
-    this.dictamenForm.get('mensajeDictamen')?.setValue(this.obtenerNombreDelTítulo(1));
-    this.dictamenForm.get('mensajeDictamen')?.disable();
+    this.dictamenForm.get('mensajeDictamen')?.setValue(AutorizarDictamenComponent.obtenerNombreDelTítulo(1));
+    this.dictamenForm.get('mensajeDictamen')?.disable();    
   }
-
   /**
    * @description Metodo que redirige a la pantalla de firma electronica
    */
   navegarAFirmaElectronica(): void {  
     this.router.navigate(['funcionario/firma-electronica']);    
-  } 
-
+  }
   /**
    * @description Metodo que redirige a la pantalla de rechazo de dictamen
    */
-  obtenerNombreDelTítulo(valor: number): string {
+  static obtenerNombreDelTítulo(valor: number): string {
     switch (valor) {
       case 1:
         return 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
@@ -56,5 +53,4 @@ export class AutorizarDictamenComponent implements OnInit{
   navegarAObservaciones(): void {  
     this.router.navigate(['funcionario/observaciones-dictamen']);    
   } 
-
 }
