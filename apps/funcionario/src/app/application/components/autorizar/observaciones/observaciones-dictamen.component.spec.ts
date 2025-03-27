56d959ad78145bc9d54050842e061f2db34e17f2
Panfilo Santiago Lorenzo <psantiagol@ultrasist.com.mx>
2025-03-27 17:57:40 -0600

    atencion a los comandos de calidad

--- apps/funcionario/src/app/application/components/autorizar/observaciones/observaciones-dictamen.component.spec.ts
+++ apps/funcionario/src/app/application/components/autorizar/observaciones/observaciones-dictamen.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ObservacionesDictamenComponent } from './observaciones-dictamen.component';
+import { HttpClientModule } from '@angular/common/http'; // Importar HttpClientModule
+import { ReactiveFormsModule } from '@angular/forms'; // Importar ReactiveFormsModule si es necesario
+import { DictamenObservacionesService } from '../../../core/service/dictamen-observaciones.service'; // Importar el servicio si es necesario
 
 describe('ObservacionesDictamenComponent', () => {
   let component: ObservacionesDictamenComponent;
@@ -7,7 +10,12 @@ describe('ObservacionesDictamenComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ObservacionesDictamenComponent],
+      imports: [
+        ObservacionesDictamenComponent,
+        HttpClientModule, // Agregar HttpClientModule aquí
+        ReactiveFormsModule // Agregar ReactiveFormsModule si el componente usa formularios reactivos
+      ],
+      providers: [DictamenObservacionesService] // Asegurarse de que el servicio esté disponible
     }).compileComponents();
 
     fixture = TestBed.createComponent(ObservacionesDictamenComponent);
