d3a584642cff9a66957d31bd47611586f8021a1b
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-27 17:19:39 +0530

    module created

---
+++ apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { DepositoFiscalManufacturaVehiculosRoutingModule } from './deposito-fiscal-manufactura-vehiculos-routing.module';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DepositoFiscalManufacturaVehiculosRoutingModule
+  ]
+})
+export class DepositoFiscalManufacturaVehiculosModule { }
