e78b5339f11f206b3a3e79be5aee9d68f309a32d
varshakatre15 <varsha.katre@ilink-systems.com>
2025-03-27 11:50:28 +0530

    URL added

--- apps/cofepris/src/app/application/shared/components/tercerosRelacionado/tercerosRelacionado.component.ts
+++ apps/cofepris/src/app/application/shared/components/tercerosRelacionado/tercerosRelacionado.component.ts
@@ -22,6 +22,7 @@ import { Tramites260604Query } from '../../../shared/estados/tramites260604.quer
 import { InputRadioComponent } from '@libs/shared/data-access-user/src';
 
 import tipoPersonaradio from 'libs/shared/theme/assets/json/260604/tipoPersonaradio.json';
+import { NICO_TABLA } from '../../models/aviso-exportacion.model';
 
 @Component({
   selector: 'app-terceros-relacionado',
@@ -50,27 +51,10 @@ export class TercerosRelacionadoComponent implements OnInit, OnDestroy {
 
   @ViewChild('closeModal') closeModal!: ElementRef;
 
-  configuracionTabla: ConfiguracionColumna<PermisoModel>[] = [
-    { encabezado: 'Nombre/denominacion o razon social', clave: (item: PermisoModel) => item.Nombre, orden: 1 },
-    { encabezado: 'RFC', clave: (item: PermisoModel) => item.RFC, orden: 2 },
-    { encabezado: 'CURP', clave: (item: PermisoModel) => item.CURP, orden: 3 },
-    { encabezado: 'Telefono', clave: (item: PermisoModel) => item.Teléfono, orden: 4 },
-    { encabezado: 'Correo electronico', clave: (item: PermisoModel) => item.CorreoElectrónico, orden: 5 },
-    { encabezado: 'Calle', clave: (item: PermisoModel) => item.calle, orden: 6 },
-    { encabezado: 'numeroExterior', clave: (item: PermisoModel) => item.numeroExterior, orden: 7 },
-    { encabezado: 'numeroInterior', clave: (item: PermisoModel) => item.numeroInterior, orden: 8 },
-    { encabezado: 'pais', clave: (item: PermisoModel) => item.calle, orden: 9 },
-    { encabezado: 'colonia', clave: (item: PermisoModel) => item.colonia, orden: 10 },
-    { encabezado: 'municipio', clave: (item: PermisoModel) => item.municipio, orden: 11 },
-    { encabezado: 'localidad', clave: (item: PermisoModel) => item.localidad, orden: 12 },
-    { encabezado: 'entidadFederativa', clave: (item: PermisoModel) => item.entidadFederativa, orden: 13 },
-    { encabezado: 'estadoLocalidad', clave: (item: PermisoModel) => item.estadoLocalidad, orden: 14 },
-    { encabezado: 'codigoPostal', clave: (item: PermisoModel) => item.codigoPostal, orden: 15 },
-  ];
-
+  configuracionTabla: ConfiguracionColumna<PermisoModel>[] = NICO_TABLA
+    
   ngOnInit(): void {
-
-    this.tramites260604Query.selectSolicitud$
+  this.tramites260604Query.selectSolicitud$
           .pipe(
             takeUntil(this.destroyNotifier$),
             map((seccionState) => {
--- apps/cofepris/src/app/application/shared/models/aviso-exportacion.model.ts
+++ apps/cofepris/src/app/application/shared/models/aviso-exportacion.model.ts
@@ -20,3 +20,23 @@ export interface AccionBoton {
         estadoLocalidad:string;
         codigoPostal:number;
 }
+
+export const NICO_TABLA = [
+  { encabezado: 'Nombre/denominacion o razon social', clave: (item: PermisoModel) => item.Nombre, orden: 1 },
+  { encabezado: 'RFC', clave: (item: PermisoModel) => item.RFC, orden: 2 },
+  { encabezado: 'CURP', clave: (item: PermisoModel) => item.CURP, orden: 3 },
+  { encabezado: 'Telefono', clave: (item: PermisoModel) => item.Teléfono, orden: 4 },
+  { encabezado: 'Correo electronico', clave: (item: PermisoModel) => item.CorreoElectrónico, orden: 5 },
+  { encabezado: 'Calle', clave: (item: PermisoModel) => item.calle, orden: 6 },
+  { encabezado: 'numeroExterior', clave: (item: PermisoModel) => item.numeroExterior, orden: 7 },
+  { encabezado: 'numeroInterior', clave: (item: PermisoModel) => item.numeroInterior, orden: 8 },
+  { encabezado: 'pais', clave: (item: PermisoModel) => item.calle, orden: 9 },
+  { encabezado: 'colonia', clave: (item: PermisoModel) => item.colonia, orden: 10 },
+  { encabezado: 'municipio', clave: (item: PermisoModel) => item.municipio, orden: 11 },
+  { encabezado: 'localidad', clave: (item: PermisoModel) => item.localidad, orden: 12 },
+  { encabezado: 'entidadFederativa', clave: (item: PermisoModel) => item.entidadFederativa, orden: 13 },
+  { encabezado: 'estadoLocalidad', clave: (item: PermisoModel) => item.estadoLocalidad, orden: 14 },
+  { encabezado: 'codigoPostal', clave: (item: PermisoModel) => item.codigoPostal, orden: 15 },
+]
+
+
