240bfeab7a52c5ea63ead494c25ba8414f410d1e
swamini1212-ilink <swamini.godse@ilink-systems.com>
2025-03-27 11:54:35 +0530

    added the  setup for the components and tab folder

--- apps/cofepris/src/app/application/tramites/260214/importacion-dispositivos-mediocos-uso-routing.module.ts
+++ apps/cofepris/src/app/application/tramites/260214/importacion-dispositivos-mediocos-uso-routing.module.ts
@@ -1,6 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarDestinatarioFinalComponent } from '../../shared/components/agregar-destinatario-final/agregar-destinatario-final.component';
+import { AgregarFabricanteComponent } from '../../shared/components/agregar-fabricante/agregar-fabricante.component';
+import { AgregarFacturadorComponent } from '../../shared/components/agregar-facturador/agregar-facturador.component';
+import { AgregarProveedorComponent } from '../../shared/components/agregar-proveedor/agregar-proveedor.component';
 import { ContenedorDePasosComponent } from './pages/contenedor-de-paso/contenedor-de-pasos.component';
+import { DatosMercanciaContenedoraComponent } from './components/datos-mercancia-contenedora/datos-mercancia-contenedora.component';
 import { NgModule } from '@angular/core';
+import { ScianTablaContenedoraComponent } from './components/scian-tabla-contenedora/scian-tabla-contenedora.component';
 
 
 const ROUTES: Routes = [
@@ -8,6 +14,30 @@ const ROUTES: Routes = [
     path: 'contenedor-de-pasos',
     component: ContenedorDePasosComponent,
   },
+  {
+      path: 'scian-selecion',
+      component: ScianTablaContenedoraComponent,
+    },
+    {
+      path: 'mercancia-datos',
+      component: DatosMercanciaContenedoraComponent,
+    },
+    {
+      path: 'agregar-fabricante',
+      component: AgregarFabricanteComponent,
+    },
+    {
+      path: 'agregar-destinatario-final',
+      component: AgregarDestinatarioFinalComponent,
+    },
+    {
+      path: 'agregar-proveedor',
+      component: AgregarProveedorComponent,
+    },
+    {
+      path: 'agregar-facturador',
+      component: AgregarFacturadorComponent,
+    },
   {
     path: '',
     pathMatch: 'full',
--- apps/cofepris/src/app/application/tramites/260214/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260214/pages/paso-uno/paso-uno.component.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContenedorDeDatosSolicitudComponent } from '../../components/contenedor-de-datos-solicitud/contenedor-de-datos-solicitud.component';
+import { PagoDeDerechosComponent } from '../../../../shared/components/pago-de-derechos/pago-de-derechos.component';
 import { SolicitanteComponent } from '@ng-mf/data-access-user';
+import { TercerosRelacionadosVistaComponent } from '../../components/terceros-relacionados-vista/terceros-relacionados-vista.component';
 
 
 @Component({
   selector: 'app-paso-uno',
   standalone: true,
-  imports: [CommonModule, SolicitanteComponent],
+  imports: [CommonModule, SolicitanteComponent,
+    ContenedorDeDatosSolicitudComponent,TercerosRelacionadosVistaComponent,
+    PagoDeDerechosComponent
+  ],
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.css',
 })
