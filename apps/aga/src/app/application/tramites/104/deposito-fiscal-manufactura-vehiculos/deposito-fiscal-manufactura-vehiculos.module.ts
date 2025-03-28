9cad3629f1177776a8a2844fde9f5c41dc45970b
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-28 12:30:07 +0530

    page added

--- apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
+++ apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { DepositoFiscalManufacturaVehiculosRoutingModule } from './deposito-fiscal-manufactura-vehiculos-routing.module';
 import { NgModule } from '@angular/core';
+import { SolicitudPageComponent } from '../pages/solicitud-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SolicitudPageComponent],
   imports: [
     CommonModule,
     DepositoFiscalManufacturaVehiculosRoutingModule
---
+++ apps/aga/src/app/application/tramites/104/pages/solicitud-page.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud-page',
+  templateUrl: './solicitud-page.component.html',
+})
+export class SolicitudPageComponent {}
