3a2cfd94e02a79336a431f6d390eb205b6e17e22
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-28 15:30:53 +0530

    reusable component added

--- apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
+++ apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { DepositoFiscalManufacturaVehiculosRoutingModule } from './deposito-fiscal-manufactura-vehiculos-routing.module';
 import { NgModule } from '@angular/core';
+import { PasoUnoComponent } from '../pages/paso-uno/paso-uno.component';
+import { SolicitanteComponent } from '@ng-mf/data-access-user';
 import { SolicitudPageComponent } from '../pages/solicitud-page/solicitud-page.component';
 import { WizardComponent } from '@libs/shared/data-access-user/src';
 
 @NgModule({
-  declarations: [SolicitudPageComponent,],
+  declarations: [SolicitudPageComponent,PasoUnoComponent],
   imports: [
     CommonModule,
     DepositoFiscalManufacturaVehiculosRoutingModule,
-    WizardComponent
+    WizardComponent,
+    SolicitanteComponent
   ]
 })
 export class DepositoFiscalManufacturaVehiculosModule { }
