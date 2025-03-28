de4c3511ddba0ddd0bf33d27a44e5c55cdceec4d
Oscar Valois <ovaloism@desarrollo-ultrasist.com.mx>
2025-03-28 15:51:55 -0600

    Update webpack.config.ts

--- apps/sener/webpack.config.ts
+++ apps/sener/webpack.config.ts
@@ -1,25 +1,4 @@
 const { withModuleFederation } = require('@nx/angular/module-federation');
+const config = require('./module-federation.config');
 
-// Configuración específica de Module Federation para sener
-const moduleFederationConfig = {
-  name: 'sener',
-  exposes: {
-    './Module': './apps/sener/src/app/application/app.module.ts',
-    './Routes': './apps/sener/src/app/remote-entry/entry.routes.ts'
-  },
-  shared: {
-    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular-architects/module-federation': { 
-      singleton: true, 
-      strictVersion: true, 
-      requiredVersion: 'auto' 
-    },
-  }
-};
-
-// Aplicar la configuración de Module Federation utilizando el wrapper de NX
-module.exports = withModuleFederation(moduleFederationConfig);
+module.exports = withModuleFederation(config);
