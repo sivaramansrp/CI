4b10f042ace1ad00fa54c1ced86aef9f39668414
swamini1212-ilink <swamini.godse@ilink-systems.com>
2025-03-28 22:22:44 +0530

    fixed the title of the procedure

--- apps/cofepris/src/app/application/tramites/260216/constants/medicos-uso.enum.ts
+++ apps/cofepris/src/app/application/tramites/260216/constants/medicos-uso.enum.ts
@@ -19,7 +19,7 @@ export const PASOS = [
     },
   ];
   export const TITULOMENSAJE =
-  'Permiso sanitario de importación de dispositivos médicos para uso médico';
+  'Permiso sanitario de importación de dispositivos médicos para donación';
 export const TEXTOS_REQUISITOS =
   'La solicitud ha quedado registrada con el número temporal [202767640]. Este no tiene validez legal y sirve solamente para efectos de identificar tu Solicitud. Un folio oficial le será asignado a la solicitud al momento en que esta sea firmada.';
 
