8581f6d57fa0d7166608dbc03de1e4f97b8759b6
atripathi860 <amit.tripathi@ilink-systems.com>
2025-03-28 14:43:50 +0530

    paso dos, paso tres done

--- apps/cofepris/src/app/application/tramites/260501/permiso-plaguicidas-importacion.module.ts
+++ apps/cofepris/src/app/application/tramites/260501/permiso-plaguicidas-importacion.module.ts
@@ -1,5 +1,13 @@
+import {
+  CatalogosService,
+  InicioSesionService,
+  SubirDocumentoService,
+} from '@libs/shared/data-access-user/src';
+import { AlertComponent } from '@libs/shared/data-access-user/src/tramites/components/alert/alert.component';
+import { AnexarDocumentosComponent } from '@libs/shared/data-access-user/src/tramites/components/anexar-documentos/anexar-documentos.component';
 import { BtnContinuarComponent } from '@libs/shared/data-access-user/src/tramites/components/btn-continuar/btn-continuar.component';
 import { CommonModule } from '@angular/common';
+import { FirmaElectronicaComponent } from '@libs/shared/data-access-user/src/tramites/components/firma-electronica/firma-electronica.component';
 import { NgModule } from '@angular/core';
 import { PagoDeDerechosBancoComponent } from '../../shared/components/pago-de-derechos-banco/pago-de-derechos-banco.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
@@ -8,8 +16,11 @@ import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { PermisoPlaguicidasImportacionRoutingModule } from './permiso-plaguicidas-importacion-routing.module';
 import { PlaguicidasComponent } from './pages/plaguicidas/plaguicidas.component';
 import { SolicitanteComponent } from '@libs/shared/data-access-user/src/tramites/components/solicitante/solicitante.component';
+import { TercerosRelacionadosComponent } from '../../shared/components/terceros-fabricante/terceros-fabricante.component';
+import { TituloComponent } from '@libs/shared/data-access-user/src/tramites/components/titulo/titulo.component';
+import { ToastrService } from 'ngx-toastr';
 import { WizardComponent } from '@libs/shared/data-access-user/src/tramites/components/wizard/wizard.component';
-import { TercerosRelacionadosComponent } from "../../shared/components/terceros-fabricante/terceros-fabricante.component";
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -25,7 +36,18 @@ import { TercerosRelacionadosComponent } from "../../shared/components/terceros-
     WizardComponent,
     SolicitanteComponent,
     PagoDeDerechosBancoComponent,
-    TercerosRelacionadosComponent
-],
+    TercerosRelacionadosComponent,
+    FirmaElectronicaComponent,
+    TituloComponent,
+    AlertComponent,
+    AnexarDocumentosComponent,
+  ],
+  providers: [
+    ToastrService,
+    CatalogosService,
+    InicioSesionService,
+    provideHttpClient(),
+    SubirDocumentoService,
+  ],
 })
 export class PermisoPlaguicidasImportacionModule {}
