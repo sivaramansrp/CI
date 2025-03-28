09df5f063f5f5f4b7a8664fc71531209d1946c0d
Oscar Valois <ovaloism@desarrollo-ultrasist.com.mx>
2025-03-28 16:02:13 -0600

    Update module-federation.config.ts

--- apps/sener/module-federation.config.ts
+++ apps/sener/module-federation.config.ts
@@ -6,17 +6,13 @@ const CONFIG: ModuleFederationConfig = {
     './Module': 'apps/sener/src/app/application/app.module.ts',
     './Routes': 'apps/sener/src/app/remote-entry/entry.routes.ts',
   },
+  // Definido como un objeto compatible con NX
   shared: {
-    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular/forms': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
-    '@angular-architects/module-federation': { 
-      singleton: true, 
-      strictVersion: true, 
-      requiredVersion: 'auto' 
-    }
+    '@angular/core': { singleton: true, strictVersion: true },
+    '@angular/common': { singleton: true, strictVersion: true },
+    '@angular/common/http': { singleton: true, strictVersion: true },
+    '@angular/router': { singleton: true, strictVersion: true },
+    '@angular/forms': { singleton: true, strictVersion: true }
   }
 };
 
