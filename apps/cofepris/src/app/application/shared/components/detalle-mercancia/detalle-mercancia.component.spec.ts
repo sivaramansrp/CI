5fb61208ef3fc42c8be4c4e93d283e39908ba791
Atul Falle <atul.falle@ilink-systems.com>
2025-03-28 18:21:32 +0530

    updated Detalles mercancia component

--- apps/cofepris/src/app/application/shared/components/detalle-mercancia/detalle-mercancia.component.spec.ts
+++ apps/cofepris/src/app/application/shared/components/detalle-mercancia/detalle-mercancia.component.spec.ts
@@ -1,21 +1,86 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// @ts-nocheck
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
 import { DetalleMercanciaComponent } from './detalle-mercancia.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('DetalleMercanciaComponent', () => {
-  let component: DetalleMercanciaComponent;
-  let fixture: ComponentFixture<DetalleMercanciaComponent>;
+  let fixture;
+  let component;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DetalleMercanciaComponent],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        FormBuilder
+      ]
+    }).overrideComponent(DetalleMercanciaComponent, {
+
     }).compileComponents();
-
     fixture = TestBed.createComponent(DetalleMercanciaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
-  it('should create', () => {
+
+  it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
   });
+
+  it('should run #ngOnInit()', async () => {
+    component.formaDetalleMercancia = component.formaDetalleMercancia || {};
+    component.formaDetalleMercancia.patchValue = jest.fn();
+    component.ngOnInit();
+    expect(component.formaDetalleMercancia.patchValue).toHaveBeenCalled();
+  });
+
+  it('should run #isValid()', async () => {
+
+    component.isValid({
+      controls: {
+        campo: {
+          errors: {},
+          touched: {}
+        }
+      },
+      errors: {},
+      touched: {}
+    }, {});
+
+  });
+
+  it('should run #eliminarMercancias()', async () => {
+    component.eliminarMercancia = component.eliminarMercancia || {};
+    component.tablaMercanciasLista = [0];
+    component.eliminarMercancia.emit = jest.fn();
+    component.eliminarMercancias();
+    expect(component.eliminarMercancia.emit).toHaveBeenCalled();
+  });
+
+  it('should run #agregarMercancias()', async () => {
+    component.formaDetalleMercancia = component.formaDetalleMercancia || {};
+    component.formaDetalleMercancia.valid = 'valid';
+    component.formaDetalleMercancia.value = {
+      formaFormaceutica: {}
+    };
+    component.formaDetalleMercancia.reset = jest.fn();
+    component.datosFormFormaceutica = component.datosFormFormaceutica || {};
+    component.datosFormFormaceutica.find = jest.fn().mockReturnValue([
+      {
+        "id": '1'
+      }
+    ]);
+    component.aggregarMercancia = component.aggregarMercancia || {};
+    component.aggregarMercancia.emit = jest.fn();
+    component.agregarMercancias();
+    expect(component.formaDetalleMercancia.reset).toHaveBeenCalled();
+ 
+  });
+
 });
