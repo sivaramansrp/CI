e7966948ee9c4c171962fa287f29da24279c9849
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-03-31 12:24:16 -0600

    Atiende cambios requeridos

--- apps/aga/src/app/application/shared/components/check-input-text/check-input-text.component.ts
+++ apps/aga/src/app/application/shared/components/check-input-text/check-input-text.component.ts
@@ -22,15 +22,56 @@ import { CommonModule } from '@angular/common';
   styleUrl: './check-input-text.component.scss',
 })
 export class CheckInputTextComponent implements OnChanges {
+
+  /**
+   * Identificador único para el checkbox.
+   * @required
+   */
   @Input({ required: true }) idCheckbox!: string;
+
+  /**
+   * Identificador único para el input de texto.
+   * @required
+   */
   @Input({ required: true }) idInputTexto!: string;
+
+  /**
+   * Texto que se muestra en el label del checkbox.
+   * @required
+   */
   @Input({ required: true }) label!: string;
+
+
+  /**
+   * Indica si el campo es obligatorio.
+   */
   @Input({ required: true }) required!: boolean;
+
+  /**
+   * Indica la clase CSS que se aplicará al checkbox. 
+   * Si reverse es verdadero, se aplicará la clase ''form-check-custome', que pone el checkbox al final
+   * de la etiqueta label.
+   * Por default la clase es 'form-check', donde el checkbox se encuentra al inicio de la etiqueta label.
+   */
   @Input({ required: true }) reverse!: boolean;
-  @Input({required: true}) hidden!: boolean;
+
+  /**
+   * Indica si el input de texto está oculto.
+   */
+  @Input({ required: true }) hidden!: boolean;
+
+  /**
+   * Indica el tipo del componente:
+   * - 'visible': el input de texto es visible y deshabilitado.
+   * - 'invisible': el input de texto es invisible y deshabilitado.
+   */
   @Input({ required: true }) tipo!: string;
 
 
+  /**
+   * Formulario reactivo que contiene el checkbox y el input de texto.
+   * @required
+   */
   forma: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -40,7 +81,14 @@ export class CheckInputTextComponent implements OnChanges {
     });
   }
 
-  ngOnChanges(changes: SimpleChanges): void { 
+  /**
+   * @inheritdoc
+   * Detecta cambios en las propiedades de entrada del componente.
+   * 
+   * @param changes - Objeto que contiene los cambios detectados en las propiedades de entrada.
+   * @returns void
+   */
+  ngOnChanges(changes: SimpleChanges): void {
     if (changes['reverse']) {
       this.reverse = changes['reverse'].currentValue;
     }
@@ -48,7 +96,12 @@ export class CheckInputTextComponent implements OnChanges {
       this.required = changes['required'].currentValue;
     }
   }
-  
+
+  /**
+   * Cambia el estado del input de texto basado en el tipo y el valor del checkbox.
+   * 
+   * @returns {void} No retorna ningún valor.
+   */
   cambioInputTexto(): void {
     const CHECKBOX = this.forma.get('checkbox')?.value;
 
@@ -62,19 +115,32 @@ export class CheckInputTextComponent implements OnChanges {
 
     if (CHECKBOX) {
       this.activarInputTexto();
-     
+
     } else {
       this.desactivarInputTexto();
     }
   }
 
-
+  /**
+   * Activa el campo de texto en el formulario, habilitándolo, 
+   * asignando validadores requeridos y actualizando su estado.
+   * 
+   * @returns {void} No retorna ningún valor.
+   */
   activarInputTexto(): void {
     this.forma.get('texto')?.enable();
     this.forma.get('texto')?.setValidators([Validators.required]);
     this.forma.get('texto')?.updateValueAndValidity();
   }
 
+  /**
+   * Desactiva el campo de texto en el formulario.
+   * - Deshabilita el control.
+   * - Elimina los validadores asociados.
+   * - Actualiza el estado del control.
+   * 
+   * @returns {void} No retorna ningún valor.
+   */
   desactivarInputTexto(): void {
     this.forma.get('texto')?.disable();
     this.forma.get('texto')?.clearValidators();
