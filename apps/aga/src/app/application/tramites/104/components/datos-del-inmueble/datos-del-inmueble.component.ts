fffb4eac7f1e23146125f6ae26c3718a33b2c232
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-31 17:00:49 +0530

    json and model added

--- apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
+++ apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
@@ -1,26 +1,36 @@
 import { CatalogoSelectComponent, TableComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { Component,OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-// eslint-disable-next-line @nx/enforce-module-boundaries
-import dropDown from 'libs/shared/theme/assets/json/104/drop-down.json'
+import { TableData } from '@libs/shared/data-access-user/src/core/models/104/model-104';
+import dropDown from '@libs/shared/theme/assets/json/104/selector-104.json'
 
 @Component({
   selector: 'app-datos-del-inmueble',
   standalone: true,
-  imports: [CommonModule,TituloComponent,
+  imports: [CommonModule, TituloComponent,
     TableComponent,
     CatalogoSelectComponent,
     ReactiveFormsModule],
   templateUrl: './datos-del-inmueble.component.html',
   styleUrl: './datos-del-inmueble.component.css',
 })
-export class DatosDelInmuebleComponent {
+export class DatosDelInmuebleComponent implements OnInit{
 
-  fomentoExportacionForm!:FormGroup;
+  fomentoExportacionForm!: FormGroup;
+
+  public establecimientoHeaderData: string[] = [];
+
+  public establecimientoBodyData: unknown = [];
+
+  destinatarioTableData: TableData = { encabezadoDeTabla: [], cuerpoTabla: [] };
 
   constructor(private fb: FormBuilder) {
     this.inicializarFormularioTratados();
+  }
+
+  ngOnInit(): void {
+    this.getEstableCimiento();
   }
 
 
@@ -36,4 +46,11 @@ export class DatosDelInmuebleComponent {
     { catalogos: dropDown.tipoPrograma },
     { catalogos: dropDown.folioAutorizacion },
   ];
+
+
+  private getEstableCimiento(): void {
+    this.establecimientoHeaderData = this.destinatarioTableData?.encabezadoDeTabla;
+    this.establecimientoBodyData = this.destinatarioTableData?.cuerpoTabla;
+  }
+
 }
