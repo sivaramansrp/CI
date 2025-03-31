2d3fe86cea1ae534bea731f6b2983ced9baf0274
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-03-31 15:18:41 +0530

    drop down added

--- apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
+++ apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
@@ -1,11 +1,39 @@
+import { CatalogoSelectComponent, TableComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import dropDown from 'libs/shared/theme/assets/json/104/drop-down.json'
 
 @Component({
   selector: 'app-datos-del-inmueble',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TituloComponent,
+    TableComponent,
+    CatalogoSelectComponent,
+    ReactiveFormsModule],
   templateUrl: './datos-del-inmueble.component.html',
   styleUrl: './datos-del-inmueble.component.css',
 })
-export class DatosDelInmuebleComponent {}
+export class DatosDelInmuebleComponent {
+
+  fomentoExportacionForm!:FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.inicializarFormularioTratados();
+  }
+
+
+
+  inicializarFormularioTratados(): void {
+    this.fomentoExportacionForm = this.fb.group({
+      tipoPrograma: ['', Validators.required],
+      folioAutorizacion: ['', Validators.required],
+    });
+  }
+
+  configuracionesDropdown = [
+    { catalogos: dropDown.tipoPrograma },
+    { catalogos: dropDown.folioAutorizacion },
+  ];
+}
