e4af76549bd56580c88eb5b26bfa82181d8d940e
atripathi860 <amit.tripathi@ilink-systems.com>
2025-04-01 14:04:53 +0530

    Add missing documentation to all the implemented methods and variables

--- apps/cofepris/src/app/application/tramites/260501/pages/plaguicidas/plaguicidas.component.ts
+++ apps/cofepris/src/app/application/tramites/260501/pages/plaguicidas/plaguicidas.component.ts
@@ -8,16 +8,35 @@ interface AccionBoton {
   valor: number;
 }
 
+/**
+ * Componente principal para la gestión de plaguicidas.
+ * Contiene la lógica y la estructura del asistente de plaguicidas.
+ */
 @Component({
   selector: 'app-plaguicidas',
   templateUrl: './plaguicidas.component.html',
 })
 export class PlaguicidasComponent {
+  /**
+   * Lista de pasos del asistente.
+   * Se obtiene de una constante definida en otro archivo.
+   */
   pasos: ListaPasosWizard[] = PASOS;
+
+  /**
+   * Indice actual del paso en el asistente.
+   * Se inicializa en 1.
+   */
   indice: number = 1;
 
+  /**
+   * Título del asistente.
+   */
   @ViewChild(WizardComponent) wizardComponent!: WizardComponent;
 
+  /**
+   * Título del asistente.
+   */
   datosPasos: DatosPasos = {
     nroPasos: this.pasos.length,
     indice: this.indice,
@@ -25,6 +44,12 @@ export class PlaguicidasComponent {
     txtBtnSig: 'Continuar',
   };
 
+  /**
+   * Maneja la acción del botón en el asistente.
+   * Cambia el paso actual según la acción del botón.
+   *
+   * @param e - Objeto que contiene la acción y el valor del botón.
+   */
   getValorIndice(e: AccionBoton): void {
     if (e.valor > 0 && e.valor < 5) {
       this.indice = e.valor;
