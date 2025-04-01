cbba46b1d973285f2ea4d8ddd138051281336e36
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-01 17:56:29 +0530

    Firmar solicitud added

--- apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
+++ apps/aga/src/app/application/tramites/104/deposito-fiscal-manufactura-vehiculos/deposito-fiscal-manufactura-vehiculos.module.ts
@@ -1,15 +1,16 @@
-import { AnexarDocumentosComponent, BtnContinuarComponent, SolicitanteComponent } from '@ng-mf/data-access-user';
+import { AnexarDocumentosComponent, BtnContinuarComponent, FirmaElectronicaComponent, SolicitanteComponent } from '@ng-mf/data-access-user';
 import { CommonModule } from '@angular/common';
 import { DatosDelInmuebleComponent } from '../components/datos-del-inmueble/datos-del-inmueble.component';
 import { DepositoFiscalManufacturaVehiculosRoutingModule } from './deposito-fiscal-manufactura-vehiculos-routing.module';
 import { NgModule } from '@angular/core';
+import { PasoCuatroComponent } from '../pages/paso-cuatro/paso-cuatro.component';
 import { PasoTresComponent } from '../pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from '../pages/paso-uno/paso-uno.component';
 import { SolicitudPageComponent } from '../pages/solicitud-page/solicitud-page.component';
 import { WizardComponent } from '@libs/shared/data-access-user/src';
 
 @NgModule({
-  declarations: [SolicitudPageComponent,PasoUnoComponent,PasoTresComponent],
+  declarations: [SolicitudPageComponent,PasoUnoComponent,PasoTresComponent,PasoCuatroComponent],
   imports: [
     CommonModule,
     DepositoFiscalManufacturaVehiculosRoutingModule,
@@ -17,7 +18,8 @@ import { WizardComponent } from '@libs/shared/data-access-user/src';
     SolicitanteComponent,
     DatosDelInmuebleComponent,
     AnexarDocumentosComponent,
-    BtnContinuarComponent
+    BtnContinuarComponent,
+    FirmaElectronicaComponent
   ]
 })
 export class DepositoFiscalManufacturaVehiculosModule { }
---
+++ apps/aga/src/app/application/tramites/104/pages/paso-cuatro/paso-cuatro.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paso-cuatro',
+  templateUrl: './paso-cuatro.component.html',
+})
+export class PasoCuatroComponent {}
