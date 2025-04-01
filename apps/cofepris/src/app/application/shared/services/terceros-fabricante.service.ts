ce7b160ab1864c067a53cd239e8cfa4570b3b9da
atripathi860 <amit.tripathi@ilink-systems.com>
2025-04-01 14:01:37 +0530

    Add missing documentation to all the implemented methods

--- apps/cofepris/src/app/application/shared/services/terceros-fabricante.service.ts
+++ apps/cofepris/src/app/application/shared/services/terceros-fabricante.service.ts
@@ -11,27 +11,46 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PermisoModel } from '../models/datos-domicilio-legal.model';
 
+/**
+ * Servicio para obtener datos de terceros relacionados y permisos desde archivos JSON locales.
+ */
 @Injectable({
   providedIn: 'root',
 })
 export class TercerosFabricanteService {
   constructor(public http: HttpClient) {}
 
+  /**
+   * Obtiene los datos de un archivo JSON local.
+   *
+   * @returns Observable que emite un objeto RespuestaCatalogos.
+   */
   getObtenerEstadoList() {
     return this.http.get<RespuestaCatalogos>(
       'assets/json/260501/seleccion.json'
     );
   }
 
+  /**
+   * Obtiene los datos de un archivo JSON local.
+   *
+   * @returns Observable que emite un objeto RespuestaCatalogos.
+   */
   getObtenerTablaDatos() {
     return this.http.get<RespuestaTabla>('assets/json/260501/tablaDatos.json');
   }
 
+  /**
+   * Obtiene los datos de un archivo JSON local.
+   *
+   * @returns Observable que emite un objeto RespuestaCatalogos.
+   */
   getObtenerMercanciasDatos() {
     return this.http.get<MercanciasTabla>(
       'assets/json/260501/mercanciasDatos.json'
     );
   }
+
   /**
    * Obtiene los datos de terceros relacionados desde un archivo JSON local.
    *
@@ -43,6 +62,11 @@ export class TercerosFabricanteService {
     );
   }
 
+  /**
+   * Obtiene los datos de un archivo JSON local.
+   *
+   * @returns Observable que emite un arreglo de objetos PermisoModel.
+   */
   getTable(): Observable<PermisoModel[]> {
     return this.http.get<PermisoModel[]>('assets/json/260501/terceros.json');
   }
