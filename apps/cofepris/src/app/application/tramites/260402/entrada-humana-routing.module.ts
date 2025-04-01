85413d5d120e3c754d2c7cd4320fba7cf610d4e9
Vinothini P <vinothini.p@ilink-systems.com>
2025-04-01 10:05:34 +0530

    lint fix

--- apps/cofepris/src/app/application/tramites/260402/entrada-humana-routing.module.ts
+++ apps/cofepris/src/app/application/tramites/260402/entrada-humana-routing.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EntradaHumanaComponent } from './pages/entrada-humana/entrada-humana.component';
 
-const routes: Routes = [
+const ROUTES:Routes = [
   {
     path:'solictud',
     component:EntradaHumanaComponent
@@ -11,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(ROUTES)],
   exports: [RouterModule]
 })
 export class EntradaHumanaRoutingModule { }
