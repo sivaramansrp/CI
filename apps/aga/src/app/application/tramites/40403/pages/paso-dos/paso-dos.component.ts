78fe9e4c90e9a047e75352187ae18b21d097a9e9
parmeshwars2025 <parmeshwar.vasantrao@ilink-systems.com>
2025-04-02 13:25:38 +0530

    Worked on a Pr review comments

--- apps/aga/src/app/application/tramites/40403/pages/paso-dos/paso-dos.component.ts
+++ apps/aga/src/app/application/tramites/40403/pages/paso-dos/paso-dos.component.ts
@@ -1,20 +1,50 @@
-/* eslint-disable no-empty-function */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CATALOGOS_ID } from '@ng-mf/data-access-user';
 import { Catalogo } from '@ng-mf/data-access-user';
 import { CatalogosService } from '@ng-mf/data-access-user';
+import { Subject } from 'rxjs';
 import { TEXTOS } from '@ng-mf/data-access-user';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-paso-dos',
   templateUrl: './paso-dos.component.html',
   styleUrl: './paso-dos.component.scss',
 })
-export class PasoDosComponent implements OnInit {
+export class PasoDosComponent implements OnInit, OnDestroy {
+  /**
+   * Constante que contiene los textos utilizados en el componente.
+   */
   TEXTOS = TEXTOS;
+
+  /**
+   * Lista de tipos de documentos disponibles para el trámite.
+   */
   tiposDocumentos: Catalogo[] = [];
+
+  /**
+   * Clase CSS utilizada para mostrar un mensaje de alerta informativa.
+   */
   infoAlert = 'alert-info';
+
+  /**
+   * Catálogo de documentos disponibles para el trámite.
+   */
   catalogoDocumentos: Catalogo[] = [];
+
+  /**
+   * Lista de documentos seleccionados por el usuario.
+   */
   documentosSeleccionados: Catalogo[] = [];
+
+  /**
+   * Notificador para gestionar la destrucción de suscripciones activas y evitar fugas de memoria.
+   */
+  private destroyNotifier$ = new Subject<void>();
+
+  /**
+   * Constructor del componente.
+   * @param catalogosServices - Servicio para obtener los catálogos necesarios para el trámite.
+   */
   constructor(private catalogosServices: CatalogosService) {}
   /**
    * 
@@ -22,17 +52,6 @@ Gancho del ciclo de vida angular que se llama después de que se inicializan las
    */
   ngOnInit(): void {
     this.getTiposDocumentos();
-    this.documentosSeleccionados = [
-      {
-        id: 1,
-        descripcion: 'Documentos que ampare el valor de la mercancía',
-      },
-      {
-        id: 2,
-        descripcion:
-          'Documentos del medio de transporte (Guías, BL o carta porte según corresponda)',
-      },
-    ];
   }
 
   /**
@@ -41,6 +60,7 @@ Gancho del ciclo de vida angular que se llama después de que se inicializan las
   getTiposDocumentos(): void {
     this.catalogosServices
       .getCatalogo(CATALOGOS_ID.CAT_TIPO_DOCUMENTO)
+      .pipe(takeUntil(this.destroyNotifier$)) 
       .subscribe({
         next: (resp): void => {
           if (resp.length > 0) {
@@ -48,5 +68,13 @@ Gancho del ciclo de vida angular que se llama después de que se inicializan las
           }
         },
       });
+  }
+  /**
+   * Gancho del ciclo de vida angular que se llama antes de que se destruya el componente.
+   * Se utiliza para limpiar las suscripciones y evitar fugas de memoria.
+   */
+  ngOnDestroy(): void {
+    this.destroyNotifier$.next();
+    this.destroyNotifier$.complete();
   }
 }
