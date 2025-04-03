d3305da4542761875d769eecb403ce83d409f8e0
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-04-03 16:19:19 +0530

    Clear eslint

--- apps/agace/src/app/application/tramites/32501/services/merc-desm-sin-mon.service.ts
+++ apps/agace/src/app/application/tramites/32501/services/merc-desm-sin-mon.service.ts
@@ -1,40 +1,51 @@
-import {Observable, catchError, throwError } from 'rxjs';
-import { AvisoCatalogo, OperacionDeImportacion, RequisitosObligatorios } from '../models/aviso-catalogo.model';
+import { AvisoCatalogo } from '../models/aviso-catalogo.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OperacionDeImportacion } from '../models/aviso-catalogo.model';
+import { RequisitosObligatorios } from '../models/aviso-catalogo.model';
+import { catchError } from 'rxjs';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class MercDesmSinMonService {
-
-
   constructor(private http: HttpClient) {
     // El constructor está intencionalmente vacío para la inyección de dependencias
   }
 
   obtenerAvisoDelCatalogo(): Observable<AvisoCatalogo> {
-    return this.http.get<AvisoCatalogo>('assets/json/32501/aviso-catalogo.json').pipe(
-      catchError((error) => {
-        return throwError(() => error);
-      })
-    );
+    return this.http
+      .get<AvisoCatalogo>('assets/json/32501/aviso-catalogo.json')
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
   }
 
   obtenerOperacionDeImportacion(): Observable<OperacionDeImportacion[]> {
-    return this.http.get<OperacionDeImportacion[]>('assets/json/32501/operacion-de-importacion.json').pipe(
-      catchError((error) => {
-        return throwError(() => error);
-      })
-    );
+    return this.http
+      .get<OperacionDeImportacion[]>(
+        'assets/json/32501/operacion-de-importacion.json'
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
   }
 
   obtenerDatosAgregarNuevo(): Observable<RequisitosObligatorios[]> {
-    return this.http.get<RequisitosObligatorios[]>('assets/json/32501/datos-agregar-nuevo.json').pipe(
-      catchError((error) => {
-        return throwError(() => error);
-      })
-    );
-  } 
+    return this.http
+      .get<RequisitosObligatorios[]>(
+        'assets/json/32501/datos-agregar-nuevo.json'
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(() => error);
+        })
+      );
+  }
 }
