1a15f9f053d38b13a2dc7495b61d4368e1cc5e03
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-04 12:55:10 +0530

    modules added

---
+++ apps/semarnat/src/app/application/tramites/231003/aviso-de-reciclaje.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AvisoDeReciclajeRoutingModule } from './aviso-de-reciclaje-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AvisoDeReciclajeRoutingModule
+  ]
+})
+export class AvisoDeReciclajeModule { }
