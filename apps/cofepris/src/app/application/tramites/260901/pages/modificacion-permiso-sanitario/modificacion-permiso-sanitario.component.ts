b75fd2225a45b5ac3441775106ef01588cb8d9ef
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-04-04 15:25:25 +0530

    added enum file
    added enum file

--- apps/cofepris/src/app/application/tramites/260901/pages/modificacion-permiso-sanitario/modificacion-permiso-sanitario.component.ts
+++ apps/cofepris/src/app/application/tramites/260901/pages/modificacion-permiso-sanitario/modificacion-permiso-sanitario.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { DatosPasos, ListaPasosWizard, WizardComponent } from '@libs/shared/data-access-user/src';
-import { MODIFICACION_PERMISO_ENUM } from '../../constantes/modificacion-permiso.enum';
+import { MODIFICACION_PERMISO_DATA, MODIFICACION_PERMISO_ENUM } from '../../constantes/modificacion-permiso.enum';
 interface AccionBoton {
   accion: string;
   valor: number;
@@ -11,6 +11,11 @@ interface AccionBoton {
 })
 
 export class ModificacionPermisoSanitarioComponent {
+ /*
+ *  * Se define el mensaje de datos para la modificación del permiso sanitario.
+ *  * Este mensaje se utiliza para mostrar información relevante al usuario.
+ */
+  msgData =MODIFICACION_PERMISO_DATA;
   @ViewChild(WizardComponent) wizardComponent!: WizardComponent;
   /**
    * 
