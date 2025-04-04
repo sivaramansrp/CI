55092b5d5fe7f9699bd2f18d5770ef5747472550
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-04 14:56:00 +0530

    ES lint

--- apps/semarnat/src/app/application/tramites/231003/aviso-de-reciclaje.module.ts
+++ apps/semarnat/src/app/application/tramites/231003/aviso-de-reciclaje.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { AvisoDeReciclajeRoutingModule } from './aviso-de-reciclaje-routing.module';
-import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 
 @NgModule({
   declarations: [PasoUnoComponent, PasoDosComponent],
