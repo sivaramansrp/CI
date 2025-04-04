a17d324255708764285d6a2959bbebd715d20e46
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-04-04 17:07:08 -0600

    Cambia componente selectCatalogComponent por CatalogoSelectComponent

--- apps/agriculture/src/app/application/tramites/220401/components/agregar-destinatoria/agregar-destinatoria.component.ts
+++ apps/agriculture/src/app/application/tramites/220401/components/agregar-destinatoria/agregar-destinatoria.component.ts
@@ -1,27 +1,25 @@
 /* eslint-disable sort-imports */
 import { Component, OnInit } from '@angular/core';
-import { TituloComponent } from '@ng-mf/data-access-user';
-import { CatalogosSelect } from '@ng-mf/data-access-user';
+import { CatalogoSelectComponent, TituloComponent } from '@ng-mf/data-access-user';
 import { Catalogo } from '@ng-mf/data-access-user';
-import { SelectCatalogosComponent } from '@ng-mf/data-access-user';
 
 @Component({
   selector: 'app-agregar-destinatoria',
   templateUrl: './agregar-destinatoria.component.html',
   styleUrl: './agregar-destinatoria.component.scss',
   standalone: true,
-  imports: [TituloComponent,SelectCatalogosComponent],
+  imports: [TituloComponent, CatalogoSelectComponent],
 })
 export class AgregarDestinatoriaComponent implements OnInit {
 
-  public pais!: CatalogosSelect;
+  public pais!: Catalogo[];
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   public fisica: boolean = true;
   // eslint-disable-next-line @typescript-eslint/no-inferrable-types
   public moral: boolean = false;
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function, no-empty-function
-  constructor() {}
+  constructor() { }
 
   ngOnInit(): void {
     this.getPais();
@@ -33,25 +31,20 @@ export class AgregarDestinatoriaComponent implements OnInit {
 
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   public getPais() {
-    this.pais = {
-      labelNombre: 'País',
-      required: true,
-      primerOpcion: 'Selecciona un valor',
-      catalogos: [
-        {
-          id: 1,
-          descripcion: 'Opción 1',
-        },
-        {
-          id: 2,
-          descripcion: 'Opción 1',
-        }
-      ],
-    };
+    this.pais = [
+      {
+        id: 1,
+        descripcion: 'Opción 1',
+      },
+      {
+        id: 2,
+        descripcion: 'Opción 1',
+      }
+    ]
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function, no-empty-function, @typescript-eslint/no-unused-vars, class-methods-use-this
-   docSeleccionado(e: Catalogo): void {}
+  docSeleccionado(): void { }
 
   /**
    * 
@@ -59,8 +52,8 @@ export class AgregarDestinatoriaComponent implements OnInit {
    * @description inputChecked se utiliza para verificar si el checkbox está seleccionado
    */
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  public inputChecked(checkBoxName:string) {
-    if(checkBoxName === 'fisica') {
+  public inputChecked(checkBoxName: string) {
+    if (checkBoxName === 'fisica') {
       this.fisica = true;
       this.moral = false;
     } else {
