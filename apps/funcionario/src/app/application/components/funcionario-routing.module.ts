d95c37bdc4294ac2de3bec811465ea03bc05fc33
Panfilo Santiago Lorenzo <psantiagol@ultrasist.com.mx>
2025-04-04 11:26:10 -0600

    correcion de Funcionario/ funcionario-routing

--- apps/funcionario/src/app/application/components/funcionario-routing.module.ts
+++ apps/funcionario/src/app/application/components/funcionario-routing.module.ts
@@ -34,7 +34,11 @@ const routes: Routes = [
   {
     path: 'autorizar-dictamen',
     component: AutorizarDictamenComponent,
-  },
+    },
+    {
+        path: 'consulta',
+        component: ConsultaComponent,
+    },
 
 ];
 
