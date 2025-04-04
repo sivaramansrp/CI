55db9a00351b1d3552daf0f09cc379343d99dd34
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-04 14:22:15 +0530

    paso dos added

--- apps/semarnat/src/app/application/tramites/231003/aviso-de-reciclaje.module.ts
+++ apps/semarnat/src/app/application/tramites/231003/aviso-de-reciclaje.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AvisoDeReciclajeRoutingModule } from './aviso-de-reciclaje-routing.module';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 
 @NgModule({
-  declarations: [PasoUnoComponent],
+  declarations: [PasoUnoComponent, PasoDosComponent],
   imports: [CommonModule, AvisoDeReciclajeRoutingModule],
 })
 export class AvisoDeReciclajeModule {}
