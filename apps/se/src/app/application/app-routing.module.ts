1304dbd9d953e5d257fd1244c6905cc245f32c17
balamurugan-ilink <balamurugan.govinthan@ilink-systems.com>
2025-04-04 21:32:14 +0530

    Conflict Resolved

--- apps/se/src/app/application/app-routing.module.ts
+++ apps/se/src/app/application/app-routing.module.ts
@@ -296,6 +296,13 @@ const ROUTES: Routes = [
         (m) => m.ReporteAnualModule
       ),
   },
+  {
+    path: 'validar-certificado',
+    loadChildren: () =>
+      import('./tramites/110211/validar-certificado.module').then(
+        (m) => m.ValidarCertificadoModule
+      ),
+  }
 ];
 
 @NgModule({
