225b900d2d03818fc55dbab3b8ddb617f8179207
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-07 15:31:59 +0530

    module route added

--- apps/semarnat/src/app/application/app-routing.module.ts
+++ apps/semarnat/src/app/application/app-routing.module.ts
@@ -22,6 +22,11 @@ const ROUTES: Routes = [
     path: 'tramites-disponibles',
     loadChildren: () =>
       import('./tramites/230101/tramites-disponsibles.module').then((m) => m.TramitesDisponiblesModule),
+  },
+  {
+    path: 'aviso-de-reciclaje',
+    loadChildren: () =>
+      import('./tramites/231003/aviso-de-reciclaje.module').then((m) => m.AvisoDeReciclajeModule),
   }
 ];
 
