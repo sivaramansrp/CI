fe3a62ab685aab9fc60a0f58704f4b5d1482a851
Raghul C <raghul.c@purpleslate.in>
2025-04-07 15:41:02 +0530

    Add SemarnatDesistimiento module and routing configuration

---
+++ apps/semarnat/src/app/application/tramites/230301/semarnat-desistimiento/semarnat-desistimiento.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SemarnatDesistimientoRoutingModule } from './semarnat-desistimiento-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SemarnatDesistimientoRoutingModule
+  ]
+})
+export class SemarnatDesistimientoModule { }
