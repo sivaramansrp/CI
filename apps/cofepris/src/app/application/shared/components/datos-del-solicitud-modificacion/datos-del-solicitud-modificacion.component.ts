cf27551d0338cb2a89140d6ba0e33beaf5dcc549
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-04-07 11:57:11 +0530

    Added code
    Added code

--- apps/cofepris/src/app/application/shared/components/datos-del-solicitud-modificacion/datos-del-solicitud-modificacion.component.ts
+++ apps/cofepris/src/app/application/shared/components/datos-del-solicitud-modificacion/datos-del-solicitud-modificacion.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CatalogoSelectComponent, InputRadioComponent, TablaDinamicaComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 
 @Component({
   selector: 'app-datos-del-solicitud-modificacion',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule, TituloComponent,InputRadioComponent,CatalogoSelectComponent,TablaDinamicaComponent],
+  // imports: [CommonModule, ReactiveFormsModule, FormsModule, TituloComponent],  
   templateUrl: './datos-del-solicitud-modificacion.component.html',
-  styleUrl: './datos-del-solicitud-modificacion.component.css',
+  styleUrl: './datos-del-solicitud-modificacion.component.scss',
 })
 export class DatosDelSolicitudModificacionComponent {}
