e757882bcf05752d3a9fbe43120fd5db695513f8
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-04-07 19:44:11 +0530

    fix: routing file created

---
+++ apps/cofepris/src/app/application/tramites/261401/solicitud-modificacion-permiso-salida-territorio-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
+
+
+const ROUTES: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forChild(ROUTES)],
+  exports: [RouterModule]
+})
+export class SolicitudModificacionPermisoSalidaTerritorioRoutingModule { }
---
+++ apps/cofepris/src/app/application/tramites/261401/solicitud-modificacion-permiso-salida-territorio.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SolicitudModificacionPermisoSalidaTerritorioRoutingModule } from './solicitud-modificacion-permiso-salida-territorio-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SolicitudModificacionPermisoSalidaTerritorioRoutingModule
+  ]
+})
+export class SolicitudModificacionPermisoSalidaTerritorioModule { }
