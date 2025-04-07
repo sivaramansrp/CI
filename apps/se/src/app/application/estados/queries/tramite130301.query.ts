a9d3a47554a792ea53494fb82e8ce110ebb6ddae
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-04-07 17:45:30 +0530

    Refactor comments to Spanish and clean up HTML structure in seleccion-tramite component

--- apps/se/src/app/application/estados/queries/tramite130301.query.ts
+++ apps/se/src/app/application/estados/queries/tramite130301.query.ts
@@ -10,8 +10,8 @@ import { Solicitud130301State, Tramite130301Store } from '../tramites/tramite130
 export class Tramite130301Query extends Query<Solicitud130301State> {
 
   /**
-   * Observable to select the complete state of the solicitud.
-   * @returns {Observable<Solicitud130301State>} The complete state of the solicitud.
+   * Observable para seleccionar el estado completo de la solicitud.
+   * @returns {Observable<Solicitud130301State>} El estado completo de la solicitud.
    */
   selectSolicitud$ = this.select((state) => {
     return state;
@@ -19,7 +19,7 @@ export class Tramite130301Query extends Query<Solicitud130301State> {
 
   /**
    * Constructor para Tramite130301Store.
-   * @param {Tramite130301Store} store - The store that holds the state of Solicitud130301.
+   * @param {Tramite130301Store} store - La tienda que contiene el estado de Solicitud130301.
    */
   constructor(
     protected override store: Tramite130301Store) {
