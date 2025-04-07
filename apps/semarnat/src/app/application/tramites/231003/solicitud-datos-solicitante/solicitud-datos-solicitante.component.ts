d08890324ddafe305df52934339e546c5c8e51e4
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-07 15:35:56 +0530

    reusable component added

--- apps/semarnat/src/app/application/tramites/231003/solicitud-datos-solicitante/solicitud-datos-solicitante.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/solicitud-datos-solicitante/solicitud-datos-solicitante.component.ts
@@ -4,4 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-solicitud-datos-solicitante',
   templateUrl: './solicitud-datos-solicitante.component.html',
 })
-export class SolicitudDatosSolicitanteComponent {}
+export class SolicitudDatosSolicitanteComponent {
+     /**
+   * Índice de la pestaña seleccionada.
+   * @type {number}
+   */
+     indice: number = 1;
+
+     /**
+      * Selecciona una pestaña específica.
+      * 
+      * @param {number} i - El índice de la pestaña a seleccionar.
+      */
+     seleccionaTab(i: number): void {
+       this.indice = i;
+     }
+}
