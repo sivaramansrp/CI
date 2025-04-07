f0cf98a946020d2ebd1eba52be1bf7a45a3a6844
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-07 16:57:56 +0530

    Refactor BusquedaFolioComponent tests by removing unused imports and simplifying mock services

--- apps/se/src/app/application/tramites/140104/pages/busqueda-folio/busqueda-folio.component.spec.ts
+++ apps/se/src/app/application/tramites/140104/pages/busqueda-folio/busqueda-folio.component.spec.ts
@@ -1,52 +1,32 @@
-// @ts-nocheck
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { Observable, of as observableOf, throwError } from 'rxjs';
 
-import { Component } from '@angular/core';
+import {  ComponentFixture, TestBed } from '@angular/core/testing';
+import { Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { BusquedaFolioComponent } from './busqueda-folio.component';
 import { ServicioDeMensajesService } from '../../services/servicio-de-mensajes.service';
 import { FormBuilder } from '@angular/forms';
 
 @Injectable()
-class MockServicioDeMensajesService { }
-
-@Directive({ selector: '[myCustom]' })
-class MyCustomDirective {
-  @Input() myCustom;
+class MockServicioDeMensajesService {
+  establecerDatosDePermiso(): void {
+    // Mock implementation of establecerDatosDePermiso
+  }
 }
 
-@Pipe({ name: 'translate' })
-class TranslatePipe implements PipeTransform {
-  transform(value) { return value; }
-}
 
-@Pipe({ name: 'phoneNumber' })
-class PhoneNumberPipe implements PipeTransform {
-  transform(value) { return value; }
-}
-
-@Pipe({ name: 'safeHtml' })
-class SafeHtmlPipe implements PipeTransform {
-  transform(value) { return value; }
-}
 
 describe('BusquedaFolioComponent', () => {
-  let fixture;
-  let component;
+  let fixture: ComponentFixture<BusquedaFolioComponent>;
+  let component: BusquedaFolioComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule, ReactiveFormsModule],
+      imports: [ FormsModule, ReactiveFormsModule ],
       declarations: [
         BusquedaFolioComponent,
-        TranslatePipe, PhoneNumberPipe, SafeHtmlPipe,
-        MyCustomDirective
       ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
       providers: [
         { provide: ServicioDeMensajesService, useClass: MockServicioDeMensajesService },
         FormBuilder
@@ -54,20 +34,18 @@ describe('BusquedaFolioComponent', () => {
     }).overrideComponent(BusquedaFolioComponent, {
 
     }).compileComponents();
+    
     fixture = TestBed.createComponent(BusquedaFolioComponent);
     component = fixture.debugElement.componentInstance;
   });
 
   afterEach(() => {
-    component.ngOnDestroy = function () { };
+    component.ngOnDestroy = function() {};
     fixture.destroy();
   });
 
   it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
   });
-
-
-
 
 });
