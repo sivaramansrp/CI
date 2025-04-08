c089e9d4389a143a3067bc87c223649b03ee0d0d
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-08 11:46:03 +0530

    form added

--- apps/semarnat/src/app/application/tramites/231003/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/components/datos-solicitud/datos-solicitud.component.ts
@@ -1,11 +1,45 @@
+import { Catalogo, CatalogoSelectComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-datos-solicitud',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+        CatalogoSelectComponent,
+        TituloComponent,
+        ReactiveFormsModule,],
   templateUrl: './datos-solicitud.component.html',
   styleUrl: './datos-solicitud.component.css',
 })
-export class DatosSolicitudComponent {}
+export class DatosSolicitudComponent implements OnInit{
+
+  solicitudForm!: FormGroup;
+
+    aduanas!: Catalogo[];
+  
+
+
+  constructor(public fb: FormBuilder) {
+    // Constructor logic if needed
+  }
+
+  // Add any methods or properties needed for the component here
+
+  ngOnInit():void{
+    this.solicitudForm = this.fb.group({
+      datosdelForm: this.fb.group({
+        numeroRegistroAmbiental: ['', Validators.required],
+        descripcionGenerica1: ['', Validators.required],
+        numeroProgramaImmex: ['', Validators.required],
+      }),
+    });
+  }
+
+  isInvalid(id: string): boolean | undefined {
+    const CONTROL = this.solicitudForm.get('datosdelForm')?.get(id);
+    return CONTROL?.invalid && CONTROL?.touched;
+  }
+
+}
