d484c0435698abd9e8e5e992d6fb5d8372469ae8
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-08 15:10:28 +0530

    stand alone component added

--- apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
@@ -1,11 +1,17 @@
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TituloComponent } from '@libs/shared/data-access-user/src';
 
 @Component({
   selector: 'app-datos-residuos-peligrosos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+      TituloComponent,
+      ReactiveFormsModule],
   templateUrl: './datos-residuos-peligrosos.component.html',
   styleUrl: './datos-residuos-peligrosos.component.css',
 })
-export class DatosResiduosPeligrososComponent {}
+export class DatosResiduosPeligrososComponent {
+  datosResiduospPeligrosos!: FormGroup;
+}
