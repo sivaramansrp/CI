353ff4eaaf0db5e1e91aff720fd60483a4930250
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-08 15:59:58 +0530

    radio optios addd

--- apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
@@ -1,17 +1,22 @@
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { InputRadioComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { RadioOpcion,SolicitudJson } from '@libs/shared/data-access-user/src/core/models/231003/solicitud.model'
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TituloComponent } from '@libs/shared/data-access-user/src';
+import rawData from '@libs/shared/theme/assets/json/231003/solicitud.json';
+const RADIO_OPCIONES = rawData as SolicitudJson;
 
 @Component({
   selector: 'app-datos-residuos-peligrosos',
   standalone: true,
   imports: [CommonModule,
       TituloComponent,
-      ReactiveFormsModule],
+      ReactiveFormsModule,InputRadioComponent],
   templateUrl: './datos-residuos-peligrosos.component.html',
   styleUrl: './datos-residuos-peligrosos.component.css',
 })
 export class DatosResiduosPeligrososComponent {
   datosResiduospPeligrosos!: FormGroup;
+
+  radioOptions: RadioOpcion[] = RADIO_OPCIONES.radioOptions;
 }
---
+++ libs/shared/data-access-user/src/core/models/231003/solicitud.model.ts
@@ -0,0 +1,31 @@
+export interface RadioOpcion {
+    /**
+     * Etiqueta o texto descriptivo que se muestra al usuario para esta opción.
+     * Este texto es lo que el usuario verá junto al botón de opción (radio button).
+     * Por ejemplo, "Hombre" o "Mujer" en un formulario de selección de género.
+     */
+    label: string;
+
+    /**
+     * Valor interno asociado a esta opción, que generalmente se utiliza para identificarla
+     * de manera única en el sistema. Este valor es enviado al backend cuando el formulario
+     * es enviado. Por ejemplo, "M" para "Masculino" o "F" para "Femenino".
+     */
+    value: string;
+}
+
+interface ImmexItem {
+    id: number;
+    descripcion: string;
+}
+
+interface Tabla {
+    encabezadoDeTabla: string[];
+    cuerpoTabla: { tbodyData: string[] }[];
+}
+
+export interface SolicitudJson {
+    Immex: ImmexItem[];
+    table: Tabla[];
+    radioOptions: RadioOpcion[];
+}
