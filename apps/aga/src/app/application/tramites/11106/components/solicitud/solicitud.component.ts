f9afddb89e1f94e16f70abc8e907e42e3f1565cd
shekhar-kalluri-ilink <shekhar.kalluri@ilink-systems.com>
2025-04-08 10:50:29 +0530

    "cancelacionDonaciones"

--- apps/aga/src/app/application/tramites/11106/components/solicitud/solicitud.component.ts
+++ apps/aga/src/app/application/tramites/11106/components/solicitud/solicitud.component.ts
@@ -31,14 +31,14 @@ export class SolicitudComponent implements OnInit {
 
   initializeSolicitudForm(): void {
     this.solicitudForm = this.formBuilder.group({
-      retiradaDonaciones: this.formBuilder.group({
+      cancelacionDonaciones: this.formBuilder.group({
         manifesto: [{ value: '', disabled: true }],
       }),
     });
   }
 
   get cancelacionDonaciones(): FormGroup {
-    return this.solicitudForm.get('retiradaDonaciones') as FormGroup;
+    return this.solicitudForm.get('cancelacionDonaciones') as FormGroup;
   }
 
   validarDestinatarioFormulario(): void {
