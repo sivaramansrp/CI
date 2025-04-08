6b8fec0f4d3412e8d629886d250bd7536d2375ca
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-08 12:59:44 +0530

    table json added

--- apps/semarnat/src/app/application/tramites/231003/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/components/datos-solicitud/datos-solicitud.component.ts
@@ -1,4 +1,4 @@
-import { Catalogo, CatalogoSelectComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { Catalogo, CatalogoSelectComponent, TableComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -8,19 +8,21 @@ import SolicitudeJson from '@libs/shared/theme/assets/json/231003/solicitud.json
   selector: 'app-datos-solicitud',
   standalone: true,
   imports: [CommonModule,
-        CatalogoSelectComponent,
-        TituloComponent,
-        ReactiveFormsModule],
+    CatalogoSelectComponent,
+    TituloComponent,
+    ReactiveFormsModule, TableComponent],
   templateUrl: './datos-solicitud.component.html',
   styleUrl: './datos-solicitud.component.css',
 })
-export class DatosSolicitudComponent implements OnInit{
+export class DatosSolicitudComponent implements OnInit {
 
   solicitudForm!: FormGroup;
 
-   aduanas!: Catalogo[];
-  
+  aduanas!: Catalogo[];
 
+  public establecimientoHeaderData: string[] = [];
+
+  public establecimientoBodyData: unknown = [];
 
   constructor(public fb: FormBuilder) {
     // Constructor logic if needed
@@ -28,7 +30,7 @@ export class DatosSolicitudComponent implements OnInit{
 
   // Add any methods or properties needed for the component here
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.solicitudForm = this.fb.group({
       datosdelForm: this.fb.group({
         numeroRegistroAmbiental: ['', Validators.required],
@@ -37,6 +39,9 @@ export class DatosSolicitudComponent implements OnInit{
       }),
     });
     this.aduanas = SolicitudeJson.Immex;
+    this.establecimientoHeaderData = SolicitudeJson.table[0]?.encabezadoDeTabla || [];
+    this.establecimientoBodyData = SolicitudeJson.table[0]?.cuerpoTabla || [];
+
   }
 
   isInvalid(id: string): boolean | undefined {
