a89ca3371728844ec8c3078fe074897686dde5b7
NamrataSaka <namratanikhil.saka@ilink-systems.com>
2025-04-09 19:53:47 +0530

    fix: created components for tabs

---
+++ apps/cofepris/src/app/application/tramites/261401/components/datos-de-la-solicitud/datos-de-la-solicitud.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-de-la-solicitud',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './datos-de-la-solicitud.component.html',
+  styleUrl: './datos-de-la-solicitud.component.scss',
+})
+export class DatosDeLaSolicitudComponent {}
--- apps/cofepris/src/app/application/tramites/261401/solicitud-modificacion-permiso-salida-territorio.module.ts
+++ apps/cofepris/src/app/application/tramites/261401/solicitud-modificacion-permiso-salida-territorio.module.ts
@@ -1,10 +1,15 @@
+import { BtnContinuarComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
+import { DatosDeLaSolicitudComponent } from './components/datos-de-la-solicitud/datos-de-la-solicitud.component';
+import { DatosParaMovilizacionNacionalComponent } from './components/datos-para-movilizaion-nacional/datos-para-movilizacion-nacional.component';
 import { NgModule } from '@angular/core';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
 import { SolicitudModificacionPermisoSalidaTerritorioComponent } from './pages/solicitud-modificacion-permiso-salida-territorio/solicitud-modificacion-permiso-salida-territorio.component';
 import { SolicitudModificacionPermisoSalidaTerritorioRoutingModule } from './solicitud-modificacion-permiso-salida-territorio-routing.module';
-import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
-import { BtnContinuarComponent, SolicitanteComponent, WizardComponent } from '@libs/shared/data-access-user/src';
-import { ReactiveFormsModule } from '@angular/forms';
+import { WizardComponent } from '@libs/shared/data-access-user/src';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +25,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     // PasoDosComponent,
     // PasoTresComponent,
     SolicitanteComponent,
+    DatosDeLaSolicitudComponent,
+    DatosParaMovilizacionNacionalComponent
   ],
 })
 export class SolicitudModificacionPermisoSalidaTerritorioModule {}
