a6ffe6f748fe3061cb13fd5d111a884c2cf2b3b2
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-09 15:10:30 +0530

    formatting

--- apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
+++ apps/semarnat/src/app/application/tramites/231003/components/datos-residuos-peligrosos/datos-residuos-peligrosos.component.ts
@@ -1,7 +1,7 @@
 import { Catalogo, CatalogoSelectComponent, InputRadioComponent, TableComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import {RadioOpcion,SolicitudJson} from '@libs/shared/data-access-user/src/core/models/231003/solicitud.model'
+import { RadioOpcion, SolicitudJson } from '@libs/shared/data-access-user/src/core/models/231003/solicitud.model'
 import { CommonModule } from '@angular/common';
 import rawData from '@libs/shared/theme/assets/json/231003/solicitud.json';
 const RADIO_OPCIONES = rawData as SolicitudJson;
@@ -10,8 +10,8 @@ const RADIO_OPCIONES = rawData as SolicitudJson;
   selector: 'app-datos-residuos-peligrosos',
   standalone: true,
   imports: [CommonModule,
-      TituloComponent,
-      ReactiveFormsModule,InputRadioComponent,CatalogoSelectComponent,TableComponent],
+    TituloComponent,
+    ReactiveFormsModule, InputRadioComponent, CatalogoSelectComponent, TableComponent],
   templateUrl: './datos-residuos-peligrosos.component.html',
   styleUrl: './datos-residuos-peligrosos.component.css',
 })
@@ -43,8 +43,8 @@ export class DatosResiduosPeligrososComponent implements OnInit {
 
   ngOnInit(): void {
     this.nombre = RADIO_OPCIONES.nombre;
-    this.establecimientoHeaderData= RADIO_OPCIONES.PrimasRelacionadas[0]?.encabezadoDeTabla || [];
-    this.establecimientoBodyData= RADIO_OPCIONES.PrimasRelacionadas[0]?.cuerpoTabla || [];
+    this.establecimientoHeaderData = RADIO_OPCIONES.PrimasRelacionadas[0]?.encabezadoDeTabla || [];
+    this.establecimientoBodyData = RADIO_OPCIONES.PrimasRelacionadas[0]?.cuerpoTabla || [];
     this.fraccion = RADIO_OPCIONES.arancelaria;
     this.nico = RADIO_OPCIONES.nico;
     this.unidad = RADIO_OPCIONES.unidad;
@@ -56,6 +56,7 @@ export class DatosResiduosPeligrososComponent implements OnInit {
     this.tipoContenedor = RADIO_OPCIONES.tipoContenedor;
     this.inicializarFormulario();
     this.crearFormularioResiduo();
+    this.controlarHabilitacionDeCatalogos();
   }
 
   private inicializarFormulario(): void {
@@ -73,20 +74,49 @@ export class DatosResiduosPeligrososComponent implements OnInit {
     this.formularioResiduo = new FormGroup({
       fraccionArancelaria: new FormControl('', Validators.required),
       nico: new FormControl('', Validators.required),
-      acotacion: new FormControl({value: '', disabled: true }),
+      acotacion: new FormControl({ value: '', disabled: true }, Validators.required),
       residuoPeligroso: new FormControl('', Validators.required),
       cantidad: new FormControl('', Validators.required),
       cantidadLetra: new FormControl({ value: '', disabled: true }),
       unidadMedida: new FormControl('', Validators.required),
       clasificacion: new FormControl('', Validators.required),
-      claveResiduo: new FormControl('', Validators.required),
-      nombre: new FormControl('', Validators.required),
-      descripcion: new FormControl('', Validators.required),
+      claveResiduo: new FormControl({ value: '', disabled: true }, Validators.required),
+      nombre: new FormControl({ value: '', disabled: true }, Validators.required),
+      descripcion: new FormControl({ value: '', disabled: true }, Validators.required),
       creti: new FormControl('', Validators.required),
       estadoFisico: new FormControl('', Validators.required),
       tipoContenedor: new FormControl('', Validators.required),
       capacidad: new FormControl('', Validators.required)
     });
   }
-  
+
+  private controlarHabilitacionDeCatalogos(): void {
+    const OPCION_CLAVE = 'Clave de residuo';
+    const OPCION_NOMBRE = 'Nombre';
+    const OPCION_DESCRIPCION = 'Descripción';
+
+    const MAPA_CONTROLES: { [key: string]: string } = {
+      [OPCION_CLAVE]: 'claveResiduo',
+      [OPCION_NOMBRE]: 'nombre',
+      [OPCION_DESCRIPCION]: 'descripcion'
+    };
+
+    const TODOS_LOS_CONTROLES = Object.values(MAPA_CONTROLES);
+
+    this.formularioResiduo.get('clasificacion')?.valueChanges.subscribe((VALOR_CLASIFICACION: string) => {
+      TODOS_LOS_CONTROLES.forEach((CAMPO: string) => {
+        const CONTROL = this.formularioResiduo.get(CAMPO);
+        CONTROL?.disable();
+        CONTROL?.reset();
+      });
+
+      const CAMPO_ACTIVO = MAPA_CONTROLES[VALOR_CLASIFICACION];
+      const CONTROL_ACTIVO = this.formularioResiduo.get(CAMPO_ACTIVO);
+
+      CONTROL_ACTIVO?.enable();
+    });
+  }
+
+
+
 }
