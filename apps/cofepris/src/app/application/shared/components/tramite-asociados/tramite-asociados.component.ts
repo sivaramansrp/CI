6ed1def2233c6c1dda6abc1b13183e3cc89857d9
Tanishk Kalaskar <tanishk.kalaskar@ilink-systems.com>
2025-04-09 18:50:52 +0530

    generate compodoc and testcases for tramite-asociados

--- apps/cofepris/src/app/application/shared/components/tramite-asociados/tramite-asociados.component.ts
+++ apps/cofepris/src/app/application/shared/components/tramite-asociados/tramite-asociados.component.ts
@@ -1,21 +1,32 @@
-import { Component, Input } from '@angular/core';
 import {
   ConfiguracionColumna,
   TablaDinamicaComponent,
-  TablaSeleccion,
-  TituloComponent,
+  TituloComponent
 } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { TramiteAsociados } from '../../models/tramite-asociados.model';
 
+/**
+ * Componente que representa la tabla de trámites asociados.
+ * Permite mostrar y gestionar una lista de trámites asociados con su configuración de columnas.
+ */
 @Component({
   selector: 'app-tramite-asociados',
   standalone: true,
-  imports: [CommonModule, TituloComponent ,TablaDinamicaComponent],
+  imports: [CommonModule, TituloComponent, TablaDinamicaComponent],
   templateUrl: './tramite-asociados.component.html',
   styleUrl: './tramite-asociados.component.css',
 })
 export class TramiteAsociadosComponent {
-  @Input() configuracionTabla!: ConfiguracionColumna<TramiteAsociados>[];
-  @Input() tramiteAsociados!: TramiteAsociados[];
+  /**
+   * Configuración de las columnas de la tabla.
+   * Define cómo se mostrarán los datos de los trámites asociados.
+   */
+  configuracionTabla!: ConfiguracionColumna<TramiteAsociados>[];
+
+  /**
+   * Lista de trámites asociados que se mostrarán en la tabla.
+   */
+  tramiteAsociados!: TramiteAsociados[];
 }
