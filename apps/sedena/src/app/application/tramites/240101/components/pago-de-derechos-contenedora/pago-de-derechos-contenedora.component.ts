ff77ffdbb3216615b4099f55a8d9c5c5fc0ef264
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-04-10 17:37:45 +0530

    refactor pago-de-derechos-contenedora to manage form state and update logic

--- apps/sedena/src/app/application/tramites/240101/components/pago-de-derechos-contenedora/pago-de-derechos-contenedora.component.ts
+++ apps/sedena/src/app/application/tramites/240101/components/pago-de-derechos-contenedora/pago-de-derechos-contenedora.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagoDeDerechosComponent } from '../../../../shared/components/pago-de-derechos/pago-de-derechos.component';
+import { Subject, takeUntil } from 'rxjs';
+import { PagoDerechosFormState } from '../../../../shared/models/pago-de-derechos.model';
+import { Tramite240101Query } from '../../estados/tramite240101Query.query';
+import {
+  Tramite240101State,
+  Tramite240101Store,
+} from '../../estados/tramite240101Store.store';
 
 @Component({
   selector: 'app-pago-de-derechos-contenedora',
@@ -9,4 +16,33 @@ import { PagoDeDerechosComponent } from '../../../../shared/components/pago-de-d
   templateUrl: './pago-de-derechos-contenedora.component.html',
   styleUrl: './pago-de-derechos-contenedora.component.css',
 })
-export class PagoDeDerechosContenedoraComponent {}
+export class PagoDeDerechosContenedoraComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
+  public pagoDerechoFormState!: PagoDerechosFormState;
+  private destroy$ = new Subject<void>();
+  constructor(
+    private tramiteQuery: Tramite240101Query,
+    private tramiteStore: Tramite240101Store
+  ) {}
+
+  /**
+   * @method ngOnInit
+   * @description Hook del ciclo de vida que se ejecuta al inicializar el componente.
+   * Suscribe los observables para mostrar los datos en la vista.
+   */
+  ngOnInit(): void {
+    this.tramiteQuery.getPagoDerechos$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.pagoDerechoFormState = data;
+      });
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+  updatePagoDerechos(event: PagoDerechosFormState): void {
+    this.tramiteStore.updatePagoDerechosFormState(event);
+  }
+}
