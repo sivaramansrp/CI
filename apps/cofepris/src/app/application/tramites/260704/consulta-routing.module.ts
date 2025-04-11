744b2c4d0e52fbcd6a08472904b899e374592101
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-04-11 14:51:29 +0530

    Resolved routing issues

--- apps/cofepris/src/app/application/tramites/260704/consulta-routing.module.ts
+++ apps/cofepris/src/app/application/tramites/260704/consulta-routing.module.ts
@@ -1,23 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AcusePageComponent } from '@ng-mf/data-access-user';
 import { NgModule } from '@angular/core';
-import { PaginaDosAcusesYResolucionesDetallesComponent } from './pages/pagina-dos-acuses-y-resoluciones-detalles/pagina-dos-acuses-y-resoluciones-detalles.component';
-import { PaginaUnoAcusesYResolucionesBusquedaComponent } from './pages/pagina-uno-acuses-y-resoluciones-busqueda/pagina-uno-acuses-y-resoluciones-busqueda.component';
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
 
 const ROUTES: Routes = [
   {
     path: 'solicitud',
     component: SolicitudPageComponent,
-  },
-  {
-    path: 'pagina-uno-acuses-y-resoluciones-busqueda',
-    component: PaginaUnoAcusesYResolucionesBusquedaComponent,
-  },
-
-  {
-    path: 'pagina-dos-acuses-y-resoluciones-detalles',
-    component: PaginaDosAcusesYResolucionesDetallesComponent,
   },
   {
     path: 'acuse',
