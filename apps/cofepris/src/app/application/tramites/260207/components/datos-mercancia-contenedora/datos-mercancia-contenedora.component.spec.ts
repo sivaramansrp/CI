a1f61b2fe39139bc5911dc7778922267ddc0bfbf
swamini1212-ilink <swamini.godse@ilink-systems.com>
2025-04-11 20:17:53 +0530

    fixed the lint and test issues

--- apps/cofepris/src/app/application/tramites/260207/components/datos-mercancia-contenedora/datos-mercancia-contenedora.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/260207/components/datos-mercancia-contenedora/datos-mercancia-contenedora.component.spec.ts
@@ -54,11 +54,11 @@ describe('DatosMercanciaContenedoraComponent', () => {
 
     component.tramiteState = mockState as any;
 
-    jest.spyOn(component['tramite260216Store'], 'update');
+    jest.spyOn(component['tramite260207Store'], 'update');
 
     component.mercanciaSeleccionado(mockEvent as any);
 
     expect(component.SeleccionadoDatos).toEqual(mockEvent);
-    expect(component['tramite260216Store'].update).toHaveBeenCalled();
+    expect(component['tramite260207Store'].update).toHaveBeenCalled();
   });
 });
