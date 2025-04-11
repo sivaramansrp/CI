bd2af99f22fd76dd329388853187b6f9c3542465
Ghanshyam Yadav <ghanshyam.yadav@ilink-systems.com>
2025-04-11 08:34:21 +0530

    Enhance TramiteDetails interface documentation and remove unused property

--- apps/dashboard/src/app/seleccion-tramite-desde-panel/seleccion-tramite-desde-panel.component.ts
+++ apps/dashboard/src/app/seleccion-tramite-desde-panel/seleccion-tramite-desde-panel.component.ts
@@ -7,10 +7,18 @@ import { TituloComponent } from "@ng-mf/data-access-user";
 import { Subject, takeUntil } from 'rxjs';
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
+/**
+ * Represents the details of a "Tramite" (procedure or process).
+ *
+ * @interface TramiteDetails
+ * @property {number} id - The unique identifier for the tramite.
+ * @property {number} tramite - The specific tramite number or code.
+ * @property {string} link - A hyperlink related to the tramite.
+ * @property {string} department - The department responsible for the tramite.
+ */
 interface TramiteDetails {
   id: number;
   tramite: number;
-  url: string;
   link: string;
   department: string;
 }
@@ -28,8 +36,25 @@ export class SeleccionTramiteDesdePanelComponent implements OnInit, OnDestroy {
    * Variable para asingar el endpoint de la ruta
    */
   public ruta: string = '';
+
+  /**
+   * Un arreglo que contiene los detalles de varios "Trámites" (procedimientos o procesos).
+   * Cada elemento en el arreglo es de tipo `TramiteDetails`.
+   * Estos datos se utilizan para gestionar y mostrar información relacionada con diferentes trámites.
+   */
+  
   public tramiteData: TramiteDetails[] = [];
+  /**
+   * Un Subject que emite un valor `void` para señalar la finalización de las suscripciones.
+   * Se utiliza para gestionar y limpiar observables, evitando fugas de memoria.
+   * Normalmente se completa en el hook de ciclo de vida `ngOnDestroy`.
+   */
   private destroy$ = new Subject<void>();
+
+  /**
+   * Una instancia de HttpClient utilizada para realizar solicitudes HTTP.
+   * Este servicio se utiliza típicamente para comunicarse con APIs de backend.
+   */
   private http: HttpClient;
   
   constructor(http: HttpClient) {
