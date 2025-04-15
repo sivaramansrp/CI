1ffed1a271316fccf66155d24d04d68b2ef183e5
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-04-15 13:17:11 +0530

    Created skeleton code for 32508

--- apps/agace/src/app/application/tramites/32508/components/aviso.component.ts
+++ apps/agace/src/app/application/tramites/32508/components/aviso.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { TituloComponent } from '@libs/shared/data-access-user/src';
 
 @Component({
   selector: 'app-aviso',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule,TituloComponent],
   templateUrl: './aviso.component.html',
   styleUrl: './aviso.component.css',
 })
-export class AvisoComponent {}
+export class AvisoComponent {
+  avisoForm !:FormGroup;
+}
