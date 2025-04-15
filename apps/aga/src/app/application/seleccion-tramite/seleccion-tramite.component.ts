fd1d1d55e16417f3caee5ca6fb89615322cb264f
Ghanshyam Yadav <ghanshyam.yadav@ilink-systems.com>
2025-04-15 12:44:24 +0530

    updated aga selection tramite to read tramite details list from libs location and display only aga related procedure.

--- apps/aga/src/app/application/seleccion-tramite/seleccion-tramite.component.ts
+++ apps/aga/src/app/application/seleccion-tramite/seleccion-tramite.component.ts
@@ -1,23 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AMBIENTES } from '@ng-mf/data-access-user';
+import { Subject, takeUntil } from 'rxjs';
+import tramiteDetailsData from '@libs/shared/theme/assets/json/tramiteList.json'
 
+/**
+ * Representa los detalles de un "Trámite" (procedimiento o proceso).
+ *
+ * @interface TramiteDetails
+ * @property {number} id - El identificador único para el trámite.
+ * @property {number} tramite - El número o código específico del trámite.
+ * @property {string} link - Un hipervínculo relacionado con el trámite.
+ * @property {string} department - El departamento responsable del trámite.
+ */
+interface TramiteDetails {
+  id: number;
+  tramite: number;
+  link: string;
+  department: string;
+}
 @Component({
   selector: 'seleccion-tramite',
   templateUrl: './seleccion-tramite.component.html',
 })
-export class SeleccionTramiteComponent implements OnInit {
+export class SeleccionTramiteComponent implements OnInit, OnDestroy {
 
   /**
    * Variable para asingar el endpoint de la ruta
    */
   public ruta: string = '';
+
+  /**
+   * Un arreglo que contiene los detalles de varios "Trámites" (procedimientos o procesos).
+   * Cada elemento en el arreglo es de tipo `TramiteDetails`.
+   * Estos datos se utilizan para gestionar y mostrar información relacionada con diferentes trámites.
+   */
   
+  public tramiteData: TramiteDetails[] = [];
+  /**
+   * Un Subject que emite un valor `void` para señalar la finalización de las suscripciones.
+   * Se utiliza para gestionar y limpiar observables, evitando fugas de memoria.
+   * Normalmente se completa en el hook de ciclo de vida `ngOnDestroy`.
+   */
+  private destroy$ = new Subject<void>();
+
+  /**
+   * Una instancia de HttpClient utilizada para realizar solicitudes HTTP.
+   * Este servicio se utiliza típicamente para comunicarse con APIs de backend.
+   */
+  private http: HttpClient;
+  
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+    
   ngOnInit(): void {
     if (window.location.host.indexOf('localhost') !== -1) {
       this.ruta = AMBIENTES.LOCALHOST;
     } else {
       this.ruta = AMBIENTES.DESARROLLO
     }
+
+    this.tramiteData = tramiteDetailsData.filter((v) => v.department === "aga") ;
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+    this.tramiteData = [];
   }
   
 }
