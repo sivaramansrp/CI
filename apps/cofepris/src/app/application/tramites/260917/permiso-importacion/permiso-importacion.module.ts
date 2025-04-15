a92d439305d9e6f79597aea4bbd3db5ca196cbba
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-15 17:52:28 +0530

    module added

---
+++ apps/cofepris/src/app/application/tramites/260917/permiso-importacion/permiso-importacion.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PermisoImportacionRoutingModule } from './permiso-importacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PermisoImportacionRoutingModule
+  ]
+})
+export class PermisoImportacionModule { }
