83f9da006315b8627f827f56054a5804b4769555
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-16 12:40:21 +0530

    components

---
+++ apps/cofepris/src/app/application/tramites/260917/components/datos-solicitud/datos-solicitud.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-datos-solicitud',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './datos-solicitud.component.html',
+  styleUrl: './datos-solicitud.component.css',
+})
+export class DatosSolicitudComponent {}
---
+++ apps/cofepris/src/app/application/tramites/260917/components/pago-derechos/pago-derechos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pago-derechos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pago-derechos.component.html',
+  styleUrl: './pago-derechos.component.css',
+})
+export class PagoDerechosComponent {}
