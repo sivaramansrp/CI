9c7ea261a94adc28408d45af0ef26ebd64645ebc
banshi-ilink-digital <banshi.bhatt@ilink-systems.com>
2025-04-16 19:05:51 +0530

    add  Eliminar

--- apps/sedena/src/app/application/shared/components/agregar-proveedor-custom/agregar-proveedor-custom.component.ts
+++ apps/sedena/src/app/application/shared/components/agregar-proveedor-custom/agregar-proveedor-custom.component.ts
@@ -1,8 +1,8 @@
 import { Catalogo, CatalogoSelectComponent, InputRadioComponent, TipoPersona, TituloComponent } from '@ng-mf/data-access-user';
 import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output } from '@angular/core';
+import { DestinoFinal, Proveedor } from '../../models/terceros-relacionados.model';
 
 import { CommonModule, Location } from '@angular/common';
-import { DestinoFinal, Proveedor } from '../../models/terceros-relacionados.model';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PROCEDIMIENTOS_PARA_NO_CONTRIBUYENTE, TERCEROS_NACIONALIDAD_OPCIONES, TIPO_PERSONA_OPCIONES } from '../../constants/datos-solicitud.enum';
 import { Subject, takeUntil } from 'rxjs';
@@ -69,13 +69,13 @@ export class AgregarProveedorCustomComponent implements OnDestroy, OnInit, OnCha
    */
   @Output() updateProveedorTablaDatos = new EventEmitter<Proveedor[]>();
 
-    /**
+     /**
    * Datos del formulario que pueden ser de tipo `DestinoFinal`, `Proveedor`, `null` o `undefined`.
    * Este input se utiliza para recibir la información necesaria desde el componente padre.
    *
    * @type {DestinoFinal | Proveedor | null | undefined}
    */
-    @Input() formaDatos!: DestinoFinal | Proveedor | null | undefined;
+     @Input() formaDatos!: DestinoFinal | Proveedor | null | undefined;
 
 
   /**
