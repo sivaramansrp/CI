c65d81fc299c33fa8c9285fdad764cd8e5811c85
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-16 12:53:28 +0530

    components added

--- apps/cofepris/src/app/application/tramites/260917/permiso-importacion.module.ts
+++ apps/cofepris/src/app/application/tramites/260917/permiso-importacion.module.ts
@@ -1,11 +1,15 @@
 import { BtnContinuarComponent, SolicitanteComponent, WizardComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
+import { DatosSolicitudComponent } from './components/datos-solicitud/datos-solicitud.component';
 import { ModificacionPermisoImportacionComponent } from './pages/modificacion-permiso-importacion/modificacion-permiso-importacion.component';
 import { NgModule } from '@angular/core';
+import { PagoDerechosComponent } from './components/pago-derechos/pago-derechos.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { PermisoImportacionRoutingModule } from './permiso-importacion-routing.module';
+import { TercerosRelacionadosComponent } from './components/terceros-relacionados/terceros-relacionados.component';
+import { TramitesAsociadosComponent } from './components/tramites-asociados/tramites-asociados.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,6 @@ import { PermisoImportacionRoutingModule } from './permiso-importacion-routing.m
     PasoDosComponent,
     PasoTresComponent
   ],
-  imports: [CommonModule, PermisoImportacionRoutingModule,BtnContinuarComponent,WizardComponent,SolicitanteComponent],
+  imports: [CommonModule, PermisoImportacionRoutingModule,BtnContinuarComponent,WizardComponent,SolicitanteComponent,DatosSolicitudComponent,TercerosRelacionadosComponent,PagoDerechosComponent,TramitesAsociadosComponent],
 })
 export class PermisoImportacionModule {}
