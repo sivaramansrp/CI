cca3c0f7fdafa6703e5b8e0bfb367e2ceb8b5625
navin-ilink <navin.kumar@ilink-system.com>
2025-04-17 11:59:05 +0530

    260513

--- apps/cofepris/src/app/application/tramites/260513/constantes/datos-solicitud.enum.ts
+++
@@ -1,9 +0,0 @@
-import { ConfiguracionVisibilidad } from "../models/datos-solitudes.model";
-
-export const DEFAULT_CONFIGURACION_VISIBILIDAD: ConfiguracionVisibilidad = {
-  paisOrigen: true,
-  paisFabrica: false,
-  paisElaboracion: false,
-  paisProveedor: false,
-  paisProcedencia: true
-};
--- apps/cofepris/src/app/application/tramites/260513/models/datos-solitudes.model.ts
+++
@@ -1,7 +0,0 @@
-export interface ConfiguracionVisibilidad {
-    paisOrigen: boolean;
-    paisFabrica: boolean;
-    paisElaboracion: boolean;
-    paisProveedor: boolean;
-    paisProcedencia: boolean;
-  }
