0160550634b248d99f9ca557acc5376302d0e383
Atul Falle <atul.falle@ilink-systems.com>
2025-04-17 09:44:20 +0530

    lint fixes

--- apps/sedena/src/app/application/tramites/240108/pages/solicitud-page/solicitud-page.component.ts
+++ apps/sedena/src/app/application/tramites/240108/pages/solicitud-page/solicitud-page.component.ts
@@ -1,13 +1,8 @@
 import { AccionBoton } from '@ng-mf/data-access-user';
-import { BtnContinuarComponent } from '@ng-mf/data-access-user';
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DatosPasos } from '@ng-mf/data-access-user';
 import { ListaPasosWizard } from '@ng-mf/data-access-user';
 import { PASOS } from '../../constants/importacion-armas-explosivo.enum';
-import { PasoDosComponent } from '../paso-dos/paso-dos.component';
-import { PasoTresComponent } from '../paso-tres/paso-tres.component';
-import { PasoUnoComponent } from '../paso-uno/paso-uno.component';
 import { TITULOMENSAJE } from '../../constants/importacion-armas-explosivo.enum';
 import { ViewChild } from '@angular/core';
 import { WizardComponent } from '@ng-mf/data-access-user';
