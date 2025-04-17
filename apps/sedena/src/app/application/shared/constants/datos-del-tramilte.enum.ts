6bbbe0c9bc240f020db9d1768a80899f7efdf7e0
Atul Falle <atul.falle@ilink-systems.com>
2025-04-17 12:46:24 +0530

    added tramite id

--- apps/sedena/src/app/application/shared/constants/datos-del-tramilte.enum.ts
+++ apps/sedena/src/app/application/shared/constants/datos-del-tramilte.enum.ts
@@ -27,7 +27,7 @@ export const CROSLISTA_ADUANAS_DISPONIBLES: string[] = [
  * @type {number[]}
  * @description Representa códigos específicos utilizados en el sistema para identificar tipos de permisos definitivos.
  */
-export const PERMISO_DEFINITIVO_TITULO = [240119, 240118, 240107, 240106];
+export const PERMISO_DEFINITIVO_TITULO = [240119, 240118, 240107, 240106, 240108];
 
 /**
  * Identificadores numéricos relacionados con permisos aduaneros.
