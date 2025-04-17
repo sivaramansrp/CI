87806b4d6bb30d3372ea8dbddd580d494b9ab1bd
Aditi <aditi.patil@ilink-systems.com>
2025-04-17 10:53:48 +0530

    pas-dos and paso-tres added

--- apps/cofepris/src/app/application/tramites/261401/constants/solicitud-modificacion-permiso-salida-territorio.enum.ts
+++ apps/cofepris/src/app/application/tramites/261401/constants/solicitud-modificacion-permiso-salida-territorio.enum.ts
@@ -1,26 +1,20 @@
 export const PASOS_EXPORTACION = [
-    {
-        indice: 1,
-        titulo: 'Capturar solicitud',
-        activo: true,
-        completado: true,
-    },
-    {
-        indice: 2,
-        titulo: 'Requisitos necesarios',
-        activo: false,
-        completado: false,
-      },
-      {
-        indice: 3,
-        titulo: 'Anexar requisitos',
-        activo: false,
-        completado: false,
-      },
-      {
-        indice: 4,
-        titulo: 'Firmar solicitud',
-        activo: false,
-        completado: false,
-      }
+  {
+    indice: 1,
+    titulo: 'Capturar solicitud',
+    activo: true,
+    completado: true,
+},
+{
+    indice: 2,
+    titulo: 'Requisitos necesarios',
+    activo: false,
+    completado: false,
+},
+{
+    indice: 3,
+    titulo: 'Anexar necesarios',
+    activo: false,
+    completado: false,
+}
 ];
