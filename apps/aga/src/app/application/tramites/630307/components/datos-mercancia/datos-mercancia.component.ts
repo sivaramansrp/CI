1c124ece3a67d356c2c82830588c7373c514a0b2
sonam-ilink <sonam.rajput@ilink-systems.com>
2025-04-17 14:50:13 +0530

    solved error

--- apps/aga/src/app/application/tramites/630307/components/datos-mercancia/datos-mercancia.component.ts
+++ apps/aga/src/app/application/tramites/630307/components/datos-mercancia/datos-mercancia.component.ts
@@ -1,5 +1,5 @@
 /**
- * Componente que gestiona los datos de la mercancía para el trámite 630303.
+ * Componente que gestiona los datos de la mercancía para el trámite 630307.
  */
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
@@ -9,13 +9,13 @@ import { ModeloDeFormaDinamica, REGEX_NO_ESPACIOS_AL_INICIO_NI_AL_FINAL, TituloC
 import { FormasDinamicasComponent } from '@libs/shared/data-access-user/src/tramites/components/formas-dinamicas/formas-dinamicas/formas-dinamicas.component'; // Adjust the path as needed
 
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Tramite630303State, Tramite630303Store } from '../../estados/tramite630303.store';
-import { Tramite630303Query } from '../../estados/tramite630303.query';
+import { Tramite630307State, Tramite630307Store } from '../../estados/tramite630307.store';
+import { Tramite630307Query } from '../../estados/tramite630307.query';
 
 import { Subject, takeUntil } from 'rxjs';
 import { FORMULARIO_DATOS } from '../../enum/retorno-importacion-temporal.enum';
 /**
- * Componente que gestiona los datos de la mercancía para el trámite 630303.
+ * Componente que gestiona los datos de la mercancía para el trámite 630307.
  * Permite inicializar formularios, obtener datos del estado y manejar el estado del formulario.
  */
 @Component({
@@ -33,9 +33,9 @@ export class DatosMercanciaComponent implements OnInit, OnDestroy {
   private destroyed$ = new Subject<void>();
 
   /**
-   * Estado seleccionado del trámite 630303.
+   * Estado seleccionado del trámite 630307.
    */
-  estadoSeleccionado!: Tramite630303State;
+  estadoSeleccionado!: Tramite630307State;
 
   /**
    * Formulario reactivo para gestionar los datos de la mercancía.
@@ -47,13 +47,13 @@ formularioDatos: ModeloDeFormaDinamica[]=FORMULARIO_DATOS;
    * Constructor del componente.
    * 
    * @param formBuilder - Constructor de formularios reactivos.
-   * @param tramite630303Store - Store para manejar el estado del trámite.
-   * @param tramite630303Query - Query para consultar el estado del trámite.
+   * @param tramite630307Store - Store para manejar el estado del trámite.
+   * @param tramite630307Query - Query para consultar el estado del trámite.
    */
   constructor(
     private formBuilder: FormBuilder,
-    private tramite630303Store: Tramite630303Store,
-    private tramite630303Query: Tramite630303Query
+    private tramite630307Store: Tramite630307Store,
+    private tramite630307Query: Tramite630307Query
   ) {}
 
   /**
@@ -93,7 +93,7 @@ formularioDatos: ModeloDeFormaDinamica[]=FORMULARIO_DATOS;
    */
   setValorStore(FormGroup: FormGroup, control: string): void {
     const VALOR = FormGroup.get(control)?.value;
-    this.tramite630303Store.setTramite630303State({
+    this.tramite630307Store.setTramite630307State({
       [control]: VALOR,
     });
   }
@@ -102,7 +102,7 @@ formularioDatos: ModeloDeFormaDinamica[]=FORMULARIO_DATOS;
    * Obtiene el estado actual del trámite desde el store.
    */
   getValorStore(): void {
-    this.tramite630303Query.selectTramite630303State$
+    this.tramite630307Query.selectTramite630307State$
       .pipe(takeUntil(this.destroyed$))
       .subscribe((data) => {
         this.estadoSeleccionado = data;
