ce8fd8388b31759d9d55e306181b0118786a3e81
JamunaSri S S <jamunasri.s@purpleslate.in>
2025-04-17 21:52:21 +0530

    removing duplication

--- apps/sedena/src/app/application/shared/components/agregar-destinatario-final/agregar-destinatario-final.component.ts
+++ apps/sedena/src/app/application/shared/components/agregar-destinatario-final/agregar-destinatario-final.component.ts
@@ -177,8 +177,6 @@ export class AgregarDestinatarioFinalComponent
    */
   @Input() formaDatos!: DestinoFinal | Proveedor| null | undefined;
 
-  public campoObligatorio = true
-
   /**
    * @property campoObligatorio
    * @description Indica si ciertos campos del formulario son obligatorios según el procedimiento.
@@ -307,25 +305,6 @@ export class AgregarDestinatarioFinalComponent
     if(this.formaDatos) {
       this.agregarDestinatarioFinal.patchValue(this.formaDatos);
     }
-  }
-
-  campoObligatorioChange(): void {
-    const COLONIA = this.agregarDestinatarioFinal.get('colonia')
-    const CALLE = this.agregarDestinatarioFinal.get('calle')
-    const NUMEROEXTERIOR = this.agregarDestinatarioFinal.get('numeroExterior')
-    if(this.campoObligatorio){
-      COLONIA?.clearValidators();
-      CALLE?.setValidators([Validators.required]);
-      NUMEROEXTERIOR?.setValidators([Validators.required]);
-    }
-    else{
-      COLONIA?.setValidators([Validators.required]);
-      CALLE?.clearValidators();
-      NUMEROEXTERIOR?.clearValidators();
-    }
-    COLONIA?.updateValueAndValidity();
-    CALLE?.updateValueAndValidity();
-    NUMEROEXTERIOR?.updateValueAndValidity();
   }
 
   /**
