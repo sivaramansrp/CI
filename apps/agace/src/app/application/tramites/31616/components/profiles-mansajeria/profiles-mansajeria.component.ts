22937eada27ee8335388d01ceea792ae64713c57
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-04-17 15:02:22 +0530

    Added accordion

--- apps/agace/src/app/application/tramites/31616/components/profiles-mansajeria/profiles-mansajeria.component.ts
+++ apps/agace/src/app/application/tramites/31616/components/profiles-mansajeria/profiles-mansajeria.component.ts
@@ -1,21 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SeguridadFisicaComponent } from '../seguridad-fisica/seguridad-fisica.component';
 
 @Component({
   selector: 'app-profiles-mansajeria',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule,FormsModule],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule, SeguridadFisicaComponent],
   templateUrl: './profiles-mansajeria.component.html',
   styleUrl: './profiles-mansajeria.component.css',
 })
 export class ProfilesMansajeriaComponent {
   profileForm!:FormGroup;
   mostrarContenido = false;
+  showSeguridad = false;
   constructor(private fb: FormBuilder) {
-}
+    this.profileForm = this.fb.group({
+      domicilio: [''],
+      antiguedad: [''],
+      productos: [''],
+      embarquesExp: [''],
+      embarquesImp: [''],
+      empleados: [''],
+      superficie: [''],
+      nombre: [''],
+      categoria: [''],
+      vigencia: [''],
+      nombre2: [''],
+      categoria2: [''],
+      vigencia2: [''],
+      nombre3: [''],
+      categoria3: [''],
+      vigencia3: ['']
+    })
+  }
 
 alternarContenido(): void {
   this.mostrarContenido = !this.mostrarContenido;
+}
+
+toggleSeguridad() {
+  this.showSeguridad = !this.showSeguridad;
 }
 }
---
+++ apps/agace/src/app/application/tramites/31616/components/seguridad-fisica/seguridad-fisica.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguridad-fisica',
+  standalone: true,
+  imports: [],
+  templateUrl: './seguridad-fisica.component.html',
+  styleUrl: './seguridad-fisica.component.css'
+})
+export class SeguridadFisicaComponent {
+
+}
