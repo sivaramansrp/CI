1188ade5185cdb3ae69b5c39b06e6895e7246336
Kalai-2025 <kalaiyarasan.s@ilink-systems.com>
2025-04-17 08:57:26 +0530

    UI - 09

--- apps/sedena/src/app/application/tramites/240120/constants/importacion-armas-municiones.enum.ts
+++ apps/sedena/src/app/application/tramites/240120/constants/importacion-armas-municiones.enum.ts
@@ -19,7 +19,7 @@ export const PASOS = [
   },
 ];
 export const TITULOMENSAJE =
-  'Solicitud Permiso ordinario para la importación de armamento, municiones y diverso material para personas físicas y morales';
+  'Solicitud Permiso extraordinario para la exportación de artificios pirotécnicos';
 export const TEXTOS_REQUISITOS =
   'La solicitud ha quedado registrada con el número temporal [202767640]. Este no tiene validez legal y sirve solamente para efectos de identificar tu Solicitud. Un folio oficial le será asignado a la solicitud al momento en que esta sea firmada.';
 
