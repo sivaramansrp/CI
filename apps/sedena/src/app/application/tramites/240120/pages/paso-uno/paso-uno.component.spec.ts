39b439234c31b24e0ce431153626a40b5ff9e6df
Kalai-2025 <kalaiyarasan.s@ilink-systems.com>
2025-04-17 19:48:28 +0530

    UI - 11

--- apps/sedena/src/app/application/tramites/240120/pages/paso-uno/paso-uno.component.spec.ts
+++ apps/sedena/src/app/application/tramites/240120/pages/paso-uno/paso-uno.component.spec.ts
@@ -1,21 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// @ts-nocheck
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
 import { PasoUnoComponent } from './paso-uno.component';
+import { Tramite240120Query } from '../../estados/tramite240120Query.query';
+import { Tramite240120Store } from '../../estados/tramite240120Store.store';
+
+@Injectable()
+class MockTramite240120Query {}
+
+@Injectable()
+class MockTramite240120Store {}
 
 describe('PasoUnoComponent', () => {
-  let component: PasoUnoComponent;
-  let fixture: ComponentFixture<PasoUnoComponent>;
+  let fixture;
+  let component;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PasoUnoComponent],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, ReactiveFormsModule ],
+      declarations: [
+        PasoUnoComponent,
+      ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: Tramite240120Query, useClass: MockTramite240120Query },
+        { provide: Tramite240120Store, useClass: MockTramite240120Store }
+      ]
+    }).overrideComponent(PasoUnoComponent, {
+
     }).compileComponents();
-
     fixture = TestBed.createComponent(PasoUnoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
-  it('should create', () => {
+  it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
   });
+
+  it('should run #ngOnInit()', async () => {
+    component.tramite240120Query = component.tramite240120Query || {};
+    component.tramite240120Query.getTabSeleccionado$ = observableOf({});
+    component.ngOnInit();
+
+  });
+
+  it('should run #seleccionaTab()', async () => {
+    component.tramite240120Store = component.tramite240120Store || {};
+    component.tramite240120Store.updateTabSeleccionado = jest.fn();
+    component.seleccionaTab({});
+    expect(component.tramite240120Store.updateTabSeleccionado).toHaveBeenCalled();
+  });
+
+  it('should run #ngOnDestroy()', async () => {
+    component.destroyNotifier$ = component.destroyNotifier$ || {};
+    component.destroyNotifier$.next = jest.fn();
+    component.destroyNotifier$.complete = jest.fn();
+    component.ngOnDestroy();
+    expect(component.destroyNotifier$.next).toHaveBeenCalled();
+  });
+
 });
