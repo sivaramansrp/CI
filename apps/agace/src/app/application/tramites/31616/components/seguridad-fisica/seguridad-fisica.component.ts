e63da14a86a46feeac9a73cb42f27112b8cbaddc
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-04-18 13:41:59 +0530

    Accordion 2 UI created

--- apps/agace/src/app/application/tramites/31616/components/seguridad-fisica/seguridad-fisica.component.ts
+++ apps/agace/src/app/application/tramites/31616/components/seguridad-fisica/seguridad-fisica.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { InputRadioComponent } from '@libs/shared/data-access-user/src';
+import { OPCIONES_DE_BOTON_DE_RADIO } from '@libs/shared/data-access-user/src/tramites/constantes/31616/datos-comunes.enum';
 
 @Component({
   selector: 'app-seguridad-fisica',
   standalone: true,
-  imports: [],
+  imports: [InputRadioComponent],
   templateUrl: './seguridad-fisica.component.html',
   styleUrl: './seguridad-fisica.component.css'
 })
 export class SeguridadFisicaComponent {
+
+    opcionDeBotonDeRadio = OPCIONES_DE_BOTON_DE_RADIO;
 
 }
