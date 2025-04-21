a256ebbc0aeb036b78e341c5eb5f94d8a9dae669
Kanchan Phutane <kanchan.phutane@ilink-systems.com>
2025-04-21 09:55:48 +0530

    code changes for skeleton added

---
+++ apps/semarnat/src/app/application/tramites/230202/phytosanitary-rexportation.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhytosanitaryRexportationRoutingModule } from './phytosanitary-rexportation-routing.module';
+import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PhytosanitaryRexportationRoutingModule,
+    SolicitudPageComponent
+  ]
+})
+export class PhytosanitaryRexportationModule { }
