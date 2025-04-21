c8d578e5eca781dbe9b372b8ff065d2efb73879b
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-04-21 19:33:47 +0530

    Resolved review comment s

--- apps/agace/src/app/application/tramites/31301/pages/paso-dos/paso-dos.component.ts
+++ apps/agace/src/app/application/tramites/31301/pages/paso-dos/paso-dos.component.ts
@@ -14,7 +14,7 @@ import { TituloComponent } from '@libs/shared/data-access-user/src';
 import { takeUntil } from 'rxjs';
 
 /**
- * Componente PasoDosComponent que representa el segundo paso del trámite 6101.
+ * Componente PasoDosComponent que representa el segundo paso del trámite 31301.
  */
 @Component({
   selector: 'app-paso-dos',
@@ -30,7 +30,7 @@ import { takeUntil } from 'rxjs';
   styleUrl: './paso-dos.component.scss',
 })
 /**
- * Clase PasoDosComponent encargada de manejar la lógica y vista del segundo paso del trámite 6101.
+ * Clase PasoDosComponent encargada de manejar la lógica y vista del segundo paso del trámite 31301.
  */
 export class PasoDosComponent implements OnInit, OnDestroy {
   /**
