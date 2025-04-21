63b1ee7764e5204808dbe688bb2c95110a0a3743
navin-ilink <navin.kumar@ilink-system.com>
2025-04-21 16:29:34 +0530

    comment fixation

--- apps/cofepris/src/app/application/tramites/260513/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/cofepris/src/app/application/tramites/260513/components/datos-solicitud/datos-solicitud.component.ts
@@ -1,9 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ConfiguracionVisibilidad } from '../../models/datos-solitudes.model';
 import { DEFAULT_CONFIGURACION_VISIBILIDAD } from '../../constantes/datos-solicitud.enum';
 import { DatosDeLaComponent } from '../../../../shared/components/datos-solicitud/datos-solicitud.component';
-
+/**
+ * Interfaz que define la configuración de visibilidad para diferentes atributos relacionados con países.
+ * 
+ * @property {boolean} paisOrigen - Indica si el país de origen es visible.
+ * @property {boolean} paisFabrica - Indica si el país de fabricación es visible.
+ * @property {boolean} paisElaboracion - Indica si el país de elaboración es visible.
+ * @property {boolean} paisProveedor - Indica si el país del proveedor es visible.
+ * @property {boolean} paisProcedencia - Indica si el país de procedencia es visible.
+ */
+export interface ConfiguracionVisibilidad {
+  paisOrigen: boolean;
+  paisFabrica: boolean;
+  paisElaboracion: boolean;
+  paisProveedor: boolean;
+  paisProcedencia: boolean;
+}
 @Component({
   selector: 'app-datos-solicitud',
   standalone: true,
