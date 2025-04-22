e060216b7b559fa42a588941691ec6e6e9f5ed11
navin-ilink <navin.kumar@ilink-system.com>
2025-04-22 10:40:20 +0530

    issue fixation

--- apps/cofepris/src/app/application/tramites/260513/constantes/datos-solicitud.enum.ts
+++ apps/cofepris/src/app/application/tramites/260513/constantes/datos-solicitud.enum.ts
@@ -1,5 +1,19 @@
-import { ConfiguracionVisibilidad } from "../models/datos-solitudes.model";
-
+/**
+ * Interfaz que define la configuración de visibilidad para diferentes atributos relacionados con países.
+ * 
+ * @property {boolean} paisOrigen - Indica si el país de origen es visible.
+ * @property {boolean} paisFabrica - Indica si el país de fabricación es visible.
+ * @property {boolean} paisElaboracion - Indica si el país de elaboración es visible.
+ * @property {boolean} paisProveedor - Indica si el país del proveedor es visible.
+ * @property {boolean} paisProcedencia - Indica si el país de procedencia es visible.
+ */
+export interface ConfiguracionVisibilidad {
+  paisOrigen: boolean;
+  paisFabrica: boolean;
+  paisElaboracion: boolean;
+  paisProveedor: boolean;
+  paisProcedencia: boolean;
+}
 /**
  * Configuración predeterminada de visibilidad para los datos de solicitud.
  * 
