62fb6a78a83994ad8a939f9be3df3ff942150598
maneeshangulardeveloper <maneeshpatel2024@gmail.com>
2025-04-22 12:03:53 +0530

    changes fixed

--- apps/cofepris/src/app/application/app-routing.module.ts
+++ apps/cofepris/src/app/application/app-routing.module.ts
@@ -397,7 +397,13 @@ const ROUTES: Routes = [
         (m) => m.EnmiendaPermisoSanitarioModule
       ),
   },
-
+  {
+    path: 'solicitud-modificacion',
+    loadChildren: () =>
+      import('./tramites/261101/datos-solicitude.module').then(
+        (m) => m.DatosSolicitudeModule
+      ),
+  }, 
   {
         path: 'importar-suministros-medicos',
         loadChildren: () =>
