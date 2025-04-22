4dda3eab8cad908222164c5ad0028bc602aa65eb
Raghul C <raghul.c@purpleslate.in>
2025-04-22 17:34:43 +0530

    constant-spanish-word-update

--- apps/sedena/src/app/application/tramites/240117/components/datos-del-tramite-contenedora/datos-del-tramite-contenedora.component.ts
+++ apps/sedena/src/app/application/tramites/240117/components/datos-del-tramite-contenedora/datos-del-tramite-contenedora.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { DatosDelTramiteComponent } from '../../../../shared/components/datos-del-tramite/datos-del-tramite.component';
 import { DatosDelTramiteFormState } from '../../../../shared/models/datos-del-tramite.model';
-import { ID_PROCEDIMIENTO } from '../../constants/importacion-armas-municiones.enum';
+import { ID_PROCEDIMIENTO } from '../../constantes/importacion-armas-municiones.enum';
 import { MercanciaDetalle } from '../../../../shared/models/datos-del-tramite.model';
 import { OnDestroy } from '@angular/core';
 import { OnInit } from '@angular/core';
