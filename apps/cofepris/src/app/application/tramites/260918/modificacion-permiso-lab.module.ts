4c3e77fa4f92970cb9a33ed1ad6987efa55b82c0
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-22 15:49:21 +0530

    paso uno

--- apps/cofepris/src/app/application/tramites/260918/modificacion-permiso-lab.module.ts
+++ apps/cofepris/src/app/application/tramites/260918/modificacion-permiso-lab.module.ts
@@ -1,11 +1,20 @@
-import { BtnContinuarComponent, WizardComponent } from '@libs/shared/data-access-user/src';
+import {
+  BtnContinuarComponent,
+  WizardComponent,
+} from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { ModificacionPermisoLabComponent } from './pages/modificacion-permiso-lab/modificacion-permiso-lab.component';
 import { ModificacionPermisoLabRoutingModule } from './modificacion-permiso-lab-routing.module';
 import { NgModule } from '@angular/core';
+import { PasoUnoComponent } from './pages/modificacion-permiso-lab/paso-uno/paso-uno.component';
 
 @NgModule({
-  declarations: [ModificacionPermisoLabComponent],
-  imports: [CommonModule, ModificacionPermisoLabRoutingModule,WizardComponent,BtnContinuarComponent],
+  declarations: [ModificacionPermisoLabComponent, PasoUnoComponent],
+  imports: [
+    CommonModule,
+    ModificacionPermisoLabRoutingModule,
+    WizardComponent,
+    BtnContinuarComponent,
+  ],
 })
 export class ModificacionPermisoLabModule {}
