1d3b8fafcde75209ee6293af0a7f675dfec63be2
Kanchan Phutane <kanchan.phutane@ilink-systems.com>
2025-04-22 10:36:25 +0530

    code changes for 32201

--- apps/agace/src/app/application/app-routing.module.ts
+++ apps/agace/src/app/application/app-routing.module.ts
@@ -86,14 +86,14 @@ const ROUTES: Routes = [
       import('./tramites/32508/adace.module').then(
         (m) => m.AdaceModule
       ),
-  }
+  },
   {
     path: 'anexo-veintiocho',
     loadChildren: () =>
       import('./tramites/32201/anexo-veintiocho.module').then(
         (m) => m.AnexoVeintiochoModule
       ),
-  },
+  }
 ];
 
 @NgModule({
