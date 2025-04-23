7347794145492623469af4e9de516385ef486330
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-23 12:54:22 +0530

    reusable comp used

--- apps/cofepris/src/app/application/tramites/260918/modificacion-permiso-lab.module.ts
+++ apps/cofepris/src/app/application/tramites/260918/modificacion-permiso-lab.module.ts
@@ -4,6 +4,7 @@ import {
   WizardComponent,
 } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
+import { DatosDelSolicitudModificacionComponent } from '../../shared/components/datos-del-solicitud-modificacion/datos-del-solicitud-modificacion.component';
 import { ModificacionPermisoLabComponent } from './pages/modificacion-permiso-lab/modificacion-permiso-lab.component';
 import { ModificacionPermisoLabRoutingModule } from './modificacion-permiso-lab-routing.module';
 import { NgModule } from '@angular/core';
@@ -23,7 +24,8 @@ import { PasoUnoComponent } from './pages/modificacion-permiso-lab/paso-uno/paso
     ModificacionPermisoLabRoutingModule,
     WizardComponent,
     BtnContinuarComponent,
-    SolicitanteComponent
+    SolicitanteComponent,
+    DatosDelSolicitudModificacionComponent
   ],
 })
 export class ModificacionPermisoLabModule {}
