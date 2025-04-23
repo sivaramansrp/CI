76a2c623e712848e903863bda7853a850c871a57
atripathi860 <amit.tripathi@ilink-systems.com>
2025-04-23 10:13:21 +0530

    lint issue resolved

--- apps/cofepris/src/app/application/tramites/260509/permiso-vegetales-nutrientes.module.ts
+++ apps/cofepris/src/app/application/tramites/260509/permiso-vegetales-nutrientes.module.ts
@@ -1,5 +1,3 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {
   AlertComponent,
   AnexarDocumentosComponent,
@@ -12,17 +10,19 @@ import {
   TituloComponent,
   WizardComponent,
 } from '@libs/shared/data-access-user/src';
+import { AvisoDePrivacidadComponent } from '../../shared/components/aviso-de-privacidad/aviso-de-privacidad.component';
+import { CommonModule } from '@angular/common';
+import { DatosSolicitudComponent } from './components/datos-solicitud/datos-solicitud.component';
+import { NgModule } from '@angular/core';
+import { PagoDerechosComponent } from './components/pago-derechos/pago-derechos.component';
+import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
+import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { PermisoVegetalesNutrientesRoutingModule } from './permiso-vegetales-nutrientes-routing.module';
+import { PlaguicidasComponent } from './pages/plaguicidas/plaguicidas.component';
+import { TercerosRelacionadosFabricanteComponent } from './components/terceros-relacionados-fabricante/terceros-relacionados-fabricante.component';
 import { ToastrService } from 'ngx-toastr';
 import { provideHttpClient } from '@angular/common/http';
-import { DatosSolicitudComponent } from './components/datos-solicitud/datos-solicitud.component';
-import { TercerosRelacionadosFabricanteComponent } from './components/terceros-relacionados-fabricante/terceros-relacionados-fabricante.component';
-import { PagoDerechosComponent } from './components/pago-derechos/pago-derechos.component';
-import { AvisoDePrivacidadComponent } from '../../shared/components/aviso-de-privacidad/aviso-de-privacidad.component';
-import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
-import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
-import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
-import { PlaguicidasComponent } from './pages/plaguicidas/plaguicidas.component';
 
 @NgModule({
   declarations: [
