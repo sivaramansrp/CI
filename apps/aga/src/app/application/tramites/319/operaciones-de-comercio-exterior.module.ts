ed1d1a5f84d4195ae1610682809c7b161f986233
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-23 16:25:52 +0530

    refactor: reorganize imports and add missing component declarations in operaciones-de-comercio-exterior module

--- apps/aga/src/app/application/tramites/319/operaciones-de-comercio-exterior.module.ts
+++ apps/aga/src/app/application/tramites/319/operaciones-de-comercio-exterior.module.ts
@@ -2,21 +2,45 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 
+import { OperacionesDeComercioExteriorComponent } from './components/operaciones-de-comercio-exterior/operaciones-de-comercio-exterior.component';
 import { OperacionesDeComercioExteriorRoutingModule } from './operaciones-de-comercio-exterior-routing.module';
 import { PasoDocComponent } from './pages/paso-doc/paso-doc.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
+
+import { AlertComponent, AnexarDocumentosComponent, BtnContinuarComponent, CatalogoSelectComponent, CrosslistComponent, FirmaElectronicaComponent, InputCheckComponent, InputFechaComponent, InputRadioComponent,SharedModule, SolicitanteComponent, TablaDinamicaComponent, TituloComponent, WizardComponent } from '@ng-mf/data-access-user';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [
     PasoDocComponent,
     PasoUnoComponent,
-    SolicitudPageComponent
+    SolicitudPageComponent,
+    OperacionesDeComercioExteriorComponent
   ],
   imports: [
     CommonModule,
-    OperacionesDeComercioExteriorRoutingModule
+    OperacionesDeComercioExteriorRoutingModule,
+    SharedModule,
+    WizardComponent,
+    TituloComponent,
+    BtnContinuarComponent,
+    ReactiveFormsModule,
+    CrosslistComponent,
+    InputCheckComponent,
+    AlertComponent,
+    InputFechaComponent,
+    AnexarDocumentosComponent,
+    FirmaElectronicaComponent,
+    SolicitanteComponent,
+    InputRadioComponent,
+    CatalogoSelectComponent,
+    ToastrModule.forRoot(),
+    TablaDinamicaComponent
+    
+    
   ]
 })
 export class OperacionesDeComercioExteriorModule { }
