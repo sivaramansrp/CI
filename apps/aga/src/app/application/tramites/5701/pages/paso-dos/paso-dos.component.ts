a1d94dc31bffbedd4751f50fbabb8ac26d508aef
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-04-23 16:16:05 -0600

    Documentacion de las variables y funciones

--- apps/aga/src/app/application/tramites/5701/pages/paso-dos/paso-dos.component.ts
+++ apps/aga/src/app/application/tramites/5701/pages/paso-dos/paso-dos.component.ts
@@ -17,26 +17,80 @@ import { map } from 'rxjs';
   styleUrl: './paso-dos.component.scss'
 })
 export class PasoDosComponent implements OnInit {
+
+  /**
+   * Escucha el evento para cargar los documentos que se emite desde <solicitud-page>.
+   * @type {EventEmitter<void>}
+   */
   @Input() cargaArchivosEvento!: EventEmitter<void>;
+
+  /**
+   * Escucha el evento para regresar a la sección de cargar documentos que se emite desde <solicitud-page>.
+   * @type {EventEmitter<void>}
+   */
   @Input() regresarSeccionCargarDocumentoEvento!: EventEmitter<void>;
 
+  /**
+   * Propaga al componente <anexar-documentos> el evento para disparar el metodo confirmUpload en <anexar-documentos>.
+   */
   @Output() reenviarEvento = new EventEmitter<void>();
+
+  /**
+   * Propaga al componente <anexar-documentos> el evento para disparar el metodo mostrarSeccionCargaArchivosAccion() en <anexar-documentos>.
+   * Este evento se utiliza para regresar a la sección de carga de documentos.
+  */
   @Output() reenviarRegresarSeccion = new EventEmitter<void>();
+
+
+  /**
+   * Evento que se emite para indicar si existen documentos para cargar, y así activar el botón de "Cargar Archivos en <solicitud-page>".
+   * Este evento se utiliza para habilitar o deshabilitar el botón de carga de archivos en <solicitud-page>.
+   */
   @Output() reenviarEventoCarga = new EventEmitter<boolean>();
+
+
+  /**
+   * Evento que se emite para indicar si la carga de documentos se realizó correctamente.
+   * Este evento se utiliza para habilitar o deshabilitar el botón de "Continuar"  en <solicitud-page>.
+   */
   @Output() reenviarCargaRealizada = new EventEmitter<boolean>();
 
+  /**
+   * Referencia inyectada para gestionar la destrucción del componente y terminar las suscripciones.
+   * @type {DestroyRef}
+   */
   private destroyRef = inject(DestroyRef)
 
 
+  /**
+   * Texto de instrucciones para el usuario que se mostraran el alert.
+   * @type {string}
+   */
   TEXTOS = TEXTOS;
 
-  tiposDocumentos: CatalogoDocumento[] = [];
 
-  infoAlert = 'alert-info';
+  /**
+   * Catalogo de documentos obligatorios.
+   * @type {CatalogoDocumento[]}
+   */
   catalogoDocumentos: CatalogoDocumento[] = documentosObligatorios.documentosObligatorios;
+
+  /**
+   * Catalogo de documentos opcionales.
+   * @type {CatalogoDocumento[]}
+   */
   catalogoDocumentosOpcionales: CatalogoDocumento[] = documentosOpcionales.documentosOpcionales
+
+  /**
+   * Indica si la carga de documentos se realizó correctamente.
+   * @type {boolean}
+   */
   cargaRealizada = false;
 
+  /**
+   * Inicializa la variable de alertaNotificación con un objeto de tipo Notificacion.
+   * @type {Notificacion}
+   */
   public alertaNotificacion: Notificacion = {
     tipoNotificacion: 'banner',
     categoria: 'warning',
@@ -83,11 +137,21 @@ export class PasoDosComponent implements OnInit {
       });
   }
 
+  /**
+   * Actualiza el estado de carga de documentos y emite un evento con el nuevo valor.
+   * @param cargaRealizada Indica si la carga de documentos se realizó correctamente.
+   * @returns void
+   */
   documentosCargados(cargaRealizada: boolean): void {
     this.cargaRealizada = cargaRealizada;
     this.reenviarCargaRealizada.emit(this.cargaRealizada);
   }
 
+  /**
+   * Maneja el evento de carga de documentos y emite un evento con el estado.
+   * @param existenDocumentosParaCargar - Indica si hay documentos para cargar.
+   * @returns void
+   */
   manejarEventoCargaDocumento(existenDocumentosParaCargar: boolean): void {
     this.reenviarEventoCarga.emit(existenDocumentosParaCargar);
   }
