d65891de32c0a53b7eebebe427b07afd857fef9a
simrat-ilink <simratpal.gyani@ilink-systems.com>
2025-04-24 14:04:17 +0530

    procedure-setup

--- apps/sedena/src/app/application/app-routing.module.ts
+++ apps/sedena/src/app/application/app-routing.module.ts
@@ -95,7 +95,14 @@ const ROUTES: Routes = [
       import(
       './tramites/240117/permiso-ordinario-para-la-exportacion-de-sustancias-quimicas.module'
       ).then((m) => m.PermisoOrdinarioParaLaExportacionDeSustanciasQuimicasModule),
-  }
+  },
+  {
+    path: 'aviso-de-exportacion',
+    loadChildren: () =>
+      import(
+        './tramites/240123/aviso-de-exportacion.module'
+      ).then((m) => m.AvisoDeExportacionModule),
+  },
 ];
 
 @NgModule({
