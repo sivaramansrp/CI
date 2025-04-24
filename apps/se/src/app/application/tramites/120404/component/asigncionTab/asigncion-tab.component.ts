60a311d3442c6d607b5647185320ed680b1e637a
Moses Ramadoss <moses.ramadoss@ilink-systems.com>
2025-04-24 21:25:53 +0530

    'Refs#140101-build-fix'

--- apps/se/src/app/application/tramites/120404/component/asigncionTab/asigncion-tab.component.ts
+++ apps/se/src/app/application/tramites/120404/component/asigncionTab/asigncion-tab.component.ts
@@ -19,7 +19,7 @@ import { InputRadioComponent } from '@ng-mf/data-access-user';
 import { Subject, map, takeUntil } from 'rxjs';
 
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { SolicitanteasigncionserviceService } from '@libs/shared/data-access-user/src/core/services/120404/solicitanteAsigncionservice.service';
+import { SolicitanteasigncionserviceService } from '@libs/shared/data-access-user/src';
 
 import { Catalogo } from '@ng-mf/data-access-user';
 import { Tramite120404Query } from '../../estados/queries/tramite120404.query';
