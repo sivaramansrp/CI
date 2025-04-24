8d77ea790793cdc7e7cab6a3469a307cb9695300
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-24 13:31:25 +0530

    refactor: update PasoDocComponent constructor and method documentation for clarity

--- apps/aga/src/app/application/tramites/319/pages/paso-doc/paso-doc.component.ts
+++ apps/aga/src/app/application/tramites/319/pages/paso-doc/paso-doc.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paso-doc',
   templateUrl: './paso-doc.component.html',
   styleUrl: './paso-doc.component.scss',
 })
-export class PasoDocComponent {}
+export class PasoDocComponent {
+  /**
+   * Constructor del componente.
+   * @param router Servicio de enrutamiento.
+   */
+  constructor(private router: Router
+    
+  ) {
+    // Inicializa el componente.
+  }
+
+  /**
+   * Obtiene la firma y navega a la página de acuse si la firma es válida.
+   * @param {string} ev - La firma obtenida.
+   */
+  obtieneFirma(ev: string): void {
+    const FIRMA: string = ev;
+    if (FIRMA) {
+      this.router.navigate(['servicios-extraordinarios/acuse']);
+    }
+  }
+}
