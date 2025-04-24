4e8bc3fa24a05a525fe35297e30a435df7a6d0b9
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-24 13:23:21 +0530

    refactor: clean up SolicitudPageComponent documentation for clarity and consistency

--- apps/aga/src/app/application/tramites/319/pages/solicitud-page/solicitud-page.component.ts
+++ apps/aga/src/app/application/tramites/319/pages/solicitud-page/solicitud-page.component.ts
@@ -11,6 +11,49 @@ import { AccionBoton, DatosPasos, ListaPasosWizard, SeccionLibQuery, SeccionLibS
   templateUrl: './solicitud-page.component.html',
   styleUrl: './solicitud-page.component.scss',
 })
+/**
+ * @component
+ * @name SolicitudPageComponent
+ * @description
+ * Componente encargado de gestionar la lógica y el estado de la página de solicitud dentro del flujo de un wizard.
+ * Este componente interactúa con un store y un query para manejar el estado de la sección, y controla la navegación
+ * entre los pasos del wizard.
+ * 
+ * @usageNotes
+ * Este componente utiliza un `WizardComponent` para la navegación entre pasos y un `SeccionLibQuery` junto con un 
+ * `SeccionLibStore` para manejar el estado de la sección.
+ * 
+ * @example
+ * <app-solicitud-page></app-solicitud-page>
+ * 
+ * @property {ListaPasosWizard[]} pasos - Lista de pasos del wizard.
+ * @property {number} indice - Índice del paso actual.
+ * @property {SeccionLibState | undefined} seccion - Estado de la sección.
+ * @property {Subject<void>} destroyNotifier$ - Notificador para destruir observables.
+ * @property {WizardComponent} wizardComponent - Referencia al componente del wizard.
+ * @property {DatosPasos} datosPasos - Datos de configuración de los pasos del wizard.
+ * 
+ * @method ngOnInit
+ * Inicializa el componente y suscripciones necesarias para manejar el estado de la sección.
+ * 
+ * @method seleccionaTab
+ * Cambia el índice del paso actual del wizard.
+ * @param {number} i - Índice de la pestaña a seleccionar.
+ * 
+ * @method getValorIndice
+ * Actualiza el índice del paso actual basado en el evento recibido y navega al paso correspondiente.
+ * @param {AccionBoton} e - Evento con la acción y el valor del índice.
+ * 
+ * @method destroyNotifier$
+ * Método para limpiar las suscripciones al destruir el componente.
+ * 
+ * @method datosPasos
+ * Configura los textos y el número de pasos del wizard.
+ * 
+ * @constructor
+ * @param {SeccionLibQuery} seccionQuery - Consulta de la sección.
+ * @param {SeccionLibStore} seccionStore - Almacenamiento de la sección.
+ */
 export class SolicitudPageComponent implements OnInit{
   /**
      * Lista de pasos del wizard.
