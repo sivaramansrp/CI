e8e8871eb33f7636566a9fd8ba42e279291d2b01
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-04-24 12:48:16 +0530

    init procedure

---
+++ apps/se/src/app/application/tramites/80303/modificacion-programa-immex-baja-submanufacturera.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModificacionProgramaImmexBajaSubmanufactureraRoutingModule } from './modificacion-programa-immex-baja-submanufacturera-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModificacionProgramaImmexBajaSubmanufactureraRoutingModule
+  ]
+})
+export class ModificacionProgramaImmexBajaSubmanufactureraModule { }
