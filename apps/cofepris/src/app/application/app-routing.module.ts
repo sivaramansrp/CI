57c0ff4257dbcb2ffb83f853b7b4b1d6c3aa8fea
navin-ilink <navin.kumar@ilink-system.com>
2025-04-25 10:16:23 +0530

    resolved conflict

--- apps/cofepris/src/app/application/app-routing.module.ts
+++ apps/cofepris/src/app/application/app-routing.module.ts
@@ -467,6 +467,13 @@ const ROUTES: Routes = [
     import(
       './tramites/260302/exportacion-materias-primas-estupefacientes.module'
     ).then((m) => m.ExportacionMateriasPrimasEstupefacientesModule),
+},
+{
+  path: 'sustancias-permitidas',
+  loadChildren: () =>
+    import('./tramites/260515/permit-de-substances.module').then(
+      (m) => m.PermitDeSubstancesModule
+    )
 }
 ];
 
