0a6d333a27649eddf7daa1c991dc1dff9c0164b4
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-25 13:16:32 +0530

    refactor: update PASOS constant documentation and correct index for 'Firmar solicitud'

--- apps/aga/src/app/application/tramites/319/constantes/operaciones-de-comercio-exterior.enum.ts
+++ apps/aga/src/app/application/tramites/319/constantes/operaciones-de-comercio-exterior.enum.ts
@@ -1,3 +1,12 @@
+/**
+ * @const PASOS
+ * @description Arreglo que define los pasos de un proceso en una solicitud de operaciones de comercio exterior.
+ * Cada paso incluye un índice, un título descriptivo, y los estados de actividad y completitud.
+ * @property {number} indice - Número que identifica el orden del paso.
+ * @property {string} titulo - Descripción del paso.
+ * @property {boolean} activo - Indica si el paso está activo para ser realizado.
+ * @property {boolean} completado - Indica si el paso ya ha sido completado.
+ */
 export const PASOS = [
     {
       indice: 1,
@@ -6,10 +15,17 @@ export const PASOS = [
       completado: true,
     },
     {
-      indice: 3,
+      indice: 2,
       titulo: 'Firmar solicitud',
       activo: false,
       completado: false,
     },
   ];
+  /**
+   * @const URL
+   * @description Ruta relativa a los archivos JSON utilizados para las operaciones de comercio exterior.
+   * @type {string}
+   * @compodoc
+   * Esta constante define la ubicación de los recursos JSON específicos para el trámite 319.
+   */
   export const URL = '../../../../../assets/json/319/';
