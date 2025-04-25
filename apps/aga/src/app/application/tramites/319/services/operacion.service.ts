c5fc0c876728df407170deac7669dc39344b2c6d
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-25 13:21:55 +0530

    refactor: enhance documentation and structure in OperacionService

--- apps/aga/src/app/application/tramites/319/services/operacion.service.ts
+++ apps/aga/src/app/application/tramites/319/services/operacion.service.ts
@@ -9,21 +9,61 @@ import { URL } from '../constantes/operaciones-de-comercio-exterior.enum';
 import { Personas } from '../models/personas.module';
 
 
+/**
+ * Servicio para realizar operaciones relacionadas con la obtención de datos desde el backend.
+ * Proporciona métodos para obtener listas de catálogos y personas desde un archivo específico.
+ * 
+ * @example
+ * // Ejemplo de uso:
+ * const fileName = 'catalogos.json';
+ * operacionService.obtenerSelectorList(fileName).subscribe(catalogos => {
+ *   console.log(catalogos);
+ * });
+ * 
+ * @example
+ * const fileName = 'personas.json';
+ * operacionService.obtenerTablerList(fileName).subscribe(personas => {
+ *   console.log(personas);
+ * });
+ * 
+ * @injectable
+ * Este servicio está disponible en el nivel raíz del módulo.
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class OperacionService {
+  /**
+   * URL base para las solicitudes HTTP.
+   */
   url: string = URL;
-  constructor(private readonly http: HttpClient
-  ) {
-    // Constructor logic can be added here if needed
-  }
+
+  /**
+   * Constructor del servicio.
+   * 
+   * @param http - Cliente HTTP para realizar solicitudes al backend.
+   */
+  constructor(private readonly http: HttpClient) {}
+
+  /**
+   * Obtiene una lista de catálogos desde un archivo específico.
+   * 
+   * @param fileName - Nombre del archivo que contiene los datos del catálogo.
+   * @returns Un observable que emite una lista de catálogos.
+   */
   obtenerSelectorList(fileName: string): Observable<Catalogo[]> {
     const BASEURL = this.url + fileName;
     return this.http.get<RespuestaCatalogos>(BASEURL).pipe(
       map(response => response.data)
     );
   }
+
+  /**
+   * Obtiene una lista de personas desde un archivo específico.
+   * 
+   * @param fileName - Nombre del archivo que contiene los datos de las personas.
+   * @returns Un observable que emite una lista de personas.
+   */
   obtenerTablerList(fileName: string): Observable<Personas[]> {
     const BASEURL = this.url + fileName;
     return this.http.get<Personas[]>(BASEURL).pipe(
