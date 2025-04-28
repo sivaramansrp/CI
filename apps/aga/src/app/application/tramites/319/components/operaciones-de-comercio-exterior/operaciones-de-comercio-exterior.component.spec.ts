15ab27fd2054a76eebae1387d1ace98ccca746ec
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-04-28 15:32:32 +0530

    Add unit tests for OperacionesDeComercioExteriorComponent

--- apps/aga/src/app/application/tramites/319/components/operaciones-de-comercio-exterior/operaciones-de-comercio-exterior.component.spec.ts
+++ apps/aga/src/app/application/tramites/319/components/operaciones-de-comercio-exterior/operaciones-de-comercio-exterior.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { OperacionesDeComercioExteriorComponent } from './operaciones-de-comercio-exterior.component';
+import { CommonModule } from '@angular/common';
+import { CatalogoSelectComponent, SharedModule, TablaDinamicaComponent, TituloComponent } from '@ng-mf/data-access-user';  // Import shared modules if needed
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { AlertComponent } from 'ngx-bootstrap/alert';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('OperacionesDeComercioExteriorComponent', () => {
   let component: OperacionesDeComercioExteriorComponent;
@@ -7,7 +12,8 @@ describe('OperacionesDeComercioExteriorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OperacionesDeComercioExteriorComponent],
+      imports: [CommonModule, SharedModule,HttpClientTestingModule,TablaDinamicaComponent,CatalogoSelectComponent,AlertComponent,TituloComponent,ReactiveFormsModule],  // Import necessary modules here
+      declarations: [OperacionesDeComercioExteriorComponent],  // Declare the component in the declarations array
     }).compileComponents();
 
     fixture = TestBed.createComponent(OperacionesDeComercioExteriorComponent);
