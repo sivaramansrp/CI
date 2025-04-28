a00865e050dc20d6f0bac41cf276c5c2c3dbbf4a
sravanimeegada <sravani.meegada@ilink-systems.com>
2025-04-28 21:04:08 +0530

    Dev done

---
+++ apps/amecafe/src/app/application/tramites/290301/components/datos-de-la-solicitud/datos-de-la-solicitud.component.ts
@@ -0,0 +1,194 @@
+import { BENEFICIOS_TABLA, BODEGAS_TABLA, CAFE_EXPORTADORES_TABLA, RADIO_OPCION, REGIONES_TABLA } from '../../constants/constants.enum';
+import { BeneficiosData, BodegasData, CafeExportadoresData, RegionesData } from '../../models/filadata.model';
+import { InputRadioComponent, TablaDinamicaComponent, TablaSeleccion, TituloComponent } from '@libs/shared/data-access-user/src';
+import { ReplaySubject, Subscription, map, takeUntil } from 'rxjs';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Solicitud290301State, Solicitud290301Store } from '../../estados/tramite290301.store';
+
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { InputCheckComponent } from '@libs/shared/data-access-user/src';
+
+import { NacionalRegistroDelCafeExportadoresService } from '../../services/nacional-registro-del-cafe-exportadores.service';
+import { Solicitud290301Query } from '../../estados/tramite290301.query';
+@Component({
+  selector: 'app-datos-de-la-solicitud',
+  standalone: true,
+  imports: [CommonModule, TituloComponent, ReactiveFormsModule, InputRadioComponent, TablaDinamicaComponent, InputCheckComponent],
+  templateUrl: './datos-de-la-solicitud.component.html',
+  styleUrl: './datos-de-la-solicitud.component.scss',
+})
+export class DatosDeLaSolicitudComponent implements OnInit, OnDestroy {
+
+  /** Formulario reactivo para los datos de la solicitud */
+  datosSolicitudForma!: FormGroup;
+
+  /** Opciones de radio para el formulario */
+  radioOpcion = RADIO_OPCION;
+
+  /** Lista de suscripciones activas */
+  private subscriptions: Subscription[] = [];
+
+  /** Tabla de regiones */
+  regionesTabla = REGIONES_TABLA;
+
+  /** Tabla de beneficios */
+  beneficiosTabla = BENEFICIOS_TABLA;
+
+  /** Tabla de bodegas */
+  bodegasTabla = BODEGAS_TABLA;
+
+  /** Tabla de exportadores de café */
+  cafeExportadoresTabla = CAFE_EXPORTADORES_TABLA;
+
+  /** Tipo de selección para las tablas */
+  tipoSeleccion: TablaSeleccion = TablaSeleccion.CHECKBOX;
+
+  /** Datos de la tabla de regiones */
+  regionesTableDatos: RegionesData[] = [];
+
+  /** Datos de la tabla de beneficios */
+  beneficiosTableDatos: BeneficiosData[] = [];
+
+  /** Datos de la tabla de bodegas */
+  bodegasTableDatos: BodegasData[] = [];
+
+  /** Datos de la tabla de exportadores de café */
+  cafeExportadoresTableDatos: CafeExportadoresData[] = [];
+
+  /** Estado de la solicitud 290301 */
+  solicitud290301State: Solicitud290301State = {} as Solicitud290301State;
+
+  /** Estado de los datos de la solicitud */
+  dataDeLaSolicitudState!: Solicitud290301State;
+
+  /** Observable para manejar la destrucción del componente */
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+
+  constructor(
+    /** Constructor para inicializar servicios y dependencias */
+    private fb: FormBuilder,
+    private nacionalRegistroDelCafeExportadoresService: NacionalRegistroDelCafeExportadoresService,
+    public solicitud290301Store: Solicitud290301Store,
+    public solicitud290301Query: Solicitud290301Query
+  ) {}
+
+  /** Método que se ejecuta al inicializar el componente */
+  ngOnInit(): void {
+    this.solicitud290301Query.selectSolicitud$
+      .pipe(
+        takeUntil(this.destroyed$),
+        map((seccionState: Solicitud290301State) => {
+          this.dataDeLaSolicitudState = seccionState;
+        })
+      )
+      .subscribe();
+
+    this.createForm();
+    this.subscribeToProductorDeCafeChanges();
+    this.getRegionsData();
+    this.getBeneficiosData();
+    this.getBodegasData();
+    this.getCafeExportadoresData();
+  }
+
+  /** Método para crear el formulario reactivo */
+  createForm(): void {
+    this.datosSolicitudForma = this.fb.group({
+      justificacion: [this.dataDeLaSolicitudState?.justificacion],
+      productorDeCafe: [this.dataDeLaSolicitudState?.productorDeCafe],
+      claveDelPadron: [{ value: this.dataDeLaSolicitudState?.claveDelPadron || '', disabled: false }],
+      observaciones: [this.dataDeLaSolicitudState?.observaciones],
+      requiereInspeccionInmediata: [this.dataDeLaSolicitudState?.requiereInspeccionInmediata],
+      informacionConfidencial: [this.dataDeLaSolicitudState?.informacionConfidencial],
+    });
+  }
+
+  /** Método para suscribirse a los cambios en el campo "productorDeCafe" */
+  private subscribeToProductorDeCafeChanges(): void {
+    const PRODUCTOR_DE_CAFE_CONTROL = this.datosSolicitudForma.get('productorDeCafe');
+    const CLAVE_DEL_PADRON_CONTROL = this.datosSolicitudForma.get('claveDelPadron');
+
+    if (PRODUCTOR_DE_CAFE_CONTROL?.value === 'false') {
+      CLAVE_DEL_PADRON_CONTROL?.disable();
+    } else {
+      CLAVE_DEL_PADRON_CONTROL?.enable();
+    }
+
+    const EXENTO_DE_PAGO_SUBSCRIPTION = this.datosSolicitudForma.get('productorDeCafe')?.valueChanges.subscribe((value) => {
+      if (value === 'false') {
+        this.datosSolicitudForma.get('claveDelPadron')?.disable();
+      } else {
+        this.datosSolicitudForma.get('claveDelPadron')?.enable();
+      }
+    });
+
+    if (EXENTO_DE_PAGO_SUBSCRIPTION) {
+      this.subscriptions.push(EXENTO_DE_PAGO_SUBSCRIPTION);
+    }
+  }
+
+  /** Método para obtener los datos de las regiones */
+  getRegionsData(): void {
+    this.nacionalRegistroDelCafeExportadoresService
+      .getRegionsData()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data) => {
+        this.regionesTableDatos = data as RegionesData[];
+      });
+  }
+
+  /** Método para obtener los datos de los beneficios */
+  getBeneficiosData(): void {
+    this.nacionalRegistroDelCafeExportadoresService
+      .getBeneficiosData()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data) => {
+        this.beneficiosTableDatos = data as BeneficiosData[];
+      });
+  }
+
+  /** Método para obtener los datos de las bodegas */
+  getBodegasData(): void {
+    this.nacionalRegistroDelCafeExportadoresService
+      .getBodegasData()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data) => {
+        this.bodegasTableDatos = data as BodegasData[];
+      });
+  }
+
+  /** Método para obtener los datos de los exportadores de café */
+  getCafeExportadoresData(): void {
+    this.nacionalRegistroDelCafeExportadoresService
+      .getCafeExportadoresData()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((data) => {
+        this.cafeExportadoresTableDatos = data as CafeExportadoresData[];
+      });
+  }
+
+  /**
+   * Método para establecer valores en el store
+   * @param form Formulario reactivo
+   * @param campo Campo del formulario
+   * @param metodoNombre Método del store
+   */
+  setValoresStore(
+    form: FormGroup,
+    campo: string,
+    metodoNombre: keyof Solicitud290301Store
+  ): void {
+    const VALOR = form.get(campo)?.value;
+    (this.solicitud290301Store[metodoNombre] as (value: unknown) => void)(VALOR);
+  }
+
+  /** Método que se ejecuta al destruir el componente */
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+}
--- apps/amecafe/src/app/application/tramites/290301/services/nacional-registro-del-cafe-exportadores.service.ts
+++ apps/amecafe/src/app/application/tramites/290301/services/nacional-registro-del-cafe-exportadores.service.ts
@@ -11,21 +11,35 @@ export class NacionalRegistroDelCafeExportadoresService {
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Obtiene los datos de las regiones desde un archivo JSON
+   * @returns Observable con la lista de datos de las regiones
+   */
   getRegionsData(): Observable<RegionesData[]> {
       return this.http.get<RegionesData[]>('./assets/json/290301/regionesdecompraData.json');
     }
 
+  /**
+   * Obtiene los datos de los beneficios desde un archivo JSON
+   * @returns Observable con la lista de datos de los beneficios
+   */
   getBeneficiosData(): Observable<BeneficiosData[]> {
       return this.http.get<BeneficiosData[]>('./assets/json/290301/beneficiosData.json');
     }
 
+  /**
+   * Obtiene los datos de las bodegas desde un archivo JSON
+   * @returns Observable con la lista de datos de las bodegas
+   */
   getBodegasData(): Observable<BodegasData[]> {
       return this.http.get<BodegasData[]>('./assets/json/290301/bodegasData.json');
     }
    
+  /**
+   * Obtiene los datos de los exportadores de café desde un archivo JSON
+   * @returns Observable con la lista de datos de los exportadores de café
+   */
   getCafeExportadoresData(): Observable<CafeExportadoresData[]> {
       return this.http.get<CafeExportadoresData[]>('./assets/json/290301/cafedeexportacionData.json');
     }
-
-
 }
