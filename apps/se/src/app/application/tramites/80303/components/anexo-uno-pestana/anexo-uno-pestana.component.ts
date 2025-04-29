22affb5f1c2885194c4a6f3602832518a1d1de9c
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-04-29 15:29:29 +0530

    services

--- apps/se/src/app/application/tramites/80303/components/anexo-uno-pestana/anexo-uno-pestana.component.ts
+++ apps/se/src/app/application/tramites/80303/components/anexo-uno-pestana/anexo-uno-pestana.component.ts
@@ -1,9 +1,22 @@
-import { AnexoExportacion, AnexoImportacion, Sensible } from '../../models/complementaria.model';
-import { CONFIGURACION_ANEXO_EXPORTACION, CONFIGURACION_ANEXO_IMPORTACION, CONFIGURACION_SENSIBLES } from '../../constants/complementaria.enum';
-import { ConfiguracionColumna, TablaDinamicaComponent, TablaSeleccion, TituloComponent } from '@libs/shared/data-access-user/src';
+import {
+  AnexoExportacion,
+  AnexoImportacion,
+  Sensible,
+} from '../../models/complementaria.model';
+import {
+  CONFIGURACION_ANEXO_EXPORTACION,
+  CONFIGURACION_ANEXO_IMPORTACION,
+  CONFIGURACION_SENSIBLES,
+} from '../../constants/complementaria.enum';
+import {
+  ConfiguracionColumna,
+  TablaDinamicaComponent,
+  TablaSeleccion,
+  TituloComponent,
+} from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ModificacionProgramaImmexBajaSubmanufactureraService } from '../../service/modificacion-programa-immex-baja-submanufacturera.service';
+import { ModificacionProgramaImmexBajaSubmanufactureraService } from '../../services/modificacion-programa-immex-baja-submanufacturera.service';
 
 @Component({
   selector: 'app-anexo-uno-pestana',
@@ -13,35 +26,50 @@ import { ModificacionProgramaImmexBajaSubmanufactureraService } from '../../serv
   styleUrl: './anexo-uno-pestana.component.scss',
 })
 export class AnexoUnoPestanaComponent {
-
   /**
    * Tipo de selección de la tabla.
    * @property {TablaSeleccion} tablaSeleccion
    */
   public tablaSeleccion: TablaSeleccion = TablaSeleccion.UNDEFINED;
 
-  public configuracionAnexoExportacionTabla: ConfiguracionColumna<AnexoExportacion>[] = CONFIGURACION_ANEXO_EXPORTACION;
+  public configuracionAnexoExportacionTabla: ConfiguracionColumna<AnexoExportacion>[] =
+    CONFIGURACION_ANEXO_EXPORTACION;
 
   public anexoExportacionTablaDatos: AnexoExportacion[] = [];
 
   public anexoExportacionTablaSeleccionada: AnexoExportacion[] = [];
 
-  public configuracionAnexoImportacionTabla: ConfiguracionColumna<AnexoImportacion>[] = CONFIGURACION_ANEXO_IMPORTACION;
+  public configuracionAnexoImportacionTabla: ConfiguracionColumna<AnexoImportacion>[] =
+    CONFIGURACION_ANEXO_IMPORTACION;
 
   public anexoImportacionTablaDatos: AnexoImportacion[] = [];
 
   public anexoImportacionTablaSeleccionada: AnexoImportacion[] = [];
 
-  public configuracionSensiblesTabla: ConfiguracionColumna<Sensible>[] = CONFIGURACION_SENSIBLES;
+  public configuracionSensiblesTabla: ConfiguracionColumna<Sensible>[] =
+    CONFIGURACION_SENSIBLES;
 
   public sensiblesTablaDatos: Sensible[] = [];
 
   public sensiblesTablaSeleccionada: Sensible[] = [];
 
-  constructor(public modificacionProgramaImmexBajaSubmanufactureraService: ModificacionProgramaImmexBajaSubmanufactureraService) {
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'anexoExportacionTablaDatos', '/80303/anexoExportacion.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'anexoImportacionTablaDatos', '/80303/anexoImportacion.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'sensiblesTablaDatos', '/80303/sensible.json');
+  constructor(
+    public modificacionProgramaImmexBajaSubmanufactureraService: ModificacionProgramaImmexBajaSubmanufactureraService
+  ) {
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'anexoExportacionTablaDatos',
+      '/80303/anexoExportacion.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'anexoImportacionTablaDatos',
+      '/80303/anexoImportacion.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'sensiblesTablaDatos',
+      '/80303/sensible.json'
+    );
   }
-
 }
--- apps/se/src/app/application/tramites/80303/components/complementaria/complementaria.component.ts
+++ apps/se/src/app/application/tramites/80303/components/complementaria/complementaria.component.ts
@@ -1,9 +1,28 @@
-import { CONFIGURACION_CONTRIBUYENTES_ACCIONISTAS, CONFIGURACION_EMPRESAS_SUBMANUFACTURERAS, CONFIGURACION_FEDERATARIOS, CONFIGURACION_FEDERATARIOS_DOMICILIO, CONFIGURACION_PLANTAS_MANUFACTURERAS, CONFIGURACION_SERVICIOS_IMMEX } from '../../constants/complementaria.enum';
-import { ConfiguracionColumna, TablaDinamicaComponent, TablaSeleccion, TituloComponent } from '@libs/shared/data-access-user/src';
-import { DatosContribuyente, DatosEmpresaSubmanufacturera, DatosPlantaManufacturera, Federatario, FederatarioRealizaranLasOperaciones, ServicioImmex } from '../../models/complementaria.model';
+import {
+  CONFIGURACION_CONTRIBUYENTES_ACCIONISTAS,
+  CONFIGURACION_EMPRESAS_SUBMANUFACTURERAS,
+  CONFIGURACION_FEDERATARIOS,
+  CONFIGURACION_FEDERATARIOS_DOMICILIO,
+  CONFIGURACION_PLANTAS_MANUFACTURERAS,
+  CONFIGURACION_SERVICIOS_IMMEX,
+} from '../../constants/complementaria.enum';
+import {
+  ConfiguracionColumna,
+  TablaDinamicaComponent,
+  TablaSeleccion,
+  TituloComponent,
+} from '@libs/shared/data-access-user/src';
+import {
+  DatosContribuyente,
+  DatosEmpresaSubmanufacturera,
+  DatosPlantaManufacturera,
+  Federatario,
+  FederatarioRealizaranLasOperaciones,
+  ServicioImmex,
+} from '../../models/complementaria.model';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ModificacionProgramaImmexBajaSubmanufactureraService } from '../../service/modificacion-programa-immex-baja-submanufacturera.service';
+import { ModificacionProgramaImmexBajaSubmanufactureraService } from '../../services/modificacion-programa-immex-baja-submanufacturera.service';
 import { signal } from '@angular/core';
 
 @Component({
@@ -15,82 +34,112 @@ import { signal } from '@angular/core';
 })
 export class ComplementariaComponent {
   public certificacionSAT$ = signal('Sí');
-  
+
   /**
    * Tipo de selección de la tabla.
    * @property {TablaSeleccion} tablaSeleccion
    */
   public tablaSeleccion: TablaSeleccion = TablaSeleccion.UNDEFINED;
 
-  public configuracionAccionistasTabla: ConfiguracionColumna<DatosContribuyente>[] = CONFIGURACION_CONTRIBUYENTES_ACCIONISTAS;
+  public configuracionAccionistasTabla: ConfiguracionColumna<DatosContribuyente>[] =
+    CONFIGURACION_CONTRIBUYENTES_ACCIONISTAS;
 
   public accionistasTablaDatos: DatosContribuyente[] = [];
 
   public accionistasTablaSeleccionada: DatosContribuyente[] = [];
 
-
-  public configuracionFederatariosTabla: ConfiguracionColumna<Federatario>[] = CONFIGURACION_FEDERATARIOS;
+  public configuracionFederatariosTabla: ConfiguracionColumna<Federatario>[] =
+    CONFIGURACION_FEDERATARIOS;
 
   public federatariosTablaDatos: Federatario[] = [];
 
   public federatariosTablaSeleccionada: Federatario[] = [];
 
-
-  public configuracionPlantasIMMEX: ConfiguracionColumna<FederatarioRealizaranLasOperaciones>[] = CONFIGURACION_FEDERATARIOS_DOMICILIO;
+  public configuracionPlantasIMMEX: ConfiguracionColumna<FederatarioRealizaranLasOperaciones>[] =
+    CONFIGURACION_FEDERATARIOS_DOMICILIO;
 
   public plantasIMMEXDatos: FederatarioRealizaranLasOperaciones[] = [];
 
   public plantasIMMEXSeleccionada: FederatarioRealizaranLasOperaciones[] = [];
 
   /**
- * Configuración de columnas para la tabla de empresas submanufactureras.
- * Define cómo se mostrarán los datos en la tabla dinámica.
- */
-public configuracionEmpresasSubmanufacturerasTabla: ConfiguracionColumna<DatosEmpresaSubmanufacturera>[] = CONFIGURACION_EMPRESAS_SUBMANUFACTURERAS;
+   * Configuración de columnas para la tabla de empresas submanufactureras.
+   * Define cómo se mostrarán los datos en la tabla dinámica.
+   */
+  public configuracionEmpresasSubmanufacturerasTabla: ConfiguracionColumna<DatosEmpresaSubmanufacturera>[] =
+    CONFIGURACION_EMPRESAS_SUBMANUFACTURERAS;
 
-/**
- * Arreglo que contiene los datos que se mostrarán en la tabla de empresas submanufactureras.
- */
-public empresasSubmanufacturerasTablaDatos: DatosEmpresaSubmanufacturera[] = [];
+  /**
+   * Arreglo que contiene los datos que se mostrarán en la tabla de empresas submanufactureras.
+   */
+  public empresasSubmanufacturerasTablaDatos: DatosEmpresaSubmanufacturera[] =
+    [];
 
-/**
- * Arreglo que almacena las filas seleccionadas por el usuario desde la tabla dinámica.
- * Se actualiza cada vez que el usuario selecciona una fila.
- */
-public empresasSubmanufacturerasTablaSeleccionada: DatosEmpresaSubmanufacturera[] = [];
+  /**
+   * Arreglo que almacena las filas seleccionadas por el usuario desde la tabla dinámica.
+   * Se actualiza cada vez que el usuario selecciona una fila.
+   */
+  public empresasSubmanufacturerasTablaSeleccionada: DatosEmpresaSubmanufacturera[] =
+    [];
 
-/**
- * Configuración de columnas para la tabla de plantas manufactureras.
- */
-public configuracionPlantasManufacturerasTabla: ConfiguracionColumna<DatosPlantaManufacturera>[] = CONFIGURACION_PLANTAS_MANUFACTURERAS;
+  /**
+   * Configuración de columnas para la tabla de plantas manufactureras.
+   */
+  public configuracionPlantasManufacturerasTabla: ConfiguracionColumna<DatosPlantaManufacturera>[] =
+    CONFIGURACION_PLANTAS_MANUFACTURERAS;
 
-/**
- * Datos que se mostrarán en la tabla de plantas manufactureras.
- */
-public plantasManufacturerasTablaDatos: DatosPlantaManufacturera[] = [];
+  /**
+   * Datos que se mostrarán en la tabla de plantas manufactureras.
+   */
+  public plantasManufacturerasTablaDatos: DatosPlantaManufacturera[] = [];
 
-/**
- * Filas seleccionadas por el usuario en la tabla de plantas manufactureras.
- */
-public plantasManufacturerasTablaSeleccionada: DatosPlantaManufacturera[] = [];
+  /**
+   * Filas seleccionadas por el usuario en la tabla de plantas manufactureras.
+   */
+  public plantasManufacturerasTablaSeleccionada: DatosPlantaManufacturera[] =
+    [];
 
-public configuracionServiciosImmexTabla: ConfiguracionColumna<ServicioImmex>[] = CONFIGURACION_SERVICIOS_IMMEX;
+  public configuracionServiciosImmexTabla: ConfiguracionColumna<ServicioImmex>[] =
+    CONFIGURACION_SERVICIOS_IMMEX;
 
-public serviciosImmexTablaDatos: ServicioImmex[] = [];
+  public serviciosImmexTablaDatos: ServicioImmex[] = [];
 
-public serviciosImmexTablaSeleccionada: ServicioImmex[] = [];
+  public serviciosImmexTablaSeleccionada: ServicioImmex[] = [];
 
-  constructor(public modificacionProgramaImmexBajaSubmanufactureraService: ModificacionProgramaImmexBajaSubmanufactureraService) {
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'accionistasTablaDatos', '/80303/accionistasTablaDatos.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'federatariosTablaDatos', '/80303/federatariosTablaDatos.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'plantasIMMEXDatos', '/80303/plantasIMMEXDatos.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'empresasSubmanufacturerasTablaDatos', '/80303/empresasSubmanufacturerasTablaDatos.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'plantasManufacturerasTablaDatos', '/80303/plantasManufacturerasTablaDatos.json');
-    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(this, 'serviciosImmexTablaDatos', '/80303/serviciosImmexTablaDatos.json');
-
-
+  constructor(
+    public modificacionProgramaImmexBajaSubmanufactureraService: ModificacionProgramaImmexBajaSubmanufactureraService
+  ) {
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'accionistasTablaDatos',
+      '/80303/accionistasTablaDatos.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'federatariosTablaDatos',
+      '/80303/federatariosTablaDatos.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'plantasIMMEXDatos',
+      '/80303/plantasIMMEXDatos.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'empresasSubmanufacturerasTablaDatos',
+      '/80303/empresasSubmanufacturerasTablaDatos.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'plantasManufacturerasTablaDatos',
+      '/80303/plantasManufacturerasTablaDatos.json'
+    );
+    this.modificacionProgramaImmexBajaSubmanufactureraService.obtenerRespuestaPorUrl(
+      this,
+      'serviciosImmexTablaDatos',
+      '/80303/serviciosImmexTablaDatos.json'
+    );
   }
-
 
   onInputChange(event: Event): void {
     const INPUT_ELEMENT = event.target as HTMLInputElement;
