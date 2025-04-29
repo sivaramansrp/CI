87186736e63877b4c60e8a4cbebe864a1bb84d2f
Mkushwah1001 <madhu.kushwah@ilink-systems.com>
2025-04-29 15:31:48 +0530

    90302- code clean up

--- apps/se/src/app/application/tramites/90302/components/bitacora/bitacora.component.ts
+++ apps/se/src/app/application/tramites/90302/components/bitacora/bitacora.component.ts
@@ -1,37 +1,12 @@
-/**
- * @fileoverview
- * El `Ampliacion3RsComponent` es un componente de Angular diseñado para gestionar la funcionalidad del módulo "Ampliación de Servicios".
- * Maneja formularios reactivos, catálogos y la interacción con el estado para la gestión de datos relacionados con sectores y servicios.
- * 
- * @module Ampliacion3RsComponent
- * @description
- * Este componente proporciona funcionalidad para la ampliación de servicios, incluyendo la inicialización de formularios, 
- * la obtención de datos y la interacción con el estado para la gestión de sectores y reglas.
- */
-
-import {
-  Catalogo,
-  TablaSeleccion,
-} from '@ng-mf/data-access-user';
-
-import {
-  FormBuilder,
-  FormGroup,
-} from '@angular/forms';
 import { OnDestroy, OnInit } from '@angular/core';
-import { map, takeUntil } from 'rxjs/operators';
-import { AmpliacionServiciosQuery } from '../../estados/tramite90302.query';
+import { takeUntil } from 'rxjs/operators';
 import { AmpliacionServiciosService } from '../../services/ampliacion-servicios.service';
-import { AmpliacionServiciosState } from '../../estados/tramite90302.store';
 import { CONFIGURACION_BITCORA, CONFIGURACION_MERCANCIAS_A_PRODUCIR, CONFIGURACION_PLANTAS, CONFIGURACION_PRODUCTOR_INDIRECTO, CONFIGURACION_SECTOR, CONFIGURACION_SECTOR1 } from "../../constantes/modificacion.constants";
 import { Component,Input } from '@angular/core';
 import { ConfiguracionColumna } from '../../models/configuracion-columna.model';
-import { HttpClient } from '@angular/common/http';
-import { Sector } from "../../models/datos-info.model";
 import { Bitacora,MercanciasAProducir, Plantas,ProductorIndirecto,Sector1} from "../../models/datos-info.model";
-
 import { Subject } from 'rxjs';
-import { Tramite90302Store } from '../../estados/tramite90302.store';
+
 
 @Component({
   selector: 'app-bitacora',
@@ -39,36 +14,10 @@ import { Tramite90302Store } from '../../estados/tramite90302.store';
   styleUrl: './bitacora.component.scss',
 })
 export class BitacoraComponent implements OnInit, OnDestroy {
-  /**
-   * Indica si una regla ha sido seleccionada.
-   * @property {boolean} isSelectedRegla
-   */
-  isSelectedRegla: boolean = false;
+ 
   
 @Input() esDeSolicitante: boolean = false; 
-  /**
-   * Lista de sectores recibidos.
-   * @property {Sector[]} recibioSector
-   */
-  recibioSector: Sector[] = [];
-
-  /**
-   * Formulario reactivo para la información de registro.
-   * @property {FormGroup} formularioInfoRegistro
-   */
-  formularioInfoRegistro!: FormGroup;
-
-  /**
-   * Tipo de selección de tabla (checkbox).
-   * @property {TablaSeleccion} tablaSeleccion
-   */
-  tablaSeleccion: TablaSeleccion = TablaSeleccion.CHECKBOX;
-
-  /**
-   * Configuración de la tabla para sectores.
-   * @property {ConfiguracionColumna<Sector>[]} configuracionTablaSector
-   */
-  configuracionTablaSector: ConfiguracionColumna<Sector>[] = CONFIGURACION_SECTOR;
+  
 
   configuracionTablaBitacora: ConfiguracionColumna<Bitacora>[] = CONFIGURACION_BITCORA;
 
@@ -78,11 +27,7 @@ export class BitacoraComponent implements OnInit, OnDestroy {
   configuracionTablaProductor: ConfiguracionColumna<ProductorIndirecto>[] = CONFIGURACION_PRODUCTOR_INDIRECTO;
   configuracionTablaSector1: ConfiguracionColumna<Sector1>[] = CONFIGURACION_SECTOR1;
 
-  /**
-   * Lista de datos de sectores.
-   * @property {Sector[]} datosSector
-   */
-  datosSector: Sector[] = [];
+  
 
   datosBitacora: Bitacora[] = [];
   datosMercancias: MercanciasAProducir[] = [];
@@ -90,35 +35,6 @@ export class BitacoraComponent implements OnInit, OnDestroy {
   datosProductor: ProductorIndirecto[] = [];
   datosSector1: Sector1[] = [];
 
-  /**
-   * Lista de domicilios seleccionados.
-   * @property {Sector[]} domiciliosSeleccionados
-   */
-  domiciliosSeleccionados: Sector[] = [];
-
-  /**
-   * Formulario reactivo para datos adicionales.
-   * @property {FormGroup} forma
-   */
-  forma!: FormGroup;
-
-  /**
-   * Lista de reglas seleccionadas.
-   * @property {Catalogo[]} reglaSeleccionada
-   */
-  reglaSeleccionada!: Catalogo[];
-
-  /**
-   * Lista desplegable de sectores.
-   * @property {Catalogo[]} sectorDesplegable
-   */
-  sectorDesplegable!: Catalogo[];
-
-  /**
-   * Estado actual del trámite.
-   * @property {AmpliacionServiciosState} tramiteState
-   */
-  tramiteState: AmpliacionServiciosState = {} as AmpliacionServiciosState;
 
   /**
    * Notificador para gestionar la destrucción o desuscripción de observables.
@@ -134,13 +50,9 @@ export class BitacoraComponent implements OnInit, OnDestroy {
    * @param {HttpClient} httpServicios - Servicio HTTP para realizar peticiones.
    */
   constructor(
-    private fb: FormBuilder,
     private ampliacionServiciosService: AmpliacionServiciosService,
-    private ampliacionServiciosQuery: AmpliacionServiciosQuery,
-    private tramite80206Store: Tramite90302Store,
-    private readonly httpServicios: HttpClient
   ) {
-    this.inicializarFormularioInfoRegistro();
+    // No se necesita lógica de inicialización adicional.
   }
 
   /**
@@ -207,26 +119,6 @@ export class BitacoraComponent implements OnInit, OnDestroy {
       }
     });
   }
-  
-
-
-
-  /**
-   * Inicializa el formulario de información de registro.
-   * @method inicializarFormularioInfoRegistro
-   */
-  inicializarFormularioInfoRegistro(): void {
-    this.formularioInfoRegistro = this.fb.group({
-      seleccionaLaModalidad: [{ value: '', disabled: true }],
-      seleccionarRegla: [{ value: '', disabled: false }],
-      sector: [{ value: '', disabled: false }],
-    });
-  }
-
- 
-
-
-  
   /**
    * Método del ciclo de vida de Angular que se ejecuta al destruir el componente.
    * Limpia las suscripciones.
@@ -236,18 +128,5 @@ export class BitacoraComponent implements OnInit, OnDestroy {
     
     this.destroyNotifier$.next();
     this.destroyNotifier$.complete();
-  }
-
-  
-
- 
-
-  /**
-   * Actualiza la lista de domicilios seleccionados.
-   * @method seleccionarDomicilios
-   * @param {Sector[]} domicilios - Domicilios seleccionados.
-   */
-  seleccionarDomicilios(domicilios: Sector[]): void {
-    this.domiciliosSeleccionados = [...domicilios];
   }
 }
