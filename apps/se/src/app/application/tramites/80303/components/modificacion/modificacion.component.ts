eed477e8996bd9c7fe13e559ad3025194db37db0
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-04-29 15:11:23 +0530

    update modificacion

--- apps/se/src/app/application/tramites/80303/components/modificacion/modificacion.component.ts
+++ apps/se/src/app/application/tramites/80303/components/modificacion/modificacion.component.ts
@@ -1,18 +1,64 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { TituloComponent } from '@ng-mf/data-access-user';
+import {
+  TablaDinamicaComponent,
+  TablaSeleccion,
+  TituloComponent,
+} from '@ng-mf/data-access-user';
+import { EMPRESA_SUBMANUFACTURERA_ENCABEZADO_DE_TABLA } from '../../models/modificacion-programa-immex-baja-submanufacturera.model';
 
 @Component({
   selector: 'app-modificacion',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, TituloComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TituloComponent,
+    TablaDinamicaComponent,
+  ],
   templateUrl: './modificacion.component.html',
   styleUrl: './modificacion.component.css',
 })
 export class ModificacionComponent {
   modificacionForm!: FormGroup;
+  submanufacturerasTablaConfiguracion = {
+    tipoSeleccionTabla: TablaSeleccion.BUTTON,
+    configuracionTabla: EMPRESA_SUBMANUFACTURERA_ENCABEZADO_DE_TABLA,
+  };
 
+  //estatus: string;
+  // rfc: string;
+  // razonSocial: string;
+  // calle: string;
+  // numeroInterior: string;
+  // numeroExterior: string;
+  // codigoPostal: string;
+  // localidad: string;
+  // municipioAlcaldia: string;
+  // entidadFederativa: string;
+  // pais: string;
+  // telefono: string;
+  // fax: string;
+  // correoElectronico: string;
+  submanufacturerasTablaDatos = [
+    {
+      estatus: 'Activo',
+      rfc: 'AAL0409235E6',
+      razonSocial: 'AEROPUERTO INTERNACIONAL DE CULIACAN S.A. DE C.V.',
+      calle: 'AVENIDA EJERCITO MEXICANO',
+      numeroInterior: 'S/N',
+      numeroExterior: 'S/N',
+      codigoPostal: '80000',
+      localidad: 'CULIACAN',
+      municipioAlcaldia: 'CULIACAN',
+      entidadFederativa: 'SINALOA',
+      pais: 'MEXICO',
+      telefono: '667 716 00 00',
+      fax: '667 716 00 00',
+      correoElectronico: '',
+    },
+  ];
   constructor(private fb: FormBuilder) {
     this.crearFormaulario();
   }
