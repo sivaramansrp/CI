a792524f55e172ae28b0c45bf71f91e7632ecc80
Kanchan Phutane <kanchan.phutane@ilink-systems.com>
2025-04-29 19:57:18 +0530

    folder created

---
+++ apps/se/src/app/application/tramites/80316/modificaicones-immex-prosec.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ModificaiconesImmexProsecRoutingModule } from './modificaicones-immex-prosec-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ModificaiconesImmexProsecRoutingModule
+  ]
+})
+export class ModificaiconesImmexProsecModule { }
