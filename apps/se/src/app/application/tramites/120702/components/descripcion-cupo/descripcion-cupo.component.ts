d8247c5d83534a13342f915ad4dd30edb19896ee
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-04-30 18:23:30 +0530

    added form controls

--- apps/se/src/app/application/tramites/120702/components/descripcion-cupo/descripcion-cupo.component.ts
+++ apps/se/src/app/application/tramites/120702/components/descripcion-cupo/descripcion-cupo.component.ts
@@ -1,13 +1,19 @@
+import { INFORMACION_DE_LA_OBRA_ARTE ,INPUT_FECHA_FIN_CUPO,INPUT_FECHA_INICIO_CUPO} from '../../constantes/expedicion-certificados-frontera.enum';
 import { Component } from '@angular/core';
+import { FormasDinamicasComponent } from '@libs/shared/data-access-user/src/tramites/components/formas-dinamicas/formas-dinamicas/formas-dinamicas.component';
+import { InputFechaComponent } from '@libs/shared/data-access-user/src';
 import { TituloComponent } from '@libs/shared/data-access-user/src';
+
 
 @Component({
   selector: 'app-descripcion-cupo',
   standalone: true,
-  imports: [TituloComponent],
+  imports: [TituloComponent,InputFechaComponent,FormasDinamicasComponent],
   templateUrl: './descripcion-cupo.component.html',
-  styleUrl: './descripcion-cupo.component.scss'
+  styleUrl: './descripcion-cupo.component.scss',
 })
 export class DescripcionCupoComponent {
-
+  FECHA_INICIO_CUPO = INPUT_FECHA_INICIO_CUPO;
+  FECHA_FIN_CUPO = INPUT_FECHA_FIN_CUPO;
+  public informacionFormData = INFORMACION_DE_LA_OBRA_ARTE;
 }
--- apps/se/src/app/application/tramites/120702/components/expedicion-asignacion/expedicion-asignacion.component.ts
+++ apps/se/src/app/application/tramites/120702/components/expedicion-asignacion/expedicion-asignacion.component.ts
@@ -1,26 +1,48 @@
 import {
   Catalogo,
   CatalogoSelectComponent,
+  InputFechaComponent,
   TituloComponent,
 } from '@libs/shared/data-access-user/src';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { INFORMACION_DE_LA_OBRA_ARTE,INPUT_FECHA_FIN,INPUT_FECHA_INICIO } from '../../constantes/expedicion-certificados-frontera.enum';
 import { Subject, takeUntil } from 'rxjs';
 import { DescripcionCupoComponent } from '../descripcion-cupo/descripcion-cupo.component';
 import { ExpedicionCertificadosFronteraService } from '../../services/expedicion-certificados-frontera.service';
+import { FormasDinamicasComponent } from '@libs/shared/data-access-user/src/tramites/components/formas-dinamicas/formas-dinamicas/formas-dinamicas.component';
+
 @Component({
   selector: 'app-expedicion-asignacion',
   standalone: true,
-  imports: [TituloComponent, CatalogoSelectComponent,DescripcionCupoComponent],
+  imports: [
+    TituloComponent,
+    CatalogoSelectComponent,
+    DescripcionCupoComponent,
+    InputFechaComponent,
+    ReactiveFormsModule,
+    FormasDinamicasComponent
+  ],
   templateUrl: './expedicion-asignacion.component.html',
   styleUrl: './expedicion-asignacion.component.scss',
 })
 export class ExpedicionAsignacionComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
-
+  FECHA_INICIO = INPUT_FECHA_INICIO;
+  FECHA_FIN = INPUT_FECHA_FIN;
   anoOficioDatos: Catalogo[] = [];
   numeroOficioDatos: Catalogo[] = [];
+public informacionFormData = INFORMACION_DE_LA_OBRA_ARTE;
+  public asignacionForm!: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private expedicionCertificadosFronteraService: ExpedicionCertificadosFronteraService
   ) {
     //
@@ -33,7 +55,22 @@ export class ExpedicionAsignacionComponent implements OnInit, OnDestroy {
       .subscribe((data) => {
         this.anoOficioDatos = data;
       });
+
+      this.establecerAsignacionFormGroup();
   }
+
+establecerAsignacionFormGroup():void{
+this.asignacionForm = this.fb.group({
+  anoDelOficio: new FormControl('', [Validators.required]),
+  numeroOficio: new FormControl('', [Validators.required]),
+  estado: new FormControl({value: 'CHIHUAHUA',disabled:true}),
+  representacionFederal: new FormControl({value: 'CIUDAD JUAREZ',disabled:true}),
+  monteAsignado: new FormControl({value: '500',disabled:true}),
+  monteExpedido: new FormControl({value: '130',disabled:true}),
+  monteDisponible: new FormControl({value: '370',disabled:true}),
+  datosNumeroOficio: new FormControl({value: '2',disabled:true}),
+})
+}
 
   ngOnDestroy(): void {
     this.destroy$.next();
