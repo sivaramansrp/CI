84c325ef0ded710a97c4b3277daec29b76799243
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-30 11:18:07 +0530

    module

---
+++ apps/aga/src/app/application/tramites/5601/solicitud-despacho-exportacion.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SolicitudDespachoExportacionRoutingModule } from './solicitud-despacho-exportacion-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SolicitudDespachoExportacionRoutingModule
+  ]
+})
+export class SolicitudDespachoExportacionModule { }
