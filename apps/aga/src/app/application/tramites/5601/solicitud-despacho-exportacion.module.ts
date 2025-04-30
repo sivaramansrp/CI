abcc8462c0451e67465afd52e9fd63a221e8efad
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-04-30 11:42:25 +0530

    pages

--- apps/aga/src/app/application/tramites/5601/solicitud-despacho-exportacion.module.ts
+++ apps/aga/src/app/application/tramites/5601/solicitud-despacho-exportacion.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
+import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { SolicitudDespachoExportacionComponent } from './pages/solicitud-despacho-exportacion/solicitud-despacho-exportacion.component';
 import { SolicitudDespachoExportacionRoutingModule } from './solicitud-despacho-exportacion-routing.module';
-
 @NgModule({
-  declarations: [SolicitudDespachoExportacionComponent],
+  declarations: [
+    SolicitudDespachoExportacionComponent,
+    PasoUnoComponent,
+    PasoDosComponent,
+    PasoTresComponent,
+  ],
   imports: [CommonModule, SolicitudDespachoExportacionRoutingModule],
 })
 export class SolicitudDespachoExportacionModule {}
