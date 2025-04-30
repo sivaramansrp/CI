1ae6c5c4a0a5b3c080fd2c9497a9fe7f2bd18f36
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-04-30 16:08:45 -0600

    Api ferro

--- libs/shared/data-access-user/src/core/constants/api-constants.ts
+++ libs/shared/data-access-user/src/core/constants/api-constants.ts
@@ -107,7 +107,3 @@ export const API_VALIDA_HORARIO = 'sat-t5701/horario/valida';
  */
 export const API_CERTIFICACION = `programa-se/certificacion/${RFC_QUERY}`;
 
-/**
- * 
- */
-export const API_CONSULTAR_FERRO = `privado/ferro/valida`
