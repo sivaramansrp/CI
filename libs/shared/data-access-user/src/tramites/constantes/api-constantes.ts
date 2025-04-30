a81da32884ae5fd1804977ea597b1074b6156cbc
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-04-30 15:27:48 -0600

    DOcumenta API catalogos

--- libs/shared/data-access-user/src/tramites/constantes/api-constantes.ts
+++ libs/shared/data-access-user/src/tramites/constantes/api-constantes.ts
@@ -1,1 +1,5 @@
+/**
+ * API para obtener el catálogo de tipo de equipo en el trasnporte ferroviario.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/catalogo/swagger-ui/index.html#/Tipos%20de%20equipo./consulta-tipos-equipo
+ */
 export const API_GET_TIPO_EQUIPO = 'catalogo/busca/tipo-equipo';
