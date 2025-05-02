ad8f65f9a88fa12b6fdbf8daa1fc187d7ede8a9a
Yael Velasco <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-05-02 12:54:36 -0600

    Atiende comentarios

--- apps/aga/src/app/application/core/services/5701/consulta-responsable.service.ts
+++ apps/aga/src/app/application/core/services/5701/consulta-responsable.service.ts
@@ -1,6 +1,6 @@
 
 
-import { API_GET_CONSULTA_RESPONSABLE, API_GET_INDUSTRIA_AUTOMOTRIZ, enviroment, NUMERO_GAFETE_QUERY, TIPO_GAFETE_QUERY } from '@libs/shared/data-access-user/src';
+import { API_GET_CONSULTA_RESPONSABLE, enviroment, NUMERO_GAFETE_QUERY, TIPO_GAFETE_QUERY } from '@libs/shared/data-access-user/src';
 import { catchError, map, Observable, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
