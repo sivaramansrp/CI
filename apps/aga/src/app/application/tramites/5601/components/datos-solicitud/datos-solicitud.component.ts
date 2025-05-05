cff3436abc3a1ef4baa52a5eb9d881c2710644ab
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-05 17:41:32 +0530

    form group amd controls

--- apps/aga/src/app/application/tramites/5601/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/aga/src/app/application/tramites/5601/components/datos-solicitud/datos-solicitud.component.ts
@@ -1,4 +1,4 @@
-import { Catalogo, CatalogoSelectComponent } from '@libs/shared/data-access-user/src';
+import { Catalogo, CatalogoSelectComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -7,7 +7,7 @@ import seleccionarOpciones from '@libs/shared/theme/assets/json/5601/selector-56
 @Component({
   selector: 'app-datos-solicitud',
   standalone: true,
-  imports: [CommonModule, CatalogoSelectComponent, ReactiveFormsModule],
+  imports: [CommonModule, CatalogoSelectComponent, ReactiveFormsModule,TituloComponent],
   templateUrl: './datos-solicitud.component.html',
   styleUrl: './datos-solicitud.component.css',
 })
@@ -15,11 +15,15 @@ export class DatosSolicitudComponent implements OnInit {
 
   formulario!: FormGroup;
 
+  formularioMercancia!: FormGroup;
+
   aduanas!: Catalogo[];
 
   seccionAduanera!: Catalogo[];
 
   tipoOperacion!: Catalogo[];
+
+  tipoMoneda!: Catalogo[];
 
   mostrarFechaOperacion: boolean = false;
 
@@ -27,6 +31,7 @@ export class DatosSolicitudComponent implements OnInit {
     this.aduanas = seleccionarOpciones?.aduanas;
     this.seccionAduanera = seleccionarOpciones?.seccionAduanera;
     this.tipoOperacion = seleccionarOpciones?.tipoOperacion;
+    this.tipoMoneda = seleccionarOpciones?.tipoMoneda;
   }
 
   ngOnInit(): void {
@@ -34,7 +39,16 @@ export class DatosSolicitudComponent implements OnInit {
       aduana: [null, Validators.required],
       seccionAduanera: [null, Validators.required],
       tipoOperacion: [null, Validators.required],
-      fechaOperacion: [null]
+      fechaOperacion: [null],
+      motivoDespachoDomicilio: [null],
+      observaciones: [null]
+    });
+
+    this.formularioMercancia = this.fb.group({
+      especificacionesMercancia: [null, Validators.required],
+      descripcionMercancia: [null, Validators.required],
+      tipoMoneda: [null, Validators.required],
+      valorMercancia: [null, Validators.required],
     });
   }
 
