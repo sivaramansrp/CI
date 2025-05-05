8c02def32a7a7becd5bbbd12c731ae43a190fee0
simrat-ilink <simratpal.gyani@ilink-systems.com>
2025-05-05 14:36:56 +0530

    json-procedure-folder

--- apps/sedena/src/app/application/tramites/240123/services/datos-solicitud.service.ts
+++ apps/sedena/src/app/application/tramites/240123/services/datos-solicitud.service.ts
@@ -15,7 +15,7 @@ export class DatosSolicitudService {
    * Usado para cargar información desde el frontend (assets).
    * @private
    */
-  private jsonUrl = 'assets/json/sedena/exportacion-datos.json';
+  private jsonUrl = 'assets/json/240123/exportacion-datos.json';
 
   constructor(public httpServicios: HttpClient) {}
   /**
