d45539058b7b76b3f6f7ba1fe23a16e83d02b6ea
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-05 12:41:08 +0530

    reusable check box

--- apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
+++ apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
@@ -1,12 +1,12 @@
+import { InputCheckComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { MENSAJE_MODAL, TITULO_MODAL } from '../../constantes/tramite5601.enum';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TITULO_MODAL } from '../../constantes/tramite5601.enum';
-import { TituloComponent } from '@libs/shared/data-access-user/src';
 
 @Component({
   selector: 'app-certificaciones',
   standalone: true,
-  imports: [CommonModule,TituloComponent],
+  imports: [CommonModule,TituloComponent,InputCheckComponent],
   templateUrl: './certificaciones.component.html',
   styleUrl: './certificaciones.component.scss',
 })
@@ -21,7 +21,7 @@ export class CertificacionesComponent {
     const CHECKBOX = event.target as HTMLInputElement;
     if (CHECKBOX.checked) {
       this.tituloModal = TITULO_MODAL
-      this.mensajeModal = '¿Cuenta con algún documento que acredite la autorización y A vigencia de su esquema de certificación?';
+      this.mensajeModal = MENSAJE_MODAL;
       this.abrirModal();
     }
   }
--- apps/aga/src/app/application/tramites/5601/constantes/tramite5601.enum.ts
+++ apps/aga/src/app/application/tramites/5601/constantes/tramite5601.enum.ts
@@ -1,1 +1,2 @@
 export const TITULO_MODAL = 'Aviso';
+export const MENSAJE_MODAL='¿Cuenta con algún documento que acredite la autorización y A vigencia de su esquema de certificación?'
