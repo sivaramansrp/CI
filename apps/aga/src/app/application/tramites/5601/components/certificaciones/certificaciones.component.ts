f3413e449779cbbb26b76f0a20bc07adeab2457d
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-05 12:28:46 +0530

    modal added

--- apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
+++ apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TITULO_MODAL } from '../../constantes/tramite5601.enum';
 import { TituloComponent } from '@libs/shared/data-access-user/src';
 
 @Component({
@@ -7,6 +8,40 @@ import { TituloComponent } from '@libs/shared/data-access-user/src';
   standalone: true,
   imports: [CommonModule,TituloComponent],
   templateUrl: './certificaciones.component.html',
-  styleUrl: './certificaciones.component.css',
+  styleUrl: './certificaciones.component.scss',
 })
-export class CertificacionesComponent {}
+export class CertificacionesComponent {
+  modal: string = '';
+
+  tituloModal!: string;
+
+  mensajeModal!: string;
+
+  mostrarModalSiSeleccionado(event: Event): void {
+    const CHECKBOX = event.target as HTMLInputElement;
+    if (CHECKBOX.checked) {
+      this.tituloModal = TITULO_MODAL
+      this.mensajeModal = '¿Cuenta con algún documento que acredite la autorización y A vigencia de su esquema de certificación?';
+      this.abrirModal();
+    }
+  }
+
+  abrirModal(): void {
+    this.modal = 'show';
+  }
+
+  cerrarModal(): void {
+    this.modal = '';
+    this.tituloModal = '';
+    this.mensajeModal = '';
+  }
+
+  confirmarAccion(): void {
+    this.cerrarModal();
+  }
+
+  cancelarAccion(): void {
+    this.cerrarModal();
+  }
+
+}
---
+++ apps/aga/src/app/application/tramites/5601/constantes/tramite5601.enum.ts
@@ -0,0 +1,1 @@
+export const TITULO_MODAL = 'Aviso';
