fb0907092589e9046fc4341b74582c9ecd971aa2
maneeshangulardeveloper <maneeshpatel2024@gmail.com>
2025-05-06 13:10:14 +0530

    fix

--- apps/profepa/src/app/application/tramites/250102/estados/tramite250102.query.ts
+++ apps/profepa/src/app/application/tramites/250102/estados/tramite250102.query.ts
@@ -6,9 +6,27 @@ import { Query } from "@datorama/akita";
     providedIn: 'root',
 })
 export class Tramite250102Query extends Query<Tramite250102State>{
+    tipoAduana$ = this.select((state) => state.tipoAduana);
+    tipoInspectoria$ = this.select((state) => state.tipoInspectoria);
+    tipoMunicipio$ = this.select((state) => state.tipoMunicipio);
+    destinatarioDenominacion$ = this.select((state) => state.destinatarioDenominacion);
+    destinatarioPais$ = this.select((state) => state.destinatarioPais);
+    destinatarioEstado$ = this.select((state) => state.destinatarioEstado);
+    destinatarioCodigoPostal$ = this.select((state) => state.destinatarioCodigoPostal);
+    destinatarioDomicilio$ = this.select((state) => state.destinatarioDomicilio);
+    agenteAduanalNombre$ = this.select((state) => state.agenteAduanalNombre);
+    agenteAduanalPrimerApellido$ = this.select((state) => state.agenteAduanalPrimerApellido);
+    agenteAduanalSegundoApellido$ = this.select((state) => state.agenteAduanalSegundoApellido);
+    agenteAduanalPatente$ = this.select((state) => state.agenteAduanalPatente);
 
 
     constructor(private tramiteStore: Tramite250102Store){
         super(tramiteStore);
     }
+        /**
+   * Selecciona el estado completo de la solicitud
+   */
+  selectSolicitud$ = this.select((state) => {
+    return state;
+  });
 }
