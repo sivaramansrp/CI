fdb22c685c210849c5400a56d9bf2588e5036194
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-06 14:41:39 +0530

    reusable components added

--- apps/aga/src/app/application/tramites/5601/pages/paso-tres/paso-tres.component.ts
+++ apps/aga/src/app/application/tramites/5601/pages/paso-tres/paso-tres.component.ts
@@ -1,7 +1,43 @@
+import { TramiteFolioService, TramiteFolioStore } from '@libs/shared/data-access-user/src';
+import { catchError, map } from 'rxjs';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-paso-tres',
   templateUrl: './paso-tres.component.html',
 })
-export class PasoTresComponent {}
+export class PasoTresComponent {
+
+  constructor(
+      private router: Router,
+      private tramiteFolioServices: TramiteFolioService,
+      private tramiteStore: TramiteFolioStore) 
+    {
+  
+    }
+  
+    /**
+    * Maneja el evento para obtener la firma y realiza acciones adicionales.
+    * @param ev - La cadena de texto que representa la firma obtenida.
+    */
+    obtieneFirma(ev: string): void {
+      const FIRMA: string = ev;
+      if (FIRMA) {
+        // Obtiene el número de trámite
+        this.tramiteFolioServices
+          .obtenerTramite(19)
+          .pipe(
+            map((tramite) => {
+              this.tramiteStore.establecerTramite(tramite.data, FIRMA);
+              this.router.navigate(['servicios-extraordinarios/acuse']);
+            }),
+            catchError((_error) => {
+              return _error;
+            })
+          )
+          .subscribe();
+      }
+    }
+  
+}
