922c5d6e8bcb8c46114fafe514a2cde382f39a05
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-06 15:31:18 +0530

    form groups and form controls

--- apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
+++ apps/aga/src/app/application/tramites/5601/components/certificaciones/certificaciones.component.ts
@@ -1,21 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { InputCheckComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 import { MENSAJE_MODAL, TITULO_MODAL } from '../../constantes/tramite5601.enum';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-certificaciones',
   standalone: true,
-  imports: [CommonModule,TituloComponent,InputCheckComponent],
+  imports: [CommonModule,TituloComponent,InputCheckComponent,ReactiveFormsModule],
   templateUrl: './certificaciones.component.html',
   styleUrl: './certificaciones.component.scss',
 })
-export class CertificacionesComponent {
+export class CertificacionesComponent implements OnInit {
+
+  formularioCertificacion!: FormGroup;
   modal: string = '';
 
   tituloModal!: string;
 
   mensajeModal!: string;
+
+  constructor(private fb: FormBuilder) {
+    // Inicializa el formulario de certificación
+  }
+
+  ngOnInit(): void {
+    this.formularioCertificacion = this.fb.group({
+      tieneCertificacion: [false],
+      certificacionEmpresa: [''], 
+      otraCertificacion: [''] 
+    });
+  }
   
 
   mostrarModalSiSeleccionado(event: Event): void {
