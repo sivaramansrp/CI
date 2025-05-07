d4e0e4f1777f6fd4943c2a884d53209757882d42
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-07 17:19:49 +0530

    Refactor PasoUno component structure and improve tab selection logic

--- apps/sedena/src/app/application/tramites/240112/constants/agregar-destinatario.enum.ts
+++ apps/sedena/src/app/application/tramites/240112/constants/agregar-destinatario.enum.ts
@@ -29,5 +29,5 @@ export const PASOS = [
     'La solicitud ha quedado registrada con el número temporal [202767640]. Este no tiene validez legal y sirve solamente para efectos de identificar tu Solicitud. Un folio oficial le será asignado a la solicitud al momento en que esta sea firmada.';
   
   // Identificador único del procedimiento administrativo
-  export const ID_PROCEDIMIENTO = 240118;
+  export const ID_PROCEDIMIENTO = 240112;
   
