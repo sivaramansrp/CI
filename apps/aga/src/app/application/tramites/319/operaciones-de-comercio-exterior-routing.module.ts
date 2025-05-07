640f66e238ca22bc5243296a8b116ef84a508422
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-07 10:53:47 +0530

    Add detailed file overview comments for operaciones-de-comercio-exterior-routing.module.ts

--- apps/aga/src/app/application/tramites/319/operaciones-de-comercio-exterior-routing.module.ts
+++ apps/aga/src/app/application/tramites/319/operaciones-de-comercio-exterior-routing.module.ts
@@ -5,6 +5,18 @@ import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.co
 
 
 
+/**
+ * @fileoverview Define las rutas para el módulo de operaciones de comercio exterior.
+ * 
+ * @const ROUTES
+ * @type {Routes}
+ * @description
+ * Este arreglo de rutas configura las rutas para el módulo de operaciones de comercio exterior.
+ * Contiene las siguientes rutas:
+ * - `exterior`: Muestra el componente `SolicitudPageComponent`.
+ * - Ruta por defecto (`''`): Redirige automáticamente a la ruta `exterior`.
+ * 
+ */
 const ROUTES: Routes = [
   {path:'exterior' , component: SolicitudPageComponent},
   {
