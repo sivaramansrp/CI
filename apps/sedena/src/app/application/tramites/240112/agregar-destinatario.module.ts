fd107a46cf74316a3f35cc486efa48334bb98244
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-07 13:23:54 +0530

    Add PasoUno, PasoDos, and PasoTres components with associated templates and styles

--- apps/sedena/src/app/application/tramites/240112/agregar-destinatario.module.ts
+++ apps/sedena/src/app/application/tramites/240112/agregar-destinatario.module.ts
@@ -1,9 +1,24 @@
-import { AgregarDestinatarioRoutingModule } from './agregar-destinatario-routing.module';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+
+import { AgregarDestinatarioRoutingModule } from './agregar-destinatario-routing.module';
+import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
+import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
+import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
+
+
 @NgModule({
-  declarations: [SolicitudPageComponent],
-  imports: [CommonModule, AgregarDestinatarioRoutingModule],
+  declarations: [
+    SolicitudPageComponent,
+    PasoUnoComponent,
+    PasoDosComponent,
+    PasoTresComponent
+  ],
+  imports: [
+    CommonModule,
+    AgregarDestinatarioRoutingModule
+  ]
 })
-export class AgregarDestinatarioModule {}
+export class AgregarDestinatarioModule { }
---
+++ apps/sedena/src/app/application/tramites/240112/pages/paso-uno/paso-uno.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paso-uno',
+  templateUrl: './paso-uno.component.html',
+  styleUrl: './paso-uno.component.css',
+})
+export class PasoUnoComponent {}
