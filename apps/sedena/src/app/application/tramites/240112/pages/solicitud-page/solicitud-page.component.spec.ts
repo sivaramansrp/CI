652bf323dc806467dc1ae923d49707a2c50819ff
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-08 14:35:31 +0530

    Refactor test setup for SolicitudPageComponent to enhance clarity and maintainability

--- apps/sedena/src/app/application/tramites/240112/pages/solicitud-page/solicitud-page.component.spec.ts
+++ apps/sedena/src/app/application/tramites/240112/pages/solicitud-page/solicitud-page.component.spec.ts
@@ -1,5 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SolicitudPageComponent } from './solicitud-page.component';
+import { BtnContinuarComponent, SolicitanteComponent, WizardComponent } from '@ng-mf/data-access-user';
+import { PasoUnoComponent } from '../paso-uno/paso-uno.component';
+import { PasoDosComponent } from '../paso-dos/paso-dos.component';
+import { PasoTresComponent } from '../paso-tres/paso-tres.component';
 
 describe('SolicitudPageComponent', () => {
   let component: SolicitudPageComponent;
@@ -7,7 +13,8 @@ describe('SolicitudPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SolicitudPageComponent],
+      declarations: [SolicitudPageComponent,PasoUnoComponent,PasoDosComponent,PasoTresComponent],
+      imports: [HttpClientTestingModule,WizardComponent,BtnContinuarComponent,SolicitanteComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SolicitudPageComponent);
