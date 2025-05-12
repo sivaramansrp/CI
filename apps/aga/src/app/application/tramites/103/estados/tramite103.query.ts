43dc4fc85a83ceb7f2319eef0f31c22ce012ce8c
himanshu-ilink <himanshu.v@ilink-systems.com>
2025-05-12 08:33:56 +0530

    update comments in spanish

--- apps/aga/src/app/application/tramites/103/estados/tramite103.query.ts
+++ apps/aga/src/app/application/tramites/103/estados/tramite103.query.ts
@@ -3,20 +3,20 @@ import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
 
 /**
- * Tramite103Query provides reactive access to the state of Solicitud103.
+ * Tramite103Query proporciona acceso reactivo al estado de Solicitud103.
  */
 @Injectable({ providedIn: 'root' })
 export class Tramite103Query extends Query<Solicitud103State> {
   /**
-   * Observable that emits the full state of the solicitud.
+   * Observable que emite el estado completo de la solicitud.
    */
   selectSolicitud$ = this.select((state) => {
     return state;
   });
 
   /**
-   * Creates an instance of Tramite103Query.
-   * @param store The store instance that holds the Solicitud103 state.
+   * Crea una instancia de Tramite103Query.
+   * @param store Instancia del store que contiene el estado de Solicitud103.
    */
   constructor(protected override store: Tramite103Store) {
     super(store);
