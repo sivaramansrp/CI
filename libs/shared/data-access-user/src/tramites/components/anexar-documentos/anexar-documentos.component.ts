ad273b03024d9bdbdee9a9a405fd3b9a51d36065
shekhar-kalluri-ilink <shekhar.kalluri@ilink-systems.com>
2025-05-13 19:17:08 +0530

    "revert changes"

--- libs/shared/data-access-user/src/tramites/components/anexar-documentos/anexar-documentos.component.ts
+++ libs/shared/data-access-user/src/tramites/components/anexar-documentos/anexar-documentos.component.ts
@@ -34,7 +34,7 @@ import { DocumentosParaCargar } from '../../../core/models/shared/anexar-documen
 @Component({
   selector: 'anexar-documentos',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgSelectModule, NotificacionesComponent,ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgSelectModule, FormsModule, NotificacionesComponent],
   templateUrl: './anexar-documentos.component.html',
   styleUrl: './anexar-documentos.component.scss'
 })
