06c7555fe3c91d1f151d6570727c33d3768fc337
Vinothini P <vinothini.p@ilink-systems.com>
2025-05-13 12:22:37 +0530

    added documentation

--- apps/sener/src/app/application/tramites/140218/estados/store/tramite140218.store.ts
+++ apps/sener/src/app/application/tramites/140218/estados/store/tramite140218.store.ts
@@ -62,6 +62,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece el tipo de solicitud en el estado de la tienda.
+   *
+   * @param tipoSolicitud - El nuevo tipo de solicitud que se va a asignar al estado.
+   */
   public setTipoSolicitud(tipoSolicitud: string): void {
     this.update((state) => ({
       ...state,
@@ -69,6 +74,12 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * @comdoc
+   * Establece el valor del régimen en el estado de la tienda.
+   *
+   * @param regimen - El nuevo valor de régimen que se asignará al estado.
+   */
   public setRegimen(regimen: string): void {
     this.update((state) => ({
       ...state,
@@ -76,6 +87,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
   
+  /**
+   * Establece la clasificación del régimen en el estado de la tienda.
+   *
+   * @param clasificacionRegimen - La nueva clasificación del régimen que se asignará al estado.
+   */
   public setClasificacionRegimen(clasificacionRegimen: string): void {
     this.update((state) => ({
       ...state,
@@ -83,6 +99,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece el valor del periodo de vigencia en el estado.
+   *
+   * @param periodoVigencia - El nuevo periodo de vigencia que se asignará al estado.
+   */
   public setPeriodoVigencia(periodoVigencia: string): void {
     this.update((state) => ({
       ...state,
@@ -90,6 +111,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la unidad de medida en el estado de la tienda.
+   *
+   * @param unidadMedida - La nueva unidad de medida que se asignará al estado.
+   */
   public setUnidadMedida(unidadMedida: string): void {
     this.update((state) => ({
       ...state,
@@ -97,6 +123,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la fracción arancelaria en el estado de la tienda.
+   *
+   * @param fraccionArancelaria - La nueva fracción arancelaria que se asignará al estado.
+   */
   public setFraccionArancelaria(fraccionArancelaria: string): void {
     this.update((state) => ({
       ...state,
@@ -104,6 +135,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la cantidad autorizada en el estado de la tienda.
+   *
+   * @param cantidadAutorizada - La nueva cantidad autorizada que se asignará al estado.
+   */
   public setCantidadAutorizada(cantidadAutorizada: string): void {
     this.update((state) => ({
       ...state,
@@ -111,6 +147,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece el valor autorizado en el estado de la tienda.
+   *
+   * @param valorAutorizado - El nuevo valor autorizado que se asignará al estado.
+   */
   public setValorAutorizado(valorAutorizado: string): void {
     this.update((state) => ({
       ...state,
@@ -118,6 +159,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece el NICO (Número de Identificación Comercial) en el estado de la tienda.
+   *
+   * @param nico - El nuevo NICO que se asignará al estado.
+   */
   public setNico(nico: string): void {
     this.update((state) => ({
       ...state,
@@ -125,6 +171,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la descripción del NICO en el estado de la tienda.
+   *
+   * @param descripcionNico - La nueva descripción del NICO que se asignará al estado.
+   */
   public setDescripcionNico(descripcionNico: string): void {
     this.update((state) => ({
       ...state,
@@ -132,6 +183,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la acotación en el estado de la tienda.
+   *
+   * @param acotacion - La nueva acotación que se asignará al estado.
+   */
   public setAcotacion(acotacion: string): void {
     this.update((state) => ({
       ...state,
@@ -139,6 +195,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la fecha de inicio del permiso en el estado de la tienda.
+   *
+   * @param permisoDesde - La nueva fecha de inicio del permiso que se asignará al estado.
+   */
   public setPermisoDesde(permisoDesde: string): void {
     this.update((state) => ({
       ...state,
@@ -146,6 +207,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece la fecha de fin del permiso en el estado de la tienda.
+   *
+   * @param permisoHasty - La nueva fecha de fin del permiso que se asignará al estado.
+   */
   public setPermisoHasty(permisoHasty: string): void {
     this.update((state) => ({
       ...state,
@@ -153,6 +219,11 @@ export class Tramite140218Store extends Store<DatosSolicitudState> {
     }));
   }
 
+  /**
+   * Establece el motivo de renuncia en el estado de la tienda.
+   *
+   * @param motivoRenuncia - El nuevo motivo de renuncia que se asignará al estado.
+   */
   public setMotivoRenuncia(motivoRenuncia: string): void {
     this.update((state) => ({
       ...state,
--- apps/sener/src/app/application/tramites/140218/renuncia-de-permiso-routing.module.ts
+++ apps/sener/src/app/application/tramites/140218/renuncia-de-permiso-routing.module.ts
@@ -8,8 +8,6 @@ const ROUTES: Routes = [
       path: 'solicitud',
       component: RenunciaDePermisoComponent,
     }
-   
-  
 ];
 
 @NgModule({
