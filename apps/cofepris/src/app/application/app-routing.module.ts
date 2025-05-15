5e3a6e447f20c816bea469566df4fad13d50353e
SubodhSingh11 <subodh.singh@ilink-systems.com>
2025-05-15 17:00:08 +0530

    tramites route conflicts resolved

--- apps/cofepris/src/app/application/app-routing.module.ts
+++ apps/cofepris/src/app/application/app-routing.module.ts
@@ -547,6 +547,13 @@ const ROUTES: Routes = [
       (m) => m.ImportarDeRemediosHerbalsModule
     ),
 },
+{
+  path: 'permiso-pruebas-nutrientes',
+  loadChildren: () =>
+    import('./tramites/260510/permiso-pruebas-nutrientes.module').then(
+      (m) => m.PermisoPruebasNutrientesModule
+    ),
+},
 ];
 
 @NgModule({
