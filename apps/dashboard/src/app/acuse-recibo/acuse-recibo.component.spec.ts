4bc1bd993dd2e1003e573cb55d441bf7ead770f6
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-05-16 20:46:14 +0530

    tesing detalles folio

--- apps/dashboard/src/app/acuse-recibo/acuse-recibo.component.spec.ts
+++ apps/dashboard/src/app/acuse-recibo/acuse-recibo.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AcuseReciboComponent } from './acuse-recibo.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TablaAcciones } from '@libs/shared/data-access-user/src';
 
 describe('AcuseReciboComponent', () => {
   let component: AcuseReciboComponent;
@@ -7,7 +9,7 @@ describe('AcuseReciboComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AcuseReciboComponent],
+      imports: [AcuseReciboComponent, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AcuseReciboComponent);
@@ -17,5 +19,16 @@ describe('AcuseReciboComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize acciones with correct values', () => {
+    expect(component.acciones).toEqual([
+      TablaAcciones.VER,
+      TablaAcciones.DESCARGAR,
+    ]);
+  });
+
+  it('should initialize acuseReciboTablaDatos as an empty array', () => {
+    expect(component.acuseReciboTablaDatos).toEqual([]);
   });
 });
