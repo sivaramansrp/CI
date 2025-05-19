94a980879532d539cf93ac251b7f752933aab957
shilpa-ilink <shilpa.k@ilink-systems.com>
2025-05-19 10:21:35 +0530

    removed console logs

--- apps/se/src/app/application/tramites/130103/components/datos-del-tramite-realizer/datos-del-tramite-realizer.component.ts
+++ apps/se/src/app/application/tramites/130103/components/datos-del-tramite-realizer/datos-del-tramite-realizer.component.ts
@@ -150,11 +150,6 @@ export class DatosDelTramiteRealizerComponent implements OnInit, OnDestroy {
   * }
   */
   ngOnInit(): void {
-     this.ninoFormGroup.get('regimen')?.valueChanges.subscribe(() => {
-    // Reset 'clasificacion' when 'regimen' changes
-    console.log('Regimen changed');
-    this.ninoFormGroup.get('clasificacion')?.reset('');
-  });
     this.tramite130103Query.selectImportacion$
       .pipe(
         takeUntil(this.destroyNotifier$),
@@ -272,10 +267,9 @@ export class DatosDelTramiteRealizerComponent implements OnInit, OnDestroy {
   * // Actualiza el estado dinámico del campo "regimen" con el valor "Régimen B".
   */
   establecerCambioDeValor(event: { campo: string; valor?: object }): void {
-   if (event.campo === 'regimen') {
-    // Reset clasificacion when regimen changes
-    this.ninoFormGroup.get('clasificacion')?.reset('');
-  }
+    if (event.campo === 'regimen') {
+      this.ninoFormGroup.get('clasificacion')?.reset('');
+    }
     if (event && typeof event.valor === 'object' && event.valor !== null && 'id' in event.valor) {
       const VALOR = event.valor.id;
       this.tramite130103Store.setDynamicFieldValue(event.campo, VALOR);
