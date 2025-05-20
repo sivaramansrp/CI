3ddb8f3f04d7cdb1318b058bbc7934296ccbe0ae
Rajalakshmi Lakshmanan <rajalakshmi.lakshmanan@ilink-systems.com>
2025-05-20 20:16:30 +0530

    Bug Fix Changes

--- apps/agriculture/src/app/application/tramites/220701/constantes/inspeccion-fisica-zoosanitario.enums.ts
+++ apps/agriculture/src/app/application/tramites/220701/constantes/inspeccion-fisica-zoosanitario.enums.ts
@@ -138,7 +138,7 @@ export const MANDATORY_INSTRUCTION = 'Las tablas con asterisco son obligatorias 
  * Define las propiedades del campo de fecha de expedición.
  */
 export const EXPEDICION_FACTURA_FECHA = {
-    labelNombre: 'Fecha de expedición de la factura',
+    labelNombre: 'Fecha de pago',
     required: true,
     habilitado: true,
 };
