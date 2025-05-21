688ff1092e7429abe51bd5655385ee994e4188e1
Swapnali-Mahadar-iLink <swapnali.mahadar@ilink-systems.com>
2025-05-21 14:09:31 +0530

    alert message added after click on continuar button

--- apps/se/src/app/application/tramites/120402/pages/asignacion-directa-de-cupo/asignacion-directa-de-cupo.component.ts
+++ apps/se/src/app/application/tramites/120402/pages/asignacion-directa-de-cupo/asignacion-directa-de-cupo.component.ts
@@ -8,7 +8,11 @@ import { ASIGNACION } from '@ng-mf/data-access-user';
 
 import { ListaPasosWizard } from '@ng-mf/data-access-user';
 
-import { DatosPasos, WizardComponent } from '@ng-mf/data-access-user';
+import { DatosPasos, Notificacion, WizardComponent } from '@ng-mf/data-access-user';
+
+import { Tramite120402Query } from '../../estados/queries/tramite120402.query';
+
+import { NOTA} from '../../constantes/definiciones.enum';
 /**
  * Interface representing the action of a button.
  */
@@ -45,11 +49,39 @@ export class AsignacionDirectaDeCupoComponent {
    * Índice actual del paso en el asistente.
    */
   indice: number = 1;
- 
-  /**
-   * Clase CSS para aplicar estilos específicos a los elementos de la interfaz.
+
+   /**
+   * Mensaje de información para la alerta.
    */
-  class: string = 'alert-danger';
+  public infoAlert = 'alert-info';
+
+  public showAlert: boolean = false;
+
+ /**
+   * Mensaje de confirmación para campos obligatorios no seleccionados.
+   * @type {string}
+   */
+  
+  MENSAJE_CONFIRMACION: string = NOTA.CONTINUAR_BUTTON_ALERT;
+
+    /**
+     * Constructor del componente.
+   
+     * @param tramite120402Query - Query para obtener datos del store.
+     */
+    constructor(
+  
+      private tramite120402Query: Tramite120402Query
+    ) {}
+ 
+
+
+
+  /**
+   * Notificación para mostrar alertas al usuario.
+   * @type {Notificacion}
+   */
+  nuevaAlertaNotificacion!: Notificacion;
 
     /**
    * The data for the steps in the wizard.
@@ -66,6 +98,10 @@ export class AsignacionDirectaDeCupoComponent {
    * @param e The action button event containing the action and value.
    */
   public getValorIndice(e: AccionBoton): void {
+       console.log('inside getValorIndice', this.showAlert);
+    
+       if(this.showAlert===true){
+    
     if (e.valor > 0 && e.valor < 5) {
       this.indice = e.valor;
       if (e.accion === 'cont') {
@@ -74,5 +110,32 @@ export class AsignacionDirectaDeCupoComponent {
         this.wizardComponent.atras();
       }
     }
+  }}
+  /**
+   * Actualiza el estado del grid de comercializadores de productos.
+   */
+  actualizaGridComercializadoresProductos(): void {
+     const VALOR_ENTIDAD = this.tramite120402Query.getValue().entidad;
+    const VALOR_REPRESENTACION =
+      this.tramite120402Query.getValue().representacion;
+       
+      if (!VALOR_ENTIDAD || !VALOR_REPRESENTACION) {//
+          console.log('inside actualizaGridComercializadoresProductos');
+        this.showAlert = true;
+        }
+        else{
+          this.showAlert = false;
+        }
+        
+      this.nuevaAlertaNotificacion = {
+          tipoNotificacion: 'banner',
+          categoria: 'danger', 
+          modo: 'action',
+          titulo: '',
+          mensaje: this.MENSAJE_CONFIRMACION,
+          cerrar: true,
+          txtBtnAceptar: '',
+          txtBtnCancelar: '',
   }
+}
 }
