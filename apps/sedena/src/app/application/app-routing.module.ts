dec31cbaf5b3504fb535ba2142fbd9558d2b43cf
dnyanesh-ilink <dnyaneshwar.suryawanshi@ilink-systems.com>
2025-05-21 14:05:29 +0530

    added proceso-requerimiento for inbal,sedena,profepa,semarnat deprtment
    added proceso-requerimiento for inbal,sedena,profepa,semarnat deprtment

--- apps/sedena/src/app/application/app-routing.module.ts
+++ apps/sedena/src/app/application/app-routing.module.ts
@@ -186,6 +186,13 @@ const ROUTES: Routes = [
         (m) => m.AutorizarComponent
       ),
   },
+  {
+    path: 'proceso-requerimiento',
+      loadComponent: () =>
+        import('./proceso-requerimiento/proceso-requerimiento.component').then(
+          (m) => m.ProcesoRequerimientoComponent
+        ),
+    }
 ];
 
 @NgModule({
