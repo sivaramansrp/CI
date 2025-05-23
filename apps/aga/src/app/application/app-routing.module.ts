3b85b70ca8c4f46fa468a4979cbd4da0b0163fc2
Ghanshyam Yadav <ghanshyam.yadav@ilink-systems.com>
2025-05-23 18:09:15 +0530

    Updated urls and navigation for 40101 & 40102 procedures.

--- apps/aga/src/app/application/app-routing.module.ts
+++ apps/aga/src/app/application/app-routing.module.ts
@@ -96,10 +96,10 @@ const ROUTES: Routes = [
       ),
   },
   {
-    path: 'transportista-terrestre',
+    path: 'transportista-renovacion',
     loadChildren: () =>
-      import('./tramites/40102/transportista-terrestre.module').then(
-        (m) => m.TransportistaTerrestreModule
+      import('./tramites/40102/transportista-renovacion.module').then(
+        (m) => m.TransportistaRenovacionModule
       ),
   },
   {
