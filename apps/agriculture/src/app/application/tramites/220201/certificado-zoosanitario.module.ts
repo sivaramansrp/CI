cd92e5ff6cd113586812f60e0d954510168bc102
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-23 16:52:46 +0530

    Refactor certificado-zoosanitario.module.ts for improved organization and clarity

--- apps/agriculture/src/app/application/tramites/220201/certificado-zoosanitario.module.ts
+++ apps/agriculture/src/app/application/tramites/220201/certificado-zoosanitario.module.ts
@@ -1,33 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import {
-  AlertComponent,
-  AnexarDocumentosComponent,
-  BtnContinuarComponent,
-  CatalogoSelectComponent,
-  CrosslistComponent,
-  FirmaElectronicaComponent,
-  InputCheckComponent,
-  InputFechaComponent,
-  InputRadioComponent,
-  SharedModule,
-  SolicitanteComponent,
-  TablaDinamicaComponent,
-  TableComponent,
-  TercerosComponent,
-  TituloComponent,
-  WizardComponent
-} from '@ng-mf/data-access-user';
 
 import { CertificadoZoosanitario } from './certificado-zoosanitario-routing.module';
 import { DatosDeLaSolicitudComponent } from './components/datos-de-la-solicitud/datos-de-la-solicitud.component';
 import { DatosParaMovilizacionNacionalComponent } from './components/datos-para-movilizacion-nacional/datos-para-movilizacion-nacional.component';
+import { NgModule } from '@angular/core';
 import { PagoDeDerechosComponent } from './components/pago-de-derechos/pago-de-derechos.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ZoosanitarioPageComponent } from './pages/zoosanitario-page/zoosanitario-page.component';
 
 import { ToastrModule, ToastrService } from 'ngx-toastr';
@@ -40,33 +20,16 @@ import { ToastrModule, ToastrService } from 'ngx-toastr';
   
   ],
   imports: [
-    SharedModule,
-    CommonModule,
     CertificadoZoosanitario,
-    WizardComponent,
-    TituloComponent,
-    BtnContinuarComponent,
     ReactiveFormsModule,
-    CrosslistComponent,
-    InputCheckComponent,
-    AlertComponent,
-    InputFechaComponent,
-    AnexarDocumentosComponent,
-    FirmaElectronicaComponent,
-    SolicitanteComponent,
-    InputRadioComponent,
-    TableComponent,
-    CatalogoSelectComponent,
-    TercerosComponent,
-    TablaDinamicaComponent,
     PagoDeDerechosComponent,
     ToastrModule.forRoot(),
     DatosDeLaSolicitudComponent,
     DatosParaMovilizacionNacionalComponent,
-        PasoTresComponent,
-        PasoDosComponent,
-           PasoUnoComponent,
-            ZoosanitarioPageComponent,
+    PasoTresComponent,
+    PasoDosComponent,
+    PasoUnoComponent,
+    ZoosanitarioPageComponent,
         
   ],
   providers: [
