a772bb73f62936009c9928010d19e760b935583b
binaydas8 <binaykumar.das@ilink-systems.com>
2025-05-26 19:00:41 +0530

    110101 consulta
    110101 consulta

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -13,7 +13,7 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
     {
         tramite: 110101,
         listaComponentes: [{
-            id: 'solicitud110101',
+            id: 'solicitud',
             componentPath: () => import('../../tramites/110101/pages/datos/datos.component').then(m => m.DatosComponent),
             componentName: 'DatosComponent',
 
