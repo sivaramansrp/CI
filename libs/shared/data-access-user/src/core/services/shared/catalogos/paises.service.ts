9f12aebb5674122c4042e22534f664df3b28cbe6
Dayniz Yael Velasco Coronel <dyvelascovc@desarrollo-ultrasist.com.mx>
2025-05-26 14:57:16 -0600

    Corrige constante en paises servicio

--- libs/shared/data-access-user/src/core/services/shared/catalogos/paises.service.ts
+++ libs/shared/data-access-user/src/core/services/shared/catalogos/paises.service.ts
@@ -1,9 +1,9 @@
 import { Observable, catchError, map, throwError } from 'rxjs';
 import { API_GET_PAISES } from '../../../constants/api-constants';
 import { CatalogoPaisesResponse } from '../../../models/shared/catalogos.model';
+import { ENVIRONMENT } from '../../../../enviroments/enviroment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { enviroment } from '../../../../enviroments/enviroment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +12,7 @@ export class PaisesService {
   private readonly host: string;
 
   constructor(private http: HttpClient) {
-    this.host = `${enviroment.API_HOST}/api/`
+    this.host = `${ENVIRONMENT.API_HOST}/api/`
   }
 
   /**
