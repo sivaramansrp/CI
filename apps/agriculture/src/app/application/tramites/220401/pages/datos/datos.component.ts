93d2c875538bcb9ac80bf4c8dd88c523d939d3ca
Vinothini P <vinothini.p@ilink-systems.com>
2025-05-27 10:30:06 +0530

    create a json

--- apps/agriculture/src/app/application/tramites/220401/pages/datos/datos.component.ts
+++ apps/agriculture/src/app/application/tramites/220401/pages/datos/datos.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+import { map, Subject, takeUntil } from 'rxjs';
+import { Solocitud220401Service } from '../../services/service220401.service';
 /**
  * Este componente se utiliza para mostrar el subtítulo del asistente - 220401
  * Establecer el índice del subtítulo
@@ -9,6 +12,9 @@ import { Component } from '@angular/core';
 })
 
 export class DatosComponent {
+  public esDatosRespuesta: boolean = false;
+   public consultaState!:ConsultaioState;
+   private destroyNotifier$: Subject<void> = new Subject();
   /**
    * Esta variable se utiliza para almacenar el índice del subtítulo.
    */
@@ -18,5 +24,37 @@ export class DatosComponent {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+    constructor(
+    private solocitud220401Service: Solocitud220401Service,
+    private consultaQuery: ConsultaioQuery
+  ) {
+// Constructor vacío: La inicialización se realizará en métodos específicos según sea necesario.
+  }
+
+  
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
+          this.consultaState = seccionState;
+      })).subscribe();
+    if(this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+    guardarDatosFormulario(): void {
+    this.solocitud220401Service
+      .getRegistroTomaMuestrasMercanciasData().pipe(
+        takeUntil(this.destroyNotifier$)
+      )
+      .subscribe((resp) => {
+        if(resp){
+        this.esDatosRespuesta = true;
+        this.solocitud220401Service.actualizarEstadoFormulario(resp);
+        }
+      });
   }
 }
