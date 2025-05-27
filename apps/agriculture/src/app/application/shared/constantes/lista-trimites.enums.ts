cab6c703176328bc7074bbd0069dad74aeaf3008
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-05-27 11:38:34 +0530

    feat: add new tramite and associated components for tramite 220201 in enums and JSON data

--- apps/agriculture/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/agriculture/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -9,4 +9,12 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentName: 'DatosComponent',
         },]
     },
+     {
+        tramite: 220201,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/220201/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'DatosComponent',
+        },]
+    },
 ];
