7f2ec44453c324d9845952aef0d3edf84451e186
Valeria Alvarado <valvaradot@ultrasist.com.mx>
2025-05-27 14:05:16 -0600

    4526 - Implementacion de disabled
    - Implementación de la propiedad disabled en el componente crosslist
    - Implementación de la propiedad disabled en el componente input-fecha

--- libs/shared/data-access-user/src/tramites/components/crosslist/crosslist.component.ts
+++ libs/shared/data-access-user/src/tramites/components/crosslist/crosslist.component.ts
@@ -31,7 +31,11 @@ export class CrosslistComponent implements OnInit, OnChanges {
   @Output() fechasSeleccionadasChange = new EventEmitter<string[]>();
 
   @Input() fechasSeleccionadas: string[] = []; 
-  @Input() fechas: string[] = []; 
+  @Input() fechas: string[] = [];
+  /**
+   * Bandera para indicar si el control debe estar deshabilitado.
+   */
+  @Input() isDisabled!: boolean;
   fecha: FormControl = new FormControl('');
   fechaSeleccionada: FormControl = new FormControl('', [Validators.required]);
   fechasDatos: string[] = [];
@@ -70,6 +74,18 @@ export class CrosslistComponent implements OnInit, OnChanges {
         this.fechasDatos = [...changes['fechas'].currentValue];
         this.fechasSeleccionadas = [];
       }
+    }
+
+    if (changes['isDisabled']) {
+      if (this.isDisabled) {
+        this.fecha.disable();
+        this.fechaSeleccionada.disable();
+      } else {
+        this.fecha.disable();
+        this.fechaSeleccionada.disable();
+      }
+      this.fecha.updateValueAndValidity({ emitEvent: false });
+      this.fechaSeleccionada.updateValueAndValidity({ emitEvent: false });
     }
   }
  
@@ -169,5 +185,14 @@ export class CrosslistComponent implements OnInit, OnChanges {
   isInvalid(): boolean | null {
     const CONTROL = this.fechaSeleccionada;
     return CONTROL ? CONTROL.invalid && CONTROL.touched : null;
+  }
+
+  /**
+   * Establece el estado deshabilitado del control.
+   * @param isDisabled - Indica si el control debe estar deshabilitado.
+   * @returns void
+   */
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 }
