3b07636e6b5a1e436702f8f2735151b65c6f83d1
bhuvaneshwari-ilink <bhuvaneshwari.y@ilink-systems.com>
2025-05-27 12:25:22 +0530

    enable/disable radio/checkbox

--- libs/shared/data-access-user/src/tramites/components/tabla-dinamica/tabla-dinamica.component.ts
+++ libs/shared/data-access-user/src/tramites/components/tabla-dinamica/tabla-dinamica.component.ts
@@ -24,6 +24,10 @@ export class TablaDinamicaComponent<T> {
   @Output() filaClic = new EventEmitter<T>();
 
   @Input() tipoSeleccionTabla!: TablaSeleccion;
+
+  @Input() disableSeleccionTablaCheckBox!: boolean;
+
+  @Input() disableSeleccionTablaRadio!: boolean;
   /*
      * Este valor es necesario para que la plantilla pueda acceder a los diferentes tipos de selección como "CHECKBOX", "RADIO", etc., que definen el comportamiento de la tabla.
      *
