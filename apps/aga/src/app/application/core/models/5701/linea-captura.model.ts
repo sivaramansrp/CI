e8c506965da1b9b099d8699666ad8292e5ce477e
Dayniz Yael Velasco Coronel <dyvelascovc@desarrollo-ultrasist.com.mx>
2025-05-28 09:32:36 -0600

    Documenta linea de captura modelo

--- apps/aga/src/app/application/core/models/5701/linea-captura.model.ts
+++ apps/aga/src/app/application/core/models/5701/linea-captura.model.ts
@@ -1,3 +1,9 @@
+/**
+ * Representa una línea de captura utilizada para pagos o transacciones.
+ *
+ * @property lineaCaptura - Cadena que identifica la línea de captura.
+ * @property monto - Monto asociado a la línea de captura.
+ */
 export interface LineaCaptura {
     lineaCaptura: string;
     monto: number;
