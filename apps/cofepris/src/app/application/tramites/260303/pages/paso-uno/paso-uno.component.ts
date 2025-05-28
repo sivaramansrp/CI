e110d7b82e33251bc08a464d7da4715a817f3311
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-05-28 17:22:40 +0530

    format

--- apps/cofepris/src/app/application/tramites/260303/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260303/pages/paso-uno/paso-uno.component.ts
@@ -33,14 +33,14 @@ export class PasoUnoComponent implements OnInit {
   constructor(private consultaQuery: ConsultaioQuery,private certificadosLicenciasPermisosService: CertificadosLicenciasPermisosService){}
 
     ngOnInit(): void {
-    this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
-          this.consultaState = seccionState;
+      this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$), map((seccionState) => {
+        this.consultaState = seccionState;
       })).subscribe();
-    if(this.consultaState.update) {
-      this.guardarDatosFormulario();
-    } else {
-      this.esDatosRespuesta = true;
-    }
+      if (this.consultaState.update) {
+        this.guardarDatosFormulario();
+      } else {
+        this.esDatosRespuesta = true;
+      }
     }
 
 
