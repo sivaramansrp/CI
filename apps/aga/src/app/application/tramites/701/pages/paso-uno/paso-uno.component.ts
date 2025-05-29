db65618235cc9f0889025b15c468c69267376373
sravanimeegada <sravani.meegada@ilink-systems.com>
2025-05-29 07:54:50 +0530

    done

--- apps/aga/src/app/application/tramites/701/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/701/pages/paso-uno/paso-uno.component.ts
@@ -1,8 +1,11 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject,map,takeUntil } from 'rxjs';
 
 import { BtnContinuarComponent, SolicitanteComponent } from '@ng-mf/data-access-user';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
 
 /**
@@ -15,17 +18,40 @@ import { Component } from '@angular/core';
    standalone: true,
    imports: [BtnContinuarComponent,FormsModule,ReactiveFormsModule,CommonModule,SolicitanteComponent],
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit, OnDestroy {
   /**
    * Índice del paso actual.
    */
   indice: number = 1;
+  public consultaState!:ConsultaioState;
+    private destroyNotifier$: Subject<void> = new Subject();
+  
 
-  /**
-   * Selecciona una pestaña del wizard.
-   * @param {number} i - Índice de la pestaña.
-   */
-  seleccionaTab(i: number): void {
-    this.indice = i;
-  }
+  constructor(
+    private consultaQuery: ConsultaioQuery
+) {}
+
+/**
+ * Selecciona una pestaña del wizard.
+ * @param {number} i - Índice de la pestaña.
+ */
+seleccionaTab(i: number): void {
+  this.indice = i;
+}
+ngOnInit(): void {
+  this.consultaQuery.selectConsultaioState$
+    .pipe(
+      takeUntil(this.destroyNotifier$),
+      map((seccionState: ConsultaioState) => {
+        this.consultaState = seccionState;
+      })
+    )
+    .subscribe();
+}
+
+ngOnDestroy(): void {
+  this.destroyNotifier$.next();
+  this.destroyNotifier$.complete();
+}
+
 }
