a26980eced3777269fb8795700c8aa29b776964f
bhuvaneshwari-ilink <bhuvaneshwari.y@ilink-systems.com>
2025-05-30 11:13:20 +0530

    Routing

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -10,4 +10,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 120204,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/120204/pages/datos/datos.component').then(m => m.DatosComponent),
+            componentName: 'DatosComponent',
+
+        }]
+    },
 ];
