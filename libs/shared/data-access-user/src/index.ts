6a4d98e41d667b9768fe9eceafad7279a24befe5
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-05-30 19:35:07 +0530

    Resolved review comments for CJ

--- libs/shared/data-access-user/src/index.ts
+++ libs/shared/data-access-user/src/index.ts
@@ -89,7 +89,6 @@ export * from './core/services/120402/representacionFederal/representacion-feder
 export * from './core/services/120402/seleccion-del-cupo/seleccion-del-cupo.service';
 export * from './core/services/120501/licitacionesDisponibles.service';
 export * from './core/services/120602/empresa-frontera-solicitud.enum';
-export * from './core/services/130118/pexim/pexim.service';
 export * from './core/services/220471/servicios-pantallas.service';
 export * from './core/services/231001/administrar-residuos.service';
 export * from './core/services/231001/materia-prima-formservice.service';
