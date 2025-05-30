b222c447f336b498c232e59ed86597edab2cf896
sonam-ilink <sonam.rajput@ilink-systems.com>
2025-05-30 17:59:20 +0530

    Added method for paso-uno

--- apps/aga/src/app/application/tramites/630103/pages/autorizacion-importacion-temporal/autorizacion-importacion-temporal.component.ts
+++ apps/aga/src/app/application/tramites/630103/pages/autorizacion-importacion-temporal/autorizacion-importacion-temporal.component.ts
@@ -2,18 +2,12 @@
  * Componente que representa la página de retorno de importación temporal.
  * Permite gestionar los pasos del asistente y la navegación entre ellos.
  */
+import { Component, ViewChild } from '@angular/core';
 
-import { AutorizacionImportacionTemporalService } from '../../services/autorizacion-importacion-temporal.service';
-import { Subject } from 'rxjs';
-
-import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-
-import { AVISO, ConsultaioQuery, ConsultaioState, DatosPasos, PASOS_REGISTRO, SolicitanteComponent, TIPO_PERSONA } from '@ng-mf/data-access-user';
+import { AVISO, DatosPasos, PASOS_REGISTRO } from '@ng-mf/data-access-user';
 import { ListaPasosWizard } from '@ng-mf/data-access-user';
 
 import { WizardComponent } from '@ng-mf/data-access-user';
-
-import { map, takeUntil } from 'rxjs';
 
 /**
  * Interfaz que representa la acción de un botón.
@@ -37,7 +31,7 @@ interface AccionBoton {
   templateUrl:'./autorizacion-importacion-temporal.component.html',
   standalone:false
 })
-export class AutorizacionImportacionTemporalComponent implements OnInit,OnDestroy,AfterViewInit{
+export class AutorizacionImportacionTemporalComponent {
 
 
   /**
@@ -63,55 +57,11 @@ export class AutorizacionImportacionTemporalComponent implements OnInit,OnDestro
    * Variable utilizada para almacenar la lista de pasos.
    */
   pantallasPasos: ListaPasosWizard[] = PASOS_REGISTRO;
-/**
-     * Referencia al componente SolicitanteComponent para acceder a sus métodos y propiedades.
-     */
-    @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
-  
-    /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
-    public esDatosRespuesta: boolean = false;
-  
-    /** Subject para notificar la destrucción del componente. */
-    private destroyNotifier$: Subject<void> = new Subject();
-    public consultaState!:ConsultaioState;
+
   /**
    * Variable utilizada para almacenar el índice del paso actual.
    */
   indice: number = 1;
-   constructor(
-        private autorizacionImportacionTemporalService: AutorizacionImportacionTemporalService,
-        private consultaQuery: ConsultaioQuery,
-       
-      ) {
-    // Constructor vacío: La inicialización se realizará en métodos específicos según sea necesario.
-      }
-    ngOnInit(): void {
-       this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
-             this.consultaState = seccionState;
-         })).subscribe();
-       if(this.consultaState.update) {
-         this.guardarDatosFormulario();
-       } else {
-         this.esDatosRespuesta = true;
-       }
-     }
-   
-     /**
-      * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
-      * Luego reinicializa el formulario con los valores actualizados desde el store.
-      */
-  
-   guardarDatosFormulario(): void {
-      this.autorizacionImportacionTemporalService
-      .getRegistroTomaMuestrasMercanciasData()
-      .pipe(takeUntil(this.destroyNotifier$))
-      .subscribe((resp) => {
-        this.esDatosRespuesta = true;
-        this.autorizacionImportacionTemporalService.actualizarEstadoFormulario(resp);
-      });
-    }
-   
-     
 
   /**
    * Datos para los pasos en el asistente.
@@ -137,18 +87,4 @@ export class AutorizacionImportacionTemporalComponent implements OnInit,OnDestro
       }
     }
   }
-
-   /**
-      * Se ejecuta después de que la vista ha sido inicializada.
-      * Llama al método `obtenerTipoPersona` del componente SolicitanteComponent
-      * para establecer el tipo de persona como MORAL_NACIONAL.
-      */
-     ngAfterViewInit(): void {
-      this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
-    }
-   
-     ngOnDestroy(): void {
-       this.destroyNotifier$.next();
-       this.destroyNotifier$.complete();
-     }  
 }
--- apps/aga/src/app/application/tramites/630103/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/630103/pages/paso-uno/paso-uno.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
+import { Subject } from 'rxjs';
+
+import { AfterViewInit, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ConsultaioState, SolicitanteComponent, TIPO_PERSONA } from '@libs/shared/data-access-user/src';
+import { map, takeUntil } from 'rxjs';
+import { AutorizacionImportacionTemporalService } from '../../services/autorizacion-importacion-temporal.service';
+
 
 /**
  * Componente que representa el primer paso en un proceso de múltiples pasos.
@@ -8,12 +15,23 @@ import { Component } from '@angular/core';
   templateUrl: './paso-uno.component.html',
   standalone:false,
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit,OnDestroy,AfterViewInit {
+  /**
+     * Referencia al componente SolicitanteComponent para acceder a sus métodos y propiedades.
+     */
+    @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
+  
+    /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+    public esDatosRespuesta: boolean = false;
+  
+    /** Subject para notificar la destrucción del componente. */
+    private destroyNotifier$: Subject<void> = new Subject();
+    public consultaState!:ConsultaioState;
   /**
    * El índice de la pestaña actualmente seleccionada.
    */
   indice: number = 1;
-
+ 
   /**
    * Selecciona una pestaña estableciendo su índice.
    * @param i El índice de la pestaña a seleccionar.
@@ -21,4 +39,51 @@ export class PasoUnoComponent {
   seleccionaTab(i: number): void {
     this.indice = i;
   }
+  constructor(
+      private autorizacionImportacionTemporalService: AutorizacionImportacionTemporalService,
+      private consultaQuery: ConsultaioQuery,
+      private cdr: ChangeDetectorRef
+    ) {
+  // Constructor vacío: La inicialización se realizará en métodos específicos según sea necesario.
+    }
+  ngOnInit(): void {
+     this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
+           this.consultaState = seccionState;
+       })).subscribe();
+     if(this.consultaState.update) {
+       this.guardarDatosFormulario();
+     } else {
+       this.esDatosRespuesta = true;
+     }
+   }
+ 
+   /**
+    * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+    * Luego reinicializa el formulario con los valores actualizados desde el store.
+    */
+
+ guardarDatosFormulario(): void {
+    this.autorizacionImportacionTemporalService
+    .getRegistroTomaMuestrasMercanciasData()
+    .pipe(takeUntil(this.destroyNotifier$))
+    .subscribe((resp) => {
+      this.esDatosRespuesta = true;
+      this.autorizacionImportacionTemporalService.actualizarEstadoFormulario(resp);
+    });
+  }
+ 
+   /**
+    * Se ejecuta después de que la vista ha sido inicializada.
+    * Llama al método `obtenerTipoPersona` del componente SolicitanteComponent
+    * para establecer el tipo de persona como MORAL_NACIONAL.
+    */
+   ngAfterViewInit(): void {
+     this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+     this.cdr.detectChanges(); // <-- Agrega esto
+    }
+ 
+   ngOnDestroy(): void {
+     this.destroyNotifier$.next();
+     this.destroyNotifier$.complete();
+   }   
 }
