36bdf95f05e6e0540ca36987ba8bea8ec96a3a37
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-06-02 23:21:25 +0530

    consulta changes

--- apps/se/src/app/application/tramites/80101/component/federatarios-y-plantas-vista/federatarios-y-plantas-vista.component.ts
+++ apps/se/src/app/application/tramites/80101/component/federatarios-y-plantas-vista/federatarios-y-plantas-vista.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { TablaSeleccion } from '@ng-mf/data-access-user';
 
 import {
@@ -11,7 +11,7 @@ import {
   PlantasImmex,
 } from '../../../../shared/models/federatarios-y-plantas.model';
 import { FederatariosYPlantasComponent } from '../../../../shared/components/federatarios-y-plantas/federatarios-y-plantas.component';
-import { Observable } from 'rxjs';
+import { Observable, Subject, takeUntil } from 'rxjs';
 import { Tramite80101Query } from '../../estados/tramite80101.query';
 import { Tramite80101Store } from '../../estados/tramite80101.store';
 
@@ -27,7 +27,13 @@ import { Tramite80101Store } from '../../estados/tramite80101.store';
   templateUrl: './federatarios-y-plantas-vista.component.html',
   styleUrl: './federatarios-y-plantas-vista.component.css',
 })
-export class FederatariosYPlantasVistaComponent {
+export class FederatariosYPlantasVistaComponent implements OnDestroy, OnInit {
+  /**
+   * @property {boolean} formularioDeshabilitado - Indica si el formulario está deshabilitado.Add commentMore actions
+   */
+  @Input() formularioDeshabilitado: boolean = false;
+
+  datosFederatarios!: FederatariosEncabezado;
   /**
    * Configuración de la tabla de federatarios
    * @property {Object} federatariosTablaConfiguracion
@@ -36,6 +42,7 @@ export class FederatariosYPlantasVistaComponent {
     TablaSeleccion: TablaSeleccion.CHECKBOX,
     TablaEncabezado: FEDERATARIOS,
   };
+  private destroy$ = new Subject<void>();
 
   /**
    * Configuración de la tabla de plantas disponibles
@@ -72,31 +79,54 @@ export class FederatariosYPlantasVistaComponent {
    * @property {PlantasImmex[]} plantasImmexTablaLista
    */
   public plantasImmexTablaLista: PlantasImmex[] = [];
-    /**
+  /**
    * Lista de federatarios para mostrar en la tabla
    * @property {FederatariosEncabezado[]} federatariosTablaLista
    */
-    public federatariosTablaLista$!: Observable<FederatariosEncabezado[]>;
+  public federatariosTablaLista$!: Observable<FederatariosEncabezado[]>;
 
   /**
    * Constructor de la clase FederatariosYPlantasVistaComponent.
-   * 
+   *
    * @param store - Inyección de dependencia del servicio `Tramite80101Store` para gestionar el estado de la aplicación.
    * @param query - Inyección de dependencia del servicio `Tramite80101Query` para realizar consultas sobre el estado.
-   * 
+   *
    * Inicializa la propiedad `federatariosTablaLista$` con un observable que selecciona los datos de federatarios.
    */
-  constructor(private store: Tramite80101Store, private query: Tramite80101Query) {
+  constructor(
+    private store: Tramite80101Store,
+    private query: Tramite80101Query
+  ) {
     this.federatariosTablaLista$ = this.query.selectDatosFederatarios$;
   }
 
+  ngOnInit(): void {
+    this.query.selectDatosFederatariosFormulario$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((datos) => {
+        console.log(datos);
+        this.datosFederatarios = datos;
+      });
+  }
   /**
    * Establece los datos de los federatarios en el almacén.
-   * 
+   *
    * @param datos - Objeto de tipo `FederatariosEncabezado` que contiene la información
    *                de los federatarios a ser almacenada.
    */
   setFormaDatos(datos: FederatariosEncabezado): void {
     this.store.setFederatarios(datos);
+  }
+
+  /**
+   * Establece los datos de las plantas disponibles en el almacén.
+   *
+   * @param datos - Objeto de tipo `PlantasDisponibles` que contiene la información
+   *                de las plantas a ser almacenada.
+   */
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
