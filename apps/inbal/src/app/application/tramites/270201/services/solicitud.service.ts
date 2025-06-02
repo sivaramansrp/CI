47a1b5757232c17838d4019fb85dab01dbb82efe
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-06-02 17:27:57 +0530

    Added routing and json for consulta journey

--- apps/inbal/src/app/application/tramites/270201/services/solicitud.service.ts
+++ apps/inbal/src/app/application/tramites/270201/services/solicitud.service.ts
@@ -1,4 +1,6 @@
+import { Tramite270201State, Tramite270201Store } from '../estados/tramites/tramite270201.store';
 import { Catalogo } from '@libs/shared/data-access-user/src';
+import { ENVIRONMENT } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ObraTablaDatos } from '../models/aviso-siglos.models'; 
@@ -15,13 +17,18 @@ import { Observable } from 'rxjs';
 })
 export class SolicitudService {
   /**
+   * AppConfig es una inyección de dependencias que proporciona la configuración de la aplicación.
+   */
+  urlServer = ENVIRONMENT.URL_SERVER;
+  urlServerCatalogos = ENVIRONMENT.URL_SERVER_JSON_AUXILIAR;
+  /**
    * @constructor
    * @description
    * Inicializa el servicio con una instancia de HttpClient para realizar solicitudes HTTP.
    * @param {HttpClient} http - Cliente HTTP para realizar las solicitudes.
    */
-  constructor(private http: HttpClient) {
-    //
+  constructor(private http: HttpClient, private tramite270201Store: Tramite270201Store) {
+    // Lógica de inicialización si es necesario
   }
 
   /**
@@ -112,5 +119,50 @@ export class SolicitudService {
  */
   getObraDeArteTabla(): Observable<ObraTablaDatos> {
     return this.http.get<ObraTablaDatos>('assets/json/270201/obra-de-arte.json');
+  }
+
+  actualizarEstadoFormulario(DATOS: Tramite270201State): void {
+    if(DATOS.operacion){
+      this.tramite270201Store.setOperacion(DATOS.operacion);
+    }
+     if(DATOS.movimiento){
+      this.tramite270201Store.setMovimiento(DATOS.movimiento);
+    }
+    if(DATOS.motivo){
+      this.tramite270201Store.setMotivo(DATOS.motivo);
+    }
+    if(DATOS.pais){
+      this.tramite270201Store.setPais(DATOS.pais);
+    }
+    this.tramite270201Store.setCiudad(DATOS.ciudad);
+    if(DATOS.transporte){
+      this.tramite270201Store.setTransporte(DATOS.transporte);
+    }
+    if(DATOS.aduana){
+      this.tramite270201Store.setAduana(DATOS.aduana);
+    }
+    this.tramite270201Store.setAutor(DATOS.autor);
+    this.tramite270201Store.setTitulo(DATOS.titulo);
+    this.tramite270201Store.setTecnica(DATOS.tecnica);
+    this.tramite270201Store.setAlto(DATOS.alto);
+    this.tramite270201Store.setAncho(DATOS.ancho);
+    this.tramite270201Store.setProfundidad(DATOS.profundidad);
+    this.tramite270201Store.setDiametro(DATOS.diametro);
+    this.tramite270201Store.setVariables(DATOS.variables);
+    this.tramite270201Store.setAnoDeCreacion(DATOS.anoDeCreacion);
+    this.tramite270201Store.setAvaluo(DATOS.avaluo);
+    if(DATOS.moneda){
+      this.tramite270201Store.setMoneda(DATOS.moneda);
+    }
+    this.tramite270201Store.setPropietario(DATOS.propietario);
+    if(DATOS.fraccionArancelaria){
+      this.tramite270201Store.setFraccionArancelaria(DATOS.fraccionArancelaria);
+    }
+    this.tramite270201Store.setDescripcionArancelaria(DATOS.descripcionArancelaria);
+    this.tramite270201Store.setObraDeArte(DATOS.ObraDeArte);
+    }
+
+    getRegistroTomaMuestrasMercanciasData(): Observable<Tramite270201State> {
+    return this.http.get<Tramite270201State>('assets/json/270201/aviso-de-importacion.json');
   }
 }
