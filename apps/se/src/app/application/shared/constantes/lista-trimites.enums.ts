60cfe5d428d2a95dfba76fba4df131c8855a1f6c
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-02 16:10:08 +0530

    Add new tramite and task entries for solicitud de permiso and proceso interno in JSON configurations

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -62,4 +62,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+     {
+        tramite: 80203,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/80203/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    },
 ];
