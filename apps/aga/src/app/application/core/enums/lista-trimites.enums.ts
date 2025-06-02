b7402a8bf19f1545355b33d37a2d0fdb265c4009
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-02 12:33:50 +0530

    JSON list and Path

--- apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
+++ apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
@@ -10,4 +10,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 104,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/104/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    },
 ];
