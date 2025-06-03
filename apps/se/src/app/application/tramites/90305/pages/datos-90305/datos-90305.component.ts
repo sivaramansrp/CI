001c345ea04d1923f9c6c06cc4c884b33cc9c68b
SubodhSingh11 <subodh.singh@ilink-systems.com>
2025-06-03 12:53:27 +0530

    consol error fixed

--- apps/se/src/app/application/tramites/90305/pages/datos-90305/datos-90305.component.ts
+++ apps/se/src/app/application/tramites/90305/pages/datos-90305/datos-90305.component.ts
@@ -105,7 +105,9 @@ export class Datos90305Component implements OnInit, AfterViewInit, OnDestroy {
    * para establecer el tipo de persona como MORAL_NACIONAL.
    */
   ngAfterViewInit(): void {
-    this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+    if (this.solicitante) {
+      this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+    }
   }
 
   /**
