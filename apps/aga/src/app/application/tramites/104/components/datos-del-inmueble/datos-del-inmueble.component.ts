82362c017bbc7736fffcc422bfddcef1257fc8fd
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-03 17:41:54 +0530

    ready only disable

--- apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
+++ apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
@@ -1,8 +1,9 @@
-import { Catalogo, CatalogoSelectComponent, ConsultaioQuery, TableBodyData, TableComponent, TablePaginationComponent, TituloComponent } from '@libs/shared/data-access-user/src';
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Catalogo, CatalogoSelectComponent, ConsultaioQuery,TableBodyData, TableComponent, TablePaginationComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { Component, EventEmitter,Input,OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Subject, distinctUntilChanged,map,takeUntil } from 'rxjs';
+import { Subject, distinctUntilChanged,takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import {ConsultaioState} from '@ng-mf/data-access-user';
 import { DatosDelInmueble104Query } from '../../../../core/queries/tramite104.query';
 import { DatosDelInmueble104Store } from '../../../../core/estados/tramites/tramite104.store';
 import { MENSAJEDE_ALERTA } from '@libs/shared/data-access-user/src/core/enums/104/104.enum';
@@ -29,6 +30,14 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
    * Puede utilizarse para manejar el cierre del componente desde el padre.
    */
   @Output() cerrarClicado = new EventEmitter();
+
+    /**
+  * @property consultaState
+  * @description
+  * Estado actual de la consulta gestionado por el store `ConsultaioQuery`.
+  */
+  @Input() consultaState!: ConsultaioState;
+
 
   /**
    * **Formulario de Fomento a la Exportación**  
@@ -131,15 +140,6 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
 
   constructor(private fb: FormBuilder, private datosDelInmueble104Store: DatosDelInmueble104Store, private datosDelInmueble104Query: DatosDelInmueble104Query,private consultaioQuery: ConsultaioQuery,) {
     // Inicializa
-        this.consultaioQuery.selectConsultaioState$
-        .pipe(
-          takeUntil(this.destroy$),
-          map((seccionState)=>{
-            this.esFormularioSoloLectura = seccionState.readonly; 
-            this.inicializarEstadoFormulario();
-          })
-        )
-        .subscribe()
   }
 
   /**
@@ -168,6 +168,7 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
     });
     this.cargarDatosGuardados(); // Carga los datos guardados en el formulario.
     this.escucharCambiosFormulario();
+    this.deshabilitarFormularios(); // Guarda los datos del formulario en el store.
   }
 
 
@@ -358,32 +359,22 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
   }
 
   /**
- * Evalúa si se debe inicializar o cargar datos en el formulario.  
- * Además, obtiene la información del catálogo de mercancía.
- */
-  inicializarEstadoFormulario(): void {
-    if (this.esFormularioSoloLectura) {
-      this.guardarDatosFormulario();
-    } else {
-      this.inicializarFormulario();
-    }
-  }
-
-    /**
-   * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
-   * Luego reinicializa el formulario con los valores actualizados desde el store.
+   * Habilita o deshabilita los formularios según el estado de solo lectura.
+   * 
+   * Si `consultaState.readonly` es verdadero, deshabilita ambos formularios para que no puedan ser editados.
+   * Si es falso, los habilita para permitir la edición.
    */
-  guardarDatosFormulario(): void {
-    this.inicializarFormulario();
-    if (this.esFormularioSoloLectura) {
+  deshabilitarFormularios(): void {
+    if (this.consultaState.readonly) {
+      // Deshabilita los formularios si el estado es solo lectura
       this.fomentoExportacionForm.disable();
       this.formularioDireccion.disable();
     } else {
+      // Habilita los formularios si el estado permite edición
       this.fomentoExportacionForm.enable();
       this.formularioDireccion.enable();
     }
   }
-
 
   /**
    * **Limpia las suscripciones al destruir el componente**  
