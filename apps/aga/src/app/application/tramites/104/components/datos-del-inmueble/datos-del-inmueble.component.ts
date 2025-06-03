e354d69906d2a6a5ef29574cdbe09ab2c45be1ed
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-03 14:33:35 +0530

    component read only

--- apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
+++ apps/aga/src/app/application/tramites/104/components/datos-del-inmueble/datos-del-inmueble.component.ts
@@ -1,11 +1,10 @@
 import { Catalogo, CatalogoSelectComponent, ConsultaioQuery, TableBodyData, TableComponent, TablePaginationComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Subject, distinctUntilChanged,takeUntil } from 'rxjs';
+import { Subject, distinctUntilChanged,map,takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { DatosDelInmueble104Query } from '../../../../core/queries/tramite104.query';
 import { DatosDelInmueble104Store } from '../../../../core/estados/tramites/tramite104.store';
-import { DepositoFiscalManufacturaVehiculosApiService } from '../../services/deposito-fiscal-manufactura-vehiculos-api.service';
 import { MENSAJEDE_ALERTA } from '@libs/shared/data-access-user/src/core/enums/104/104.enum';
 import { TableData } from '@libs/shared/data-access-user/src/core/models/104/model-104';
 import destinatarioTableData from '@libs/shared/theme/assets/json/104/table-104.json'
@@ -117,14 +116,30 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
    */
   itemsPerPage: number = 5;
 
+    /**
+  * Indica si el formulario está en modo solo lectura.
+  * Cuando es `true`, los campos del formulario no se pueden editar.
+  */
+  esFormularioSoloLectura: boolean = false; 
+
+
   /**
    * **Constructor del componente**  
    * 
    * - Inicializa el `FormBuilder` para la creación de formularios reactivos.
    */
 
-  constructor(private fb: FormBuilder, private datosDelInmueble104Store: DatosDelInmueble104Store, private datosDelInmueble104Query: DatosDelInmueble104Query,private depositoFiscalManufacturaVehiculosApiService:DepositoFiscalManufacturaVehiculosApiService,private consultaQuery: ConsultaioQuery) {
+  constructor(private fb: FormBuilder, private datosDelInmueble104Store: DatosDelInmueble104Store, private datosDelInmueble104Query: DatosDelInmueble104Query,private consultaioQuery: ConsultaioQuery,) {
     // Inicializa
+        this.consultaioQuery.selectConsultaioState$
+        .pipe(
+          takeUntil(this.destroy$),
+          map((seccionState)=>{
+            this.esFormularioSoloLectura = seccionState.readonly; 
+            this.inicializarEstadoFormulario();
+          })
+        )
+        .subscribe()
   }
 
   /**
@@ -341,6 +356,34 @@ export class DatosDelInmuebleComponent implements OnInit, OnDestroy {
     this.currentPage = 1;
     this.updatePagination();
   }
+
+  /**
+ * Evalúa si se debe inicializar o cargar datos en el formulario.  
+ * Además, obtiene la información del catálogo de mercancía.
+ */
+  inicializarEstadoFormulario(): void {
+    if (this.esFormularioSoloLectura) {
+      this.guardarDatosFormulario();
+    } else {
+      this.inicializarFormulario();
+    }
+  }
+
+    /**
+   * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+   * Luego reinicializa el formulario con los valores actualizados desde el store.
+   */
+  guardarDatosFormulario(): void {
+    this.inicializarFormulario();
+    if (this.esFormularioSoloLectura) {
+      this.fomentoExportacionForm.disable();
+      this.formularioDireccion.disable();
+    } else {
+      this.fomentoExportacionForm.enable();
+      this.formularioDireccion.enable();
+    }
+  }
+
 
   /**
    * **Limpia las suscripciones al destruir el componente**  
