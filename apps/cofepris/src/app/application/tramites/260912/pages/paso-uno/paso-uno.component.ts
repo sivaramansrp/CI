65bf96ffaf7911b89bab1244276f1c83c5495985
Swapnali-Mahadar-iLink <swapnali.mahadar@ilink-systems.com>
2025-06-03 19:10:22 +0530

    consulta-journey implemetation started

--- apps/cofepris/src/app/application/tramites/260912/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260912/pages/paso-uno/paso-uno.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+
+import { Subject, map, takeUntil } from 'rxjs';
+import { Solocitud260912Service } from '../../services/service260911.service';
+
 
 /**
  * Componente que representa el primer paso en un proceso de múltiples pasos.
@@ -7,7 +13,74 @@ import { Component } from '@angular/core';
   selector: 'app-paso-uno',
   templateUrl: './paso-uno.component.html',
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit {
+
+   /**
+   * Indica si se han recibido correctamente los datos desde el servidor.
+   */
+  public esDatosRespuesta: boolean = false;
+
+  /**
+   * Subject utilizado para cancelar suscripciones y evitar fugas de memoria al destruir el componente.
+   * Se emite un valor y se completa cuando el componente se destruye.
+   * @private
+   */
+  private destroyNotifier$: Subject<void> = new Subject();
+
+  /**
+   * Estado actual de la consulta obtenido desde el store.
+   */
+  public consultaState!: ConsultaioState;
+
+  /**
+   * Constructor del componente.
+   * @param consultaQuery Consulta de estado de solo lectura.
+   * @param solocitud260912Service Servicio para obtener y actualizar datos del formulario.
+   */
+  constructor(
+    private consultaQuery: ConsultaioQuery,
+    private solocitud260912Service: Solocitud260912Service,
+  ) {}
+
+  /**
+   * Hook del ciclo de vida de Angular.
+   * Se ejecuta al inicializar el componente y se suscribe al estado del store.
+   * Si el estado indica actualización, solicita los datos del formulario.
+   */
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((seccionState) => {
+          this.consultaState = seccionState;
+        })
+      )
+      .subscribe();
+
+    if (this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+  /**
+   * Solicita los datos del[] formulario al servicio y actualiza el store si la respuesta es válida.
+   * Marca la bandera de datos recibidos si la respuesta es exitosa.
+   */
+  guardarDatosFormulario(): void {
+    this.solocitud260912Service
+      .getRegistroTomaMuestrasMercanciasData()
+      .pipe(takeUntil(this.destroyNotifier$))
+      .subscribe((resp) => {
+        if (resp) {
+          this.esDatosRespuesta = true;
+          this.solocitud260912Service.actualizarEstadoFormulario(resp);
+        }
+      });
+  }
+
+
   /**
    * El índice de la pestaña actualmente seleccionada.
    */
