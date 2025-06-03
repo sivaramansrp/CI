66e5f6148a81d7937321c5856e02f1a4a6c684d7
Mkushwah1001 <madhu.kushwah@ilink-systems.com>
2025-06-03 18:33:59 +0530

    80207-comment resolved

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -50,7 +50,7 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         listaComponentes: [{
             id: 'solicitud',
             componentPath: () => import('../../tramites/80207/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
-            componentName: 'DatosComponent',
+            componentName: 'PasoUnoComponent',
 
         }]
     },
