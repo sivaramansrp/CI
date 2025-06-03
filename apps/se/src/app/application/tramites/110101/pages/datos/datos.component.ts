8ced0ff17d37e251b2b82f3cecbf4b9bb0b70277
binaydas8 <binaykumar.das@ilink-systems.com>
2025-06-03 13:05:55 +0530

    bugfix
    bugfix

--- apps/se/src/app/application/tramites/110101/pages/datos/datos.component.ts
+++ apps/se/src/app/application/tramites/110101/pages/datos/datos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ConsultaioQuery, ConsultaioState } from '@libs/shared/data-access-user/src';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
 import { Subject,map, takeUntil } from 'rxjs';
 import { PantallasSvcService } from '../../services/pantallas-svc.service';
 
@@ -49,6 +49,7 @@ export class DatosComponent implements OnInit, OnDestroy {
     this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
         this.consultaState = seccionState;
     })).subscribe();
+    console.log('DatosComponent - ngOnInit', this.consultaState);
     if(this.consultaState.update) {
       this.guardarDatosFormulario();
     }
