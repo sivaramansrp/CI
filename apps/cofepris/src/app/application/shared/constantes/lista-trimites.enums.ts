e557f8fae0f14df7edc5000d168d3a49e878175d
atripathi860 <amit.tripathi@ilink-systems.com>
2025-06-04 10:40:57 +0530

    consulta for 4th tab service

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -18,5 +18,14 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentName: 'Datos260212Component',
 
         }]
+    },
+    {
+        tramite: 260501,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/260501/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
     }
 ];
