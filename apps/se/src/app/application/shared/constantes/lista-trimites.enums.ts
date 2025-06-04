aa3732733f4149287c1a2ab5f8b1cd3976bca89d
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-04 10:44:13 +0530

    consulta flow

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -174,5 +174,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/90201/pages/datos/datos.component').then(m => m.DatosComponent),
             componentName: 'DatosComponent',
       }]
-    }
+    },
+  {
+    tramite: 110212,
+    listaComponentes: [{
+      id: 'solicitud',
+      componentPath: () => import('../../tramites/110212/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+      componentName: 'PasoUnoComponent',
+    }]
+  }
 ];
