f21c5ab5b20ac69a44988cbe8f5dcc1b125d3667
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-04 15:26:37 +0530

    Refactor CamCertificadoService for improved readability and maintainability

--- apps/se/src/app/application/tramites/110211/services/cam-certificado.service.ts
+++ apps/se/src/app/application/tramites/110211/services/cam-certificado.service.ts
@@ -1,5 +1,6 @@
 import { Catalogo, RespuestaCatalogos } from '@ng-mf/data-access-user';
 import { Observable, map } from 'rxjs';
+import { CamState } from '../estados/cam-certificado.store';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Mercancia } from '../../../shared/models/modificacion.enum';
@@ -58,4 +59,27 @@ export class CamCertificadoService {
     const JSON_URL = this.url + fileName;
       return this.http.get<Mercancia[]>(JSON_URL);
   }
+
+  /**
+   * @description Obtiene todos los datos del certificado CAM desde un archivo JSON ubicado en la URL especificada.
+   * @param fileName El nombre del archivo JSON desde el cual se obtendrán los datos.
+   * @returns Un `Observable` que emite un objeto `CamState`.
+   * @method obtenerTodosDatosCamCertificado
+   * @memberof CamCertificadoService
+   * @usageNotes
+   * Este método construye la URL completa añadiendo el `fileName` a la URL base (`this.url`) 
+   * y realiza una solicitud HTTP GET para recuperar los datos.
+   * 
+   * Ejemplo:
+   * ```typescript
+   * this.camCertificadoService.obtenerTodosDatosCamCertificado('camcertificado.json').subscribe(data => {
+   *   console.log(data);
+   * });
+   * ```
+   */
+  obtenerTodosDatosCamCertificado(fileName: string): Observable<CamState> {
+    const JSON_URL = this.url + fileName;
+    return this.http.get<CamState>(JSON_URL);
+  }
+
 }
