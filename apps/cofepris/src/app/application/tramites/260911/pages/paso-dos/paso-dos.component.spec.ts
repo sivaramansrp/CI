a1926d854c1ab91d9fa40b2119a7b96fa514e429
Swapnali-Mahadar-iLink <swapnali.mahadar@ilink-systems.com>
2025-06-04 16:24:27 +0530

    removed unwanted lines

--- apps/cofepris/src/app/application/tramites/260911/pages/paso-dos/paso-dos.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/260911/pages/paso-dos/paso-dos.component.spec.ts
@@ -9,7 +9,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
-// Best approach: Simple test without template override
+
 describe('PasoDosComponent - Recommended Approach', () => {
   let component: PasoDosComponent;
   let fixture: ComponentFixture<PasoDosComponent>;
