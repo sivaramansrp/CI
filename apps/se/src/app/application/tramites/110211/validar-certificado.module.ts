01bd902561ffe37c51b20fff76f63a5ac85a9a4a
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-04 15:14:37 +0530

    Refactor ValidarCertificadoModule for improved structure and maintainability

--- apps/se/src/app/application/tramites/110211/validar-certificado.module.ts
+++ apps/se/src/app/application/tramites/110211/validar-certificado.module.ts
@@ -1,16 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-
-import { BtnContinuarComponent, CatalogoSelectComponent, FirmaElectronicaComponent, InputFechaComponent, SharedModule, SolicitanteComponent, TituloComponent, WizardComponent } from '@libs/shared/data-access-user/src';
+import { BtnContinuarComponent, CatalogoSelectComponent, FirmaElectronicaComponent, SharedModule, SolicitanteComponent, TituloComponent, WizardComponent } from '@libs/shared/data-access-user/src';
 import { CamCertificadoComponent } from './page/cam-certificado/cam-certificado.component';
-import { CamDatosCertificadoComponent } from './components/cam-datos-certificado/cam-datos-certificado.component';
-import { CamDestinatarioComponent } from './components/cam-destinatario/cam-destinatario.component';
-import { CertificadoDeOrigenComponent } from '../../shared/components/certificado-de-origen/certificado-de-origen.component';
-import { CertificadoOrigenComponent } from './components/certificado-origen/certificado-origen.component';
-import { DatosCertificadoDeComponent } from '../../shared/components/datos-certificado-de/datos-certificado-de.component';
-import { DatosDelDestinatarioComponent } from '../../shared/components/datos-del-destinatario/datos-del-destinatario.component';
+import { CommonModule } from '@angular/common';
 import { DestinatarioComponent } from '../../shared/components/destinatario/destinatario.component';
-import { MercanciaComponent } from './components/mercancia/mercancia.component';
+import { NgModule } from '@angular/core';
 import { PasoDosComponent } from './page/paso-dos/paso-dos.component';
 import { PasoUnoComponent } from './page/paso-uno/paso-uno.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -34,16 +26,8 @@ import { ValidarCertificadoRoutingModule } from './validar-certificado-routing.m
     FirmaElectronicaComponent,
     CatalogoSelectComponent,
     RouterModule,
-    InputFechaComponent,
-    CertificadoDeOrigenComponent,
-    DatosCertificadoDeComponent,
-    DatosDelDestinatarioComponent,
     DestinatarioComponent,
     TituloComponent,
-    CertificadoOrigenComponent,
-    MercanciaComponent,
-    CamDestinatarioComponent,
-    CamDatosCertificadoComponent,
     PasoUnoComponent,
   ]
 })
