5a2b12a46746e806facf98c372924978b00802ea
revankale-ilink <revansiddh.kale@ilink-systems.com>
2025-06-04 19:26:21 +0530

    110205 consulta

--- apps/se/src/app/application/tramites/110205/components/mercancia/mercancia.component.ts
+++ apps/se/src/app/application/tramites/110205/components/mercancia/mercancia.component.ts
@@ -1,4 +1,4 @@
-import { Catalogo, InputFecha, Notificacion, SeccionLibQuery, SeccionLibState } from '@libs/shared/data-access-user/src';
+import { Catalogo, ConsultaioQuery, InputFecha, Notificacion, SeccionLibQuery, SeccionLibState } from '@libs/shared/data-access-user/src';
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject, delay, map, of, takeUntil } from 'rxjs';
@@ -108,6 +108,12 @@ export class MercanciaComponent implements OnInit, OnDestroy {
 
   /**
    * @descripcion
+   * Indica si el formulario de mercancía se encuentra en modo solo lectura.
+   */
+   @Input() esFormularioSoloLectura!: boolean;
+
+  /**
+   * @descripcion
    * Constructor que inicializa los servicios y dependencias requeridas.
    * @param fb - Instancia de FormBuilder para gestionar formularios.
    * @param peruCertificadoService - Servicio para obtener datos relacionados con el certificado.
@@ -121,7 +127,8 @@ export class MercanciaComponent implements OnInit, OnDestroy {
     private peruCertificadoService: PeruCertificadoService,
     private store: Tramite110205Store,
     private query: Tramite110205Query,
-    private seccionQuery: SeccionLibQuery
+    private seccionQuery: SeccionLibQuery,
+    private consultaQuery: ConsultaioQuery
   ) {}
 
   /**
@@ -145,6 +152,15 @@ export class MercanciaComponent implements OnInit, OnDestroy {
         map((state) => {
           this.mercanciaState = state as Tramite110205State;
           this.initActionFormBuild();
+        })
+      )
+      .subscribe();
+
+    this.consultaQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((seccionState) => {
+          this.esFormularioSoloLectura = seccionState.readonly;
         })
       )
       .subscribe();
