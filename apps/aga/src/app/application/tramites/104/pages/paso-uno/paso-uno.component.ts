e16a41e8b5e8db00f197a072005bb29a466d27d4
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-04 11:20:51 +0530

    import

--- apps/aga/src/app/application/tramites/104/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/104/pages/paso-uno/paso-uno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ConsultaioQuery, ConsultaioState } from '@libs/shared/data-access-user/src';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
 import {Subject, map, takeUntil } from 'rxjs';
 import { DepositoFiscalManufacturaVehiculosApiService } from '../../services/deposito-fiscal-manufactura-vehiculos-api.service';
 /**
