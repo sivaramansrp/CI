8c93d3b3da740bacf58d7de8edf8901049d3d807
atripathi860 <amit.tripathi@ilink-systems.com>
2025-06-04 18:01:02 +0530

    pull from develop

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -37,4 +37,22 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 260502,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/260502/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'DatosComponent',
+
+        }]
+    },
+    {
+        tramite: 260501,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/260501/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    }
 ];
