72428201d89e7e433476343f3a37b227fc00fc8a
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-05 13:25:22 +0530

    Update lista-trimites.enums.ts with new enum values

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -330,5 +330,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         componentPath: () => import('../../tramites/80203/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
         componentName: 'PasoUnoComponent',
     }]
+  },
+    {
+    tramite: 110211,
+    listaComponentes: [{
+        id: 'solicitud',
+        componentPath: () => import('../../tramites/110211/page/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+    }]
   }
 ];
