3e1d1193aba830f9e0cfa220a1cfe35fdfccd6f3
binaydas8 <binaykumar.das@ilink-systems.com>
2025-06-05 15:16:02 +0530

    paso update
    paso update

--- apps/aga/src/app/application/tramites/6001/services/registro-cuentas-bancarias.enum.ts
+++ apps/aga/src/app/application/tramites/6001/services/registro-cuentas-bancarias.enum.ts
@@ -1,20 +1,20 @@
 export const PANTA_PASOS = [
-    {
-      indice: 1,
-      titulo: 'Capturar solicitud',
-      activo: true,
-      completado: false,
-    },
-      {
+  {
+    indice: 1,
+    titulo: 'Capturar solicitud',
+    activo: true,
+    completado: false,
+  },
+  {
       indice: 2,
       titulo: 'Anexar requisitos',
       activo: false,
       completado: false,
-    },
-    {
+  },
+  {
       indice: 3,
       titulo: 'Firmar solicitud',
       activo: false,
       completado: false,
-    }
-  ];
+  },
+];
