a7f98bded55372ab4ccc37a9eb54661b9c556393
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-05 16:48:01 +0530

    test: enhance MercanciaComponent tests with mock services and improved setup

--- apps/se/src/app/application/tramites/110211/components/mercancia/mercancia.component.spec.ts
+++ apps/se/src/app/application/tramites/110211/components/mercancia/mercancia.component.spec.ts
@@ -1,29 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MercanciaComponent } from './mercancia.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injectable, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CamCertificadoService } from '../../services/cam-certificado.service';
+@Injectable()
+class MockFitosanitarioService {}
 
 describe('MercanciaComponent', () => {
   let component: MercanciaComponent;
   let fixture: ComponentFixture<MercanciaComponent>;
+   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule,MercanciaComponent],
+      declarations: [
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      providers: [
+        FormBuilder,
+        { provide: CamCertificadoService, useClass: MockFitosanitarioService }
+      ]
+    }).overrideComponent(MercanciaComponent,  {
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [],
-      imports: [HttpClientTestingModule,MercanciaComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
     }).compileComponents();
-
     fixture = TestBed.createComponent(MercanciaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.debugElement.componentInstance;
   });
 
-it('should create', async () => {
-  const fixture = TestBed.createComponent(MercanciaComponent);
-  const component = fixture.componentInstance;
-  fixture.detectChanges();
-  expect(component).toBeTruthy();
-});
+  it('should run #constructor()', async () => {
+    expect(component).toBeTruthy();
+  });
+
 
 });
