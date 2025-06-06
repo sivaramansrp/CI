c0d54022165baff662b8f885aaefa2105e245389
Aditi <aditi.patil@ilink-systems.com>
2025-06-06 11:30:12 +0530

    json and component added

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -270,5 +270,14 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/120204/pages/datos/datos.component').then(m => m.DatosComponent),
             componentName: 'DatosComponent',
         }]
-    }
+    },
+    {
+        tramite: 130113,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/130113/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    },
 ];
