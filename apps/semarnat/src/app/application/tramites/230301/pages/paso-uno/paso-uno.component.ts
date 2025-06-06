2d72250583a8f5a645da1bc87dd26d3c0a1aef3b
muneez-ilink <muneeshwaran.n@ilink-systems.com>
2025-06-06 11:52:57 +0530

    initial set up changes for paso compoenent

--- apps/semarnat/src/app/application/tramites/230301/pages/paso-uno/paso-uno.component.ts
+++ apps/semarnat/src/app/application/tramites/230301/pages/paso-uno/paso-uno.component.ts
@@ -1,12 +1,15 @@
-import { AVISO, SeccionLibStore } from '@libs/shared/data-access-user/src';
-import { Component } from '@angular/core';
+import { AVISO, ConsultaioQuery, ConsultaioState, SeccionLibStore } from '@ng-mf/data-access-user';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, map, takeUntil } from 'rxjs';
+import { DesistimientoSolicitudService } from '../../services/desistimiento-solicitud.service';
+import { Solicitud230301Store } from '../../estados/tramites/tramites230301.store';
 
 @Component({
   selector: 'app-paso-uno',
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.scss',
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit , OnDestroy{
 
   /**
    * @property {number} indice - Índice actual del paso.
@@ -16,9 +19,55 @@ export class PasoUnoComponent {
    * Asigna el aviso de privacidad simplificado al atributo `TEXTOS`.
    */
   TEXTOS = AVISO.Aviso;
+  /**
+   * Esta variable se utiliza para almacenar el índice del subtítulo.
+   */
+  public consultaState!: ConsultaioState;
 
-   constructor(private seccionStore: SeccionLibStore) {
-        // Se puede agregar aquí la lógica del constructor si es necesario
+  /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+  public esDatosRespuesta: boolean = false;
+
+  /**
+   * Subject utilizado para notificar la destrucción del componente y evitar fugas de memoria.
+   * @type {Subject<void>}
+   */
+  public formularioDeshabilitado: boolean = false;
+
+  /**
+   * Subject para notificar la destrucción del componente y evitar fugas de memoria.
+   * @type {Subject<void>}
+   */
+  public destroyNotifier$: Subject<void> = new Subject();
+
+  /**
+   * Constructor del componente `PasoUnoComponent`.
+   * @param {Solicitud230301Store} tramite230301Store - Almacén para gestionar el estado de la solicitud.
+   * @param {ConsultaioQuery} consultaQuery - Consulta para obtener el estado de la consulta.
+   * @param {DesistimientoSolicitudService} desistimientoSolicitudService - Servicio para manejar solicitudes de desistimiento.
+   * @description
+   * Este constructor inicializa el componente y se suscribe al estado de la consulta para obtener
+   * información sobre el procedimiento actual. Si el estado de la consulta indica que se debe actualizar,
+   * se llama al método `guardarDatosFormulario` para cargar los datos desde un archivo JSON.
+   * @returns {void}
+   */
+   constructor(private tramite230301Store:Solicitud230301Store,
+    private consultaQuery: ConsultaioQuery,
+    private desistimientoSolicitudService: DesistimientoSolicitudService) {
+       this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$), map((seccionState) => {
+      this.consultaState = seccionState;
+      this.formularioDeshabilitado = seccionState.readonly;
+    })).subscribe();
+   }
+
+
+   ngOnInit(): void {
+    // this.consultaState && this.consultaState.procedureId === '230301' &&
+      if (
+      this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
    }
 
   /**
@@ -30,5 +79,29 @@ export class PasoUnoComponent {
   seleccionaTab(i: number): void {
     this.indice = i;
   }
-  
+
+  /**
+ * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+ * Luego reinicializa el formulario con los valores actualizados desde el store.
+ */
+  guardarDatosFormulario(): void {
+    this.desistimientoSolicitudService
+      .getRegistroTomaMuestrasMercanciasData().pipe(
+        takeUntil(this.destroyNotifier$)).subscribe((resp) => {
+          if (resp) {
+            this.esDatosRespuesta = true;
+            this.desistimientoSolicitudService.actualizarEstadoFormulario(resp);
+          }
+        });
+  }
+
+  /**
+   * Método del ciclo de vida de Angular que se ejecuta al destruir el componente.
+   * Emite un valor en `destroyNotifier$` y completa el observable para limpiar las suscripciones activas.
+   * @returns {void}
+   */
+  ngOnDestroy(): void {
+    this.destroyNotifier$.next();
+    this.destroyNotifier$.complete();
+  }
 }
