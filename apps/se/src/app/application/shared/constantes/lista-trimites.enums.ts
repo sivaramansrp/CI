d9bb8031efe8a046e4fa6a8ca50f1301ea91ae5a
Rajalakshmi Lakshmanan <rajalakshmi.lakshmanan@ilink-systems.com>
2025-06-09 15:56:54 +0530

    Added in shared components

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -412,5 +412,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         componentPath: () => import('../../tramites/110211/page/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
         componentName: 'PasoUnoComponent',
   }]
+},
+{
+      tramite: 120401,
+    listaComponentes: [{
+        id: 'solicitud',
+        componentPath: () => import('../../tramites/120401/pages/datos/datos.component').then(m => m.DatosComponent),
+        componentName: 'DatosComponent',
+  }]
 }
 ];
