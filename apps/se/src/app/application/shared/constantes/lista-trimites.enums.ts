a4a475e10199163af77a786bf129c39ddd98bcff
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-06-09 18:09:08 +0530

    updated lista-trimites and bandeja-solicitud-tabla.json

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -387,8 +387,33 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       componentPath: () => import('../../tramites/130217/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
       componentName: 'PasoUnoComponent',
     }]
+    },
+    {
+       tramite: 150102,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/150102/pages/datos/datos.component').then(m => m.DatosComponent),
+            componentName: 'DatosComponent',
+        }]
   },
-   {
+  {
+    tramite: 150103,
+    listaComponentes: [{
+        id: 'solicitud',
+        componentPath: () => import('../../tramites/150103/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+
+    }]
+},
+ {
+      tramite: 110211,
+    listaComponentes: [{
+        id: 'solicitud',
+        componentPath: () => import('../../tramites/110211/page/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+  }]
+},
+ {
     tramite: 110201,
     listaComponentes: [{
       id: 'solicitud',
