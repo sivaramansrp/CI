5f29586e386312bda5cf86ebd0243475085e6751
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-06-09 14:58:54 +0530

    consulta journey implemented

--- apps/cofepris/src/app/application/tramites/260401/pages/datos-territorio.component/datos-territorio.component.ts
+++ apps/cofepris/src/app/application/tramites/260401/pages/datos-territorio.component/datos-territorio.component.ts
@@ -2,7 +2,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
 import { SolicitanteComponent, TIPO_PERSONA } from '@libs/shared/data-access-user/src';
-import { Subject, map, takeUntil } from 'rxjs';
+import { Subject, forkJoin, map, takeUntil } from 'rxjs';
 import { Solicitud260401Service } from '../../services/service260401.service';
 
 /**
@@ -17,11 +17,17 @@ import { Solicitud260401Service } from '../../services/service260401.service';
 })
 export class DatosTerritorioComponent implements AfterViewInit, OnInit {
 
-
+  /**
+   * Indica si se están mostrando los datos de respuesta.
+   */
   public esDatosRespuesta: boolean = false;
-
+  /**
+   * Estado actual de la consulta.
+   */
   public consultaState!: ConsultaioState;
-
+  /**
+   * Notificador para destruir las suscripciones y evitar fugas de memoria.
+   */
   private destroyNotifier$: Subject<void> = new Subject();
   
   /**
@@ -29,38 +35,62 @@ export class DatosTerritorioComponent implements AfterViewInit, OnInit {
    */
   @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
 
+  /**
+ * Constructor del componente.
+ *
+ * Se inyectan los siguientes servicios:
+ * - `ConsultaioQuery`: Servicio para consultar y acceder al estado de la solicitud desde la capa de estado (por ejemplo, usando Akita).
+ * - `Solicitud260401Service`: Servicio que maneja las operaciones relacionadas con la solicitud 260401, como guardar, actualizar o recuperar información.
+ *
+ * Aunque el constructor está vacío, esta inyección permite el uso de estos servicios en los métodos del componente.
+ *
+ * @param consultaQuery Servicio para acceder al estado reactivo de la solicitud.
+ * @param solicitud260401Service Servicio que gestiona las operaciones del proceso 260401.
+ */
   constructor(private consultaQuery: ConsultaioQuery, private solicitud260401Service: Solicitud260401Service) {
     // Constructor del componente, se pueden inyectar servicios aquí si es necesario.
   }
 
- ngOnInit(): void {
+   /**
+   * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+   * Se suscribe al estado de consulta y, dependiendo de si hay una actualización,
+   * guarda los datos del formulario o muestra la respuesta.
+   */
+  ngOnInit(): void {
     this.consultaQuery.selectConsultaioState$
       .pipe(
-        takeUntil(this.destroyNotifier$),
-        map((seccionState) => {
-          this.consultaState = seccionState;
+        takeUntil(this.destroyNotifier$))
+        .subscribe((seccionState) => {
+          this.consultaState = seccionState
+          if (this.consultaState.update) {
+             this.guardarDatosFormulario()
+             } else {
+              this.esDatosRespuesta = true;
+            }
         })
-      )
-      .subscribe();
-    if (this.consultaState.update) {
-      // this.guardarDatosFormulario();
-    } else {
-      this.esDatosRespuesta = true;
-    }
   }
 
-  // guardarDatosFormulario(): void {
-  //   this.solicitud260401Service
-  //     .getRegistroTomaMuestrasMercanciasData().pipe(
-  //       takeUntil(this.destroyNotifier$)
-  //     )
-  //     .subscribe((resp) => {
-  //       if(resp){
-  //       this.esDatosRespuesta = true;
-  //       this.solicitud260401Service.actualizarEstadoFormulario(resp);
-  //       }
-  //     });
-  // }
+  /**
+   * Método para guardar los datos del formulario.
+   * Realiza llamadas a los servicios para obtener los datos de registro y pago de derechos,
+   * y actualiza el estado del formulario según la respuesta.
+   */
+  guardarDatosFormulario(): void {
+    forkJoin({
+      registro: this.solicitud260401Service.getRegistroTomaMuestrasMercanciasData(),
+      permiso: this.solicitud260401Service.getPagoDerechos()
+    })
+      .pipe(takeUntil(this.destroyNotifier$))
+      .subscribe(({ registro, permiso }) => {
+        if (registro) {
+          this.esDatosRespuesta = true;
+          this.solicitud260401Service.actualizarEstadoFormulario(registro);
+        }
+        if (permiso) {
+          this.solicitud260401Service.actualizarPagoDerechosFormulario(permiso);
+        }
+      });
+  }
 
   /**
    * Se ejecuta después de que la vista ha sido inicializada.
--- apps/cofepris/src/app/application/tramites/260401/services/service260401.service.ts
+++ apps/cofepris/src/app/application/tramites/260401/services/service260401.service.ts
@@ -1,12 +1,17 @@
+import { PermisoImportacionBiologicaState, PermisoImportacionBiologicaStore } from '../../../shared/estados/permiso-importacion-biologica.store';
+import { DatosDelSolicituteSeccionState } from '../../../shared/estados/stores/datos-del-solicitute-seccion.store';
+import { DatosDelSolicituteSeccionStateStore } from '../../../shared/estados/stores/datos-del-solicitute-seccion.store';
 import { ENVIRONMENT } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { DatosDelSolicituteSeccionState } from '../../../shared/estados/stores/datos-del-solicitute-seccion.store';
-import { DatosDelSolicituteSeccionStateStore } from '../../../shared/estados/stores/datos-del-solicitute-seccion.store';
-
-
+/**
+ * Servicio para manejar la lógica relacionada con Solicitud260401.
+ * 
+ * @export
+ * @class Solicitud260401Service
+ */
 @Injectable({
   providedIn: 'root',
 })
@@ -20,9 +25,80 @@ export class Solicitud260401Service {
      */
   urlServerCatalogos = ENVIRONMENT.URL_SERVER_JSON_AUXILIAR;
 
-  constructor(private http: HttpClient, private tramite301Store: DatosDelSolicituteSeccionStateStore,) {
+  /**
+ * Constructor del servicio o componente.
+ *
+ * Inyecta los siguientes servicios y stores:
+ * 
+ * - `HttpClient`: Servicio para realizar peticiones HTTP.
+ * - `DatosDelSolicituteSeccionStateStore`: Store que maneja el estado de la sección de datos del solicitante.
+ * - `PermisoImportacionBiologicaStore`: Store que administra el estado relacionado con permisos de importación biológica.
+ *
+ * El constructor puede incluir lógica de inicialización si es necesario, aunque actualmente está vacío.
+ *
+ * @param http Servicio Angular para solicitudes HTTP.
+ * @param tramite260401Store Store para gestionar estado de datos de solicitante.
+ * @param tramite260401 Store para gestionar estado del permiso de importación biológica.
+ */
+  constructor(private http: HttpClient, private tramite260401Store: DatosDelSolicituteSeccionStateStore,private tramite260401:PermisoImportacionBiologicaStore) {
     // Lógica de inicialización si es necesario
   }
 
+  /**
+   * Actualiza el estado del formulario con los datos proporcionados.
+   * @param DATOS Estado con la información del solicitante y establecimiento.
+   */
+  actualizarEstadoFormulario(DATOS: DatosDelSolicituteSeccionState): void {
+    this.tramite260401Store.setEstablecimientoDenominacionRazonSocial(DATOS.establecimientoDenominacionRazonSocial);
+    this.tramite260401Store.setEstablecimientoCorreoElectronico(DATOS.establecimientoCorreoElectronico);
+    this.tramite260401Store.setEstablecimientoDomicilioCodigoPostal(DATOS.establecimientoDomicilioCodigoPostal);
+    this.tramite260401Store.setEstablecimientoDomicilioEstado(DATOS.establecimientoDomicilioEstado);
+    this.tramite260401Store.setEstablecimientoMunicipioYAlcaldia(DATOS.establecimientoMunicipioYAlcaldia);
+    this.tramite260401Store.setEstablecimientoDomicilioLocalidad(DATOS.establecimientoDomicilioLocalidad);
+    this.tramite260401Store.setEstablecimientoDomicilioColonia(DATOS.establecimientoDomicilioColonia);
+    this.tramite260401Store.setEstablecimientoDomicilioCalle(DATOS.establecimientoDomicilioCalle);
+    this.tramite260401Store.setEstablecimientoDomicilioLada(DATOS.establecimientoDomicilioLada);
+    this.tramite260401Store.setEstablecimientoDomicilioTelefono(DATOS.establecimientoDomicilioTelefono);
+    this.tramite260401Store.setRfcDelProfesionalResponsable(DATOS.rfcDelProfesionalResponsable);
+    this.tramite260401Store.setNombreDelProfesionalResponsable(DATOS.nombreDelProfesionalResponsable);
+    this.tramite260401Store.setRepresentanteRfc(DATOS.representanteRfc);
+    this.tramite260401Store.setRepresentanteNombre(DATOS.representanteNombre); 
+    this.tramite260401Store.setRepresentanteApellidos(DATOS.apellidoMaterno,DATOS.apellidoPaterno);
+    this.tramite260401Store.setInformacionConfidencial(DATOS.informacionConfidencialRadio)
+    this.tramite260401Store.setAduanaDeSalida(DATOS.aduanaDeSalida);
+    this.tramite260401Store.setRegimenAlQueSeDestinaraLaMercancía(DATOS.regimenAlQueSeDestinaraLaMercancía);
+    this.tramite260401Store.setNoDeLicenciaSanitariaObservaciones(DATOS.noDeLicenciaSanitariaObservaciones);
+    this.tramite260401Store.setNoDeLicenciaSanitaria(DATOS.noDeLicenciaSanitaria);
+  }
 
+  /**
+ * Actualiza los datos relacionados con el pago de derechos en el formulario,
+ * utilizando la información proporcionada en el estado de PermisoImportacionBiologica.
+ *
+ * @param DATOS - Objeto que contiene los datos necesarios para actualizar el pago de derechos,
+ * incluyendo clave de referencia, cadena de la dependencia, llave de pago, fecha e importe.
+ */
+actualizarPagoDerechosFormulario(DATOS: PermisoImportacionBiologicaState): void {
+    this.tramite260401.setClaveDeReferncia(DATOS.setClaveDeReferncia);
+    this.tramite260401.setCadenaDeLaDependencia(DATOS.setCadenaDeLaDependencia);
+    this.tramite260401.setLlaveDePago(DATOS.setLlaveDePago);
+    this.tramite260401.setFechaDePago(DATOS.setFechaDePago);
+    this.tramite260401.setImporteDePago(DATOS.setImporteDePago);
+}
+
+  /**
+   * Obtiene los datos del registro de toma de muestras de mercancías.
+   * @returns Observable con el estado de los datos del solicitante.
+   */
+  getRegistroTomaMuestrasMercanciasData(): Observable<DatosDelSolicituteSeccionState> {
+    return this.http.get<DatosDelSolicituteSeccionState>('assets/json/260401/territorio-nacional-solicitud.json');
+  }
+
+    /**
+   * Obtiene los datos del pago de derechos.
+   * @returns Observable con el estado del permiso de importación biológica.
+   */
+  getPagoDerechos(): Observable<PermisoImportacionBiologicaState> {
+    return this.http.get<PermisoImportacionBiologicaState>('assets/json/260401/pagoDerechos.json');
+  }
 }
