91fb70ef498ebe642ecfe177f41064dded6cb512
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-09 14:08:13 +0530

    pre fetch data

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -405,4 +405,12 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
     }]
 },
+  {
+    tramite: 110214,
+    listaComponentes: [{
+      id: 'solicitud',
+      componentPath: () => import('../../tramites/110214/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+      componentName: 'PasoUnoComponent',
+    }]
+  },
 ];
