ffb3418810ff00e2cae817fe89b6d8377b6296d4
karthick <karthick.p@ilink-systems.com>
2025-06-10 18:10:42 +0530

    component updated in lista trimites

--- apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -43,5 +43,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/32514/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
             componentName: 'PasoUnoComponent',
         }]
+    },
+     {
+        tramite: 32301,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/32301/pages/peso-uno/PasoUno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+        }]
     }
 ];
