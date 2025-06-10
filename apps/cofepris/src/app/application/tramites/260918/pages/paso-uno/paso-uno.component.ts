5f1bdf086555ece65fc1423320e0a836d8e8a887
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-10 14:51:45 +0530

    compo doc

--- apps/cofepris/src/app/application/tramites/260918/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260918/pages/paso-uno/paso-uno.component.ts
@@ -10,17 +10,17 @@ import { ModificacionPermisoLabService } from '../../services/modificacion-permi
   templateUrl: './paso-uno.component.html',
 })
 export class PasoUnoComponent implements OnInit {
-     /**
-     * showPreFillingOptions
-     * Indica si se deben mostrar las opciones de prellenado.
-     */
- showPreFillingOptions: boolean = false; 
+  /**
+  * showPreFillingOptions
+  * Indica si se deben mostrar las opciones de prellenado.
+  */
+  showPreFillingOptions: boolean = false;
 
- /**
-* Índice de la pestaña actualmente seleccionada.
-* Inicializado a 1 por defecto.
-*/
- indice = 1;
+  /**
+ * Índice de la pestaña actualmente seleccionada.
+ * Inicializado a 1 por defecto.
+ */
+  indice = 1;
 
   /** Subject para notificar la destrucción del componente. */
   private destroyNotifier$: Subject<void> = new Subject();
@@ -29,32 +29,44 @@ export class PasoUnoComponent implements OnInit {
   public consultaState!: ConsultaioState;
 
 
- /**
-  * Método para seleccionar una pestaña específica.
-  *
-  * @param i El índice de la pestaña a seleccionar.
-  */
- seleccionaTab(i: number): void {
-   this.indice = i;
- }
+  /**
+   * Método para seleccionar una pestaña específica.
+   *
+   * @param i El índice de la pestaña a seleccionar.
+   */
+  seleccionaTab(i: number): void {
+    this.indice = i;
+  }
 
+  /**
+   * Constructor del componente.
+   * @param consultaQuery Servicio para consultar el estado de la consulta.
+   * @param modificacionPermisoLabService Servicio para manejar la modificación del permiso de laboratorio.
+   */
   constructor(
-    private consultaQuery: ConsultaioQuery,private modificacionPermisoLabService: ModificacionPermisoLabService) {
+    private consultaQuery: ConsultaioQuery,
+    private modificacionPermisoLabService: ModificacionPermisoLabService
+  ) {
 
   }
 
+  /**
+   * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+   * Se suscribe al estado de la consulta y actualiza la propiedad consultaState.
+   * Si el estado indica que hay una actualización, guarda los datos del formulario y de pago de derechos.
+   */
   ngOnInit(): void {
-    // Se suscribe al observable del estado y actualiza la propiedad local.
     this.consultaQuery.selectConsultaioState$
       .pipe(
         takeUntil(this.destroyNotifier$),
         map((seccionState) => {
+          // Actualiza el estado de la consulta con el valor recibido del store
           this.consultaState = seccionState;
         })
       )
       .subscribe();
 
-    // Si el estado indica actualización, carga los datos del formulario.
+    // Si el estado indica que hay una actualización, guarda los datos del formulario y de pago de derechos
     if (this.consultaState.update) {
       this.guardarDatosFormulario();
       this.guardarDatosFormularioPagoDerechos();
@@ -62,18 +74,27 @@ export class PasoUnoComponent implements OnInit {
   }
  
 
+  /**
+   * Método para guardar los datos iniciales del formulario.
+   * Obtiene los datos iniciales del formulario desde el servicio y actualiza el estado del formulario.
+   */
   guardarDatosFormulario(): void {
     this.modificacionPermisoLabService
-      .obtenerDatosInicialesFormulario().pipe(
+      .obtenerDatosInicialesFormulario()
+      .pipe(
         takeUntil(this.destroyNotifier$)
       )
       .subscribe((resp) => {
-        if(resp){
-        this.modificacionPermisoLabService.actualizarEstadoFormulario(resp);
+        if (resp) {
+          this.modificacionPermisoLabService.actualizarEstadoFormulario(resp);
         }
       });
   }
 
+  /**
+   * Método para guardar los datos del formulario de pago de derechos.
+   * Obtiene los valores del formulario de pago de derechos desde el servicio y actualiza el estado correspondiente.
+   */
   guardarDatosFormularioPagoDerechos(): void {
     this.modificacionPermisoLabService
       .obtenerValoresFormularioPagoDerechos()
