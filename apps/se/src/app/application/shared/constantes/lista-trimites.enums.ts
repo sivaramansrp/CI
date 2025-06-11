0e684b787c7c785d9e545838c3dbd594fac58f8d
TejaswiniB-ilink <tejaswini.bhandare@ilink-systems.com>
2025-06-11 09:59:01 +0530

    Trámite 130105 :consulta journey

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -471,5 +471,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       componentPath: () => import('../../tramites/110221/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
       componentName: 'PasoUnoComponent',
     }]
+  },
+  {
+     tramite: 130105,
+    listaComponentes: [{
+      id: 'solicitud',
+      componentPath: () => import('../../tramites/130105/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+      componentName: 'PasoUnoComponent',
+    }]
   }
 ];
