a5dcd7f3206134519016b6dff3af05764cba6370
avinash-ilink <avinash.batule@ilink-system.com>
2025-06-11 22:27:00 +0530

    eslint and test cases are successfully complited

--- apps/aga/src/app/application/tramites/10301/services/datos-del-tramite.service.ts
+++ apps/aga/src/app/application/tramites/10301/services/datos-del-tramite.service.ts
@@ -1,7 +1,7 @@
-import { catchError, Observable, throwError } from "rxjs";
-import { Solicitud10301State } from "../estados/tramite10301.store";
+import { Observable, catchError, throwError } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Solicitud10301State } from "../estados/tramite10301.store";
 
 @Injectable({
   providedIn: 'root'
