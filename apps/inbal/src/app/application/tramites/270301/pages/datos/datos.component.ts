1c1530faf2ca012ba090dd9e4cd71ffab0d21de8
varshakatre15 <varsha.katre@ilink-systems.com>
2025-06-11 12:45:04 +0530

    update added

--- apps/inbal/src/app/application/tramites/270301/pages/datos/datos.component.ts
+++ apps/inbal/src/app/application/tramites/270301/pages/datos/datos.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+import { SolicitanteComponent, TIPO_PERSONA } from '@libs/shared/data-access-user/src';
+import { Subject, map, takeUntil } from 'rxjs';
+import { Service270301Service } from '../../services/service270301.service';
 
 /**
  * Componente que maneja los datos del trámite 270201.
@@ -8,7 +12,64 @@ import { Component } from '@angular/core';
   standalone: false,
   templateUrl: './datos.component.html',
 })
-export class DatosComponent {
+export class DatosComponent implements OnInit, AfterViewInit, OnDestroy {
+
+   /**
+    * Referencia al componente SolicitanteComponent para acceder a sus métodos y propiedades.
+    */
+  @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
+
+  /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+  public esDatosRespuesta: boolean = false;
+
+  /** Subject para notificar la destrucción del componente. */
+  private destroyNotifier$: Subject<void> = new Subject();
+  /**
+   * @property
+   * @description Estado actual de la consulta para el trámite 270301.
+   * @type {ConsultaioState}
+   * @memberof Datos270301Component
+   */
+  public consultaState!: ConsultaioState;
+
+  /**
+   * @constructor
+   * @param consultaQuery - Servicio para realizar consultas relacionadas con la aplicación.
+   * @param service270301Service - Servicio específico para manejar la lógica del trámite 270301.
+   * @description Inyecta los servicios necesarios para la gestión de datos en el componente de datos del trámite 270301.
+   */
+  constructor(private consultaQuery: ConsultaioQuery,
+    private service270301Service: Service270301Service) { }
+  /**
+     * @inheritdoc
+     * @method ngOnInit
+     * @description
+     * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+     * Suscribe al estado de consulta y actualiza la propiedad `consultaState` con el estado recibido.
+     * Si el estado indica que se debe actualizar (`update` es verdadero), guarda los datos del formulario.
+     * En caso contrario, establece la bandera `esDatosRespuesta` en verdadero.
+     *
+     * @returns {void}
+     */
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$), map((seccionState) => {
+      this.consultaState = seccionState;
+    })).subscribe();
+    if (this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+  /**
+   * Se ejecuta después de que la vista ha sido inicializada.
+   * Llama al método `obtenerTipoPersona` del componente SolicitanteComponent
+   * para establecer el tipo de persona como MORAL_NACIONAL.
+   */
+  ngAfterViewInit(): void {
+    this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+  }
   /**
    * Índice actual del tab seleccionado.
    */
@@ -20,5 +81,37 @@ export class DatosComponent {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+  /**
+   * @method
+   * @description
+   * Obtiene los datos del registro de toma de muestras de mercancías a través del servicio,
+   * y si la respuesta es válida, actualiza el estado del formulario y marca que se recibió una respuesta.
+   * 
+   * @returns {void}
+   * 
+   * @memberof Datos270301Component
+   */
+  guardarDatosFormulario(): void {
+    this.service270301Service
+      .getRegistroTomaMuestrasMercanciasData().pipe(
+        takeUntil(this.destroyNotifier$)
+      )
+      .subscribe((resp) => {
+        if (resp) {
+          this.esDatosRespuesta = true;
+          this.service270301Service.actualizarEstadoFormulario(resp);
+        }
+      });
+  }
+  /**
+    * Método del ciclo de vida de Angular que se ejecuta al destruir el componente.
+    * Libera recursos y cancela suscripciones.
+    * @returns {void}
+    */
+  ngOnDestroy(): void {
+    this.destroyNotifier$.next();
+    this.destroyNotifier$.complete();
   }
 }
---
+++ apps/inbal/src/app/application/tramites/270301/services/service270301.service.ts
@@ -0,0 +1,55 @@
+import { Agregar270301Store, Solicitud270301State } from '../estados/tramites/agregar270301.store';
+import { ENVIRONMENT } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+/**
+ * Servicio encargado de manejar la lógica y comunicación relacionada con el trámite 270301.
+ * Proporciona métodos para actualizar el estado del formulario y obtener datos de consulta.
+ */
+@Injectable({
+  providedIn: 'root',
+})
+export class Service270301Service {
+  /**
+   * URL del servidor principal.
+   */
+  urlServer = ENVIRONMENT.URL_SERVER;
+  /**
+   * URL del servidor de catálogos auxiliares.
+   */
+  urlServerCatalogos = ENVIRONMENT.URL_SERVER_JSON_AUXILIAR;
+
+  /**
+   * Constructor del servicio.
+   * @param http Cliente HTTP para realizar peticiones.
+   * @param tramite270301Store Almacén de estado para el trámite 270301.
+   */
+  constructor(private http: HttpClient, private agregar270301Store: Agregar270301Store,) {
+    // Lógica de inicialización si es necesario
+  }
+
+  /**
+   * Actualiza el estado del formulario en el store a partir de los datos proporcionados.
+   * @param DATOS Estado actual del formulario de trámite 270301.
+   */
+  actualizarEstadoFormulario(DATOS: Solicitud270301State): void {
+    if (DATOS.ciudad) {
+      this.agregar270301Store.setciudad(DATOS.ciudad);
+    }
+    if (DATOS.emprsaTransportista) {
+      this.agregar270301Store.setemprsaTransportista(DATOS.emprsaTransportista);
+    }
+    
+  }
+
+  /**
+   * Obtiene los datos de consulta para el registro de toma de muestras de mercancías.
+   * @returns Observable con el estado del trámite 270301.
+   */
+  getRegistroTomaMuestrasMercanciasData(): Observable<Solicitud270301State> {
+    return this.http.get<Solicitud270301State>('assets/json/270301/consulta.json');
+  }
+
+}
