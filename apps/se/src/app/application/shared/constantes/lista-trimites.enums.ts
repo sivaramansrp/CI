42201b82efd4dfb3343885551678346e7cfc0dba
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-12 14:56:34 +0530

    disable form field

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -546,5 +546,14 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
     componentPath: () => import('../../tramites/140101/pages/datos/datos.component').then(m => m.DatosComponent),
     componentName: 'DatosComponent',
     }]
-    }
+    },
+  {
+    tramite: 110216,
+    listaComponentes: [{
+      id: 'solicitud',
+      componentPath: () => import('../../tramites/110216/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+      componentName: 'PasoUnoComponent',
+
+    }]
+  },
 ];
