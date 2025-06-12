d50a28bec751424e4fd4a372ebbdab464475a98a
Rajalakshmi Lakshmanan <rajalakshmi.lakshmanan@ilink-systems.com>
2025-06-12 15:29:21 +0530

    Resolved Conflicts

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -731,14 +731,6 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       },
     ],
   },
-   {
-      tramite: 140101,
-    listaComponentes: [{
-    id: 'solicitud',
-    componentPath: () => import('../../tramites/140101/pages/datos/datos.component').then(m => m.DatosComponent),
-    componentName: 'DatosComponent',
-    }]
-    },
     {
       tramite: 120401,
       listaComponentes: [{
