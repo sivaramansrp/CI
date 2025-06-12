fe2a44d250301fd60449204235789ac6da6ed28c
SubodhSingh11 <subodh.singh@ilink-systems.com>
2025-06-12 12:31:20 +0530

    solicitud

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -521,4 +521,17 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       },
     ],
   },
+      {
+    tramite: 260516,
+    listaComponentes: [
+      {
+        id: 'solicitud',
+        componentPath: () =>
+          import(
+            '../../tramites/260515/pages/paso-uno/paso-uno.component'
+          ).then((m) => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+      },
+    ],
+  },
 ];
