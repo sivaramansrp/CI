5d5b29898776630cfa148d3866a79f66f18fd8e9
PiyusshSahare <piyush.sahare@ilink-systems.com>
2025-06-13 16:59:28 +0530

    Implemented Consulta Journey in 31202 under Aga department

--- apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
+++ apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
@@ -154,4 +154,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 31202,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/31202/pages/datos/datos.component').then(m => m.DatosComponent),
+            componentName: 'DatosComponent',
+
+        }]
+    }
 ];
