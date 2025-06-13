e34c9d51ab13be67624b42184665832d2a0d8391
balamurugan-ilink <balamurugan.govinthan@ilink-systems.com>
2025-06-13 15:25:14 +0530

    pull from develop

--- apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -91,7 +91,6 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/30505/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
             componentName: 'DatosComponent',  
     }]
-        }]
     },
     {
         tramite: 32501,
