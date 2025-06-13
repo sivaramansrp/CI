c1eaaab7a3e2d7b033f1952a1487644519973570
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-13 15:53:46 +0530

    Add updateState method to Tramite240321Store for state management

--- apps/sedena/src/app/application/tramites/240321/pages/paso-uno/paso-uno.component.ts
+++ apps/sedena/src/app/application/tramites/240321/pages/paso-uno/paso-uno.component.ts
@@ -1,12 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState, SolicitanteComponent } from '@libs/shared/data-access-user/src';
+import { Subject,map,takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
 import { DatosDelTramiteContenedoraComponent } from '../../components/datos-del-tramite-contenedora/datos-del-tramite-contenedora.component';
 import { FolioComponent } from '../../components/folio/folio.component';
-import { PagoDeDerechosContenedoraComponent } from '../../components/pago-de-derechos-contenedora/pago-de-derechos-contenedora.component';
-import { ConsultaioQuery, ConsultaioState, SolicitanteComponent } from '@libs/shared/data-access-user/src';
-import { TercerosRelacionadosContenedoraComponent } from '../../components/terceros-relacionados-contenedora/terceros-relacionados-contenedora.component';
-import { map, Subject, takeUntil } from 'rxjs';
 import { ModificacionService } from '../../services/modificacion.service';
+import { PagoDeDerechosContenedoraComponent } from '../../components/pago-de-derechos-contenedora/pago-de-derechos-contenedora.component';
+import { TercerosRelacionadosContenedoraComponent } from '../../components/terceros-relacionados-contenedora/terceros-relacionados-contenedora.component';
 import { Tramite240321Store } from '../../estados/tramite240321Store.store';
 
 /**
@@ -38,14 +38,42 @@ export class PasoUnoComponent implements OnInit,AfterViewInit,OnDestroy {
        /** Subject para notificar la destrucción del componente. */
   private destroyNotifier$: Subject<void> = new Subject();
 
-constructor( private readonly consultaQuery: ConsultaioQuery, private modificacionService: ModificacionService,    private readonly tramiteStore: Tramite240321Store){
+/**
+ * @constructor
+ * @desc Constructor de la clase PasoUnoComponent.
+ * 
+ * @param consultaQuery Servicio para consultar información relacionada con el trámite.
+ * @param modificacionService Servicio para manejar las modificaciones del trámite.
+ * @param tramiteStore Almacén de estado específico para el trámite 240321.
+ */
+constructor( private readonly consultaQuery: ConsultaioQuery, private modificacionService: ModificacionService, private readonly tramiteStore: Tramite240321Store){
 
 }
+  /**
+   * @inheritdoc
+   * @description
+   * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+   * 
+   * En este método, se suscribe al estado de la consulta utilizando el servicio `consultaQuery` y actualiza el estado del componente.
+   * La suscripción se gestiona para evitar fugas de memoria utilizando el observable `destroyNotifier$`.
+   * 
+   * @see https://angular.io/api/core/OnInit
+   */
  ngOnInit(): void {
     this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
           this.consultaState = seccionState;
       })).subscribe();
   }
+   /**
+   * @inheritdoc
+   * @description
+   * Método del ciclo de vida de Angular que se ejecuta después de que la vista del componente ha sido inicializada.
+   * 
+   * En este método, se obtiene el estado del trámite utilizando el servicio `modificacionService` y se actualiza el estado en el store correspondiente.
+   * La suscripción se gestiona para evitar fugas de memoria utilizando el observable `destroyNotifier$`.
+   * 
+   * @see https://angular.io/api/core/AfterViewInit
+   */
   ngAfterViewInit(): void {
     // if(this.consultaState.update) {
     this.modificacionService.getTrimateState240321().pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
