7d29ee582bba8626d37efdaae55dc67878ec5573
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-13 16:26:52 +0530

    Refactor lista-trimites.enums.ts for improved component organization

--- apps/sedena/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/sedena/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -48,5 +48,12 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/240122/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
             componentName: 'PasoUnoComponent',
         }]
+    },{
+        tramite: 240321,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/240321/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+        }]
     },
 ];
