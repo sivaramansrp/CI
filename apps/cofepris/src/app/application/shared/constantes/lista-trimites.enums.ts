efaad5e17c2837357d576fe0eff3504e9a7374ec
SnehalTeliiLink <snehal.teli@ilink-systems.com>
2025-06-13 16:03:41 +0530

    worked on 260902, 260903 procedure consulta
    worked on 260902, 260903 procedure

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -584,5 +584,24 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentName: 'PasoUnoPagesComponent',
 
         }]
+    },
+    
+     {
+        tramite: 260902,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/260902/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    },
+      {
+        tramite: 260903,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/260903/pages/datos-260903/datos-260903.component').then(m => m.Datos260903Component),
+            componentName: 'Datos260903Component',
+
+        }]
     }
 ];
