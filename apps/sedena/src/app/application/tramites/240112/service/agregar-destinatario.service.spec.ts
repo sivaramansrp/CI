ee9362d28f3ee3a2ec321684217ce2e3c68d3495
Jamuna_Sri <jamunasri.s@purpleslate.in>
2025-06-13 11:26:54 +0530

    testcase fix

--- apps/sedena/src/app/application/tramites/240112/service/agregar-destinatario.service.spec.ts
+++ apps/sedena/src/app/application/tramites/240112/service/agregar-destinatario.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AgregarDestinatarioService } from './agregar-destinatario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AgregarDestinatarioService', () => {
   let service: AgregarDestinatarioService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(AgregarDestinatarioService);
   });
 
