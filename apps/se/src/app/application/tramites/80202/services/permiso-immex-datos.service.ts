b8968b2b0d1a493f7e35b8c2a7a340523bf2b934
Kalai-2025 <kalaiyarasan.s@ilink-systems.com>
2025-06-13 09:10:27 +0530

    UI -05

---
+++ apps/se/src/app/application/tramites/80202/services/permiso-immex-datos.service.ts
@@ -0,0 +1,61 @@
+/**
+ * @Injectable
+ * @description Servicio para obtener los datos del permiso IMMEX.
+ */
+import { ImmexRegistroState, ImmexRegistroStore } from '../../estados/tramites/tramite80202.store';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { immexRegistroform } from '../../modelos/immex-registro-de-solicitud-modality.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PermisoImmexDatosService {
+  /**
+   * @property {string} jsonUrl
+   * @description URL del archivo JSON que contiene los datos del permiso IMMEX.
+   */
+  private jsonUrl = '/assets/json/80202/immex-table.json';
+
+  /**
+   * @constructor
+   * @description Constructor que inicializa el cliente HTTP para realizar solicitudes.
+   * @param {HttpClient} httpClient - Cliente HTTP para realizar solicitudes.
+   */
+  constructor(private httpClient: HttpClient,private readonly tramite80202Store:ImmexRegistroStore) {}
+
+  /**
+   * @method getDatos
+   * @description Obtiene los datos del permiso IMMEX desde el archivo JSON.
+   * @returns {Observable<any[]>} Observable que emite los datos del permiso IMMEX.
+   */
+  getDatos(): Observable<any> {
+    return this.httpClient.get<any[]>(this.jsonUrl).pipe(
+    );
+  }  
+  /**
+   * @description
+   * Obtiene los datos del registro de toma de muestras de mercancías para el trámite IMMEX.
+   * Realiza una petición HTTP GET para recuperar la información desde un archivo JSON local.
+   *
+   * @returns Un observable que emite el estado del registro IMMEX.
+   *
+   * @memberof PermisoImmexDatosService
+   */
+  getRegistroTomaMuestrasMercanciasData(): Observable<ImmexRegistroState> {
+    return this.httpClient.get<ImmexRegistroState>('assets/json/80202/immexRegistro.json');
+  }
+  /**
+   * @method actualizarEstadoFormulario
+   * @description
+   * Actualiza el estado del formulario IMMEX en el store con los datos proporcionados.
+   *
+   * @param {immexRegistroform} DATOS - Los datos del formulario IMMEX a registrar en el estado.
+   *
+   * @returns {void}
+   */
+   actualizarEstadoFormulario(DATOS:immexRegistroform): void {
+    this.tramite80202Store.setImmexRegistro(DATOS);
+   }
+}
