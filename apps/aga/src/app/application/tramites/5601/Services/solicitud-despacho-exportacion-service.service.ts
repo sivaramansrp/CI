bf1112ad54a0c49f2abe1e3a175c96f0bb16c6f1
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-13 14:43:10 +0530

    consulta Journey

--- apps/aga/src/app/application/tramites/5601/Services/solicitud-despacho-exportacion-service.service.ts
+++ apps/aga/src/app/application/tramites/5601/Services/solicitud-despacho-exportacion-service.service.ts
@@ -1,11 +1,27 @@
+import { Tramite5601State, Tramite5601Store } from '../estados/stores/tramite5601.store';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SolicitudDespachoExportacionServiceService {
+export class SolicitudDespachoExportacionService {
 
-  constructor() { 
+  constructor(private http: HttpClient,private tramite5601Store: Tramite5601Store) { 
     //
+  }
+
+  actualizarEstadoFormulario(DATOS: Tramite5601State): void {
+    Object.entries(DATOS).forEach(([clave, valor]) => {
+      this.tramite5601Store.setDynamicFieldValue(
+        clave as keyof Tramite5601State,
+        valor as Tramite5601State[keyof Tramite5601State]
+      );
+    });
+  }
+
+  obtenerDatosInicialesFormulario(): Observable<Tramite5601State> {
+    return this.http.get<Tramite5601State>('assets/json/5601/inicializar-formulario.json');
   }
 }
--- apps/aga/src/app/application/tramites/5601/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/5601/pages/paso-uno/paso-uno.component.ts
@@ -1,4 +1,7 @@
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+import {Subject,map,takeUntil } from 'rxjs';
 import { Component } from '@angular/core';
+import { SolicitudDespachoExportacionService } from '../../Services/solicitud-despacho-exportacion-service.service';
 /**
  * Componente que representa el paso uno del formulario o proceso.
  */
@@ -11,6 +14,42 @@ export class PasoUnoComponent {
  * Índice actual del paso activo en el flujo.
  */
   indice: number = 1;
+
+  private destroy$ = new Subject<void>();
+
+  /** Subject para notificar la destrucción del componente. */
+  public consultaState!: ConsultaioState;
+
+  constructor(private consultaQuery: ConsultaioQuery,private solicitudDespachoExportacionService: SolicitudDespachoExportacionService){
+    // Se suscribe al observable del estado y actualiza la propiedad local.
+    this.consultaQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroy$),
+        map((seccionState) => {
+          this.consultaState = seccionState;
+        })
+      )
+      .subscribe();
+
+    // Si el estado indica actualización, carga los datos del formulario.
+    if (this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } 
+
+  }
+
+  
+  guardarDatosFormulario(): void {
+    this.solicitudDespachoExportacionService
+      .obtenerDatosInicialesFormulario().pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe((resp) => {
+        if (resp) {
+          this.solicitudDespachoExportacionService.actualizarEstadoFormulario(resp);
+        }
+      });
+  }
 
   /**
    * Selecciona una pestaña específica y actualiza el índice actual.
