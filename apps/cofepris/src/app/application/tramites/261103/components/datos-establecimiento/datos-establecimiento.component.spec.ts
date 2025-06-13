a7966f383e203234969cf61c155e489f940950a8
maneeshangulardeveloper <maneeshpatel2024@gmail.com>
2025-06-13 20:25:41 +0530

    changes fixed

--- apps/cofepris/src/app/application/tramites/261103/components/datos-establecimiento/datos-establecimiento.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/261103/components/datos-establecimiento/datos-establecimiento.component.spec.ts
@@ -1,21 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { DatosestablecimientoComponent } from './datos-establecimiento.component';
 
 describe('DatosestablecimientoComponent', () => {
-  let COMPONENT: DatosestablecimientoComponent;
-  let FIXTURE: ComponentFixture<DatosestablecimientoComponent>;
+  let component: DatosestablecimientoComponent;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DatosestablecimientoComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [DatosestablecimientoComponent],
+      providers: [FormBuilder],
+    });
 
-    FIXTURE = TestBed.createComponent(DatosestablecimientoComponent);
-    COMPONENT = FIXTURE.componentInstance;
-    FIXTURE.detectChanges();
+    const fixture = TestBed.createComponent(DatosestablecimientoComponent);
+    component = fixture.componentInstance;
+    component.datosdelestablecimiento = new FormBuilder().group({
+      denominacion: [''],
+    });
   });
 
-  it('should create', () => {
-    expect(COMPONENT).toBeTruthy();
+  it('should create the form and disable it when esFormularioSoloLectura is true', () => {
+    component.esFormularioSoloLectura = true;
+    component.crearFormulario = jest.fn(() => {
+      component.datosdelestablecimiento = new FormBuilder().group({
+        denominacion: ['Test Denominacion'],
+      });
+    });
+
+    component.guardarDatosFormulario();
+
+    expect(component.crearFormulario).toHaveBeenCalled();
+    expect(component.datosdelestablecimiento.disabled).toBe(true);
+  });
+
+  it('should create the form and enable it when esFormularioSoloLectura is false', () => {
+    component.esFormularioSoloLectura = false;
+    component.crearFormulario = jest.fn(() => {
+      component.datosdelestablecimiento = new FormBuilder().group({
+        denominacion: ['Test Denominacion'],
+      });
+    });
+
+    component.guardarDatosFormulario();
+    expect(component.crearFormulario).toHaveBeenCalled();
+    expect(component.datosdelestablecimiento.enabled).toBe(true);
   });
 });
--- apps/cofepris/src/app/application/tramites/261103/components/representantelegal/representante-legal.component.ts
+++ apps/cofepris/src/app/application/tramites/261103/components/representantelegal/representante-legal.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
 import { DatosProcedureQuery } from '../../../../estados/queries/tramites261103.query';
 import { DatosProcedureState } from '../../../../estados/tramites/tramites261103.store';
 import { DatosProcedureStore } from '../../../../estados/tramites/tramites261103.store';
@@ -14,6 +15,7 @@ import { Subject } from 'rxjs';
 import { TablaSeleccion } from '@libs/shared/data-access-user/src/core/enums/tabla-seleccion.enum';
 import { TituloComponent } from '@libs/shared/data-access-user/src';
 import { Validators } from '@angular/forms';
+import { map } from 'rxjs';
 import { takeUntil } from 'rxjs';
 @Component({
   selector: 'app-representante-legal',
@@ -52,6 +54,17 @@ export class RepresentanteLegalComponent implements OnInit, OnDestroy {
  */
   private seccionState!: DatosProcedureState;
   
+
+  /**
+   * Subject para notificar la destrucción del componente.
+   */
+  private destroyNotifier$: Subject<void> = new Subject();
+  
+  /**
+  * Indica si el formulario está en modo solo lectura.
+  * Cuando es `true`, los campos del formulario no se pueden editar.
+  */
+  esFormularioSoloLectura: boolean = false; 
   /**
    * Constructor para SolicitanteComponent.
    * 
@@ -60,8 +73,17 @@ export class RepresentanteLegalComponent implements OnInit, OnDestroy {
   constructor(private fb: FormBuilder,
     private store: DatosProcedureStore,
     private query: DatosProcedureQuery,
+    private consultaioQuery: ConsultaioQuery
   ) {
     // Constructor del componente
+    this.consultaioQuery.selectConsultaioState$
+    .pipe(
+      takeUntil(this.destroyNotifier$),
+      map((seccionState: { readonly: boolean })=>{
+        this.esFormularioSoloLectura = seccionState.readonly; 
+      })
+    )
+    .subscribe()
   }
 
   /**
@@ -81,6 +103,14 @@ export class RepresentanteLegalComponent implements OnInit, OnDestroy {
  * Los controles del formulario incluyen:
  */
   public establecerdomicilioEstablecimiento(): void {
+    this.query.selectProrroga$
+    .pipe(
+      takeUntil(this.destroyNotifier$),
+      map((seccionState) => {
+        this.seccionState = seccionState;
+      })
+    )
+    .subscribe()
     this.domicilioEstablecimiento = this.fb.group({
       representanteLegalRFC: [{ value: this.seccionState?.representanteLegalRFC,disabled:false},[Validators.required]],
       buscar: [{ value: this.seccionState?.buscar,disabled:false },[Validators.required]],
@@ -88,6 +118,11 @@ export class RepresentanteLegalComponent implements OnInit, OnDestroy {
       representanteLegalApPaterno: [{ value: this.seccionState?.representanteLegalApPaterno ,disabled:false},[Validators.required]],
       representanteLegalApMaterno: [{ value: this.seccionState?.representanteLegalApMaterno,disabled:false },[Validators.required]],
     });
+    if (this.esFormularioSoloLectura) {
+      this.domicilioEstablecimiento.disable();
+    }else{
+      this.domicilioEstablecimiento.enable();
+    }
   }
 
    /**
@@ -124,5 +159,21 @@ obtenerDatosFormulario():void{
     this.seccionState = data;
   });
 }
+
+/**
+ * Inicializa el estado del formulario.
+ * 
+ * Este método realiza las siguientes acciones:
+ * 1. Obtiene los datos del formulario desde el estado mediante el método `obtenerDatosFormulario`.
+ * 2. Configura el formulario reactivo para el domicilio del representante legal utilizando el método `establecerdomicilioEstablecimiento`.
+ * 
+ * Es útil para establecer los valores iniciales del formulario y sincronizarlos con el estado global de la aplicación.
+ * 
+ * @returns {void}
+ */
+inicializarEstadoFormulario(): void {
+  this.obtenerDatosFormulario();
+  this.establecerdomicilioEstablecimiento();        
+    }
 }
 
