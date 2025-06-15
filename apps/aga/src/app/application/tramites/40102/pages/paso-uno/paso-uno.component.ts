d762fb94e4226a44c7bd46b9161a43c38b9bea98
Ghanshyam Yadav <ghanshyam.yadav@ilink-systems.com>
2025-06-15 23:31:01 +0530

    updated to handle the consulta journey

--- apps/aga/src/app/application/tramites/40102/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/40102/pages/paso-uno/paso-uno.component.ts
@@ -1,27 +1,73 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
-import {
-  DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL,
-  PERSONA_MORAL_NACIONAL,
-} from 'libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
-import {
-  FormularioDinamico,
-  SolicitanteComponent,
-  TIPO_PERSONA,
-} from '@ng-mf/data-access-user';
+
+import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ChoferesExtranjeros, DatosDelChoferNacional } from '../../models/registro-muestras-mercancias.model';
+import {ConsultaioQuery, FormularioDinamico,SolicitanteComponent} from '@ng-mf/data-access-user';
+import {DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL,PERSONA_MORAL_NACIONAL} from '@libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
+import { ReplaySubject, map, takeUntil } from 'rxjs';
+import { Chofer40102Query } from '../../estados/chofer40102.query';
+import { Chofer40102Service } from '../../estados/chofer40102.service';
+import { Chofer40102Store } from '../../estados/chofer40102.store';
 
 @Component({
   selector: 'paso-uno',
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.scss',
 })
-export class PasoUnoComponent implements AfterViewInit {
+export class PasoUnoComponent implements AfterViewInit, OnInit, OnDestroy {
+  /**
+ * Referencia al componente hijo `SolicitanteComponent` dentro de la plantilla.
+ * Permite acceder a las propiedades y métodos públicos del componente hijo.
+ *
+ * @type {SolicitanteComponent}
+ */
   @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
+  /**
+   * Representa el tipo de persona asociado.
+   * 
+   * @type {number}
+   * @remarks
+   * Este valor puede ser utilizado para determinar el tipo de persona
+   * (por ejemplo, física o moral) en el contexto de la aplicación.
+   */
   tipoPersona!: number;
+
+
+  /**
+   * Arreglo que contiene objetos de tipo FormularioDinamico.
+   * Representa la información relacionada con una persona en el formulario dinámico.
+   */
   persona: FormularioDinamico[] = [];
+
+  /**
+   * Arreglo que contiene los formularios dinámicos relacionados con el domicilio fiscal.
+   * Este arreglo se utiliza para almacenar y gestionar los datos del formulario
+   * en el paso uno del trámite.
+   */
   domicilioFiscal: FormularioDinamico[] = [];
+
+  /**
+   * Índice que representa un número inicial o posición en un flujo o proceso.
+   * Se utiliza para controlar el estado o paso actual en la lógica de la aplicación.
+   */
   indice: number = 1;
+
+  /**
+   * Indica si la validación es exitosa o no.
+   * 
+   * @type {boolean}
+   * @default false
+   */
   validacion: boolean = false;
-  @Input() datosNroPedimento!: any;
+
+  /**
+   * Propiedad de entrada que representa el número de pedimento.
+   * Este valor es proporcionado desde el componente padre y se utiliza
+   * para mostrar o procesar información relacionada con el pedimento.
+   */
+  @Input() datosNroPedimento!: string;
+  
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  
   /**
    * Gancho de ciclo de vida angular que se llama después de que la vista del componente se haya inicializado por completo.
    */
@@ -36,4 +82,63 @@ export class PasoUnoComponent implements AfterViewInit {
   seleccionaTab(i: number): void {
     this.indice = i;
   }
+
+  /**
+   *
+   */
+  constructor(        
+    private chofer40102Service: Chofer40102Service,
+    private chofer40102Query: Chofer40102Query,
+    private chofer40102Store: Chofer40102Store,
+    private consultaQuery: ConsultaioQuery
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$
+    .pipe(
+      takeUntil(this.destroyed$), 
+      map((seccionState) => {
+        if( seccionState.update ) {
+          this.chofer40102Service
+            .getDirectorGeneralData()
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe((data) => {
+              // Actualiza el estado del chofer40102Store con los datos del director general
+              this.chofer40102Service.updateStateDirectorGeneralData(data);
+          });
+
+          this.chofer40102Service
+            .obtenerTablaDatos<DatosDelChoferNacional>('mock-data-choferes-nacionales.json')
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe( (response) => {
+                this.chofer40102Service.updateDatosDelChoferNacional(response);
+                this.chofer40102Service.updateDatosDelChoferNacionalModification(response);
+                this.chofer40102Service.updateDatosDelChoferNacionalRetirada(response);
+            });
+
+          this.chofer40102Service
+            .obtenerTablaDatos<ChoferesExtranjeros>('mock-data-choferes-extranjero.json')
+            .pipe(takeUntil(this.destroyed$))
+            .subscribe( (response) => {
+                this.chofer40102Service.updateDatosDelChoferExtranjero(response);
+                this.chofer40102Service.updateDatosDelChoferExtranjeroModification(response);
+                this.chofer40102Service.updateDatosDelChoferExtranjeroRetirada(response);
+            });
+
+        }
+    })).subscribe();
+  }
+
+  /**
+   * Método del ciclo de vida de Angular que se llama cuando el componente se destruye.
+   * Se utiliza para limpiar recursos y evitar fugas de memoria.
+   */
+  ngOnDestroy(): void {
+
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
 }
