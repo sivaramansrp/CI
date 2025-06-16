9de0c999a473cf62ecc5fd1a5d9705857b8cd6d2
gopalvilink <gopal.vasudevan@ilink-systems.com>
2025-06-16 13:34:59 +0530

    consulta changes

--- apps/sedena/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/sedena/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -105,4 +105,17 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       },
     ],
   },
+  {
+    tramite: 240118,
+    listaComponentes: [
+      {
+        id: 'solicitud',
+        componentPath: () =>
+          import(
+            '../../tramites/240118/pages/paso-uno/paso-uno.component'
+          ).then((m) => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+      },
+    ],
+  },
 ];
