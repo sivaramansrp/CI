b0b10c28a97167af34bda25b3d917ac7b708d1ad
TejaswiniB-ilink <tejaswini.bhandare@ilink-systems.com>
2025-06-16 11:13:53 +0530

    Trámite 250103: First, Second, and Fifth Tab Consulta Journey

--- apps/profepa/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/profepa/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -10,4 +10,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 250103,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/250103/pages/datos/datos.component').then(m => m.DatosComponent),
+            componentName: 'DatosComponent',
+
+        }]
+    }
 ];
