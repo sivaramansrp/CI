f58a4debdde9353e0b8a979427e59752d3a7019d
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-16 18:16:51 +0530

    name change

--- apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -97,7 +97,7 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         listaComponentes: [{
             id: 'solicitud',
             componentPath: () => import('../../tramites/32503/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
-            componentName: 'DatosComponent',
+            componentName: 'PasoUnoComponent',
         }]
     }
 ];
