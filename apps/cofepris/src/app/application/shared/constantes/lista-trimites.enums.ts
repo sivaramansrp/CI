9e8e115975df9da29e1a393e66f714a647733f00
banshi-ilink-digital <banshi.bhatt@ilink-systems.com>
2025-06-17 16:09:26 +0530

    consulta journey

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -639,5 +639,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         componentPath: () => import('../../tramites/260217/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
         componentName: 'PasoUnoComponent',
     }]
+  },
+   {
+    tramite: 260102,
+    listaComponentes: [{
+        id: 'solicitud',
+        componentPath: () => import('../../tramites/260102/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+    }]
   }
 ];
