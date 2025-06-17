73f91ed8b596669bbf1da463fa9b3f75ac361c64
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-06-17 12:53:35 +0530

    Added CJ in 120403

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -807,5 +807,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       componentPath: () => import('../../tramites/130107/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
       componentName: 'PasoUnoComponent',
     }]
+  },
+   {
+    tramite: 120403,
+    listaComponentes: [{
+      id: 'solicitud',
+      componentPath: () => import('../../tramites/120403/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+      componentName: 'PasoUnoComponent',
+    }]
   }
 ];
--- apps/se/src/app/application/tramites/120403/pages/paso-uno/paso-uno.component.ts
+++ apps/se/src/app/application/tramites/120403/pages/paso-uno/paso-uno.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewInit, Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState, FormularioDinamico, TIPO_PERSONA } from '@ng-mf/data-access-user';
 import { DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL, PERSONA_MORAL_NACIONAL } from '@libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
-import { FormularioDinamico, TIPO_PERSONA } from '@ng-mf/data-access-user';
-import { CuposService } from '../../services/cupos.service';
+import { map, ReplaySubject, takeUntil } from 'rxjs';
 import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
+import { CuposService } from '../../services/cupos.service';
 
 /**
  * Componente que representa el primer paso del trámite.
@@ -12,7 +13,15 @@ import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
   templateUrl: './paso-uno.component.html',
   styles: ``,
 })
-export class PasoUnoComponent implements AfterViewInit {
+export class PasoUnoComponent implements AfterViewInit, OnDestroy, OnInit {
+  /**
+   * Indica si los datos de respuesta están disponibles.
+   */
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+   /**
+     * Estado de la consulta, utilizado para manejar el estado de la aplicación.
+     */
+  public consultaState!: ConsultaioState;
   /**
    * Catálogo de entidades federativas.
    */
@@ -48,6 +57,51 @@ export class PasoUnoComponent implements AfterViewInit {
   */
   @Output() dataEmitter = new EventEmitter<number>();
   /**
+   * Indica si existen datos de respuesta del servidor para actualizar el formulario.
+   */
+   public esDatosRespuesta: boolean = false;
+  /**
+   * Constructor del componente.
+   * Inicializa el componente y puede ser utilizado para inyecciones de dependencias si es necesario.
+   */
+  constructor(private consultaioQuery: ConsultaioQuery,
+    private cupos: CuposService ){
+    } 
+    /**
+   * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+   * Suscribe al estado de consulta y carga datos si es necesario.
+   */
+  ngOnInit(): void {
+     this.consultaioQuery.selectConsultaioState$.pipe(
+      takeUntil(this.destroyed$),
+      map((seccionState) => {
+        this.consultaState = seccionState;
+      })
+    ).subscribe();
+    if (this.consultaState.update) {
+      this.guardarDatosFormularios();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+  /**
+   * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+   * Luego reinicializa el formulario con los valores actualizados desde el store.
+   */
+  guardarDatosFormularios(): void {
+    this.cupos
+      .getRegistroTomaMuestrasMercanciasData().pipe(
+        takeUntil(this.destroyed$)
+      )
+      .subscribe((resp) => {
+        if (resp) {
+          this.esDatosRespuesta = true;
+          this.cupos.actualizarEstadoFormulario(resp);
+        }
+      });
+  }
+  /**
    * Método que se ejecuta después de que las vistas del componente han sido inicializadas.
    * Configura los formularios dinámicos y obtiene el tipo de persona.
    */
@@ -65,5 +119,14 @@ export class PasoUnoComponent implements AfterViewInit {
     this.indice = i;
     this.dataEmitter.emit(this.indice);
 
+  }
+
+  /**
+   * Método del ciclo de vida de Angular que se ejecuta cuando el componente es destruido.
+   * Aquí se pueden realizar tareas de limpieza, pero en este caso lanza un error indicando que no está implementado.
+   */
+  ngOnDestroy(): void {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
