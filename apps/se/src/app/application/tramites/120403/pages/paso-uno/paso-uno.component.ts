872e3bf4c2379039562d5ecf9c86566a1d1e1893
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-06-17 14:45:01 +0530

    Added UT and eslint in 120403

--- apps/se/src/app/application/tramites/120403/pages/paso-uno/paso-uno.component.ts
+++ apps/se/src/app/application/tramites/120403/pages/paso-uno/paso-uno.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { ConsultaioQuery, ConsultaioState, FormularioDinamico, TIPO_PERSONA } from '@ng-mf/data-access-user';
 import { DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL, PERSONA_MORAL_NACIONAL } from '@libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
-import { map, ReplaySubject, takeUntil } from 'rxjs';
-import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
+import { ReplaySubject, map,takeUntil } from 'rxjs';
 import { CuposService } from '../../services/cupos.service';
+import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
 
 /**
  * Componente que representa el primer paso del trámite.
