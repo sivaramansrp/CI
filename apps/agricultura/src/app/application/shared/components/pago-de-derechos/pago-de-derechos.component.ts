967757a2faa217a81f19c1008420b668845627e0
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-18 13:21:52 +0530

    add PagoDeDerechos component with template, styles, and tests

---
+++ apps/agricultura/src/app/application/shared/components/pago-de-derechos/pago-de-derechos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pago-de-derechos',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pago-de-derechos.component.html',
+  styleUrl: './pago-de-derechos.component.css',
+})
+export class PagoDeDerechosComponent {}
