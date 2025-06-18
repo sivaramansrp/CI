e8610ef6fe00bae321c2ed1d47ef26780e4cffb4
sravanimeegada <sravani.meegada@ilink-systems.com>
2025-06-18 10:52:14 +0530

    Consulta changes done

--- apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/cofepris/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -425,4 +425,17 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
       },
     ],
   },
+  {
+    tramite: 260919,
+    listaComponentes: [
+      {
+        id: 'solicitud',
+        componentPath: () =>
+          import(
+            '../../tramites/260919/pages/paso-uno/paso-uno.component'
+          ).then((m) => m.PasoUnoComponent),
+        componentName: 'PasoUnoComponent',
+      },
+    ],
+  },
 ];
