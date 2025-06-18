302cb12df429e7d4db6eadc0c04785c9dd9298a5
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-18 11:51:59 +0530

    ompo doc

--- apps/aga/src/app/application/tramites/5601/Services/solicitud-despacho-exportacion-service.service.ts
+++ apps/aga/src/app/application/tramites/5601/Services/solicitud-despacho-exportacion-service.service.ts
@@ -2,16 +2,25 @@ import { Tramite5601State, Tramite5601Store } from '../estados/stores/tramite560
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+// Decorador Injectable para indicar que este servicio puede ser inyectado en otros componentes o servicios
 @Injectable({
   providedIn: 'root'
 })
 export class SolicitudDespachoExportacionService {
 
-  constructor(private http: HttpClient,private tramite5601Store: Tramite5601Store) { 
-    //
+  /**
+   * Constructor del servicio.
+   * Inyecta HttpClient para realizar peticiones HTTP
+   * e inyecta Tramite5601Store para manejar el estado del trámite.
+   */
+  constructor(private http: HttpClient, private tramite5601Store: Tramite5601Store) { 
+    // Constructor vacío
   }
 
+  /**
+   * Actualiza el estado del formulario en el store.
+   * @param DATOS Objeto con los datos del estado a actualizar.
+   */
   actualizarEstadoFormulario(DATOS: Tramite5601State): void {
     Object.entries(DATOS).forEach(([clave, valor]) => {
       this.tramite5601Store.setDynamicFieldValue(
@@ -21,6 +30,10 @@ export class SolicitudDespachoExportacionService {
     });
   }
 
+  /**
+   * Obtiene los datos iniciales del formulario desde un archivo JSON.
+   * @returns Observable con el estado inicial del formulario.
+   */
   obtenerDatosInicialesFormulario(): Observable<Tramite5601State> {
     return this.http.get<Tramite5601State>('assets/json/5601/inicializar-formulario.json');
   }
