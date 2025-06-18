5fc4053676660dc0cf0a7d83e3c6cfb7d9e048dd
Sohel Arif <sohel.arif@ilink-systems.com>
2025-06-18 18:42:33 +0530

    Add-on Verify Opinion and Read Only journey

--- apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
+++ apps/agace/src/app/application/shared/constants/lista-trimites.enums.ts
@@ -175,5 +175,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/31801/pages/datos/datos.component').then(m => m.DatosComponent),
             componentName: 'DatosComponent',
         }]
+    },
+    {
+        tramite: 32516,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/32516/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+        }]
     }
 ];
