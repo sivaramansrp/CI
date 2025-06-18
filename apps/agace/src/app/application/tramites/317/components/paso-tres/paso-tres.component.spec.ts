fda601e85aaf3fc6e711da9afe90fe9b1349d107
Karthik Reddy <karthik.reddy@ilink-systems.com>
2025-06-18 18:16:22 +0530

    test cases fix in 317

--- apps/agace/src/app/application/tramites/317/components/paso-tres/paso-tres.component.spec.ts
+++ apps/agace/src/app/application/tramites/317/components/paso-tres/paso-tres.component.spec.ts
@@ -1,13 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PasoTresComponent } from './paso-tres.component';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('PasoTresComponent', () => {
   let component: PasoTresComponent;
   let fixture: ComponentFixture<PasoTresComponent>;
+  let routerMock: any;
 
   beforeEach(async () => {
+    routerMock = { navigate: jest.fn() };
     await TestBed.configureTestingModule({
-      imports: [PasoTresComponent],
+      imports: [PasoTresComponent,ToastrModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasoTresComponent);
@@ -17,5 +20,15 @@ describe('PasoTresComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should not navigate if FIRMA is an empty string', () => {
+    component.obtieneFirma('');
+    expect(routerMock.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should not navigate if FIRMA is undefined', () => {
+    component.obtieneFirma(undefined as any);
+    expect(routerMock.navigate).not.toHaveBeenCalled();
   });
 });
