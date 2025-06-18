240be3f7079ba2909f99bbf2542e31bdc287349a
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-06-18 18:31:35 +0530

    Updated consulta journey

--- apps/agace/src/app/application/tramites/31301/components/datos-por-garantia/datos-por-garantia.component.ts
+++ apps/agace/src/app/application/tramites/31301/components/datos-por-garantia/datos-por-garantia.component.ts
@@ -3,6 +3,7 @@ import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src';
 import { CatalogosSelect } from '@libs/shared/data-access-user/src';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ConsultaioQuery } from '@libs/shared/data-access-user/src';
 import { DatosPorGarantia } from '../../models/solicitud.model';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
@@ -72,6 +73,12 @@ export class DatosPorGarantiaComponent implements OnInit, OnDestroy {
   solicitud31301State: Solicitud31301State = {} as Solicitud31301State;
 
   /**
+   * Indica si el formulario está en modo solo lectura.
+   * Cuando es `true`, los campos del formulario no se pueden editar.
+   */
+  esFormularioSoloLectura: boolean = false;
+
+  /**
    * Constructor que inyecta servicios y realiza la carga inicial de datos.
    *
    * @param fb FormBuilder para crear formularios reactivos
@@ -83,8 +90,25 @@ export class DatosPorGarantiaComponent implements OnInit, OnDestroy {
     public fb: FormBuilder,
     public solicitudService: SolicitudService,
     public solicitud31301Store: Solicitud31301Store,
-    public solicitud31301Query: Solicitud31301Query
+    public solicitud31301Query: Solicitud31301Query,
+    public consultaioQuery: ConsultaioQuery
   ) {
+    /**
+     * Se suscribe al estado de `Consultaio` para obtener información actualizada del estado del formulario.
+     *
+     * - Asigna el valor de solo lectura (`readonly`) a la propiedad `esFormularioSoloLectura`.
+     * - Llama a `inicializarEstadoFormulario()` para aplicar configuraciones basadas en el estado recibido.
+     * - La suscripción se cancela automáticamente cuando `destroyNotifier$` emite un valor (para evitar fugas de memoria).
+     */
+    this.consultaioQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroy$),
+        map((seccionState) => {
+          this.esFormularioSoloLectura = seccionState.readonly;
+          this.inicializarEstadoFormulario();
+        })
+      )
+      .subscribe();
     this.conseguirNombreInstitucionCatalogo();
     this.conseguirDatosPorGarantia();
   }
@@ -94,6 +118,36 @@ export class DatosPorGarantiaComponent implements OnInit, OnDestroy {
    * y se suscribe al estado para sincronizar los datos mostrados.
    */
   ngOnInit(): void {
+    this.inicializarEstadoFormulario();
+  }
+
+  /**
+   * Evalúa si se debe inicializar o cargar datos en el formulario.
+   */
+  inicializarEstadoFormulario(): void {
+    if (this.esFormularioSoloLectura) {
+      this.guardarDatosFormulario(); // Llama al método para cargar los datos del formulario
+    } else {
+      this.inicializarFormulario();
+    }
+  }
+
+  /**
+   * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+   * Luego reinicializa el formulario con los valores actualizados desde el store.
+   */
+  guardarDatosFormulario(): void {
+    this.inicializarFormulario();
+    if (this.esFormularioSoloLectura) {
+      this.polizaDeFianzaForm.disable();
+    } else if (!this.esFormularioSoloLectura) {
+      this.polizaDeFianzaForm.enable();
+    } else {
+      // No se requiere ninguna acción en el formulario
+    }
+  }
+
+  inicializarFormulario(): void {
     this.polizaDeFianzaForm = this.fb.group({
       polizaDeFianzaActual: [this.solicitud31301State.polizaDeFianzaActual],
       numeroFolio: [
--- apps/agace/src/app/application/tramites/31301/services/solicitud.service.ts
+++ apps/agace/src/app/application/tramites/31301/services/solicitud.service.ts
@@ -10,6 +10,8 @@ import { ModificacionDenominacionRazonSocial } from '../models/solicitud.model';
 import { Observable } from 'rxjs';
 import { RecibirNotificaciones } from '../models/solicitud.model';
 import { SeccionSociosIC } from '../models/solicitud.model';
+import { Solicitud31301State } from '../estados/solicitud31301.store';
+import { Solicitud31301Store } from '../estados/solicitud31301.store';
 import { SubContratistas } from '../models/solicitud.model';
 import { TipoDeInversion } from '../models/solicitud.model';
 
@@ -25,7 +27,10 @@ export class SolicitudService {
    * Constructor que inyecta el cliente HTTP.
    * @param http - Cliente HTTP para realizar peticiones.
    */
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    public solicitud31301Store: Solicitud31301Store
+  ) {
     // Lógica del constructor aquí
   }
 
@@ -131,5 +136,124 @@ export class SolicitudService {
     return this.http.get<DatosGeneralesDeLaSolicitudDatos>(
       'assets/json/31301/datos-generales-de-la-solicitud-datos.json'
     );
+  }
+
+  guardarDatosFormulario(): Observable<Solicitud31301State> {
+    return this.http.get<Solicitud31301State>(
+      'assets/json/31301/solicitud-31301-datos.json'
+    );
+  }
+
+  actualizarEstadoFormulario(resp: Solicitud31301State): void {
+    this.solicitud31301Store.actualizarTipoDeEndoso(resp.tipoDeEndoso);
+    this.solicitud31301Store.actualizarTipoDeGarantia(resp.tipoDeGarantia);
+    this.solicitud31301Store.actualizarModalidadDeLaGarantia(
+      resp.modalidadDeLaGarantia
+    );
+    this.solicitud31301Store.actualizarTipoSector(resp.tipoSector);
+    this.solicitud31301Store.actualizarConcepto(resp.concepto);
+    this.solicitud31301Store.actualizar3500(resp['3500']);
+    this.solicitud31301Store.actualizar3501(resp['3501']);
+    this.solicitud31301Store.actualizar3502(resp['3502']);
+    this.solicitud31301Store.actualizarDatosGeneralesRFC(
+      resp.datosGeneralesRFC
+    );
+    this.solicitud31301Store.actualizar3503(resp['3503']);
+    this.solicitud31301Store.actualizar3504(resp['3504']);
+    this.solicitud31301Store.actualizar3505(resp['3505']);
+    this.solicitud31301Store.actualizar3506(resp['3506']);
+    this.solicitud31301Store.actualizar3507(resp['3507']);
+    this.solicitud31301Store.actualizar3508(resp['3508']);
+    this.solicitud31301Store.actualizar3509(resp['3509']);
+    this.solicitud31301Store.actualizar3511(resp['3511']);
+    this.solicitud31301Store.actualizar3512(resp['3512']);
+    this.solicitud31301Store.actualizar3513(resp['3513']);
+    this.solicitud31301Store.actualizarTextoGenerico1(resp.textoGenerico1);
+    this.solicitud31301Store.actualizarTextoGenerico2(resp.textoGenerico2);
+    this.solicitud31301Store.actualizar3514(resp['3514']);
+    this.solicitud31301Store.actualizar3515(resp['3515']);
+    this.solicitud31301Store.actualizar3516(resp['3516']);
+    this.solicitud31301Store.actualizarTextoGenerico3(resp.textoGenerico3);
+    this.solicitud31301Store.actualizar3517(resp['3517']);
+    this.solicitud31301Store.actualizar3518(resp['3518']);
+    this.solicitud31301Store.actualizar3519(resp['3519']);
+    this.solicitud31301Store.actualizar3520(resp['3520']);
+    this.solicitud31301Store.actualizarTipoInversion(resp.tipoInversion);
+    this.solicitud31301Store.actualizarCantidadInversion(
+      resp.cantidadInversion
+    );
+    this.solicitud31301Store.actualizarDescInversion(resp.descInversion);
+    this.solicitud31301Store.actualizar3521(resp['3521']);
+    this.solicitud31301Store.actualizar3522(resp['3522']);
+    this.solicitud31301Store.actualizarClaveEnumeracionD0(
+      resp.claveEnumeracionD0
+    );
+    this.solicitud31301Store.actualizarClaveEnumeracionD1(
+      resp.claveEnumeracionD1
+    );
+    this.solicitud31301Store.actualizarClaveEnumeracionD2(
+      resp.claveEnumeracionD2
+    );
+    this.solicitud31301Store.actualizarClaveEnumeracionD3(
+      resp.claveEnumeracionD3
+    );
+    this.solicitud31301Store.actualizarClaveEnumeracionH(
+      resp.claveEnumeracionH
+    );
+    this.solicitud31301Store.actualizarTextoGenerico4(resp.textoGenerico4);
+    this.solicitud31301Store.actualizarTextoGenerico5(resp.textoGenerico5);
+    this.solicitud31301Store.actualizar3523(resp['3523']);
+    this.solicitud31301Store.actualizar3528(resp['3528']);
+    this.solicitud31301Store.actualizar3529(resp['3529']);
+    this.solicitud31301Store.actualizarTextoGenerico6(resp.textoGenerico6);
+    this.solicitud31301Store.actualizarTextoGenerico7(resp.textoGenerico7);
+    this.solicitud31301Store.actualizar3530(resp['3530']);
+    this.solicitud31301Store.actualizar3531(resp['3531']);
+    this.solicitud31301Store.actualizarTextoGenerico9(resp.textoGenerico9);
+    this.solicitud31301Store.actualizarTextoGenerico10(resp.textoGenerico10);
+    this.solicitud31301Store.actualizarTextoGenerico11(resp.textoGenerico11);
+    this.solicitud31301Store.actualizarTextoGenerico12(resp.textoGenerico12);
+    this.solicitud31301Store.actualizarTextoGenerico13(resp.textoGenerico13);
+    this.solicitud31301Store.actualizarTextoGenerico14(resp.textoGenerico14);
+    this.solicitud31301Store.actualizarTextoGenerico15(resp.textoGenerico15);
+    this.solicitud31301Store.actualizarTextoGenerico16(resp.textoGenerico16);
+    this.solicitud31301Store.actualizarTextoGenerico17(resp.textoGenerico17);
+    this.solicitud31301Store.actualizarTextoGenerico18(resp.textoGenerico18);
+    this.solicitud31301Store.actualizarTextoGenerico19(resp.textoGenerico19);
+    this.solicitud31301Store.actualizarTextoGenerico20(resp.textoGenerico20);
+    this.solicitud31301Store.actualizarTextoGenerico21(resp.textoGenerico21);
+    this.solicitud31301Store.actualizarTextoGenerico22(resp.textoGenerico22);
+    this.solicitud31301Store.actualizarTextoGenerico23(resp.textoGenerico23);
+    this.solicitud31301Store.actualizarTextoGenerico24(resp.textoGenerico24);
+    this.solicitud31301Store.actualizarAlerta1(resp.alerta1);
+    this.solicitud31301Store.actualizarAlerta2(resp.alerta2);
+    this.solicitud31301Store.actualizarPolizaDeFianzaActual(
+      resp.polizaDeFianzaActual
+    );
+    this.solicitud31301Store.actualizarNumeroFolio(resp.numeroFolio);
+    this.solicitud31301Store.actualizarRfcInstitucion(resp.rfcInstitucion);
+    this.solicitud31301Store.actualizarFechaExpedicion(resp.fechaExpedicion);
+    this.solicitud31301Store.actualizarFechaInicioVigenciaNo(
+      resp.fechaInicioVigenciaNo
+    );
+    this.solicitud31301Store.actualizarFechaFinVigenciaNo(
+      resp.fechaFinVigenciaNo
+    );
+    this.solicitud31301Store.actualizarFechaInicioVigencia(
+      resp.fechaInicioVigencia
+    );
+    this.solicitud31301Store.actualizarFechaFinVigencia(resp.fechaFinVigencia);
+    this.solicitud31301Store.actualizarImporteTotal(resp.importeTotal);
+    this.solicitud31301Store.actualizarRazonSocialAnterior(
+      resp.razonSocialAnterior
+    );
+    this.solicitud31301Store.actualizarRazonSocialActual(
+      resp.razonSocialActual
+    );
+    this.solicitud31301Store.actualizarRfc(resp.rfc);
+    this.solicitud31301Store.actualizarCurp(resp.curp);
+    this.solicitud31301Store.actualizarNombre(resp.nombre);
+    this.solicitud31301Store.actualizarApellidoPaterno(resp.apellidoPaterno);
+    this.solicitud31301Store.actualizarApellidoMaterno(resp.apellidoMaterno);
   }
 }
