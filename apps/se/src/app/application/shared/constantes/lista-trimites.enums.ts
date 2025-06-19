cf22d1ac9266bca293fd2cedb65ff348ab6185e6
Kanchan Phutane <kanchan.phutane@ilink-systems.com>
2025-06-19 17:18:35 +0530

    code changes for consulta

--- apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
+++ apps/se/src/app/application/shared/constantes/lista-trimites.enums.ts
@@ -963,5 +963,17 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
         componentName: 'PasoUnoComponent',
       }]
   },
+  {
+    tramite: 90304,
+    listaComponentes: [
+      {
+        id: 'solicitud',
+        componentPath: () =>
+          import('../../tramites/90304/pages/datos/datos.component').then(
+            (m) => m.DatosComponent
+          ),
+        componentName: 'datosComponent',
+      }]
+  }
   
 ];
