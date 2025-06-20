fc4202ec48b8ecbd841a8f0b7b0d2f2fa2a7a269
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-20 16:48:01 +0530

    test file added

--- apps/se/src/app/application/tramites/110101/components/datos-adicionales/datos-adicionales.component.spec.ts
+++ apps/se/src/app/application/tramites/110101/components/datos-adicionales/datos-adicionales.component.spec.ts
@@ -1,22 +1,67 @@
-/* eslint-disable dot-notation */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DatosAdicionalesComponent } from './datos-adicionales.component';
-
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
-import { ValidacionesFormularioService } from '@libs/shared/data-access-user/src';
+import { of, Subject } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { AlertComponent, TituloComponent, ConsultaioQuery } from '@ng-mf/data-access-user';
+import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component';
+import { Solicitante110101Query } from '../../estados/queries/solicitante110101.query';
+import { Tramite110101Store } from '../../estados/tramites/solicitante110101.store';
 
-fdescribe('DatosAdicionalesComponent', () => {
+describe('DatosAdicionalesComponent', () => {
   let component: DatosAdicionalesComponent;
   let fixture: ComponentFixture<DatosAdicionalesComponent>;
 
+  const MOCK_STATE = {
+    entidad: 'SINALOA',
+    representacion: 'CULIACAN',
+    rfc: '',
+    denominacion: '',
+    actividadEconomica: '',
+    correoElectronico: '',
+    pais: '',
+    tratado: '',
+    origen: '',
+    nombreComercial: '',
+    nombreIngles: '',
+    fraccionArancelaria: '',
+    descripcion: '',
+    valorTransaccion: '',
+  };
+
+  const mockSolicitanteQuery = {
+    selectSolicitante$: of(MOCK_STATE)
+  };
+
+  const mockConsultaioQuery = {
+    selectConsultaioState$: of({
+      readonly: false
+    })
+  };
+
+  const mockStore = {
+    setEntidad: jest.fn(),
+    setRepresentacion: jest.fn()
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, DatosAdicionalesComponent],
-      providers: [FormBuilder, ValidacionesFormularioService]
+      imports: [
+        DatosAdicionalesComponent,
+        ReactiveFormsModule,
+        CommonModule,
+        TituloComponent,
+        AlertComponent,
+        CatalogoSelectComponent
+      ],
+      providers: [
+        FormBuilder,
+        { provide: Tramite110101Store, useValue: mockStore },
+        { provide: Solicitante110101Query, useValue: mockSolicitanteQuery },
+        { provide: ConsultaioQuery, useValue: mockConsultaioQuery }
+      ]
     }).compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(DatosAdicionalesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -26,41 +71,52 @@ fdescribe('DatosAdicionalesComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call crearFormulario and initialize data in ngOnInit', () => {
-    spyOn(component, 'crearFormulario').and.callThrough();
-    spyOn(component, 'getEntidadFederativa').and.callThrough();
-    spyOn(component, 'getRepresentacionFederal').and.callThrough();
-
-    component.ngOnInit();
-
-    expect(component.crearFormulario).toHaveBeenCalled();
-    expect(component.getEntidadFederativa).toHaveBeenCalled();
-    expect(component.getRepresentacionFederal).toHaveBeenCalled();
+  it('should initialize form with values from store', () => {
+    component.inicializarFormulario();
+    expect(component.formulario.value).toEqual({
+      entidad: 'SINALOA',
+      representacion: 'CULIACAN'
+    });
   });
 
-
-  it('should initialize the form', () => {
-    expect(component.formulario).toBeDefined();
-    expect(component.formulario.controls['entidad']).toBeDefined();
-    expect(component.formulario.controls['representacion']).toBeDefined();
+  it('should disable form in readonly mode', () => {
+    component.esFormularioSoloLectura = true;
+    component.guardarDatosFormulario();
+    expect(component.formulario.disabled).toBe(true);
   });
 
-  it('should have default entity and representation values', () => {
+  it('should enable form when not readonly', () => {
+    component.esFormularioSoloLectura = false;
+    component.guardarDatosFormulario();
+    expect(component.formulario.enabled).toBe(true);
+  });
+
+  it('should load entidad federativa catalog', () => {
     component.getEntidadFederativa();
-    component.getRepresentacionFederal();
     expect(component.entidad.length).toBeGreaterThan(0);
+  });
+
+  it('should load representacion federal catalog', () => {
+    component.getRepresentacionFederal();
     expect(component.representacion.length).toBeGreaterThan(0);
   });
 
-  it('should validate required fields', () => {
-    component.formulario.controls['entidad'].setValue('');
-    component.formulario.controls['representacion'].setValue('');
-    expect(component.formulario.valid).toBeFalsy();
+  it('should call store methods with correct values', () => {
+    component.inicializarFormulario();
+    component.setValoresStore(component.formulario, 'entidad', 'setEntidad');
+    component.setValoresStore(component.formulario, 'representacion', 'setRepresentacion');
+
+    expect(mockStore.setEntidad).toHaveBeenCalledWith('SINALOA');
+    expect(mockStore.setRepresentacion).toHaveBeenCalledWith('CULIACAN');
   });
 
-  it('should set valid values in the form', () => {
-    component.formulario.controls['entidad'].setValue('SINALOA');
-    component.formulario.controls['representacion'].setValue('CULIACAN');
-    expect(component.formulario.valid).toBeTruthy();
+  it('should complete destroy$ on ngOnDestroy', () => {
+    const nextSpy = jest.spyOn(component['destroy$'], 'next');
+    const completeSpy = jest.spyOn(component['destroy$'], 'complete');
+
+    component.ngOnDestroy();
+
+    expect(nextSpy).toHaveBeenCalled();
+    expect(completeSpy).toHaveBeenCalled();
   });
 });
