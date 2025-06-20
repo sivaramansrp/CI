e7c552d96e6aea4612b1845e8eda5d7e21b3300e
anurag-ilink <anurag.dixit@ilink-systems.com>
2025-06-20 11:00:22 +0530

    Consulta implementation for 240311

--- apps/sedena/src/app/application/tramites/240311/pages/paso-uno/paso-uno.component.ts
+++ apps/sedena/src/app/application/tramites/240311/pages/paso-uno/paso-uno.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
+import { Subject, map , takeUntil } from 'rxjs';
+import { SolicitudPermisoService } from '../../services/solicitud-permiso.service';
 
 /**
  * @title Paso Uno
@@ -10,12 +13,69 @@ import { Component } from '@angular/core';
   templateUrl: './paso-uno.component.html',
   styleUrl: './paso-uno.component.scss',
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit {
+   /**
+   * Observable utilizado para limpiar las suscripciones al destruir el componente.
+   * Esto ayuda a evitar fugas de memoria.
+   */
+  private notificadorDestruccion$: Subject<void> = new Subject();
+  /**
+   * Estado de la consulta, que contiene información sobre el estado actual del formulario.
+   */
+  public consultaState!: ConsultaioState;
    /**
    * Índice utilizado para identificar la posición actual en un proceso o lista.
    * @type {number}
    */
    indice: number = 1;
+
+  /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+  public esDatosRespuesta: boolean = false;
+
+   /**
+   * Constructor del componente.
+   * Inicializa los servicios y dependencias necesarias.
+   */
+  constructor(
+    public solicitudPermisoService: SolicitudPermisoService,
+    private consultaQuery: ConsultaioQuery
+  ) {
+    
+  }
+  /**
+   * Método del ciclo de vida que se ejecuta al inicializar el componente.
+   * Configura las suscripciones necesarias y carga los datos iniciales.
+   */
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.notificadorDestruccion$),
+        map((seccionState) => {
+          this.consultaState = seccionState;
+        })
+      )
+      .subscribe();
+    if (this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+   /**
+   * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
+   * Luego reinicializa el formulario con los valores actualizados desde el store.
+   */
+  guardarDatosFormulario(): void {
+    this.solicitudPermisoService.getRegistroTomaMuestrasMercanciasData()
+      .pipe(takeUntil(this.notificadorDestruccion$))
+      .subscribe((resp) => {
+        if (resp) {
+          this.esDatosRespuesta = true;
+          this.solicitudPermisoService.actualizarEstadoFormulario(resp);
+        }
+      });
+  }
 
    /**
     * Selecciona una pestaña específica.
