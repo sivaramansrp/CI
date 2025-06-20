9232aae71f2aa33bf6163e1e5f616da53cd3bef8
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-20 13:52:51 +0530

    refactor: improve test structure and organization for CertificadoZoosanitarioServiceService

--- apps/agricultura/src/app/application/tramites/220201/services/220201/certificado-zoosanitario.service.spec.ts
+++ apps/agricultura/src/app/application/tramites/220201/services/220201/certificado-zoosanitario.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { CertificadoZoosanitarioServiceService } from './certificado-zoosanitario.service';
 
 describe('CertificadoZoosanitarioServiceService', () => {
   let service: CertificadoZoosanitarioServiceService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CertificadoZoosanitarioServiceService]
+    });
+    
     service = TestBed.inject(CertificadoZoosanitarioServiceService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it('should have a defined service', () => {
+    expect(service).toBeDefined();
   });
 });
