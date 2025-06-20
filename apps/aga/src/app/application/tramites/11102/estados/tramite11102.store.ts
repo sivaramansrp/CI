29301e7020fddfdfc09a73b90f22486ec4bf45d6
avinash-ilink <avinash.batule@ilink-system.com>
2025-06-20 12:12:13 +0530

    dev is done

--- apps/aga/src/app/application/tramites/11102/estados/tramite11102.store.ts
+++ apps/aga/src/app/application/tramites/11102/estados/tramite11102.store.ts
@@ -11,14 +11,14 @@ export interface Catalogo {
    * @type {number}
    */
   id: number;
-
+ 
   /**
    * Descripción del catálogo.
    * @type {string}
    */
   descripcion: string;
 }
-
+ 
 /**
  * Interfaz que representa el estado inicial de la solicitud 11102.
  */
@@ -27,138 +27,168 @@ export interface Solicitud11102State {
    * Nombre del organismo público.
    */
   organismoPublico: string;
-
+ 
   /**
    * Lista de aduanas disponibles.
    */
   aduana: Catalogo[] | null;
-
+ 
   /**
    * Uso específico de la mercancía.
    */
   usoEspecifico: string;
-
+ 
   /**
    * Bandera para mostrar la tabla.
    */
   showTabla: boolean;
-
+ 
   /**
    * Tipo de mercancía.
    */
   tipoDeMercancia: string;
-
+ 
   /**
    * Unidad de medida de la mercancía.
    */
   unidadMedida: string;
-
+ 
   /**
    * Condición de la mercancía.
    */
   condicionMercancia: string;
-
+ 
   /**
    * Lista de años disponibles.
    */
   ano: Catalogo[] | null;
-
+ 
   /**
    * Cantidad de mercancía.
    */
   cantidad: string;
-
+ 
   /**
    * Marca de la mercancía.
    */
   marca: string;
-
+ 
   /**
    * Modelo de la mercancía.
    */
   modelo: string;
-
+ 
   /**
    * Serie de la mercancía.
    */
   serie: string;
-
+ 
   /**
    * Lista de países disponibles.
    */
   pais: Catalogo[] | null;
-
+ 
   /**
    * RFC del solicitante.
    */
   rfc: string;
-
+ 
   /**
    * Número del programa IMMEX.
    */
   numeroProgramaImmex: string;
-
+ 
   /**
    * Razón social del solicitante.
    */
   razonSocial: string;
-
+ 
   /**
    * Calle del domicilio fiscal.
    */
   calle: string;
-
+ 
   /**
    * Número exterior del domicilio fiscal.
    */
   numeroExterior: string;
-
+ 
   /**
    * Número interior del domicilio fiscal.
    */
   numeroInterior: string;
-
+ 
   /**
    * Correo electrónico del solicitante.
    */
   correoElectronico: string;
-
+ 
   /**
    * Teléfono del solicitante.
    */
   telefono: string;
-
+ 
   /**
    * Correo electrónico opcional del solicitante.
    */
   correoElectronicoOpcional: string;
-
+ 
   /**
    * Teléfono opcional del solicitante.
    */
   telefonoOpcional: string;
-
+ 
   /**
    * Código postal del domicilio fiscal.
    */
   codigoPostal: string;
-
+ 
   /**
    * Estado del domicilio fiscal.
    */
   estado: string;
-
+ 
   /**
    * Colonia del domicilio fiscal.
    */
   colonia: string;
-
+ 
   /**
    * Datos relacionados con la mercancía.
    */
   datosDelMercancia: [];
 }
-
+ 
+export interface ModificacionDonacionesImmexResponse {
+  modificacionDonacionesImmex: {
+    organismoPublico: string;
+    aduana: { id: number; descripcion: string }[];
+    pais: { id: number; descripcion: string }[];
+    rfc: string;
+    numeroProgramaImmex: string;
+    razonSocial: string;
+    correoElectronicoOpcional: string;
+    telefonoOpcional: string;
+    calle: string;
+    numeroExterior: string;
+    numeroInterior: string;
+    telefono: string;
+    correoElectronico: string;
+    codigoPostal: string;
+    estado: string;
+    colonia: string;
+    datosMercancia: {
+      tipoDeMercancia: string;
+      cantidad: string;
+      unidadMedida: string;
+      ano: string;
+      modelo: string;
+      marca: string;
+      serie: string;
+      condicionMercancia: string;
+    };
+  };
+}
 /**
  * Función que crea el estado inicial de la solicitud 11102.
  * @returns Estado inicial de la solicitud.
@@ -194,7 +224,7 @@ export function createInitialState(): Solicitud11102State {
     datosDelMercancia: [],
   };
 }
-
+ 
 /**
  * Clase que representa el store para manejar el estado de la solicitud 11102.
  */
@@ -210,7 +240,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
   constructor() {
     super(createInitialState());
   }
-
+ 
   /**
    * Establece el organismo público en el estado.
    * @param organismoPublico Nombre del organismo público.
@@ -221,7 +251,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       organismoPublico,
     }));
   }
-
+ 
   /**
    * Establece la lista de aduanas en el estado.
    * @param aduana Lista de aduanas.
@@ -232,7 +262,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       aduana,
     }));
   }
-
+ 
   /**
    * Establece el tipo de mercancía en el estado.
    * @param tipoDeMercancia Tipo de mercancía.
@@ -243,7 +273,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       tipoDeMercancia,
     }));
   }
-
+ 
   /**
    * Establece la unidad de medida en el estado.
    * @param unidadMedida Unidad de medida.
@@ -254,7 +284,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       unidadMedida,
     }));
   }
-
+ 
   /**
    * Establece la condición de la mercancía en el estado.
    * @param condicionMercancia Condición de la mercancía.
@@ -265,7 +295,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       condicionMercancia,
     }));
   }
-
+ 
   /**
    * Establece la lista de años en el estado.
    * @param ano Lista de años.
@@ -276,7 +306,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       ano,
     }));
   }
-
+ 
   /**
    * Establece la cantidad de mercancía en el estado.
    * @param cantidad Cantidad de mercancía.
@@ -287,7 +317,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       cantidad,
     }));
   }
-
+ 
   /**
    * Establece la marca de la mercancía en el estado.
    * @param marca Marca de la mercancía.
@@ -298,7 +328,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       marca,
     }));
   }
-
+ 
   /**
    * Establece el modelo de la mercancía en el estado.
    * @param modelo Modelo de la mercancía.
@@ -309,7 +339,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       modelo,
     }));
   }
-
+ 
   /**
    * Establece la serie de la mercancía en el estado.
    * @param serie Serie de la mercancía.
@@ -320,7 +350,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       serie,
     }));
   }
-
+ 
   /**
    * Establece la lista de países en el estado.
    * @param pais Lista de países.
@@ -331,7 +361,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       pais,
     }));
   }
-
+ 
   /**
    * Establece el uso específico de la mercancía en el estado.
    * @param usoEspecifico Uso específico de la mercancía.
@@ -342,7 +372,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       usoEspecifico,
     }));
   }
-
+ 
   /**
    * Establece la calle del domicilio fiscal en el estado.
    * @param calle Calle del domicilio fiscal.
@@ -353,7 +383,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       calle,
     }));
   }
-
+ 
   /**
    * Establece el número exterior del domicilio fiscal en el estado.
    * @param numeroExterior Número exterior del domicilio fiscal.
@@ -364,7 +394,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       numeroExterior,
     }));
   }
-
+ 
   /**
    * Establece el número interior del domicilio fiscal en el estado.
    * @param numeroInterior Número interior del domicilio fiscal.
@@ -375,7 +405,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       numeroInterior,
     }));
   }
-
+ 
   /**
    * Establece el teléfono del solicitante en el estado.
    * @param telefono Teléfono del solicitante.
@@ -386,7 +416,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       telefono,
     }));
   }
-
+ 
   /**
    * Establece el correo electrónico del solicitante en el estado.
    * @param correoElectronico Correo electrónico del solicitante.
@@ -397,7 +427,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       correoElectronico,
     }));
   }
-
+ 
   /**
    * Establece el código postal del domicilio fiscal en el estado.
    * @param codigoPostal Código postal del domicilio fiscal.
@@ -408,7 +438,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       codigoPostal,
     }));
   }
-
+ 
   /**
    * Establece el estado del domicilio fiscal en el estado.
    * @param estado Estado del domicilio fiscal.
@@ -419,7 +449,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       estado,
     }));
   }
-
+ 
   /**
    * Establece la colonia del domicilio fiscal en el estado.
    * @param colonia Colonia del domicilio fiscal.
@@ -430,7 +460,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       colonia,
     }));
   }
-
+ 
   /**
    * Establece el RFC del solicitante en el estado.
    * @param rfc RFC del solicitante.
@@ -441,7 +471,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       rfc,
     }));
   }
-
+ 
   /**
    * Establece el número del programa IMMEX en el estado.
    * @param numeroProgramaImmex Número del programa IMMEX.
@@ -452,7 +482,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       numeroProgramaImmex,
     }));
   }
-
+ 
   /**
    * Establece la razón social del solicitante en el estado.
    * @param razonSocial Razón social del solicitante.
@@ -463,7 +493,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       razonSocial,
     }));
   }
-
+ 
   /**
    * Establece el correo electrónico opcional del solicitante en el estado.
    * @param correoElectronicoOpcional Correo electrónico opcional del solicitante.
@@ -474,7 +504,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       correoElectronicoOpcional,
     }));
   }
-
+ 
   /**
    * Establece el teléfono opcional del solicitante en el estado.
    * @param telefonoOpcional Teléfono opcional del solicitante.
@@ -485,7 +515,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       telefonoOpcional,
     }));
   }
-
+ 
   /**
    * Establece los datos del contenedor.
    * @param datosDelMercancia Datos del contenedor.
@@ -496,7 +526,7 @@ export class Tramite11102Store extends Store<Solicitud11102State> {
       datosDelMercancia,
     }));
   }
-
+ 
   /**
    * Limpia los datos de la solicitud.
    */
--- apps/aga/src/app/application/tramites/11102/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/11102/pages/paso-uno/paso-uno.component.ts
@@ -1,25 +1,32 @@
-import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import {
+  ConsultaioQuery,
+  ConsultaioState,
+  DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL,
   DatosPasos,
   FormularioDinamico,
   ListaPasosWizard,
+  PERSONA_MORAL_NACIONAL,
   SolicitanteComponent,
+  TIPO_PERSONA,
   WizardComponent,
 } from '@ng-mf/data-access-user';
-import { PASOS } from '@libs/shared/data-access-user/src/tramites/constantes/11105/pasos.enum';
-
+import { Subject, map, takeUntil } from 'rxjs';
+import { ModificacionDonacionesImmexService } from '../../services/modificacion-donaciones-immex.service';
+import { PASOS } from '../../constants/pasos.enum';
+ 
 interface AccionBoton {
   /**
    * Acción a realizar (e.g., 'cont' para continuar, 'ant' para retroceder).
    */
   accion: string;
-
+ 
   /**
    * Índice del paso al que se desea navegar.
    */
   valor: number;
 }
-
+ 
 /**
  * Componente que representa el paso uno del trámite.
  */
@@ -27,47 +34,124 @@ interface AccionBoton {
   selector: 'paso-uno',
   templateUrl: './paso-uno.component.html',
 })
-export class PasoUnoComponent {
+export class PasoUnoComponent implements OnInit, OnDestroy,AfterViewInit {
+ 
+    constructor(
+    private service11102:ModificacionDonacionesImmexService, // Servicio para manejar la solicitud
+    private consultaQuery: ConsultaioQuery // Servicio para consultar el estado
+  ) { }
+ 
+ 
+ 
+  /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+  public esDatosRespuesta: boolean = false; // Indica si hay datos de respuesta del servidor
+ 
+/**
+ * Subject utilizado para notificar y manejar la destrucción de suscripciones.
+ *
+ * Se emite y completa en el método `ngOnDestroy` para evitar fugas de memoria
+ * al cancelar todas las suscripciones activas del componente.
+ */
+  private destroyNotifier$: Subject<void> = new Subject(); // Subject para manejar la destrucción de suscripciones
+ 
+  /**
+   * Estado de la consulta del trámite.
+   * Este estado se utiliza para manejar la información del trámite actual.
+   */
+  public consultaState!: ConsultaioState; // Estado de la consult
   /**
    * Evento que se emite al continuar con el flujo del trámite.
    */
   @Output() continuarEvento = new EventEmitter<string>();
-
+ 
   /**
    * Referencia al componente de solicitante.
    */
   @ViewChild(SolicitanteComponent) solicitante!: SolicitanteComponent;
-
+ 
   /**
    * Tipo de persona (e.g., física o moral).
    */
   tipoPersona!: number;
-
+ 
   /**
    * Datos del formulario dinámico de la persona.
    */
   persona: FormularioDinamico[] = [];
-
+ 
   /**
    * Datos del formulario dinámico del domicilio fiscal.
    */
   domicilioFiscal: FormularioDinamico[] = [];
-
+ 
   /**
    * Índice de la pestaña seleccionada en el wizard.
    */
   indice: number = 1;
-
+ 
   /**
    * Lista de pasos del wizard.
    */
   pasos: ListaPasosWizard[] = PASOS;
-
+ 
   /**
    * Referencia al componente del wizard.
    */
   @ViewChild(WizardComponent) wizardComponent!: WizardComponent;
-
+ 
+  /**
+ * Método del ciclo de vida de Angular que se ejecuta al inicializar el componente.
+ *
+ * - Se suscribe al observable del estado de consulta (`selectConsultaioState$`) y actualiza la propiedad `consultaState` con el valor recibido.
+ * - Utiliza `takeUntil` para cancelar la suscripción automáticamente cuando el componente se destruye, evitando fugas de memoria.
+ * - Si el estado indica que está en modo actualización (`update`), llama a `guardarDatosFormulario()` para cargar los datos.
+ * - Si no está en modo actualización, activa el modo de solo lectura para mostrar los datos de respuesta.
+ */
+    ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$.pipe(
+      takeUntil(this.destroyNotifier$), // Se desuscribe al destruir el componente
+      map((consultaState) => {
+        this.consultaState = consultaState; // Asigna el estado de la consulta
+      })
+    ).subscribe();
+   
+    if (this.consultaState?.update) {
+      this.guardarDatosFormulario(); // Si está en modo actualización, guarda los datos del formulario
+    } else {
+      this.esDatosRespuesta = true; // Si no, activa el modo de datos de respuesta
+    }
+  }
+ 
+      /**
+   * Obtiene los datos de la solicitud desde el servicio y actualiza el estado global.
+   *
+   * Realiza una petición al servicio para obtener los datos de la solicitud.
+   * Al recibir la respuesta, marca que existen datos de respuesta y construye un objeto `Solicitud31803State`
+   * con los datos recibidos. Luego, actualiza el estado global del formulario utilizando el método
+   * `actualizarEstadoFormulario` del servicio.
+   * La suscripción se cancela automáticamente al destruir el componente para evitar fugas de memoria.
+   */
+    guardarDatosFormulario(): void {
+    this.service11102
+      .getDatosDeTrtamitelDoc().pipe(
+        takeUntil(this.destroyNotifier$) // Se desuscribe al destruir el componente
+      )
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .subscribe((resp:any) => {
+        if (resp) {
+          this.esDatosRespuesta = true; // Marca que hay datos de respuesta
+          this.service11102.actualizarEstadoFormulario(resp?.modificacionDonacionesImmex); // Actualiza el estado del formulario con la respuesta
+        }
+      });
+  }
+      /**
+   * Método que se ejecuta después de que la vista ha sido inicializada.
+   */
+  ngAfterViewInit(): void {
+    this.persona = PERSONA_MORAL_NACIONAL; // Asigna los datos de persona moral nacional
+    this.domicilioFiscal = DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL; // Asigna los datos de domicilio fiscal
+    this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL); // Llama al método para obtener el tipo de persona en el componente hijo
+  }
   /**
    * Datos de configuración para los pasos del wizard.
    */
@@ -77,7 +161,7 @@ export class PasoUnoComponent {
     txtBtnAnt: 'Anterior',
     txtBtnSig: 'Continuar',
   };
-
+ 
   /**
    * Selecciona la pestaña indicada por el índice.
    * @param i Índice de la pestaña a seleccionar.
@@ -85,14 +169,14 @@ export class PasoUnoComponent {
   seleccionaTab(i: number): void {
     this.indice = i;
   }
-
+ 
   /**
    * Emite el evento para continuar con el flujo del trámite.
    */
   continuar(): void {
     this.continuarEvento.emit('');
   }
-
+ 
   /**
    * Cambia el índice del wizard según la acción recibida.
    * @param e Objeto que contiene la acción ('cont' o 'ant') y el índice del paso.
@@ -107,4 +191,15 @@ export class PasoUnoComponent {
       }
     }
   }
+/**
+ * Método del ciclo de vida de Angular que se ejecuta al destruir el componente.
+ *
+ * Este método emite un evento a través de `destroyNotifier$` para notificar a las suscripciones activas
+ * que deben cancelarse, evitando fugas de memoria. Luego, completa el subject para liberar recursos.
+ */
+ngOnDestroy(): void {
+  this.destroyNotifier$.next(); // Emite el evento de destrucción
+  this.destroyNotifier$.complete(); // Completa el subject para limpiar suscripciones
 }
+}
+ 
