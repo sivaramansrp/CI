c13fcb1a41080caf7922a581eef8ea8229987271
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-06-23 18:35:46 +0530

    120204 pages test case

--- apps/se/src/app/application/tramites/120204/pages/datos/datos.component.spec.ts
+++ apps/se/src/app/application/tramites/120204/pages/datos/datos.component.spec.ts
@@ -1,54 +1,65 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DatosComponent } from './datos.component';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
+import { ExpedicionCertificadoService } from '../../services/expedicion-certificado.service';
 import { of, Subject } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('DatosComponent', () => {
   let component: DatosComponent;
+  let fixture: ComponentFixture<DatosComponent>;
   let consultaQueryMock: any;
-  let consultaStoreMock: any;
   let expedicionServiceMock: any;
-  let tramiteStoreMock: any;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     consultaQueryMock = {
-      selectConsultaioState$: of({ update: true })
-    };
-    consultaStoreMock = {
-      establecerConsultaio: jest.fn()
+      selectConsultaioState$: of({ update: false, readonly: false }),
     };
     expedicionServiceMock = {
-      getExpedienteCertificado: jest.fn().mockReturnValue(of({ foo: 'bar' })),
-      setDatosFormulario: jest.fn()
+      getExpedienteCertificado: jest.fn().mockReturnValue(of({ campo: 'valor' })),
+      setDatosFormulario: jest.fn(),
     };
-    tramiteStoreMock = {};
-    component = new DatosComponent(
-      consultaQueryMock,
-      consultaStoreMock,
-      expedicionServiceMock,
-      tramiteStoreMock
-    );
+
+    await TestBed.configureTestingModule({
+      declarations: [DatosComponent],
+      providers: [
+        { provide: ConsultaioQuery, useValue: consultaQueryMock },
+        { provide: ExpedicionCertificadoService, useValue: expedicionServiceMock },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DatosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should set indice on seleccionaTab', () => {
-    component.seleccionaTab(2);
-    expect(component.indice).toBe(2);
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should call guardarDatosFormulario if consultaState.update is true on ngOnInit', () => {
-    const guardarSpy = jest.spyOn(component, 'guardarDatosFormulario');
-    component.consultaState = { update: true } as any;
+  it('should set esDatosRespuesta to true if consultaState.update is false on ngOnInit', () => {
+    component.consultaState = { update: false, readonly: false } as any;
     component.ngOnInit();
-    expect(guardarSpy).toHaveBeenCalled();
+    expect(component.esDatosRespuesta).toBe(true);
   });
 
-  it('should set esDatosRespuesta true and call setDatosFormulario in guardarDatosFormulario', () => {
+  it('should set esDatosRespuesta to true and call setDatosFormulario in guardarDatosFormulario', () => {
     component.guardarDatosFormulario();
     expect(component.esDatosRespuesta).toBe(true);
-    expect(expedicionServiceMock.setDatosFormulario).toHaveBeenCalledWith({ foo: 'bar' });
+    expect(expedicionServiceMock.setDatosFormulario).toHaveBeenCalledWith({ campo: 'valor' });
   });
 
-  it('should complete destroyNotifier$ on ngOnDestroy', () => {
-    const completeSpy = jest.spyOn(component['destroyNotifier$'], 'complete');
+  it('should set indice when seleccionaTab is called', () => {
+    component.seleccionaTab(5);
+    expect(component.indice).toBe(5);
+  });
+
+  it('should clean up subscriptions on ngOnDestroy', () => {
+    const nextSpy = jest.spyOn<any, any>(component['destroyNotifier$'], 'next');
+    const completeSpy = jest.spyOn<any, any>(component['destroyNotifier$'], 'complete');
     component.ngOnDestroy();
+    expect(nextSpy).toHaveBeenCalled();
     expect(completeSpy).toHaveBeenCalled();
   });
 });
