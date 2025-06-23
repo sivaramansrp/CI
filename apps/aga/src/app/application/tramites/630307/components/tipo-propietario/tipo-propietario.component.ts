d3a581faead035faa853dce4e619c635ec5b4d13
Tanishk Kalaskar <tanishk.kalaskar@ilink-systems.com>
2025-06-23 22:04:22 +0530

    tipo fix

--- apps/aga/src/app/application/tramites/630307/components/tipo-propietario/tipo-propietario.component.ts
+++ apps/aga/src/app/application/tramites/630307/components/tipo-propietario/tipo-propietario.component.ts
@@ -3,11 +3,11 @@
  * Componente que gestiona los datos del tipo de propietario para el trámite 630307.
  * Permite inicializar formularios, obtener datos de catálogos y manejar el estado del formulario.
  */
-import { CommonModule } from '@angular/common';
-
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject,Subscription, map, takeUntil } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
 
 import { Catalogo, ModeloDeFormaDinamica } from '@libs/shared/data-access-user/src';
 import { FormasDinamicasComponent } from '@libs/shared/data-access-user/src/tramites/components/formas-dinamicas/formas-dinamicas/formas-dinamicas.component';
@@ -88,6 +88,17 @@ export class TipoPropietarioComponent implements OnInit, OnDestroy {
   private destroyed$ = new Subject<void>();
 
   /**
+   * Suscripción a los cambios en el formulario reactivo.
+   */
+  private subscription: Subscription = new Subscription();
+
+  /**
+   * Indica si el formulario está en modo solo lectura.
+   * Cuando es `true`, los campos del formulario no se pueden editar.
+   */
+  esFormularioSoloLectura: boolean = false;
+
+  /**
    * Constructor del componente.
    * 
    * @param fb - Constructor de formularios reactivos.
@@ -99,8 +110,19 @@ export class TipoPropietarioComponent implements OnInit, OnDestroy {
     private fb: FormBuilder,
     private tramite630307Store: Tramite630307Store,
     private tramite630307Query: Tramite630307Query,
-    private retornoImportacionTemporalService: RetornoImportacionTemporalService
-  ) {}
+    private retornoImportacionTemporalService: RetornoImportacionTemporalService,
+    private consultaioQuery: ConsultaioQuery
+  ) {
+  this.consultaioQuery.selectConsultaioState$
+    .pipe(
+      takeUntil(this.destroyed$),
+      map((seccionState) => {
+       this.esFormularioSoloLectura = seccionState.readonly;
+       this.inicializarDatosIniciales();
+      })
+    )
+    .subscribe();
+  }
 
   /**
    * Método del ciclo de vida que se ejecuta al inicializar el componente.
@@ -108,12 +130,37 @@ export class TipoPropietarioComponent implements OnInit, OnDestroy {
    */
   ngOnInit(): void {
     this.getValorStore();
+    this.inicializarEstadoFormulario();
+  }
+
+  inicializarDatosIniciales(): void {
     this.inicializarFormulario();
     this.getPropietario();
     this.getTipoDePropietario();
     this.getPais();
     this.cambiarPropietario();
     this.cambiarTipoPropietario();
+  }
+
+  /**
+   * Evalúa si se debe inicializar o cargar datos en el formulario.
+   * Además, obtiene la información del catálogo de mercancía.
+   */
+  inicializarEstadoFormulario(): void {
+    if (this.esFormularioSoloLectura) {
+      this.guardarDatosFormulario();
+    } else {
+      this.inicializarDatosIniciales();
+    }
+  }
+
+   guardarDatosFormulario(): void {
+    this.inicializarDatosIniciales();
+    if (this.esFormularioSoloLectura) {
+      this.tipoPropietarioFormulario.disable();
+    } else {
+      this.tipoPropietarioFormulario.enable();
+    }
   }
 
   /**
