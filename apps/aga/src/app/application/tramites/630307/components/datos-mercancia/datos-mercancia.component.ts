643c982610334992f706862ab7b78c08e5a5be9f
Tanishk Kalaskar <tanishk.kalaskar@ilink-systems.com>
2025-06-23 22:31:33 +0530

    datos mercancia fix

--- apps/aga/src/app/application/tramites/630307/components/datos-mercancia/datos-mercancia.component.ts
+++ apps/aga/src/app/application/tramites/630307/components/datos-mercancia/datos-mercancia.component.ts
@@ -8,12 +8,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ModeloDeFormaDinamica } from '@ng-mf/data-access-user';
 
-import { Subject, takeUntil } from 'rxjs';
-
+import { Subject, Subscription, map, takeUntil } from 'rxjs';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
 import { FORMULARIO_DATOS_MERCANCIA } from '../../enum/retorno-importacion-temporal.enum';
 import { Tramite630307Query } from '../../estados/tramite630307.query';
 
-import { Tramite630307State, Tramite630307Store } from '../../estados/tramite630307.store';
+import {
+  Tramite630307State,
+  Tramite630307Store,
+} from '../../estados/tramite630307.store';
 import { FormasDinamicasComponent } from '@libs/shared/data-access-user/src/tramites/components/formas-dinamicas/formas-dinamicas/formas-dinamicas.component';
 /**
  * Componente que gestiona los datos de la mercancía para el trámite 630307.
@@ -44,11 +47,23 @@ export class DatosMercanciaComponent implements OnInit, OnDestroy {
   /**
    * Formulario dinámico que define la estructura del formulario de datos de mercancía.
    */
-  formularioDatosMercancia: ModeloDeFormaDinamica[] = FORMULARIO_DATOS_MERCANCIA;
+  formularioDatosMercancia: ModeloDeFormaDinamica[] =
+    FORMULARIO_DATOS_MERCANCIA;
+
+  /**
+   * Suscripción a los cambios en el formulario reactivo.
+   */
+  private subscription: Subscription = new Subscription();
+
+  /**
+   * Indica si el formulario está en modo solo lectura.
+   * Cuando es `true`, los campos del formulario no se pueden editar.
+   */
+  esFormularioSoloLectura: boolean = false;
 
   /**
    * Constructor del componente.
-   * 
+   *
    * @param formBuilder - Constructor de formularios reactivos.
    * @param tramite630307Store - Store para manejar el estado del trámite.
    * @param tramite630307Query - Query para consultar el estado del trámite.
@@ -56,24 +71,66 @@ export class DatosMercanciaComponent implements OnInit, OnDestroy {
   constructor(
     private formBuilder: FormBuilder,
     private tramite630307Store: Tramite630307Store,
-    private tramite630307Query: Tramite630307Query
-  ) {}
-
+    private tramite630307Query: Tramite630307Query,
+    private consultaioQuery: ConsultaioQuery
+  ) {
+    this.consultaioQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroyed$),
+        map((seccionState) => {
+          this.esFormularioSoloLectura = seccionState.readonly;
+          this.inicializarFormulario();
+        })
+      )
+      .subscribe();
+  }
   /**
    * Método del ciclo de vida que se ejecuta al inicializar el componente.
    * Inicializa el formulario y obtiene el estado del trámite.
    */
   ngOnInit(): void {
     this.getValorStore();
+    this.inicializarEstadoFormulario();
+  }
+
+  /**
+   * Evalúa si se debe inicializar o cargar datos en el formulario.
+   * Además, obtiene la información del catálogo de mercancía.
+   */
+  inicializarEstadoFormulario(): void {
+    if (this.esFormularioSoloLectura) {
+      this.guardarDatosFormulario();
+    } else {
+      this.inicializarFormulario();
+    }
+  }
+
+   /**
+   * @method
+   * @name guardarDatosFormulario
+   * @description
+   * Inicializa los formularios y obtiene los datos de la tabla.
+   * Dependiendo del modo de solo lectura (`esFormularioSoloLectura`),
+   * deshabilita o habilita todos los formularios del componente.
+   * Si el formulario está en modo solo lectura, todos los formularios se deshabilitan para evitar modificaciones.
+   * Si no está en modo solo lectura, todos los formularios se habilitan para permitir la edición.
+   *
+   * @returns {void}
+   */
+  guardarDatosFormulario(): void {
     this.inicializarFormulario();
+    if (this.esFormularioSoloLectura) {
+      this.datosMercancia.disable();
+    } else {
+      this.datosMercancia.enable();
+    }
   }
 
   /**
    * Inicializa el formulario reactivo con valores predeterminados y validaciones.
    */
   inicializarFormulario(): void {
-    this.datosMercancia = this.formBuilder.group({
-    });
+    this.datosMercancia = this.formBuilder.group({});
   }
 
   /**
@@ -89,7 +146,7 @@ export class DatosMercanciaComponent implements OnInit, OnDestroy {
 
   /**
    * Establece un cambio de valor en el store basado en un evento.
-   * 
+   *
    * @param $event - Evento que contiene el campo y el valor a actualizar.
    */
   establecerCambioDeValor($event: { campo: string; valor: unknown }): void {
