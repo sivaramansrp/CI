7873f504e4f80fec7ca6ef7219362e24c872cb67
TejaswiniB-ilink <tejaswini.bhandare@ilink-systems.com>
2025-06-23 19:58:26 +0530

    Trámite 420103 : consulta journey

--- apps/aga/src/app/application/tramites/420103/pages/paso-uno/paso-uno.component.ts
+++ apps/aga/src/app/application/tramites/420103/pages/paso-uno/paso-uno.component.ts
@@ -1,9 +1,11 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { FormularioDinamico } from '@ng-mf/data-access-user';
-import { SolicitanteComponent } from '@ng-mf/data-access-user';
-
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ConsultaioQuery, ConsultaioState, FormularioDinamico } from '@ng-mf/data-access-user';
 import { DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL, PERSONA_MORAL_NACIONAL } from '@libs/shared/data-access-user/src/tramites/constantes/solicitante-constantes.enum';
+import { Subject, map, takeUntil } from 'rxjs';
+import { ConcluirRelacionService } from '../../services/concluir-relacion.service';
+import { SolicitanteComponent } from '@ng-mf/data-access-user';
 import { TIPO_PERSONA } from '@ng-mf/data-access-user';
+
 /**
  * Componente que representa el paso uno del trámite.
  */
@@ -11,7 +13,7 @@ import { TIPO_PERSONA } from '@ng-mf/data-access-user';
   selector: 'paso-uno',
   templateUrl: './paso-uno.component.html',
 })
-export class PasoUnoComponent implements AfterViewInit {
+export class PasoUnoComponent implements AfterViewInit, OnInit, OnDestroy {
   /**
    * Referencia al componente de solicitante.
    */
@@ -37,13 +39,74 @@ export class PasoUnoComponent implements AfterViewInit {
    */
   indice: number = 1;
 
+  /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
+  public esDatosRespuesta: boolean = false;
+
+  /** Subject para notificar la destrucción del componente. */
+  private destroyNotifier$: Subject<void> = new Subject();
+
+  /* Estado actual de la consulta cargado desde el store.
+   * Contiene datos como modo de solo lectura y valores del formulario.
+   */
+  public consultaState!: ConsultaioState;
+  
+  /**
+   * Constructor que inyecta los servicios necesarios para manejar el estado y la consulta.
+   * La lógica de inicialización se delega a métodos específicos.
+   */
+  constructor(
+    private concluirRelacionService: ConcluirRelacionService,
+    private consultaQuery: ConsultaioQuery
+  ) {
+    // Constructor vacío: La inicialización se realizará en métodos específicos según sea necesario.
+  }
+  
+  /**
+   * Inicializa el componente suscribiéndose al estado de consulta.
+   * Según el estado, carga datos del formulario o marca como respuesta disponible.
+   */
+  ngOnInit(): void {
+    this.consultaQuery.selectConsultaioState$.pipe(takeUntil(this.destroyNotifier$),map((seccionState) => {
+        this.consultaState = seccionState;
+    })).subscribe();
+    if(this.consultaState.update) {
+      this.guardarDatosFormulario();
+    } else {
+      this.esDatosRespuesta = true;
+    }
+  }
+
+  /**
+  * Obtiene los datos de la solicitud desde un servicio y actualiza el estado del formulario.  
+  * Si la respuesta es válida, activa el indicador de datos cargados.
+  */
+  guardarDatosFormulario(): void {
+    this.concluirRelacionService
+      .getDatosDeLaSolicitud().pipe(
+        takeUntil(this.destroyNotifier$)
+      )
+      .subscribe((resp) => {
+        if(resp){
+        this.esDatosRespuesta = true;
+        this.concluirRelacionService.actualizarEstadoFormulario(resp);
+        }else {
+          this.esDatosRespuesta = false;
+        }
+      });
+  }
+
   /**
    * Método que se ejecuta después de que la vista ha sido inicializada.
    */
   ngAfterViewInit(): void {
     this.persona = PERSONA_MORAL_NACIONAL;
     this.domicilioFiscal = DOMICILIO_FISCAL_PERSONA_MORAL_O_FISICA_NACIONAL;
-    this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+
+   setTimeout(() => {
+    if (this.solicitante) {
+      this.solicitante.obtenerTipoPersona(TIPO_PERSONA.MORAL_NACIONAL);
+    }
+  });
   }
 
   /**
@@ -52,5 +115,14 @@ export class PasoUnoComponent implements AfterViewInit {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+  /**
+  * Método de limpieza que se ejecuta al destruir el componente.  
+  * Finaliza las suscripciones observables utilizando `destroyNotifier$`.
+  */
+  ngOnDestroy(): void {
+    this.destroyNotifier$.next();
+    this.destroyNotifier$.complete();
   }
 }
