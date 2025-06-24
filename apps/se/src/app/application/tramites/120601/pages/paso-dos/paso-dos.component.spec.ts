284510dbcf682a06031d6e6716b23231feb795b6
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-06-24 18:46:03 +0530

    120601 proc test case issue fixes

--- apps/se/src/app/application/tramites/120601/pages/paso-dos/paso-dos.component.spec.ts
+++ apps/se/src/app/application/tramites/120601/pages/paso-dos/paso-dos.component.spec.ts
@@ -1,34 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PasoDosComponent } from './paso-dos.component';
+import { CatalogosService } from '@ng-mf/data-access-user';
+import { of, throwError } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Subject } from 'rxjs';
 
 describe('PasoDosComponent', () => {
   let component: PasoDosComponent;
   let fixture: ComponentFixture<PasoDosComponent>;
+  let mockCatalogosService: any;
 
   beforeEach(async () => {
+    mockCatalogosService = {
+      getCatalogo: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
-      imports: [PasoDosComponent, HttpClientTestingModule],
+      imports: [PasoDosComponent, ReactiveFormsModule, HttpClientTestingModule],
+      providers: [
+        { provide: CatalogosService, useValue: mockCatalogosService }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasoDosComponent);
     component = fixture.componentInstance;
-
-    // Ensure cargaArchivosEvento is initialized (it is, in the component)
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have TEXTOS defined', () => {
-    expect(component.TEXTOS).toBeDefined();
+  it('should set catalogoDocumentos when getTiposDocumentos returns data', () => {
+    const mockDocs = [{ id: 1, descripcion: 'Doc1' }, { id: 2, descripcion: 'Doc2' }];
+    mockCatalogosService.getCatalogo.mockReturnValue(of(mockDocs));
+    component.getTiposDocumentos();
+    expect(component.catalogoDocumentos).toEqual(mockDocs);
   });
 
-  it('should have cargaArchivosEvento as a Subject', () => {
-    expect(component.cargaArchivosEvento).toBeInstanceOf(Subject);
+  it('should not set catalogoDocumentos when getTiposDocumentos returns empty array', () => {
+    mockCatalogosService.getCatalogo.mockReturnValue(of([]));
+    component.catalogoDocumentos = [{ id: 1, descripcion: 'Should be replaced' }];
+    component.getTiposDocumentos();
+    expect(component.catalogoDocumentos).toEqual([{ id: 1, descripcion: 'Should be replaced' }]);
   });
-  
+
+  it('should handle error in getTiposDocumentos gracefully', () => {
+    mockCatalogosService.getCatalogo.mockReturnValue(throwError(() => new Error('fail')));
+    expect(() => component.getTiposDocumentos()).not.toThrow();
+  });
 });
