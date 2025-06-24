f590eabd118dfc8bb7e406c06749a2959a4a43f7
dhiwarkar <dipak.hiwarkar@ilink-systems.com>
2025-06-24 10:47:23 +0530

    resolved conflicts

--- apps/se/src/app/application/tramites/140205/services/cancelacionCertificados.service.ts
+++ apps/se/src/app/application/tramites/140205/services/cancelacionCertificados.service.ts
@@ -1,4 +1,4 @@
-import { CatalogoLista, CuposTablaDatos } from '../model/cancelaciones-certificado.model';
+import { CatalogoLista, CuposTablaDatos, RespuestaConsulta } from '../model/cancelaciones-certificado.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
