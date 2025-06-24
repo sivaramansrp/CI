fd8cc3aaba0f4165560c28304a9a819c8b323e9e
revankale-ilink <revansiddh.kale@ilink-systems.com>
2025-06-24 14:58:12 +0530

    80208 code coverage fix

--- apps/se/src/app/application/tramites/80208/pages/paso-uno/paso-uno.component.spec.ts
+++ apps/se/src/app/application/tramites/80208/pages/paso-uno/paso-uno.component.spec.ts
@@ -1,23 +1,34 @@
 // @ts-nocheck
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Pipe, PipeTransform, Injectable, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, Directive, Input, Output } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Observable, of as observableOf, throwError } from 'rxjs';
+
+import { Component } from '@angular/core';
 import { PasoUnoComponent } from './paso-uno.component';
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SeccionLibStore, ConsultaioQuery } from '@ng-mf/data-access-user';
+import { CambioModalidadService } from '../../service/cambio-modalidad.service';
+
+@Injectable()
+class MockCambioModalidadService {}
 
 
 describe('PasoUnoComponent', () => {
   let fixture;
-  let component: PasoUnoComponent;
-
+  let component;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ FormsModule, ReactiveFormsModule, HttpClientTestingModule ],
+      imports: [ FormsModule, ReactiveFormsModule ],
       declarations: [
+        PasoUnoComponent
       ],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ],
       providers: [
-
+        SeccionLibStore,
+        ConsultaioQuery,
+        { provide: CambioModalidadService, useClass: MockCambioModalidadService }
       ]
     }).overrideComponent(PasoUnoComponent, {
 
@@ -27,14 +38,52 @@ describe('PasoUnoComponent', () => {
   });
 
 
-  it('debería ejecutar #constructor()', () => {
+  it('should run #constructor()', async () => {
     expect(component).toBeTruthy();
   });
 
-  it('debe ejecutar #seleccionaTab()', () => {
+  it('should run #ngOnInit()', async () => {
+    component.consultaQuery = component.consultaQuery || {};
+    component.consultaQuery.selectConsultaioState$ = observableOf({});
+    component.guardarDatosFormulario = jest.fn();
+    component.asignarSecciones = jest.fn();
+    component.ngOnInit();
+    
+    expect(component.asignarSecciones).toHaveBeenCalled();
+  });
 
-    component.seleccionaTab(1);
+  it('should run #guardarDatosFormulario()', async () => {
+    component.modalidadService = component.modalidadService || {};
+    component.modalidadService.getDatosDeLaSolicitudData = jest.fn().mockReturnValue(observableOf({}));
+    component.modalidadService.actualizarEstadoFormulario = jest.fn();
+    component.guardarDatosFormulario();
+    expect(component.modalidadService.getDatosDeLaSolicitudData).toHaveBeenCalled();
+    expect(component.modalidadService.actualizarEstadoFormulario).toHaveBeenCalled();
+  });
 
+  it('should run #seleccionaTab()', async () => {
+    component.tabChanged = component.tabChanged || {};
+    component.tabChanged.emit = jest.fn();
+    component.seleccionaTab({});
+    expect(component.tabChanged.emit).toHaveBeenCalled();
+  });
+
+  it('should run #asignarSecciones()', async () => {
+    component.seccionStore = component.seccionStore || {};
+    component.seccionStore.establecerSeccion = jest.fn();
+    component.seccionStore.establecerFormaValida = jest.fn();
+    component.asignarSecciones();
+    expect(component.seccionStore.establecerSeccion).toHaveBeenCalled();
+    expect(component.seccionStore.establecerFormaValida).toHaveBeenCalled();
+  });
+
+  it('should run #ngOnDestroy()', async () => {
+    component.destroyNotifier$ = component.destroyNotifier$ || {};
+    component.destroyNotifier$.next = jest.fn();
+    component.destroyNotifier$.complete = jest.fn();
+    component.ngOnDestroy();
+    expect(component.destroyNotifier$.next).toHaveBeenCalled();
+    expect(component.destroyNotifier$.complete).toHaveBeenCalled();
   });
 
 });
