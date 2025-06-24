41cd025af2aa480bd7be481d605d639f58512ddb
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-06-24 18:59:20 +0530

    120601 proc issue fixes

--- apps/se/src/app/application/tramites/120601/component/datos-empresa/datos-empresa.component.spec.ts
+++ apps/se/src/app/application/tramites/120601/component/datos-empresa/datos-empresa.component.spec.ts
@@ -1,12 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
-
 import { CargarArchivosComponent } from '../cargar-archivos/cargar-archivos.component';
 import { DatosDeLaSolicitudComponent } from '../datos-de-la-solicitud/datos-de-la-solicitud.component';
 import { DatosEmpresaComponent } from './datos-empresa.component';
 import { DatosGeneralesSociosComponent } from '../datos-generales-socios/datos-generales-socios.component';
 import { DomicilioComponent } from '../domicilio/domicilio.component';
 import { RepresentacionFederalComponent } from '../representacion-federal/representacion-federal.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DatosEmpresaComponent', () => {
   let component: DatosEmpresaComponent;
@@ -20,9 +20,11 @@ describe('DatosEmpresaComponent', () => {
         DatosDeLaSolicitudComponent,
         DomicilioComponent,
         DatosGeneralesSociosComponent,
-        CargarArchivosComponent
+        CargarArchivosComponent,
+        DatosEmpresaComponent,
+        HttpClientTestingModule
       ],
-      declarations: [DatosEmpresaComponent]
+      declarations: []
     }).compileComponents();
   });
 
