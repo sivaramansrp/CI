92872db3f4999429ad84e61143a412d83b4dfd3d
varshakatre15 <varsha.katre@ilink-systems.com>
2025-06-24 10:03:15 +0530

    test cases

--- apps/cofepris/src/app/application/tramites/260604/pages/pantallas/pantallas.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/260604/pages/pantallas/pantallas.component.spec.ts
@@ -1,13 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PantallasComponent } from './pantallas.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-describe('PantallasComponent', () => {
+describe('SolicitudeComponent', () => {
   let component: PantallasComponent;
   let fixture: ComponentFixture<PantallasComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PantallasComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PantallasComponent);
@@ -17,5 +19,121 @@ describe('PantallasComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call a public method if present', () => {
+    if (typeof (component as any)['someMethod'] === 'function') {
+      const spy = jest.spyOn(component as any, 'someMethod');
+      (component as any).someMethod();
+      expect(spy).toHaveBeenCalled();
+    }
+  });
+
+  it('should have default property values', () => {
+    if ('someProperty' in component) {
+      expect((component as any).someProperty).toBeDefined();
+    }
+  });
+
+  it('should handle button click', () => {
+    if (typeof (component as any)['onButtonClick'] === 'function') {
+      const spy = jest.spyOn(component as any, 'onButtonClick');
+      const button = fixture.nativeElement.querySelector('button');
+      if (button) {
+        button.click();
+        expect(spy).toHaveBeenCalled();
+      }
+    }
+  });
+
+  it('should emit output event', () => {
+    if ((component as any).someEvent && (component as any).someEvent.emit) {
+      const spy = jest.spyOn((component as any).someEvent, 'emit');
+      if (typeof (component as any)['triggerEvent'] === 'function') {
+        (component as any).triggerEvent();
+        expect(spy).toHaveBeenCalled();
+      }
+    }
+  });
+
+  describe('getValorIndice', () => {
+    beforeEach(() => {
+      (component as any).pantallasPasos = [1, 2, 3];
+      (component as any).datosPasos = { indice: 0 };
+      (component as any).wizardComponent = {
+        siguiente: jest.fn(),
+        atras: jest.fn()
+      };
+    });
+
+    it('should set indice and call siguiente when accion is "cont"', () => {
+      const accion = { valor: 2, accion: 'cont' };
+      (component as any).getValorIndice(accion);
+      expect((component as any).indice).toBe(2);
+      expect((component as any).datosPasos.indice).toBe(2);
+      expect((component as any).wizardComponent.siguiente).toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
+
+    it('should set indice and call atras when accion is not "cont"', () => {
+      const accion = { valor: 1, accion: 'back' };
+      (component as any).getValorIndice(accion);
+      expect((component as any).indice).toBe(1);
+      expect((component as any).datosPasos.indice).toBe(1);
+      expect((component as any).wizardComponent.atras).toHaveBeenCalled();
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+    });
+
+    it('should do nothing if valor is not > 0', () => {
+      const accion = { valor: 0, accion: 'cont' };
+      (component as any).indice = 5;
+      (component as any).datosPasos.indice = 5;
+      (component as any).getValorIndice(accion);
+      expect((component as any).indice).toBe(5);
+      expect((component as any).datosPasos.indice).toBe(5);
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
+
+    it('should do nothing if valor is greater than pantallasPasos.length', () => {
+      const accion = { valor: 10, accion: 'cont' };
+      (component as any).indice = 3;
+      (component as any).datosPasos.indice = 3;
+      (component as any).getValorIndice(accion);
+      expect((component as any).indice).toBe(3);
+      expect((component as any).datosPasos.indice).toBe(3);
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
+
+    it('should do nothing if e is null', () => {
+      (component as any).indice = 1;
+      (component as any).datosPasos.indice = 1;
+      (component as any).getValorIndice(null);
+      expect((component as any).indice).toBe(1);
+      expect((component as any).datosPasos.indice).toBe(1);
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
+
+    it('should do nothing if e is undefined', () => {
+      (component as any).indice = 2;
+      (component as any).datosPasos.indice = 2;
+      (component as any).getValorIndice(undefined);
+      expect((component as any).indice).toBe(2);
+      expect((component as any).datosPasos.indice).toBe(2);
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
+
+    it('should do nothing if e.valor is undefined', () => {
+      (component as any).indice = 2;
+      (component as any).datosPasos.indice = 2;
+      (component as any).getValorIndice({ accion: 'cont' });
+      expect((component as any).indice).toBe(2);
+      expect((component as any).datosPasos.indice).toBe(2);
+      expect((component as any).wizardComponent.siguiente).not.toHaveBeenCalled();
+      expect((component as any).wizardComponent.atras).not.toHaveBeenCalled();
+    });
   });
 });
--- apps/cofepris/src/app/application/tramites/260604/services/exportacion.service.spec.ts
+++ apps/cofepris/src/app/application/tramites/260604/services/exportacion.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { of, throwError } from 'rxjs';
 
 import { ExportacionService } from './exportacion.service';
+import { PermisoModel } from '@libs/shared/data-access-user/src/core/models/260604/aviso-exportacion.model';
 
 describe('ExportacionService', () => {
   let service: ExportacionService;
+  let httpClientMock: jest.Mocked<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientMock = {
+      get: jest.fn()
+    } as any;
+
+    TestBed.configureTestingModule({
+      providers: [
+        ExportacionService,
+        { provide: HttpClient, useValue: httpClientMock }
+      ]
+    });
     service = TestBed.inject(ExportacionService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getLocalidaddata', () => {
+    it('should return data from the http get', (done) => {
+      const mockData = { foo: 'bar' };
+      httpClientMock.get.mockReturnValue(of(mockData));
+      service.getLocalidaddata().subscribe(data => {
+        expect(data).toEqual(mockData);
+        done();
+      });
+    });
+
+    it('should handle error and call throwError', (done) => {
+      const error = new Error('Test error');
+      httpClientMock.get.mockReturnValue(throwError(() => error));
+      service.getLocalidaddata().subscribe({
+        error: (err) => {
+          expect(err).toBe(error);
+          done();
+        }
+      });
+    });
+  });
+
+  describe('getTable', () => {
+    it('should return PermisoModel[] from http get', (done) => {
+      const mockTable: PermisoModel[] = [{ id: 1 } as unknown as PermisoModel];
+      httpClientMock.get.mockReturnValue(of(mockTable));
+      service.getTable().subscribe(data => {
+        expect(data).toEqual(mockTable);
+        done();
+      });
+    });
   });
 });
