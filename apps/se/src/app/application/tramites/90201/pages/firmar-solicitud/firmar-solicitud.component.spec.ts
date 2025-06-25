2b98e4296dba7407da98245eaf8896ef7f6d29e6
Vishal Rokade <vishal.rokade@ilink-systems.com>
2025-06-25 15:18:37 +0530

    fix

--- apps/se/src/app/application/tramites/90201/pages/firmar-solicitud/firmar-solicitud.component.spec.ts
+++ apps/se/src/app/application/tramites/90201/pages/firmar-solicitud/firmar-solicitud.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FirmarSolicitudComponent } from './firmar-solicitud.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('FirmarSolicitudComponent', () => {
   let component: FirmarSolicitudComponent;
@@ -7,7 +8,8 @@ describe('FirmarSolicitudComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FirmarSolicitudComponent],
+      declarations: [FirmarSolicitudComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], // No schemas needed for this component
     }).compileComponents();
 
     fixture = TestBed.createComponent(FirmarSolicitudComponent);
