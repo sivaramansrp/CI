1649150a58d0255a583156a3f773b64923a92a7f
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-26 21:03:52 +0530

    update

--- apps/aga/src/app/application/tramites/11201/pages/paso-uno/paso-uno.component.spec.ts
+++ apps/aga/src/app/application/tramites/11201/pages/paso-uno/paso-uno.component.spec.ts
@@ -85,12 +85,6 @@ describe('PasoUnoComponent', () => {
   it('should run #seleccionaTab()', async () => {
     component.seleccionaTab({});
   });
-  it('should call fetchGetDatosConsulta if consultaDatos.update is true', () => {
-    consultaioQueryMock.selectConsultaioState$ = of({ update: true });
-    const spy = jest.spyOn(component, 'fetchGetDatosConsulta');
-    component.ngOnInit();
-    expect(spy).toHaveBeenCalled();
-  });
 
   it('should emit continuarEvento when continuar is called', () => {
     const spy = jest.spyOn(component.continuarEvento, 'emit');
@@ -111,24 +105,6 @@ describe('PasoUnoComponent', () => {
     expect(cancelarSpy).toHaveBeenCalled();
     expect(obtenerTipoPersonaSpy).toHaveBeenCalled();
   });
-
-  // it('should fetch datosConsulta and update tramite11201Store when fetchGetDatosConsulta is called', () => {
-  //   component.fetchGetDatosConsulta();
-  //   tick();
-  //   expect(datosTramiteServiceMock.getDatosConsulta).toHaveBeenCalled();
-  //   expect(tramite11201StoreMock.setTipoBusqueda).toHaveBeenCalledWith('Busqueda 1');
-  //   expect(tramite11201StoreMock.setAduana).toHaveBeenCalledWith('Aduana 1');
-  //   expect(tramite11201StoreMock.setFechaIngreso).toHaveBeenCalledWith('2023-01-01');
-  //   expect(tramite11201StoreMock.setInicialesContenedor).toHaveBeenCalledWith('ABC');
-  //   expect(tramite11201StoreMock.setNumeroContenedor).toHaveBeenCalledWith('12345');
-  //   expect(tramite11201StoreMock.setDigitoDeControl).toHaveBeenCalledWith('1');
-  //   expect(tramite11201StoreMock.setContenedores).toHaveBeenCalledWith([]);
-  //   expect(tramite11201StoreMock.setFechaDeIngreso).toHaveBeenCalledWith('2023-01-01');
-  //   expect(tramite11201StoreMock.setAduanaMenuDesplegable).toHaveBeenCalledWith([]);
-  //   expect(tramite11201StoreMock.setMenuDesplegable).toHaveBeenCalledWith([]);
-  //   expect(tramite11201StoreMock.setNumeroManifiesta).toHaveBeenCalledWith('123');
-  //   expect(tramite11201StoreMock.setDelContenedor).toHaveBeenCalledWith({});
-  // });
 
   it('should clean up subscriptions when ngOnDestroy is called', () => {
     const destroySpy = jest.spyOn(component.destroyNotifier$, 'next');
