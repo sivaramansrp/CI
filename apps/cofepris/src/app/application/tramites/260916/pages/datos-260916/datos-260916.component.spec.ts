e202ec0edcc82ddd4baecbca1960a02d5af86634
navin-ilink <navin.kumar@ilink-system.com>
2025-06-27 15:05:09 +0530

    datos-260616

--- apps/cofepris/src/app/application/tramites/260916/pages/datos-260916/datos-260916.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/260916/pages/datos-260916/datos-260916.component.spec.ts
@@ -1,33 +1,88 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Datos260916Component } from './datos-260916.component';
-describe('Datos260916Component', () => {
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
+
+import { of, Subject } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+// Mock para TIPO_PERSONA
+const TIPO_PERSONA = { MORAL_NACIONAL: 'MORAL_NACIONAL' };
+
+describe('PasoUnoComponent', () => {
   let component: Datos260916Component;
   let fixture: ComponentFixture<Datos260916Component>;
+  let consultaQueryMock: any;
+  let servicioMock: any;
 
   beforeEach(async () => {
+    consultaQueryMock = {
+      selectConsultaioState$: of({ update: false })
+    };
+    servicioMock = {
+      getRegistroTomaMuestrasMercanciasData: jest.fn().mockReturnValue(of({ registro: true })),
+      getPagoDerechos: jest.fn().mockReturnValue(of({ permiso: true })),
+      actualizarEstadoFormulario: jest.fn(),
+      actualizarPagoDerechosFormulario: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       declarations: [Datos260916Component],
+      imports: [ require('@angular/common/http/testing').HttpClientTestingModule],
+      providers: [
+        { provide: ConsultaioQuery, useValue: consultaQueryMock },
+        { provide: Datos260916Component, useValue: servicioMock }
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Datos260916Component);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Mock global para TIPO_PERSONA si es necesario
+    (globalThis as any).TIPO_PERSONA = TIPO_PERSONA;
   });
 
-  it('should create the component', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('debería crear el componente', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have "indice" initialized to 1', () => {
-    expect(component.indice).toBe(1);
+  it('debería inicializar y suscribirse a selectConsultaioState$ con update=false', () => {
+    component.consultaState = undefined as any;
+    component.esDatosRespuesta = false;
+    consultaQueryMock.selectConsultaioState$ = of({ update: false });
+    component.ngOnInit();
+    expect(component.consultaState).toEqual({ update: false });
+    expect(component.esDatosRespuesta).toBe(true);
   });
 
-  it('should change "indice" when seleccionaTab is called', () => {
-    component.seleccionaTab(2);
-    expect(component.indice).toBe(2);
-
-    component.seleccionaTab(5);
-    expect(component.indice).toBe(5);
+  it('debería llamar guardarDatosFormulario si update=true', () => {
+    const spy = jest.spyOn(component, 'guardarDatosFormulario').mockImplementation();
+    consultaQueryMock.selectConsultaioState$ = of({ update: true });
+    component.ngOnInit();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
   });
+
+
+  it('guardarDatosFormulario no debe actualizar si no hay datos', (done) => {
+    servicioMock.getRegistroTomaMuestrasMercanciasData.mockReturnValue(of(null));
+    servicioMock.getPagoDerechos.mockReturnValue(of(null));
+    const actualizarEstadoSpy = jest.spyOn(servicioMock, 'actualizarEstadoFormulario');
+    const actualizarPagoSpy = jest.spyOn(servicioMock, 'actualizarPagoDerechosFormulario');
+    component.guardarDatosFormulario();
+    setTimeout(() => {
+      expect(actualizarEstadoSpy).not.toHaveBeenCalled();
+      expect(actualizarPagoSpy).not.toHaveBeenCalled();
+      done();
+    }, 0);
+  });
+
+  it('debería cambiar el índice con seleccionaTab', () => {
+    component.indice = 1;
+    component.seleccionaTab(3);
+    expect(component.indice).toBe(3);
+  });
+
 });
