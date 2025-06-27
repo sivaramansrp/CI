dcf2fdd346c8088244fb7920833277783a26da2a
atripathi860 <amit.tripathi@ilink-systems.com>
2025-06-27 15:27:07 +0530

    modal - spanish updated

--- apps/cofepris/src/app/application/tramites/260211/components/model/modal.component.spec.ts
+++ apps/cofepris/src/app/application/tramites/260211/components/model/modal.component.spec.ts
@@ -17,7 +17,7 @@ describe('ModalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debe crear el componente', () => {
     expect(component).toBeTruthy();
   });
 });
