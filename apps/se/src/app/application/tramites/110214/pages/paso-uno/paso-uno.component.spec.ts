49a366bb68d0b25ec02330644bfdf1682d6a4721
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-27 14:54:39 +0530

    update unit test case

--- apps/se/src/app/application/tramites/110214/pages/paso-uno/paso-uno.component.spec.ts
+++ apps/se/src/app/application/tramites/110214/pages/paso-uno/paso-uno.component.spec.ts
@@ -6,13 +6,77 @@ import { HistoricoProductoresComponent } from '../../components/historico-produc
 import { DestinatarioComponent } from '../../components/destinatario/destinatario.component';
 import { DatosCertificadoComponent } from '../../components/datos-certificado/datos-certificado.component';
 import { provideHttpClient } from '@angular/common/http';
+import { ValidarInicialmenteCertificadoService } from '../../services/validar-inicialmente-certificado.service';
+import { Tramite110214Store } from '../../../../estados/tramites/tramite110214.store';
+import { of } from 'rxjs';
+import { Tramite110214Query } from '../../../../estados/queries/tramite110214.query';
 
 
 describe('PasoUnoComponent', () => {
   let component: PasoUnoComponent;
   let fixture: ComponentFixture<PasoUnoComponent>;
+  let validarInicialmenteCertificadoServiceMock: any;
+  let tramiteStoreMock: any;
+  let tramiteQueryMock: any;
+
 
   beforeEach(async () => {
+    validarInicialmenteCertificadoServiceMock = {
+      getDatosConsulta: jest.fn().mockReturnValue(
+        of({
+          success: true,
+          datos: {
+            observaciones: 'Test Observaciones',
+            idioma: 'Español',
+            entidadFederativa: 'Entidad Test',
+            representacionFederal: 'Representación Test',
+            grupoReceptor: { nombre: 'Receptor Test' },
+            grupoDeDirecciones: { direccion: 'Dirección Test' },
+            grupoRepresentativo: { representante: 'Representante Test' },
+            tercerOperador: true,
+            blnPeriodo: false,
+            grupoTratado: { tratado: 'Tratado Test' },
+            mercanciaSeleccionadasTablaDatos: [{ id: 1, descripcion: 'Mercancia Seleccionada' }],
+            mercanciaDisponsiblesTablaDatos: [{ id: 2, descripcion: 'Mercancia Disponible' }],
+            datosConfidencialesProductor: { confidencial: true },
+            productorMismoExportador: true,
+            productoresExportador: [{ id: 3, nombre: 'Productor Exportador' }],
+            historicoMercanciaSeleccionadasTablaDatos: [{ id: 4, descripcion: 'Historico Mercancia' }],
+          },
+        })
+      ),
+      obtenerIdioma: jest.fn().mockReturnValue(of({ datos: [{ id: 1, descripcion: 'Español' }] })),
+      obtenerEntidadFederativa: jest.fn().mockReturnValue(of({ datos: [{ id: 1, descripcion: 'Entidad 1' }] })),
+      obtenerRepresentacionFederal: jest.fn().mockReturnValue(of({ datos: [{ id: 1, descripcion: 'Representación 1' }] }))
+    };
+
+    tramiteStoreMock = {
+      setObservaciones: jest.fn(),
+      setPestanaActiva: jest.fn(),
+      setIdioma: jest.fn(),
+      setEntidadFederativa: jest.fn(),
+      setRepresentacionFederal: jest.fn(),
+      setGrupoReceptor: jest.fn(),
+      setGrupoDeDirecciones: jest.fn(),
+      setGrupoRepresentativo: jest.fn(),
+      setTercerOperador: jest.fn(),
+      setPeriodo: jest.fn(),
+      setGrupoTratado: jest.fn(),
+      setMercanciaTablaDatos: jest.fn(),
+      setMercanciaDisponsiblesTablaDatos: jest.fn(),
+      setDatosConfidencialesProductor: jest.fn(),
+      setProductorMismoExportador: jest.fn(),
+      setProductoresExportador: jest.fn(),
+      setHistoricoMercanciaSeleccionadasTablaDatos: jest.fn(),
+    };
+    tramiteQueryMock = {
+      selectSolicitud$: of({
+        observaciones: 'Observaciones de prueba',
+        idioma: 1,
+        entidadFederativa: 1,
+        representacionFederal: 1
+      }),
+    };
     await TestBed.configureTestingModule({
       imports: [
         CommonModule,
@@ -22,7 +86,11 @@ describe('PasoUnoComponent', () => {
         DatosCertificadoComponent,
         PasoUnoComponent
       ],
-      providers: [provideHttpClient()]
+      providers: [provideHttpClient(),
+      { provide: ValidarInicialmenteCertificadoService, useValue: validarInicialmenteCertificadoServiceMock },
+      { provide: Tramite110214Store, useValue: tramiteStoreMock },
+      { provide: Tramite110214Query, useValue: tramiteQueryMock },
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasoUnoComponent);
@@ -70,5 +138,27 @@ describe('PasoUnoComponent', () => {
     fixture.detectChanges();
     const datosCertificadoElement = fixture.debugElement.nativeElement.querySelector('app-datos-certificado');
     expect(datosCertificadoElement).toBeTruthy();
+  });
+
+  it('should fetch data and update the store when fetchGetDatosConsulta is called', () => {
+    component.fetchGetDatosConsulta();
+    expect(validarInicialmenteCertificadoServiceMock.getDatosConsulta).toHaveBeenCalled();
+    expect(tramiteStoreMock.setObservaciones).toHaveBeenCalledWith('Test Observaciones');
+    expect(tramiteStoreMock.setIdioma).toHaveBeenCalledWith('Español');
+    expect(tramiteStoreMock.setEntidadFederativa).toHaveBeenCalledWith('Entidad Test');
+    expect(tramiteStoreMock.setRepresentacionFederal).toHaveBeenCalledWith('Representación Test');
+    expect(tramiteStoreMock.setGrupoReceptor).toHaveBeenCalledWith({ nombre: 'Receptor Test' });
+    expect(tramiteStoreMock.setGrupoDeDirecciones).toHaveBeenCalledWith({ direccion: 'Dirección Test' });
+    expect(tramiteStoreMock.setGrupoRepresentativo).toHaveBeenCalledWith({ representante: 'Representante Test' });
+    expect(tramiteStoreMock.setTercerOperador).toHaveBeenCalledWith(true);
+    expect(tramiteStoreMock.setPeriodo).toHaveBeenCalledWith(false);
+    expect(tramiteStoreMock.setGrupoTratado).toHaveBeenCalledWith({ tratado: 'Tratado Test' });
+    expect(tramiteStoreMock.setMercanciaTablaDatos).toHaveBeenCalledWith([{ id: 1, descripcion: 'Mercancia Seleccionada' }]);
+    expect(tramiteStoreMock.setMercanciaDisponsiblesTablaDatos).toHaveBeenCalledWith([{ id: 2, descripcion: 'Mercancia Disponible' }]);
+    expect(tramiteStoreMock.setDatosConfidencialesProductor).toHaveBeenCalledWith({ confidencial: true });
+    expect(tramiteStoreMock.setProductorMismoExportador).toHaveBeenCalledWith(true);
+    expect(tramiteStoreMock.setProductoresExportador).toHaveBeenCalledWith([{ id: 3, nombre: 'Productor Exportador' }]);
+    expect(tramiteStoreMock.setHistoricoMercanciaSeleccionadasTablaDatos).toHaveBeenCalledWith([{ id: 4, descripcion: 'Historico Mercancia' }]);
+    expect(component.esDatosRespuesta).toBe(true);
   });
 });
