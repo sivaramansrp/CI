c62f6bd36744014b08ed7d6d86b4e674a7fef976
Dayniz Yael Velasco Coronel <dyvelascovc@desarrollo-ultrasist.com.mx>
2025-06-27 15:38:11 -0600

    Incidnencia 7851

--- apps/aga/src/app/application/core/enums/5701/mensajes-modal-5701.enum.ts
+++ apps/aga/src/app/application/core/enums/5701/mensajes-modal-5701.enum.ts
@@ -31,10 +31,10 @@ export const MSJ_ERROR_FECHA_DIA =
  * @description Mensaje de error cuando la fecha final es menor que la fecha inicial.
  */
 export const MSJ_ERROR_HORA_FINAL_MENOR_INICIAL =
-  'Hora inválida. La hora final no puede ser menor a la hora de incio.';
+  'Hora inválida. La hora final no puede ser menor o igual a la de incio.';
 
 export const MSJ_FECHA_DENTRO_DE_HORARIO_ADUANA =
-  'El horario que seleccionaste para llevar a cabo tu servicio extraordinario, se encuentra dentro del horario de operación de la aduana. Por favor verifica.';
+  'El horario que seleccionaste para llevar a cabo tu servicio extraordinario, se encuentra dentro del horario de operación de la aduana. Por favor verifica.';
 
 // #Mensajes del modal para el componente Persona Responsable del Despacho
 /**
