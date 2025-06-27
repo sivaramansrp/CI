bf03068e80a7d58767697388f08236238f812419
mjimenezm <mjimenezm@desarrollo-ultrasist.com.mx>
2025-06-27 13:42:04 -0600

    test abaut components in funcionario

--- apps/funcionario/src/app/application/components/autorizacion-masiva/autorizacion-masiva.component.spec.ts
+++ apps/funcionario/src/app/application/components/autorizacion-masiva/autorizacion-masiva.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AutorizacionMasivaComponent } from './autorizacion-masiva.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AutorizacionMasivaComponent', () => {
   let component: AutorizacionMasivaComponent;
@@ -8,6 +9,7 @@ describe('AutorizacionMasivaComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AutorizacionMasivaComponent],
+      providers: [provideHttpClient()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AutorizacionMasivaComponent);
