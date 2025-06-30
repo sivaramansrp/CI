8c3b2e287805c7ee8fb2a117ef79931706caba56
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-06-30 16:36:31 +0530

    Fix component name typo in test setup for OperacionesDeComercioExteriorComponent

--- apps/aga/src/app/application/tramites/319/components/operaciones-de-comercio-exterior/operaciones-de-comercio-exterior.component.spec.ts
+++ apps/aga/src/app/application/tramites/319/components/operaciones-de-comercio-exterior/operaciones-de-comercio-exterior.component.spec.ts
@@ -1,22 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { OperacionesDeComercioExteriorComponent } from './operaciones-de-comercio-exterior.component';
 import { CommonModule } from '@angular/common';
 import { CatalogoSelectComponent, SharedModule, TablaDinamicaComponent, TituloComponent } from '@ng-mf/data-access-user';  // Import shared modules if needed
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AlertComponent } from 'ngx-bootstrap/alert';
 import { ReactiveFormsModule } from '@angular/forms';
+import { OperacionesDeComercioExterioComponent } from './operaciones-de-comercio-exterior.component';
 
 describe('OperacionesDeComercioExteriorComponent', () => {
-  let component: OperacionesDeComercioExteriorComponent;
-  let fixture: ComponentFixture<OperacionesDeComercioExteriorComponent>;
+  let component: OperacionesDeComercioExterioComponent;
+  let fixture: ComponentFixture<OperacionesDeComercioExterioComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CommonModule, SharedModule,HttpClientTestingModule,TablaDinamicaComponent,CatalogoSelectComponent,AlertComponent,TituloComponent,ReactiveFormsModule,OperacionesDeComercioExteriorComponent],  // Import necessary modules here
+      imports: [CommonModule, SharedModule,HttpClientTestingModule,TablaDinamicaComponent,CatalogoSelectComponent,AlertComponent,TituloComponent,ReactiveFormsModule,OperacionesDeComercioExterioComponent],  
       declarations: [],  // Declare the component in the declarations array
     }).compileComponents();
 
-    fixture = TestBed.createComponent(OperacionesDeComercioExteriorComponent);
+    fixture = TestBed.createComponent(OperacionesDeComercioExterioComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
