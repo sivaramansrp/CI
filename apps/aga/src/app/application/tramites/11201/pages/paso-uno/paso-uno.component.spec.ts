0f3c4635713487e3a4120e95fd97100aa64392ad
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-06-30 23:17:42 +0530

    update unit test case

--- apps/aga/src/app/application/tramites/11201/pages/paso-uno/paso-uno.component.spec.ts
+++ apps/aga/src/app/application/tramites/11201/pages/paso-uno/paso-uno.component.spec.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PasoUnoComponent } from './paso-uno.component';
@@ -7,8 +6,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { of, Subject } from 'rxjs';
 import { DatosTramiteService } from '../../services/datos-tramite.service';
 import { Tramite11201Store } from '../../../../core/estados/tramites/tramite11201.store';
-import { ConsultaioQuery } from '@ng-mf/data-access-user';
 import { fakeAsync, tick } from '@angular/core/testing';
+import { TIPO_PERSONA } from '@libs/shared/data-access-user/src';
 
 
 
@@ -17,27 +16,46 @@ describe('PasoUnoComponent', () => {
   let fixture: ComponentFixture<PasoUnoComponent>;
   let datosTramiteServiceMock: any;
   let tramite11201StoreMock: any;
-  let consultaioQueryMock: any;
 
   beforeEach(() => {
     datosTramiteServiceMock = {
-      getDatosConsulta: jest.fn().mockReturnValue(of({
-        success: true,
-        datos: {
-          tipoBusqueda: 'Busqueda 1',
-          aduana: 'Aduana 1',
-          fechaIngreso: '2023-01-01',
-          inicialesContenedor: 'ABC',
-          numeroContenedor: '12345',
-          digitoDeControl: '1',
-          contenedores: [],
-          fechaDeIngreso: '2023-01-01',
-          aduanaMenuDesplegable: [],
-          menuDesplegable: [],
-          numeroManifiesta: '123',
-          datosDelContenedor: {},
-        },
-      })),
+      getDatosConsulta: jest.fn().mockReturnValue(
+        of({
+          "success": true,
+          "message": "",
+          "datos": {
+            "tipoBusqueda": "Contenedor",
+            "aduana": "0",
+            "fechaIngreso": "15/10/2025",
+            "inicialesContenedor": "equipo",
+            "numeroContenedor": "equipo",
+            "digitoDeControl": "1",
+            "contenedores": "C11223",
+            "aduanaMenuDesplegable": "0",
+            "fechaDeIngreso": "15/10/2025",
+            "menuDesplegable": "0",
+            "numeroManifiesta": "1",
+            "datosDelContenedor": [
+              {
+                "id": 1,
+                "inicialesEquipo": "BBZM",
+                "numeroEquipo": 1098765,
+                "digitoVerificador": 4,
+                "tipoEquipo": "AC",
+                "aduana": 430,
+                "fechaIngreso": "2024-03-13",
+                "vigencia": "2025-03-13",
+                "estadoConstancia": "Válido",
+                "existeEnVUCEM": "Sí",
+                "idConstancia": "CONST12345",
+                "numeroManifiesto": "MANI67890",
+                "idSolicitud": "SOLICITUD001",
+                "fechaInicio": "2024-03-01"
+              }
+            ]
+          }
+        })
+      )
     };
 
     tramite11201StoreMock = {
@@ -55,23 +73,19 @@ describe('PasoUnoComponent', () => {
       setDelContenedor: jest.fn(),
     };
 
-    consultaioQueryMock = {
-      selectConsultaioState$: of({ update: true }),
-    };
-
     TestBed.configureTestingModule({
       imports: [FormsModule, ReactiveFormsModule, PasoUnoComponent],
       providers: [
         provideHttpClient(), provideHttpClientTesting(),
         { provide: DatosTramiteService, useValue: datosTramiteServiceMock },
         { provide: Tramite11201Store, useValue: tramite11201StoreMock },
-        { provide: ConsultaioQuery, useValue: consultaioQueryMock },
       ]
     }).overrideComponent(PasoUnoComponent, {
 
     }).compileComponents();
     fixture = TestBed.createComponent(PasoUnoComponent);
     component = fixture.debugElement.componentInstance;
+    
   });
 
   it('should run #constructor()', async () => {
@@ -83,7 +97,7 @@ describe('PasoUnoComponent', () => {
   });
 
   it('should run #seleccionaTab()', async () => {
-    component.seleccionaTab({});
+    component.seleccionaTab(1);
   });
 
   it('should emit continuarEvento when continuar is called', () => {
@@ -113,5 +127,35 @@ describe('PasoUnoComponent', () => {
     expect(destroySpy).toHaveBeenCalledWith();
     expect(completeSpy).toHaveBeenCalled();
   });
+
+  it('should call obtenerTipoPersona on SolicitanteComponent with TIPO_PERSONA.MORAL_NACIONAL', (done) => {
+    component.solicitante = {
+      obtenerTipoPersona: jest.fn(),
+    } as any;
+    component.obtenerTipoPersona();
+    setTimeout(() => {
+      expect(component.solicitante.obtenerTipoPersona).toHaveBeenCalledWith(TIPO_PERSONA.MORAL_NACIONAL);
+      done();
+    }, 50);
+  });
+
+
+  it('should fetch data and update the store when fetchGetDatosConsulta is called', () => {
+    component.fetchGetDatosConsulta();
+    expect(datosTramiteServiceMock.getDatosConsulta).toHaveBeenCalled();
+    expect(tramite11201StoreMock.setTipoBusqueda).toHaveBeenCalled();
+    expect(tramite11201StoreMock.setAduana).toHaveBeenCalled();
+    expect(tramite11201StoreMock.setFechaIngreso).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setInicialesContenedor).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setNumeroContenedor).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setDigitoDeControl).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setContenedores).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setFechaDeIngreso).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setAduanaMenuDesplegable).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setMenuDesplegable).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setNumeroManifiesta).toHaveBeenCalled()
+    expect(tramite11201StoreMock.setDelContenedor).toHaveBeenCalled()
+  });
+
 
 });
--- apps/aga/src/app/application/tramites/11201/services/datos-tramite.service.spec.ts
+++ apps/aga/src/app/application/tramites/11201/services/datos-tramite.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { DatosTramiteService } from './datos-tramite.service';
-import { RespuestaApi, RespuestaAduanas, RespuestaConsulta } from '@libs/shared/data-access-user/src/core/models/11201/datos-tramite.model';
+import { RespuestaApi, RespuestaAduanas, RespuestaConsulta, RespuestaContenedor } from '@libs/shared/data-access-user/src/core/models/11201/datos-tramite.model';
 import { RespuestaCatalogos } from "@libs/shared/data-access-user/src";
 
 
@@ -136,13 +136,91 @@ describe('DatosTramiteService', () => {
       },
       message: ''
     };
-
     service.getDatosConsulta().subscribe((response) => {
       expect(response).toEqual(mockResponse);
     });
-
     const req = httpMock.expectOne('assets/json/11201/consulta_11201.json');
     expect(req.request.method).toBe('GET');
     req.flush(mockResponse);
+  });
+  it('should call agregarSolicitud and return RespuestaContenedor', () => {
+    const mockResponse: RespuestaContenedor = {
+      "success": true,
+      "message": "Solicitud enviada exitosamente",
+      "datos": {
+        "id": 1,
+        "inicialesEquipo": "BBZM",
+        "numeroEquipo": 1098765,
+        "digitoVerificador": 4,
+        "tipoEquipo": "AC",
+        "aduana": 430,
+        "fechaIngreso": "2024-03-13",
+        "vigencia": "2025-03-13",
+        "estadoConstancia": "Válido",
+        "existeEnVUCEM": "Sí",
+        "idConstancia": "CONST12345",
+        "numeroManifiesto": "MANI67890",
+        "idSolicitud": "SOLICITUD001",
+        "fechaInicio": "2024-03-01"
+      }
+    };
+    service.agregarSolicitud().subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne('assets/json/11201/contenedorLista.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse); // Simulate the HTTP response
+  });
+
+  it('should call getTransporteList and return RespuestaCatalogos', () => {
+    const mockResponse: RespuestaCatalogos = {
+      "code": 200,
+      "data": [
+        {
+          "id": 0,
+          "descripcion": "Ferroviario"
+        },
+        {
+          "id": 2,
+          "descripcion": "Marítimo"
+        }
+      ],
+      "message": "Seleccione un valor"
+    }
+    service.getTransporteList('transporte').subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+    const req = httpMock.expectOne('assets/json/11201/transporte.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse); // Simulate the HTTP response
+  });
+
+  it('should call getAduanaList and return RespuestaAduanas', () => {
+    const mockResponse: RespuestaAduanas = {
+      "code": 200,
+      "data": [
+        {
+          "id": 0,
+          "descripcion": "Aduana de México"
+        },
+        {
+          "id": 2,
+          "descripcion": "Aduana de Argentina"
+        },
+        {
+          "id": 2,
+          "descripcion": "Aduana de España"
+        }
+      ],
+      "message": "Seleccione un valor"
+    };
+
+    service.getAduanaList('aduana').subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('assets/json/11201/aduana.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse); // Simulate the HTTP response
   });
 });
