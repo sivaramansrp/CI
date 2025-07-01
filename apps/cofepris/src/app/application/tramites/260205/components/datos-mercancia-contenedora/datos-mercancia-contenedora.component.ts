59a6ce7199f394fc7ef3bd5dbfa8dcdcd639d327
muneez-ilink <muneeshwaran.n@ilink-systems.com>
2025-07-01 17:45:33 +0530

    commited code channges for 260205

--- apps/cofepris/src/app/application/tramites/260205/components/datos-mercancia-contenedora/datos-mercancia-contenedora.component.ts
+++ apps/cofepris/src/app/application/tramites/260205/components/datos-mercancia-contenedora/datos-mercancia-contenedora.component.ts
@@ -6,11 +6,28 @@ import { DatosMercanciaComponent } from '../../../../shared/components/datos-mer
 import { Subject } from 'rxjs';
 import { TablaMercanciasDatos } from '../../../../shared/models/datos-solicitud.model';
 import { Tramite260205Query } from '../../estados/queries/tramite260205.query';
+
+
 /**
  * @component DatosMercanciaContenedoraComponent
- * @description Componente encargado de gestionar y actualizar la información de una mercancía seleccionada.
- * Observa el estado del trámite y permite al usuario seleccionar y modificar datos de mercancías
- * en la tabla principal.
+ * @description Componente Angular que representa la sección de datos de mercancías contenedoras dentro de un trámite específico.
+ * Este componente es autónomo y utiliza módulos comunes y otros componentes relacionados para su funcionalidad.
+ *
+ * @selector app-datos-mercancia-contenedora
+ * Selector utilizado para instanciar este componente en una plantilla HTML.
+ *
+ * @standalone true
+ * Indica que este componente es independiente y puede ser utilizado sin necesidad de declararlo en un módulo.
+ *
+ * @imports
+ * - CommonModule: Módulo Angular que proporciona directivas comunes como `ngIf` y `ngFor`.
+ * - DatosMercanciaComponent: Componente relacionado que se utiliza dentro de este componente.
+ *
+ * @templateUrl ./datos-mercancia-contenedora.component.html
+ * Ruta del archivo HTML que define la estructura visual del componente.
+ *
+ * @styleUrl ./datos-mercancia-contenedora.component.scss
+ * Ruta del archivo SCSS que contiene los estilos específicos para este componente.
  */
 @Component({
   selector: 'app-datos-mercancia-contenedora',
