c9915d7e3455ca65a4c8ee046758343ab8a10af9
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-07-03 15:52:48 +0530

    refactor: clean up Agregardestinatario component code structure

--- apps/agricultura/src/app/application/shared/components/agregardestinatario/agregardestinatario.component.ts
+++ apps/agricultura/src/app/application/shared/components/agregardestinatario/agregardestinatario.component.ts
@@ -1,5 +1,6 @@
 import { Catalogo, CatalogoSelectComponent, InputRadioComponent } from '@libs/shared/data-access-user/src';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { OPCION_DE_BOTON_DE_RADIO } from '../../constantes/tercerosrelacionados.enum';
 import { RadioOpcion } from '../../../tramites/220201/models/220201/certificado-zoosanitario.model';
@@ -9,12 +10,13 @@ import { TituloComponent } from "../../../../../../../../libs/shared/data-access
   selector: 'app-agregardestinatario',
   standalone: true,
   imports: [CommonModule, TituloComponent,
-    InputRadioComponent,CatalogoSelectComponent
+    InputRadioComponent,CatalogoSelectComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './agregardestinatario.component.html',
   styleUrl: './agregardestinatario.component.scss',
 })
-export class AgregardestinatarioComponent {
+export class AgregardestinatarioComponent implements OnInit {
       /**
          * Indica si el formulario debe mostrarse en modo solo lectura.
          *
@@ -40,4 +42,27 @@ export class AgregardestinatarioComponent {
   estadoCatalog: Catalogo[] = [];
   municipioCatalog: Catalogo[] = [];
   coloniaCatalog:Catalogo[]=[];
+
+  destinatarioForm!: FormGroup;
+  constructor(public fb: FormBuilder){}
+  ngOnInit(): void {
+     this.destinatarioForm = this.fb.group({
+      tipoMercancia: ['yes', Validators.required],
+      nombre: ['', Validators.required],
+      primerApellido: ['', Validators.required],
+      segundoApellido: [''],
+      razonSocial: ['', Validators.required],
+      pais: ['', Validators.required],
+      codigoPostal: ['', Validators.required],
+      estado: ['', Validators.required],
+      municipio: [''],
+      colonia: [''],
+      calle: ['', Validators.required],
+      numeroExterior: ['', Validators.required],
+      numeroInterior: [''],
+      lada: [''],
+      telefono: [''],
+      correo: ['']
+    });
+  }
 }
