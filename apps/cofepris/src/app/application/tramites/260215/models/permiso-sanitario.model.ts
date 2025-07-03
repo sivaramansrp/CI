f3911a0892fde956ed4d1d45a8c25bb2dea4b63f
SubodhSingh11 <subodh.singh@ilink-systems.com>
2025-07-03 17:44:45 +0530

    4084 bugs fixes

--- apps/cofepris/src/app/application/tramites/260215/models/permiso-sanitario.model.ts
+++ apps/cofepris/src/app/application/tramites/260215/models/permiso-sanitario.model.ts
@@ -173,3 +173,19 @@ export interface SolicitudModel {
   cantidad: string;
   proveedor: string;
 }
+
+
+/**
+ * Interfaz que representa los datos de un representante.
+ * 
+ * @property {string} rfc - Registro Federal de Contribuyentes del representante.
+ * @property {string} nombre - Nombre del representante.
+ * @property {string} apellidoPaterno - Apellido paterno del representante.
+ * @property {string} apellidoMaterno - Apellido materno del representante.
+ */
+export interface ReprestantanteData {
+  rfc: string;
+  nombre: string;
+  apellidoPaterno: string;
+  apellidoMaterno: string;
+}
