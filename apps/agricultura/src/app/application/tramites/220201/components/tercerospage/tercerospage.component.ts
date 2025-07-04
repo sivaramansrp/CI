8826ee8741edd9469c94e4f38b19bd917fc71781
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-07-04 11:37:18 +0530

    feat: remove unused AfterViewInit import from TercerospageComponent

--- apps/agricultura/src/app/application/tramites/220201/components/tercerospage/tercerospage.component.ts
+++ apps/agricultura/src/app/application/tramites/220201/components/tercerospage/tercerospage.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
 import { CertificadoZoosanitarioServiceService } from '../../services/220201/certificado-zoosanitario.service';
 import { CommonModule } from '@angular/common';
