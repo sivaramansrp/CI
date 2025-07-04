9d38cb9a36b0eecfa5126899d5f1208acdfd811b
PiyusshSahare <piyush.sahare@ilink-systems.com>
2025-07-04 15:36:10 +0530

    resolved bugs for 220402 procedure under agricultura department

--- apps/agricultura/src/app/application/tramites/220402/pages/solicitud-page/solicitud-page.component.ts
+++ apps/agricultura/src/app/application/tramites/220402/pages/solicitud-page/solicitud-page.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { DatosPasos } from '@ng-mf/data-access-user';
 import { ListaPasosWizard } from '../../models/pantallas-captura.model';
+import { MENSAJE_DE_EXITO_ETAPA_UNO } from '../../constantes/certificado-zoosanitario.enum';
 import { PASOS } from '@ng-mf/data-access-user';
 import { SECCIONES_TRAMITE_220402 } from '@ng-mf/data-access-user';
 import { SeccionQuery } from '../../../../estados/queries/seccion.query';
 import { SeccionState } from '../../../../estados/seccion.store';
 import { SeccionStore } from '../../../../estados/seccion.store';
 import { Subject } from 'rxjs';
-import { WizardComponent } from '@ng-mf/data-access-user';
+import { WizardComponent } from '@libs/shared/data-access-user/src';
 import { map } from 'rxjs';
 import { takeUntil } from 'rxjs';
 
@@ -49,6 +50,12 @@ export class SolicitudPageComponent implements OnInit, OnDestroy {
    * @property {number} indice - Índice actual del paso.
    */
   indice: number = 1;
+
+  /**
+   * Objeto con los textos de los requisitos.
+   * @property {object} TEXTOS_REQUISITOS - Textos para los requisitos del certificado zoosanitario. --220201
+   */
+  TEXTOS = MENSAJE_DE_EXITO_ETAPA_UNO;
 
   /**
    * @property {SeccionState} seccion - Estado de la sección actual.
