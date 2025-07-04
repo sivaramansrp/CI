bd80ff371dfa2ce9a530ec76f49db73ecd01fac9
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-07-04 14:04:33 +0530

    test: add unit tests for AgregardestinatarioComponent initialization

--- apps/agricultura/src/app/application/tramites/220201/components/agregardestinatario/agregardestinatario.component.spec.ts
+++ apps/agricultura/src/app/application/tramites/220201/components/agregardestinatario/agregardestinatario.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgregardestinatarioComponent } from './agregardestinatario.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CatalogoSelectComponent, InputRadioComponent, TituloComponent } from '@libs/shared/data-access-user/src';
 
 describe('AgregardestinatarioComponent', () => {
   let component: AgregardestinatarioComponent;
@@ -7,7 +10,9 @@ describe('AgregardestinatarioComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AgregardestinatarioComponent],
+      imports: [AgregardestinatarioComponent,CommonModule, TituloComponent,
+          InputRadioComponent,CatalogoSelectComponent,
+          ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AgregardestinatarioComponent);
