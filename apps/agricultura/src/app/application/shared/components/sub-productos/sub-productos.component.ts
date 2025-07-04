dfdd4bf02d21faf73c6d78ded1fde92188e2d206
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-07-04 16:10:23 +0530

    refactor: remove unused imports and clean up sub-productos component

--- apps/agricultura/src/app/application/shared/components/sub-productos/sub-productos.component.ts
+++ apps/agricultura/src/app/application/shared/components/sub-productos/sub-productos.component.ts
@@ -1,4 +1,4 @@
-import { CatalogoSelectComponent, ConfiguracionColumna, InputRadioComponent, TablaDinamicaComponent, TituloComponent } from '@libs/shared/data-access-user/src';
+import { CatalogoSelectComponent, ConfiguracionColumna, InputRadioComponent, TablaDinamicaComponent, TablaSeleccion, TituloComponent } from '@libs/shared/data-access-user/src';
 import { CommonModule, Location } from '@angular/common';
 import { Component, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { DetallasDatos, ProductoDetallaEventos, ProductosCatalogosDatos } from '../../models/datos-de-la-solicitue.model';
@@ -11,7 +11,7 @@ import { Subject } from 'rxjs';
 @Component({
   selector: 'app-sub-productos',
   standalone: true,
-  imports: [CommonModule, CatalogoSelectComponent, TituloComponent, ReactiveFormsModule, TablaDinamicaComponent,InputRadioComponent],
+  imports: [CommonModule, CatalogoSelectComponent, TituloComponent, ReactiveFormsModule, TablaDinamicaComponent, InputRadioComponent],
   templateUrl: './sub-productos.component.html',
   styleUrl: './sub-productos.component.scss',
 })
@@ -29,6 +29,7 @@ public configuracionDetallasDatosTabla: ConfiguracionColumna<DetallasDatos>[] = 
 public detallasDatosTablaDatos: DetallasDatos[] = [];
 
 public detallasDatosTablaSeleccionada: DetallasDatos[] = [];
+public tablaSeleccion:TablaSeleccion= TablaSeleccion.CHECKBOX;
   /**
    * Representa el formulario reactivo utilizado para gestionar los detalles específicos
    * de los animales vivos, como número de lote, color de pelaje, edad, etc.
