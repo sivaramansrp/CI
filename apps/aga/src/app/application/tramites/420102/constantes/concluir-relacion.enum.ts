c132955eae196c9f35044f01ae1a1434aa7c9343
Raghul C <raghul.c@purpleslate.in>
2025-07-14 11:40:31 +0530

    bug-fix-for-420102-8538-MR

--- apps/aga/src/app/application/tramites/420102/constantes/concluir-relacion.enum.ts
+++ apps/aga/src/app/application/tramites/420102/constantes/concluir-relacion.enum.ts
@@ -76,6 +76,13 @@ export const FECHA_INGRESO = {
   required: false,
   habilitado: true,
 };
+/**
+ * Constante que representa la configuración para el campo "Fecha final".
+ * @const FECHA_FINAL
+ * @property {string} labelNombre - Etiqueta que se muestra para el campo.
+ * @property {boolean} required - Indica si el campo es obligatorio.
+ * @property {boolean} habilitado - Indica si el campo está habilitado.
+ */
 export const FECHA_FINAL = {
   labelNombre: 'Fecha final',
   required: false,
