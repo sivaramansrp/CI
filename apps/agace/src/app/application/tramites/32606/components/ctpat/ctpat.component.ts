c4105db2c5754b335e15860a8dc9b156fca331ea
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-07-16 11:38:33 +0530

    Ctpat tab UI done

--- apps/agace/src/app/application/tramites/32606/components/ctpat/ctpat.component.ts
+++ apps/agace/src/app/application/tramites/32606/components/ctpat/ctpat.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InputRadioComponent } from '@libs/shared/data-access-user/src';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RADIO_01 } from '../../constantes/adace32606.enum';
+import { EconomicoService } from '../../services/economico.service';
+import { Tramite32606Query } from '../../state/Tramite32606.query';
+import { Tramite32606Store } from '../../state/Tramite32606.store';
 
 @Component({
   selector: 'app-ctpat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule, InputRadioComponent],
   templateUrl: './ctpat.component.html',
   styleUrl: './ctpat.component.css',
 })
-export class CtpatComponent {}
+export class CtpatComponent implements OnDestroy , OnInit {
+  radioOpcions01 = RADIO_01;
+  public ctpatForm !: FormGroup;
+
+  constructor(private economico: EconomicoService,
+      public query: Tramite32606Query,
+      public store: Tramite32606Store,
+      private fb: FormBuilder) { }
+  
+  
+    ngOnInit(): void {
+      this.donanteDomicilio();
+    }
+  
+    donanteDomicilio(): void {
+      this.ctpatForm = this.fb.group({
+        tipoRadio05: [''],
+          
+      });
+    }
+  
+    ngOnDestroy(): void {
+    }
+  
+}
