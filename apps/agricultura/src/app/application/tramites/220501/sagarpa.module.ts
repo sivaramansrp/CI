2fcd4ff7128eb66f2f7f2c0efc699d4522cf03b0
Renuga <renugadevim@ilink-systems.com>
2025-07-17 18:40:04 +0530

    Fix for review comments

--- apps/agricultura/src/app/application/tramites/220501/sagarpa.module.ts
+++ apps/agricultura/src/app/application/tramites/220501/sagarpa.module.ts
@@ -1,22 +1,16 @@
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-import { AlertComponent, InputRadioComponent } from '@ng-mf/data-access-user';
+import { AlertComponent, AnexarDocumentosComponent, BtnContinuarComponent, FirmaElectronicaComponent, InputFechaComponent, InputRadioComponent, SolicitanteComponent, TableComponent, TituloComponent, WizardComponent } from '@ng-mf/data-access-user';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { AgregarMercanciaComponent } from './components/agregar-mercancia/agregar-mercancia.component';
-import { AnexarDocumentosComponent } from '@ng-mf/data-access-user'
-import { BtnContinuarComponent } from '@ng-mf/data-access-user';
 import { CarrosDeFerrocarrilComponent } from '../220502/shared/carros-de-ferrocarril/carros-de-ferrocarril.component';
 import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component';
+import { CommonModule } from '@angular/common';
 import { DatosDelTramiteARealizarComponent } from '../220502/shared/datos-del-tramite-a-realizar/datos-del-tramite-a-realizar.component';
 import { DatosGeneralesComponent } from './components/datos-generales/datos-generales.component';
-import { FirmaElectronicaComponent } from '@ng-mf/data-access-user';
 import { FrecuenciaInspeccionComponent } from './components/frecuencia-inspeccion/frecuencia-inspeccion.component';
 import { HistorialInspeccionFisicaComponent } from '../220502/shared/historial-inspeccion-fisica/historial-inspeccion-fisica.component';
-import { InputFechaComponent } from '@ng-mf/data-access-user';
 import { MedioTransporteComponent } from './components/medio-transporte/medio-transporte.component';
+import { NgModule } from '@angular/core';
 import { PagoDeDerechosComponent } from './components/pago-de-derechos/pago-de-derechos.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
@@ -24,17 +18,12 @@ import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { ResponsableInspeccionEnPuntoComponent } from '../220502/shared/responsable-inspeccion-en-punto/responsable-inspeccion-en-punto.component';
 import { RevisionDocumentalComponent } from './components/revision-documental/revision-documental.component';
 import { RevisionService } from './services/revision.service';
+import { RouterModule } from '@angular/router';
 import { SagarpaRoutingModule } from './sagarpa-routing.module';
-import { SolicitanteComponent } from '@ng-mf/data-access-user';
 import { SolicitudComponent } from './components/solicitud/solicitud.component';
 import { SolicitudDatosComponent } from '../220502/shared/solicitud-datos/solicitud-datos.component';
 import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.component';
-import { TableComponent } from '@ng-mf/data-access-user';
 import { TercerosRelacionadosComponent } from './components/terceros-relacionados/terceros-relacionados.component';
-import { TituloComponent } from '@ng-mf/data-access-user';
-import { ToastrModule } from 'ngx-toastr';
-import { ToastrService } from 'ngx-toastr';
-import { WizardComponent } from '@ng-mf/data-access-user';
 @NgModule({
   declarations: [
     SolicitudPageComponent,
