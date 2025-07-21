c58b76d1808585ee1a3e90ce8c7086843effa8ab
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-07-21 13:51:15 +0530

    Fix formatting issue in LISTA_TRIMITES for PasoUnoComponent

--- apps/agace/src/app/application/core/enums/lista-trimites.enums.ts
+++ apps/agace/src/app/application/core/enums/lista-trimites.enums.ts
@@ -140,7 +140,7 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
             componentPath: () => import('../../tramites/32102/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
             componentName: 'PasoUnoComponent',  
         }]
-	}
+	},
     {
         tramite: 32615,
         listaComponentes: [{
