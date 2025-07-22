6989e0422a07a26243990a3c42b9ea807f6b09aa
mehul-ilink <kamani.mehulkumar@ilink-systems.com>
2025-07-22 13:41:20 +0530

    fix build error

--- apps/aga/src/app/application/tramites/570102/registro-solicitud-desistimiento.module.ts
+++ apps/aga/src/app/application/tramites/570102/registro-solicitud-desistimiento.module.ts
@@ -1,7 +1,8 @@
-import { AlertComponent,AnexarDocumentosComponent,BtnContinuarComponent, FirmaElectronicaComponent, InputCheckComponent,NotificacionesComponent,SharedModule,SolicitanteComponent, TituloComponent,WizardComponent} from '@libs/shared/data-access-user/src';
+import { AlertComponent, AnexarDocumentosComponent, BtnContinuarComponent, FirmaElectronicaComponent, InputCheckComponent, SharedModule, SolicitanteComponent, TituloComponent, WizardComponent } from '@libs/shared/data-access-user/src';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NotificacionesComponent } from '@ng-mf/data-access-user';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
 import { RegistroSolicitudDesistimientoRoutingModule } from './registro-solicitud-desistimiento-routing.module';
@@ -26,13 +27,12 @@ import { SolicitudPageComponent } from './pages/solicitud-page/solicitud-page.co
     TituloComponent,
     AlertComponent,
     AnexarDocumentosComponent,
-    FirmaElectronicaComponent,
     SharedModule,
     TituloComponent,
     AlertComponent,
     NotificacionesComponent
   ],
-  exports:[SolicitudPageComponent],
+  exports: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class RegistroSolicitudDesistimientoModule { }
