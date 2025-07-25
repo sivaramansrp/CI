d6908798b0ade6db55382ae6bfc8369bdd27c21b
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-07-25 21:20:17 +0530

    test case fixed

--- apps/agricultura/src/app/application/tramites/220201/components/agregardestinatariofinal/agregardestinatariofinal.component.spec.ts
+++ apps/agricultura/src/app/application/tramites/220201/components/agregardestinatariofinal/agregardestinatariofinal.component.spec.ts
@@ -1,13 +1,33 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
+
+import { ZoosanitarioStore } from '../../estados/220201/zoosanitario.store';
+import { CertificadoZoosanitarioServiceService } from '../../services/220201/certificado-zoosanitario.service';
 import { AgregardestinatariofinalComponent } from './agregardestinatariofinal.component';
 
 describe('AgregardestinatariofinalComponent', () => {
   let component: AgregardestinatariofinalComponent;
   let fixture: ComponentFixture<AgregardestinatariofinalComponent>;
 
+  const mockCertificadoService = {
+    getAllDatosForma: jest.fn().mockReturnValue(of({ seletedExdora: {} }))
+  };
+
+  const mockStore = {
+    updatedatosForma: jest.fn(),
+    actualizarSelectedExdora: jest.fn()
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AgregardestinatariofinalComponent],
+      imports: [AgregardestinatariofinalComponent, HttpClientTestingModule],
+      providers: [
+        provideRouter([]),
+        { provide: CertificadoZoosanitarioServiceService, useValue: mockCertificadoService },
+        { provide: ZoosanitarioStore, useValue: mockStore }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AgregardestinatariofinalComponent);
@@ -15,7 +35,42 @@ describe('AgregardestinatariofinalComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form with default values', () => {
+    expect(component.destinatarioForm).toBeDefined();
+    expect(component.destinatarioForm.value.tipoMercancia).toBe('yes');
+  });
+
+  it('should call store methods when form is valid on save', () => {
+    component.destinatarioForm.patchValue({
+      tipoMercancia: 'yes',
+      razonSocial: 'Empresa',
+      pais: 'MX',
+      domicilio: 'Calle 1',
+    });
+    component.onGuardarDestinatarioFinal();
+    expect(mockStore.updatedatosForma).toHaveBeenCalled();
+    expect(mockStore.actualizarSelectedExdora).toHaveBeenCalled();
+  });
+
+  it('should reset form on limpiar', () => {
+    component.destinatarioForm.patchValue({ razonSocial: 'Test' });
+    component.onLimpiarDestinatario();
+    expect(component.destinatarioForm.value.tipoMercancia).toBe('yes');
+  });
+
+  it('should emit cerrar on cancel', () => {
+    jest.spyOn(component.cerrar, 'emit');
+    component.onCancelarDestinatario();
+    expect(component.cerrar.emit).toHaveBeenCalled();
+  });
+
+  it('should update validators on enCambioValorRadio', () => {
+    component.destinatarioForm.patchValue({ tipoMercancia: 'no' });
+    component.enCambioValorRadio();
+    expect(component.destinatarioForm.get('nombre')?.validator).toBeTruthy();
   });
 });
--- apps/agricultura/src/app/application/tramites/220201/components/sub-productos-contenedora/sub-productos-contenedora.component.spec.ts
+++ apps/agricultura/src/app/application/tramites/220201/components/sub-productos-contenedora/sub-productos-contenedora.component.spec.ts
@@ -1,13 +1,66 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { ZoosanitarioStore } from '../../estados/220201/zoosanitario.store';
+import { CapturarSolicitud } from '../../models/220201/capturar-solicitud.model';
+import { ZoosanitarioQuery } from '../../queries/220201/zoosanitario.query';
+import { AgriculturaApiService } from '../../services/220201/agricultura-api.service';
 import { SubProductosContenedoraComponent } from './sub-productos-contenedora.component';
 
 describe('SubProductosContenedoraComponent', () => {
   let component: SubProductosContenedoraComponent;
   let fixture: ComponentFixture<SubProductosContenedoraComponent>;
 
+  const mockCapturarSolicitud: CapturarSolicitud = {
+    datosDeLaSolicitud: {} as any,
+    datosParaMovilizacionNacional: {} as any,
+    pagoDeDerechos: {} as any,
+    tercerosRelacionados: [],
+    validarEnvio: {} as any,
+    tablaDatos: [],
+    selectedDatos: [],
+    datos: {} as any,
+    datosForma: [],
+    seletedTerceros: {} as any,
+    seletedExdora: {} as any,
+  };
+
+  let mockApiService: Partial<AgriculturaApiService>;
+  let mockStore: Partial<ZoosanitarioStore>;
+  let mockQuery: Partial<ZoosanitarioQuery>;
+
   beforeEach(async () => {
+    mockApiService = {
+      obtenerProductoRespuestaPorUrl: jest.fn().mockReturnValue(of({}))
+    };
+
+    mockQuery = {
+      seleccionarState$: of({
+        datosDeLaSolicitud: {} as any,
+        datosParaMovilizacionNacional: {} as any,
+        pagoDeDerechos: {} as any,
+        tercerosRelacionados: [],
+        validarEnvio: {} as any,
+        tablaDatos: [],
+        selectedDatos: [],
+        datos: {} as any,
+        datosForma: [],
+        seletedTerceros: {} as any,
+        seletedExdora: {} as any,
+      })
+    };
+
+
+    mockStore = {
+      update: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       imports: [SubProductosContenedoraComponent],
+      providers: [
+        { provide: AgriculturaApiService, useValue: mockApiService },
+        { provide: ZoosanitarioQuery, useValue: mockQuery },
+        { provide: ZoosanitarioStore, useValue: mockStore }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(SubProductosContenedoraComponent);
@@ -15,7 +68,28 @@ describe('SubProductosContenedoraComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch catalog data on init', () => {
+    expect(mockApiService.obtenerProductoRespuestaPorUrl).toHaveBeenCalledWith('productos.json');
+  });
+
+  it('should update store when agregarDatosFormulario is called', () => {
+    const sampleEvent: any = {
+      formulario: {
+        id: 123,
+        tipoRequisito: 'req',
+        requisito: 'test',
+        cantidadUMT: '5',
+        cantidadUMC: '10'
+        // ... add other required fields as needed
+      }
+    };
+
+    component.agregarDatosFormulario(sampleEvent);
+
+    expect(mockStore.update).toHaveBeenCalledWith(expect.any(Function));
   });
 });
