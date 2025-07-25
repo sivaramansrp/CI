2421e7a45b5242d3afc155b426f5b2739ddd351c
Vijay Amarnath M V <vijayamarnath.mv@purpleslate.in>
2025-07-25 17:39:47 +0530

    refactor: clean up unused code and comments across components

--- apps/agricultura/src/app/application/tramites/220503/pages/paso-uno/paso-uno.component.ts
+++ apps/agricultura/src/app/application/tramites/220503/pages/paso-uno/paso-uno.component.ts
@@ -20,6 +20,12 @@ import { Solocitud220503Service } from '../../services/service220503.service';
 })
 /** Componente para gestionar el primer paso del trámite */
 export class PasoUnoComponent implements OnDestroy, OnInit {
+    /**
+ * @property solicitante - Referencia al componente `SolicitanteComponent` que se utiliza para manejar
+ *                          la lógica y los datos relacionados con el solicitante en este paso del trámite.
+ * @command Este decorador `@ViewChild` permite acceder al componente hijo para interactuar con sus métodos y propiedades.
+ */
+  @ViewChild('solicitanteRef') solicitante!: SolicitanteComponent;
   /** Datos de respuesta del servidor utilizados para actualizar el formulario. */
   public esDatosRespuesta: boolean = false;
 
@@ -106,6 +112,21 @@ export class PasoUnoComponent implements OnDestroy, OnInit {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+  public validarFormularios(): boolean {
+    let isValid = true;
+
+    if (this.solicitante?.form) {
+      if (this.solicitante.form.invalid) {
+        this.solicitante.form.markAllAsTouched();
+        isValid = false;
+      }
+    } else {
+      isValid = false;
+    }
+
+    return isValid;
   }
 
   /**
