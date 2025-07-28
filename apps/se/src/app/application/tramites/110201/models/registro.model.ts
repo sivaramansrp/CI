e8d776bcfb4f34f7d251521a9b06c13271c96ab7
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-07-28 16:12:10 +0530

    Resolved bugs for 110201

--- apps/se/src/app/application/tramites/110201/models/registro.model.ts
+++ apps/se/src/app/application/tramites/110201/models/registro.model.ts
@@ -1,3 +1,5 @@
+import { CatalogosSelect } from "@libs/shared/data-access-user/src";
+
 export interface ColumnasTabla {
   fraccionArancelaria: string;
   nombreTecnico: string;
@@ -36,10 +38,79 @@ export const FECHAFACTURA = {
   habilitado: true,
 };
 
+export const OPTIONS_TRATADO: CatalogosSelect = {
+  labelNombre: 'Tratado/Acuerdo',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_PAIS: CatalogosSelect = {
+  labelNombre: 'País / Bloque',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_UMC: CatalogosSelect = {
+  labelNombre: 'UMC',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_UNIDAD_MEDIDA: CatalogosSelect = {
+  labelNombre: 'Unidad de medida de la masa bruta',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_TIPO_FACTURA: CatalogosSelect = {
+  labelNombre: 'Tipo de factura',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_IDIOMA: CatalogosSelect = {
+  labelNombre: 'Idioma',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_ENTIDAD_FEDERATIVA: CatalogosSelect = {
+  labelNombre: 'Entidad federativa',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_REPRESENTACION_FEDERAL: CatalogosSelect = {
+  labelNombre: 'Representación federal',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+
+export const OPTIONS_NACION: CatalogosSelect = {
+  labelNombre: 'País destino',
+  required: true,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+}
+export const OPTIONS_TRANSPORTE: CatalogosSelect = {
+  labelNombre: 'Medio de transporte',
+  required: false,
+  primerOpcion: 'Selecciona un valor',
+  catalogos: [],
+};
+
 export const DESPACHO_LDA = {
-    labelNombre: 'Sí',
-    maxlength: 10,
-    minlenght: 0,
-    required: false,
-    alfanumerico: true,
-  };
+  labelNombre: 'Sí',
+  maxlength: 10,
+  minlenght: 0,
+  required: false,
+  alfanumerico: true,
+};
