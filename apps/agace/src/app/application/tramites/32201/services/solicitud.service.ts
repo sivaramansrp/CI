105d738e7c166bb13e2d358caab0873b780b35ba
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-07-30 15:34:08 +0530

    BF done for 32201 and 32101

--- apps/agace/src/app/application/tramites/32201/services/solicitud.service.ts
+++ apps/agace/src/app/application/tramites/32201/services/solicitud.service.ts
@@ -1,15 +1,26 @@
+import { Solicitud32201State, Tramite32201Store } from '../estados/tramite32201.store';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { RespuestaConsulta } from '../models/solicitud.model';
-
 @Injectable({
   providedIn: 'root'
 })
 export class SolicitudService {
+ 
+  constructor(private http: HttpClient, private tramite32201Store: Tramite32201Store) { }
 
-  constructor(private http: HttpClient) { }
+  /**
+       * Actualiza el estado global del formulario con los datos proporcionados.
+       * @param DATOS Objeto con los datos del formulario de tipo Solicitud32201State.
+       */
+  actualizarEstadoFormulario(DATOS: Solicitud32201State): void {
+    this.tramite32201Store.setRegimen_0(DATOS.regimen_0);
+    this.tramite32201Store.setRegimen_1(DATOS.regimen_1);
+    this.tramite32201Store.setRegimen_2(DATOS.regimen_2);
+    this.tramite32201Store.setRegimen_3(DATOS.regimen_3);
+    this.tramite32201Store.setManifiesto(DATOS.manifiesto);
 
+  }
   /**
    * @method getDatosConsulta
    * @description Obtiene los datos de consulta desde un archivo JSON local.
@@ -18,8 +29,8 @@ export class SolicitudService {
    * 
    * @returns {Observable<RespuestaConsulta>} Un observable que emite la respuesta de los datos de consulta.
    */
-  getDatosConsulta(): Observable<RespuestaConsulta> {
-    return this.http.get<RespuestaConsulta>(`assets/json/32201/consultaDatos.json`);
+  getDatosConsulta(): Observable<Solicitud32201State> {
+    return this.http.get<Solicitud32201State>('assets/json/32201/consultaDatos.json');
   }
-  
+
 }
