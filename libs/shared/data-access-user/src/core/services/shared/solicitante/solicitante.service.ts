f372623a5e39092d253b8c79d8da5a5aa7ec2e9f
David Corza <dcorzaa@ultrasist.com.mx>
2025-08-05 14:51:24 -0600

    se modifica y ajusta para poder habilitar los dos ecenarios -los tramites que ya tienen un mock vs tramites consumiendo el api

--- libs/shared/data-access-user/src/core/services/shared/solicitante/solicitante.service.ts
+++ libs/shared/data-access-user/src/core/services/shared/solicitante/solicitante.service.ts
@@ -4,8 +4,8 @@ import { DatosGeneralesModel } from '../../../models/datos-generales.model';
 import { ENVIRONMENT } from '../../../../enviroments/enviroment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JSONResponse } from '../../../models/shared/catalogos.model';
 import { RFC_GENERICO } from '../../../constants/constantes-generales';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -17,15 +17,30 @@ export class SolicitanteService {
     this.host = `${ENVIRONMENT.API_HOST}/api/`;
   }
 
+  urlServer = 'https://dev.v30.ultrasist.net/api/json-auxiliar';
+
+  /**
+   * Obtiene los datos generales de un contribuyente desde la API.
+   * @param {string} rfc - RFC del contribuyente para realizar la consulta.
+   * @returns {Observable<DatosGeneralesModel>} - Observable con la respuesta de la API.
+   */
+  getDatosGeneralesAPI(rfc: string): Observable<DatosGeneralesModel> {
+    const ENDPOINT =
+      `${this.host}` + API_GET_IDC_CONTRIBUYENTE.replace(RFC_GENERICO, rfc);
+    return this.http.get<DatosGeneralesModel>(`${ENDPOINT}`).pipe(
+      catchError((error) => {
+        return throwError(() => error);
+      })
+    );
+  }
+
   /**
    * Obtiene los datos Generales del Usuario de una API
    * @param {id} - Id del json auxiliar que trae los datos generales del usuario
    * @returns {Observable<JSONResponse>} - Respuesta de la API de tipo observable de tipo JSONResponse.
    */
-  getDatosGenerales(rfc: string): Observable<DatosGeneralesModel> {
-    const ENDPOINT =
-      `${this.host}` + API_GET_IDC_CONTRIBUYENTE.replace(RFC_GENERICO, rfc);
-    return this.http.get<DatosGeneralesModel>(`${ENDPOINT}`).pipe(
+  getDatosGenerales(id: number): Observable<JSONResponse> {
+    return this.http.get<JSONResponse>(`${this.urlServer}/${id}`).pipe(
       catchError((error) => {
         return throwError(() => error);
       })
