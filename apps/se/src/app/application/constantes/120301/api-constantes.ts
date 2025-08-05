48e5af16c17fd02b7262e97e6022f6e19a644354
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-08-04 18:14:27 -0600

    ADD Archivo de constantes

---
+++ apps/se/src/app/application/constantes/120301/api-constantes.ts
@@ -0,0 +1,17 @@
+/**
+ * API para el tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Registro-Solicitud/iniciar
+ */
+export const API_POST_INICIO = 'sat-t120301/solicitud/iniciar';
+
+/**
+ * API para guardar la solicitud del tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Registro-Solicitud/buscar-instrumentos-TPL
+ */
+export const API_POST_TPL = 'sat-t120301/instrumentos-tpl';
+
+/**
+ * API para consultar los anios de autorizacion del tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Catalogos/consulta-anios-autorizacion
+ */
+export const API_GET_ANIOS_AUTORIZACION = 'sat-t120301/catalogo/anios/autorizacion'
