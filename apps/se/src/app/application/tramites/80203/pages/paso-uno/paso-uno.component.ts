cd9005dff32f17868136f018a11c6efa080c67d3
TejaswiniB-ilink <tejaswini.bhandare@ilink-systems.com>
2025-08-05 16:25:21 +0530

    Bug fix for Trámite 80203

--- apps/se/src/app/application/tramites/80203/pages/paso-uno/paso-uno.component.ts
+++ apps/se/src/app/application/tramites/80203/pages/paso-uno/paso-uno.component.ts
@@ -96,7 +96,7 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
     } else {
       this.esDatosRespuesta = true;
     }
-    this.asignarSecciones();
+    // this.asignarSecciones();
   }
   /**
    * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
