330932ac0d60db352cd12b25ae76afe16e4a0318
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-08-05 15:49:01 -0600

    ADD Service

--- apps/se/src/app/application/core/services/120301/Tpl.service.ts
+++ apps/se/src/app/application/core/services/120301/Tpl.service.ts
@@ -6,7 +6,10 @@ import { Observable } from 'rxjs';
 import { TplRequest } from '../../models/120301/request/tpl-request.model';
 import { TplResponse } from '../../models/120301/response/tpl-response.model';
 
-import { API_POST_TPL } from '../../../constantes/120301/api-constantes';
+import { API_GET_REPRESENTACION_FEDERAL, API_POST_DETALLE_TPL, API_POST_TPL, IDASIGNACION } from '../../../constantes/120301/api-constantes';
+import { RepresentaciónFederalResponse } from '../../models/120301/response/representacion-federal-response.model';
+import { TplDetalleRequest } from '../../models/120301/request/tpl-detalle-request.model';
+import { TplDetalleResponse } from '../../models/120301/response/tpl-detalle-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -34,5 +37,27 @@ export class TplService {
   posTpl(PAYLOAD: TplRequest): Observable<BaseResponse<TplResponse[]>> {
     const ENDPOINT = `${this.host}` + API_POST_TPL;
     return this.http.post<BaseResponse<TplResponse[]>>(ENDPOINT, PAYLOAD);
+  }
+
+  /**
+   * Consulta de representacion federal de la solicitud del trámite 120301.
+   * 
+   * @param idAsignacion Numero de la asignacion.
+   * @returns Observable con la respuesta del servidor.
+  */
+  getRepresentacionFederal(idAsignacion: number ): Observable<BaseResponse<RepresentaciónFederalResponse>> {
+    const ENDPOINT = `${this.host}${API_GET_REPRESENTACION_FEDERAL.replace(IDASIGNACION, idAsignacion.toString())}`;
+    return this.http.get<BaseResponse<RepresentaciónFederalResponse>>(ENDPOINT);
+  }
+
+  /**
+   * Consulta de tpl a detalle del trámite 120301.
+   * 
+   * @param PAYLOAD Datos de la solicitud TPL.
+   * @returns Observable con la respuesta del servidor.
+  */
+  postTplDetalle(PAYLOAD: TplDetalleRequest ): Observable<BaseResponse<TplDetalleResponse>> {
+    const ENDPOINT = `${this.host}` + API_POST_DETALLE_TPL;
+    return this.http.post<BaseResponse<TplDetalleResponse>>(ENDPOINT, PAYLOAD);
   }
 }
