5a6d4af507901b45f755fad02c1dad368603850a
binaydas8 <binaykumar.das@ilink-systems.com>
2025-08-06 18:02:37 +0530

    bugfix
    bugfix

--- apps/cofepris/src/app/application/shared/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/cofepris/src/app/application/shared/components/datos-solicitud/datos-solicitud.component.ts
@@ -25,6 +25,7 @@ import { ConfiguracionVisibilidad } from '../../models/datos-domicilio-legal.mod
 import { ConsultaioQuery } from '@ng-mf/data-access-user';
 import { DEFAULT_CONFIGURACION_VISIBILIDAD } from '../../constantes/datos-domicilio-legal.enum';
 import { DatosDomicilioLegalQuery } from '../../estados/queries/datos-domicilio-legal.query';
+import { DatosDomicilioLegalService } from '../../services/datos-domicilio-legal.service';
 import { DomicilioComponent } from '../domicilio-establecimiento/domicilio-establecimiento.component';
 import { ManifiestosComponent } from '../manifiestos-declaraciones/manifiestos-declaraciones.component';
 import { RepresentanteLegalRfcComponent } from '../representante-legal-rfc/representante-legal-rfc.component';
@@ -63,6 +64,12 @@ export class DatosDeLaComponent implements OnInit, OnDestroy {
   @Input() isAduanasEntradaVisible: boolean = false;
 
   /**
+   * Indica si el campo de domicilio debe estar habilitado.
+   * Este input controla el estado habilitado/deshabilitado de la sección de domicilio en el componente.
+   */
+  @Input() tieneDomicilioHabilitar: boolean = false;
+
+  /**
    * Estado de la solicitud.
    */
   public solicitudState!: DatosDomicilioLegalState;
@@ -81,7 +88,9 @@ export class DatosDeLaComponent implements OnInit, OnDestroy {
   eliminarPedimento(borrar: boolean): void {
     this.alternarControlesDeFormulario();
     if (borrar) {
+      this.tieneDomicilioHabilitar = false;
       this.pedimentos.splice(this.elementoParaEliminar, 1);
+      this.service.emitEvent(this.tieneDomicilioHabilitar);
     }
   }
 
@@ -122,7 +131,8 @@ export class DatosDeLaComponent implements OnInit, OnDestroy {
     public readonly fb: FormBuilder,
     private datosDomicilioLegalStore: DatosDomicilioLegalStore,
     private datosDomicilioLegalQuery: DatosDomicilioLegalQuery,
-    private consultaioQuery: ConsultaioQuery
+    private consultaioQuery: ConsultaioQuery,
+    private service: DatosDomicilioLegalService
   ) {
     // Inicializa el formulario.
     this.consultaioQuery.selectConsultaioState$
--- apps/cofepris/src/app/application/shared/components/domicilio-establecimiento/domicilio-establecimiento.component.ts
+++ apps/cofepris/src/app/application/shared/components/domicilio-establecimiento/domicilio-establecimiento.component.ts
@@ -126,6 +126,13 @@ export class DomicilioComponent implements OnInit, OnDestroy {
   @Input() esPaginacionVisible: boolean = false;
 
   /**
+   * Indica si la sección de domicilio debe estar habilitada.
+   * Cuando se establece en `true`, los campos de domicilio estarán disponibles para ingresar datos.
+   * Este valor normalmente lo proporciona un componente padre para controlar el estado habilitado.
+   */
+  @Input() tieneDomicilioHabilitar: boolean = false;
+
+  /**
     * Número total de elementos en la tabla.
     */
   totalElementos: number = 0;
@@ -275,6 +282,10 @@ export class DomicilioComponent implements OnInit, OnDestroy {
       paisDeOriginDatos: [this.solicitudState?.aduanasDeEntrada || []],
       garantiasOfrecidas: [this.solicitudState?.garantiasOfrecidas],
     });
+
+    if(this.tieneDomicilioHabilitar) {
+      this.domicilio.disable();
+    }
   }
 
 
@@ -477,24 +488,24 @@ export class DomicilioComponent implements OnInit, OnDestroy {
    * Etiqueta de la lista de fechas.
    * */
   public paisDeDondeLabel: CrossListLable = {
-    tituluDeLaIzquierda: 'País donde se elabora el producto',
-    derecha: 'País(es) seleccionado(s)',
+    tituluDeLaIzquierda: 'País donde se elabora el producto:',
+    derecha: 'País(es) seleccionado(s)*:',
   };
 
   /**
    * Etiqueta de la lista de fechas.
    * */
   public paisOrigenLabel: CrossListLable = {
-    tituluDeLaIzquierda: 'País de origen',
-    derecha: 'País(es) seleccionado(s)',
+    tituluDeLaIzquierda: 'País de origen:',
+    derecha: 'País(es) seleccionado(s)*:',
   };
 
   /**
    * Etiqueta de la lista de fechas.
    * */
   public paisEmbarqueLabel: CrossListLable = {
-    tituluDeLaIzquierda: 'País de procedencia',
-    derecha: 'País(es) seleccionado(s)',
+    tituluDeLaIzquierda: 'País de procedencia:',
+    derecha: 'País(es) seleccionado(s)*:',
   };
 
   /**
@@ -566,6 +577,19 @@ export class DomicilioComponent implements OnInit, OnDestroy {
         })
       )
       .subscribe();
+
+    
+    this.service.event$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((valor) => {
+          this.tieneDomicilioHabilitar = (valor as boolean);
+          if (!this.tieneDomicilioHabilitar) {
+            this.domicilio.enable();
+          }
+        })
+      )
+      .subscribe()
     this.obtenerEstadoList();
     this.obtenerMercanciasDatos();
     this.configurarFormularioDomicillio()
@@ -688,19 +712,19 @@ export class DomicilioComponent implements OnInit, OnDestroy {
   readonly paisDeProcedenciaBotones = [
     {
       btnNombre: 'Agregar todos',
-      class: 'btn-primary',
+      class: 'btn-default',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[0].agregar('t'),
     },
     {
       btnNombre: 'Agregar selección',
-      class: 'btn-default',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[0].agregar(''),
     },
     {
       btnNombre: 'Restar selección',
-      class: 'btn-danger',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[0].quitar(''),
     },
@@ -718,19 +742,19 @@ export class DomicilioComponent implements OnInit, OnDestroy {
   readonly paisDeProcedenciaBotonesDuos = [
     {
       btnNombre: 'Agregar todos',
-      class: 'btn-primary',
+      class: 'btn-default',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[1].agregar('t'),
     },
     {
       btnNombre: 'Agregar selección',
-      class: 'btn-default',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[1].agregar(''),
     },
     {
       btnNombre: 'Restar selección',
-      class: 'btn-danger',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[1].quitar(''),
     },
@@ -748,19 +772,19 @@ export class DomicilioComponent implements OnInit, OnDestroy {
   readonly paisDeProcedenciaBotonesTres = [
     {
       btnNombre: 'Agregar todos',
-      class: 'btn-primary',
+      class: 'btn-default',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[2].agregar('t'),
     },
     {
       btnNombre: 'Agregar selección',
-      class: 'btn-default',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[2].agregar(''),
     },
     {
       btnNombre: 'Restar selección',
-      class: 'btn-danger',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[2].quitar(''),
     },
@@ -777,19 +801,19 @@ export class DomicilioComponent implements OnInit, OnDestroy {
   readonly paisDeProcedenciaBotonesCuatro = [
     {
       btnNombre: 'Agregar todos',
-      class: 'btn-primary',
+      class: 'btn-default',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[3].agregar('t'),
     },
     {
       btnNombre: 'Agregar selección',
-      class: 'btn-default',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[3].agregar(''),
     },
     {
       btnNombre: 'Restar selección',
-      class: 'btn-danger',
+      class: 'btn-primary',
       // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
       funcion: () => this.crossList.toArray()[3].quitar(''),
     },
