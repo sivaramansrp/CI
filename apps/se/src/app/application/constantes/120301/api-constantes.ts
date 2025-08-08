eb2eafb8b902ab18684963f4efd8239bd209512b
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-08-08 10:41:12 -0600

    ADD Url combos guardado parcial

--- apps/se/src/app/application/constantes/120301/api-constantes.ts
+++ apps/se/src/app/application/constantes/120301/api-constantes.ts
@@ -33,3 +33,21 @@ export const API_GET_REPRESENTACION_FEDERAL = `sat-t120301/asignacion/${IDASIGNA
  * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Registro-Solicitud/detalle-instrumento-TPL
  */
 export const API_POST_DETALLE_TPL = 'sat-t120301/instrumento-tpl/detalle';
+
+/**
+ * API para guardar parcialmente la solicitud del tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Registro-Solicitud/guardar-parcial
+ */
+export const API_POST_GUARDADO_PARCIAL = 'sat-t120301/solicitud/guardar-parcial';
+
+/**
+ * API para consultar las unidades de medidas del tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Catalogos/consulta-unidades-medida-se
+ */
+export const API_GET_UNIDAD_MEDIDA = 'sat-t120301/catalogo/unidades-medidas';
+
+/**
+ * API para consultar las unidades de medidas del tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Catalogos/consulta-importe-of-record
+ */
+export const API_GET_IMPORTE_RECORD = 'sat-t120301/catalogo/importer-of-record';
