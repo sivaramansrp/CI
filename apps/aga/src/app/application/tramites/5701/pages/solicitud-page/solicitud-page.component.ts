ddde7ee706a3513c649c4f178a7f87567830f444
David Corza <dcorzaa@ultrasist.com.mx>
2025-08-07 22:00:08 -0600

    varios issues 5701

--- apps/aga/src/app/application/tramites/5701/pages/solicitud-page/solicitud-page.component.ts
+++ apps/aga/src/app/application/tramites/5701/pages/solicitud-page/solicitud-page.component.ts
@@ -5,7 +5,9 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
+// eslint-disable-next-line sort-imports
 import {
+  CAMPO_VACIO,
   DatosPasos,
   ListaPasosWizard,
   Notificacion,
@@ -14,6 +16,8 @@ import {
   SeccionLibQuery,
   SeccionLibState,
   SeccionLibStore,
+  TEXTO_CERRAR,
+  TITULO_MODAL_AVISO,
   TercerosQuery,
   TercerosState,
   TransporteDespacho,
@@ -36,8 +40,10 @@ import {
   Tramite5701Store,
 } from '../../../../core/estados/tramites/tramite5701.store';
 import { GuardaSolicitudService } from '../../../../core/services/5701/guardar/guarda-solicitud.service';
+import { PasoUnoComponent } from '../paso-uno/paso-uno.component';
 import { Tramite5701Query } from '../../../../core/queries/tramite5701.query';
 import { WizardComponent } from '@libs/shared/data-access-user/src';
+
 
 /**
  * Interface que representa una acción de botón con su nombre y valor asociado.
@@ -122,6 +128,10 @@ export class SolicitudPageComponent implements OnInit {
    * @type {Notificacion}
    */
   public alertaNotificacion!: Notificacion;
+
+  formularioPadreEsValido: boolean = false;
+
+  @ViewChild(PasoUnoComponent) SolicitudPasoComponent!: PasoUnoComponent;
 
   /**
    * Representa los datos de configuración para los pasos de un proceso.
@@ -229,6 +239,24 @@ export class SolicitudPageComponent implements OnInit {
    * Si la acción no es 'cont', retrocede al paso anterior del wizard.
    */
   getValorIndice(e: AccionBoton): void {
+    // Validar el formulario del componente hijo antes de continuar
+    const VALIDA_FORM=this.SolicitudPasoComponent.validarFormularioPadre();
+  if (!VALIDA_FORM) {
+    this.nuevaNotificacion = {
+      tipoNotificacion: 'alert',
+      categoria: 'success',
+      modo: 'action',
+      titulo: TITULO_MODAL_AVISO,
+      mensaje: 'Has proporcionado información con formato incorrecto o no proporcionaste información en campos obligatorios.',
+      cerrar: false,
+      txtBtnAceptar: TEXTO_CERRAR,
+      txtBtnCancelar: CAMPO_VACIO,
+    };
+    this.indice = 1;
+    this.wizardComponent.indiceActual = 1;
+    
+    return;
+  }
     // Nos encontramos en el paso 1, se guarda parcialmente la información.
     if (this.indice === 1) {
       this.enviaSolicitudRequest()
@@ -763,5 +791,9 @@ export class SolicitudPageComponent implements OnInit {
    */
   resetearEstadoArchivos(): void {
     this.hayArchivosSeleccionados = false;
+  }
+
+  onFormularioPadreValido(isValid: boolean): void {
+    this.formularioPadreEsValido = isValid;
   }
 }
