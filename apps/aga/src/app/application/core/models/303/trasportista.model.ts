032c15da6e4868650074f2b692e0ffe27108ae0e
mjimenezm <mjimenezm@desarrollo-ultrasist.com.mx>
2025-08-08 14:55:14 -0600

    modelos

---
+++ apps/aga/src/app/application/core/models/303/trasportista.model.ts
@@ -0,0 +1,14 @@
+export interface Transportista {
+    idPersonaTransportista: string;
+    tipoPersona: string;
+    nacionalidad: string;
+    esExtranjero: boolean;
+    esNacional: boolean;
+    rfc: string;
+    rfcExtranjero: string;
+    nombre: string;
+    apellidoPaterno: string;
+    apellidoMaterno: string;
+    denominacionRazonSocial: string;
+    taxId: string;
+}
