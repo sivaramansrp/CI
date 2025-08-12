c58f7fae5b73b39ca2548b09f2c82e19e927a348
simrat-ilink <simratpal.gyani@ilink-systems.com>
2025-08-13 02:25:09 +0530

    consulta-flow-bugfixes

--- apps/cofepris/src/app/application/shared/constantes/datos-solicitud.enum.ts
+++ apps/cofepris/src/app/application/shared/constantes/datos-solicitud.enum.ts
@@ -703,7 +703,7 @@ export const MENSAJE_SIN_FILA_SELECCIONADA = 'Seleciona un registro';
  * @description
  *  Identificadores de notificación.
  */
-export const MOSTRAR_NOTIFICACION = [260219,260302,260304, 260206, 260201,260103,260203];
+export const MOSTRAR_NOTIFICACION = [260219,260302,260304, 260206, 260201,260103,260203, 260301];
 
 /**
  * Enumera los tipos de actualización que se pueden realizar.
