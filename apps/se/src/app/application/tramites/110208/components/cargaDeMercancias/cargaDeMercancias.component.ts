a124aa51f03f38d3c4bab0cfd6d079b694123985
shilpa-ilink <shilpa.k@ilink-systems.com>
2025-08-12 18:23:15 +0530

    unit testing fixes

--- apps/se/src/app/application/tramites/110208/components/cargaDeMercancias/cargaDeMercancias.component.ts
+++ apps/se/src/app/application/tramites/110208/components/cargaDeMercancias/cargaDeMercancias.component.ts
@@ -1,5 +1,5 @@
 import { ALERTA_PARA, FECHA_DE_FACTURA } from '@libs/shared/data-access-user/src/tramites/constantes/110208/certificado.enum';
-import { AlertComponent, Catalogo, CatalogoSelectComponent, ConfiguracionColumna, InputFecha, InputFechaComponent, TablaDinamicaComponent, TablaSeleccion, TituloComponent, ValidacionesFormularioService } from '@libs/shared/data-access-user/src';
+import { AlertComponent, Catalogo, CatalogoSelectComponent, ConfiguracionColumna, InputFecha, TablaDinamicaComponent, TablaSeleccion, TituloComponent, ValidacionesFormularioService } from '@libs/shared/data-access-user/src';
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MERCANCIA_TABLA, MercanciasFormInfo, MercanciasInfo } from '@libs/shared/data-access-user/src/core/models/110208/certificado.model';
@@ -7,10 +7,10 @@ import { Solicitud110208State, Tramite110208Store } from '../../../../estados/tr
 import { Subject, map, takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { ConsultaioQuery } from '@ng-mf/data-access-user';
+import { InputFechaComponent } from '@libs/shared/data-access-user/src/tramites/components/input-fecha/input-fecha.component';
 import { Modal } from 'bootstrap';
 import { Tramite110208Query } from '../../../../estados/queries/tramite110208.query';
 import { ValidarInicalmenteService } from '../../services/validar-inicalmente/validar-inicalmente.service';
-
 /**
  * Componente que gestiona la carga de mercancías.
  */
@@ -299,7 +299,7 @@ export class CargaDeMercanciasComponent implements OnInit, OnDestroy {
     return this.validacionesService.isValid(this.formMercancia, campo);
   }
 
-  
+
   /**
    * Método que se ejecuta al destruir el componente.
    */
