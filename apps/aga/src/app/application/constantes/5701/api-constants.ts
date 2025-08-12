2bfc396f462ae94f0c3c943ec8fdb1dd16e510ea
David Corza <dcorzaa@ultrasist.com.mx>
2025-08-12 14:38:32 -0600

    aga verificar dictamen 5701

--- apps/aga/src/app/application/constantes/5701/api-constants.ts
+++ apps/aga/src/app/application/constantes/5701/api-constants.ts
@@ -143,3 +143,83 @@ export const API_GET_VALIDA_LINEA_CAPTURA_PAGADA = `pago/valida/${LINEA_CAPTURA_
  * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/catalogo/swagger-ui/index.html#/LDA/valida-lda
  */
 export const API_POST_VALIDA_LDA = `catalogo/solicitud/valida-lda`;
+
+/**
+ * Constante para el número de folio del tramite.
+ * Debe ser reemplazada por el número de folio real del tramite.
+ */
+export const NUMFOLIOTRAMITE = '{numFolioTramite}';
+
+/**
+ * ID de la solicitud del dictamen que se utilizará en las consultas.
+ * Este valor debe ser reemplazado por el ID real de la solicitud del dictamen.
+ */
+export const IDSOLICITUDDICTAMEN = '{idSolicitudDictamen}';
+
+/**
+ * Tramite que se utilizará en las consultas.
+ * Este valor debe ser reemplazado por el tramite correspondiente.
+ */
+export const TRAMITE= '{tramite}';
+
+/**
+ * ID de la solicitud que se utilizará en las consultas.
+ * Este valor debe ser reemplazado por el ID real de la solicitud.
+ */
+export const IDSOLICITUD = '{idSolicitud}';
+
+/**
+ * API para obtener las opciones de evaluación del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Evaluar-Solicitud/opciones-evaluacion
+ */
+export const API_POST_OPCIONES_EVALUACION = `sat-t130118/tramite/${NUMFOLIOTRAMITE}/opciones-evaluacion`;
+
+/**
+ * API para generar el dictamen del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Dictamen/dictamen-criterios-by-idSolicitud
+ */
+export const API_GET_DICTAMEN = `sat-t130118/solicitud/${IDSOLICITUDDICTAMEN}/dictamen/generar/criterios`;
+
+/**
+ * API para guardar el dictamen del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Dictamen/generar-dictamen-by-numFolioTramite
+ */
+export const API_POST_GUARDAR_DICTAMEN = `sat-t130118/tramite/${NUMFOLIOTRAMITE}/dictamen/generar/guardar`;
+
+/**
+ * API para iniciar el dictamen del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Dictamen/generar-dictamen-by-numFolioTramite
+ */
+export const API_GET_INICAR_DICTAMEN = `sat-t130118/tramite/${NUMFOLIOTRAMITE}/dictamen/generar/iniciar`;
+
+/**
+ * API para Consultar acuses de resolución del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Acuse/consulta-acuse-resoluciones-funcionario
+ */
+export const API_GET_ACUSES_RESOLUCION = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/funcionario/acuses-resoluciones`;
+
+/**
+ * API para Consultar dictamenes del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitudes/consultar-dictamenes-by-numFolioTramite
+ */
+export const API_GET_DICTAMENES = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/dictamenes`;
+
+/**
+ * API para Consultar requerimientos del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitudes/get-requerimientos-by-numFolioTramite
+ */
+export const API_GET_REQUERIMIENTOS = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/requerimientos`;
+
+/**
+ * API para Consultar documentos de solicitud del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitudes/consulta-documentos-solicitud
+ */
+export const API_GET_SOLICITUD_DOCUMENTOS = `sat-t${TRAMITE}/solicitud/${IDSOLICITUD}/documentos`;
+
+/**
+ * API para Consultar tareas de solicitud del tramite 130118.
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitudes/get-tareas-tramite 
+ */
+export const API_GET_TAREAS_DOCUMENTOS = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/bitacora`;
+
+
---
+++ apps/aga/src/app/application/core/services/evaluar-tramite/iniciar.service.ts
@@ -0,0 +1,40 @@
+import { BaseResponse } from '@libs/shared/data-access-user/src/core/models/5701/base-response.model';
+import { ENVIRONMENT } from '@libs/shared/data-access-user/src';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { API_GET_INICAR_DICTAMEN, NUMFOLIOTRAMITE } from '../../../constantes/5701/api-constants';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class IniciarService {
+
+    /**
+    * URL base del servidor al que se realizarán las solicitudes relacionadas con aduanas.
+    * Esta variable almacena la dirección del host para los servicios compartidos de catálogos.
+    * Es de solo lectura y se inicializa en el constructor del servicio.
+    */
+    private readonly host: string;
+
+    /**
+    * Constructor del servicio que inicializa la URL base del host.
+    * @param http Instancia de HttpClient para realizar solicitudes HTTP.
+    */
+    constructor(private http: HttpClient) {
+        this.host = `${ENVIRONMENT.API_HOST}/api/`;
+    }
+
+    /**
+    * Inicia el dictamen del trámite 130118.
+    * @param numFolio Número de folio del trámite.
+    * @returns Observable con la respuesta del servidor.
+    */
+    getIniciarDictamen(numFolio: string): Observable<BaseResponse<null>> {
+        const ENDPOINT = `${this.host}${API_GET_INICAR_DICTAMEN.replace(NUMFOLIOTRAMITE, numFolio)}`;
+        return this.http.get<BaseResponse<null>>(ENDPOINT);
+    }
+
+}
