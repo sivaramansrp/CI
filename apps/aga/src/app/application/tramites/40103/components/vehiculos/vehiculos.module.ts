6980cb16366eaa0de86197cb8f0faa0a069af955
Sohel Arif <sohel.arif@ilink-systems.com>
2025-08-13 13:28:34 +0530

    Bug Fixes 40103

--- apps/aga/src/app/application/tramites/40103/components/vehiculos/vehiculos.module.ts
+++ apps/aga/src/app/application/tramites/40103/components/vehiculos/vehiculos.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CatalogoSelectComponent, TablaDinamicaComponent, TituloComponent } from '@ng-mf/data-access-user';
 
-import { VehiculosComponent } from './vehiculos.component';
-import { ModificacionVehiculoComponent } from './parque-vehicular/modificacion/modificacion-vehiculo.component';
-import { BajaVehiculoComponent } from './parque-vehicular/baja/baja-vehiculo.component';
-import { VehiculoDialogComponent } from './parque-vehicular/dialog/vehiculo-dialog.component';
-import { ModificacionUnidadComponent } from './unidad-de-arrastre/modificacion/modificacion-unidad.component';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BajaUnidadComponent } from './unidad-de-arrastre/baja/baja-unidad.component';
+
+import { CatalogoSelectComponent, TablaDinamicaComponent, TituloComponent } from '@ng-mf/data-access-user';
+import { BajaVehiculoComponent } from './parque-vehicular/baja/baja-vehiculo.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+import { ModificacionUnidadComponent } from './unidad-de-arrastre/modificacion/modificacion-unidad.component';
+import { ModificacionVehiculoComponent } from './parque-vehicular/modificacion/modificacion-vehiculo.component';
 import { UnidadDialogComponent } from './unidad-de-arrastre/dialog/unidad-dialog.component';
+import { VehiculoDialogComponent } from './parque-vehicular/dialog/vehiculo-dialog.component';
+import { VehiculosComponent } from './vehiculos.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +30,7 @@ import { UnidadDialogComponent } from './unidad-de-arrastre/dialog/unidad-dialog
     CatalogoSelectComponent,
     TablaDinamicaComponent,
     TituloComponent,
+    TooltipModule,
     VehiculoDialogComponent,
     UnidadDialogComponent
   ],
--- apps/aga/src/app/application/tramites/40103/enum/choferes.enum.ts
+++ apps/aga/src/app/application/tramites/40103/enum/choferes.enum.ts
@@ -13,17 +13,17 @@ export const CHOFERES_NACIONALES_ALTA: ConfiguracionColumna<DatosDelChoferNacion
         orden: 2,
     },
     {
-        encabezado: 'calle',
+        encabezado: 'Calle',
         clave: (item: DatosDelChoferNacional) => item.calle,
         orden: 3,
     },
     {
-        encabezado: 'Numero exterior',
+        encabezado: 'Número exterior',
         clave: (item: DatosDelChoferNacional) => item.numeroExterior,
         orden: 4,
     },
     {
-        encabezado: 'Numero interior',
+        encabezado: 'Número interior',
         clave: (item: DatosDelChoferNacional) => item.numeroInterior,
         orden: 5,
     },
@@ -53,12 +53,12 @@ export const CHOFERES_NACIONALES_ALTA: ConfiguracionColumna<DatosDelChoferNacion
         orden: 10,
     },
     {
-        encabezado: 'Codigo Postal',
+        encabezado: 'Código Postal',
         clave: (item: DatosDelChoferNacional) => item.codigoPostal,
         orden: 11,
     },
     {
-        encabezado: 'País de Residencia',
+        encabezado: 'País de residencia',
         clave: (item: DatosDelChoferNacional) => item.paisDeResidencia,
         orden: 12,
     }
@@ -71,7 +71,7 @@ export const CHOFERES_EXTRANJEROS_TABLA: ConfiguracionColumna<ChoferesExtranjero
         orden: 1,
     },
     {
-        encabezado: 'Número',
+        encabezado: 'Nombre',
         clave: (item: ChoferesExtranjeros) => item.numero,
         orden: 2,
     },
