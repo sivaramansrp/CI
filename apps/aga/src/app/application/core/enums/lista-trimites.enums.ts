ad2f59a844084b0fe288dff07f2cb746d91c4af3
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-08-14 12:34:26 +0530

    added 570101 proc navigation

--- apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
+++ apps/aga/src/app/application/core/enums/lista-trimites.enums.ts
@@ -438,4 +438,13 @@ export const LISTA_TRIMITES: AccuseComponentes[] = [
 
         }]
     },
+    {
+        tramite: 570101,
+        listaComponentes: [{
+            id: 'solicitud',
+            componentPath: () => import('../../tramites/570101/pages/paso-uno/paso-uno.component').then(m => m.PasoUnoComponent),
+            componentName: 'PasoUnoComponent',
+
+        }]
+    },
 ];
