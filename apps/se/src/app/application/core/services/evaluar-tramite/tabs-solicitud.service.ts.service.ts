9dbbd11c32ce676705d860f8b590b5ccdd8e161e
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-08-15 14:12:33 -0600

    ADD Servicios de  envio digital y opinion

--- apps/se/src/app/application/core/services/evaluar-tramite/tabs-solicitud.service.ts.service.ts
+++ apps/se/src/app/application/core/services/evaluar-tramite/tabs-solicitud.service.ts.service.ts
@@ -5,12 +5,16 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { API_GET_ACUSES_RESOLUCION, API_GET_DICTAMENES, API_GET_REQUERIMIENTOS, API_GET_SOLICITUD_DOCUMENTOS, API_GET_TAREAS_DOCUMENTOS, IDSOLICITUD, NUMFOLIOTRAMITE, TRAMITE } from '../../../constantes/130118/api-constants';
+import { API_GET_ACUSES_RESOLUCION, API_GET_DICTAMENES, API_GET_ENVIO_DIGITAL, API_GET_OPINION, API_GET_REQUERIMIENTOS, API_GET_SOLICITUD_DOCUMENTOS, API_GET_TAREAS_DOCUMENTOS, IDSOLICITUD, NUMFOLIOTRAMITE, TRAMITE } from '../../../constantes/130118/api-constants';
 import { AcusesResolucionResponse } from '@libs/shared/data-access-user/src/core/models/130118/consulta-acuses-response.model';
 import { DictamenesResponse } from '@libs/shared/data-access-user/src/core/models/130118/dictamenes-response.model';
 import { DocumentoSolicitud } from '@libs/shared/data-access-user/src/core/models/130118/consulta-documentos-response.model';
+import { EnvioDigitalResponse } from '@libs/shared/data-access-user/src/core/models/130118/envio-digital-response.model';
+import { OpinionResponse } from '@libs/shared/data-access-user/src/core/models/130118/opinion-response.model';
 import { RequerimientosResponse } from '@libs/shared/data-access-user/src/core/models/130118/requerimientos-response.model';
 import { TareasSolicitud } from '@libs/shared/data-access-user/src/core/models/130118/consulta-tareas-response.model';
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -76,7 +80,7 @@ export class TabsSolicitudServiceTsService {
    * @returns Observable con la respuesta del servidor.
    */
   getRequerimientos(tramite: number, numFolioTramite: string): Observable<BaseResponse<RequerimientosResponse[]>> {
-     const ENDPOINT = `${this.host}${API_GET_REQUERIMIENTOS.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
+    const ENDPOINT = `${this.host}${API_GET_REQUERIMIENTOS.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
     return this.http.get<BaseResponse<RequerimientosResponse[]>>(ENDPOINT);
   }
 
@@ -88,7 +92,31 @@ export class TabsSolicitudServiceTsService {
    * @returns Observable con la respuesta del servidor.
    */
   getDictamenes(tramite: number, numFolioTramite: string): Observable<BaseResponse<DictamenesResponse[]>> {
-     const ENDPOINT = `${this.host}${API_GET_DICTAMENES.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
+    const ENDPOINT = `${this.host}${API_GET_DICTAMENES.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
     return this.http.get<BaseResponse<DictamenesResponse[]>>(ENDPOINT);
+  }
+
+  /**
+   * Obtiene el envío digital del trámite 130118.
+   *
+   * @param tramite Numero del tramite.
+   * @param numFolioTramite Número de folio del trámite.
+   * @returns Observable con la respuesta del servidor.
+   */
+  getEnvioDigital(tramite: number, numFolioTramite: string): Observable<BaseResponse<EnvioDigitalResponse>> {
+    const ENDPOINT = `${this.host}${API_GET_ENVIO_DIGITAL.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
+    return this.http.get<BaseResponse<EnvioDigitalResponse>>(ENDPOINT);
+  }
+
+  /**
+   * Obtiene las opiniones relacionadas a un trámite específico.  
+   * 
+   * @param tramite - ID numérico del trámite.  
+   * @param numFolioTramite - Número de folio asociado al trámite.  
+   * @returns Observable con la respuesta del servidor, incluyendo un arreglo de opiniones.  
+   */
+  getOpiniones(tramite: number, numFolioTramite: string): Observable<BaseResponse<OpinionResponse[]>> {
+    const ENDPOINT = `${this.host}${API_GET_OPINION.replace(TRAMITE, tramite.toString()).replace(NUMFOLIOTRAMITE, numFolioTramite)}`;
+    return this.http.get<BaseResponse<OpinionResponse[]>>(ENDPOINT);
   }
 }
