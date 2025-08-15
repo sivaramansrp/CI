90f4ec888f965f557332b6cf40ac6e98a2832969
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-08-15 15:29:29 -0600

    ADD Urls facturas

--- apps/se/src/app/application/constantes/120301/api-constantes.ts
+++ apps/se/src/app/application/constantes/120301/api-constantes.ts
@@ -51,3 +51,39 @@ export const API_GET_UNIDAD_MEDIDA = 'sat-t120301/catalogo/unidades-medidas';
  * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Catalogos/consulta-importe-of-record
  */
 export const API_GET_IMPORTE_RECORD = 'sat-t120301/catalogo/importer-of-record';
+
+/**
+ * API para consultar facturas-tpl tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Servicios-Cupos/buscarFacturasDisponiblesTPL
+ */
+export const API_GET_FACTURAS_TPL = 'sat-t120301/facturas-tpl';
+
+/**
+ * API asociar facturas-tpl tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Servicios-Cupos/asociarFacturaTPL
+ */
+export const API_POST_FACTURAS_TPL_ASOCIAR = 'sat-t120301/facturas-tpl/asociar';
+
+/**
+ * Id expedicion que se utilizará en las consultas.
+ * Este valor debe ser reemplazado por el id asignacion correspondiente.
+ */
+export const IDEXPEDICION= '{idExpedicion}';
+
+/**
+ * API para facturas asociadas tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Servicios-Cupos/buscarExpedicionFacturasTPL
+ */
+export const API_GET_FACTURAS_TPL_ASOCIADAS = `sat-t120301/facturas/asociada/${IDEXPEDICION}`;
+
+/**
+ * API para total de unidades tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Registro-Solicitud/factura-asociada-total-unidad
+ */
+export const API_GET_FACTURAS_TPL_TOTAL_UNIDAD = `sat-t120301/factura-asociada/${IDEXPEDICION}/total-unidad`;
+
+/**
+ * API para eliminar facturas-tpl asociadas tramite 120301.
+ * @see http://api-vucem-1069277193.us-east-1.elb.amazonaws.com/api/sat-t120301/swagger-ui/index.html#/Servicios-Cupos/eliminarFactura
+ */
+export const API_DELETE_FACTURAS_TPL_ELIMINAR = 'sat-t120301/expedicion-factura';
