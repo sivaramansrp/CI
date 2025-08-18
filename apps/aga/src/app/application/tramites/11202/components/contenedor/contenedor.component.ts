3290cc3465cbdb7d919adbc0c199f3960c8289b0
Kanchan Phutane <kanchan.phutane@ilink-systems.com>
2025-08-18 21:04:15 +0530

    code changes for popup

--- apps/aga/src/app/application/tramites/11202/components/contenedor/contenedor.component.ts
+++ apps/aga/src/app/application/tramites/11202/components/contenedor/contenedor.component.ts
@@ -1,7 +1,7 @@
 import { Catalogo, ConfiguracionColumna, ConsultaioQuery, ConsultaioState } from '@ng-mf/data-access-user';
 import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { Contenedor11202State, Contenedor11202Store } from '../../estados/contenedor11202.store';
-import { CSV_DE_TABLA, GRID_CONTENEDORES, SOLICITUD_11202_ENUM } from '../../constantes/retorno-contenedores.enum';
+import { CSV_DE_TABLA, ELGIR_DE_ARCHIVO, GRID_CONTENEDORES, SOLICITUD_11202_ENUM } from '../../constantes/retorno-contenedores.enum';
 import { DatosDelCsvArchivo, GridContenedores } from '../../models/datos-tramite.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subject, map, takeUntil } from 'rxjs';
@@ -69,6 +69,11 @@ export class ContenedorComponent implements OnInit, OnDestroy {
    * Indicates whether the add container type section is visible.
    */
   seccionContenedor: boolean = false;
+
+  /**
+   * Referencia al elemento del modal para agregar mercancías.
+   */
+  @ViewChild('modalArchivoCsv') modalArchivo!: ElementRef;
 
   /**
    * @property {Catalogo[]} catalogAduanas
@@ -177,6 +182,11 @@ export class ContenedorComponent implements OnInit, OnDestroy {
    * Etiqueta del archivo seleccionado.
    */
   elgirDeArchivo: string = SOLICITUD_11202_ENUM.ELGIR_DE_ARCHIVO;
+
+  /**
+   * Descripción del archivo seleccionado.
+   */
+  archivo_descripcion: string = ELGIR_DE_ARCHIVO.ARCHIVO_DESCRIPCION;
 
   /**
    * Elemento de entrada de archivo HTML.
@@ -374,12 +384,11 @@ export class ContenedorComponent implements OnInit, OnDestroy {
           if (respuesta?.success) {
             respuesta.datos.id = this.datosDelCsvArchivo.length + 1;
             this.datosDelCsvArchivo = [...this.datosDelCsvArchivo, respuesta.datos];
-            console.log('Datos del CSV:', this.datosDelCsvArchivo);
             (this.contenedorStore.setDelCsv as (valor: DatosDelCsvArchivo[]) => void)(this.datosDelCsvArchivo);
           }
         }
       );
-    } 
+    }
   }
 
   /**
