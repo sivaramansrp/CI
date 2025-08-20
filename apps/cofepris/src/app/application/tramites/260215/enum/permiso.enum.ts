53c0698116d89308017cfaf14a00b5f4d6c93a36
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-08-20 20:51:33 +0530

    bug fixes

--- apps/cofepris/src/app/application/tramites/260215/enum/permiso.enum.ts
+++ apps/cofepris/src/app/application/tramites/260215/enum/permiso.enum.ts
@@ -26,7 +26,7 @@ export const MENSAJE_DE_ALERTA: string =
   'Cumplo con los requisitos y normatividad aplicable, sin que me eximan de que la autoridad sanitaria verifique su cumplimiento, esto sin perjuicio de las sanciones en que puedo incurrir por falsedad de declaraciones dadas a una autoridad. Asimismo acepto que la notificación de este trámite, sea a través de la Ventanilla Única de Comercio Exterior por los mecanismos de la misma.';
 
 export const INPUT_FECHA_CONFIG = {
-  labelNombre: 'Fecha de pago',
+  labelNombre: 'Fecha de pago:',
   required: false,
   habilitado: true,
 };
