ec7fb9a45d33c715cbcc885f139230728bc96089
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-08-22 02:24:49 -0600

    CHANGE Tabs requerimiento

--- libs/shared/data-access-user/src/tramites/components/reviewers-tabs/reviewers-tabs.component.ts
+++ libs/shared/data-access-user/src/tramites/components/reviewers-tabs/reviewers-tabs.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { AcusesResolucionResponse } from "../../../core/models/130118/consulta-acuses-response.model";
 import { BandejaAcusesComponent } from "../consulta-generica/bandeja-acuses/bandeja-acuses.component";
 import { CommonModule } from "@angular/common";
-import { ConsultarequerimientosComponent } from "../consulta-generica/consulta-requerimientos/consulta-requerimientos.component";
 import { DictamenesResponse } from "../../../core/models/130118/dictamenes-response.model";
 import { DocumentoSolicitud } from "../../../core/models/130118/consulta-documentos-response.model";
 import { DocumentosComponent } from "../consulta-generica/bandeja-documentos/documentos.component";
@@ -12,11 +11,13 @@ import { OpinionResponse } from "../../../core/models/130118/opinion-response.mo
 import { RequerimientosResponse } from "../../../core/models/130118/requerimientos-response.model";
 import { TabDictamenComponent } from "../consulta-generica/tab-dictamen/tab-dictamen.component";
 import { TabOpinionComponent } from "../consulta-generica/tab-opinion/tab-opinion.component";
+import { TabRequerimientoComponent } from "../consulta-generica/tab-requerimiento/tab-requerimiento.component";
 import { TabsResponse } from "../../../core/models/130118/consulta-tabs-response.model";
 import { Tabulaciones } from "../../../core/models/lista-trimites.model";
 import { TareasSolicitud } from "../../../core/models/130118/consulta-tareas-response.model";
 import { TareasTramiteComponent } from "../consulta-generica/bandeja-tareas-tramite/tareas-tramite.component";
 import tramiteDetailsData from '@libs/shared/theme/assets/json/shared/lista-trimites-tabs.json';
+
 
 
 
@@ -37,7 +38,7 @@ import tramiteDetailsData from '@libs/shared/theme/assets/json/shared/lista-trim
  */@Component({
   selector: 'app-reviewers-tabs',
   standalone: true,
-  imports: [CommonModule,DocumentosComponent, TabDictamenComponent, ConsultarequerimientosComponent, TabOpinionComponent, BandejaAcusesComponent, TareasTramiteComponent, EnvioDigitalComponent],
+  imports: [CommonModule,DocumentosComponent, TabDictamenComponent, TabRequerimientoComponent, TabOpinionComponent, BandejaAcusesComponent, TareasTramiteComponent, EnvioDigitalComponent],
   templateUrl: './reviewers-tabs.component.html',
   styleUrl: './reviewers-tabs.component.scss',
 })
