4176cbb9e2be79592a3e258b687f4d1aeec67cea
sivaramansrp <sivaraman.selvaraj@ilink-systems.com>
2025-08-22 13:08:30 +0530

    Delete k8s/profepa-microfront/script directory

--- k8s/profepa-microfront/script/inject-version.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { version } = require('../package.json');
-
-const distIndex = path.join(__dirname, '../dist/apps/profepa/index.html');
-let indexHtml = fs.readFileSync(distIndex, 'utf8');
-indexHtml = indexHtml.replace('Version: __BUILD_VERSION__', `Version: ${version}`);
-fs.writeFileSync(distIndex, indexHtml);
-console.log(`Injected version ${version} into index.html`);
