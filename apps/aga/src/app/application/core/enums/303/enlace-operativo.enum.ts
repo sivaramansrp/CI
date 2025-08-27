343fac4a81ebea7c22e33a830e5845d7688ed27e
mjimenezm <mjimenezm@desarrollo-ultrasist.com.mx>
2025-08-27 10:40:01 -0600

    Documentación de enum

--- apps/aga/src/app/application/core/enums/303/enlace-operativo.enum.ts
+++ apps/aga/src/app/application/core/enums/303/enlace-operativo.enum.ts
@@ -1,14 +1,16 @@
 import { EnlaceOperativo } from "../../models/303/enlace-operativo.model";
 
-
+/**
+ * Definición de las columnas para la tabla de Enlace Operativo
+ */
 export const ENLACE_OPERATIVO = [
-    { encabezado: 'RFC', clave: (item: EnlaceOperativo) => item.rfc, orden: 1 },
-    { encabezado: 'Nombre', clave: (item: EnlaceOperativo) => item.nombre, orden: 2 },
-    { encabezado: 'Primer apellido', clave: (item: EnlaceOperativo) => item.apellidoPaterno, orden: 3 },
-    { encabezado: 'Segundo apellido', clave: (item: EnlaceOperativo) => item.apellidoMaterno, orden: 4 },
-    { encabezado: 'Ciudad o estado de Recidencia', clave: (item: EnlaceOperativo) => item.ciudad, orden: 5 },
-    { encabezado: 'Cargo o Puesto', clave: (item: EnlaceOperativo) => item.cargoPuesto, orden: 6 },
-    { encabezado: 'Telefono', clave: (item: EnlaceOperativo) => item.telefono, orden: 7 },
-    { encabezado: 'Correo Electronico', clave: (item: EnlaceOperativo) => item.correoElectronico, orden: 8 },
-    { encabezado: 'Suplente', clave: (item: EnlaceOperativo) => item.suplente, orden: 9 }
+    { encabezado: 'RFC', clave: (item: EnlaceOperativo): string => item.rfc, orden: 1 },
+    { encabezado: 'Nombre', clave: (item: EnlaceOperativo): string => item.nombre, orden: 2 },
+    { encabezado: 'Primer apellido', clave: (item: EnlaceOperativo): string => item.apellidoPaterno, orden: 3 },
+    { encabezado: 'Segundo apellido', clave: (item: EnlaceOperativo): string => item.apellidoMaterno, orden: 4 },
+    { encabezado: 'Ciudad o estado de Recidencia', clave: (item: EnlaceOperativo): string => item.ciudad, orden: 5 },
+    { encabezado: 'Cargo o Puesto', clave: (item: EnlaceOperativo): string => item.cargoPuesto, orden: 6 },
+    { encabezado: 'Telefono', clave: (item: EnlaceOperativo): string => item.telefono, orden: 7 },
+    { encabezado: 'Correo Electronico', clave: (item: EnlaceOperativo): string => item.correoElectronico, orden: 8 },
+    { encabezado: 'Suplente', clave: (item: EnlaceOperativo): boolean => item.suplente, orden: 9 }
 ]
