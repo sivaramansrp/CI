7546dee22e2c51f4a66ae8cbdb3824aa0106bd8b
sivaramansrp <sivaraman.selvaraj@ilink-systems.com>
2025-08-28 16:30:57 +0530

    Update inject-version.js

--- script/inject-version.js
+++ script/inject-version.js
@@ -3,15 +3,10 @@ const path = require('path');
 
 const projectName = process.argv[2];
 const indexPath = path.join('dist', 'apps', projectName, 'index.html');
-let html = fs.readFileSync(indexPath, 'utf8');
-
 const version = process.env.VERSION || require('../../package.json').version;
 
-// Ensure this ID exists in your index.html
-html = html.replace(
-  '</body>',
-  `<script>document.getElementById('app-version').textContent = 'v${version}';</script></body>`
-);
-
+let html = fs.readFileSync(indexPath, 'utf8');
+html = html.replace('__BUILD_VERSION__', `v${version}`);
 fs.writeFileSync(indexPath, html, 'utf8');
+
 console.log(`✅ Injected version ${version} into ${indexPath}`);
