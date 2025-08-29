ee533b35dc45bbd3b7f42d205490b2d0ae51ae89
varshakatre15 <varsha.katre@ilink-systems.com>
2025-08-29 13:45:36 +0530

    Bugfixes

--- apps/agricultura/src/app/application/tramites/221601/components/terceros/terceros.component.ts
+++ apps/agricultura/src/app/application/tramites/221601/components/terceros/terceros.component.ts
@@ -396,8 +396,21 @@ export class TercerosComponent implements OnInit, OnDestroy {
     this.datosPersonales.get('pais')?.setValue(this.paisCatalogo[0].id);
     this.tipoPersonaForm.get('tipoPersona')?.valueChanges.subscribe(value => {
       this.handleTipoPersonaChange(value);
+    
+      this.resetRadioStates();
+      this.inputChecked(value);
     });
     this.updateStoreWithFormData();
+  }
+
+  /**
+   * Resets all radio button states
+   * @memberof TercerosComponent
+   */
+  private resetRadioStates(): void {
+    this.fisica = false;
+    this.moral = false;
+    this.planta = false;
   }
 
   /**
@@ -651,19 +664,18 @@ export class TercerosComponent implements OnInit, OnDestroy {
      * param checkBoxName Nombre del checkbox seleccionado.
      */
   public inputChecked(checkBoxName: string): void {
+    this.resetRadioStates();
+    
     if (checkBoxName === 'fisica') {
       this.fisica = true;
-      this.moral = false;
-      this.planta = false;
     } else if (checkBoxName === 'moral') {
-      this.fisica = false;
       this.moral = true;
-      this.planta = false;
     } else if (checkBoxName === 'planta') {
-      this.fisica = false;
-      this.moral = false;
       this.planta = true;
     }
+    
+    // Update form control value
+    this.tipoPersonaForm.get('tipoPersona')?.setValue(checkBoxName, { emitEvent: false });
   }
 
   /**
@@ -674,6 +686,7 @@ export class TercerosComponent implements OnInit, OnDestroy {
   cambiarRadioFisica(value: string | number): void {
     const VALOR_SELECCIONADO = value as string;
     this.inputChecked(VALOR_SELECCIONADO);
+    this.handleTipoPersonaChange(VALOR_SELECCIONADO);
   }
 
 
