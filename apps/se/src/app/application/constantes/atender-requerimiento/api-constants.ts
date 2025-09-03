9504c8e25345062c30e1fc5eb096f076d1626475
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-09-02 19:49:08 -0600

    CHANGE Paths

--- apps/se/src/app/application/constantes/atender-requerimiento/api-constants.ts
+++ apps/se/src/app/application/constantes/atender-requerimiento/api-constants.ts
@@ -14,16 +14,16 @@ export const TRAMITE = '{tramite}';
  * API para iniciar el atender requerimiento
  * @see https://api-v30.cloud-ultrasist.net/api/tramite-flujo/swagger-ui/index.html#/Atender-Requerimiento/atender-requerimiento-iniciar
  */
-export const API_GET_INICIAR_ATENDER_REQUERIMIENTO = `tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/iniciar`;
+export const API_GET_INICIAR_ATENDER_REQUERIMIENTO = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/iniciar`;
 
 /**
  * API para generar la cadena original 
  * @see https://api-v30.cloud-ultrasist.net/api/tramite-flujo/swagger-ui/index.html#/Atender-Requerimiento/atender-requerimiento-mostrar-firma
  */
-export const API_POST_MOSTRAR_FIRMAR = `tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/mostrar-firma`;
+export const API_POST_MOSTRAR_FIRMAR = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/mostrar-firma`;
 
 /**
  * API parA firmar el atender el requerimiento
  * @see https://api-v30.cloud-ultrasist.net/api/tramite-flujo/swagger-ui/index.html#/Atender-Requerimiento/atender-requerimiento-firmar
  */
-export const API_POST_FIRMAR = `tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/firmar`;
+export const API_POST_FIRMAR = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/firmar`;
