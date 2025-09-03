1c4a48147bd4914daaf98a466163f08a504bd4d5
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-09-02 19:13:44 -0600

    ADD Url acuse guardar

--- apps/se/src/app/application/constantes/atender-requerimiento/api-constants.ts
+++ apps/se/src/app/application/constantes/atender-requerimiento/api-constants.ts
@@ -11,6 +11,11 @@ export const NUMFOLIOTRAMITE = '{numFolioTramite}';
 export const TRAMITE = '{tramite}';
 
 /**
+ * ID de la solicitud que se utilizará en las rutas de la API.
+ */
+export const IDSOLICITUD = '{idSolicitud}';
+
+/**
  * API para iniciar el atender requerimiento
  * @see https://api-v30.cloud-ultrasist.net/api/tramite-flujo/swagger-ui/index.html#/Atender-Requerimiento/atender-requerimiento-iniciar
  */
@@ -27,3 +32,9 @@ export const API_POST_MOSTRAR_FIRMAR = `tramite/${NUMFOLIOTRAMITE}/requerimiento
  * @see https://api-v30.cloud-ultrasist.net/api/tramite-flujo/swagger-ui/index.html#/Atender-Requerimiento/atender-requerimiento-firmar
  */
 export const API_POST_FIRMAR = `tramite/${NUMFOLIOTRAMITE}/requerimiento/atender/firmar`;
+
+/**
+ * API parA firmar generar acuse requerimiento
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Atender-Requerimiento/guardar-acuse-promocion
+ */
+export const API_POST_ACUSE_REQUERIMIENTO= `sat-t${TRAMITE}/solicitud/${IDSOLICITUD}/requerimiento/atender/acuse/guardar`;
