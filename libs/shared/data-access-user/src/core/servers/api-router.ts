fb1d2cb87750363f72a5f3918374491d23ce881d
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-09-08 16:16:54 -0600

    ADD Path del servicio

--- libs/shared/data-access-user/src/core/servers/api-router.ts
+++ libs/shared/data-access-user/src/core/servers/api-router.ts
@@ -497,3 +497,9 @@ export const API_GET_ENVIO_DIGITAL = (NUMFOLIOTRAMITE: string) : string => `tram
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitud/estado-consulta-solicitud
  */
 export const API_GET_TABS = (TRAMITE: string, IDSOLICITUD: string) : string => `sat-t${TRAMITE}/tramite/solicitud/${IDSOLICITUD}/estado`;
+
+/**
+ * API para Consultar los datos del solicitante
+ * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Consulta-Solicitud/estado-consulta-solicitud
+ */
+export const API_GET_DATOS_SOLICITANTE = (IDSOLICITUD: string) : string => `tramite/solicitud/${IDSOLICITUD}/solicitante`;
