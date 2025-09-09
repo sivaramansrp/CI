9063153e70055555f2595b756afd8c33e2a4eb46
banshi-ilink-digital <banshi.bhatt@ilink-systems.com>
2025-09-09 19:15:40 +0530

    fix 24xxx/26xxx/110216

--- apps/cofepris/src/app/application/shared/components/agregar-destinatario-final/agregar-destinatario-final.component.ts
+++ apps/cofepris/src/app/application/shared/components/agregar-destinatario-final/agregar-destinatario-final.component.ts
@@ -8,7 +8,6 @@ import {
 } from '@angular/forms';
 import {
   Catalogo,
-  CatalogoSelectComponent,
   Notificacion,
   NotificacionesComponent,
   Pedimento,
@@ -31,6 +30,7 @@ import {
   Output,
   SimpleChanges,
 } from '@angular/core';
+import {CatalogoSelectComponent} from '@libs/shared/data-access-user/src';
 import { DatosSolicitudService } from '../../services/datos-solicitud.service';
 import { Destinatario } from '../../models/terceros-relacionados.model';
 import { PROCEDIMIENTOS_PARA_NO_CONTRIBUYENTE } from '../../constantes/datos-solicitud.enum';
