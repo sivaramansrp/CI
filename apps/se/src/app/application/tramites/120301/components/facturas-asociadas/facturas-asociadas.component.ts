e42183b4002aa5ad3f9481605e19ac0f5607ad92
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-09-11 18:33:08 -0600

    CHANGE Consulta de datos en la vista

--- apps/se/src/app/application/tramites/120301/components/facturas-asociadas/facturas-asociadas.component.ts
+++ apps/se/src/app/application/tramites/120301/components/facturas-asociadas/facturas-asociadas.component.ts
@@ -151,6 +151,13 @@ export class FormularioAsociacionFacturaComponent implements OnInit, OnDestroy {
   colapsable: boolean = false;
 
   /**
+   * @property {string} labelUnidad - Unidad de medida obtenida del servicio.
+   * Almacena la unidad de medida (kg, lt, etc.) obtenida al consultar los totales de facturas asociadas.
+   * Se utiliza para mostrar la unidad correcta junto a los valores en el formulario.
+   */
+  labelUnidad: string = '';
+
+  /**
    * @property {FormGroup} ConstanciaDelRegistro - El grupo de formularios para los datos del certificado de registro.
    * Formulario secundario que maneja información complementaria relacionada con la constancia del registro.
    * Se utiliza en conjunto con el formulario principal para completar la información requerida.
@@ -621,9 +628,10 @@ export class FormularioAsociacionFacturaComponent implements OnInit, OnDestroy {
       .subscribe({
         next: (response) => {
           if (response.codigo === '00' && response.datos) {
+            this.labelUnidad = response.datos.unidad_label;
             this.formularioAsociacionFactura.patchValue({
-              cantidadFacturasTotal: response.datos.cantidad,
-              metrosCuadradosEquivalentes: response.datos.total
+              cantidadFacturasTotal: response.datos.cantidad_factura,
+              metrosCuadradosEquivalentes: response.datos.total_equivalente
             });
           }
         },
