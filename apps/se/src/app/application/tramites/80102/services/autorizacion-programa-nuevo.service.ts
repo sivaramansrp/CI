8de703d50c281da18d233ed92ab892fa46c38664
shilpa-ilink <shilpa.k@ilink-systems.com>
2025-09-17 13:08:39 +0530

    catalogo api integration in Anexo 1 and Federatarios tab of 801* series

--- apps/se/src/app/application/tramites/80102/services/autorizacion-programa-nuevo.service.ts
+++ apps/se/src/app/application/tramites/80102/services/autorizacion-programa-nuevo.service.ts
@@ -19,6 +19,11 @@ export class AutorizacionProgrmaNuevoService {
  constructor(private readonly http: HttpClient, public tramite80102Store: Tramite80102Store,
   private tramite80102Query:Tramite80102Query,public httpService: HttpCoreService,private complimentosService:ComplimentosService) {
    this.setProcedure();
+   this.setProcedureNo();
+  }
+
+   setProcedureNo(): void {
+    this.complimentosService.setProcedureNo('80102');
   }
 
   /**
@@ -143,8 +148,8 @@ getAllState(): Observable<Tramite80102State> {
  * @param body - Objeto que contiene los datos a enviar en el cuerpo de la solicitud.
  * @returns Observable con la respuesta de la solicitud POST.
  */
-guardarDatosPost(body: any) {
-  return this.httpService.post<any>(PROC_80102.GUARDAR, { body: body });
+guardarDatosPost(body: Record<string, unknown>): Observable<Record<string, unknown>> {
+  return this.httpService.post<Record<string, unknown>>(PROC_80102.GUARDAR, { body: body });
 }
 
 /**
