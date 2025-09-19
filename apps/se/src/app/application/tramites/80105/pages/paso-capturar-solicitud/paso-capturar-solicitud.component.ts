11880f5125cd5fd8ad408e432ed03a2b2c4258f4
navin kumar <navin.kumar@ilink-systems.com>
2025-09-19 18:56:41 +0530

    added some field in anexo1

--- apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
+++ apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
@@ -820,6 +820,8 @@ buildPlantasSubmanufactureras(arr: any[] = [], base: Record<string, any>, data: 
         fraccionValorMonedaAI: item.encabezadoValorEnMonedaAnual,
         fraccionValorProdMI: item.encabezadoValorEnMonedaMensual,
         categoriaFraccion: item.encabezadoCategoria,
+        tipoFraccion:item.encabezadoTipo,
+        umt:item.encabezadoUmt
       });
     });
       
