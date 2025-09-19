1e573479c9eb74bf223b7fd9b9a993393d47ba7b
simrat-ilink <simratpal.gyani@ilink-systems.com>
2025-09-19 17:47:21 +0530

    paso2-changes

--- apps/se/src/app/application/tramites/80104/pages/paso-dos/paso-dos.component.spec.ts
+++ apps/se/src/app/application/tramites/80104/pages/paso-dos/paso-dos.component.spec.ts
@@ -1,42 +1,75 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AlertComponent } from '@ng-mf/data-access-user';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PasoDosComponent } from './paso-dos.component';
-import { TituloComponent } from '@ng-mf/data-access-user';
-import { ToastrModule } from 'ngx-toastr';
-import { Component } from '@angular/core';
-import { of } from 'rxjs';
-@Component({
-  selector: 'anexar-documentos',
-  template: ''
-})
 
-class MockAnexarDocumentosComponent {
-  cargaArchivosEvento = {
-    pipe: () => ({ subscribe: () => {} })
-  };
-}
+import { of, Subject } from 'rxjs';
+import { EventEmitter } from '@angular/core';
+import { DestroyRef } from '@angular/core';
+import { CatalogosService } from '@libs/shared/data-access-user/src';
 
 describe('PasoDosComponent', () => {
   let component: PasoDosComponent;
-  let fixture: ComponentFixture<PasoDosComponent>;
+  let catalogosServiceMock: jest.Mocked<CatalogosService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [PasoDosComponent, MockAnexarDocumentosComponent],
-      imports: [ TituloComponent, AlertComponent, HttpClientTestingModule,ToastrModule.forRoot()  ],
-      providers: [
-        { provide: 'ToastConfig', useValue: {} }
-      ],
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(PasoDosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    catalogosServiceMock = {
+      getCatalogo: jest.fn()
+    } as any;
+
+    component = new PasoDosComponent(catalogosServiceMock);
+    component.cargaArchivosEvento = new EventEmitter<void>();
+    component.regresarSeccionCargarDocumentoEvento = new EventEmitter<void>();
+    jest.spyOn(component.reenviarEvento, 'emit');
+    jest.spyOn(component.reenviarRegresarSeccion, 'emit');
+    jest.spyOn(component.reenviarCargaRealizada, 'emit');
+    jest.spyOn(component.reenviarEventoCarga, 'emit');
   });
 
-  it('debería crear', () => {
-    expect(component).toBeTruthy();
+  it('debería emitir reenviarEvento al activarse cargaArchivosEvento', () => {
+    component.ngOnInit();
+    component.cargaArchivosEvento.emit();
+
+    expect(component.reenviarEvento.emit).toHaveBeenCalled();
+  });
+
+  it('debería emitir reenviarRegresarSeccion al activarse regresarSeccionCargarDocumentoEvento', () => {
+    component.ngOnInit();
+    component.regresarSeccionCargarDocumentoEvento.emit();
+
+    expect(component.reenviarRegresarSeccion.emit).toHaveBeenCalled();
+  });
+
+  it('debería llamar getCatalogo y asignar el catálogo', () => {
+    const fakeCatalog = [
+      { id: 1, nombre: 'Doc A', descripcion: 'Descripción A' }
+    ];
+
+    catalogosServiceMock.getCatalogo.mockReturnValue(of(fakeCatalog));
+
+    component.getTiposDocumentos();
+
+    expect(catalogosServiceMock.getCatalogo).toHaveBeenCalled();
+    expect(component.catalogoDocumentos).toEqual(fakeCatalog);
+  });
+
+  it('debería actualizar cargaRealizada y emitir evento en documentosCargados()', () => {
+    component.documentosCargados(true);
+
+    expect(component.cargaRealizada).toBe(true);
+    expect(component.reenviarCargaRealizada.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('debería emitir evento desde manejarEventoCargaDocumento()', () => {
+    component.manejarEventoCargaDocumento(true);
+
+    expect(component.reenviarEventoCarga.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('debería limpiar destroyed$ al destruir el componente', () => {
+    const completeSpy = jest.spyOn(component['destroyed$'], 'complete');
+    const nextSpy = jest.spyOn(component['destroyed$'], 'next');
+
+    component.ngOnDestroy();
+
+    expect(nextSpy).toHaveBeenCalled();
+    expect(completeSpy).toHaveBeenCalled();
   });
 });
