cc3d9368821696634a8ba4b7420d364cdca2dc78
Vigneshwar N <vigneshwar.n@ilink-systems.com>
2025-09-25 13:41:56 +0530

    Empresas submanufactureras

--- apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
+++ apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
@@ -500,6 +500,13 @@ getValorIndice(e: AccionBoton): void {
       rfc: arr.rfc ?? '',
       domicilioFiscal: arr.domicilioFiscalSolicitante ?? '',
       razonSocial: arr.razonSocial ?? '',
+       datosComplementarios: Array.isArray((ITEM as any)?.datosComplementarios)
+          ? (ITEM as any).datosComplementarios.map((dc:any) => ({
+              idPlantaC: dc.idPlantaC ?? '',
+              idDato: dc.idDato ?? '',
+              amparoPrograma: dc.amparoPrograma ?? '',              
+            }))
+          : []
         });
       });
     });
