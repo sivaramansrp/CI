4cca7046a71414c1ae1e7bd8595e2ee6d39265c2
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-09-29 13:09:32 +0530

    bug fixes and CR

--- apps/se/src/app/application/shared/components/montos-de-inversion/montos-de-inversion.component.ts
+++ apps/se/src/app/application/shared/components/montos-de-inversion/montos-de-inversion.component.ts
@@ -8,7 +8,7 @@ import { ComplimentosService } from '../../services/complimentos.service';
 import { CATALOGO_TIPO, MontoDeInversion } from '../../constantes/complementar-planta.enum';
 import { Notificacion,NotificacionesComponent } from '@ng-mf/data-access-user';
 import { CatalogoSelectComponent } from '@libs/shared/data-access-user/src/tramites/components/catalogo-select/catalogo-select.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { Location } from '@angular/common';
 import { MONTOS_DE_INVERSION } from '../../constantes/montos-de-inversion.enum';
@@ -174,9 +174,9 @@ export class MontosDeInversionComponent implements OnInit {
       .subscribe();
     this.montosDeInversionForm = this.fb.group({
       tipos: [this.solicitudState.tipos],
-      cantidad: [this.solicitudState.cantidad],
-      descripsion: [this.solicitudState.descripsion],
-      mnx: [this.solicitudState.mnx],
+      cantidad: [this.solicitudState.cantidad, [Validators.required , Validators.max(11)]],
+      descripsion: [this.solicitudState.descripsion, [Validators.required , Validators.max(1000)]],
+      mnx: [this.solicitudState.mnx,[ Validators.required , Validators.max(16)]],
     });
   }
   /**
