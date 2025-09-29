bb24b84eb6def620c9f5174663a38caaa67bb179
Sachin-Kumar2330 <sachin.kumar@ilink-systems.com>
2025-09-29 18:29:36 +0530

    fixes

--- apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
+++ apps/se/src/app/application/tramites/80105/pages/paso-capturar-solicitud/paso-capturar-solicitud.component.ts
@@ -210,11 +210,7 @@ export class PasoCapturarSolicitudComponent implements OnDestroy, OnInit {
     private consultaQuery: ConsultaioQuery,
     private servicioDeFormularioService: ServicioDeFormularioService,
   ) {
-    this.tramiteQuery.FormaValida$.pipe(
-      takeUntil(this.destroyNotifier$)
-    ).subscribe((res) => {
-      
-    });
+    // Constructor vacío: La inicialización se realizará en métodos específicos según sea necesario.
   }
 
 
