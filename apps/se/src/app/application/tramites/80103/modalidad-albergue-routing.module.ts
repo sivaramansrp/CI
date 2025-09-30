b5616b97eba29f50acd781ba29b3259ebde93a89
binaydas8 <binaykumar.das@ilink-systems.com>
2025-09-30 15:21:43 +0530

    bugfix
    bugfix

--- apps/se/src/app/application/tramites/80103/modalidad-albergue-routing.module.ts
+++ apps/se/src/app/application/tramites/80103/modalidad-albergue-routing.module.ts
@@ -1,13 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
+import { IniciarTramiteResolver } from '@libs/shared/data-access-user/src';
 import { NgModule } from '@angular/core';
 import { PasoCapturarSolicitudComponent } from './pages/paso-capturar-solicitud/paso-capturar-solicitud.component';
-import { IniciarTramiteResolver } from '@libs/shared/data-access-user/src';
 
 const ROUTES: Routes = [
    
     {
       canActivate: [IniciarTramiteResolver],
-          resolve: { iniciarResolverData: IniciarTramiteResolver },
           data: {
             iniciarConfig: {
               procedureId: '80103'
