cca1a7f7ff4a3156ea9bae581c2c519ca2a40bad
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-10-01 17:24:56 +0530

    Created shared service for Destinatario component

---
+++ apps/se/src/app/application/shared/services/destinatario.service.ts
@@ -0,0 +1,38 @@
+import { CATALOGO_MEDIO_TRANSPORTE, CATALOGO_PAISES, COMUN_URL, Catalogo } from '@libs/shared/data-access-user/src';
+import { BaseResponse } from '@libs/shared/data-access-user/src/core/models/shared/base-response.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+/**
+ * Servicio para gestionar las solicitudes relacionadas con los catálogos y datos del trámite 110201.
+ */
+@Injectable({
+    providedIn: 'root',
+})
+export class DestinatarioService {
+
+
+    host: string;
+    tramite: string = '110202';
+
+    constructor(private http: HttpClient) {
+        this.host = `${COMUN_URL.BASE_URL}`
+    }
+
+    getPaisDestino(tramite: string): Observable<BaseResponse<Catalogo[]>> {
+        const ENDPOINT = `${this.host}${CATALOGO_PAISES(tramite)}`;
+        return this.http.get<BaseResponse<Catalogo[]>>(ENDPOINT);
+    }
+
+     getTransporte(tramite: string): Observable<BaseResponse<Catalogo[]>> {
+        const ENDPOINT = `${this.host}${CATALOGO_MEDIO_TRANSPORTE(tramite)}`;
+        return this.http.get<BaseResponse<Catalogo[]>>(ENDPOINT);
+      }
+
+    //  nicosCatalogo(tramite: string, claveFraccion: string): Observable<BaseResponse<Catalogo[]>> {
+    //     const ENDPOINT = `${this.host}${CATALOGO_NICO(tramite, claveFraccion)}`;
+    //     return this.http.get<BaseResponse<Catalogo[]>>(ENDPOINT);
+    //   }
+
+}
