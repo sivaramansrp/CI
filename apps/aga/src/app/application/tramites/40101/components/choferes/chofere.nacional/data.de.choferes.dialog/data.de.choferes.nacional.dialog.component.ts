702e9b463029b80f616d2fd00f4ecc7b5b4e50d4
shahil <mshahilkv@gmail.com>
2025-10-07 20:27:58 +0530

    loader implemented for client demo

--- apps/aga/src/app/application/tramites/40101/components/choferes/chofere.nacional/data.de.choferes.dialog/data.de.choferes.nacional.dialog.component.ts
+++ apps/aga/src/app/application/tramites/40101/components/choferes/chofere.nacional/data.de.choferes.dialog/data.de.choferes.nacional.dialog.component.ts
@@ -71,6 +71,8 @@ export class DatosDeChoferesNacionalDialogComponent implements OnInit, OnDestroy
    */
   isEditando: boolean = false;
 
+  isLoading: boolean = false
+
   /**
    * Índice del registro que se está editando.
    */
@@ -492,12 +494,15 @@ export class DatosDeChoferesNacionalDialogComponent implements OnInit, OnDestroy
    * Obtener datos para el conductor nacional usando CURP y RFC
    */
   ObtenerDatosConductorNacional(): void {
+    this.isLoading = true
     const CURP_VALUE = this.formChoferes.get('curp')?.value;
     const RFC_VALUE = this.formChoferes.get('rfc')?.value;
     this.modificacarTerrestreService.buscarChoferNacional(CURP_VALUE, RFC_VALUE).subscribe((data: ApiResponseChofer) => {
       if (data && data.datos) {
         const DATOS = data.datos;
+        this.isLoading = false
         this.completarFormularioConDatos(DATOS);
+        this.onEstadoChange();
       }
     });
   }
