af6a7616534804d484d464df789095b06fd24890
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-10-08 16:51:55 +0530

    Added editor button functionality in  empleados popup

--- apps/se/src/app/application/shared/components/empleados/empleados.component.ts
+++ apps/se/src/app/application/shared/components/empleados/empleados.component.ts
@@ -49,6 +49,11 @@ export class EmpleadosComponent implements OnInit {
    * @property {Notificacion} nuevaNotificacion
    */
   public nuevaNotificacion!: Notificacion;
+/**
+ * Nueva notificación para mostrar mensajes al usuario en el editor.
+ * @property {Notificacion} nuevaNotificacionEditor
+ */
+  public nuevaNotificacionEditor!: Notificacion;
   /**
  * Estado actual de la solicitud, utilizado para inicializar y gestionar los datos del formulario de empleados.
  * @property {ComplementarState} solicitudState
@@ -119,6 +124,11 @@ export class EmpleadosComponent implements OnInit {
    */
   @Output() cerrarPopup = new EventEmitter<void>();
   
+  /**
+   * Índice de la capacidad instalada que se está editando actualmente.
+   */
+  editingIndex: number | null = null;
+
 /**  
  * Evento de salida que emite una lista de empleados directos al componente padre.
  */
@@ -306,10 +316,11 @@ export class EmpleadosComponent implements OnInit {
    * Actualiza el estado del almacén con la razón social proporcionada.  
    */
   agregar(): void {
+    let TABLA_VALOR: Directos | null = null;
     if (this.empleadosForm.valid) {
       const DIRECTOS = this.empleadosForm.get('directos')?.value;
       const INDIRECTOS = this.empleadosForm.get('indirectos')?.value;
-      const TABLA_VALOR: Directos = {
+    TABLA_VALOR = {
         PLANTA: this.empleadosForm.get('directo')?.value,
         TOTAL: this.empleadosForm.get('totalDeEmpleados')?.value,
         DIRECTOS: DIRECTOS ? this.empleadosForm.get('directo')?.value : '',
@@ -323,10 +334,16 @@ export class EmpleadosComponent implements OnInit {
         RFC: INDIRECTOS ? this.empleadosForm.get('rfcEmpresa')?.value : '',
         RAZON_SOCIAL: INDIRECTOS ? this.empleadosForm.get('razonSocial')?.value : ''
       };
-
-      this.directosDatos = [...this.directosDatos, TABLA_VALOR];
-     
     }
+     if (this.editingIndex !== null && this.editingIndex > -1 && TABLA_VALOR !== null) {
+      this.directosDatos[this.editingIndex] = TABLA_VALOR;
+      this.directosDatos = [...this.directosDatos];
+      this.editingIndex = null;
+     
+    } else if (TABLA_VALOR !== null) {  
+      this.directosDatos = [...this.directosDatos, TABLA_VALOR];
+    }
+    this.selectedDirectosDatos=[];
     this.agregarValidacion();
   }
   /**
@@ -448,5 +465,53 @@ export class EmpleadosComponent implements OnInit {
       this.directosDatos = [...this.directosDatos];
     }
   }
+/**
+ * Maneja la edición de empleados directos.
+ * 
+ * Si hay exactamente un empleado seleccionado en `selectedDirectosDatos`, llena el formulario
+ * con los datos de ese empleado y establece `editingIndex` al índice correspondiente en `directosDatos`.
+ * Si no hay empleados seleccionados, muestra una notificación de advertencia.
+ */
+   editarDirectos(): void {
+    if (this.selectedDirectosDatos?.length === 1) {
+      const SELECTED = this.selectedDirectosDatos[0];
+      this.empleadosForm.patchValue({
+        directo: SELECTED.PLANTA,
+        totalDeEmpleados: SELECTED.TOTAL,
+        directos: SELECTED.DIRECTOS,
+        cedula: SELECTED.CEDULA_DE_CUOTAS,
+        fechaCedula: SELECTED.FECHA_DE_CEDULA,
+        indirectos: SELECTED.INDIRECTOS,
+        contrato: SELECTED.CONTRATO,
+        objeto: SELECTED.OBJETO_DEL_CONTRATO_DEL_SERVICIO,
+        fechaFirma: SELECTED.FECHA_FIRMA,
+        fechaFinVigencia: SELECTED.FECHA_FIN_VIGENCIA,
+        rfcEmpresa: SELECTED.RFC,
+        razonSocial: SELECTED.RAZON_SOCIAL
+      });
+
+      setTimeout(() => {
+        const NATIVE_EL = document.querySelector(
+          'input[formControlName="totalDeEmpleados"]'
+        ) as HTMLElement | null;
+        if (NATIVE_EL) {
+          NATIVE_EL.focus();
+        }
+      }, 0);
+      this.editingIndex = this.directosDatos.findIndex(row => row === SELECTED);
+    }
+    else if (!this.selectedDirectosDatos || this.selectedDirectosDatos.length === 0) {
+      this.nuevaNotificacionEditor = {
+        tipoNotificacion: 'alert',
+        categoria: 'warning',
+        modo: 'action',
+        titulo: '',
+        mensaje: 'Debe elegir un registro de complemento para actualizar.',
+        cerrar: true,
+        txtBtnAceptar: 'Aceptar',
+        txtBtnCancelar: '',
+      };
+    }
+}
 
 }
