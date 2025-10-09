76da480bee054a31faf04fc2fa98bff7a4d9ebf6
selvakumarilink <selvakumar.k@ilink-systems.com>
2025-10-09 17:58:57 +0530

    Updated code

---
+++ apps/se/src/app/application/tramites/110207/components/certificado-origen/certificado-origen.component.ts
@@ -0,0 +1,411 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  Catalogo,
+  ConfiguracionColumna,
+  SeccionLibQuery,
+  SeccionLibState,
+} from '@libs/shared/data-access-user/src';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Observable, Subject, delay, map, of, takeUntil } from 'rxjs';
+import {
+  Solicitud110207State,
+  Tramite110207Store,
+} from '../../state/Tramite110207.store';
+import { CARGA_MERCANCIA_EXPORT } from '../../../../shared/constantes/modificacion.enum';
+import { CertificadoDeOrigenComponent } from '../../../../shared/components/certificado-de-origen/certificado-de-origen.component';
+import { CommonModule } from '@angular/common';
+import { ConsultaioQuery } from '@ng-mf/data-access-user';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Mercancia } from '../../../../shared/models/modificacion.enum';
+import { MercanciaComponent } from '../../../../shared/components/mercancia/mercancia.component';
+import { Modal } from 'bootstrap';
+import { PeruCertificadoService } from '../../../110205/services/peru-certificado.service';
+import { Tramite110207Query } from '../../state/Tramite110207.query';
+
+@Component({
+  selector: 'app-certificado-origen',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    CertificadoDeOrigenComponent,
+    MercanciaComponent,
+  ],
+  templateUrl: './certificado-origen.component.html',
+  styleUrl: './certificado-origen.component.css',
+})
+export class CertificadoOrigenComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
+  /**
+   * @descripcion
+   * Lista de estados disponibles.
+   */
+  estado: Catalogo[] = [];
+
+  /**
+   * @descripcion
+   * Lista de países disponibles.
+   */
+  pais: Catalogo[] = [];
+
+  /**
+   * @descripcion
+   * Lista de datos disponibles relacionados con mercancías.
+   */
+  disponiblesDatos: Mercancia[] = [];
+
+  /**
+   * @descripcion
+   * Indica si el operador está activo.
+   */
+  operador: boolean = false;
+
+  /**
+   * @descripcion
+   * Datos seleccionados para modificación.
+   */
+  datosSeleccionados!: Mercancia;
+
+  /**
+   * @descripcion
+   * Instancia del modal de modificación.
+   */
+  modalInstance!: Modal;
+
+  /**
+   * @descripcion
+   * Evento para indicar si se seleccionó una fila en la tabla.
+   */
+  tablaSeleccionEvent: boolean = true;
+
+  /**
+   * @descripcion
+   * Indica si el campo de mercancías está activo.
+   */
+  cargoDeMercancias: boolean = true;
+
+  /**
+   * @descripcion
+   * Indica si hay mercancías disponibles.
+   */
+  mercanciasDisponibles: boolean = true;
+
+  /**
+   * @descripcion
+   * Observable para los datos de la tabla.
+   */
+  datosTabla$: Mercancia[] = [];
+
+  /**
+   * @descripcion
+   * Observable para los datos de la tabla.
+   */
+  datosTablaUno: Mercancia[] = [];
+
+  /**
+   * @descripcion
+   * Valores actuales del formulario de certificado.
+   */
+  formCertificadoValues!: { [key: string]: unknown };
+
+  /**
+   * @descripcion
+   * Estado actual del certificado.
+   */
+  private certificadoState!: Solicitud110207State;
+
+  /**
+   * @descripcion
+   * Notificador para gestionar la destrucción de suscripciones.
+   */
+  private destroyNotifier$: Subject<void> = new Subject();
+
+  /**
+   * @descripcion
+   * Estado actual de la sección.
+   */
+  private seccionState!: SeccionLibState;
+
+  /**
+   * @descripcion
+   * Indica si el formulario se encuentra en modo solo lectura.
+   */
+  esFormularioSoloLectura: boolean = false;
+
+  /**
+   * @property {string} idProcedimiento
+   * @description Identificador del procedimiento, utilizado para la gestión del trámite.
+   */
+  public idProcedimiento = 110207;
+
+  /**
+   * Configuración de las columnas de la tabla de carga de mercancías.
+   * Contiene la definición de cada columna utilizada para mostrar los datos de las mercancías.
+   *
+   * @type {ConfiguracionColumna<Mercancia>[]}
+   */
+  cargaMercanciaConfiguracionTabla: ConfiguracionColumna<Mercancia>[] = CARGA_MERCANCIA_EXPORT;
+
+  /**
+   * Indica si la información de la mercancía proviene del listado de mercancías disponibles.
+   *
+   * Cuando es `true`, significa que el usuario seleccionó la mercancía desde una lista precargada.
+   * Cuando es `false`, la mercancía fue ingresada manualmente por el usuario.
+   *
+   * @type {boolean}
+   * @default false
+   */
+  fromMercanciasDisponibles: boolean = false;
+
+  /**
+   * @descripcion
+   * Referencia al elemento del modal de modificación.
+   */
+  @ViewChild('modifyModal', { static: false }) modifyModal!: ElementRef;
+
+  /**
+   * @descripcion
+   * Constructor que inicializa los servicios y dependencias requeridas.
+   * @param fb - Instancia de FormBuilder para gestionar formularios.
+   * @param peruCertificadoService - Servicio para obtener datos relacionados con el certificado.
+   * @param store - Almacén para gestionar el estado del formulario de certificado.
+   * @param query - Consulta para obtener el estado del formulario.
+   * @param seccionStore - Almacén para gestionar el estado de la sección.
+   * @param seccionQuery - Consulta para obtener el estado de la sección.
+   */
+  constructor(
+    private fb: FormBuilder,
+    private peruCertificadoService: PeruCertificadoService,
+    private store: Tramite110207Store,
+    private query: Tramite110207Query,
+    private seccionQuery: SeccionLibQuery,
+    public consultaQuery: ConsultaioQuery
+  ) {
+    this.query.selectSolicitud$
+      .pipe(takeUntil(this.destroyNotifier$), delay(100))
+      .subscribe((estado) => {
+        //  this.formCertificadoValues = estado;
+      });
+  }
+
+  /**
+   * @descripcion
+   * Hook del ciclo de vida que se llama después de inicializar el componente.
+   * Obtiene los datos iniciales para el formulario.
+   */
+  ngOnInit(): void {
+    this.seccionQuery.selectSeccionState$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((seccionState) => {
+          this.seccionState = seccionState;
+        })
+      )
+      .subscribe();
+
+    this.consultaQuery.selectConsultaioState$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((seccionState) => {
+          this.esFormularioSoloLectura = seccionState.readonly;
+        })
+      )
+      .subscribe();
+
+    this.query.selectSolicitud$
+      .pipe(
+        takeUntil(this.destroyNotifier$),
+        map((state: Solicitud110207State) => {
+          this.certificadoState = state;
+           this.datosTabla$ = state.mercanciaTabla;
+           this.datosTablaUno = state.disponiblesDatos;
+        })
+      )
+      .subscribe();
+
+    this.estadoOpcion();
+    this.paisOpcion();
+  }
+
+  /**
+   * @descripcion
+   * Actualiza el almacén con los datos del formulario de certificado.
+   * @param event - Objeto que contiene el nombre del grupo de formulario, el campo, el valor y el nombre del estado del almacén.
+   */
+  setValoresStore(event: {
+    formGroupName: string;
+    campo: string;
+    valor: undefined;
+    storeStateName: string;
+  }): void {
+    const { campo: CAMPO, valor: VALOR } = event;
+     this.store.setFormCertificadoGenric({ [CAMPO]: VALOR });
+  }
+
+  /**
+   * @descripcion
+   * Obtiene la lista de estados disponibles.
+   */
+  estadoOpcion(): void {
+    this.peruCertificadoService
+      .obtenerMenuDesplegable('estados.json')
+      .pipe(takeUntil(this.destroyNotifier$))
+      .subscribe({
+        next: (data) => {
+          this.estado = data as Catalogo[];
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('Error al obtener los datos:', error);
+          this.estado = [];
+        },
+      });
+  }
+
+  /**
+   * @descripcion
+   * Obtiene la lista de países disponibles.
+   */
+  paisOpcion(): void {
+    this.peruCertificadoService
+      .obtenerMenuDesplegable('pais.json')
+      .pipe(takeUntil(this.destroyNotifier$))
+      .subscribe({
+        next: (data) => {
+          this.pais = data as Catalogo[];
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('Error al obtener los datos:', error);
+          this.pais = [];
+        },
+      });
+  }
+
+  /**
+   * @descripcion
+   * Obtiene los datos disponibles relacionados con mercancías.
+   */
+  conseguirDisponiblesDatos(): void {
+    this.peruCertificadoService
+      .obtenerTablaDatos('disponibles-datos.json')
+      .pipe(takeUntil(this.destroyNotifier$))
+      .subscribe({
+        next: (response: Mercancia[]) => {
+          if (response && Array.isArray(response)) {
+            this.disponiblesDatos = response as Mercancia[];
+             this.store.setDisponsiblesDatos(this.disponiblesDatos);
+          } else {
+            this.disponiblesDatos = [];
+          }
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('Error al obtener los datos:', error);
+        },
+      });
+  }
+
+  /**
+   * @descripcion
+   * Actualiza el almacén con el estado seleccionado.
+   * @param estado - El estado seleccionado.
+   */
+  tipoEstadoSeleccion(estado: Catalogo): void {
+     this.store.setEstado(estado);
+  }
+
+  /**
+   * @descripcion
+   * Actualiza el almacén con el bloque seleccionado.
+   * @param estado - El bloque seleccionado.
+   */
+  tipoSeleccion(estado: Catalogo): void {
+     this.store.setBloque([estado]);
+  }
+
+  /**
+   * @descripcion
+   * Abre el modal de modificación con los datos seleccionados.
+   * @param disponiblesDatos - Los datos seleccionados para modificación.
+   */
+  abrirModificarModal(
+    disponiblesDatos: Mercancia,
+    fromMercanciasDisponibles: boolean
+  ): void {
+    this.datosSeleccionados = disponiblesDatos;
+    this.fromMercanciasDisponibles = fromMercanciasDisponibles;
+    //  this.store.setFormMercancia({ ...disponiblesDatos });
+    if (this.modalInstance) {
+      this.modalInstance.show();
+    }
+  }
+
+  /**
+   * @descripcion
+   * Cierra el modal de modificación.
+   */
+  cerrarModificarModal(): void {
+    if (this.modalInstance) {
+      this.tablaSeleccionEvent = true;
+      this.modalInstance.hide();
+    }
+  }
+
+  /**
+   * Envía los datos de una mercancía al estado global (store) para su almacenamiento o actualización.
+   *
+   * Este método recibe un objeto de tipo `Mercancia`, lo encapsula dentro de un arreglo
+   * y lo pasa al método `setmercanciaTabla` del store, con el fin de actualizar la lista
+   * de mercancías en el estado de la aplicación.
+   *
+   * @param {Mercancia} evento - Objeto que contiene la información de la mercancía seleccionada o editada.
+   */
+  emitmercaniasDatos(evento: Mercancia): void {
+     this.store.setmercanciaTabla([evento]);
+  }
+
+  /**
+   * @descripcion
+   * Hook del ciclo de vida que se llama después de que la vista del componente se haya inicializado.
+   * Inicializa el modal de modificación.
+   */
+  ngAfterViewInit(): void {
+    if (this.modifyModal) {
+      this.modalInstance = new Modal(this.modifyModal.nativeElement);
+    }
+  }
+
+  /**
+   * @descripcion
+   * Actualiza el almacén con el estado de validación del formulario.
+   * @param valida - El estado de validación del formulario.
+   */
+  setFormValida(valida: boolean): void {
+     this.store.setFormValida({ certificado: valida });
+  }
+
+  /**
+   * @descripcion
+   * Método que actualiza el observable `datosTabla$` con un nuevo arreglo de objetos de tipo `Mercancia`.
+   * @param {Mercancia[]} evento - Arreglo de objetos de tipo `Mercancia` que han sido seleccionados o procesados.
+   */
+  guardarClicado(evento: Mercancia[]): void {
+    this.datosTabla$ = evento;
+  }
+
+  /**
+   * @descripcion
+   * Hook del ciclo de vida que se llama cuando el componente se destruye.
+   * Limpia los recursos y suscripciones.
+   */
+  ngOnDestroy(): void {
+    this.destroyNotifier$.next();
+    this.destroyNotifier$.complete();
+  }
+}
