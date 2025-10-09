40ea08ad5539952a6e3510fa3808442aa8cfd348
Jamuna_Sri <jamunasri.s@purpleslate.in>
2025-10-09 16:46:42 +0530

    testcase coverage

--- apps/se/src/app/application/tramites/80208/component/cambio-de-modalidad/cambio-de-modalidad.component.ts
+++ apps/se/src/app/application/tramites/80208/component/cambio-de-modalidad/cambio-de-modalidad.component.ts
@@ -774,7 +774,7 @@ export class CambioDeModalidadComponent implements OnInit, OnDestroy {
    */
 
   seleccionarDesplegableServicios(): void {
-    const ID = this.cambioDeModalidadForm.value.serviciosImmx.toString();
+    const ID = this.cambioDeModalidadForm.value?.serviciosImmx.toString() || '';
     this.serviciosImmexServId = ID;
     this.cambioModalidadStore.actualizarEstado({
       serviciosImmx: ID
