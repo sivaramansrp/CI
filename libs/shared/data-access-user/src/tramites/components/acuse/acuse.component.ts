23075c616aab8d188571144b4ff4285d93cff890
alberto ubamea <atorres@toggle.com.mx>
2025-10-22 12:52:38 -0700

    se ajusta componente acuse
    se ajusta para manejar los 3 diferentes subtramites para el tramite 231000

--- libs/shared/data-access-user/src/tramites/components/acuse/acuse.component.ts
+++ libs/shared/data-access-user/src/tramites/components/acuse/acuse.component.ts
@@ -23,7 +23,7 @@ import { AcusesService } from '../../../core/services/120301/acuses.service';
 import { DocumentoService } from '../../..';
 import { DocumentosRequest } from '../../../core/models/shared/documentos-request.model';
 import { DocumentosService } from '../../../core/services/shared/documentos.service';
-import { DocumentosT231001Service } from '../../../core/services/shared/documentos-t231001.service';
+import { DocumentosT2310Service } from '../../../core/services/shared/documentos-t231001.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -119,7 +119,7 @@ export class AcuseComponent implements OnChanges, OnDestroy {
     private documentosService: DocumentoService,
     private route: ActivatedRoute,
     private documentosService130118: DocumentosService,
-    private documentosService231001: DocumentosT231001Service,
+    private documentosService2310: DocumentosT2310Service,
     private acuseDetalleService: AcuseDetalleService,
     private acusesService: AcusesService
   ) {}
@@ -156,8 +156,9 @@ export class AcuseComponent implements OnChanges, OnDestroy {
       this.url === 'pexim' ||
       [
         80101, 80102, 80103, 80104, 80105, 80202, 80203, 80205, 80206, 80207,
-        80208, 80210, 80211, 110101, 120301, 110201, 110202, 110203, 110204, 110205,
-        110207, 110208, 110209, 110210, 110212, 110214, 110216, 110217, 110218, 110219, 110221, 110222, 110223,130102
+        80208, 80210, 80211, 110101, 120301, 110201, 110202, 110203, 110204,
+        110205, 110207, 110208, 110209, 110210, 110212, 110214, 110216, 110217,
+        110218, 110219, 110221, 110222, 110223, 130102,
       ].includes(this.procedure)
     ) {
       this.documentosService130118
@@ -191,8 +192,8 @@ export class AcuseComponent implements OnChanges, OnDestroy {
           },
           error: (err) => console.error('Error:', err),
         });
-    } else if (this.url === 'aviso-de-materiales') {
-      this.descargarDocumentoTramite231001();
+    } else if ([231001, 231002, 231003].includes(this.procedure)) {
+      this.descargarDocumentoTramite2310();
     } else {
       const BODY: DocumentosRequest = {
         tipo_dependencia: 'AGA',
@@ -338,8 +339,6 @@ export class AcuseComponent implements OnChanges, OnDestroy {
     this.base64Archivos(url, 'descargar');
   }
 
-
-
   /**
    * Método que maneja la navegación al salir del componente.
    *
@@ -355,19 +354,19 @@ export class AcuseComponent implements OnChanges, OnDestroy {
   }
 
   /**
-   * Descarga los documentos asociados al trámite 231001 (Aviso de Materiales).
+   * Descarga los documentos asociados a los trámites 2310XX (Aviso de Materiales,Aviso de reciclaje y aviso de retorno).
    */
-  private descargarDocumentoTramite231001(): void {
+  private descargarDocumentoTramite2310(): void {
     const ID = this.idSolicitud.toString();
 
-  /**
-   * Observable que guarda el acuse de la solicitud y luego obtiene su vista previa.
-   */
-    const ACUSE_SOLICITUD = this.documentosService231001
+    /**
+     * Observable que guarda el acuse de la solicitud y luego obtiene su vista previa.
+     */
+    const ACUSE_SOLICITUD = this.documentosService2310
       .guardarDocumento(ID, this.procedure, true)
       .pipe(
         switchMap(() =>
-          this.documentosService231001.vistaPreviaDocumento(
+          this.documentosService2310.vistaPreviaDocumento(
             ID,
             this.procedure,
             true
@@ -375,14 +374,14 @@ export class AcuseComponent implements OnChanges, OnDestroy {
         )
       );
 
-  /**
-   * Observable que guarda la constancia de la solicitud y luego obtiene su vista previa.
-   */
-    const CONSTANCIA_SOLICITUD = this.documentosService231001
+    /**
+     * Observable que guarda la constancia de la solicitud y luego obtiene su vista previa.
+     */
+    const CONSTANCIA_SOLICITUD = this.documentosService2310
       .guardarDocumento(ID, this.procedure, false)
       .pipe(
         switchMap(() =>
-          this.documentosService231001.vistaPreviaDocumento(
+          this.documentosService2310.vistaPreviaDocumento(
             ID,
             this.procedure,
             false
@@ -390,9 +389,9 @@ export class AcuseComponent implements OnChanges, OnDestroy {
         )
       );
 
-  /**
-   * Se utiliza forkJoin para ejecutar ambos observables en paralelo y esperar a que ambos completen.
-   */
+    /**
+     * Se utiliza forkJoin para ejecutar ambos observables en paralelo y esperar a que ambos completen.
+     */
     forkJoin([ACUSE_SOLICITUD, CONSTANCIA_SOLICITUD])
       .pipe(
         takeUntil(this.destroyed$),
