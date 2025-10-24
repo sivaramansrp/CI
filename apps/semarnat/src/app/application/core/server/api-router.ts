ea2884e69f1109fad72fffb37a8ca99a658c04d3
alberto ubamea <atorres@toggle.com.mx>
2025-10-24 14:44:58 -0700

    ajuste de conflictos en merge

--- apps/semarnat/src/app/application/core/server/api-router.ts
+++ apps/semarnat/src/app/application/core/server/api-router.ts
@@ -1,15 +1,3 @@
-import { IDSOLICITUD } from '../../constantes/230301/api-constants';
-
-
-/**
- * URLs de API para guardar la solicitud de desistimiento para los tramites T231002 y T231003.
- */
-export const API_POST_GUARDAR_SOLICITUD = 'sat-t230301/solicitud/guardar';
-
-/**
- * URL de API para generar la cadena original del trámite 230301
- */
-export const API_POST_CADENA_ORIGINAL = `sat-t230301/solicitud/${IDSOLICITUD}/genera-cadena-original`;
 
 
 /**
