361d4f70105ec4d404f09ee93ff09b7522c3b000
Renuga <renugadevim@ilink-systems.com>
2025-10-24 11:48:05 +0530

    Representate legal exportador Shared component issue fix

--- apps/se/src/app/application/shared/components/representante-legal-exportador/representante-legal-exportador.component.ts
+++ apps/se/src/app/application/shared/components/representante-legal-exportador/representante-legal-exportador.component.ts
@@ -19,10 +19,10 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { PAIS_CATALOGO, REPRESENTATE_LEGAL_EXPORTADOR_CONFIG } from '../../constantes/representate-legal-exportador-config.enum';
 import { Subject, takeUntil } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FieldConfig } from '../../models/representate-legal-exportador.model';
-import { REPRESENTATE_LEGAL_EXPORTADOR_CONFIG } from '../../constantes/representate-legal-exportador-config.enum';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ValidarInicialmenteCertificadoService } from '../../../tramites/110221/services/validar-inicialmente-certificado.service';
 
@@ -141,7 +141,9 @@ export class RepresentanteLegalExportadorComponent
     this.form = this.fb.group({});
     this.crearFormulario();
 
-    this.obtenerPaisDestinoCatalogo();
+    if (PAIS_CATALOGO.includes(this.procedimiento)) {
+      this.obtenerPaisDestinoCatalogo();
+    }
   }
 
    /** Método público para marcar todos los campos como tocados y mostrar errores */
