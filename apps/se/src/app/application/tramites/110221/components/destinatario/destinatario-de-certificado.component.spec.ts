79465e607c286e747098ca6e8f2e59b04adcd7a8
sravanimeegada <sravani.meegada@ilink-systems.com>
2025-10-28 17:09:11 +0530

    testing

--- apps/se/src/app/application/tramites/110221/components/destinatario/destinatario-de-certificado.component.spec.ts
+++ apps/se/src/app/application/tramites/110221/components/destinatario/destinatario-de-certificado.component.spec.ts
@@ -1,11 +1,20 @@
-import { DestinatarioComponent } from './destinatario.component';
+//@ts-nocheck
+import { Tramite110221Store } from '../../estados/tramite110221.store';
+
+import { DestinatarioDeCertificadoComponent } from './destinatario-de-certificado.component';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
-describe('DestinatarioComponent', () => {
-  let component: DestinatarioComponent;
+describe('DestinatarioDeCertificadoComponent', () => {
+  let component: DestinatarioDeCertificadoComponent;
+  let store: Tramite110221Store;
+
 
   beforeEach(() => {
     // Create mocks for injected services
+    providers: [
+        FormBuilder,
+        Tramite110221Store,
+      ]
     const mockStore = { 
       setRepresentanteLegalForm: jest.fn(), 
       setDomicilioForm: jest.fn(), 
@@ -17,7 +26,7 @@ describe('DestinatarioComponent', () => {
     const mockValidacionesService = { isValid: jest.fn(() => true) };
     const mockValidarInicialmenteCertificadoService = { getPaisDestino: jest.fn(() => ({ pipe: jest.fn(() => ({ subscribe: jest.fn() })) })) };
 
-    component = new DestinatarioComponent(
+    component = new DestinatarioDeCertificadoComponent(
       mockValidarInicialmenteCertificadoService as any,
       new FormBuilder(),
       mockStore as any,
@@ -25,6 +34,44 @@ describe('DestinatarioComponent', () => {
       mockValidacionesService as any,
       mockConsultaioQuery as any
     );
+  });
+
+
+  it('should call setFormValida', () => {
+    component.setFormValida(true);
+   
+  });
+
+
+  it('should call setFormValidaExportador', () => {
+    component.setFormValidaExportador(false);
+  });
+
+
+  it('should call setFormValidaDestinatario', () => {
+    component.setFormValidaDestinatario(true);
+  });
+
+
+  it('should call validateAllForms with all valid/invalid branches', () => {
+    const validForm = new FormGroup({});
+    Object.defineProperty(validForm, 'valid', { get: () => true });
+    const invalidForm = new FormGroup({});
+    Object.defineProperty(invalidForm, 'valid', { get: () => false });
+    component.destinatarioComponent = { markAllFieldsTouched: jest.fn(), formDestinatario: validForm } as any;
+    component.datosDelDestinatarioComponent = { markAllFieldsTouched: jest.fn(), formDatosDelDestinatario: validForm } as any;
+    component.representanteLegalExportadorComponent = { markAllFieldsTouched: jest.fn(), form: validForm } as any;
+    expect(component.validateAllForms()).toBe(true);
+    component.destinatarioComponent = { markAllFieldsTouched: jest.fn(), formDestinatario: invalidForm } as any;
+    expect(component.validateAllForms()).toBe(false);
+    component.destinatarioComponent = undefined;
+    expect(component.validateAllForms()).toBe(false);
+    component.destinatarioComponent = { markAllFieldsTouched: jest.fn(), formDestinatario: validForm } as any;
+    component.datosDelDestinatarioComponent = undefined;
+    expect(component.validateAllForms()).toBe(false);
+    component.datosDelDestinatarioComponent = { markAllFieldsTouched: jest.fn(), formDatosDelDestinatario: validForm } as any;
+    component.representanteLegalExportadorComponent = undefined;
+    expect(component.validateAllForms()).toBe(false);
   });
 
   it('should create', () => {
