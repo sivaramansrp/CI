45df9147409f4d2198e64f687e1977b7a41027c1
Kevin Martinez Bautista <kmartinezb@ultrasist.com.mx>
2025-10-29 13:14:03 -0600

    FIX se quita leyenda

--- apps/aga/src/app/application/constantes/autorizar-dictamen/api-constants.ts
+++ apps/aga/src/app/application/constantes/autorizar-dictamen/api-constants.ts
@@ -13,37 +13,37 @@ export const TRAMITE = '{tramite}';
 export const NUMFOLIOTRAMITE = '{numFolioTramite}';
 
 /**
- * API para iniciar requerimiento trámite 130118
+ * API para iniciar requerimiento trámite 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Requerimiento/iniciar-generar-requerimiento
  */
 export const API_POST_INICIAR_AUTORIZAR_DICTAMEN = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/dictamen/autorizar/iniciar`;
 
 /**
- * API para iniciar requerimiento trámite 130118
+ * API para iniciar requerimiento trámite 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Requerimiento/iniciar-generar-requerimiento
  */
 export const API_POST_MOSTRAR_FIRMAR = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/autorizar/mostrar-firmar`;
 
 /**
- * API para iniciar requerimiento trámite 130118
+ * API para iniciar requerimiento trámite 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Generar-Requerimiento/iniciar-generar-requerimiento
  */
 export const API_POST_FIRMAR = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/autorizar/firmar`;
 
 /**
- * API para obtener oficio resolucion 130118
+ * API para obtener oficio resolucion 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Autorizar-Dictamen/guardar-documento
  */
 export const API_POST_OFICIO_AUTORIZACION = `sat-t${TRAMITE}/solicitud/${IDSOLICITUD}/dictamen/autorizar/oficio-autorizacion/guardar`;
 
 /**
- * API para obtener oficio resolucion rechazado 130118
+ * API para obtener oficio resolucion rechazado 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Autorizar-Dictamen/guardar-documento-rechazado
  */
 export const API_POST_OFICIO_RECHAZADO = `sat-t${TRAMITE}/solicitud/${IDSOLICITUD}/dictamen/autorizar/oficio-rechazado/guardar`;
 
 /**
- * API para guardar observacion 130118
+ * API para guardar observacion 
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130118/swagger-ui/index.html#/Autorizar-Dictamen
  */
 export const API_POST_OBSERVACION_GUARDAR = `sat-t${TRAMITE}/tramite/${NUMFOLIOTRAMITE}/dictamen/observacion/guardar`;
