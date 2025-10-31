98dac29873e17e4c093a79d2f81a5d6f6903dfad
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-10-31 15:32:11 +0530

    Refactor SolitudeComponent imports for improved modularity

--- apps/se/src/app/application/tramites/140103/components/solitude/solitude.component.ts
+++ apps/se/src/app/application/tramites/140103/components/solitude/solitude.component.ts
@@ -1,10 +1,13 @@
+import { CancelacionDeCertificateComponent } from '../cancelacionde/cancelacion-de-certificado.component';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DetalleComponent } from '../detalle/detalle.component';
+import { OficioComponent } from '../oficio/oficio.component';
 
 @Component({
   selector: 'app-solitude',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CancelacionDeCertificateComponent,DetalleComponent,OficioComponent],
   templateUrl: './solitude.component.html',
   styleUrls: ['./solitude.component.scss'],
 })
