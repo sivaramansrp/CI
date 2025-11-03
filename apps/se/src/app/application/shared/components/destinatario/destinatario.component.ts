cb2d9e83828811020896e331855a02b51d149a8c
Rakhi Patil <rakhisagar.patil@ilink-systems.com>
2025-11-03 17:41:31 +0530

    Removed 110222 blocker for testing

--- apps/se/src/app/application/shared/components/destinatario/destinatario.component.ts
+++ apps/se/src/app/application/shared/components/destinatario/destinatario.component.ts
@@ -289,6 +289,7 @@ export class DestinatarioComponent
     });
   }
 
+  /** Obtiene la lista de medios de transporte desde el servicio */
   get paisDestinDestinatario(): Catalogo[]{
     return this.circulacion?.length
       ? this.circulacion
