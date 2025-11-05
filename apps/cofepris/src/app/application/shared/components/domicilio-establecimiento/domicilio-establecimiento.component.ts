48e271ba68cc63d4be31fc1bdfab038f1ce2bdc2
Vigneshwar N <vigneshwar.n@ilink-systems.com>
2025-11-06 01:06:14 +0530

    bugfixes 260

--- apps/cofepris/src/app/application/shared/components/domicilio-establecimiento/domicilio-establecimiento.component.ts
+++ apps/cofepris/src/app/application/shared/components/domicilio-establecimiento/domicilio-establecimiento.component.ts
@@ -826,6 +826,16 @@ export class DomicilioComponent
     this.inicializarEstadoFormulario();
   }
 
+/**
+   * Valida un campo del formulario.
+   *
+   * @param {FormGroup} form - El formulario reactivo.
+   * @param {string} field - El nombre del campo a validar.
+   * @returns {boolean} `true` si el campo es válido, de lo contrario `false`.
+   */
+  isValid(form: FormGroup, field: string): boolean {
+    return this.validacionesService.isValid(form, field) || false;
+  }
   /**
    * @method cerrarModalScian
    * @description Oculta el modal relacionado con el catálogo SCIAN.
