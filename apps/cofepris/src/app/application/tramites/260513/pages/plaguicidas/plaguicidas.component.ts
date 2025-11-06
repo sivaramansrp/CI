3afb5573d64bd2c4b60b8b8f7cbd32c57b6083c1
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-11-06 16:27:40 +0530

    Refactor components for improved structure and clarity

--- apps/cofepris/src/app/application/tramites/260513/pages/plaguicidas/plaguicidas.component.ts
+++ apps/cofepris/src/app/application/tramites/260513/pages/plaguicidas/plaguicidas.component.ts
@@ -59,20 +59,39 @@ export class PlaguicidasComponent {
    *
    * @param e - Objeto que contiene la acción y el valor del botón.
    */
+
+
   getValorIndice(e: AccionBoton): void {
-    if(this.pasoUnoComponent.validOnButtonClick()){
       this.esFormaValido = false;
-    if (e.valor > 0 && e.valor < 5) {
-      this.indice = e.valor;
+      // Validar formularios antes de continuar desde el paso uno
+      if (this.indice === 1 && e.accion === 'cont') {
+        const ISVALID = this.pasoUnoComponent.validOnButtonClick();
+        if (!ISVALID) {
+          this.esFormaValido = true;
+          return; // Detener ejecución si los formularios son inválidos
+        }
+      }
+  
+      // Calcular el nuevo índice basado en la acción
+      let indiceActualizado = e.valor;
       if (e.accion === 'cont') {
-        this.wizardComponent.siguiente();
-      } else {
-        this.wizardComponent.atras();
+        indiceActualizado = e.valor + 1;
+      } else if (e.accion === 'ant') {
+        indiceActualizado = e.valor - 1;
+      }
+  
+      // Validar que el nuevo índice esté dentro de los límites permitidos
+      if (indiceActualizado > 0 && indiceActualizado <= this.pasos.length) {
+  
+        // Actualizar el índice y datosPasos
+        this.indice = indiceActualizado;
+        this.datosPasos.indice = indiceActualizado;
+  
+        if (e.accion === 'cont') {
+          this.wizardComponent.siguiente();
+        } else if (e.accion === 'ant') {
+          this.wizardComponent.atras();
+        }
       }
     }
-    }
-    else{
-      this.esFormaValido = true;
-    }
-  }
 }
