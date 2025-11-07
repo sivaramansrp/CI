a0600f3098149052bc183cabf55edb716a2ade85
binaydas8 <binaykumar.das@ilink-systems.com>
2025-11-07 17:41:12 +0530

    WIP
    WIP

--- apps/cofepris/src/app/application/tramites/260508/components/datos-solicitud/datos-solicitud.component.ts
+++ apps/cofepris/src/app/application/tramites/260508/components/datos-solicitud/datos-solicitud.component.ts
@@ -37,4 +37,10 @@ export class DatosSolicitudComponent {
    * Indica si se debe habilitar el domicilio.
    */
   tieneDomicilioHabilitar: boolean = true;
+  /**
+   * Identificador del procedimiento que se recibe como entrada desde el componente padre.
+   * Este valor se utiliza para cargar datos específicos relacionados con el procedimiento,
+   * como catálogos o listas asociadas.
+   */
+  public idProcedimiento: number = 260508;
 }
