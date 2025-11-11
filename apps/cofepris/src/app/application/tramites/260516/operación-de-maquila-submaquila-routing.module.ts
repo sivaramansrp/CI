488c33df45a674d4a1195ec3749873eabd7bca40
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-11-11 15:49:14 +0530

    Added canactive for all

--- apps/cofepris/src/app/application/tramites/260516/operación-de-maquila-submaquila-routing.module.ts
+++ apps/cofepris/src/app/application/tramites/260516/operación-de-maquila-submaquila-routing.module.ts
@@ -1,18 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
-import { IniciarTramiteResolver } from '@libs/shared/data-access-user/src';
 import { NgModule } from '@angular/core';
 import { PlaguicidasComponent } from './pages/plaguicidas/plaguicidas.component';
 
 const ROUTES: Routes = [
    {
         path: 'plaguicidas',
-        component: PlaguicidasComponent,
-        canActivate: [IniciarTramiteResolver],
-            data: {
-              iniciarConfig: {
-                procedureId: '260516'
-              }
-        },
+        component: PlaguicidasComponent
       },
       {
         path: '',
