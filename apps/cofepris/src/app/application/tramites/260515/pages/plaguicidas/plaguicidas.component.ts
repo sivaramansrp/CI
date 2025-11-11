866c81e25a5862de92205190bba15a48b92c58ba
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-11-11 17:22:05 +0530

    Refactor PlaguicidasComponent for improved code clarity and maintainability

--- apps/cofepris/src/app/application/tramites/260515/pages/plaguicidas/plaguicidas.component.ts
+++ apps/cofepris/src/app/application/tramites/260515/pages/plaguicidas/plaguicidas.component.ts
@@ -1,7 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, EventEmitter, ViewChild } from '@angular/core';
 import {ERROR_FORMA_ALERT, ListaPasosWizard, PASOS } from '@libs/shared/data-access-user/src';
 import { DatosPasos } from '@libs/shared/data-access-user/src/core/models/shared/components.model';
 import { PasoUnoComponent } from '../paso-uno/paso-uno.component';
+import { Tramite260515State } from '../../estados/stores/tramite260515Store.store';
 import { WizardComponent } from '@libs/shared/data-access-user/src/tramites/components/wizard/wizard.component';
 
 /**
@@ -21,6 +22,49 @@ interface AccionBoton {
   templateUrl: './plaguicidas.component.html',
 })
 export class PlaguicidasComponent {
+        /**
+   * @property {boolean} isSaltar
+   * @description
+   * Indica si se debe saltar al paso de firma. Controla la navegación
+   * directa al paso de firma en el wizard.
+   * @default false - No salta por defecto
+   */
+  isSaltar: boolean = false;
+     /**
+    /**
+   * @ignore
+   * Este método es ignorado por Compodoc.
+   */
+  cargaEnProgreso: boolean = true;
+      /**
+   * Indica si la sección de carga de documentos está activa.
+   * Se inicializa en true para mostrar la sección de carga de documentos al inicio.
+   */
+  seccionCargarDocumentos: boolean = true;
+  
+  /**
+   * Indica si el botón para cargar archivos está habilitado.
+   */
+  activarBotonCargaArchivos: boolean = false;
+  
+       /**
+     * Evento que se emite para cargar archivos.
+     * Este evento se utiliza para notificar a otros componentes que se debe realizar una acción de
+     */
+    cargarArchivosEvento = new EventEmitter<void>();
+  
+   /**
+   * Estado de la solicitud actual.
+   *
+   * @type {Tramite260203State}
+   * @memberof SolicitudPageComponent
+   */
+  idTipoTRamite: string = '260515';
+  
+    /**
+     * URL de la página actual.
+     */
+      public solicitudState!: Tramite260515State;
   /**
    * Lista de pasos del asistente.
    * Se obtiene de una constante definida en otro archivo.
@@ -36,6 +80,7 @@ export class PlaguicidasComponent {
    * Se inicializa en 1.
    */
   indice: number = 1;
+  
 
   /**
    * Título del asistente.
@@ -93,5 +138,43 @@ export class PlaguicidasComponent {
           this.wizardComponent.atras();
         }
       }
+  }
+
+  
+  /**
+   * Método para manejar el evento de carga de documentos.
+   * Actualiza el estado del botón de carga de archivos.
+   *  carga - Indica si la carga de documentos está activa o no.
+   * {void} No retorna ningún valor.
+   */
+  manejaEventoCargaDocumentos(carga: boolean): void {
+    this.activarBotonCargaArchivos = carga;
+  }
+    /**
+   * Método para manejar el evento de carga de documentos.
+   * Actualiza el estado de la sección de carga de documentos.
+   *  cargaRealizada - Indica si la carga de documentos se realizó correctamente.
+   * {void} No retorna ningún valor.
+   */
+  cargaRealizada(cargaRealizada: boolean): void {
+    this.seccionCargarDocumentos = cargaRealizada ? false : true;
+  }
+  /**
+   * Maneja el estado de progreso de la carga de documentos.
+   * Actualiza la variable `cargaEnProgreso` según el estado recibido.
+   * @param carga - Indica si la carga está en progreso (`true`) o no (`false`).
+   */
+  onCargaEnProgreso(carga: boolean): void {
+    this.cargaEnProgreso = carga;
+  }
+  /**
+   * @method blancoObligatoria
+   * @description Método para manejar el evento de documentos obligatorios en blanco.
+   * Actualiza la bandera `isSaltar` basada en el estado recibido.
+   * @param {boolean} enBlanco - Indica si hay documentos obligatorios en blanco.
+   * @return {void}
+   */
+  onBlancoObligatoria(enBlanco: boolean): void {
+    this.isSaltar = enBlanco;
   }
 }
