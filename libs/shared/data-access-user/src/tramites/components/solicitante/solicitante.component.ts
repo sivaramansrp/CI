40291ca97d9d8396fd065881f9f66194cb224833
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-11-12 10:59:43 -0600

    ADD Solicitante

--- libs/shared/data-access-user/src/tramites/components/solicitante/solicitante.component.ts
+++ libs/shared/data-access-user/src/tramites/components/solicitante/solicitante.component.ts
@@ -106,7 +106,7 @@ export class SolicitanteComponent implements OnInit, OnDestroy {
    */
   ngOnInit(): void {
      if (this.guardarDatos.id_solicitud && (this.guardarDatos.procedureId === '130118' || this.guardarDatos.procedureId === '5701' 
-      || this.guardarDatos.procedureId === '120301' || this.guardarDatos.procedureId === '110101')) {
+      || this.guardarDatos.procedureId === '120301' || this.guardarDatos.procedureId === '110101' || this.guardarDatos.procedureId === '130120')) {
       this.getDatosSolicitanteEvaluar(this.guardarDatos.id_solicitud);
     } else {
       this.getDatosGenerales(this.RFC);
