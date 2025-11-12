59433622ec36d162b1440921c36813a89a008950
Jamuna_Sri <jamunasri.s@purpleslate.in>
2025-11-12 15:36:33 +0530

    fix

--- apps/cofepris/src/app/application/shared/estados/stores/datos-domicilio-legal.store.ts
+++ apps/cofepris/src/app/application/shared/estados/stores/datos-domicilio-legal.store.ts
@@ -1065,6 +1065,11 @@ export class DatosDomicilioLegalStore extends Store<DatosDomicilioLegalState> {
   }
 
   public setPaisDeProcedenciaDatos(paisDeProcedenciaDatos: string[]): void {
+    this.update((state) => ({
+      ...state,
+      paisDeProcedenciaDatos,
+    }));
+  }
 
   /**
   * Establece el estado de garantiasOfrecidas.
