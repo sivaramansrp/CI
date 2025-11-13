90780515e1f444a6d6d51b0adcc262a14443e9e4
vijayamarnathmv <vijayamarnath.mv@purpleslate.in>
2025-11-13 15:27:37 +0530

    Refactor PlaguicidasComponent for improved code clarity and maintainability

--- apps/cofepris/src/app/application/tramites/260516/pages/plaguicidas/plaguicidas.component.ts
+++ apps/cofepris/src/app/application/tramites/260516/pages/plaguicidas/plaguicidas.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnDestroy, ViewChild } from '@angular/core';
-import { ERROR_FORMA_ALERT,ListaPasosWizard,PASOS,RegistroSolicitudService, esValidObject, getValidDatos, } from '@libs/shared/data-access-user/src';
+import { ERROR_FORMA_ALERT, ListaPasosWizard, PASOS } from '@libs/shared/data-access-user/src';
 import { DatosDomicilioLegalService } from '../../../../shared/services/datos-domicilio-legal.service';
 import { DatosDomicilioLegalState } from '../../../../shared/estados/stores/datos-domicilio-legal.store';
 import { DatosPasos } from '@libs/shared/data-access-user/src/core/models/shared/components.model';
-import { GuardarAdapter_260515 } from '../../../260515/adapters/guardar-payload.adapter';
 import { PagoBancoService } from '../../../../shared/services/pago-banco.service';
 import { PasoUnoComponent } from '../paso-uno/paso-uno.component';
 import { SolicitudPagoBancoState } from '../../../../shared/estados/stores/pago-banco.store';
 import { Subject } from 'rxjs';
-import { ToastrService } from 'ngx-toastr';
-import { Tramite260515Query } from '../../../260515/estados/queries/tramite260515Query.query';
-import { Tramite260515Store } from '../../../260515/estados/stores/tramite260515Store.store';
 import { WizardComponent } from '@libs/shared/data-access-user/src/tramites/components/wizard/wizard.component';
 import { takeUntil } from 'rxjs/operators';
 
@@ -66,10 +62,7 @@ public formErrorAlert = ERROR_FORMA_ALERT;
    * Notificador para destruir observables al destruir el componente.
    */
   private destroyNotifier$: Subject<void> = new Subject();
-  constructor(private datosDomicilioLegalService: DatosDomicilioLegalService,private pagoBancoService:PagoBancoService,
-    private Tramite260515Query:Tramite260515Query,
-  private GuardarAdapter260515: GuardarAdapter_260515,private registroSolicitudService: RegistroSolicitudService, private tramite260515Store:Tramite260515Store, private toastrService: ToastrService,
-  ) {
+  constructor(private datosDomicilioLegalService: DatosDomicilioLegalService,private pagoBancoService:PagoBancoService) {
 
 }
 
@@ -80,98 +73,39 @@ public formErrorAlert = ERROR_FORMA_ALERT;
    * @param e - Objeto que contiene la acción y el valor del botón.
    */
 
- getValorIndice(e: AccionBoton): void {
+  getValorIndice(e: AccionBoton): void {
+      this.esFormaValido = false;
+      // Validar formularios antes de continuar desde el paso uno
+      if (this.indice === 1 && e.accion === 'cont') {
+        const ISVALID = this.solicitante.validOnButtonClick();
+        if (!ISVALID) {
+          this.esFormaValido = true;
+          return; // Detener ejecución si los formularios son inválidos
+        }
+      }
+  
+      // Calcular el nuevo índice basado en la acción
+      let indiceActualizado = e.valor;
       if (e.accion === 'cont') {
-              // let isValid = true;
-
-    //           if (this.indice === 1 && this.pasoUnoComponent) {
-    //           isValid = this.pasoUnoComponent.validarPasoUno();
-    //         }
-
-    //         if(!this.pasoUnoComponent.contenedorDeDatosSolicitudComponent?.validarContenedor() && this.requiresPaymentData) {
-    //             this.confirmarSinPagoDeDerechos = 2;
-    //           }else {
-    //             this.confirmarSinPagoDeDerechos = 3;
-    //           }
-
-    //         if(!this.requiresPaymentData) {
-    //           if(!this.pasoUnoComponent.pagoDeDerechosContenedoraComponent.validarContenedor()){
-    //             this.mostrarAlerta=true;
-    //             this.seleccionarFilaNotificacion = {
-    //               tipoNotificacion: 'alert',
-    //               categoria: 'danger',
-    //               modo: 'action',
-    //               titulo: '',
-    //               mensaje: MENSAJE_DE_VALIDACION,
-    //               cerrar: true,
-    //               tiempoDeEspera: 2000,
-    //               txtBtnAceptar: 'SI',
-    //               txtBtnCancelar: 'NO',
-    //               alineacionBtonoCerrar:'flex-row-reverse'
-    //             }
-    //             setTimeout(() => window.scrollTo({ top: 0, behavior: 'smooth' }), 0);
-    //  } else if(this.pasoUnoComponent.pagoDeDerechosContenedoraComponent.validarContenedor() && !this.pasoUnoComponent.contenedorDeDatosSolicitudComponent?.validarContenedor()) {
-    //             this.confirmarSinPagoDeDerechos = 2;
-    //           } else if(this.pasoUnoComponent.pagoDeDerechosContenedoraComponent.validarContenedor() && this.pasoUnoComponent.contenedorDeDatosSolicitudComponent?.validarContenedor() && !this.pasoUnoComponent.tercerosRelacionadosVistaComponent.validarContenedor()) {
-    //             this.confirmarSinPagoDeDerechos = 3;
-    //           }
-    //       }
-    //         if (!isValid) {
-    //           this.formErrorAlert = this.MENSAJE_DE_ERROR;
-    //           this.esFormaValido = true;
-    //           this.datosPasos.indice = this.indice;
-    //           setTimeout(() => window.scrollTo({ top: 0, behavior: 'smooth' }), 0);
-    //           return;
-    //         }
-            const PAYLOAD = this.GuardarAdapter260515.toFormPayload();
-            let shouldNavigate = false;
-            this.registroSolicitudService.postGuardarDatos('260215', PAYLOAD).subscribe(response => {
-              shouldNavigate = response.codigo === '00';
-              if (!shouldNavigate) {
-                const ERROR_MESSAGE = response.mensaje || 'Error desconocido en la solicitud';
-                // this.formErrorAlert = ContenedorDePasosComponent.generarAlertaDeError(ERROR_MESSAGE);
-                this.esFormaValido = true;
-                this.indice = 1;
-                this.datosPasos.indice = 1;
-                this.wizardComponent.indiceActual = 1;
-                setTimeout(() => window.scrollTo({ top: 0, behavior: 'smooth' }), 0);
-                return;
-              }
-              if(shouldNavigate) {
-                if(esValidObject(response) && esValidObject(response.datos)) {
-                  this.esFormaValido = false;
-                  const DATOS = response.datos as { id_solicitud?: number };
-                  const ID_SOLICITUD = getValidDatos(DATOS.id_solicitud) ? (DATOS.id_solicitud ?? 0) : 0;
-                  this.tramite260515Store.setIdSolicitud(ID_SOLICITUD);
-                }
-                // Calcular el nuevo índice basado en la acción
-                let indiceActualizado = e.valor;
-                if (e.accion === 'cont') {
-                  indiceActualizado = e.valor;
-                }
-                this.toastrService.success(response.mensaje);
-                if (indiceActualizado > 0 && indiceActualizado < 5) {
-                  this.indice = indiceActualizado;
-                  this.datosPasos.indice = indiceActualizado;
-                  if (e.accion === 'cont') {
-                    this.wizardComponent.siguiente();
-                  } else {
-                    this.wizardComponent.atras();
-                  }
-                }
-              } else {
-                this.toastrService.error(response.mensaje);
-              }
-            });
-          }else{
-            this.indice = e.valor;
-            this.datosPasos.indice = this.indice;
-            this.wizardComponent.atras();
-          }
-
+        indiceActualizado = e.valor + 1;
+      } else if (e.accion === 'ant') {
+        indiceActualizado = e.valor - 1;
+      }
+  
+      // Validar que el nuevo índice esté dentro de los límites permitidos
+      if (indiceActualizado > 0 && indiceActualizado <= this.pasos.length) {
+  
+        // Actualizar el índice y datosPasos
+        this.indice = indiceActualizado;
+        this.datosPasos.indice = indiceActualizado;
+  
+        if (e.accion === 'cont') {
+          this.wizardComponent.siguiente();
+        } else if (e.accion === 'ant') {
+          this.wizardComponent.atras();
+        }
+      }
     }
-
-
   
 
   /**
