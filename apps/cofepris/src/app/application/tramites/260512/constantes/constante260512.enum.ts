f723a7477f098d26d66adb074888839fd5d9d77b
Adipatil125 <aditi.patil@ilink-systems.com>
2025-11-14 12:47:52 +0530

    api integration in progress

--- apps/cofepris/src/app/application/tramites/260512/constantes/constante260512.enum.ts
+++ apps/cofepris/src/app/application/tramites/260512/constantes/constante260512.enum.ts
@@ -30,28 +30,40 @@ export const ERROR_FORMA_ALERT =
  * @property {boolean} paisProcedencia - Indica si el país de procedencia es visible.
  */
 export interface ConfiguracionVisibilidad {
-    paisOrigen: boolean;
-    paisFabrica: boolean;
-    paisElaboracion: boolean;
-    paisProveedor: boolean;
-    paisProcedencia: boolean;
-  }
-  /**
-   * Configuración predeterminada de visibilidad para los datos de solicitud.
-   * 
-   * Esta constante define qué campos relacionados con los países son visibles
-   * o no en la configuración inicial de la solicitud.
-   * 
-   * @property paisOrigen - Indica si el país de origen es visible (true) o no (false).
-   * @property paisFabrica - Indica si el país de la fábrica es visible (true) o no (false).
-   * @property paisElaboracion - Indica si el país de elaboración es visible (true) o no (false).
-   * @property paisProveedor - Indica si el país del proveedor es visible (true) o no (false).
-   * @property paisProcedencia - Indica si el país de procedencia es visible (true) o no (false).
-   */
-  export const DEFAULT_CONFIGURACION_VISIBILIDAD: ConfiguracionVisibilidad = {
-    paisOrigen: true,
-    paisFabrica: false,
-    paisElaboracion: false,
-    paisProveedor: false,
-    paisProcedencia: true
-  };
+  paisOrigen: boolean;
+  paisFabrica: boolean;
+  paisElaboracion: boolean;
+  paisProveedor: boolean;
+  paisProcedencia: boolean;
+}
+/**
+ * Configuración predeterminada de visibilidad para los datos de solicitud.
+ * 
+ * Esta constante define qué campos relacionados con los países son visibles
+ * o no en la configuración inicial de la solicitud.
+ * 
+ * @property paisOrigen - Indica si el país de origen es visible (true) o no (false).
+ * @property paisFabrica - Indica si el país de la fábrica es visible (true) o no (false).
+ * @property paisElaboracion - Indica si el país de elaboración es visible (true) o no (false).
+ * @property paisProveedor - Indica si el país del proveedor es visible (true) o no (false).
+ * @property paisProcedencia - Indica si el país de procedencia es visible (true) o no (false).
+ */
+export const DEFAULT_CONFIGURACION_VISIBILIDAD: ConfiguracionVisibilidad = {
+  paisOrigen: true,
+  paisFabrica: false,
+  paisElaboracion: false,
+  paisProveedor: false,
+  paisProcedencia: true
+};
+/**
+ * Mensaje de validación que solicita confirmación al usuario
+ * sobre la ausencia de datos relacionados con el pago de derechos.
+ */
+export const MENSAJE_DE_VALIDACION = `
+<div class="d-flex justify-content-center text-center">
+  <div>
+    <strong>¡Error de registro!</strong>
+    <div>Faltan campos por capturar.</div>
+  </div>
+</div>
+`;
