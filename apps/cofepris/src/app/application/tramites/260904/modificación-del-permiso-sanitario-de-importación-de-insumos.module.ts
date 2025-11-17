f49ec7a4c5fbc3c3e6141f1837e50e98fa4e9586
Mkushwah1001 <madhu.kushwah@ilink-systems.com>
2025-11-17 12:09:52 +0530

    260904 and 260905- bug fixed changes

--- apps/cofepris/src/app/application/tramites/260904/modificación-del-permiso-sanitario-de-importación-de-insumos.module.ts
+++ apps/cofepris/src/app/application/tramites/260904/modificación-del-permiso-sanitario-de-importación-de-insumos.module.ts
@@ -1,22 +1,26 @@
 import { AlertComponent,BtnContinuarComponent } from '@libs/shared/data-access-user/src';
+import { AvisoDePrivacidadComponent } from '../../shared/components/aviso-de-privacidad/aviso-de-privacidad.component';
 import { CommonModule } from '@angular/common';
-import { DatosDeLaSolicitud260904Component } from './components/datos-de-la-solicitud-260904/datos-de-la-solicitud-260904.component';
-import { DomicilioDelEstablecimiento260904Component } from './components/domicilio-del-establecimiento-260904/domicilio-del-establecimiento-260904.component';
+import { DatosDelSolicitudModificacionComponent } from '../../shared/components/datos-del-solicitud-modificacion/datos-del-solicitud-modificacion.component';
+import { ManifiestosComponent } from '../../shared/components/manifiestos-declaraciones/manifiestos-declaraciones.component';
 import { ModPermisoSanitarioImportacion260904Component } from './pages/mod-permiso-sanitario-importacion-260904/mod-permiso-sanitario-importacion-260904.component';
 import { ModificaciónDelPermisoSanitarioDeImportaciónDeInsumosRoutingModule } from './modificación-del-permiso-sanitario-de-importación-de-insumos-routing.module';
 import { NgModule } from '@angular/core';
-import { PagoDeDerechosComponent } from './components/pago-de-derechos/pago-de-derechos.component';
+import { PagoDeDerechosContenedoraComponent} from './components/pago-de-derechos-contenedora/pago-de-derechos-contenedora/pago-de-derechos-contenedora.component';
 import { PasoDosComponent } from './pages/paso-dos/paso-dos.component';
 import { PasoTresComponent } from './pages/paso-tres/paso-tres.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SolicitanteComponent } from '@libs/shared/data-access-user/src';
-import { TercerosRelacionadosVistaComponent } from './components/terceros-relacionados/terceros-relacionados-vista.component';
+import {TercerosRelacionadosVistaComponent} from './components/terceros-relacionados-vista/terceros-relacionados-vista.component';
 import { ToastrService } from 'ngx-toastr';
-import { TramitesAsociadoComponent } from './components/tramites-asociado/tramites-asociado.component';
+import { TramitesAsociadosSeccionComponent } from '../../shared/components/tramites-asociados-seccion/tramites-asociados-seccion.component';
+
 import { WizardComponent } from '@libs/shared/data-access-user/src';
 
 import { InicioSesionService } from '@libs/shared/data-access-user/src/core/services/shared/inicio-sesion/inicio-sesion.service';
 import { PasoUnoComponent } from './pages/paso-uno/paso-uno.component';
+import { RepresentanteLegalComponent } from '../../shared/components/representante-legal/representante-legal.component';
+
 import { SubirDocumentoService } from '@libs/shared/data-access-user/src/core/services/shared/subir-documento/subir-documento.service';
 
 
@@ -34,10 +38,12 @@ import { SubirDocumentoService } from '@libs/shared/data-access-user/src/core/se
     SolicitanteComponent,
     ReactiveFormsModule,
     BtnContinuarComponent,
-    DatosDeLaSolicitud260904Component,
-    DomicilioDelEstablecimiento260904Component,
-    PagoDeDerechosComponent,
-    TramitesAsociadoComponent,
+    DatosDelSolicitudModificacionComponent,
+    PagoDeDerechosContenedoraComponent,
+    TramitesAsociadosSeccionComponent,
+    RepresentanteLegalComponent,
+    ManifiestosComponent,
+    AvisoDePrivacidadComponent,
     TercerosRelacionadosVistaComponent,
     AlertComponent
   ],
