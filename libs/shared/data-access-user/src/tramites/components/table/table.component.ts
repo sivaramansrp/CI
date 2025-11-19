3f836665d0b0b14292df00f78f6d64eb650a2729
Vigneshwar N <vigneshwar.n@ilink-systems.com>
2025-11-19 11:58:36 +0530

    resolve review comments

--- libs/shared/data-access-user/src/tramites/components/table/table.component.ts
+++ libs/shared/data-access-user/src/tramites/components/table/table.component.ts
@@ -26,6 +26,11 @@ import { FormsModule } from '@angular/forms';
   host: {}
 })
 export class TableComponent implements OnInit, OnChanges {
+  /** 
+ * Emite la fila seleccionada cuando el usuario marca una fila en la tabla.
+ * Permite al componente padre conocer exactamente qué registro fue elegido.
+ * No afecta otros componentes que ya usan este <ng-table>, ya que es un evento adicional.
+ */
 @Output() rowSelected = new EventEmitter<TableBodyData>();
 
   /**
