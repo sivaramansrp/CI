95ef048b41487648397e96f28d7a1c63a7c403c0
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-11-19 10:36:20 -0600

    DELETE Codigo coomentado

--- apps/se/src/app/application/tramites/130120/server/api-router.ts
+++ apps/se/src/app/application/tramites/130120/server/api-router.ts
@@ -115,8 +115,6 @@ export const API_POST_CADENA_ORIGINAL = (IDSOLICITUD: string) : string => `sat-t
  */
 export const API_POST_FIRMAR = (IDSOLICITUD: string) : string => `sat-t130120/solicitud/${IDSOLICITUD}/firmar`;
 
-//https://api-v30.cloud-ultrasist.net/api/sat-t130120/swagger-ui/index.html#/Consulta-Solicitudes/solicitud-detalle
-//sat-t130120/tramite/{numFolioTramite}/solicitud/detalle
 /**
  * Api para consultar el detalle de una solicitud por su folio del tramite 120301.
  * @see https://api-v30.cloud-ultrasist.net/api/sat-t130120/swagger-ui/index.html#/Consulta-Solicitudes/solicitud-detalle
