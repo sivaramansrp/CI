dceb01f009aee8ed52b77fd1018e9ffaf004c740
varshakatre15 <varsha.katre@ilink-systems.com>
2025-11-21 11:03:06 +0530

    bugfixes for 260912

--- apps/cofepris/src/app/application/tramites/260912/component/domicilio-del-establecimiento/domicilio-del-establecimiento.component.ts
+++ apps/cofepris/src/app/application/tramites/260912/component/domicilio-del-establecimiento/domicilio-del-establecimiento.component.ts
@@ -1760,25 +1760,5 @@ public markMercanciasTableTouched(): void {
   // O podrías marcar un campo de error en el formulario principal si lo deseas
   this.mercanciasTableTouched = true;
 }
-/**
-   * Valida si el campo de un formulario no contiene errores
-   * @param {AbstractControl} control  : Control del formulario
-   * @param {string} campo  : Nombre del campo a validar, si el control es un FormGroup
-   * @returns {boolean | null} : Retorna true si el campo contiene errores y ha sido tocado, de lo contrario retorna false
-   */
- 
-  public static isValid(control: AbstractControl, campo?: string): boolean | null {
-    if (!control) {
-      return null;
-    }
-    if (control instanceof FormGroup && campo) {
-      const CHILD = control.controls[campo];
-      if (!CHILD) {
-        return null;
-      }
-      return CHILD.errors && CHILD.touched;
-    }
-    return control.errors && control.touched;
-  }
-
-  }
+  
+}
