c2e11de22ca892bb5159de0e9ce421dab24f4d1f
sravanimeegada <sravani.meegada@ilink-systems.com>
2025-11-24 13:35:09 +0530

    api integration changes done

--- apps/se/src/app/application/tramites/130202/estados/tramites/tramites130202.store.ts
+++ apps/se/src/app/application/tramites/130202/estados/tramites/tramites130202.store.ts
@@ -1,35 +1,148 @@
+/**
+ * Tramite130202Store
+ * Este módulo define el `Store` de Akita para gestionar el estado del trámite con ID 130202.
+ * Proporciona métodos para actualizar diferentes partes del estado del trámite, como la fracción arancelaria,
+ * la solicitud, la información del producto, los valores de las partidas de la mercancía, y otros datos relevantes.
+ */
+
 import { Store, StoreConfig } from '@datorama/akita';
 import { Injectable } from '@angular/core';
+
+import { MostrarPartidas } from '@libs/shared/data-access-user/src';
 import { PartidasDeLaMercanciaModelo } from '../../../../shared/models/partidas-de-la-mercancia.model';
 
-/**
- * Estado inicial y configuración del store para el trámite 130202.
- */
+
 export interface Tramite130202State {
-  producto: string;
-  descripcion: string;
-  fraccion: string;
-  cantidad: string;
-  valorPartidaUSD: number;
-  unidadMedida: string;
-  solicitud: string;
-  defaultSelect: string;
-  defaultProducto: string;
-  regimen: string;
-  clasificacion: string;
-  filaSeleccionada: PartidasDeLaMercanciaModelo[];
-  cantidadPartidasDeLaMercancia: string;
-  valorPartidaUSDPartidasDeLaMercancia: string;
-  descripcionPartidasDeLaMercancia: string;
-  valorFacturaUSD: string;
-  bloque: string;
-  usoEspecifico: string;
-  justificacionImportacionExportacion: string;
-  observaciones: string;
-  entidad: string;
-  representacion: string;
-  mostrarTabla: boolean;
+    /**
+   * ID de la solicitud asociada al trámite.
+   */
+  idSolicitud: number;
   /**
+  /**
+   * Producto seleccionado en el formulario.
+   */
+  producto: string;
+
+  /**
+   * Descripción del producto ingresada en el formulario.
+   */
+  descripcion: string;
+
+  /**
+   * Fracción arancelaria seleccionada en el formulario.
+   */
+  fraccion: string;
+
+  /**
+   * Cantidad del producto ingresada en el formulario.
+   */
+  cantidad: string;
+
+  /**
+   * Valor en USD de la partida ingresada en el formulario.
+   */
+  valorPartidaUSD: number;
+
+  /**
+   * Unidad de medida seleccionada en el formulario.
+   */
+  unidadMedida: string;
+
+  /**
+   * Solicitud seleccionada en el formulario.
+   */
+  solicitud: string;
+
+  /**
+   * Valor por defecto para el campo de selección de solicitud.
+   */
+  defaultSelect: string;
+
+  /**
+   * Valor por defecto para el campo de selección de producto.
+   */
+  defaultProducto: string;
+
+  /**
+   * Régimen seleccionado en el formulario.
+   */
+  regimen: string;
+
+  /**
+   * Clasificación del régimen seleccionada en el formulario.
+   */
+  clasificacion: string;
+
+  /**
+   * Lista de filas seleccionadas en la tabla dinámica.
+   */
+  filaSeleccionada: PartidasDeLaMercanciaModelo[];
+
+  /**
+   * Cantidad de partidas de la mercancía ingresada en el formulario.
+   */
+  cantidadPartidasDeLaMercancia: string;
+/**
+   * Formulario para modificar las partidas de la mercancía.
+   */
+  modificarPartidasDelaMercanciaForm: {
+    /** Cantidad de partidas de la mercancía */
+    cantidadPartidasDeLaMercancia: string;
+    /** Valor en USD de las partidas de la mercancía */
+    valorPartidaUSDPartidasDeLaMercancia: string;
+    /** Descripción de las partidas de la mercancía */
+    descripcionPartidasDeLaMercancia: string;
+  };
+  /**
+   * Valor en USD de las partidas de la mercancía ingresado en el formulario.
+   */
+  valorPartidaUSDPartidasDeLaMercancia: string;
+
+  /**
+   * Descripción de las partidas de la mercancía ingresada en el formulario.
+   */
+  descripcionPartidasDeLaMercancia: string;
+
+  /**
+   * Valor de la factura en USD ingresado en el formulario.
+   */
+  valorFacturaUSD: string;
+
+  /**
+   * Bloque seleccionado en el formulario.
+   */
+  bloque: string;
+
+  /**
+   * Uso específico seleccionado en el formulario.
+   */
+  usoEspecifico: string;
+
+  /**
+   * Justificación para la importación/exportación ingresada en el formulario.
+   */
+  justificacionImportacionExportacion: string;
+
+  /**
+   * Observaciones ingresadas en el formulario.
+   */
+  observaciones: string;
+
+  /**
+   * Entidad seleccionada en el formulario.
+   */
+  entidad: string;
+
+  /**
+   * Representación seleccionada en el formulario.
+   */
+  representacion: string;
+
+  /**
+   * Indica si la tabla dinámica debe mostrarse.
+   */
+  mostrarTabla: boolean;
+   /**
      * Datos del cuerpo de la tabla dinámica.
      */
     tableBodyData: PartidasDeLaMercanciaModelo[];
@@ -41,25 +154,46 @@ export interface Tramite130202State {
     Valor total en USD de las partidas de la mercancía.
     */
     valorTotalUSD: string;
+    
+      /**
+       * Lista de partidas a mostrar.
+       */
+      mostrarPartidas: MostrarPartidas[];
+       /**   
+    * Fechas seleccionadas en el formulario.
+    */
+  fechasSeleccionadas: string[];
 }
 
+
 /**
- * Crea el estado inicial del store.
+ * createInitialState
+ * Función que define y retorna el estado inicial para el `Tramite130202State`.
+ * Inicializa todas las propiedades del estado con valores por defecto, como arrays vacíos para las filas seleccionadas,
+ * `false` para mostrar la tabla, y cadenas vacías o cero para los demás campos.
+ *
+ * El estado inicial del trámite 130202.
  */
 export function createInitialState(): Tramite130202State {
   return {
+    idSolicitud: 0,
     filaSeleccionada: [],
-    mostrarTabla: false,
+    mostrarTabla: true,
     solicitud: '',
     fraccion: '',
-    defaultSelect: 'Inicial',
-    producto: '',
+     defaultSelect: 'TISOL.I',
+    producto: 'CONDMER.U',
     descripcion: '',
     cantidad: '',
     valorPartidaUSD: 0,
     unidadMedida: '',
     defaultProducto: 'Nuevo',
     regimen: '',
+     modificarPartidasDelaMercanciaForm: {
+      cantidadPartidasDeLaMercancia: '',
+      valorPartidaUSDPartidasDeLaMercancia: '',
+      descripcionPartidasDeLaMercancia: '',
+    },
     clasificacion: '',
     cantidadPartidasDeLaMercancia: '',
     valorPartidaUSDPartidasDeLaMercancia: '',
@@ -73,18 +207,40 @@ export function createInitialState(): Tramite130202State {
     representacion: '',
     tableBodyData: [],
     cantidadTotal: '',
-    valorTotalUSD: ''
+    valorTotalUSD: '',
+    mostrarPartidas: [],
+    fechasSeleccionadas: [],
+    
+    
+
   };
 }
 
 /**
- * Store para manejar el estado del trámite 130202.
+ * Clase que representa el `Store` de Akita para el trámite 130202.
+ * Contiene el estado actual del trámite y proporciona métodos para actualizar porciones específicas de este estado.
+ * Está decorado con `@Injectable({ providedIn: 'root' })`, lo que significa que Angular lo proveerá como un singleton
+ * en toda la aplicación.
+ *
+ * Inicializa el `Store` llamando al constructor de la clase padre `Store` con el estado inicial definido por `createInitialState()`.
+ *
  */
 @Injectable({ providedIn: 'root' })
 @StoreConfig({ name: 'tramite130202' })
 export class Tramite130202Store extends Store<Tramite130202State> {
   constructor() {
     super(createInitialState());
+  }
+  /**
+   * Guarda el ID de la solicitud en el estado.
+   *
+   * @param idSolicitud - El ID de la solicitud que se va a guardar.
+   */
+  public setIdSolicitud(idSolicitud: number): void {
+    this.update((state) => ({
+      ...state,
+      idSolicitud,
+    }));
   }
   /**
    * Actualiza el estado del store con los valores proporcionados.
@@ -95,5 +251,11 @@ export class Tramite130202Store extends Store<Tramite130202State> {
       ...state,
       ...valores,
     }));
+  } 
+    /**
+    * Restablece el estado de la tienda a su estado inicial.
+    */
+  resetStore(): void {
+    this.reset();
   }
 }
