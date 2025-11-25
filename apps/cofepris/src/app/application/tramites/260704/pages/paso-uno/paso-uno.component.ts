e9812b7432148b4d029412c5b69d16c3231fc34b
darekarharshada2025 <harshada.darekar@ilink-systems.com>
2025-11-25 18:10:29 +0530

    continuar validation fixes

--- apps/cofepris/src/app/application/tramites/260704/pages/paso-uno/paso-uno.component.ts
+++ apps/cofepris/src/app/application/tramites/260704/pages/paso-uno/paso-uno.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ConsultaioQuery, ConsultaioState, FormularioDinamico } from '@ng-mf/data-access-user';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ReplaySubject, map, takeUntil } from 'rxjs';
 import { ConsultaService } from '../../service/consulta.service';
-
+import { DatosdelasolicitudComponent } from '../../../../shared/components/shared2607/datos-del/datos-de-la-solicitud.component';
+import { Solicitud260702Query } from '../../../../shared/estados/queries/shared2607/tramites260702.query';
+import { Solicitud260702Store } from '../../../../shared/estados/stores/shared2607/tramites260702.store';
+ 
 /**
  * Componente que representa el primer paso del trámite.
  *
@@ -17,10 +20,19 @@ import { ConsultaService } from '../../service/consulta.service';
   styles: ``,
 })
 export class PasoUnoComponent implements OnInit, OnDestroy {
-  pasoUnoForm: FormGroup;
- /**
-   * Indica si los datos de respuesta están disponibles.
+  /**
+   * Formulario reactivo que contiene los campos del primer paso del trámite.
    */
+  pasoUnoForm: FormGroup;
+ 
+/**
+ * Referencia al componente hijo "DatosdelasolicitudComponent" para acceder a sus métodos y propiedades.
+ */
+ @ViewChild(DatosdelasolicitudComponent)datosdelasolicitudComponent!: DatosdelasolicitudComponent;
+ /**
+  * Notificador utilizado para cancelar suscripciones al destruir el componente.
+  * Ayuda a prevenir fugas de memoria en flujos observables.
+  */
   private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
    /**
      * Estado de la consulta, utilizado para manejar el estado de la aplicación.
@@ -32,6 +44,8 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
    */
   public esDatosRespuesta: boolean = false;
   
+    /** Indicadores booleanos que validan el estado de los componentdatos de la solicitud */
+  private isDatosDeLaSolicitudComponentValid: boolean = false;
   /**
    * Tipo de persona seleccionada.
    *
@@ -46,6 +60,8 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
   /*** Configuración del formulario dinámico para el domicilio fiscal.*/
   domicilioFiscal: FormularioDinamico[] = [];
 
+    /** Indica si el botón continuar ha sido activado para ejecutar las validaciones del formulario. */
+  @Input() isContinuarTriggered: boolean = false;
   /**
    * Índice de la pestaña actual del asistente.
    */
@@ -62,7 +78,9 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
   constructor(
     private fb: FormBuilder,
     private consultaQuery: ConsultaioQuery,
-    private consulta: ConsultaService
+    private consulta: ConsultaService,
+    public solicitud260704Store:Solicitud260702Store,
+    private solicitud260704Query:Solicitud260702Query
   ) {
     this.pasoUnoForm = this.fb.group({
       folioDeDesistimiento: [''],
@@ -85,7 +103,7 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
     } else {
       this.esDatosRespuesta = true;
     }
-
+  this.solicitud260704Store.setContinuarTriggered(false);
   }
   /**
      * Carga datos desde un archivo JSON y actualiza el store con la información obtenida.
@@ -111,6 +129,34 @@ export class PasoUnoComponent implements OnInit, OnDestroy {
    */
   seleccionaTab(i: number): void {
     this.indice = i;
+  }
+
+   /**
+   * Maneja el cambio de validez del formulario.
+   * 
+   * @param event - Valor booleano que indica si el formulario es válido o no.
+   * Establece el estado de validez del formulario 'datosDelSolicitude' en el store de solicitud260703.
+   */
+  onFormValidityChange(event:boolean):void {
+   this.solicitud260704Store.setFormValidity('datosDelSolicitude', event);
+  }
+  /**
+   * Valida los formularios relacionados con la solicitud actual.
+   * 
+   * Esta función verifica la validez del componente de datos de la solicitud
+   * accediendo al estado actual de `solicitud260703Query` y consultando la propiedad
+   * `formValidity.datosDelSolicitude`. Si la propiedad no está definida, retorna `false`.
+   * 
+   * @returns {boolean} `true` si el formulario de datos de la solicitud es válido, `false` en caso contrario.
+   */
+  public validarFormularios(): boolean { 
+
+     return this.datosdelasolicitudComponent?.validarFormularios() ?? false;
+
+
+    // this.isDatosDeLaSolicitudComponentValid = (
+    //   this.solicitud260704Query.getValue().formValidity?.datosDelSolicitude ) ?? false;
+    // return this.isDatosDeLaSolicitudComponentValid;
   }
 /**
    * Método del ciclo de vida de Angular que se ejecuta cuando el componente es destruido.
