dbc3124033fa68a6f37c6ef5a8d60d41371a9d9e
Isaac Gamaliel Muniz Amaro <igmuniza@desarrollo-ultrasist.com.mx>
2025-11-25 19:15:35 -0600

    ADD Response

---
+++ apps/se/src/app/application/tramites/110102/models/response/tratado-configuracion-response.model.ts
@@ -0,0 +1,91 @@
+/**
+ * Modelo de respuesta tratado configuracion response
+ */
+export interface CriterioConfiguracionResponse {
+    /** Mostrar datos de mercancía */
+    mostrar_datos_mercancia: boolean;
+    
+    /** Mostrar sección de insumos */
+    mostrar_insumos: boolean;
+    
+    /** Mostrar sección de empaques */
+    mostrar_empaques: boolean;
+    
+    /** Mostrar otras instancias */
+    mostrar_otras_instancias: boolean;
+    
+    /** Mostrar otras instancias alianza pacífico */
+    mostrar_otras_instancias_alianza_p: boolean;
+    
+    /** Mostrar tipo método alianza pacífico */
+    mostrar_tipo_metodo_alianza_p: boolean;
+    
+    /** Mostrar juegos y surtidos alianza pacífico */
+    mostrar_juegos_y_surtidos_alianza_p: boolean;
+    
+    /** Mostrar acumulación alianza pacífico */
+    mostrar_acumulacion_alianza_p: boolean;
+    
+    /** Mostrar juegos y surtidos */
+    mostrar_juegos_y_surtidos: boolean;
+    
+    /** Mostrar tipo método */
+    mostrar_tipo_metodo: boolean;
+    
+    /** Mostrar nombre en inglés */
+    mostrar_nombre_ingles: boolean;
+    
+    /** Mostrar precio franco fábrica */
+    mostrar_precio_franco_fabrica: boolean;
+    
+    /** Mostrar clasificación ALADI */
+    mostrar_clasificacion_aladi: boolean;
+    
+    /** Mostrar valor transaccional FOB */
+    mostrar_valor_transaccional_fob: boolean;
+    
+    /** Mostrar exportador autorizado */
+    mostrar_exportador_autorizado: boolean;
+    
+    /** Mostrar procesos mercancía partida 4 */
+    mostrar_procesos_mercancia_par4: boolean;
+    
+    /** Mostrar país de insumo */
+    mostrar_pais_insumo: boolean;
+    
+    /** Mostrar exportador autorizado Japón */
+    mostrar_exportador_autorizado_jpn: boolean;
+    
+    /** Mostrar tipo método Panamá */
+    mostrar_tipo_metodo_panama: boolean;
+    
+    /** Mostrar tipo método no obligatorio Panamá */
+    mostrar_tipo_metodo_no_obligatorio_panama: boolean;
+    
+    /** Mostrar tipo método Panamá Uruguay */
+    mostrar_tipo_metodo_panama_uruguay: boolean;
+    
+    /** Mostrar encabezado Uruguay */
+    mostrar_encabezado_uruguay: boolean;
+    
+    /** Mostrar costo neto FOB */
+    mostrar_costo_neto_fob: boolean;
+    
+    /** Mostrar otras instancias Perú */
+    mostrar_otras_instancias_peru: boolean;
+    
+    /** Mostrar otras instancias Uruguay */
+    mostrar_otras_instancias_uruguay: boolean;
+    
+    /** Mostrar NALADI */
+    mostrar_naladi: boolean;
+    
+    /** Mostrar NALADISA 93 */
+    mostrar_naladisa_93: boolean;
+    
+    /** Mostrar NALADISA 96 */
+    mostrar_naladisa_96: boolean;
+    
+    /** Mostrar NALADISA 02 */
+    mostrar_naladisa_02: boolean;
+}
