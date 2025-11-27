d9f041f51b706bf5ae06a816f7d3fbab70de7f72
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-11-26 21:05:09 -0600

    ADD Interfaz de historial de observaciones

--- libs/shared/data-access-user/src/core/models/shared/iniciar-autorizar-dictamen-response.model.ts
+++ libs/shared/data-access-user/src/core/models/shared/iniciar-autorizar-dictamen-response.model.ts
@@ -60,4 +60,29 @@ export interface IniciarAutorizacionResponse {
     mostrar_calificacion_ue?:boolean | null;
     /** Indica si el dictaminador califica al exportador JPN */
     mostrar_calificacion_jpn?:boolean | null;
+
+    historial_observaciones?: HistorialObservacione[];
+}
+
+export interface HistorialObservacione {
+    /** ID de la observación */
+    id_observacion: number;
+    /** ID del estado de la observación */
+    ide_estado_observacion: string;
+    /** Estado de la observación */
+    estado_observacion: string;
+    /** Observación */
+    observacion: string;
+    /** Clave del usuario */
+    cve_usuario: string;
+    /** Fecha de la observación */
+    fecha_observacion: string;
+    /** Fecha de atención de la observación */
+    fecha_atencion: string;
+    /** ID del dictamen relacionado */
+    id_dictamen: number;
+    /** ID del requerimiento relacionado */
+    id_requerimiento: number;
+    /** ID de la opinión relacionada */
+    id_opinion: number;
 }
