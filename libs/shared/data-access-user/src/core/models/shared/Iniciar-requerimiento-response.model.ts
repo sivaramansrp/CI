b730ee3501e46b087f2eb64bde2cc55f3a74eb2f
Miguel Arturo Rojas Hernández <marojash@desarrollo-ultrasist.com.mx>
2025-11-26 21:05:35 -0600

    ADD Interfaz de documentos para iniciar

--- libs/shared/data-access-user/src/core/models/shared/Iniciar-requerimiento-response.model.ts
+++ libs/shared/data-access-user/src/core/models/shared/Iniciar-requerimiento-response.model.ts
@@ -15,7 +15,7 @@ export interface IniciarRequerimientoResponse {
     /** Indica si requiere documentos específicos */
     requiere_doc_especificos: boolean;
     /** Lista de documentos requeridos */
-    documentos:[];
+    documentos: Documentos[];
     /** Lista de áreas y dependencias disponibles */
     areas_dependencias: AreasDependencia[];
 
@@ -30,3 +30,28 @@ export interface AreasDependencia {
     /** Clave identificadora del área o dependencia */
     clave: string;
 }
+
+/** Documentos */
+export interface Documentos {
+    /** ID del documento solicitud */
+    id_documento_solicitud: number;
+    /** ID del documento */
+    id_documento: number;
+    /** Nombre del documento */
+    nombre: string;
+    /** ID del tipo de documento */
+    id_tipo_documento: number;
+    /** Tipo de documento */
+    tipo_documento: string;
+    /** Estado del documento solicitud */
+    estado_documento_solicitud: string;
+    /** Indica si es requerido */
+    requerido: boolean;
+    /** ID del documento requerimiento */
+    id_documento_requerimiento: null;
+    /** ID del e-document */
+    id_e_document: string;
+    /** UUID del documento */
+    documento_uuid?: string;
+}
+
